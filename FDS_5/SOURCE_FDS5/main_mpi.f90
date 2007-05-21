fd5158ad2958d693f44bd82d75a6cce159a4381a
mcgratta <mcgratta@gmail.com>
2007-05-21 16:03:52 +0000

    Update the compile date
    Former-commit-id: b9fc55937ecb7e23ead30bd596e13896e4d5393c [formerly bee9ad572228335bc8d838b13699f574f8549cb4]
    Former-commit-id: 73b667e20b5962ebf28e7af4ff6fd869f4b3458b

--- FDS_5/SOURCE_FDS5/main_mpi.f90
+++ FDS_5/SOURCE_FDS5/main_mpi.f90
@@ -70,7 +70,7 @@ WALL_CLOCK_START = WALL_CLOCK_TIME()
  
 ! Assign a compilation date (All Nodes)
  
-COMPILE_DATE   = 'May 20, 2007'
+COMPILE_DATE   = 'May 21, 2007'
 VERSION_STRING = '5_RC4+_MPI'
 VERSION_NUMBER = 5.0
  
