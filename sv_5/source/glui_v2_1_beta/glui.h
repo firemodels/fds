2747d2b4fb8b77fbefaaa27162949ba9df701a6b
gforney <gforney@gmail.com>
2007-08-22 19:35:11 +0000

    add "class" to friend keywords to make gcc happy
    Former-commit-id: a1d7fb7885e74a1705aab5425f3afeb7c42fdcd2 [formerly c9f664d48c3cc3bce7a26f35badefabe812e15e0]
    Former-commit-id: 77db126c8114d62d1afc26a8a82bb5bf59b2b15c
    Former-commit-id: 46d2953e7bb411c2397e810a8dde02f9115f73b9

--- sv_5/source/glui_v2_1_beta/glui.h
+++ sv_5/source/glui_v2_1_beta/glui.h
@@ -295,8 +295,8 @@ public:
     parent_node= child_head = child_tail = next_sibling = prev_sibling = NULL;
   }; 
 
-  friend GLUI_Rollout;
-  friend GLUI_Main;
+  friend class GLUI_Rollout;
+  friend class GLUI_Main;
 };
 
 
@@ -559,11 +559,11 @@ protected:
 
   /********** Friend classes *************/
 
-  friend GLUI_Control;
-  friend GLUI_Rotation;
-  friend GLUI_Translation;
-  friend GLUI;
-  friend GLUI_Master_Object;
+  friend class GLUI_Control;
+  friend class GLUI_Rotation;
+  friend class GLUI_Translation;
+  friend class GLUI;
+  friend class GLUI_Master_Object;
 
 
   /********** Misc functions *************/
