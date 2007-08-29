8a7568f534bf08b5bb9458ae587a3dec7a4618b0
mcgratta <mcgratta@gmail.com>
2007-08-29 18:01:27 +0000

    Fix minor bug in assigning file units
    Former-commit-id: b5effc756118d0de3e318a0494541bcab44db07e [formerly 407c2470e89d282c6f6cae7fb0c55d6e326c3884]
    Former-commit-id: d67309431290ae63c818660840fcc5189144a94f
    Former-commit-id: b87dbc90a322baf01d1e7c7ca24b6b4c78f6bfcf

--- FDS_5/SOURCE_FDS5/main_mpi.f90
+++ FDS_5/SOURCE_FDS5/main_mpi.f90
@@ -70,7 +70,7 @@ WALL_CLOCK_START = WALL_CLOCK_TIME()
  
 ! Assign a compilation date (All Nodes)
  
-COMPILE_DATE   = 'August 24, 2007'
+COMPILE_DATE   = 'August 29, 2007'
 VERSION_STRING = '5_RC8+_MPI'
 VERSION_NUMBER = 5.0
 PARALLEL       = .TRUE.
