492b19850ea3d6a0dc31c3d956daa5622b9ce41f
gforney <gforney@gmail.com>
2007-11-10 14:37:35 +0000

    SMV source: GPU shaders source now within Smokeview (and working)
    Former-commit-id: c6aa6d7454f19ab520e57031ca334e3eb52b51e0 [formerly 02e607fc89af442e6c99ba0de55258ac1d8993b1]
    Former-commit-id: bf71a5553b2f5b1cbe58dccb76859aa193a10cd8
    Former-commit-id: 3b4b50aae35d77b49367d9e294faca83b0ec3bf1

--- SMV_5/source/smokeview/shaders.c
+++ SMV_5/source/smokeview/shaders.c
@@ -1,6 +1,6 @@
-// $Date: 2007-10-07 22:08:47 -0400 (Sun, 07 Oct 2007) $ 
-// $Revision: 800 $
-// $Author: gforney $
+// $Date$ 
+// $Revision$
+// $Author$
 
 #include "options.h"
 #ifdef pp_GPU
@@ -15,52 +15,186 @@
 #include "smokeheaders.h"
 
 GLhandleARB v,f,f2,p_smoke, p_slice ;
-
-#define printOpenGLError() printOglError(__FILE__, __LINE__)
-
-/* ------------------ printfOglError ------------------------ */
-
-int printOglError(char *file, int line)
-{
-    //
-    // Returns 1 if an OpenGL error occurred, 0 otherwise.
-    //
-    GLenum glErr;
-    int    retCode = 0;
-
-    glErr = glGetError();
-    while (glErr != GL_NO_ERROR)
-    {
-        printf("glError in file %s @ line %d: %s\n", file, line, gluErrorString(glErr));
-        retCode = 1;
-        glErr = glGetError();
-    }
-    return retCode;
-}
-
-/* ------------------ printfInfoLog ------------------------ */
-
-void printInfoLog(GLhandleARB obj)
-{
-    int infologLength = 0;
-    int charsWritten  = 0;
-    char *infoLog;
-
-	glGetObjectParameterivARB(obj, GL_OBJECT_INFO_LOG_LENGTH_ARB,
-                                         &infologLength);
-
-    if (infologLength > 0)
-    {
-        infoLog = (char *)malloc(infologLength);
-        glGetInfoLogARB(obj, infologLength, &charsWritten, infoLog);
-		printf("%s\n",infoLog);
-        free(infoLog);
-    }
-}
+char *textFileRead(char *fn);
+void printInfoLog(GLhandleARB obj);
 
 /* ------------------ setSmokeShaders ------------------------ */
 
 int setSmokeShaders() {
+  GLint error_code;
+  int nfrag=7;
+  int nvertex=63;
+  int i,j;
+  int fraglen=0, vertexlen=0;
+  GLchar *fragcopy;
+  const GLchar **FragmentShader, **VertexShader;
+
+  const GLchar *FragmentShaderSource2[]={
+    "void main()"
+    "	{"
+    "		gl_FragColor = vec4(0.4,0.4,0.8,1.0);"
+    "	}"
+  };
+  const GLchar *FragmentShaderSource[]={
+    "varying vec4 newcolor;"
+    "uniform sampler2D depthmap;"
+    "uniform float nearPlane;"
+    "uniform float farPlane;"
+    "void main(){"
+    "  gl_FragColor = newcolor;"
+    "}"
+  };
+
+  const GLchar *VertexShaderSource2[]={
+    "void main()"
+    "	{	"
+    "		gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;"
+    "	}"
+  };
+  const GLchar *VertexShaderSource[]={
+    "uniform float aspectratio,normx,normy,normz;"
+    "uniform float eyex,eyey,eyez;"
+    "uniform float fire_red, fire_green, fire_blue, fire_alpha;"
+    "uniform int skip;"
+    "varying vec4 newcolor;"
+    "uniform float hrrcutoff;"
+    "uniform float smoke_shade;"
+    "uniform int smoke3d_thick;"
+    "attribute float hrr, smoke_alpha;"
+    "void main()"
+    "{"
+    "  float bottom,top,alpha,r;"
+    "  float term1, term2, term3, term4;"
+    "  vec3 rel_pos,eyexyz;"
+    "  int drawfire=0;"
+    "  if(hrrcutoff>0.0&&hrr>hrrcutoff){"
+    "    newcolor = vec4(fire_red,fire_green,fire_blue,fire_alpha);"
+    "  }"
+    "  else{"
+    "    eyexyz = vec3(eyex,eyey,eyez);"
+    "    rel_pos=vec3(gl_Vertex)-eyexyz;"
+    "    bottom = abs(rel_pos.x*normx+rel_pos.y*normy+rel_pos.z*normz);"
+    "    top=length(rel_pos);"
+    "    r=aspectratio*top/bottom;"
+    "    alpha=smoke_alpha/256.0;"
+    "    term1 = alpha*r;"
+    "    term2 = -term1*alpha*(r-1.0)/2.0;"
+    "    term3 = -term2*alpha*(r-2.0)/3.0;"
+    "    term4 = -term3*alpha*(r-3.0)/4.0;"
+    "    alpha = term1+term2+term3+term4;"
+    // newcolor.a *= (1.0 - pow(1.0-gl_Color.a,aspectratio*top/bottom));
+    "    if(skip==2){"
+    "      alpha = 2.0*alpha*(1.0-alpha);"
+    "    }"
+    "    else if(skip==3){"
+    "      alpha = 3.0*alpha*(1.0-alpha-alpha*alpha/3.0);"
+    "    }"
+    "    if(smoke3d_thick==1){"
+    "      alpha=alpha/2;"
+    "      }"
+    "    else if (smoke3d_thick==2){"
+    "      alpha=alpha/4;"
+    "    }"
+    "    else if (smoke3d_thick==3){"
+    "      alpha=alpha/8;"
+    "    }"
+    "    else if (smoke3d_thick==4){"
+    "      alpha=alpha/16;"
+    "    }"
+    "    else if (smoke3d_thick==5){"
+    "      alpha=alpha/32;"
+    "    }"
+    "    else if (smoke3d_thick==6){"
+    "      alpha=alpha/64;"
+    "    }"
+    "    else if (smoke3d_thick==7){"
+    "      alpha=alpha/128;"
+    "    }"
+    "    newcolor = vec4(smoke_shade,smoke_shade,smoke_shade,alpha);"
+    "  }"
+    "  gl_Position = ftransform();"
+    "}"
+  };
+
+  FragmentShader=FragmentShaderSource;
+  VertexShader=VertexShaderSource;
+
+
+  v = glCreateShaderObjectARB(GL_VERTEX_SHADER_ARB);
+  f = glCreateShaderObjectARB(GL_FRAGMENT_SHADER_ARB);
+  f2 = glCreateShaderObjectARB(GL_FRAGMENT_SHADER_ARB);
+
+  // vertex shader
+
+  // pass norm to GPU as uniform vec3
+  // pass xyzeyeorig to GPU as uniform vec3
+  // pass aspectratio to GPU as uniform float
+  // pass skip to GPU as uniform int
+
+
+    glShaderSource(v,1, VertexShader,NULL);
+    glCompileShaderARB(v);
+
+    glShaderSource(f, 1, FragmentShader,NULL);
+    glCompileShaderARB(f);
+
+#ifdef _DEBUG
+  printInfoLog(v);
+  printInfoLog(f);
+#endif
+
+  p_smoke = glCreateProgramObjectARB();
+  glAttachObjectARB(p_smoke,v);
+  glAttachObjectARB(p_smoke,f);
+
+  glLinkProgramARB(p_smoke);
+  glGetObjectParameterivARB(p_smoke,GL_OBJECT_LINK_STATUS_ARB,&error_code);
+#ifdef _DEBUG
+  printf("  Smoke shader completion code:");
+  switch (error_code){
+  case GL_INVALID_VALUE:
+    printf(" INVALID VALUE\n");
+    break;
+  case GL_INVALID_OPERATION:
+    printf(" INVALID OPERATION\n");
+    break;
+  case GL_INVALID_ENUM:
+    printf(" INVALID ENUM\n");
+    break;
+  case 0:
+    printf(" Link failed\n");
+    break;
+  case 1:
+    printf(" Link succeeded\n");
+    break;
+  default:
+    printf(" unknown error\n");
+    break;
+  }
+  printInfoLog(p_smoke);
+#endif
+  GPU_hrrcutoff = glGetUniformLocationARB(p_smoke,"hrrcutoff");
+  GPU_hrr = glGetAttribLocationARB(p_smoke,"hrr");
+  GPU_smokealpha = glGetAttribLocationARB(p_smoke,"smoke_alpha");
+  GPU_skip = glGetUniformLocationARB(p_smoke,"skip");
+  GPU_smoke3d_thick = glGetUniformLocationARB(p_smoke,"smoke3d_thick");
+  GPU_smokeshade = glGetUniformLocationARB(p_smoke,"smoke_shade");
+  GPU_firealpha = glGetUniformLocationARB(p_smoke,"fire_alpha");
+  GPU_firered = glGetUniformLocationARB(p_smoke,"fire_red");
+  GPU_firegreen = glGetUniformLocationARB(p_smoke,"fire_green");
+  GPU_fireblue = glGetUniformLocationARB(p_smoke,"fire_blue");
+  GPU_aspectratio = glGetUniformLocationARB(p_smoke,"aspectratio");
+  GPU_normx = glGetUniformLocationARB(p_smoke,"normx");
+  GPU_normy = glGetUniformLocationARB(p_smoke,"normy");
+  GPU_normz = glGetUniformLocationARB(p_smoke,"normz");
+  GPU_eyex = glGetUniformLocationARB(p_smoke,"eyex");
+  GPU_eyey = glGetUniformLocationARB(p_smoke,"eyey");
+  GPU_eyez = glGetUniformLocationARB(p_smoke,"eyez");
+  return error_code;
+
+}
+
+int setSmokeShaders2() {
 
   char *vs = NULL,*fs = NULL;
   const char * vv;
@@ -162,7 +296,6 @@ int setSmokeShaders() {
   return error_code;
 
 }
-
 /* ------------------ useSmokeShaders ------------------------ */
 
 void useSmokeShaders(void){
@@ -258,5 +391,46 @@ void getDepthTexture( void ){
 	sniffErrors("after getDepthTexture");
 }
 
+#define printOpenGLError() printOglError(__FILE__, __LINE__)
+
+/* ------------------ printfOglError ------------------------ */
+
+int printOglError(char *file, int line)
+{
+    //
+    // Returns 1 if an OpenGL error occurred, 0 otherwise.
+    //
+    GLenum glErr;
+    int    retCode = 0;
+
+    glErr = glGetError();
+    while (glErr != GL_NO_ERROR)
+    {
+        printf("glError in file %s @ line %d: %s\n", file, line, gluErrorString(glErr));
+        retCode = 1;
+        glErr = glGetError();
+    }
+    return retCode;
+}
+
+/* ------------------ printfInfoLog ------------------------ */
+
+void printInfoLog(GLhandleARB obj)
+{
+    int infologLength = 0;
+    int charsWritten  = 0;
+    char *infoLog;
+
+	glGetObjectParameterivARB(obj, GL_OBJECT_INFO_LOG_LENGTH_ARB,
+                                         &infologLength);
+
+    if (infologLength > 0)
+    {
+        infoLog = (char *)malloc(infologLength);
+        glGetInfoLogARB(obj, infologLength, &charsWritten, infoLog);
+		printf("%s\n",infoLog);
+        free(infoLog);
+    }
+}
 #endif
 
