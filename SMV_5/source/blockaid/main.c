939b96908dfb676b546eaf771062eab667e5627f
gforney <gforney@gmail.com>
2007-11-16 16:32:51 +0000

    SMV: blockaid source, initial revision

---
+++ SMV_5/source/blockaid/main.c
@@ -0,0 +1,78 @@
+// $Date: 2007-10-09 15:35:36 -0400 (Tue, 09 Oct 2007) $ 
+// $Revision: 824 $
+// $Author: gforney $
+
+#define INMAIN
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "svn_revision.h"
+#include "blockaid.h"
+
+// svn revision character string
+char main_revision[]="$Revision: 824 $";
+
+/* ------------------ main ------------------------ */
+
+int main(int argc, char **argv){
+
+  int i;
+  char *arg, *prog;
+  char *fdsfile=NULL;
+
+  prog=argv[0];
+  fdsfile=NULL;
+  if(argc==1){
+    version();
+    return 1;
+  }
+
+  for(i=1;i<argc;i++){
+    size_t lenarg;
+
+    arg=argv[i];
+    lenarg=strlen(arg);
+    if(arg[0]=='-'&&lenarg>1){
+      switch(arg[1]){
+      case 'b':
+        break;
+      default:
+        usage(prog);
+        return 1;
+      }
+    }
+    else{
+      if(fdsfile==NULL){
+        fdsfile=argv[i];
+      }
+    }
+  }
+
+  // construct smv filename
+  
+  if(fdsfile==NULL){
+    usage(prog);
+    return 1;
+  }
+
+  // make sure smv file name exists
+
+  if(getfileinfo(fdsfile,NULL,NULL)!=0){
+    printf("file: %s does not exist\n",fdsfile);
+    return 1;
+  }
+  startup();
+  readfds(fdsfile);
+
+}
+
+void startup(void){
+  blockaid_first=&ba_first;
+  blockaid_last=&ba_last;
+  blockaid_first->prev=NULL;
+  blockaid_first->next=blockaid_last;
+  blockaid_last->prev=blockaid_first;
+  blockaid_last->next=NULL;
+  nblockaid=0;
+}
---
+++ SMV_5/source/blockaid/svn_revision.h
@@ -0,0 +1,7 @@
+// $Date: 2007-10-10 10:18:07 -0400 (Wed, 10 Oct 2007) $ 
+// $Revision: 826 $
+// $Author: gforney $
+
+extern char assert_revision[];
+extern char main_revision[];
+extern char readfds_revision[];
