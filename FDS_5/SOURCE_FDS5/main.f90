3ee8d3d8fe05173023e1ac0f3feb0d53eee49af2
mcgratta <mcgratta@gmail.com>
2007-12-05 18:28:33 +0000

    FDS Source: Change version number to 5.0.3
    Former-commit-id: 49c18c3831d790ab8d61ba096a7d7a36b4a56b30 [formerly c0215cc66bb461c67e4e1a8eafc30328c45a7ad9]
    Former-commit-id: 230f73e137ad596ea19ab88629e8cdc6355e241a
    Former-commit-id: 4ed41922ef6ef32bde407de228211b1d339a2936

--- FDS_5/SOURCE_FDS5/main.f90
+++ FDS_5/SOURCE_FDS5/main.f90
@@ -47,7 +47,7 @@ WALL_CLOCK_START = WALL_CLOCK_TIME()
  
 ! Assign a compilation date, version number, revision number
 
-WRITE(VERSION_STRING,'(A)') '5.0.2'
+WRITE(VERSION_STRING,'(A)') '5.0.3'
 SERIAL = .TRUE.
 
 IF (INDEX(mainrev,':',BACK=.TRUE.)>0) THEN
