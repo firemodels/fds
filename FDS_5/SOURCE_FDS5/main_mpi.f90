e0b7de6896f1dd5512689b9717edcf75f6f31aef
mcgratta <mcgratta@gmail.com>
2007-12-05 18:28:33 +0000

    FDS Source: Change version number to 5.0.3
    Former-commit-id: 49c18c3831d790ab8d61ba096a7d7a36b4a56b30 [formerly c0215cc66bb461c67e4e1a8eafc30328c45a7ad9]
    Former-commit-id: 230f73e137ad596ea19ab88629e8cdc6355e241a
    Former-commit-id: 4ed41922ef6ef32bde407de228211b1d339a2936

--- FDS_5/SOURCE_FDS5/main_mpi.f90
+++ FDS_5/SOURCE_FDS5/main_mpi.f90
@@ -73,7 +73,7 @@ WALL_CLOCK_START = WALL_CLOCK_TIME()
  
 ! Assign a compilation date (All Nodes)
 
-WRITE(VERSION_STRING,'(A)') '5.0.2'
+WRITE(VERSION_STRING,'(A)') '5.0.3'
 PARALLEL = .TRUE.
 
 IF (INDEX(mainmpirev,':',BACK=.TRUE.)>0) THEN
