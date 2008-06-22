1ac43a9b7c45f9e06e925d0da8465cc02556d2be
gforney <gforney@gmail.com>
2008-06-22 22:15:54 +0000

    SMV: implement LOADISO script command

--- SMV_5/source/smokeview/smokeviewdefs.h
+++ SMV_5/source/smokeview/smokeviewdefs.h
@@ -26,6 +26,7 @@ void _sniffErrors(char *whereat);
 #define SCRIPT_UNLOADALL 109
 #define SCRIPT_LOADBOUNDARY 110
 #define SCRIPT_LOAD3DSMOKE 111
+#define SCRIPT_LOADISO 112
 
 #define SCRIPT_SLICE_FILE 0
 #define SCRIPT_BOUNDARY_FILE 1
