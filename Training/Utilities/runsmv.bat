9d48261d8e17a02dec5c480b80df57c9d4476cb2
gforney <gforney@gmail.com>
2008-07-14 00:23:58 +0000

    Trainer: preliminary scripts to generate SMV figures that run under Windows
    Former-commit-id: 82be0b2e1cfc138c928e70e85911351c0ce49d16 [formerly ff0512bda5a9b6028a3c28571c3f342b4d10ad55]
    Former-commit-id: 5bcf564cedda9e37335d2d4c11b4b2202d497579
    Former-commit-id: 935602e7b1eb8a5753f82c824192771b505d423a

---
+++ Training/Utilities/runsmv.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+set dir=%1
+set infile=%2
+
+set fulldir=%curdir%\..\%dir%
+
+cd %fulldir%
+IF EXIST %infile%.smv (
+smokeview -runscript %infile%
+) ELSE (
+echo %infile%.smv does not exist
+)
+cd %curdir%
