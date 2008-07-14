d15f963f0cba4cd5c2ce08311c55061e7c6efde5
gforney <gforney@gmail.com>
2008-07-14 00:23:58 +0000

    Trainer: preliminary scripts to generate SMV figures that run under Windows

---
+++ Training/Utilities/makefigures.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+set RUNSMV=.\runsmv.bat
+set curdir=%CD%
+
+call %RUNSMV Demonstrations\2Room_Ranch ranch_00 
+call %RUNSMV Demonstrations\2Room_Ranch ranch_01 
+call %RUNSMV Demonstrations\2Room_Ranch ranch_02 
