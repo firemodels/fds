d847f96784fa51631159019df4f7792a2bd60213
gforney <gforney@gmail.com>
2008-07-16 14:07:00 +0000

    Trainer: convert Win-Dos scripts to Dos text format
    Former-commit-id: eba84bb9e62449df2b8e7a912ac286b3c75b03c9 [formerly 714f278796275a6f4cf49f0ccc0db616d1d28ad4]
    Former-commit-id: 802ab534029d3096429898bcfd952896a66e12a9
    Former-commit-id: 4fb63c0f76d47db56e601d83295f66f463863c4d

--- Training/Utilities/makefigures_svn.bat
+++ Training/Utilities/makefigures_svn.bat
@@ -2,6 +2,8 @@
 set RUNSMV=.\runsmv.bat
 set curdir=%CD%
 
-call %RUNSMV Demonstrations\2Room_Ranch ranch_00 
 call %RUNSMV Demonstrations\2Room_Ranch ranch_01 
 call %RUNSMV Demonstrations\2Room_Ranch ranch_02 
+call %RUNSMV Demonstrations\2Room_Ranch ranch_03 
+call %RUNSMV Demonstrations\2Room_Ranch ranch_04 
+
