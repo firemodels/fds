2c98a8f8982fb470bec5621e8777824ca32d04f5
gforney <gforney@gmail.com>
2008-08-09 02:42:46 +0000

    SMV test cases:  add smoke visualization test case  and update visualization batch file

--- Manuals/SMV_5_User_Guide/make_figures.bat
+++ Manuals/SMV_5_User_Guide/make_figures.bat
@@ -1,6 +1,6 @@
 @echo off
 echo creating some figures for the Smokeview User's guide
-cd ..\..\Test_cases\Visualization
 erase scriptfigures\*.png
+cd ..\..\Test_cases\Visualization
 smokeview -runscript thouse5
 smokeview -runscript plume5c
