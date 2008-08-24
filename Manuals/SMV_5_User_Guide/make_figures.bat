a55765afed47d3f94c97f18e40c44a0f8e31adfb
gforney <gforney@gmail.com>
2008-08-24 00:15:12 +0000

    SMV UG:  use scripts to generate clipping, and  texture mapping color figures for SMV UG; update .bat and .csh scripts to generate figures;  get smokeview and smokezip usage text from files
    Former-commit-id: e4015f2112c88965c34aceddecabe2d78067e61d [formerly 54a2807b796a387ef8512fa43eef116e5f7849dc]
    Former-commit-id: 0c0344cadfcb8ad59b7c06be997d1ea2fd728795
    Former-commit-id: 5756fd8ca798e81001d00b418f0e43880c3866b9

--- Manuals/SMV_5_User_Guide/make_figures.bat
+++ Manuals/SMV_5_User_Guide/make_figures.bat
@@ -7,3 +7,5 @@ smokeview -runscript plume5c
 smokeview -runscript smoke_test
 smokeview -runscript smoke_test2
 smokeview -runscript smoke_sensor
+smokeview -runscript sillytexture
+smokeview -runscript plume5a
