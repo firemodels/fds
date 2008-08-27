79eb7e3814744a9054142fae8a093fb77bab2cb8
gforney <gforney@gmail.com>
2008-08-27 15:33:05 +0000

    FDS/SMV UG: remove fds file format info from SMV UG and place it in a separate file in the FDS UG directory

--- Manuals/SMV_5_User_Guide/make_figures.bat
+++ Manuals/SMV_5_User_Guide/make_figures.bat
@@ -3,9 +3,9 @@ echo creating some figures for the Smokeview User's guide
 erase scriptfigures\*.png
 erase scriptfigures\*.help
 erase scriptfigures\*.version
-smokeview -help scriptfigures\smokeview.help
-smokeview -version scriptfigures\smokeview.version
-smokezip -help scriptfigures\smokezip.help
+smokeview -help > scriptfigures\smokeview.help
+smokeview -version > scriptfigures\smokeview.version
+smokezip -help > scriptfigures\smokezip.help
 cd ..\..\Test_cases\Visualization
 smokeview -runscript thouse5
 smokeview -runscript plume5c
