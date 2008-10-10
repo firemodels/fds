ae68c7f57592e60e357482dd5f12d7f1755fcfe9
gforney <gforney@gmail.com>
2008-10-10 14:45:37 +0000

    Trainer: minor edits to training make_figures script
    Former-commit-id: d4f36ee35660ec30eeb8aae4e5b8841d696e0ce3 [formerly 10d9425f9e9efb4952c3b95dafd71ad9cdd579e9]
    Former-commit-id: e71b7823426512781452b9eb4fc569c7941ad63d
    Former-commit-id: c7fa3a362c743fde52f87b17f9e3c177fecf179d

--- Manuals/FDS_SMV_Training_Guide/make_figures.bat
+++ Manuals/FDS_SMV_Training_Guide/make_figures.bat
@@ -1,8 +1,14 @@
 @echo off
 echo creating some figures for the  Fire fighter Training guide
 erase scriptfigures\*.png
-cd ..\..\Training\Demonstrations\2Room_Ranch
+cd z:\Training\Demonstrations\2Room_Ranch
 smokeview -runscript ranch_01
 smokeview -runscript ranch_02
 smokeview -runscript ranch_03
 smokeview -runscript ranch_04
+
+cd z:\Training\MCFRS\MCFRS_Flashover
+smokeview -runscript MCFRS_Flashover_00
+
+cd z:\Training\MFRI\MFRI_Training_Tower
+smokeview -runscript MFRI_Training_Tower_00
