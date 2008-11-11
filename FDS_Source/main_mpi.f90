68a7bdcf4c88929920475eff058b2fafbec12dce
mcgratta <mcgratta@gmail.com>
2008-11-11 19:16:07 +0000

    FDS Source: Change version number to 5.2.4

--- FDS_Source/main_mpi.f90
+++ FDS_Source/main_mpi.f90
@@ -78,7 +78,7 @@ WALL_CLOCK_START = WALL_CLOCK_TIME()
  
 ! Assign a compilation date (All Nodes)
 
-WRITE(VERSION_STRING,'(A)') '5.2.3'
+WRITE(VERSION_STRING,'(A)') '5.2.4'
 PARALLEL = .TRUE.
 
 IF (INDEX(mainmpirev,':',BACK=.TRUE.)>0) THEN
