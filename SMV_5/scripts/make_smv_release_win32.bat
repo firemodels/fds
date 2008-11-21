45db7459b4894a64d947d57d43db5b0faacfc248
gforney <gforney@gmail.com>
2008-11-21 01:44:21 +0000

    SMV: update readme file

--- SMV_5/scripts/make_smv_release_win32.bat
+++ SMV_5/scripts/make_smv_release_win32.bat
@@ -2,11 +2,11 @@
 set version=%1
 REM
 REM This batch file creates a self unarchiving file containing
-REM a test version of smokeview and associated files
+REM a release version of smokeview and associated files
 REM
 REM usage: 
 REM  make_smv_release_win32 version
-REM    where version is of the from test_svn#
+REM    where version is of the from X.Y_svn#
 
 cd ..\for_bundle
 set smvdir=to_google\smv_%version%_win32
@@ -14,10 +14,10 @@ set smvdir=to_google\smv_%version%_win32
 echo
 echo filling distribution directory
 mkdir %smvdir%
-copy smokeview_test.exe %smvdir%\smokeview.exe
+copy smokeview_release.exe %smvdir%\smokeview.exe
 copy smokezip_release.exe %smvdir%\smokezip.exe
 copy devices.svo %smvdir%\.
-copy note.txt %smvdir%\.
+copy readme.html %smvdir%\.
 
 echo
 echo winzipping distribution directory
