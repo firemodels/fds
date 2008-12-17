b0ee1937f57f3e024e0ff9c5fa42899be6d89555
gforney <gforney@gmail.com>
2008-12-17 18:13:28 +0000

    SMV: fix problem with LOADISO script command
    Former-commit-id: fc3fc811a0c19c75234240ab04f1a0725ce796af [formerly 0f16315b4a09dab76436e5cb5fb4d0680255683b]
    Former-commit-id: 241c3c38160b797ddc93699649e903ce15c13159
    Former-commit-id: 5115d6d7c4a7b5212ff87d5f12acfe8b05b46684

--- SMV_5/source/smokeview/readsmv.c
+++ SMV_5/source/smokeview/readsmv.c
@@ -5980,6 +5980,7 @@ int match_upper(char *buffer, const char *key, unsigned int lenkey){
   for(i=0;i<lenkey;i++){
     if(toupper(buffer[i])!=toupper(key[i]))return 0;
   }
+  if(lenbuffer>lenkey&&buffer[lenkey]==':')return 2;
   if(lenbuffer>lenkey&&buffer[lenkey]!=' ')return 0;
   return 1;
 }
