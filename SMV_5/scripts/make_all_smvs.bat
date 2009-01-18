b32171ec328a3cb9ea8f42fc2bdddc1885d29a99
gforney <gforney@gmail.com>
2009-01-18 23:26:09 +0000

    SMV: more smokeview build script udpates
    Former-commit-id: 47f0f539ab9fe26f8b2bb0ef90502fb87594d15b [formerly d7b6bdc02c30f3a83e75018cf500396986a354a5]
    Former-commit-id: 97ba0bd446019d08b5b9727e1efccf999dacd361
    Former-commit-id: e54ab5ce8b01260b12ecd59169368c72bbcb470a

--- SMV_5/scripts/make_all_smvs.bat
+++ SMV_5/scripts/make_all_smvs.bat
@@ -1,5 +1,5 @@
 @echo off
-set version=5.3.5_3003
+set version=5.3.5_3003yyy
 set logon=gforney@acrux.cfr.nist.gov
 
 set scriptdir=FDS-SMV/SMV_5/scripts
@@ -10,9 +10,10 @@ plink %logon% %scriptdir%/make_smvs.csh
 plink %logon% %scriptdir%/make_dists.csh %version%
 pscp %logon%:%bundledir%/smv_%version%_osx.tar.gz ..\for_bundle\to_google\.
 pscp %logon%:%bundledir%/smv_%version%_linux.tar.gz ..\for_bundle\to_google\.
-pscp %logon%:%scriptdir%/make_linux.out ..\for_bundle\to_google\.
-pscp %logon%:%scriptdir%/make_osx.out ..\for_bundle\to_google\.
-
+pscp %logon%:%bundledir%/smv_%version%_linux_64.tar.gz ..\for_bundle\to_google\.
+pscp %logon%:%scriptdir%/make_intel_linux_32.out ..\for_bundle\to_google\.
+pscp %logon%:%scriptdir%/make_intel_linux_64.out ..\for_bundle\to_google\.
+pscp %logon%:%scriptdir%/make_intel_osx_32.out ..\for_bundle\to_google\.
 
 make_smv_release_win32.bat %version%
 pause
