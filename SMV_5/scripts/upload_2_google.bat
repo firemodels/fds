e66c1585d5ceb7f6b1d8b2818ea98a7e19682372
gforney <gforney@gmail.com>
2009-01-24 13:53:14 +0000

    SMV: edits to script to upload smokeview installation files to googlecode
    Former-commit-id: d43a597da523a04fcc7e419f7d411a47f0a16506 [formerly a334e18ed9fa6f98d598087d2a025793bbb1a508]
    Former-commit-id: 4b89a2535defc77b1f13a485161974a12a510104
    Former-commit-id: ada0174dfdbe24cb856fbfc1e977adae398e9b3f

--- SMV_5/scripts/upload_2_google.bat
+++ SMV_5/scripts/upload_2_google.bat
@@ -1,43 +1,72 @@
 @echo off
-set version=5.3.7
+set version=test
 set revision=3110
 
-Rem select level of release to be uploaded by adding/removing Rem's to/from 
-Rem    the following lines
+set release=1
 
-Rem set level=Release-1_Major
-Rem set level=Release-2_Minor
-set level=Release-3_Maintenance
-Rem set level=Release-4_Test
+if not %release% == 1 goto endif_release
+  Rem set level=Release-1_Major
+  Rem set level=Release-2_Minor
+  set level=Release-3_Maintenance
+  set upload_win32=1
+  set upload_linux32=1
+  set upload_osx32=1
+:endif_release
 
+if not %release% == 0 goto endif_test
+  set level=Release-4_Test
+  set upload_win32=1
+  set upload_linux32=0
+  set upload_osx32=0
+:endif_test
 
-Rem - do not edit below except to select which files to be uploaded 
-Rem     (by adding/removing Rem's to/from googlecode_upload.py lines )
+echo Uploading Smokeview %level% version=%version% revision=%revision%
+pause
 
 cd ..\for_bundle\to_google
+set pwdir=d:\bin\
 
-set glabels=Type-Installer,Opsys-Windows,%level%
-set platform=win32
-set summary=smokeview %version% for %platform% (build %revision%)
-set exe=smv_%version%_%revision%_%platform%.exe
-echo googlecode_upload.py -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-rem remove rem on next line to upload to google
-rem googlecode_upload.py -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+Rem --------------- 32 bit Windows ----------------
+if not %upload_win32% == 1 goto endif_win32
+  set glabels=Type-Installer,Opsys-Windows,%level%
+  set dplatform=32 bit Windows
+  set platform=win32
+  set summary=Smokeview %version% for %platform% build %revision%
+  set exe=smv_%version%_%revision%_%platform%.exe
+  echo -----
+  echo Uploading %summary% - %exe%
+Rem  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+Rem       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+:endif_win32
 
-set glabels=Type-Archive,Opsys-Linux,%level%
-set platform=Linux
-set summary=smokeview %version% for %platform% (build %revision%)
-set exe=smv_%version%_%revision%_%platform%.exe
-echo googlecode_upload.py -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-rem remove rem on next line to upload to google
-rem googlecode_upload.py -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+Rem --------------- 32 bit Linux ----------------
 
+if not %upload_linux32% == 1 goto endif_linux32
+  set glabels=Type-Archive,Opsys-Linux,%level%
+  set dplatform=32 bit Linux
+  set platform=linux
+  set summary=Smokeview %version% for %dplatform% (build %revision%)
+  set exe=smv_%version%_%revision%_%platform%.exe
+  echo -----
+  echo Uploading %summary% - %exe%
+Rem  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+Rem       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+:endif_linux32
 
-set glabels=Type-Archive,Opsys-OSX,%level%
-set platform=OSX
-set summary=smokeview %version% for %platform% (build %revision%)
-set exe=smv_%version%_%revision%_%platform%.exe
-echo googlecode_upload.py -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-rem remove rem on next line to upload to google
-rem googlecode_upload.py -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+Rem --------------- 32 bit OSX ----------------
+
+if not %upload_osx32% == 1 goto endif_osx32
+  set glabels=Type-Archive,Opsys-OSX,%level%
+  set dplatform=32 bit OSX
+  set platform=osx
+  set summary=Smokeview %version% for %dplatform% (build %revision%)
+  set exe=smv_%version%_%revision%_%platform%.exe
+  echo -----
+  echo Uploading %summary% - %exe%
+Rem  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+Rem       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+:endif_osx32
+
+echo -----
+echo Uploads complete
 pause
