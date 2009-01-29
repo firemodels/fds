8bf674c84076faedfc9b65ea3a2fc703b8667c0d
gforney <gforney@gmail.com>
2009-01-29 02:39:26 +0000

    SMV: minor edits to Smokeview building scripts
    Former-commit-id: b3d6c137402df9498de76274abf3e96b91731b03 [formerly 8ac3d7533b9ec8aff10d55e01e46d930d19f62c9]
    Former-commit-id: 070f91d83efdd48924f7b4256c5b5033a1b1e95d
    Former-commit-id: 4e7657c0a1f7b70eded0d16487eac7da93ca1b36

--- SMV_5/scripts/MAKEtest.bat
+++ SMV_5/scripts/MAKEtest.bat
@@ -1,4 +1,8 @@
 @echo off
-set version=test_3110
+
+Rem create an installation for a test smokeview
+Rem Put the installation file in ..\for_bundle\to_google
+
+set version=test_3166
 
 make_smv_test_win32 %version%
