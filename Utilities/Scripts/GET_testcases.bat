cec5b53c98eeac9eabb7c6dced0075d391300bc7
gforney <gforney@gmail.com>
2009-02-06 03:56:36 +0000

    FDS: update scripts to retrieve FDS test cases
    Former-commit-id: ebb5ebafa5f64577979801cfbc62b4142ebe39b6 [formerly 47f27509859777964848ea9a1c16c4216070b5ff]
    Former-commit-id: 36a90fd2b89969e07cd12c703f41d9382fe0ecce
    Former-commit-id: d86271979a88600fa18d27f210a3e7678921fb9a

--- Utilities/Scripts/GET_testcases.bat
+++ Utilities/Scripts/GET_testcases.bat
@@ -1,19 +1,19 @@
 @echo off
 
-Rem get FDS-SMV test cases for current revision
+Rem get FDS-SMV test cases for Windows
 
-set revision=3222
 set REPOS=d:\fds-smv
+
+Rem --------- should not need to edit below ----------
+
+set revision=%1
 
 cd %REPOS%\Utilities\Scripts\to_google\
 
 set testdir=FDS_Test_cases_%revision%
 
-svn export https://fds-smv.googlecode.com/svn/trunk/FDS/trunk/Test_cases %testdir%
-cd %testdir%
-wzzip -a -r -P %testdir%.zip *
+svn export https://fds-smv.googlecode.com/svn/trunk/FDS/trunk/Test_cases Test_cases
+wzzip -a -r -P %testdir%.zip Test_cases
 d:\bin\winzip\wzipse32 %testdir%.zip -d "c:\program files\nist\"
-copy %testdir%.exe ..\.
 erase %testdir%.zip
-erase %testdir%.exe
 pause
