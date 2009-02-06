0c3c8ce66adcf0e1617db44eb23179e348f7c8f7
gforney <gforney@gmail.com>
2009-02-06 01:18:04 +0000

    FDS: add batch files to build FDS, create an archive and upload it to googlecode (for 64 bit windows)
    Former-commit-id: ab416c9f855523d4b4f5e9ff21e02b871c3bff75 [formerly e366c96ed4df51a166e612de0ee8eb8595539b38]
    Former-commit-id: 9f79d09acbffc771994a71568008836682990bd2
    Former-commit-id: 34fe6c3a991ad5717e4a582717a7ac1dd7d264d5

---
+++ Utilities/Makefile/Intel_Win_32/MAKE_fds.bat
@@ -0,0 +1,9 @@
+set intelbin=d:\bin
+
+call %intelbin%\iclvars ia32
+call %intelbin%\ifortvars ia32
+
+Rem erase *.obj
+Rem erase *.mod
+make VPATH="../../../FDS_Source" -f ..\makefile intel_win_32
+pause
