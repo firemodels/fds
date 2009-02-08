e7052fb0fbdd9c7b882b901c4e085f388ae34bf9
gforney <gforney@gmail.com>
2009-02-08 23:04:08 +0000

    SMV: create a web page to both build smokeview and upload it to googlecode (@ SMV_5/scripts/readme.html)
    Former-commit-id: 808e5edfd8e3293e4decc81a649b09487daa4294 [formerly 5a4f1ad35a5180ae91152c3fc36777ebd0ed5bf8]
    Former-commit-id: 4773aabb58f228ff07126d06bec7e90ebb625d24
    Former-commit-id: 7a86a7bf00f601d1251b52e0c85853be5a1af935

--- SMV_5/scripts/UPLOADrelease.bat
+++ SMV_5/scripts/UPLOADrelease.bat
@@ -1,18 +1,8 @@
 @echo off
 
-Rem Usage:
-
-Rem  UPLOAD2google release
-
-Rem  where release is 0 (for test version) or 1 (for release version)
-
-Rem Windows batch file to upload Smokeview release or test files to
+Rem Windows batch file to upload Smokeview test files to
 Rem the google download site.  This script assume that the Windows
-Rem batch file, MAKEall_smvs.bat, has already been run.
-
-Rem release is 0 or 1 
-
-set release=%1
+Rem batch file, MAKEtest.bat, has already been run.
 
 set envfile=c:\bin\fds_smv_env.bat
 IF EXIST %envfile% GOTO endif_envexist
@@ -32,30 +22,25 @@ goto:eof
 
 :endif_envexist
 
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\SMV_5\for_bundle\to_google
+
 Rem ----------------------------------------------------------
 Rem should not need to edit any lines below
 
-if not %release% == 1 goto endif_release
-  Rem set level=Release-1_Major
-  Rem set level=Release-2_Minor
-  set level=Release-3_Maintenance
-  set upload_win32=1
-  set upload_linux32=1
-  set upload_osx32=1
-:endif_release
-
-if not %release% == 0 goto endif_test
-  set level=Release-4_Test
-  set upload_win32=1
-  set upload_linux32=0
-  set upload_osx32=0
-:endif_test
+Rem set level=Release-1_Major
+Rem set level=Release-2_Minor
+set level=Release-3_Maintenance
+set upload_win32=1
+set upload_linux32=1
+set upload_osx32=1
 
 echo Uploading Smokeview %level% version=%smv_version% revision=%smv_revision%
 pause
 
-cd ..\for_bundle\to_google
-set pwdir=d:\bin\
+set pwdir=c:\bin\
 
 Rem --------------- 32 bit Windows ----------------
 if not %upload_win32% == 1 goto endif_win32
@@ -64,10 +49,10 @@ if not %upload_win32% == 1 goto endif_win32
   set platform=win32
   set summary=Smokeview %smv_version% for %dplatform% (SVN r%smv_revision%)
   set exe=smv_%smv_version%_%smv_revision%_%platform%.exe
-  echo -----
+  echo.
   echo Uploading %summary% - %exe%
-  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
+       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
 :endif_win32
 
 Rem --------------- 32 bit Linux ----------------
@@ -78,10 +63,10 @@ if not %upload_linux32% == 1 goto endif_linux32
   set platform=linux
   set summary=Smokeview %smv_version% for %dplatform% (SVN r%smv_revision%)
   set exe=smv_%smv_version%_%smv_revision%_%platform%.tar.gz
-  echo -----
+  echo.
   echo Uploading %summary% - %exe%
-  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
+       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
 :endif_linux32
 
 Rem --------------- 32 bit OSX ----------------
@@ -91,13 +76,13 @@ if not %upload_osx32% == 1 goto endif_osx32
   set dplatform=32 bit OSX
   set platform=osx
   set summary=Smokeview %smv_version% for %dplatform% (SVN r%smv_revision%)
-  set exe=smv_%smv_version%_%smv_smv_revision%_%platform%.tar.gz
-  echo -----
+  set exe=smv_%smv_version%_%smv_revision%_%platform%.tar.gz
+  echo.
   echo Uploading %summary% - %exe%
-  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
+       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none  -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
 :endif_osx32
 
-echo -----
+echo.
 echo Uploads complete
 pause
