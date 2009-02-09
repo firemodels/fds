3513de0b268644c22dde8050e99c1a4c2a259700
gforney <gforney@gmail.com>
2009-02-09 14:33:57 +0000

    SMV: update scripts
    Former-commit-id: 8099c4636600f8170ec6ad7a532c28cf0b5b1b55 [formerly 52b247db67bf4fdf348dbaddf60e4d4dd7c6a373]
    Former-commit-id: 8caaad50edb92b8e12ea7d205940218324e8efb0
    Former-commit-id: ab0d949c7dd6f2a730dcbe30620452c47cc7fe7c

--- Utilities/Scripts/GETALL_testcases.bat
+++ Utilities/Scripts/GETALL_testcases.bat
@@ -2,24 +2,33 @@
 
 Rem get test cases for Windows and Linux/OSX 
 
-set revision=3222
-set REPOS=d:\fds-smv
+set envfile=c:\bin\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+cd %svn_root%/Utilities/Scripts
+
 set LREPOS=FDS-SMV
-set logon=gforney@acrux.cfr.nist.gov
 
-Rem -------- should not need to edit below -----------
-
-set scriptdir=%LREPOS%/Utilities/Scripts
-set testdir=FDS_Test_cases_%revision%
+set testdir=FDS_Test_cases_%test_cases_revision%
 
 Rem get test cases for Linux/OSX systems
 
-plink %logon% %scriptdir%/GET_testcases.csh %revision%
-pscp %logon%:%scriptdir%/to_google/%testdir%.tar.gz to_google\.
+plink %svn_logon% %LREPOS%/Utilities/Scripts/get_testcases.csh %test_cases_revision%
+pscp %svn_logon%:%LREPOS%/Utilities/Scripts/to_google/%testdir%.tar.gz to_google\.
 
 
 Rem get test cases for Windows systems
 
-call GET_testcases %revision%
+call %svn_root%\Utilities\Scripts\get_testcases
 
 pause
