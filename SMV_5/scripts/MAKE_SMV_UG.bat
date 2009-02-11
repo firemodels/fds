debf2cbe2b13753b6d7d14748ffe7d40734947f9
gforney <gforney@gmail.com>
2009-02-11 22:46:28 +0000

    SMV scripts: add script links to smokeview build readme.html file to verify smokeview
    Former-commit-id: 9840e6b30ca8cb46aa0128803f96788f66a284fd [formerly 06b036ccb30874e1cfc818bdff20cc764f224e55]
    Former-commit-id: caaf4f4fdfca4c31f6c9a5446de348f3ab6d821c
    Former-commit-id: d08f7e707b134f41e126e3aa98f9d4274e3abbb3

---
+++ SMV_5/scripts/MAKE_SMV_UG.bat
@@ -0,0 +1,27 @@
+@echo off
+
+Rem Windows batch file for creating Smokeview User guide figures
+
+set envfile=c:\bin\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+echo Creating figures for the Smokeview User's guide
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\Manuals\SMV_5_User_Guide
+
+pdflatex SMV_5_User_Guide
+bibtex SMV_5_User_Guide
+pdflatex SMV_5_User_Guide
+pdflatex SMV_5_User_Guide
