a2395ea12412b27d43f00fca62b63a33cb3b52a6
gforney <gforney@gmail.com>
2009-02-11 22:46:28 +0000

    SMV scripts: add script links to smokeview build readme.html file to verify smokeview
    Former-commit-id: 9840e6b30ca8cb46aa0128803f96788f66a284fd [formerly 06b036ccb30874e1cfc818bdff20cc764f224e55]
    Former-commit-id: caaf4f4fdfca4c31f6c9a5446de348f3ab6d821c
    Former-commit-id: d08f7e707b134f41e126e3aa98f9d4274e3abbb3

---
+++ SMV_5/scripts/MAKE_SMV_UG_figures.bat
@@ -0,0 +1,40 @@
+@echo off
+
+Rem Windows batch file for creating Smokeview User guide figures
+
+set envfile=c:\bin\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+echo Creating figures for the Smokeview User's guide
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\Manuals\SMV_5_User_Guide
+
+erase scriptfigures\*.png
+erase scriptfigures\*.help
+erase scriptfigures\*.version
+smokeview -help > scriptfigures\smokeview.help
+smokeview -version > scriptfigures\smokeview.version
+smokezip -help > scriptfigures\smokezip.help
+cd ..\..\Test_cases\Visualization
+smokeview -runscript colorconv
+smokeview -runscript plume5a
+smokeview -runscript plume5b
+smokeview -runscript plume5c
+smokeview -runscript sillytexture
+smokeview -runscript smoke_sensor
+smokeview -runscript smoke_test
+smokeview -runscript smoke_test2
+smokeview -runscript thouse5
+smokeview -runscript script_test
---
+++ SMV_5/scripts/RUN_smv_test_cases.bat
@@ -0,0 +1,36 @@
+@echo off
+
+Rem Batch script to run FDS cases used to test Smokeview
+
+set envfile=c:\bin\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+echo About to run FDS cases used to test Smokeview
+echo   (will take several hours depending on computer speed)
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\Test_cases\Visualization
+
+echo "Press <CTRL> c to abort"
+echo "Press any other key to start FDS cases"
+pause>NUL
+fds5 colorconv
+fds5 plume5a.fds
+fds5 plume5b.fds
+fds5 plume5c.fds
+fds5 sillytexture.fds
+fds5 smoke_sensor.fds
+fds5 smoke_test.fds
+fds5 smoke_test2.fds
+fds5 thouse5.fds
