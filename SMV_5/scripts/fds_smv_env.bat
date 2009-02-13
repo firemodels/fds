7ed2b3c222cb8a9d34920fcdba0e7099f4d02c73
gforney <gforney@gmail.com>
2009-02-13 03:35:29 +0000

    SMV scripts: update fds_smv_smv.bat (environment definition batch file)
    Former-commit-id: b662eea2dd35df9dfac2dd79058629dafdcf91f1 [formerly e20b69e4b4bf8c706068e28e8c81e4bca0304f36]
    Former-commit-id: 7977f0432df85b36369d2979d60dccdde7fa897d
    Former-commit-id: 31dd4bb05ab925bfe7e6f89b03d83378665b70a8

--- SMV_5/scripts/fds_smv_env.bat
+++ SMV_5/scripts/fds_smv_env.bat
@@ -5,13 +5,32 @@ Rem This file contains user customizable settings for the FDS and Smokeview buil
 
 
 Rem ----------------------------------------------------------------------
+Rem ------ settings below changed only once ------------------------------------
+Rem ----------------------------------------------------------------------
+
+Rem repository directory 
+set svn_root=d:\fds-smv
+
+Rem repository hard drive
+set svn_drive=d:
+
+Rem Linux cluster user name
+set linux_username=gforney
+
+Rem google code user name
+set google_username=gforney
+
+Rem linux cluster hostname
+set linux_hostname=acrux.cfr.nist.gov
+
+Rem ----------------------------------------------------------------------
 Rem ------ settings updated updated as repository changes ----------------
 Rem ----------------------------------------------------------------------
 
 Rem define Smokeview version and svn revision info
 
-set smv_version=zzzz
-set smv_revision=qqqq
+set smv_version=5.3.11
+set smv_revision=3279
 
 Rem define FDS version and svn revision info
 
@@ -20,29 +39,11 @@ set fds_revision=zzzz
 
 Rem revision number for Documentation directory
 
-set docs_revision=wwww
+set docs_revision=3235
 
 Rem define revision number for test cases
 
-set test_cases_revision=tttt
-
-Rem ----------------------------------------------------------------------
-Rem ------ settings changed only once ------------------------------------
-Rem ----------------------------------------------------------------------
-
-Rem define svn_root and svn_drive to point to FDS-SMV svn repository
-
-set svn_root=d:\fds-smv
-set svn_drive=d:
-
-Rem set username for Linux cluster and google code
-
-set linux_username=username
-set google_username=username
-
-Rem set linux cluster hostname
-
-set linux_hostname=hostname
+set test_cases_revision=3222a
 
 Rem --------------------------------------
 Rem --- do not edit below ----------------
