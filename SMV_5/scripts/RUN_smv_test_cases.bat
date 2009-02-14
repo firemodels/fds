debe14809c0b5492d6e3f9a600a64f6b3767d6ba
gforney <gforney@gmail.com>
2009-02-14 16:18:16 +0000

    SMV scripts: correct typo in script to run visulation test cases
    Former-commit-id: a5ee675eec132cca91028c025c29dfd2b28332c9 [formerly 62b89d281371ff8ab5f2b544b24672c7aabd7115]
    Former-commit-id: e8a4113d305c5f940e56adbeb8446f8c75c357f6
    Former-commit-id: ef746cac6d80cbbbdb902bbfd80c13a832ffc5ee

--- SMV_5/scripts/RUN_smv_test_cases.bat
+++ SMV_5/scripts/RUN_smv_test_cases.bat
@@ -25,7 +25,7 @@ cd %svn_root%\Test_cases\Visualization
 echo "Press <CTRL> c to abort"
 echo "Press any other key to start FDS cases"
 pause>NUL
-fds5 colorconv
+fds5 colorconv.fds
 fds5 plume5a.fds
 fds5 plume5b.fds
 fds5 plume5c.fds
