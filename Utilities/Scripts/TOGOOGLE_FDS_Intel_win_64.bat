b996737c7f6afb5741a2a356505caf7076aee237
gforney <gforney@gmail.com>
2009-02-17 00:34:30 +0000

    FDS build: edit windows upload scripts and add upload scripts for linux and osx , add upload section for readme file
    Former-commit-id: 45557f877b99e80c6866a73cc5e658e95ed08ddf [formerly 03121f00b5b7fe8edbc7b852a2194b7909455248]
    Former-commit-id: d8cb085ec6c5f47993ba46c4d6434905eaa8c1ae
    Former-commit-id: c82bdb948c56a12f6ffb0fd2ee9a029636cd42f5

--- Utilities/Scripts/TOGOOGLE_FDS_Intel_win_64.bat
+++ Utilities/Scripts/TOGOOGLE_FDS_Intel_win_64.bat
@@ -2,41 +2,46 @@
 
 Rem Windows batch file to upload FDS release to the google download site.
 
-Rem set version and revision to match those set in ZIP_FDS_Intel_win_64.bat
+set envfile=c:\bin\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
 
-set version=5.3.0
-set revision=3193
+:endif_envexist
 
-Rem uncomment 2 of the following 3 lines depending on what type of release is
-Rem being uploaded
+call %envfile%
 
-  Rem set level=Release-1_Major
-  Rem set level=Release-2_Minor
-  set level=Release-3_Maintenance
-
-Rem directory containing googlecode password file (gc.passwd)
-  set pwdir=c:\bin\
-
-Rem -------- should not need to edit any lines below ---------
+%svn_drive%
+cd %svn_root%\Utilities\Scripts\to_google
 
 
-echo Uploading FDS %level% version=%version% revision=%revision%
-pause
-
-set upload_win64=1
-Rem --------------- 64 bit Windows ----------------
-if not %upload_win64% == 1 goto endif_win64
-  set glabels=Type-Installer,Opsys-Windows,%level%
+  set glabels=Type-Installer,Opsys-Windows,%fds_google_level%
   set dplatform=64 bit Windows
   set platform=win64
-  set summary=FDS Executable for %dplatform% (SVN r%revision%)
-  set exe=fds_%version%_%platform%.exe
-  echo -----
-  echo Uploading %summary% - %exe%
-  echo googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-       googlecode_upload.py --passwd-file-dir %pwdir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
-:endif_win64
+  set summary=FDS %fds_version% for %dplatform% (SVN r%fds_revision%)
+  set exe=fds_%fds_version%_%fds_revision%_%platform%.exe
+echo Uploading %exe%
+echo FDS %fds_google_level% version=%fds_version% revision=%fds_revision%
+echo.
+echo press any key to proceed with upload, CTRL c to abort
+pause>NUL
 
-echo -----
+  if not exist %exe% goto abort_upload
+  echo.
+  echo Uploading %summary% - %exe%
+  echo googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+       googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+
+echo.
 echo Uploads complete
+pause
+goto:eof
+
+:abort_upload
+echo %exe% does not exist - upload to google failed
 pause
