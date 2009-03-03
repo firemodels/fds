ac6e4a213f37ea1e0ae4084b52c986059fe686ef
Bryan Klein <bryan@bryanklein.com>
2009-03-03 19:30:53 +0000

    Data Processor: Scatter and Comparison Plotting now working.  Still connecting up the various bits, but very close now.
    Former-commit-id: 01e2d00851acc1a0aa16472138117fc1577999bb [formerly 5ebd28dab8ae19e2c1bcd657d1998fed6f95aafa]
    Former-commit-id: 23428efef8caacce56bc524eb81892095ce96548
    Former-commit-id: 710203e931d83cf058ddd0cf3b2cf630482ab63f

--- Utilities/VnV_Processing/Data_Processor/Data_Processor.py
+++ Utilities/VnV_Processing/Data_Processor/Data_Processor.py
@@ -79,7 +79,6 @@ print "Parsing Configs"
 pr.parse_configs(config_files,diagnostic_level)
 print "Parsing Data Info"
 pr.parse_data_info(config_file_name,data_line_char,diagnostic_level)
-print "Finished Parsing Files"
 
 data_info = pr.read_pickle(config_file_name+'_object.pkl',diagnostic_level)
 print "There are",len(data_info),"data records to process."
@@ -87,60 +86,51 @@ print "There are",len(data_info),"data records to process."
 #Start Processing records in data config file.
 data_sets = pr.collect_data_sets(data_info,data_directory,diagnostic_level)
 
-data_index_records = {}
+data_index_records = {} 
 for key in data_sets:
     data_index_records[key] = {}
-    #print "Find Start/Stop for d1 in", data_info[key]['d1_Filename']
-    data_index_records[key]['d1_index_set'] = pr.find_start_stop_index(data_sets[key][0][0],data_info[key]['d1_Start'],data_info[key]['d1_End'],data_info[key]['d1_Comp_Start'],data_info[key]['d1_Comp_End'],data_info[key]['Scale_Ind'],diagnostic_level)
-    #print "Find Start/Stop for d2 in", data_info[key]['d2_Filename']
-    data_index_records[key]['d2_index_set'] = pr.find_start_stop_index(data_sets[key][1][0],data_info[key]['d2_Start'],data_info[key]['d2_End'],data_info[key]['d2_Comp_Start'],data_info[key]['d2_Comp_End'],data_info[key]['Scale_Ind'],diagnostic_level)
+    data_index_records[key]['d1_index_set'] = pr.find_start_stop_index(data_sets[key][0][0],data_info[key]['d1_Start'],data_info[key]['d1_End'],data_info[key]['d1_Comp_Start'],data_info[key]['d1_Comp_End'],diagnostic_level)
+    data_index_records[key]['d2_index_set'] = pr.find_start_stop_index(data_sets[key][1][0],data_info[key]['d2_Start'],data_info[key]['d2_End'],data_info[key]['d2_Comp_Start'],data_info[key]['d2_Comp_End'],diagnostic_level)
     #print data_index_records[key]['d1_index_set'],",",data_index_records[key]['d2_index_set']
-print "Number of start/stop records",len(data_index_records.keys())
 
-# Create Scatter Plot Data Object
+# Make Comparison Plots.
+for key in data_info:
+    print key, data_info[key]['Plot_Filename']
+    plot.comparison_plot(data_sets[key],data_info[key],data_index_records[key]['d1_index_set'],data_index_records[key]['d2_index_set'],output_directory,diagnostic_level)
+    
+
+# Create Scatter Plot Data Object based on quantities object.
 pr.make_scatter_dict(diagnostic_level)
 
-#This allows the d line Quantity value to be set to 0 when either d1 or d2 data is missing.
-
+# Find metric data for scatter plots and write data to scatter_data_dict object.
 for key in data_info:
     if data_info[key]['Quantity'] == str(0):
         if diagnostic_level >= 2:
-            print "Quantity set to 0, no comparison made."
+            print "Quantity set to 0, no scatter data."
+            #This allows the d line Quantity value to be set to 0 when either d1 or d2 data is missing.
     else:
         d1_metric_start_index = data_index_records[key]['d1_index_set'][2]
         d1_metric_stop_index = data_index_records[key]['d1_index_set'][3]
         d2_metric_start_index = data_index_records[key]['d2_index_set'][2]
         d2_metric_stop_index = data_index_records[key]['d2_index_set'][3]
         
-        #print "Indices:", d1_metric_start_index, d1_metric_stop_index, d2_metric_start_index, d2_metric_stop_index
-        
         if data_info[key]['Metric'] == 'max':
-            #print "Max Calc..."
             for data_index in range(len(data_sets[key][0][1:])):
                 max_results = calc.calc_max(data_sets[key][0][data_index+1][d1_metric_start_index:d1_metric_stop_index],data_sets[key][1][data_index+1][d2_metric_start_index:d2_metric_stop_index],data_info[key]['d1_Initial_Value'],data_info[key]['d2_Initial_Value'],diagnostic_level)
-                #Add Results to Scatter Data Dictionary.
-                #print "Data Index:", data_index
-                #print "Max Results:", max_results[:2]
                 pr.add_group_data_to_scatter_data_dict(data_info[key]['Quantity'],data_info[key]['Group'],max_results[:2],diagnostic_level)
         elif data_info[key]['Metric'] == 'min':
-            #print "Min Calc..."
             for data_index in range(len(data_sets[key][0][1:])):
                 min_results = calc.calc_min(data_sets[key][0][data_index+1][d1_metric_start_index:d1_metric_stop_index],data_sets[key][1][data_index+1][d2_metric_start_index:d2_metric_stop_index],data_info[key]['d1_Initial_Value'],data_info[key]['d2_Initial_Value'],diagnostic_level)
-                #Add Results to Scatter Data Dictionary.
-                #print "Data Index:", data_index
-                #print "Min Results:", min_results[:2]
                 pr.add_group_data_to_scatter_data_dict(data_info[key]['Quantity'],data_info[key]['Group'],min_results[:2],diagnostic_level)
         else:
             print "!!! Metric is undefined in the input file. !!!"
             exit()
 
+# Make Scatter Plots.
 scatter_data_dict = pr.read_pickle('scatter_data_dict_object.pkl',diagnostic_level)
 for quantity_key in scatter_data_dict:
-    print quantity_key
     if scatter_data_dict[quantity_key] != {}:
         plot.scatter_plot(quantity_key,scatter_data_dict[quantity_key],output_directory,diagnostic_level)
-    #scatter_data_dict[quantity_key]
-
 
 
 ## Call stats functions, which write Summary of results to csv file.
@@ -156,65 +146,6 @@ for quantity_key in scatter_data_dict:
 #*DeltaM
 #*Rel Diff
 
-
-## Call Plotter for Comparison Data.
-if diagnostic_level >= 1:
-    print "**** CREATING COMPARISON DATA ****"
-
-d_count = 1
-# 
-# for data_record in group_quantity_data_dicts[2]:
-#     # Each d line, data_record, may contain compound column names from the config file.
-#     if diagnostic_level >= 1:
-#         print "*** #"+str(d_count)+" of "+str(len(group_quantity_data_dicts[2]))+" comparison records. ***"
-#     # Extract relevant portions of comparison data as defined in config file.
-#     comp_data_to_plot = extract_comp_data(group_quantity_data_dicts[2][data_record])
-#     if diagnostic_level >= 3:
-#         print "Comparison Data to Plot:", comp_data_to_plot
-# 
-#     #Seperate d1 and d2 data lists.
-#     d1_plot_data = comp_data_to_plot[0]
-#     d2_plot_data = comp_data_to_plot[1]
-# 
-#     if diagnostic_level >= 3:
-#         print "Data Set 1, Plot Data:", d1_plot_data
-#         print "Data Set 2, Plot Data:", d2_plot_data
-# 
-#     # Create plot for data_record.
-#     if process_set == 1 or process_set == 2:
-#         comparison_plot(group_quantity_data_dicts[2][data_record],d1_plot_data,d2_plot_data)
-#     d_count = d_count + 1
-# 
-# ## Call Plotter for Scatter Data.
-# if process_set == 1 or process_set == 3:
-#     if diagnostic_level >= 1:
-#         print "**** CREATING SCATTER PLOTS ****"
-#     scatter_quantity = 1
-#     scatter_group = 1
-#     temp_scatter_data_list = []
-# 
-#     #Grouping Scatter Data by Quantity
-#     for scatter_plot_record in sorted(group_quantity_data_dicts[1]):
-#         if diagnostic_level >= 3:
-#             print "Scatter Plot:", scatter_plot_record
-#             print "Quantity:", group_quantity_data_dicts[1][scatter_plot_record]['Comparison_Quantities']
-#     
-#         for scatter_data_key in sorted(scatter_data_dict):
-#             split_key = split("~",scatter_data_key)
-#             if split_key[0] == str(scatter_plot_record) and scatter_data_dict[scatter_data_key] != []:
-#                 temp_scatter_data_list.append([split_key[1], scatter_data_dict[scatter_data_key][:2]])
-#             else:
-#                 pass
-# 
-#         combined_scatter_data[scatter_quantity] = temp_scatter_data_list
-#         temp_scatter_data_list = []
-#         scatter_quantity = scatter_quantity + 1
-# 
-#     # Plot Data
-#     if diagnostic_level >= 3:
-#         print "Data to Scatter Plot:", combined_scatter_data
-#     scatter_plot(group_quantity_data_dicts[0],group_quantity_data_dicts[1],combined_scatter_data)
-# 
 
 ## Report Completion.
 if diagnostic_level >= 1:
--- Utilities/VnV_Processing/Data_Processor/plotter.py
+++ Utilities/VnV_Processing/Data_Processor/plotter.py
@@ -34,12 +34,11 @@ def scatter_plot(quantity_id,data_set,output_directory,diagnostic_level):
     output_filename = quantities[quantity_id]["Plot_Filename"]
     legend_loc= quantities[quantity_id]["Key_Position"]
     ind_axis_title = quantities[quantity_id]["Ind_Title"]
-    dep_axis_title = quantities[quantity_id]["Ind_Title"]
+    dep_axis_title = quantities[quantity_id]["Dep_Title"]
     title_position = eval(quantities[quantity_id]["Title_Position"])
     percent_error = int(quantities[quantity_id]["%error"])
-    mask_value = -9999
     
-    print "Plot Title:",title
+    print "Write Scatter Plot:",title
     
     x_data_set = []
     y_data_set = []
@@ -49,10 +48,9 @@ def scatter_plot(quantity_id,data_set,output_directory,diagnostic_level):
     
     # Iterate through data_set and plot by Group_ID.
     for group_id in data_set:
-        #print "Group #:",group_id
         group_title = groups[group_id]["Group_Title"]
         symboltype = styles[int(groups[group_id]["Style_ID"])]['Symbol_Style']
-        colorname = styles[int(groups[group_id]["Style_ID"])]['Fill_Color']
+        symbolcolor = styles[int(groups[group_id]["Style_ID"])]['Fill_Color']
         symbolsize = int(styles[int(groups[group_id]["Style_ID"])]['Symbol_Size'])
         edgecolor = styles[int(groups[group_id]["Style_ID"])]['Edge_Color']
         
@@ -61,8 +59,7 @@ def scatter_plot(quantity_id,data_set,output_directory,diagnostic_level):
         y_data = [x[1] for x in data_set[group_id]]
         x_data_set.append(x_data)
         y_data_set.append(y_data)
-        ax.scatter(x_data, y_data, s=symbolsize, c=colorname, marker=symboltype, edgecolors=edgecolor, label=group_title)
-
+        ax.scatter(x_data, y_data, s=symbolsize, c=symbolcolor, marker=symboltype, edgecolors=edgecolor, label=group_title)
     
     mu_sigma = clc.mu_2sigma(x_data_set,y_data_set)
     mu_val = mu_sigma[0]
@@ -80,8 +77,8 @@ def scatter_plot(quantity_id,data_set,output_directory,diagnostic_level):
     plt.xlabel(ind_axis_title)
     plt.ylabel(dep_axis_title)
     ax.text(plot_max*title_position[0], plot_max*title_position[1], title, horizontalalignment='left') 
-    ax.text(plot_max*(title_position[0]+0.05), plot_max*(title_position[1]-0.05), r'$2\sigma='+"%2.2f" % (sigma_2_val*100)+'\%$')
     ax.text(plot_max*(title_position[0]+0.05), plot_max*(title_position[1]-0.10), r'$\mu='+"%2.2f" % (mu_val*100)+'\%$')
+    ax.text(plot_max*(title_position[0]+0.05), plot_max*(title_position[1]-0.05), r'$2\sigma='+"%2.2f" % (sigma_2_val*100)+'\%$')
     
     if legend_loc != '':
         leg = ax.legend(loc=legend_loc)
@@ -92,93 +89,173 @@ def scatter_plot(quantity_id,data_set,output_directory,diagnostic_level):
             
     plt.show()
     plt.savefig(output_directory+output_filename)
-# 
-# def comparison_plot():
-#     
-# 
-# for pl in range(len(x_data_set)):
-#     if plotTypeVal == 's':
-#         ax.scatter(x_data_set[pl], y_data_to_plot[pl], s=point_size[pl], c=colorname[pl], marker=symboltype[pl], edgecolors='none', label='Data Set '+str(pl))
-#     else:
-#         ax.plot(x_data_set[pl], y_data_to_plot[pl], c=colorname[pl], linestyle=linetype[pl], linewidth=linethickness[pl], label='Data Set '+str(pl))
-# 
-# if plotTypeVal == 's':
-#     mu_sigma = clc.mu2sigma()
-#     mu_val = mu_sigma[0]
-#     sigma_2_val = mu_sigma[1]
-#     mu_max = 
-#     
-#     ax.plot([xmin,xmax],[ymin,ymax], 'k-', linewidth=0.5, label='_nolegend_') # Center Line
-#     muline = ax.plot([xmin,xmax],[ymin,mu_max], 'g:', linewidth=2.0, label='_nolegend_') # Mu Line
-#     sigma2upper = ax.plot([xmin,xmax], [ymin,(mu_max*(1+sigma_2_val))], 'r-.', linewidth=2.0, label='_nolegend_') # Upper 2 Sigma Line
-#     sigma2lower = ax.plot([xmin,xmax], [ymin,(mu_max*(1-sigma_2_val))], 'r-.', linewidth=2.0, label='_nolegend_') # Lower 2 Sigma Line
-#     plt.xlabel('Measured')
-#     plt.ylabel('Predicted')
-#     ax.text(xmax*0.015, ymax*0.94, 'Really Long Scatter Plot Title', horizontalalignment='left') 
-#     ax.text(xmax*0.030, ymax*0.90, r'$2\sigma='+"%2.2f" % (sigma_2_val*100)+'\%$')
-#     ax.text(xmax*0.030, ymax*0.85, r'$\mu='+"%2.2f" % (mu_val*100)+'\%$')
-#     # matplotlib.text.Text instances
-#     if legend_loc != '':
-#         leg = ax.legend(loc=legend_loc)
-# else:
-#     plt.xlabel('Time (s)')
-#     plt.ylabel('Data Values '+r'$^\circ$C')
-#     ax.text(xmax*0.015, ymax*0.94, 'Really Long Scatter Plot Title', horizontalalignment='left') 
-#     ax.grid(False)
-#     if legend_loc != '':
-#         leg = ax.legend(loc=legend_loc)
-# 
-# ax.axis([xmin, xmax, ymin, ymax])
-# if legend_loc != '':
-#     for t in leg.get_texts():
-#         t.set_fontsize('small')    # the legend text fontsize
-# 
-# plt.show()
-# plt.savefig(output_filename)
-# 
-# def test_scatter_plot():
-#     colorname = ['r','g','b']
-#     symboltype = ['o','^','s']
-#     linethickness = [1.0,0.5,1.0]
-#     linetype = [':','-','-']
-#     point_size = [30,20,80]
-#     mask_value = -9999
-#     output_filename = 'tester'
-#     legend_loc='lower right'
-#     x_data_set = [[40.00,60.00,125.00,165.00,245.00],[300.00, 430.00, 500.00, 555.00, 600.00],[620.00, 615.00, 600.00, 653.00, 660.00, 646.00]]
-#     y_data_set = [[50.80,69.23,119.71,167.59,256.20],[356.02, 425.82, 504.18, 553.45, 602.47],[624.12, 624.60, 656.12, 670.82, 668.81, 660.29]]
-#     xmin = 0
-#     xmax = 700
-#     ymin = 0
-#     ymax = 700
-#     fig = plt.figure(figsize=(5,5)) # (w,h) values are size in inches.
-#     ax = fig.add_subplot(111, aspect='equal')
-#     
-# def test_comparison_plot():
-#     colorname = ['r','g','b']
-#     symboltype = ['o','^','s']
-#     linethickness = [1.0,0.5,1.0]
-#     linetype = [':','-','-']
-#     point_size = [30,20,80]
-#     mask_value = -9999
-#     output_filename = 'tester'
-#     legend_loc='lower right'
-#     x_data_set = [[1.00,5.00,10.00,15.00,20.00,50.00,80.00],[10.00, 40.00, 50.00, 55.00, 60.00, 100.00],[1.00, 15.00, 60.00, 65.00, 70.00, 100.00]]
-#     y_data_set = [[50.80,69.23,-9999,167.59,256.20,450.00,600.00],[356.02, 425.82, -9999, 553.45, 602.47, 650.00],[624.12, 624.60, 656.12, 670.82, 668.81, 660.29]]
-#     xmin = 0
-#     xmax = 100
-#     ymin = 0
-#     ymax = 700
-#     
-#     fig = plt.figure(figsize=(6,4)) # (w,h) values are size in inches.
-#     ax = fig.add_subplot(111)
-#     
-#     # Mask out missing or bad data in y_data_set
-#     y_data_to_plot = []
-#     for y_data_subset in y_data_set:
-#         y_values = M.array(y_data_subset)
-#         y_values_masked = M.masked_where(y_values == mask_value , y_values)
-#         y_data_to_plot.append(y_values_masked)
+
+
+def comparison_plot(data_set,data_info,d1_index_set,d2_index_set,output_directory,diagnostic_level):
+    styles = pr.read_pickle("styles_object.pkl",diagnostic_level)
+    
+    title = data_info["Plot_Title"]
+    size = float(data_info["Plot_Width"])
+    output_filename = data_info["Plot_Filename"]
+    legend_loc= data_info["Key_Position"]
+    ind_axis_title = data_info["Ind_Title"]
+    dep_axis_title = data_info["Dep_Title"]
+    
+    xmin = float(data_info['Min_Ind'])
+    xmax = float(data_info['Max_Ind'])
+    ymin = float(data_info['Min_Dep'])
+    ymax = float(data_info['Max_Dep'])
+    
+    #title_position = eval(data_info["Title_Position"])
+    
+    mask_value = -9999.0
+    
+    if data_info['d1_Key'][0] == '[':
+        d1_column_names = eval(data_info['d1_Key'])
+    else:
+        d1_column_names = [data_info['d1_Key']]
+        
+    if data_info['d2_Key'][0] == '[':
+        d2_column_names = eval(data_info['d2_Key'])
+    else:
+        d2_column_names = [data_info['d2_Key']]
+    
+    fig = plt.figure(figsize=(size,size*0.75)) # (w,h) values are size in inches.
+    ax = fig.add_subplot(111)
+    
+    plot_type_function = {'linear':ax.plot,'loglog':ax.loglog,'semilogx':ax.semilogx,'semilogy':ax.semilogy}
+    
+    d1_data = data_set[0]
+    d2_data = data_set[1]
+    d1_x_data = d1_data[0]
+    d2_x_data = d2_data[0]
+    
+    d1_y_data = d1_data[1:]
+    d2_y_data = d2_data[1:]
+    
+    # Mask out data set to -9999.0 in y_data_set
+    d1_y_data_to_plot = []
+    d2_y_data_to_plot = []
+    
+    for d1_y_data_subset in d1_y_data:
+        y_values = M.array(d1_y_data_subset)
+        d1_y_values_masked = M.masked_where(y_values == mask_value , y_values)
+        d1_y_data_to_plot.append(d1_y_values_masked)
+    
+    for d2_y_data_subset in d2_y_data:
+        y_values = M.array(d2_y_data_subset)
+        d2_y_values_masked = M.masked_where(y_values == mask_value , y_values)
+        d2_y_data_to_plot.append(d2_y_values_masked)
+    
+    linecount = 0
+    for d1_y_masked_data in d1_y_data_to_plot:
+        
+        d1_style = eval(data_info["d1_Style"])
+        d1_key = d1_column_names[linecount]
+        symboltype = styles[int(d1_style[linecount])]['Symbol_Style']
+        edgecolor = styles[int(d1_style[linecount])]['Edge_Color']
+        symbolcolor = styles[int(d1_style[linecount])]['Fill_Color']
+        if styles[int(d1_style[linecount])]['Symbol_Size'] == 'none':
+            symbolsize = 'none'
+        else:
+            symbolsize = int(styles[int(d1_style[linecount])]['Symbol_Size'])
+        linecolor = styles[int(d1_style[linecount])]['Line_Color']
+        linestyle = styles[int(d1_style[linecount])]['Line_Style']
+        if styles[int(d1_style[linecount])]['Line_Width'] == 'none':
+            linewidth = 'none'
+        else:
+            linewidth = float(styles[int(d1_style[linecount])]['Line_Width'])
+        
+        plot_type_function[data_info['Plot_Type']](d1_x_data, d1_y_masked_data, c=linecolor, linestyle=linestyle, linewidth=linewidth, label=d1_key)
+        linecount += 1
+        
+    linecount = 0
+    for d2_y_masked_data in d2_y_data_to_plot:
+        
+        d2_style = eval(data_info["d2_Style"])
+        d2_key = d2_column_names[linecount]
+        symboltype = styles[int(d2_style[linecount])]['Symbol_Style']
+        edgecolor = styles[int(d2_style[linecount])]['Edge_Color']
+        symbolcolor = styles[int(d2_style[linecount])]['Fill_Color']
+        if styles[int(d2_style[linecount])]['Symbol_Size'] == 'none':
+            symbolsize = 'none'
+        else:
+            symbolsize = int(styles[int(d2_style[linecount])]['Symbol_Size'])
+        linecolor = styles[int(d2_style[linecount])]['Line_Color']
+        linestyle = styles[int(d2_style[linecount])]['Line_Style']
+        if styles[int(d2_style[linecount])]['Line_Width'] == 'none':
+            linewidth = 'none'
+        else:
+            linewidth = float(styles[int(d2_style[linecount])]['Line_Width'])
+        
+        plot_type_function[data_info['Plot_Type']](d2_x_data, d2_y_masked_data, c=linecolor, linestyle=linestyle, linewidth=linewidth, label=d2_key)
+        linecount += 1
+        
+    plt.xlabel(ind_axis_title)
+    plt.ylabel(dep_axis_title)
+    
+    #ax.text(5.0, 70.0, title, horizontalalignment='left') 
+    
+    ax.grid(False)
+    ax.axis([xmin,xmax,ymin,ymax])
+    ax.text(xmax*0.05, ymax*0.94, title, horizontalalignment='left')
+    
+    if legend_loc != 'none':
+        leg = ax.legend(loc=legend_loc)
+    
+    if legend_loc != 'none':
+            for t in leg.get_texts():
+                t.set_fontsize('small')    # the legend text fontsize
+            
+    plt.show()
+    plt.savefig(output_directory+output_filename)
+
+
+def test_scatter_plot():
+    symbolcolor = ['r','g','b']
+    symboltype = ['o','^','s']
+    linethickness = [1.0,0.5,1.0]
+    linetype = [':','-','-']
+    point_size = [30,20,80]
+    mask_value = -9999
+    output_filename = 'tester'
+    legend_loc='lower right'
+    x_data_set = [[40.00,60.00,125.00,165.00,245.00],[300.00, 430.00, 500.00, 555.00, 600.00],[620.00, 615.00, 600.00, 653.00, 660.00, 646.00]]
+    y_data_set = [[50.80,69.23,119.71,167.59,256.20],[356.02, 425.82, 504.18, 553.45, 602.47],[624.12, 624.60, 656.12, 670.82, 668.81, 660.29]]
+    xmin = 0
+    xmax = 700
+    ymin = 0
+    ymax = 700
+    fig = plt.figure(figsize=(5,5)) # (w,h) values are size in inches.
+    ax = fig.add_subplot(111, aspect='equal')
+
+
+def test_comparison_plot():
+    symbolcolor = ['r','g','b']
+    symboltype = ['o','^','s']
+    linethickness = [1.0,0.5,1.0]
+    linetype = [':','-','-']
+    point_size = [30,20,80]
+    mask_value = -9999
+    output_filename = 'tester'
+    legend_loc='lower right'
+    x_data_set = [[1.00,5.00,10.00,15.00,20.00,50.00,80.00],[10.00, 40.00, 50.00, 55.00, 60.00, 100.00],[1.00, 15.00, 60.00, 65.00, 70.00, 100.00]]
+    y_data_set = [[50.80,69.23,-9999,167.59,256.20,450.00,600.00],[356.02, 425.82, -9999, 553.45, 602.47, 650.00],[624.12, 624.60, 656.12, 670.82, 668.81, 660.29]]
+    xmin = 0
+    xmax = 100
+    ymin = 0
+    ymax = 700
+    
+    fig = plt.figure(figsize=(6,4)) # (w,h) values are size in inches.
+    ax = fig.add_subplot(111)
+    
+    # Mask out missing or bad data in y_data_set
+    y_data_to_plot = []
+    for y_data_subset in y_data_set:
+        y_values = M.array(y_data_subset)
+        y_values_masked = M.masked_where(y_values == mask_value , y_values)
+        y_data_to_plot.append(y_values_masked)
+
 #     
 #     
 # #return(0)
