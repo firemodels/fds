ed0cf1802b12965d1c0ba071c66e93bc86af3246
gforney <gforney@gmail.com>
2009-03-08 15:54:58 +0000

    Trainer: add some flashover trainer plots to python configuration spreadsheet
    Former-commit-id: e5ba3e90d11abd603fa0cd1eff4f479d8873b0b0 [formerly 77ebfabb33ffa4f5b5bcb056e7fd3f27e4cbd5c9]
    Former-commit-id: d804ecd042bf7695d6eb2010e3df3b43f9a74704
    Former-commit-id: 14d1a406c0941a3d6c3c1c564d0f56423fa3fbbc

---
+++ Training/MCFRS/MCFRS_Flashover/Scripts/transpose_mcfrs_flashover_exp.bat
@@ -0,0 +1,5 @@
+cd  ..\Experimental_Data
+Rem transpose -c mcfrs_flashover_exp.cfg mcfrs_flashover_test_01_thcp.csv
+transpose -c mcfrs_flashover_exp_a.cfg mcfrs_flashover_test_03_thcp -o a
+transpose -c mcfrs_flashover_exp_b.cfg mcfrs_flashover_test_03_thcp -o b
+transpose -c mcfrs_flashover_exp_c.cfg mcfrs_flashover_test_03_thcp -o c
