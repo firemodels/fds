3352c02521dbb5482540caa40a4af3421f3a8c83
gforney <gforney@gmail.com>
2009-03-27 13:42:19 +0000

    SMV verification cases: update runfds script to run fire_line.fds case
    Former-commit-id: 07cf5f0c90f35d4562cb250461d0d0194b1372ed [formerly 81f09a6ec8835228c176134a94074f1d2eb2bd9f]
    Former-commit-id: de0045edd0c7f7be5ef79db99da9b91c06918d0d
    Former-commit-id: 2f7d09ba990f6cf6d20d341d2c5c114c9b2a56b7

--- Verification/Visualization/runcases.bat
+++ Verification/Visualization/runcases.bat
@@ -2,7 +2,8 @@
 echo "Press <CTRL> c to abort"
 echo "Press any other key to start FDS cases"
 pause
-fds5 colorconv
+fds5 colorconv.fds
+fds5 fire_line.fds
 fds5 plume5a.fds
 fds5 plume5b.fds
 fds5 plume5c.fds
