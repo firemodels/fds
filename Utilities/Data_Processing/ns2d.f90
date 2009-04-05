cf30cb5c67d1dcd07d9644c97d21f596b105c637
mcgratta <mcgratta@gmail.com>
2009-04-05 20:49:36 +0000

    Add various data processing programs for validation suite post-processing.
    Former-commit-id: c67652df3937b964fd6484fedb731e0ee196f2fb [formerly 8d08e72b41273a8be98e655da7193c6107fff5d8]
    Former-commit-id: 0bad129673128f928798dc2fa716ce49b0af04c0
    Former-commit-id: 6efcf7f3e0116e18818fbf778f62a16f31fcbc41

--- Utilities/Data_Processing/ns2d.f90
+++ Utilities/Data_Processing/ns2d.f90
@@ -1,6 +1,6 @@
 program ns2d
 
-character(30) :: infile(4,2)
+character(30) :: infile(4,2),outfile(4,2)
 real :: t(10000),u(10000,4,2),dx(4),rms(4,2),cnt(4,2),nu(2),pi
 integer :: i,j,k
 
@@ -12,6 +12,15 @@ infile(1,2) = 'ns2d_8_nupt1_devc.csv'
 infile(2,2) = 'ns2d_16_nupt1_devc.csv'
 infile(3,2) = 'ns2d_32_nupt1_devc.csv'
 infile(4,2) = 'ns2d_64_nupt1_devc.csv'
+
+outfile(1,1) = 'ns2d_8_exact.csv'
+outfile(2,1) = 'ns2d_16_exact.csv'
+outfile(3,1) = 'ns2d_32_exact.csv'
+outfile(4,1) = 'ns2d_64_exact.csv'
+outfile(1,2) = 'ns2d_8_nupt1_exact.csv'
+outfile(2,2) = 'ns2d_16_nupt1_exact.csv'
+outfile(3,2) = 'ns2d_32_nupt1_exact.csv'
+outfile(4,2) = 'ns2d_64_nupt1_exact.csv'
 
 nu(1) = 0.0
 nu(2) = 0.1
@@ -28,6 +37,8 @@ dx(4) = 2.*pi/64.
 case_loop: do k=1,2
    resolution_loop: do j=1,4
       open(10,file=infile(j,k),form='formatted',status='old')
+      open(11,file=outfile(j,k),form='formatted',status='replace')
+      write(11,'(a)') 'Time,u-vel'
       read(10,*)
       read(10,*)
       rms(j,k) = 0.
@@ -35,11 +46,13 @@ case_loop: do k=1,2
       time_loop: do i=1,10000
          read(10,*,end=20) t(i),u(i,j,k)
          u_exact = 1. - 2.*cos(x-t(i))*sin(y-0.5*dx(j)-t(i))*exp(-2.*nu(k)*t(i))
+         write(11,"(f8.3,',',f9.5)") t(i),u_exact
          rms(j,k) = rms(j,k) + (u(i,j,k)-u_exact)**2
          cnt(j,k) = cnt(j,k) + 1.
       enddo time_loop
    20 continue
       close(10)
+      close(11)
       rms(j,k) = sqrt(rms(j,k)/cnt(j,k))
    enddo resolution_loop
 enddo case_loop
