b2a4fd95b449569550df14701fef615e100051eb
gforney <gforney@gmail.com>
2009-04-08 13:16:09 +0000

    build scripts: convert windows compile scripts to new directory structure
    Former-commit-id: 17073ad5121f4d610a131c884d0143bb3fbafad3 [formerly 133d6a90182564e03032b7aa083310753a019056]
    Former-commit-id: 0dd2e603dcd971f4a030a98a05c078bc0b6c5143
    Former-commit-id: 27bbf308310415bf016339b3814630ba99e67d01

---
+++ FDS_Compilation/Scripts/MAKE_FDS_Intel_win_mpi32.bat
@@ -0,0 +1,36 @@
+@echo off
+Title Building Parallel FDS for 32 bit Windows
+
+Rem Batch file used to build a 32 bit version of FDS
+
+set envfile=%homedrive%\%homepath%\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+Rem location of batch files used to set up Intel compilation environment
+
+set intelbin=c:\bin
+
+call %intelbin%\iclvars ia32
+call %intelbin%\ifortvars ia32
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\FDS_Compilation\mpi_intel_win_32
+
+set out=mpi_intel_win_32.out
+echo. | tee -a %out%
+date /t | tee -a  %out%
+time /t | tee -a  %out%
+make VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_32 | tee -a %out%
+
+pause
