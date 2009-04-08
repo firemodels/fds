e6fda8c421f5ba57e2312408c80e018ed659ab0b
gforney <gforney@gmail.com>
2009-04-08 16:11:24 +0000

    build scripts: add ZIP/archive scripts, add bundle info text files
    Former-commit-id: 6803df4039ccbce7fe819383d1ea3851e199460f [formerly ab4c7b48124ed4acc342c978f9329b96b1071480]
    Former-commit-id: a0e4b49522c9c1668c3787ddeb7e3f4ba0161e70
    Former-commit-id: 684fd252703fdb06e1463957f63834a261f962dc

---
+++ FDS_Compilation/Scripts/ZIP_FDS_Intel_OSX_32.bat
@@ -0,0 +1,47 @@
+@echo off
+
+Rem Batch file used to create a self-extracting archive containing FDS
+
+set envfile=%homedrive%\%homepath%\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\FDS_Compilation
+
+set fdsroot=fds_%fds_version%_%fds_revision%_OSX32
+set togoogle=%svn_root%\FDS_Compilation\to_google
+set fdsrootdir=%togoogle%\%fdsroot%
+set scriptdir=%linux_svn_root%/FDS_Compilation/Scripts
+
+IF EXIST %fdsrootdir% rmdir /S /Q %fdsrootdir%
+mkdir %fdsrootdir%
+
+echo.
+echo moving 32 bit OSX fds files to an interim directory
+plink %svn_logon% %scriptdir%/bundle_osx_32.csh %linux_svn_root% %linux_svn_root%
+
+echo.
+echo downloading 32 bit OSX fds files
+pscp %svn_logon%:%scriptdir%/../to_google/fds5_intel_osx_32 %fdsrootdir%\fds5_intel_osx_32
+pscp %svn_logon%:%scriptdir%/../to_google/fds5_mpi_intel_osx_32 %fdsrootdir%\fds5_mpi_intel_osx_32
+
+echo.
+echo winzipping distribution directory
+cd %fdsrootdir%
+if exist ..\%fdsroot%.zip erase ..\%fdsroot%.zip
+wzzip -a -r -P ..\%fdsroot%.zip *
+cd ..
+
+echo %fdsroot%.zip located in %togoogle%
+pause
