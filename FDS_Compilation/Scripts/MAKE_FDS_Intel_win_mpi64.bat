254ace0f91a1fcf51289a780cdc4353e2656306c
gforney <gforney@gmail.com>
2009-04-08 18:08:14 +0000

    build scripts: fix typo, add pause
    Former-commit-id: 68b02d54beb32d6a135afd759f99fc920d771f64 [formerly 599345511acfaa8b6e3df19f85cf422f8b28357d]
    Former-commit-id: db53bdc145d8fea6b5a11fcfa72cf3b863e2b55c
    Former-commit-id: 7ad03c9af8f4b0c69c960514683c7672d1b44919

--- FDS_Compilation/Scripts/MAKE_FDS_Intel_win_mpi64.bat
+++ FDS_Compilation/Scripts/MAKE_FDS_Intel_win_mpi64.bat
@@ -29,3 +29,5 @@ date /t | tee -a  %out%
 time /t | tee -a  %out%
 make VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_64 | tee -a %out%
 pscp %out% %svn_logon%:%linux_svn_root%/FDS_Compilation/mpi_intel_win_64/mpi_intel_win_64.out
+
+pause
