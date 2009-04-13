7d1893d4ea34dff38e619c85f81c4dbf76f3d2e7
mcgratta <mcgratta@gmail.com>
2009-04-13 11:36:21 +0000

    Utilities: Remove old makefile directory and old python data processing directory
    Former-commit-id: 5449a3ac0a647c1bc6824d6828e91497273a5159 [formerly 3c254510ec0829a24be9f762eed2935bec820ce9]
    Former-commit-id: 38bb7b701cbbccb8da394f6a8601b11004850d0b
    Former-commit-id: 597d28050b2084a41bb39cc8ab0b360d37608f7f

--- Utilities/Makefile/Intel_Win_32/MAKE_fds.bat
+++
@@ -1,9 +0,0 @@
-set intelbin=c:\bin
-
-call %intelbin%\iclvars ia32
-call %intelbin%\ifortvars ia32
-
-Rem erase *.obj
-Rem erase *.mod
-make VPATH="../../../FDS_Source" -f ..\makefile intel_win_32
-pause
--- Utilities/Makefile/Intel_Win_64/MAKE_fds.bat
+++
@@ -1,9 +0,0 @@
-set intelbin=c:\bin
-
-call %intelbin%\iclvars intel64
-call %intelbin%\ifortvars intel64
-
-Rem erase *.obj
-Rem erase *.mod
-%intelbin%\make VPATH="../../../FDS_Source" -f ..\makefile intel_win_64
-pause
