67a96bfad89e719a0ce66c79f4470e23baabf509
gforney <gforney@gmail.com>
2009-04-15 19:28:36 +0000

    build scripts: simplify setup of compilers in startup web page
    Former-commit-id: d7445365dfc01dd39fa835b9ee2c1e0e76820e6e [formerly e38e9e0ec06052e7a822b9b75b8eef2ee611d935]
    Former-commit-id: 9ccd62528ce88ac7ac1891eaf0b31e16bd677e66
    Former-commit-id: 6ba72b54b9c5e96b6b7fd36b9abcb4556394074e

---
+++ FDS_Compilation/Scripts/COPY_clvars.bat
@@ -0,0 +1,7 @@
+@echo off
+echo copying clvars.bat to c:\bin
+echo press any key to proceed or ^<CTRL^> C to abort
+pause > NUL
+copy /-Y clvars c:\bin
+echo copy complete.  press any key to finish.
+pause > NUL
