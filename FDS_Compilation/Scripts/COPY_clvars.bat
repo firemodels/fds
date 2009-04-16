f17093d89597fe7da286390593c9c49c4a5cbb85
mcgratta <mcgratta@gmail.com>
2009-04-16 21:36:11 +0000

    Minor fix to a script

--- FDS_Compilation/Scripts/COPY_clvars.bat
+++ FDS_Compilation/Scripts/COPY_clvars.bat
@@ -2,6 +2,6 @@
 echo copying clvars.bat to c:\bin
 echo press any key to proceed or ^<CTRL^> C to abort
 pause > NUL
-copy /-Y clvars c:\bin
+copy /-Y clvars.bat c:\bin
 echo copy complete.  press any key to finish.
 pause > NUL
