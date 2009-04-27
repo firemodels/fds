ecd5ecb60f80a5997cf6f703fba53aa2c7625ac1
gforney <gforney@gmail.com>
2009-04-27 00:38:05 +0000

    SMV: add file, IOshooter.c, to contain code for implementing particle shooting
    Former-commit-id: 8be07a26b374f5bda1fe85780e4b7f9aa9bbc3ef [formerly adc278634466864fa2fd4a47fa3963801e5c8eff]
    Former-commit-id: d10629b5b3825a8df60a77fb137d2e5575dfd0e8
    Former-commit-id: 628a956230b22a94aefbdde40c20d7fc09a1061f

---
+++ SMV_5/source/smokeview/IOshooter.c
@@ -0,0 +1,25 @@
+// $Date: 2009-04-26 19:26:12 -0400 (Sun, 26 Apr 2009) $ 
+// $Revision: 3861 $
+// $Author: gforney $
+
+#include "options.h"
+#ifdef pp_SHOOTER
+#include <string.h>
+#ifdef pp_OSX
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <math.h>
+#include "flowfiles.h"
+#include "ASSERT.h"
+#include "smokeviewdefs.h"
+#include "smokeviewvars.h"
+#include "smokeheaders.h"
+
+// svn revision character string
+char IOshooter_revision[]="$Revision: 3861 $";
+// $Date: 2009-04-26 19:26:12 -0400 (Sun, 26 Apr 2009) $ $Author: gforney $
+
+
+#endif
