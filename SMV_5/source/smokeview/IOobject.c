67042348fe7007e1be6168aadd57ed7a88497179
gforney <gforney@gmail.com>
2009-05-03 04:47:52 +0000

    SMV: implement method for dynamically changing a device by adding parameters to DEVICE entry in .smv file
    Former-commit-id: d623984c98f72d2e33afbc4ae9c4a3f607094407 [formerly 6574c8850d696f4a686d42f74c8f2610acc982e7]
    Former-commit-id: dc997cb89ed2c8a369c53307917686d5beb3adf7
    Former-commit-id: 78b388f09254fae6229502bfe6fd209bc31f21b9

--- SMV_5/source/smokeview/IOobject.c
+++ SMV_5/source/smokeview/IOobject.c
@@ -399,6 +399,13 @@ void draw_devices(void){
     if(sensorrelsize!=1.0){
       glScalef(sensorrelsize,sensorrelsize,sensorrelsize);
     }
+    if(devicei->use_params==1){
+      int i;
+
+      for(i=0;i<NDEVICE_PARAMS;i++){
+        valstack[i]=devicei->params[i];
+      }
+    }
     if(showtime==1&&itime>=0&&itime<ntimes&&devicei->showstatelist!=NULL){
       int state;
 
@@ -2120,7 +2127,7 @@ float get_point2box_dist(float boxmin[3], float boxmax[3], float p1[3], float p2
 
 /* ----------------------- init_device ----------------------------- */
 
-void init_device(device *devicei, float *xyz, float *xyzn, int state0){
+void init_device(device *devicei, float *xyz, float *xyzn, int state0, int ndevice_params, float *params){
   if(xyz!=NULL){
     devicei->xyz[0]=xyz[0];
     devicei->xyz[1]=xyz[1];
@@ -2137,4 +2144,13 @@ void init_device(device *devicei, float *xyz, float *xyzn, int state0){
   devicei->act_time=-1.0;
   devicei->device_mesh=NULL;
   devicei->state0=state0;
+  devicei->use_params=0;
+  if(ndevice_params>0&&params!=NULL){
+    int i;
+
+    devicei->use_params=1;
+    for(i=0;i<ndevice_params;i++){
+      devicei->params[i]=params[i];
+    }
+  }
 }
