427d5b8d3055a9d5c5ddc425dbacf9a1bfbcb6fc
gforney <gforney@gmail.com>
2009-05-21 19:16:49 +0000

    SMV: update scripts for building 64 bit windows smokeview
    Former-commit-id: fbf4fcb651b5e1e52c0435a59f6b481a10a3052d [formerly 04e604df60078a21d21abdd8aea3e11fd06ab696]
    Former-commit-id: cc2c9b5fdae956d04add1f8bb3440153804f2899
    Former-commit-id: 0550117b4aeb1c4dec00771f73f4e21bde02c32b

---
+++ SMV_5/scripts/make_smv_test_win64.bat
@@ -0,0 +1,35 @@
+@echo off
+set version=%1
+REM
+REM This batch file creates a self unarchiving file containing
+REM a test version of smokeview and associated files
+REM
+REM usage: 
+REM  make_smv_release_win64 version
+REM    where version is of the from test_svn#
+
+cd ..\for_bundle
+set zipbase=smv_%version%_win64
+set smvdir=to_google\%zipbase%
+
+echo
+echo filling distribution directory
+mkdir %smvdir%
+copy smokeview64_test.exe %smvdir%\smokeview.exe
+copy smokezip_release.exe %smvdir%\smokezip.exe
+copy devices.svo %smvdir%\.
+copy glew32.dll %smvdir%\.
+copy pthreadVC.dll %smvdir%\.
+copy note.txt %smvdir%\.
+
+echo
+echo winzipping distribution directory
+cd %smvdir%
+wzzip -a %zipbase%.zip *
+
+echo
+echo creating self-extracting archive
+c:\bin\winzip\wzipse32 %zipbase%.zip -d "c:\program files\nist\smokeview"
+copy %zipbase%.exe ..\.
+
+cd ..\..\..\scripts
