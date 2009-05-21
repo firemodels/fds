ce18fd91ba168b13525098a01623641ec4509158
gforney <gforney@gmail.com>
2009-05-21 20:19:00 +0000

    SMV: dummy edit to force revision  number update
    Former-commit-id: 9b6208485768e11b5a2ffab9bed13429dc23b0b6 [formerly 61e79886f3ecf959f0baecf0cd6e5a9607ce2441]
    Former-commit-id: 5a0aa16f217571179e5c9de39fa02e0123ac7f8d
    Former-commit-id: 0b0f9d701668ed3520cf11b5c9d311e9dbb5e106

--- SMV_5/source/smokeview/main.c
+++ SMV_5/source/smokeview/main.c
@@ -20,7 +20,7 @@
 // svn revision character string
 char main_revision[]="$Revision$";
 //   dummy change to update version to 5.3.14
-
+// dummy change to force revision update
 
 /* ------------------ main ------------------------ */
 
