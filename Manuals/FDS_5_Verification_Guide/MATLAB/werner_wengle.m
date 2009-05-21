c5af3fd459f94713e051b49d0ec38b1284bf31c1
rmcdermo <randy.mcdermott@gmail.com>
2009-05-21 20:10:32 +0000

    Verification Guide: add Matlab scripts to generate wall model figures
    Former-commit-id: 6c8ede96f5b5b31055d9a2c6dbcf446099d56c3f [formerly 1cd9ca6452d9580604a685b2d85a917e18a7058f]
    Former-commit-id: 00bc82dff0fd289abd4c3aeef7d8ddd74d60a58c
    Former-commit-id: 818308bbc6fab2f837eef2ebb21d7ac4a3d1f708

---
+++ Manuals/FDS_5_Verification_Guide/MATLAB/werner_wengle.m
@@ -0,0 +1,66 @@
+% McDermott
+% 3-4-09
+% werner_wengle.m
+
+close all
+clear all
+
+
+% plot Werner and Wengle velocity profile
+A = 8.3;
+B = 1/7;
+n = 100;
+zp = logspace(0,4,n);
+roughness = 1e-3;
+uu = zp; % log law
+for j = 1:n
+    if zp(j)<11.81
+        up(j) = zp(j);
+    else
+        up(j) = A*zp(j)^B; % Werner and Wengle (power law)
+        uu(j) = (1/0.41)*log(zp(j))+5.2; % log law
+    end
+end
+
+% figure
+% plot(up,zp)
+% xlabel('u+')
+% ylabel('z+')
+
+figure
+set(gca,'FontSize',12)
+semilogx(zp,up,'Linewidth',1.5); hold on
+semilogy(zp,uu,'r--','Linewidth',1.5)
+text(2,10,'u^+ = z^+','Fontsize',12)
+annotation('textarrow',[.5 .6],[.7 .560],'String','u^+ = 2.4 ln z^+ + 5.2','Fontsize',12);
+annotation('textarrow',[.65 .6],[.38 .545],'String','u^+ = A(z^+)^B','Fontsize',12);
+H = annotation('line',[.34 .34],[.15 .6]);
+set(H,'LineStyle','--')
+annotation('textarrow',[.44 .34],[.22 .26],'String',' z^+ = 11.81','Fontsize',12);
+xlabel('z^+','Fontsize',12)
+ylabel('u^+','Fontsize',12)
+set(gcf,'PaperUnits','inches');
+set(gcf,'PaperSize',[6 4.5]);
+set(gcf,'PaperPositionMode','auto');
+print(gcf,'-dpdf','../FIGURES/lawofthewall')
+
+% % check FDS parameters
+% 
+% alpha = (1-B)/2*A^((1+B)/(1-B))
+% beta = 1+B
+% eta = (1+B)/A
+% gamma = 2/(1+B)
+% H = 1;
+% N = 16;
+% dz = H/N
+% rho = 1.198;
+% mu = 1.81e-5;
+% nu = mu/rho;
+% nu_over_dz = nu/dz
+% 
+% u1 = 8.311075e-2;
+% tau_w = ( alpha*nu_over_dz^beta + eta*nu_over_dz^B*abs(u1) )^gamma
+% zplus = dz*sqrt(tau_w)/nu
+
+close all
+clear all
