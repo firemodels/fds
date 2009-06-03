9e3433b5cc29e07d6f91e273f685e532a68f020d
rmcdermo <randy.mcdermott@gmail.com>
2009-06-03 12:29:33 +0000

    Matlab scripts: updates to read_dline.m, add define_drow_variables.m
    Former-commit-id: 162fd6ab652f326adb4aec80e1b8b1a98948e294 [formerly 1a4ca932dfce39eed40702f5f203ce5725fee191]
    Former-commit-id: 088a5a4115c16e85bc2c4b4676e2a70320dd300a
    Former-commit-id: eb49f279ea4fc49a328e16f538bfb4e9bf1fafcf

--- Utilities/Matlab/scripts/read_dline.m
+++ Utilities/Matlab/scripts/read_dline.m
@@ -3,11 +3,14 @@
 % read_dline.m
 %
 % Reads verification data configuration file.
+%
+% Dependencies:
+%    define_drow_variables.m
+%    verification_data_config_matlab.csv
 
 close all
 clear all
 
-addpath('../functions')
 vdir = '../../../Verification/';
 
 A = importdata('../verification_data_config_matlab.csv');
@@ -20,52 +23,7 @@ parameters = P{:}';
 
 if strcmp(parameters(find(strcmp(headers,'switch_id'))),'d')
     
-    Quantity          = str2num(char(parameters(find(strcmp(headers,'Quantity')))));
-    Group             = char(parameters(find(strcmp(headers,'Group'))));
-    d1_Filename       = [vdir,char(parameters(find(strcmp(headers,'d1_Filename'))))];
-    d1_Col_Name_Row   = str2num(char(parameters(find(strcmp(headers,'d1_Col_Name_Row')))));
-    d1_Ind_Col_Name   = char(parameters(find(strcmp(headers,'d1_Ind_Col_Name'))));
-    d1_Dep_Col_Name   = char(parameters(find(strcmp(headers,'d1_Dep_Col_Name'))));
-    d1_Data_Row       = str2num(char(parameters(find(strcmp(headers,'d1_Data_Row')))));
-    d1_Key            = char(parameters(find(strcmp(headers,'d1_Key'))));
-    d1_Style          = str2num(char(parameters(find(strcmp(headers,'d1_Style')))));
-    d1_Start          = str2num(char(parameters(find(strcmp(headers,'d1_Start')))));
-    d1_End            = str2num(char(parameters(find(strcmp(headers,'d1_End')))));
-    d1_Comp_Start     = str2num(char(parameters(find(strcmp(headers,'d1_Comp_Start')))));
-    d1_Comp_End       = str2num(char(parameters(find(strcmp(headers,'d1_Comp_End')))));
-    d1_Initial_Value  = str2num(char(parameters(find(strcmp(headers,'d1_Initial_Value')))));
-    d2_Filename       = [vdir,char(parameters(find(strcmp(headers,'d2_Filename'))))];  
-    d2_Col_Name_Row   = str2num(char(parameters(find(strcmp(headers,'d2_Col_Name_Row')))));
-    d2_Ind_Col_Name   = char(parameters(find(strcmp(headers,'d2_Ind_Col_Name')))); 
-    d2_Dep_Col_Name   = char(parameters(find(strcmp(headers,'d2_Dep_Col_Name'))));
-    d2_Data_Row       = char(parameters(find(strcmp(headers,'d2_Data_Row'))));  
-    d2_Key            = char(parameters(find(strcmp(headers,'d2_Key'))));  
-    d2_Style          = str2num(char(parameters(find(strcmp(headers,'d2_Style')))));
-    d2_Start          = str2num(char(parameters(find(strcmp(headers,'d2_Start')))));
-    d2_End            = str2num(char(parameters(find(strcmp(headers,'d2_End')))));
-    d2_Comp_Start     = str2num(char(parameters(find(strcmp(headers,'d2_Comp_Start')))));
-    d2_Comp_End       = str2num(char(parameters(find(strcmp(headers,'d2_Comp_End')))));
-    d2_Initial_Value  = str2num(char(parameters(find(strcmp(headers,'d2_Initial_Value')))));
-    Metric            = char(parameters(find(strcmp(headers,'Metric')))); 
-    Plot_Title        = char(parameters(find(strcmp(headers,'Plot_Title'))));
-    Ind_Title         = char(parameters(find(strcmp(headers,'Ind_Title'))));
-    Dep_Title         = char(parameters(find(strcmp(headers,'Dep_Title'))));
-    Min_Ind           = str2num(char(parameters(find(strcmp(headers,'Min_Ind')))));
-    Max_Ind           = str2num(char(parameters(find(strcmp(headers,'Max_Ind')))));
-    Scale_Ind         = str2num(char(parameters(find(strcmp(headers,'Scale_Ind')))));
-    Min_Dep           = str2num(char(parameters(find(strcmp(headers,'Min_Dep')))));
-    Max_Dep           = str2num(char(parameters(find(strcmp(headers,'Max_Dep')))));
-    Scale_Dep         = str2num(char(parameters(find(strcmp(headers,'Scale_Dep')))));
-    Flip_Axis         = char(parameters(find(strcmp(headers,'Flip_Axis'))));
-    Title_Position    = str2num(char(parameters(find(strcmp(headers,'Title_Position')))));
-    Key_Position      = char(parameters(find(strcmp(headers,'Key_Position'))));
-    Plot_Width        = str2num(char(parameters(find(strcmp(headers,'Plot_Width')))));
-    Plot_Type         = char(parameters(find(strcmp(headers,'Plot_Type'))));
-    Plot_Filename     = char(parameters(find(strcmp(headers,'Plot_Filename'))));  
-    
-   
-    
-    
+    define_drow_variables
     
     M = csvread(d1_Filename,2,0);   
     plot(M(:,1),M(:,2))
