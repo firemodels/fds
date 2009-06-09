1c4e3f46454d2beb4b91bed68ae5600fc96b35a5
gforney <gforney@gmail.com>
2009-06-09 13:23:21 +0000

    SMV: modify error reporting dialog box code
    Former-commit-id: 076afb8317ee5296553d697f39f9ba67d7852d4b [formerly 9ba977101f70f115473becc57ab34e054709d2c4]
    Former-commit-id: 82e94b5636a872f955494671347ee482a5adf046
    Former-commit-id: f3d72c8c1e50d1cf6b72e733383519b11988891c

--- SMV_5/source/smokeview/glui_message.cpp
+++ SMV_5/source/smokeview/glui_message.cpp
@@ -22,38 +22,29 @@
 // svn revision character string
 extern "C" char glui_message_revision[]="$Revision$";
 
-#define message_OK 1
-#define warning_OK 2
-#define error_OK 3
-#define abort_OK 4
+#define warning_OK 1
+#define error_OK 2
+#define abort_OK 3
 
 void Message_CB(int var);
-GLUI *glui_message=NULL,*glui_warning=NULL,*glui_error=NULL,*glui_abort=NULL;
-GLUI_StaticText *message_text=NULL;
+GLUI *glui_warning=NULL,*glui_error=NULL,*glui_abort=NULL;
 GLUI_StaticText *warning_text=NULL;
+GLUI_StaticText *warning_text2=NULL;
 GLUI_StaticText *error_text=NULL;
+GLUI_StaticText *error_text2=NULL;
 GLUI_StaticText *abort_text=NULL;
+GLUI_StaticText *abort_text2=NULL;
 
 
 /* ------------------ glui_message_setup ------------------------ */
 
 extern "C" void glui_message_setup(int main_window){
 
-  if(glui_message!=NULL)glui_message->close();
-  glui_message = GLUI_Master.create_glui("Message:",0,0,0);
-  glui_message->add_statictext("");
-  message_text=glui_message->add_statictext("");
-  glui_message->add_statictext("");
-  glui_message->add_separator();
-  glui_message->add_button("OK",message_OK,Message_CB);
-  glui_message->set_main_gfx_window( main_window );
-  glui_message->hide();
-
   if(glui_warning!=NULL)glui_warning->close();
   glui_warning = GLUI_Master.create_glui("Warning:",0,0,0);
   glui_warning->add_statictext("");
   warning_text=glui_warning->add_statictext("");
-  glui_warning->add_statictext("");
+  warning_text2=glui_warning->add_statictext("");
   glui_warning->add_separator();
   glui_warning->add_button("OK",warning_OK,Message_CB);
   glui_warning->set_main_gfx_window( main_window );
@@ -63,7 +54,7 @@ extern "C" void glui_message_setup(int main_window){
   glui_error = GLUI_Master.create_glui("Error",0,0,0);
   glui_error->add_statictext("");
   error_text=glui_error->add_statictext("");
-  glui_error->add_statictext("");
+  error_text2=glui_error->add_statictext("");
   glui_error->add_separator();
   glui_error->add_button("OK",error_OK,Message_CB);
   glui_error->set_main_gfx_window( main_window );
@@ -73,7 +64,7 @@ extern "C" void glui_message_setup(int main_window){
   glui_abort = GLUI_Master.create_glui("Abort",0,0,0);
   glui_abort->add_statictext("");
   abort_text=glui_abort->add_statictext("");
-  glui_abort->add_statictext("");
+  abort_text2=glui_abort->add_statictext("");
   glui_abort->add_separator();
   glui_abort->add_button("OK",abort_OK,Message_CB);
   glui_abort->set_main_gfx_window( main_window );
@@ -81,39 +72,66 @@ extern "C" void glui_message_setup(int main_window){
 
 }
 
-/* ------------------ message_message ------------------------ */
-
-extern "C" void message_message(char *message){
-  printf("%s\n",message);
-  if(message_text!=NULL)message_text->set_name(message);
-  if(glui_message!=NULL)glui_message->show();
-}
-
-
 /* ------------------ warning_message ------------------------ */
 
 extern "C" void warning_message(char *message){
-  printf("***Warning: %s\n",message);
-  if(warning_text!=NULL)warning_text->set_name(message);
-  if(glui_warning!=NULL)glui_warning->show();
+  char message2[sizeof(GLUI_String)];
+
+  strcpy(message2,"*** Warning: ");
+  strcat(message2,message);
+  printf("%s\n",message2);
+  if(glui_warning==NULL)return;
+
+  if(show_glui_warning==1){
+    warning_text2->set_name("*** Additional warnings have occurred.  See command shell for details.");
+  }
+  else{
+    warning_text->set_name(message2);
+    warning_text2->set_name("");
+  }
+  show_glui_warning=1;
+  glui_warning->show();
 }
 
 
-/* ------------------ message_message ------------------------ */
+/* ------------------ error_message ------------------------ */
 
 extern "C" void error_message(char *message){
-  printf("***Error: %s\n",message);
-  if(error_text!=NULL)error_text->set_name(message);
-  if(glui_error!=NULL)glui_error->show();
+  char message2[sizeof(GLUI_String)];
+
+  strcpy(message2,"*** Error: ");
+  strcat(message2,message);
+  printf("%s\n",message2);
+  if(glui_error==NULL)return;
+
+  if(show_glui_error==1){
+    error_text2->set_name("*** Additional errors have occurred.  See command shell for details.");
+  }
+  else{
+    error_text->set_name(message2);
+    error_text2->set_name("");
+  }
+  show_glui_error=1;
+  glui_error->show();
 }
 
 
-/* ------------------ message_message ------------------------ */
+/* ------------------ abort_message ------------------------ */
 
 extern "C" void abort_message(char *message){
-  printf("***Fatal Error: %s\n",message);
-  if(abort_text!=NULL)abort_text->set_name(message);
-  if(glui_abort!=NULL)glui_abort->show();
+  char message2[sizeof(GLUI_String)];
+
+  strcpy(message2,"*** Fatal error: ");
+  strcat(message2,message);
+  printf("%s\n",message2);
+  if(glui_abort==NULL)return;
+
+  if(show_glui_abort==0){
+    abort_text->set_name(message2);
+    abort_text2->set_name("Press OK to terminate Smokeview");
+  }
+  show_glui_abort=1;
+  glui_abort->show();
 }
 
 /* ------------------ MESSAGE_CB ------------------------ */
@@ -122,17 +140,16 @@ void Message_CB(int var){
   int i;
 
   switch (var){
-    case message_OK:
-      glui_message->hide();
-      break;
     case warning_OK:
       glui_warning->hide();
       break;
     case error_OK:
       glui_error->hide();
+      show_glui_error=0;
       break;
     case abort_OK:
       glui_abort->hide();
+      exit(1);
       break;
   }
 }
