8c30f110ea85a181f445ede9c9da72cf353aa1a2
rmcdermo <randy.mcdermott@gmail.com>
2009-06-11 20:37:42 +0000

    Matlab scripts: general updates
    Former-commit-id: e82d46a6d54b24f30a8d35fda3fc50db88252475 [formerly 7dab22b8cddb237559322b35142bf3170d953556]
    Former-commit-id: 1a18b9efe8e89354b5a81e273d0fea4462288ea2
    Former-commit-id: a45a45170533a39334dace8985c7bc9a0a74888b

--- Utilities/Matlab/scripts/scatterplot.m
+++ Utilities/Matlab/scripts/scatterplot.m
@@ -15,6 +15,8 @@ qfil = ['../scatterplot_config_matlab.csv'];
 qrange = 2:2;
 
 addpath('../functions')
+paper_width  = 4.5; % inches
+paper_height = 4.5; % inches
 
 Q = importdata(qfil);
 H = textscan(Q{1},'%q','delimiter',',');
@@ -27,8 +29,27 @@ for j=qrange
     
     for i=drange
         if strcmp(Save_Quantity(i),Quantity_Label)
-            plot(Save_Measured_Metric(i),Save_Predicted_Metric(i),'b^'); hold on
+            plot(Save_Measured_Metric(i),Save_Predicted_Metric(i),char(Save_Group_Style(i))); hold on
+            %legend(K,char(Save_Group_Key_Label(i)),'Location',Key_Position);
+            %legend boxoff
         end
     end
+    
+    % format the legend and axis labels
+    
+    xlabel(Ind_Title,'Interpreter','LaTeX','FontSize',16)
+    ylabel(Dep_Title,'Interpreter','LaTeX','FontSize',16)
+    axis([Plot_Min Plot_Max Plot_Min Plot_Max])
+    
+    set(gca,'FontName','Times')
+    set(gca,'FontSize',14)
+    set(gca,'YTick',get(gca,'XTick'))
+    
+    text(Title_Position(1)*(Plot_Max-Plot_Min),Title_Position(2)*(Plot_Max-Plot_Min),...
+        Scatter_Plot_Title,'FontSize',16,'FontName','Times','Interpreter','LaTeX')
+%     if size(Key_Position)>0
+%         legend(K,[parse(d1_Key),parse(d2_Key)],'Location',Key_Position,'Interpreter','LaTeX','FontSize',10)
+%         legend boxoff
+%     end
     
 end
