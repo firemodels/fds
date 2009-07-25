150c97e27f89a5fb8edcdaac6ca67c79d8c10b86
gforney <gforney@gmail.com>
2009-07-25 01:15:16 +0000

    build scripts: updates to bundle script
    Former-commit-id: 2550f086fdcc7f48880795eb483a855ead859fc6 [formerly 236dd4c08053d3188869051576d11fef2873f66a]
    Former-commit-id: d69ba34253a1c55fc3c1c8742144806278554b3a
    Former-commit-id: 496145b76e87f7761eb44e377a8083165d588003

--- Utilities/Scripts/bundle_setup/setup.bat
+++ Utilities/Scripts/bundle_setup/setup.bat
@@ -1,19 +1,19 @@
 @echo off
-set fromdir=%CD%\fds5
+set fromdir=%CD%
 set todir=%ProgramFiles%\FDS5
 set bindir=%todir%\bin
 
 echo .
-echo copying files to %todir%
+echo copying files to %todir% from %fromdir%
 xcopy  "%fromdir%" "%todir%" /E /C /I /Y /Q
 erase "%todir%"\setup.bat
-
+erase "%todir%"\set_fds5_path.bat
 
 Rem assoc .smv=smokeview.Document>Nul
 Rem ftype smokeview.Document=%bindir%\smokeview.exe %1 >Nul
 echo.
 echo Setting Path for FDS
-%fromdir%\fds5_setup.exe
+%fromdir%\set_fds5_path.exe
 echo.
 echo Installation Complete.  
 echo Press any key to continue . . . 
