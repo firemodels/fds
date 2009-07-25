b45aa180d7baa63ed9bb3f3e2acc44150b1d923b
gforney <gforney@gmail.com>
2009-07-25 02:39:02 +0000

    build scripts: update bundle scripts

---
+++ Utilities/Scripts/bundle_setup/wrapup.bat
@@ -0,0 +1,17 @@
+@echo off
+set bindir="%CD%"\bin
+
+echo .
+echo current directory is %CD%
+
+Rem assoc .smv=smokeview.Document>Nul
+Rem ftype smokeview.Document=%bindir%\smokeview.exe %1 >Nul
+echo.
+echo Setting Path for FDS
+"%CD%"\set_fds5_path.exe
+echo.
+erase "%CD%"\set_fds5_path.exe
+erase "%CD%"\setup.bat
+echo Installation Complete.  
+echo Press any key to continue . . . 
+pause>NUL
