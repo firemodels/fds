3dfa37c3cd4dd0e2d4d9b22615b1d1cdcab86cf0
gforney <gforney@gmail.com>
2009-07-26 14:51:08 +0000

    build scripts:  change the fds/smokeview bundle script (temporarily) so that it installs fds5t.exe and smokeviewt.exe (rather than fds5.exe and smokeview.exe) so that one can test the bundle without interfering with a "production" fds/smokeview setup.  Note, to test smokeviewt.exe copy casename.smv to casename.smt .
    Former-commit-id: 5c4d5ab234f03ad9f29ad666de238860f182360f [formerly 6b0de7b07621b7a8826e70d81f713feae2e27560]
    Former-commit-id: f24396cb5df9ea268020c2305c5fd0048e0a03cd
    Former-commit-id: 1c486907670fc40848092b3d37afd93d5d7a5fda

--- Utilities/Scripts/bundle_setup/wrapup.bat
+++ Utilities/Scripts/bundle_setup/wrapup.bat
@@ -1,13 +1,18 @@
 @echo off
-set bindir="%CD%"\bin
+set bindir=%CD%\bin
 
 echo .
 echo current directory is %CD%
 
-Rem assoc .smv=smokeview.Document>Nul
-Rem ftype smokeview.Document=%bindir%\smokeview.exe %1 >Nul
+echo .
+echo associating the smt extension with smokeviewt.exe
+
+ftype smtDoc="%bindir%\smokeviewt.exe" "%%1" >Nul
+assoc .smt=smtDoc>Nul
+
 echo.
 echo Setting Path for FDS
+
 "%CD%"\set_fds5_path.exe
 echo.
 erase "%CD%"\set_fds5_path.exe
