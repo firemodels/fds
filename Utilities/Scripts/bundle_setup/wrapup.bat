b314c8051c3963224ab9a729761270df92dfcce4
gforney <gforney@gmail.com>
2009-07-26 20:26:53 +0000

    build scripts: edits to bundle build script to add FDS website url shortcuts to Start menu

--- Utilities/Scripts/bundle_setup/wrapup.bat
+++ Utilities/Scripts/bundle_setup/wrapup.bat
@@ -1,22 +1,37 @@
 @echo off
-set bindir=%CD%\bin
 
-echo .
-echo current directory is %CD%
+IF not EXIST placeholder.txt goto dircheck
+echo ***error: wrapup script running in wrong directory.
+pause
+exit
+:dircheck
+
 
 echo .
 echo associating the smt extension with smokeviewt.exe
 
-ftype smtDoc="%bindir%\smokeviewt.exe" "%%1" >Nul
+ftype smtDoc="%CD%\bin\smokeviewt.exe" "%%1" >Nul
 assoc .smt=smtDoc>Nul
+
+echo. 
+echo Setting shortcuts to FDS/Smokeview documentation and web sites.
+rmdir /q /s "%USERPROFILE%\Start Menu\Programs\FDS5"
+mkdir "%USERPROFILE%\Start Menu\Programs\FDS5"
+"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\FDS Web Site.lnk"  /T:"%CD%\Documentation\FDS Web Site.url" /A:C
+"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\FDS Development Web Site.lnk"  /T:"%CD%\Documentation\FDS Development Web Site.url" /A:C
+"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\Documentation.lnk"  /T:"%CD%"\Documentation /A:C
 
 echo.
 echo Setting Path for FDS
 
-"%CD%"\set_fds5_path.exe
-echo.
+call "%CD%"\set_fds5_path.exe
+
 erase "%CD%"\set_fds5_path.exe
+erase "%CD%"\shortcut.exe
 erase "%CD%"\wrapup.bat
+
 echo Installation Complete.  
 echo Press any key to continue . . . 
+
 pause>NUL
+
