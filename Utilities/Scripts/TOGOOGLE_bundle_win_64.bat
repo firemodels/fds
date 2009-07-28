593ca7b459c6e9c75800018be388f36b9a3717d1
gforney <gforney@gmail.com>
2009-07-28 19:52:56 +0000

    build scripts: add script to upload 64 bit windows installer to google code (preliminary)
    Former-commit-id: adeec0b687a3807e4afcbeef6c8314216f30cbb8 [formerly e7332c9302a1337563c179a3580b183bb77d822c]
    Former-commit-id: 9f63dde1a708080619d378b8b200a7f981602ab4
    Former-commit-id: 51c9658f60a4bd8f8313ccbada23efbcec3333eb

---
+++ Utilities/Scripts/TOGOOGLE_bundle_win_64.bat
@@ -0,0 +1,46 @@
+@echo off
+
+Rem Windows batch file to upload 64 bit windows bundle to the google download site
+
+set envfile="%homedrive%\%homepath%\fds_smv_env.bat"
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\Utilities\to_google
+
+set glabels=Type-Installer,Opsys-Windows,%fds_google_level%
+set dplatform=64 bit Windows
+set platform=win64
+set summary=Bundled FDS and Smokeview for %dplatform% (SVN r%fds_revision%,%smv_revision)
+set exe=FDS_%fds_version%_SMV_%smv_version%_%platform%.exe
+
+echo Uploading %exe%
+echo.
+echo press any key to proceed with upload, CTRL c to abort
+pause>NUL
+
+  if not exist %exe% goto abort_upload
+  echo.
+  echo Uploading %summary% - %exe%
+  echo googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+       googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none -s "%summary%" -p fds-smv -u gforney -l %glabels% %exe%
+
+echo.
+echo Uploads complete
+pause
+goto:eof
+
+:abort_upload
+echo %exe% does not exist - upload to google failed
+pause
