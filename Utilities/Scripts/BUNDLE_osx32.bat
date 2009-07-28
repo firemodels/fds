e062fa1c72c0bbb42024d059c10566afb1917141
gforney <gforney@gmail.com>
2009-07-28 16:19:42 +0000

    build scripts: implement scripts to bundle and upload to google 32 and 64 bit OSX FDS/Smokeview distributions

---
+++ Utilities/Scripts/BUNDLE_osx32.bat
@@ -0,0 +1,27 @@
+@echo off
+Rem Batch file used to bundle a 32 bit OSX of FDS/Smokeview
+
+set envfile="%homedrive%\%homepath%\fds_smv_env.bat"
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+Rem location of batch files used to set up Intel compilation environment
+
+call %envfile%
+
+set bundledir=FDS_%fds_version%-SMV_%smv_version%_osx_32
+plink %svn_logon% %linux_svn_root%/Utilities/Scripts/bundle_osx_32.csh %linux_svn_root% %bundledir%
+
+echo Downloading compressed archive to:
+echo   %svn_root%\Utilities\to_google\%bundledir%.tar.gz
+pscp %svn_logon%:%linux_svn_root%/Utilities/to_google/%bundledir%.tar.gz %svn_root%/Utilities/to_google/.
+
+pause
