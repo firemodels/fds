3e47033b48b06b1db3b1010397ba6129aea56e1a
gforney <gforney@gmail.com>
2009-08-03 15:01:27 +0000

    build scripts: update and rename windows script to build fds2ascii
    Former-commit-id: e73977967cc5014b18d142ee85e082cd4b89c53c [formerly cdc18ffb2550247421453565c338bb3cc8cb77ab]
    Former-commit-id: d40c21ac00b318ac07ce94ed8c84a8e4a327a686
    Former-commit-id: 86b6e4c2a5b5e1d4e2e2f638b75212efbe391f1b

--- Utilities/fds2ascii/make_fds2ascii_win.bat
+++ Utilities/fds2ascii/make_fds2ascii_win.bat
@@ -4,5 +4,5 @@ call %intelbin%\ifortvars ia32
 
 Rem erase *.obj
 Rem erase *.mod
-ifort -o fds2ascii ..\Data_Processing\fds2ascii.f90
+ifort -o fds2ascii_win32.exe ..\Data_Processing\fds2ascii.f90
 pause
