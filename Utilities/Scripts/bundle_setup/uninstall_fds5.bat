a2470ed777597b2f8ee51a8d8eaf7f5ee43232e6
gforney <gforney@gmail.com>
2009-08-04 16:55:05 +0000

    build scripts: add uninstall script to win32 bundle
    Former-commit-id: 174cb8886d3ecaf6145452f5100732b7cb8d6c71 [formerly c1f35ab3fa9c6b227d113b68523e7b62556d552b]
    Former-commit-id: e9999b252a7eb09daf7ec7d5877a893c990b169b
    Former-commit-id: f851a748aaac48d4a1a40a4e80a65f590bc90ae9

---
+++ Utilities/Scripts/bundle_setup/uninstall_fds5.bat
@@ -0,0 +1,39 @@
+@echo off
+
+IF not EXIST placeholder.txt goto dircheck
+echo ***error: This script is running in wrong directory.
+pause
+exit
+:dircheck
+
+echo.
+echo FDS and Smokeview uninstall script
+echo.
+echo This script removes path entries for the current FDS/Smokeview installation
+echo.
+echo Press any key to proceed or CTRL C to abort
+pause>NUL
+
+echo.  
+echo Proceeding...
+
+echo.
+echo removing association between .smv and smokeview
+
+assoc .smv=
+Rem ftype smvDoc=
+
+echo. 
+echo Removing FDS and Smokeview shortcuts from the Start menu.
+rmdir /q /s "%USERPROFILE%\Start Menu\Programs\FDS5"
+
+
+cd ..\bin
+echo.
+echo Removing directory %CD% from the user path variable for: %USERNAME%
+call ..\Uninstall\set_path.exe -u "%CD%"
+
+echo.
+echo Uninstall Complete.  Press any key to continue.
+pause>NUL
+
