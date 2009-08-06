3ccb5fdd7d3d11ccccab5483bdcab5a729e77a08
gforney <gforney@gmail.com>
2009-08-06 02:59:34 +0000

    build scripts:  add issue tracker and discussion group to startup menus

--- Utilities/Scripts/bundle_setup/wrapup_fds_install.bat
+++ Utilities/Scripts/bundle_setup/wrapup_fds_install.bat
@@ -36,12 +36,14 @@ copy "%CD%\Documentation\FDS_on_the_Web\Updates.url" "%USERPROFILE%\Start Menu\P
 mkdir "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web"
 copy "%CD%\Documentation\FDS_on_the_Web\D*"          "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web"
 copy "%CD%\Documentation\FDS_on_the_Web\O*"          "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web"
+copy "%CD%\Documentation\FDS_on_the_Web\Discussion_Group.url"        %USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Discussion_Group.url"
+copy "%CD%\Documentation\FDS_on_the_Web\Issue_Tracker.url"          "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Issue_Tracker.url"
 
 mkdir "%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes"
 "%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\FDS_5_User_Guide.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_5_User_Guide.pdf" /A:C >NUL
 "%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\SMV_5_User_Guide.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\SMV_5_User_Guide.pdf" /A:C >NUL
 "%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\FDS_Release_Notes.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_Release_Notes.htm" /A:C >NUL
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\Smokeview_release_notes.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\Smokeview_release_notes.pdf" /A:C >NUL
+"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\Smokeview_release_notes.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\Smokeview_release_notes.html" /A:C >NUL
 
 echo.
 echo Adding the directory %CD% to the user path variable for: %USERNAME%
