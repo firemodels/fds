c8fbdbd76e8132c3bb83aa2b2211161b44e72dd9
mcgratta <mcgratta@gmail.com>
2009-08-07 20:46:40 +0000

    FDS Users Guide: Update verification cases and smokeview scripts for Guides.
    Former-commit-id: 6084ce9f1e1b3c46614b1057766c34dc083ba59b [formerly 3cb6738b88b8a8e3f436acf8d5df86855796ad26]
    Former-commit-id: e818e94d1a5c789b79ea4d6e53069cc7c3fee7e5
    Former-commit-id: b350f8bfcc8eac0953ea6e633daaeb061ff78818

---
+++ Verification/Process_Pictures.bat
@@ -0,0 +1,24 @@
+cd Detectors
+smokeview -runscript beam_detector
+cd ..
+cd Fires
+smokeview -runscript room_fire
+cd ..
+cd Flowfields
+smokeview -runscript helium_2d
+smokeview -runscript sawtooth
+cd ..
+cd Miscellaneous
+smokeview -runscript pyramid
+cd ..
+cd NS_Analytical_Solution
+smokeview -runscript ns2d_64
+cd ..
+cd Pressure_Effects
+smokeview -runscript pressure_boundary
+cd ..
+cd Sprinklers_and_Sprays
+smokeview -runscript cascade
+cd ..
+
+
