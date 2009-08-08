3c5c5079c312349b9d69cbac8e15def52f8f51d3
gforney <gforney@gmail.com>
2009-08-08 16:49:38 +0000

    build scripts: edits to wrapup script
    Former-commit-id: d2e8d682088ca8bd733fe66414047f39aa70f5f7 [formerly 606e7ce6d88bd6514a79864d5f8631c1abdcda58]
    Former-commit-id: 0f540e5699b575f3d280b828299ca9f687f8d362
    Former-commit-id: dab3bb5ac057a8bd39e23e121d1d27c3461e5fe3

--- Utilities/Scripts/bundle_setup/wrapup_fds_install.bat
+++ Utilities/Scripts/bundle_setup/wrapup_fds_install.bat
@@ -30,20 +30,21 @@ echo Adding FDS and Smokeview shortcuts to the Start menu.
 if exist "%USERPROFILE%\Start Menu\Programs\FDS5" rmdir /q /s "%USERPROFILE%\Start Menu\Programs\FDS5"
 
 mkdir "%USERPROFILE%\Start Menu\Programs\FDS5"
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\Overview.lnk"  /T:"%CD%\Documentation\Overview.html" /A:C >NUL
-copy "%CD%\Documentation\FDS_on_the_Web\Updates.url" "%USERPROFILE%\Start Menu\Programs\FDS5\Updates.url"  
 
 mkdir "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web"
 copy "%CD%\Documentation\FDS_on_the_Web\D*"          "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web"
 copy "%CD%\Documentation\FDS_on_the_Web\O*"          "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web"
 copy "%CD%\Documentation\FDS_on_the_Web\Discussion_Group.url"        %USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Discussion_Group.url"
 copy "%CD%\Documentation\FDS_on_the_Web\Issue_Tracker.url"          "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Issue_Tracker.url"
+copy "%CD%\Documentation\FDS_on_the_Web\Updates.url" "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Updates.url"  
 
 mkdir "%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes"
 "%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\FDS_5_User_Guide.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_5_User_Guide.pdf" /A:C >NUL
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\SMV_5_User_Guide.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\SMV_5_User_Guide.pdf" /A:C >NUL
 "%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\FDS_Release_Notes.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_Release_Notes.htm" /A:C >NUL
+"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\SMV_5_User_Guide.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\SMV_5_User_Guide.pdf" /A:C >NUL
 "%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\Smokeview_release_notes.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\Smokeview_release_notes.html" /A:C >NUL
+
+"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\Overview.lnk"  /T:"%CD%\Documentation\Overview.html" /A:C >NUL
 
 echo.
 echo Adding the directory %CD% to the user path variable for: %USERNAME%
