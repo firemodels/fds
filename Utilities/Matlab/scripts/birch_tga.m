b8f45d6cfb78e8841727e45065058dd64649b2d3
mcgratta <mcgratta@gmail.com>
2009-08-11 17:59:29 +0000

    FDS Verification: Commit birch_tga case.

---
+++ Utilities/Matlab/scripts/birch_tga.m
@@ -0,0 +1,37 @@
+% Hostikka
+% 8-11-2009
+% birch_tga.m
+
+close all
+clear all
+
+addpath('../../../Verification/Pyrolysis')
+
+plot_style
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
+set(gcf,'DefaultLineLineWidth',Line_Width)
+set(gca,'FontName',Font_Name)
+
+TGA_20_N2 = csvread('birch_tga_20_exp.csv',34,0);
+TGA_2_N2 = csvread('birch_tga_2_exp.csv',34,0);
+
+M_2 = csvread('birch_tga_1step_2_devc.csv',2,0);
+M_20 = csvread('birch_tga_1step_20_devc.csv',2,0);
+
+h=plot(TGA_2_N2(:,1),TGA_2_N2(:,2)/100,TGA_20_N2(:,1),TGA_20_N2(:,2)/100,M_2(:,3),M_2(:,2)/M_2(1,2),M_20(:,3),M_20(:,2)/M_20(1,2));
+hold on
+set(gca,'FontName',Font_Name)
+axis([0 800 0 1.1])
+xlabel('Temperature ($^\circ$C)','Interpreter','LaTeX','FontSize',Label_Font_Size)
+ylabel('Mass Fraction','Interpreter','LaTeX','FontSize',Label_Font_Size)
+set(h([1 3]),'Color','b')
+set(h([2 4]),'Color','r')
+set(h([3 4]),'LineStyle','--')
+legend('Exp. 2 K/min','Exp. 20 K/min','FDS 2 K/min','FDS 20 K/min')
+
+% print pdf
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf','../../../Manuals/FDS_5_Verification_Guide/FIGURES/birch_tga')
