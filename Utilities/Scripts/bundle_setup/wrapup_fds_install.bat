41457ca9fa5d740edecd1567962720ee9cf5eb0f
gforney <gforney@gmail.com>
2009-08-22 01:02:23 +0000

    build scripts: use blanks (rather than underscores) in start menu entries

--- Utilities/Scripts/bundle_setup/wrapup_fds_install.bat
+++ Utilities/Scripts/bundle_setup/wrapup_fds_install.bat
@@ -1,22 +1,19 @@
 @echo off
 
 IF not EXIST placeholder.txt goto dircheck
-echo ***error: This script is running in wrong directory.
+echo ***error: This script is running in the wrong directory.
 pause
 exit
 :dircheck
 
 echo.
-echo FDS and Smokeview installation
+echo FDS and Smokeview installation.
 echo.
-echo ***Note*** Path entries for pre 5.4 versions of FDS/Smokeview will be removed.
-echo Program and data files WILL NOT.
-echo.
+echo Removing pre 5.4 FDS/Smokeview entries (if present) from the System Path.
 echo Press any key to proceed or CTRL C to abort
 pause>NUL
 
-echo.  
-echo Proceeding...
+call "%CD%"\set_path.exe -r
 
 echo.
 echo Associating the smv file extension with smokeview.exe
@@ -28,35 +25,30 @@ set FDS5START=%USERPROFILE%\Start Menu\Programs\FDS5
 
 echo. 
 echo Adding FDS and Smokeview shortcuts to the Start menu.
-if exist "%USERPROFILE%\Start Menu\Programs\FDS5" rmdir /q /s "%USERPROFILE%\Start Menu\Programs\FDS5"
+if exist "%FDS5START%" rmdir /q /s "%FDS5START%"
 
-mkdir "%USERPROFILE%\Start Menu\Programs\FDS5"
+mkdir "%FDS5START%"
 
-mkdir "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web"
-copy "%CD%\Documentation\FDS_on_the_Web\Developer_Web_Site.url" "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Developer_Web_Site.url"
-copy "%CD%\Documentation\FDS_on_the_Web\Discussion_Group.url"   "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Discussion_Group.url"
-copy "%CD%\Documentation\FDS_on_the_Web\Official_Web_Site.url"  "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Official_Web_Site.url"
-copy "%CD%\Documentation\FDS_on_the_Web\Discussion_Group.url"   "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Discussion_Group.url"
-copy "%CD%\Documentation\FDS_on_the_Web\Issue_Tracker.url"      "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Issue_Tracker.url"
-copy "%CD%\Documentation\FDS_on_the_Web\Updates.url"            "%USERPROFILE%\Start Menu\Programs\FDS5\FDS_on_the_Web\Updates.url"  
+mkdir "%FDS5START%\FDS on the Web"
+copy "%CD%\Documentation\FDS_on_the_Web\Developer_Web_Site.url" "%FDS5START%\FDS on the Web\Developer Web Site.url"
+copy "%CD%\Documentation\FDS_on_the_Web\Discussion_Group.url"   "%FDS5START%\FDS on the Web\Discussion Group.url"
+copy "%CD%\Documentation\FDS_on_the_Web\Official_Web_Site.url"  "%FDS5START%\FDS on the Web\Official Web Site.url"
+copy "%CD%\Documentation\FDS_on_the_Web\Discussion_Group.url"   "%FDS5START%\FDS on the Web\Discussion Group.url"
+copy "%CD%\Documentation\FDS_on_the_Web\Issue_Tracker.url"      "%FDS5START%\FDS on the Web\Issue Tracker.url"
+copy "%CD%\Documentation\FDS_on_the_Web\Updates.url"            "%FDS5START%\FDS on the Web\Updates.url"  
 
-mkdir "%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes"
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\FDS_5_User_Guide.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_5_User_Guide.pdf" /A:C >NUL
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\FDS_Release_Notes.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_Release_Notes.htm" /A:C >NUL
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\SMV_5_User_Guide.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\SMV_5_User_Guide.pdf" /A:C >NUL
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\User_Guides_and_Release_Notes\Smokeview_release_notes.lnk"  /T:"%CD%\Documentation\Guides_and_Release_Notes\Smokeview_release_notes.html" /A:C >NUL
+mkdir "%FDS5START%\User Guides and Release Notes"
+"%CD%\shortcut.exe" /F:"%FDS5START%\User Guides and Release Notes\FDS 5 User Guide.lnk"        /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_5_User_Guide.pdf" /A:C >NUL
+"%CD%\shortcut.exe" /F:"%FDS5START%\User Guides and Release Notes\FDS Release Notes.lnk"       /T:"%CD%\Documentation\Guides_and_Release_Notes\FDS_Release_Notes.htm" /A:C >NUL
+"%CD%\shortcut.exe" /F:"%FDS5START%\User Guides and Release Notes\SMV 5 User Guide.lnk"        /T:"%CD%\Documentation\Guides_and_Release_Notes\SMV_5_User_Guide.pdf" /A:C >NUL
+"%CD%\shortcut.exe" /F:"%FDS5START%\User Guides and Release Notes\Smokeview release notes.lnk" /T:"%CD%\Documentation\Guides_and_Release_Notes\Smokeview_release_notes.html" /A:C >NUL
 
-"%CD%\shortcut.exe" /F:"%USERPROFILE%\Start Menu\Programs\FDS5\Overview.lnk"  /T:"%CD%\Documentation\Overview.html" /A:C >NUL
+"%CD%\shortcut.exe" /F:"%FDS5START%\Overview.lnk"  /T:"%CD%\Documentation\Overview.html" /A:C >NUL
 
 echo.
-echo Adding %CD%\bin to the User Path for: %USERNAME%
+echo Adding %CD%\bin to the User Path (if absent) for: %USERNAME%
 
 call "%CD%"\set_path.exe -a "%CD%\bin"
-
-echo.
-echo Removing pre 5.4 FDS/Smokeview entries (if present) from the System Path
-
-call "%CD%"\set_path.exe -r
 
 erase "%CD%"\set_path.exe
 erase "%CD%"\shortcut.exe
