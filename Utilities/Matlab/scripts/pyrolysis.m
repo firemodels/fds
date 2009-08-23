6378bc3bf5875be80558362807ce7560bae0f510
mcgratta <mcgratta@gmail.com>
2009-08-23 20:59:29 +0000

    FDS User Guide: Add pyrolysis test cases and update Matlab pyrolysis script.
    Former-commit-id: eb63f07f7a38dac9850ca92326e5b78cee23b19b [formerly 67185bafba97df42faad16f724db961f0b2102e6]
    Former-commit-id: bf1d94ca63900138bed2ae20f218294f516d5100
    Former-commit-id: 256b5c2e5b646f0698d08177d09a8cc47cd2df9f

--- Utilities/Matlab/scripts/pyrolysis.m
+++ Utilities/Matlab/scripts/pyrolysis.m
@@ -9,14 +9,9 @@
 % Both Y and T are functions of time, t, but dT/dt is a specified constant.
 % Typical TGA heating rates are 5, 10 and 20 degrees C per minute.
 
+
 close all
 clear all
-
-plot_style
-set(gcf,'DefaultLineLineWidth',Line_Width)
-set(gca,'FontName',Font_Name)
-set(gca,'Units',Plot_Units)
-set(gca,'Position',[Plot_X-.15,Plot_Y,Plot_Width,Plot_Height])
 
 % Set global reaction rate parameters
 
@@ -24,57 +19,131 @@ global dTdt
 global R0
 global E
 global A
+global residue
 
-dTdt = 5./60.;
-R0 = 8314.3;
-T_0 = 300.+273.;
-delta_T = 80.;
-r_0=2*dTdt/delta_T;
-E=r_0*R0*T_0*T_0/(dTdt*0.4);
-A=(E./(R0.*T_0.*T_0))*dTdt*exp(E./(R0.*T_0));
+addpath('../../../Verification/Pyrolysis')
 
-% Solve the ODE dY/dT=f(T,Y)
+show_fds = 0;
 
-options = odeset('RelTol',1e-10,'AbsTol',1e-8);
-[T,Y] = ode15s(@reaction_rate,[373 673],1.,options);
+for i_plot=1:2
+    
+    close all
+    
+    plot_style
+    set(gcf,'DefaultLineLineWidth',Line_Width)
+    set(gca,'FontName',Font_Name)
+    set(gca,'Units',Plot_Units)
+    set(gca,'Position',[Plot_X-.15,Plot_Y,Plot_Width,Plot_Height])
 
-% Compute dY/dT
+    dTdt = 5./60.;
+    R0 = 8314.3;
 
-for i=2:length(Y)-1
-    dYdT(i) = (Y(i+1)-Y(i-1))/(T(i+1)-T(i-1));
+    if i_plot==1
+        n_components = 3;
+        T_p(1) = 100.+273.;
+        T_p(2) = 300.+273.;
+        T_p(3) = 300.+273.;
+        Y_0(1)=0.00;
+        Y_0(2)=1.00;
+        Y_0(3)=0.00;
+        delta_T(1) = 10.;
+        delta_T(2) = 80.;
+        delta_T(3) = 20.;
+        residue(1) = 0.0;
+        residue(2) = 0.0;
+        residue(3) = 0.0;
+    else
+        n_components = 3;
+        T_p(1) = 100.+273.;
+        T_p(2) = 300.+273.;
+        T_p(3) = 300.+273.;
+        Y_0(1)=0.10;
+        Y_0(2)=0.90;
+        Y_0(3)=0.00;
+        delta_T(1) = 10.;
+        delta_T(2) = 80.;
+        delta_T(3) = 20.;
+        residue(1) = 0.0;
+        residue(2) = 0.2;
+        residue(3) = 0.0;
+    end
+
+    % Calculate A and E
+    
+    for i=1:n_components
+        Y_p=max(0.01,0.4*Y_0(i));
+        r_p(i)=Y_0(i)*2*dTdt*(1.-residue(i))/delta_T(i);
+        E(i)=r_p(i)*R0*T_p(i)*T_p(i)/(dTdt*Y_p);
+        A(i)=(r_p(i)/Y_p)*exp(E(i)./(R0.*T_p(i)));
+    end
+
+    % Solve the ODE dY/dT=f(T,Y)
+
+    options = odeset('RelTol',1e-10,'AbsTol',1e-8);
+    [T,Y] = ode15s(@reaction_rate,[273 673],Y_0,options);
+
+    % Compute dm/dT
+
+    m = sum(Y,2);
+    for i=2:length(Y(:,1))-1
+        dmdT(i) = (m(i+1,1)-m(i-1,1))/(T(i+1,1)-T(i-1,1));
+    end
+    dmdT(1) = 0.;
+    dmdT(length(Y(:,1))) = 0.;
+
+    % Plot the solution
+
+    TC = T-273;
+    [AX,H1,H2] = plotyy(TC,m,TC,-dmdT*dTdt*1000,'plot'); hold on
+
+    % Add the FDS solution
+
+    if show_fds==1
+        if i_plot==1
+            FDS = csvread('pyrolysis_1_devc.csv',2,0);
+        else
+            FDS = csvread('pyrolysis_2_devc.csv',2,0);
+        end
+        h=plot(FDS(:,3),FDS(:,2)/FDS(1,2),'bo');
+    end
+
+    % Plot attributes
+    
+    set(gca,'FontName',Font_Name)
+    set(AX(1),'FontName',Font_Name)
+    set(AX(2),'FontName',Font_Name)
+    xlabel('Temperature ($^\circ$C)','Interpreter','LaTeX','FontSize',Label_Font_Size)
+    set(get(AX(1),'Ylabel'),'String','Mass Fraction','Interpreter','LaTeX','FontSize',Label_Font_Size)
+    set(get(AX(2),'Ylabel'),'String','Reaction Rate (s$^{-1}$) $\times$ 10$^3$','Interpreter','LaTeX','FontSize',Label_Font_Size)
+    set(AX(1),'YLim',[0 1.1])
+    set(AX(2),'YLim',[0 2.2])
+    set(AX(1),'YTickMode','manual')
+    set(AX(2),'YTickMode','manual')
+    set(AX(1),'YTick',[0 0.2 0.4 0.6 0.8 1.0])
+    set(AX(2),'YTick',[0 0.4 0.8 1.2 1.6 2.0])
+    set(H1,'LineStyle','-')
+    set(H2,'LineStyle','-')
+
+    % Add vertical lines to indicate the temperature peaks
+    
+    for i=1:n_components
+        axes(AX(2));
+        line([T_p(i)-273 T_p(i)-273],[0.00 r_p(i)*1000],'LineStyle','--','Color','black','LineWidth',1)
+    end
+    
+    % Create the PDF files
+    
+    set(gcf,'Visible',Figure_Visibility);
+    set(gcf,'PaperUnits',Paper_Units);
+    set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+    set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+    if i_plot==1
+        print(gcf,'-dpdf','../../../Manuals/FDS_5_User_Guide/FIGURES/pyrolysis_1')
+    else
+        print(gcf,'-dpdf','../../../Manuals/FDS_5_User_Guide/FIGURES/pyrolysis_2')
+    end
+    
 end
-dYdT(1) = 0.;
-dYdT(length(Y)) = 0.;
-
-% Plot the solution
-
-dTdt = 5./60.;
-TC = T-273;
-[AX,H1,H2] = plotyy(TC,Y,TC,-dYdT*dTdt*1000,'plot'); hold on
-
-set(gca,'FontName',Font_Name)
-set(AX(1),'FontName',Font_Name)
-set(AX(2),'FontName',Font_Name)
-xlabel('Temperature ($^\circ$C)','Interpreter','LaTeX','FontSize',Label_Font_Size)
-set(get(AX(1),'Ylabel'),'String','Mass Fraction','Interpreter','LaTeX','FontSize',Label_Font_Size) 
-set(get(AX(2),'Ylabel'),'String','Reaction Rate (s$^{-1}$) $\times$ 10$^3$','Interpreter','LaTeX','FontSize',Label_Font_Size) 
-set(AX(1),'YLim',[0 1.1])
-set(AX(2),'YLim',[0 2.2])
-set(AX(1),'YTickMode','manual')
-set(AX(2),'YTickMode','manual')
-set(AX(1),'YTick',[0 0.2 0.4 0.6 0.8 1.0])
-set(AX(2),'YTick',[0 0.4 0.8 1.2 1.6 2.0])
-set(H1,'LineStyle','-')
-set(H2,'LineStyle','-')
-
-line([300. 300.],[0.00 0.96],'LineStyle','--','Color','black','LineWidth',1)
-%line([300. 400.],[0.96 0.96],'LineStyle','--','Color','black','LineWidth',1)
-
-set(gcf,'Visible',Figure_Visibility);
-set(gcf,'PaperUnits',Paper_Units);
-set(gcf,'PaperSize',[Paper_Width Paper_Height]);
-set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
-print(gcf,'-dpdf','../../../Manuals/FDS_5_User_Guide/FIGURES/pyrolysis')
 
 
 
--- Utilities/Matlab/scripts/reaction_rate.m
+++ Utilities/Matlab/scripts/reaction_rate.m
@@ -11,18 +11,15 @@
 
 function r = reaction_rate(T,Y)
 
-% dTdt = 5./60.;
-% R0 = 8314.3;
-% T_0 = 300.+273.;
-% delta_T = 80.;
-% r_0=2*dTdt/delta_T;
-% E=r_0*R0*T_0*T_0/(dTdt*0.4);
-% A=(E./(R0.*T_0.*T_0))*dTdt*exp(E./(R0.*T_0));
-
 global dTdt
 global R0
 global E
 global A
+global residue
 
-r = -A.*Y*exp(-E./(R0.*T))./dTdt;
+r(1) = -A(1).*Y(1)*exp(-E(1)./(R0.*T))./dTdt;
+r(2) = -A(2).*Y(2)*exp(-E(2)./(R0.*T))./dTdt;
+r(3) = -residue(2)*r(2);
+r=r';
+
 
