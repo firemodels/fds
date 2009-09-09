3b56d83c3748c67e60091c6ddcad8f065348243b
mcgratta <mcgratta@gmail.com>
2009-09-09 22:04:18 +0000

    FDS Source: Change FDS version number to 5.4.1.
    Former-commit-id: b27506d29c2ea329b264a0e899f5d6d0a440d3a9 [formerly e4209b5f9c57f21b9e3b25078369016c1c5c9a13]
    Former-commit-id: eef3a18dbb977ed599aec4e218d0378aca944dab
    Former-commit-id: e0196dfd76d4498d4f67fb9516d12bce4d0c32e8

--- FDS_Source/main.f90
+++ FDS_Source/main.f90
@@ -56,7 +56,7 @@ WALL_CLOCK_START = WALL_CLOCK_TIME()
  
 ! Assign a compilation date, version number, revision number
 
-WRITE(VERSION_STRING,'(A)') '5.4.0'
+WRITE(VERSION_STRING,'(A)') '5.4.1'
 
 !! FDS6=.TRUE.
 !! WRITE(VERSION_STRING,'(A)') '6.0.0'
