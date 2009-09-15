2c664a0a6ab8302288c2aa4ec66b1f04a31678bf
shostikk <simo.hostikka@aalto.fi>
2009-09-15 10:10:11 +0000

    FDS Utilities: Add VTT versions of runfds and run_all scripts. The runfds_VTT.sh makes use of sge job control system (qsub).

---
+++ Utilities/Scripts/runfds_VTT.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -f
+scratchdir=$SVNROOT/Utilities/Scripts/tmp
+dir=$1
+infile=$2
+
+fulldir=$BASEDIR/$dir
+in=$infile.fds
+out=$infile.err
+stopfile=$infile.stop
+
+scriptfile=$scratchdir/script.$$
+if ! [ -e $FDS ];  then
+  echo "The file $FDS does not exit. Run aborted"
+  exit
+fi
+if ! [ -d $fulldir ]; then
+  echo "The directory $fulldir does not exit. Run aborted."
+  exit
+fi
+if ! [ -e $fulldir/$in ]; then
+  echo "The fds input  file, $fulldir/$in does not exit. Run aborted."
+  exit
+fi
+if [ $STOPFDS ]; then
+ echo "stopping case: $infile"
+ touch $fulldir/$stopfile
+ exit
+fi
+if [ -e $fulldir/$stopfile ]; then
+ rm $fulldir/$stopfile
+fi
+cat << EOF > $scriptfile
+#!/bin/bash -f
+cd $fulldir
+#$ -N Ver_'$infile'
+#$ -wd $fulldir
+#$ -e $out -o /dev/null 
+$FDS $in 
+EOF
+chmod +x $scriptfile
+echo Running $in 
+qsub $scriptfile
+rm $scriptfile
