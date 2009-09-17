ad134a2ce04186f3681944ced2c0c6e861b72f4b
gforney <gforney@gmail.com>
2009-09-17 17:28:25 +0000

    verification scripts: change runfds.csh back to runfds.sh in Run_All.sh
    Former-commit-id: 42d27c84c626a039ec09dc3fb8775c81be2df776 [formerly 5b436b2107bd7b7c149a658da7944718d7428118]
    Former-commit-id: 1e3eb5095a3818a0ed1a08fc2d19858ea6725219
    Former-commit-id: d3ed261f29e4fe61e2cf25833e9ff3a5cfe22064

--- Verification/Run_All.sh
+++ Verification/Run_All.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -f
 export SVNROOT=~/FDS-SMV
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds5_intel_linux_64
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.csh
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
 export BASEDIR=`pwd`
 
 ./FDS_Cases.csh
