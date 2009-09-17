14210bdd275086fc22afc6b397f19779a39ba5ba
gforney <gforney@gmail.com>
2009-09-17 17:18:40 +0000

    verification scripts: modify Runall scripts for .csh, .sh and .bat to run verification cases from a list.  The .sh and .csh Runall scripts uses the same list (FDS_Cases.csh) .  The .bat Runall script uses FDS_Cases.bat which is a trivial modification of FDS_Cases.csh (use any text editor to change all occurences of $FDS to %RUNFDS% .
    Former-commit-id: 2baec95b313d623b9612767b03c776b415d29232 [formerly cd64443bb4ba2134c26b75b5a8cff061f593c332]
    Former-commit-id: 4804cda59103b595c2ba0252210c26887d7c84c3
    Former-commit-id: 7c4c66d417e1efad8928cd473370fee3174f4dc2

--- Verification/Run_All.bat
+++ Verification/Run_All.bat
@@ -1,60 +1,11 @@
-cd Atmospheric_Effects
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Controls
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Detectors
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Evacuation
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Fires
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Flowfields
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Heat_Transfer
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Miscellaneous
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd NS_Analytical_Solution
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Pressure_Effects
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Pyrolysis
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Radiation
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Species
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Sprinklers_and_Sprays
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Timing_Benchmarks
-for %%i in (*.fds) do %1 %%i
-cd ..
-cd Visualization
-%1 colorconv.fds
-%1 plume5a.fds
-%1 plume5b.fds
-%1 plume5c.fds
-%1 plume5c_bounddef.fds
-%1 sillytexture.fds
-%1 script_test.fds
-%1 smoke_sensor.fds
-%1 smoke_test.fds
-%1 smoke_test2.fds
-%1 thouse5.fds
-cd ..
-cd Wui
-%1 fire_line.fds
+@echo off
+set svn_drive=d:
+set SVNROOT=d:\FDS-SMV
+set FDS=%SVNROOT%\FDS_Compilation\intel_win_32\fds5_win_32
+set RUNFDS=call %SVNROOT%\Utilities\Scripts\runfds_win32.bat
+set BASEDIR=%CD%
+%svn_drive%
+
+call ./FDS_Cases.bat
+
+echo FDS cases submitted
