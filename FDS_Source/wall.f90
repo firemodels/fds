cc18fb4873710b46d612cc347953b5411694c596
rmcdermo <randy.mcdermott@gmail.com>
2009-10-05 18:11:33 +0000

    FDS Source: fix initialization error for periodic test of velocity field with FDS6 (add MATCH_VELOCITY after initialization); also some clean up of FL calls in main and main_mpi
    Former-commit-id: b8731c24e8472ebd2bc0a40ca31ea3649fac5a7f [formerly a93f882bf52075fe69f532aa8a0f3e62ff7ef0c5]
    Former-commit-id: bcae1a62ce7cac6eb3a52bed2dc229ed3b4d22b9
    Former-commit-id: 6235fedee5869050f2ad398586ab7022c84b6d09

--- FDS_Source/wall.f90
+++ FDS_Source/wall.f90
@@ -570,33 +570,33 @@ WALL_CELL_LOOP: DO IW=1,NWC
 
    END SELECT METHOD_OF_MASS_TRANSFER
    
-   ! pure periodic bcs for single mesh case
-   IF (FISHPAK_BC(1)==0) THEN
-      RHOP(0,:,:) = RHOP(IBAR,:,:)
-      RHOP(IBP1,:,:) = RHOP(1,:,:)
-   ENDIF
-   IF (FISHPAK_BC(2)==0) THEN
-      RHOP(:,0,:) = RHOP(:,JBAR,:)
-      RHOP(:,JBP1,:) = RHOP(:,1,:)
-   ENDIF
-   IF (FISHPAK_BC(3)==0) THEN
-      RHOP(:,:,0) = RHOP(:,:,KBAR)
-      RHOP(:,:,KBP1) = RHOP(:,:,1)
-   ENDIF
-   DO N=1,N_SPECIES
-      IF (FISHPAK_BC(1)==0) THEN
-         YYP(0,:,:,N) = YYP(IBAR,:,:,N)
-         YYP(IBP1,:,:,N) = YYP(1,:,:,N)
-      ENDIF
-      IF (FISHPAK_BC(2)==0) THEN
-         YYP(:,0,:,N) = YYP(:,JBAR,:,N)
-         YYP(:,JBP1,:,N) = YYP(:,1,:,N)
-      ENDIF
-      IF (FISHPAK_BC(3)==0) THEN
-         YYP(:,:,0,N) = YYP(:,:,KBAR,N)
-         YYP(:,:,KBP1,N) = YYP(:,:,1,N)
-      ENDIF
-   ENDDO
+!   ! pure periodic bcs for single mesh case
+!   IF (FISHPAK_BC(1)==0) THEN
+!      RHOP(0,:,:) = RHOP(IBAR,:,:)
+!      RHOP(IBP1,:,:) = RHOP(1,:,:)
+!   ENDIF
+!   IF (FISHPAK_BC(2)==0) THEN
+!      RHOP(:,0,:) = RHOP(:,JBAR,:)
+!      RHOP(:,JBP1,:) = RHOP(:,1,:)
+!   ENDIF
+!   IF (FISHPAK_BC(3)==0) THEN
+!      RHOP(:,:,0) = RHOP(:,:,KBAR)
+!      RHOP(:,:,KBP1) = RHOP(:,:,1)
+!   ENDIF
+!   DO N=1,N_SPECIES
+!      IF (FISHPAK_BC(1)==0) THEN
+!         YYP(0,:,:,N) = YYP(IBAR,:,:,N)
+!         YYP(IBP1,:,:,N) = YYP(1,:,:,N)
+!      ENDIF
+!      IF (FISHPAK_BC(2)==0) THEN
+!         YYP(:,0,:,N) = YYP(:,JBAR,:,N)
+!         YYP(:,JBP1,:,N) = YYP(:,1,:,N)
+!      ENDIF
+!      IF (FISHPAK_BC(3)==0) THEN
+!         YYP(:,:,0,N) = YYP(:,:,KBAR,N)
+!         YYP(:,:,KBP1,N) = YYP(:,:,1,N)
+!      ENDIF
+!   ENDDO
 
    ! Only set species mass fraction in the ghost cell if it is solid
     
