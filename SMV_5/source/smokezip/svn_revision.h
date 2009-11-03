8db7e76dfc3634667d4b5183355ee0ac4f7d11f9
gforney <gforney@gmail.com>
2009-11-03 01:48:54 +0000

    smokezip: initial revision to add compression support for PLOT3D files (for large mult-mesh wui cases)
    Former-commit-id: f32a7056a7d2d23e6038e19805f602aebb9a2f3c [formerly c46e4a5459dfafdb73d1c1a90e9c11f89750522b]
    Former-commit-id: 747804576ab7e4c243d3b5d9552db5740239c173
    Former-commit-id: e99ebed01c5193cc33ee26fa5dc36863e64de3ba

--- SMV_5/source/smokezip/svn_revision.h
+++ SMV_5/source/smokezip/svn_revision.h
@@ -8,6 +8,7 @@ extern char CNVboundary_revision[];
 extern char CNViso_revision[];
 extern char CNVpart_revision[];
 extern char CNVslice_revision[];
+extern char CNVplot3d_revision[];
 extern char csphere_revision[];
 extern char dmalloc_revision[];
 extern char egz_stdio_revision[];
