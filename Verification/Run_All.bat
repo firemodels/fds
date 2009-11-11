6ae62dae869d63e1a3b345b133465e17591f1057
drjfloyd <drjfloyd@gmail.com>
2009-11-11 21:46:17 +0000

    FDS Source: Changes to data tables for liquid and gas properties.  Changes to initialization of liquid properties.  Allow specification of FUEL on REAC for MIXTURE_FRACTION.  Ties to thermophysical properties for liquid and gas for the FUEL (does not change stoichiometry)
    Former-commit-id: c0d9c768a30ff3cbd4a9dab5e5d032fea45f48a9 [formerly f3b8f465f03c17a9b762ecef35307de6fd73c189]
    Former-commit-id: a1a000194b4ae849bf9b0bf91d7a1d7df6c944bc
    Former-commit-id: c5753e2c4e0b2bc565d43cdd6e10dae6d7a983ea

--- Verification/Run_All.bat
+++ Verification/Run_All.bat
@@ -1,15 +1,13 @@
 @echo off
-set svn_drive=d:
-set SVNROOT=d:\FDS-SMV
-set FDS=%SVNROOT%\FDS_Compilation\intel_win_32\fds5_win_32
+set SVNROOT="C:\Projects\NIST 2821-000\SVN\FDS"
+set FDS="C:\Projects\NIST 2821-000\5.0source\c2\fds532.exe"
 set RUNFDS=call %SVNROOT%\Utilities\Scripts\runfds_win32.bat
 set BASEDIR=%CD%
-%svn_drive%
 
 echo You are about to run the Verification Test Suite.
 echo Press any key begin.
 pause > Nul
 
-call ./FDS_Cases.bat
+call FDS_Cases.bat
 
 echo FDS cases submitted
