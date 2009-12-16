d4dcfe8eaba37481248835030acfa70a3a84917c
gforney <gforney@gmail.com>
2009-12-16 02:54:46 +0000

    smokeview: parse smokeview devices with new parsing routine
    Former-commit-id: cad2cb0368ba3e3bb61e08a7cbd74b24dfcb9f3e [formerly e76c47e720f4a38d9b23c55f63a37372c1663281]
    Former-commit-id: a997aeab4bcd7ca1b4b87c1f37c3d462097bf304
    Former-commit-id: 970a13c1b3f5ce1058718d2f3d7ce7bde3d6fb6a

--- SMV_5/source/smokeview/string_util.c
+++ SMV_5/source/smokeview/string_util.c
@@ -357,46 +357,10 @@ void array2string(float *vals, int nvals, char *string){
 
 /* ------------------ array2string ------------------------ */
 
-void parse_string(char *string, char ***tokens_ptr, int *ntokens){
+void parse_string(char *string, char **tokens, int *ntokens){
   int i, len, in_quote, ntok, in_token, last_in_token;
   char *c;
-  char **tokens;
 
-  ntok=0;
-  in_quote=0;
-  in_token=0;
-  last_in_token=0;
-  len=strlen(string);
-  c=string;
-  for(i=0;i<=len;i++){
-    switch (*c) {
-      case '"':
-        in_quote=1-in_quote;
-        in_token=1;
-        break;
-      case ' ':
-        if(in_quote==0){
-          in_token=0;
-        }
-        break;
-      case 0:
-        in_token=0;
-        break;
-      default:
-        in_token=1;
-        break;
-    }
-    if(in_token>last_in_token)ntok++;
-    last_in_token=in_token;
-    c++;
-  }
-  if(ntok==0){
-    *ntokens=0;
-    *tokens_ptr=NULL;
-    return;
-  }
-  NewMemory((void **)&tokens,ntok*sizeof(char *));
-  *tokens_ptr=tokens;
   c=string;
   ntok=0;
   in_quote=0;
