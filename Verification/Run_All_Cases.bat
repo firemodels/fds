f863c4122294515dac6e1fc5a817e2b07aec874a
gforney <gforney@gmail.com>
2009-12-23 03:25:51 +0000

    verification: reorganize and rename fds/smokeview verificaiton scripts so that file names are clearer and case names for fds and smv are in distinct files
    Former-commit-id: 85452bf885f49d97cbf0663f95aaa681d2af88e7 [formerly af94758ea64a19ac575bad815f4159e89617afc1]
    Former-commit-id: 80f7ae321a404fa3faa79ee15051d43eaca33efa
    Former-commit-id: 743a6604eddc4f90a02665969def24cde540e03c

---
+++ Verification/Run_All_Cases.bat
@@ -0,0 +1,28 @@
+@echo off
+set svn_drive=d:
+
+set BASEDIR=%CD%
+set SVNROOT=%BASEDIR%\..\
+
+set FDS=..\FDS_Compilation\intel_win_32\fds5_win_32
+
+set RUNFDS=call ..\Utilities\Scripts\runfds_win32.bat
+
+echo You are about to run the Verification Test Suite.
+pause > Nul
+
+echo FDS cases submitted
+
+call FDS_Cases.bat
+
+cd %BASEDIR%
+
+Rem create a text file containing the FDS5 version used to run these tests.
+Rem This file is included in the smokeview user's guide
+
+set smvug="%CD%\..\Manuals\SMV_5_User_Guide\"
+echo | fds5 2> "%smvug%\figures\fds5.version"
+
+call SMV_Cases.bat
+
+pause
