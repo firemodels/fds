dac2559a352da697ec0f607c77a8a253e4e636cf
gforney <gforney@gmail.com>
2010-01-09 04:08:31 +0000

    build scripts: update fds win64 build scripts
    Former-commit-id: e459c73cee1f5a31c24c7df9cc90e01a0ef6b92a [formerly 77f97541d2fda27c0fcb1ecbcb97b0f15f9e2c3c]
    Former-commit-id: 7269dbc5a8b122ee7015deab2dcaa3d657b30dc8
    Former-commit-id: 984b16cf255fed8e41a6b25cb499ee1343a596b5

---
+++ FDS_Compilation/Scripts/TOWIN64_FDS_Intel_win_64.bat
@@ -0,0 +1,24 @@
+@echo off
+Title Building FDS for 32 bit Windows
+
+Rem Batch file used to build a 32 bit version of FDS
+
+set envfile="%homedrive%\%homepath%\fds_smv_env.bat"
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+pause
+
+%svn_drive%
+cd %svn_root%\FDS_Compilation\intel_win_64
+copy fds5_win_64.exe "c:\program files\fds\fds5\bin\fds5_win_64.exe"
+pause
