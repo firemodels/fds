a73add3b420d6cc70636aec7f77c534b9a808396
gforney <gforney@gmail.com>
2010-01-24 16:32:33 +0000

    build scripts: change  "c:\bin\make" back to "make" in batch files that build windows versions of FDS .  We now assume that the windows Make utility is an installed program and therefore in the path
    Former-commit-id: 1e790629f1dd26c6b6a56ad9e0cd98565fda57e7 [formerly 71cdaf45ca03ddd467939545e1bb21edf481c374]
    Former-commit-id: cb0dbcd846513f6265cb0d54cbab8754aed1eaf1
    Former-commit-id: 95c2f2838071685192709207652e48899c1690d0

--- FDS_Compilation/intel_win_32_db/make_fds.bat
+++ FDS_Compilation/intel_win_32_db/make_fds.bat
@@ -2,5 +2,5 @@ set intelbin=c:\bin
 
 call %intelbin%\ifortvars ia32
 
-%intelbin%\make VPATH="../../FDS_Source" -f ..\makefile intel_win_32_db
+make VPATH="../../FDS_Source" -f ..\makefile intel_win_32_db
 pause
