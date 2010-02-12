2fb1911ffc45c99b31aa56c1faa21a33aaa18a8c
gforney <gforney@gmail.com>
2010-02-12 20:43:37 +0000

    build scripts: simplify build scripts
    Former-commit-id: a07bc9ff6065aee6d550040533d7f16971483867 [formerly de226efebc0e27ae78c68819edff899ee1c3acaa]
    Former-commit-id: 2d54a546fb8a7c1bd5e3cb4defb745516bcde0d7
    Former-commit-id: 035dc1ec3fc7318d105397af9dbc884a26353f69

---
+++ SMV_5/scripts/UPLOADosxtest64.bat
@@ -0,0 +1,47 @@
+@echo off
+
+Rem Windows batch file to upload Smokeview osx test files to
+Rem the google download site.
+
+set envfile="%homedrive%\%homepath%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\SMV_5\for_bundle\to_google
+
+Rem ----------------------------------------------------------
+Rem should not need to edit any lines below
+
+set level=Release-4_Test
+
+set version=%smv_version%
+
+echo Uploading Smokeview %level% version=%smv_version% revision=%smv_revision%
+pause
+
+Rem --------------- 64 bit OSX ----------------
+
+  set glabels=Type-Archive,Opsys-OSX,%level%
+  set dplatform=64 bit OSX
+  set platform=osx64
+  set summary=Smokeview %smv_version% for %dplatform% (SVN r%smv_revision%)
+  set exe=smv_%version%_%smv_revision%_%platform%.tar.gz
+  echo.
+  echo Uploading %summary% - %exe%
+  echo googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none  -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
+       googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none  -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
+
+echo.
+echo Uploads complete
+pause
