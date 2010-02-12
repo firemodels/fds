e55c247ef34d00b4ac22c8095b3a1ba7fb7261ea
gforney <gforney@gmail.com>
2010-02-12 20:43:37 +0000

    build scripts: simplify build scripts
    Former-commit-id: a07bc9ff6065aee6d550040533d7f16971483867 [formerly de226efebc0e27ae78c68819edff899ee1c3acaa]
    Former-commit-id: 2d54a546fb8a7c1bd5e3cb4defb745516bcde0d7
    Former-commit-id: 035dc1ec3fc7318d105397af9dbc884a26353f69

---
+++ SMV_5/scripts/MAKEosxtestdists64.bat
@@ -0,0 +1,35 @@
+@echo off
+
+Rem  Windows batch file to create an OSX achive for an OSX test smokeview
+
+Rem setup environment variables (defining where repository resides etc) 
+
+set envfile="%homedrive%\%homepath%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\smv_5\scripts
+set version=%smv_version%
+
+set scriptdir=FDS-SMV/SMV_5/scripts
+set bundledir=FDS-SMV/SMV_5/for_bundle
+
+echo making OSX test distribution archive
+plink %svn_logon% %scriptdir%/make_test_osx_dists64.csh %version% %smv_revision%
+
+echo downloading OSX test distribution archive
+pscp %svn_logon%:%bundledir%/smv_%version%_%smv_revision%_osx64.tar.gz ..\for_bundle\to_google\.
+
+
+pause
