7608531c77fdeb4fa657006aabf6edb0a25162e8
gforney <gforney@gmail.com>
2010-02-13 12:43:19 +0000

    build scripts: update build scripts
    Former-commit-id: e465f8efd53e77703355a291045a2cf45fa35ae4 [formerly e5f4f8574e9770680026694a4c88b14ca701769f]
    Former-commit-id: dd8e1d7f9fefa54de9d02c956753a70d6a64f0b1
    Former-commit-id: 11b77d0e47259da95bb2055dd30ff6f0b52bc832

--- SMV_5/scripts/make_smv_release_win32.bat
+++ SMV_5/scripts/make_smv_release_win32.bat
@@ -19,7 +19,7 @@ mkdir %smvdir%
 copy smokeview.ini %smvdir%\smokeview.ini
 copy smokeview32_release.exe %smvdir%\smokeview.exe
 copy smokediff32_release.exe %smvdir%\smokediff.exe
-copy smokezip_release.exe %smvdir%\smokezip.exe
+copy smokezip32_release.exe %smvdir%\smokezip.exe
 copy objects.svo %smvdir%\.
 copy glew32.dll %smvdir%\.
 copy pthreadVC.dll %smvdir%\.
