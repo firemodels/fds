4bb5c665c210a7ed6a50cc19c9854c112624034f
gforney <gforney@gmail.com>
2010-02-13 04:17:11 +0000

    build scripts: update build scripts
    Former-commit-id: 56d732ffc9afeb9a75f606ebca04be594ff03ab2 [formerly 2b21a4abf1b57c5aeaa066546ec9801ef5c53e27]
    Former-commit-id: ea7be46bf97df8cedd6bc2d8c1cd8d4adc899ade
    Former-commit-id: c4b6075d8ece740700f7dee6d5cd4dfee3f59472

--- SMV_5/scripts/UPLOADreleasewin32.bat
+++ SMV_5/scripts/UPLOADreleasewin32.bat
@@ -1,8 +1,6 @@
 @echo off
 
-Rem Windows batch file to upload Smokeview test files to
-Rem the google download site.  This script assume that the Windows
-Rem batch file, MAKEtest.bat, has already been run.
+Rem Windows batch file to upload 32 bit Smokeview windows archive
 
 set envfile="%homedrive%\%homepath%"\fds_smv_env.bat
 IF EXIST %envfile% GOTO endif_envexist
@@ -16,14 +14,14 @@ goto:eof
 
 :endif_envexist
 
-  set level=Release-4_Test
+  set level=Release-3_Maintenance
   set upload_win32=1
   set upload_linux32=0
   set upload_osx32=0
 
 call %envfile%
 
-echo Uploading Smokeview %level% version=test revision=%smv_revision%
+echo Uploading Smokeview %level% version=%smv_version% revision=%smv_revision%
 pause
 
 %svn_drive%
@@ -34,14 +32,15 @@ if not %upload_win32% == 1 goto endif_win32
   set glabels=Type-Installer,Opsys-Windows,%level%
   set dplatform=32 bit Windows
   set platform=win32
-  set summary=Smokeview test for %dplatform% (SVN r%smv_revision%)
-  set exe=smv_test_%smv_revision%_%platform%.exe
+  set summary=Smokeview %version% for %dplatform% (SVN r%smv_revision%)
+  set exe=smv_%smv_version%_%platform%.exe
   echo.
   echo Uploading %summary% - %exe%
   echo googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
        googlecode_upload.py --passwd-file-dir %google_password_dir% --config-dir none -s "%summary%" -p fds-smv -u %google_username% -l %glabels% %exe%
 :endif_win32
 
+
 echo.
-echo Uploads complete
+echo Upload complete
 pause
