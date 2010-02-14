dcb6df70517a3600ae2d24151255ff93aff54187
gforney <gforney@gmail.com>
2010-02-14 21:40:19 +0000

    bulid scripts: pass smokeview make file fortran compiler library location using an environment variable (so the makefile is not hardwired)
    Former-commit-id: 8b37ee85e8da70a5d6a4641a8419a5e7ce4aee0c [formerly cd46b71e3c5c3cd6725fb747dfdecafd119f2fb6]
    Former-commit-id: 34d2b55a64f9987a1e91ab39ba856e7b9f8a909d
    Former-commit-id: 9265887a8ab7eb3f58f41146e5c19f4aabb7c964

--- SMV_5/Build/INTEL_WIN_32/make_smv.bat
+++ SMV_5/Build/INTEL_WIN_32/make_smv.bat
@@ -1,6 +1,4 @@
-set intelbin="%IFORT_COMPILER11%\bin"
-
-call %intelbin%\ifortvars ia32
-call %intelbin%\iclvars ia32
-make -f ..\Makefile intel_win_32
+call "%IFORT_COMPILER11%\bin\ifortvars" ia32
+call "%IFORT_COMPILER11%\bin\iclvars" ia32
+make WIN32FORTLIBDIR="%IFORT_COMPILER11%\lib" -f ..\Makefile intel_win_32
 pause
--- SMV_5/Build/INTEL_WIN_64/make_smv.bat
+++ SMV_5/Build/INTEL_WIN_64/make_smv.bat
@@ -1,6 +1,4 @@
-set intelbin="%IFORT_COMPILER11%\bin"
-
-call %intelbin%\ifortvars intel64
-call %intelbin%\iclvars intel64
-make -f ..\Makefile intel_win_64
+call "%IFORT_COMPILER11%\bin\ifortvars" intel64
+call "%IFORT_COMPILER11%\bin\iclvars" intel64
+make WIN64FORTLIBDIR="%IFORT_COMPILER11%\lib" -f ..\Makefile intel_win_64
 pause
