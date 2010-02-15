7b72fb72dea0c7654b25b4f009c33392e5c6161e
gforney <gforney@gmail.com>
2010-02-15 19:08:28 +0000

    build scripts: fix the win 32 bit Makefile entry so that smokeview can be built from the command line
    Former-commit-id: 06f3d835e5f7a1e25e7fda66fcabe29d949796ef [formerly 4fd007f722374247990e9a936f155de4c46f5070]
    Former-commit-id: 2dad87bb4590720c8493599b0c5ea7bda26e7fab
    Former-commit-id: 08bf63b2011d0807ad41b8e7a95768e2631a8404

--- SMV_5/Build/INTEL_WIN_32/make_smv.bat
+++ SMV_5/Build/INTEL_WIN_32/make_smv.bat
@@ -1,4 +1,16 @@
+@echo off
+
+Rem windows batch file to build smokeview from the command line
+
+IF EXIST "%IFORT_COMPILER11%" GOTO endif_envexist
+echo *** error:  Environment variable IFORT_COMPILER11 not defined.
+echo             Compilation aborted
+pause>NUL
+goto:eof
+:endif_envexist
+
 call "%IFORT_COMPILER11%\bin\ifortvars" ia32
 call "%IFORT_COMPILER11%\bin\iclvars" ia32
-make WIN32FORTLIBDIR="%IFORT_COMPILER11%\lib" -f ..\Makefile intel_win_32
+
+make -f ..\Makefile intel_win_32
 pause
