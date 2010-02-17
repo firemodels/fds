578de6378d90bce6827a292ce82fc0483fe4fd31
gforney <gforney@gmail.com>
2010-02-17 21:26:11 +0000

    build scripts: update smokeview 64 bit win makefile entry

--- SMV_5/Build/INTEL_WIN_64/make_smv.bat
+++ SMV_5/Build/INTEL_WIN_64/make_smv.bat
@@ -1,4 +1,17 @@
+@echo off
+
+Rem windows batch file to build smokeview from the command line
+
+IF EXIST "%SETUP_IFORT_COMPILER11%" GOTO endif_envexist
+IF EXIST "%IFORT_COMPILER11%" GOTO endif_envexist
+echo *** error:  Environment variable IFORT_COMPILER11 not defined.
+echo             Compilation aborted
+pause>NUL
+goto:eof
+:endif_envexist
+set SETUP_IFORT_COMPILER11=1
+
 call "%IFORT_COMPILER11%\bin\ifortvars" intel64
 call "%IFORT_COMPILER11%\bin\iclvars" intel64
-make WIN64FORTLIBDIR="%IFORT_COMPILER11%\lib" -f ..\Makefile intel_win_64
+make -f ..\Makefile intel_win_64
 pause
