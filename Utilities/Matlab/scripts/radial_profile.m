0c567079dc95bb0b8813d0645d4624d94ad53bd0
rmcdermo <randy.mcdermott@gmail.com>
2010-02-23 16:06:16 +0000

    Matlab: updates for helium plume scripts
    Former-commit-id: f4c644804ebd438aa0f1341063f8efe28f3a6a56 [formerly cde65b403d49ac27679614a9e6b704538e62f953]
    Former-commit-id: 6e06fe6e4d01de60ec68a54c5e305158518ab4df
    Former-commit-id: c7d32a642add26dd5277abf64e059068a606dd25

--- Utilities/Matlab/scripts/radial_profile.m
+++ Utilities/Matlab/scripts/radial_profile.m
@@ -7,7 +7,7 @@
 % Example:
 %
 % function []=radial_profile(plot_file,devc_col,error_frac, ...
-%                            xaxis_label,yaxis_label, ...
+%                            xaxis_label,yaxis_label,title_label,text_label,legend_pos, ...
 %                            rmin,rmax,dr,xmin,xmax,dx,ymin,ymax,dy, ...
 %                            exp_file,exp_format,exp_label, ...
 %                            fds_file1,fds_format1,fds_label1, ...
@@ -17,7 +17,7 @@
 
 function []=radial_profile(varargin)
 
-if nargin<1|nargin>29; 
+if nargin<1|nargin>32; 
     display('Error in argument list')
 end
 if nargin>=1
@@ -27,42 +27,43 @@ if nargin>=1
     error_frac  = varargin{3};
     xaxis_label = varargin{4};
     yaxis_label = varargin{5};
-    text_label  = varargin{6};
-    legend_pos  = varargin{7};
-    rmin        = varargin{8};
-    rmax        = varargin{9};
-    dr          = varargin{10};
-    xmin        = varargin{11};
-    xmax        = varargin{12};
-    dx          = varargin{13};
-    ymin        = varargin{14};
-    ymax        = varargin{15};
-    dy          = varargin{16};
-    exp_file    = varargin{17};
-    exp_format  = varargin{18};
-    exp_label   = varargin{19};
-    if nargin>19
-        fds_file1   = varargin{20};
-        fds_format1 = varargin{21};
-        fds_label1  = varargin{22};
+    title_label = varargin{6};
+    text_label  = varargin{7};
+    legend_pos  = varargin{8};
+    rmin        = varargin{9};
+    rmax        = varargin{10};
+    dr          = varargin{11};
+    xmin        = varargin{12};
+    xmax        = varargin{13};
+    dx          = varargin{14};
+    ymin        = varargin{15};
+    ymax        = varargin{16};
+    dy          = varargin{17};
+    exp_file    = varargin{18};
+    exp_format  = varargin{19};
+    exp_label   = varargin{20};
+    if nargin>20
+        fds_file1   = varargin{21};
+        fds_format1 = varargin{22};
+        fds_label1  = varargin{23};
         nfds        = 1;
     end
-    if nargin>22
-        fds_file2   = varargin{23};
-        fds_format2 = varargin{24};
-        fds_label2  = varargin{25};
+    if nargin>23
+        fds_file2   = varargin{24};
+        fds_format2 = varargin{25};
+        fds_label2  = varargin{26};
         nfds        = 2;
     end
-    if nargin>25
-        fds_file3   = varargin{26};
-        fds_format3 = varargin{27};
-        fds_label3  = varargin{28};
+    if nargin>26
+        fds_file3   = varargin{27};
+        fds_format3 = varargin{28};
+        fds_label3  = varargin{29};
         nfds        = 3;
     end
-    if nargin>28
-        fds_file4   = varargin{29};
-        fds_format4 = varargin{30};
-        fds_label4  = varargin{31};
+    if nargin>29
+        fds_file4   = varargin{30};
+        fds_format4 = varargin{31};
+        fds_label4  = varargin{32};
         nfds        = 4;
     end
 end
@@ -72,6 +73,7 @@ M = csvread(exp_file,1,0);
 r = M(:,1);
 w = M(:,2);
 e = error_frac*abs(w); % percent error matches DesJardin paper
+e(2:2:length(e)) = 0;  % remove every other error bar for clarity
 H(1)=errorbar(r,w,e,exp_format);
 
 % FDS data
@@ -109,7 +111,10 @@ if nfds>=4
 end
 
 xt = xmin + .05*(xmax-xmin);
-yt = ymin + .9*(ymax-ymin);
+yt = ymin + .92*(ymax-ymin);
+text(xt,yt,title_label,'FontSize',14,'Interpreter','LaTeX')
+xt = xmin + .05*(xmax-xmin);
+yt = ymin + .84*(ymax-ymin);
 text(xt,yt,text_label,'FontSize',14,'Interpreter','LaTeX')
 
 plot_style
@@ -118,7 +123,7 @@ set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Title_Font_Size)
 
-xlabel('radial position (m)')
+xlabel(xaxis_label)
 ylabel(yaxis_label)
 axis([xmin xmax ymin ymax])
 set(gca,'YTick',ymin:dy:ymax)
@@ -132,6 +137,7 @@ if nfds==2; h = legend(H,exp_label,fds_label1,fds_label2,'Location',legend_pos);
 if nfds==3; h = legend(H,exp_label,fds_label1,fds_label2,fds_label3,'Location',legend_pos); end
 if nfds==4; h = legend(H,exp_label,fds_label1,fds_label2,fds_label3,fds_label4,'Location',legend_pos); end    
 set(h,'Interpreter','LaTeX')
+legend boxoff
 
 % print to pdf
 set(gcf,'PaperUnits',Paper_Units);
