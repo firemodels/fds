e8bf8ee637a9dfcb4803e03c06e5b4d0a9e8af63
rmcdermo <randy.mcdermott@gmail.com>
2010-03-05 22:49:03 +0000

    Matlab: updates for methane pool fire
    Former-commit-id: 0d6f3854f73f72fafe6b7ef9cae6a12b9356984b [formerly 6a285ec3476817257b6a7a755f067310141a3de9]
    Former-commit-id: 1adacc2a79ee5f5526429305b476714aee1d2cf2
    Former-commit-id: 918a28141d33ad76645f2fb09c587839a2eaa92a

---
+++ Utilities/Matlab/scripts/velocity_signal.m
@@ -0,0 +1,48 @@
+% McDermott
+% 3-5-10
+% velocity_signal.m
+
+function []=velocity_signal(devc_file,devc_col,vel_style,tmin,tmax,vmin,vmax,xaxis_title,yaxis_title, ...
+                            title_label,text_label,signal_file)
+
+plotdir = ['../../Manuals/FDS_5_Validation_Guide/FIGURES/'];
+datadir = ['../../Validation/Sandia_Plumes/FDS_Output_Files/'];
+
+M = csvread([datadir,devc_file],2,0);
+
+range = find(M(:,1)>tmin & M(:,1)<tmax);
+
+t = M(range,1);
+W = M(range,devc_col);
+
+plot_style
+
+plot(t,W,vel_style)
+
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
+set(gca,'FontName',Font_Name)
+set(gca,'FontSize',Title_Font_Size)
+
+xlabel(xaxis_title,'Interpreter','LaTeX','FontSize',Label_Font_Size)
+ylabel(yaxis_title,'Interpreter','LaTeX','FontSize',Label_Font_Size)
+axis([tmin tmax vmin vmax ])
+
+xt = tmin + .05*(tmax-tmin);
+yt = vmin + .92*(vmax-vmin);
+text(xt,yt,title_label,'FontSize',14,'Interpreter','LaTeX')
+xt = tmin + .05*(tmax-tmin);
+yt = vmin + .84*(vmax-vmin);
+text(xt,yt,text_label,'FontSize',14,'Interpreter','LaTeX')
+
+set(gca,'YTick',vmin:1:vmax)
+%set(gca,'YMinorTick','on')
+set(gca,'XTick',tmin:1:tmax)
+%set(gca,'XMinorTick','on')
+
+% print to pdf
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf',[plotdir,'Sandia_Plumes/',signal_file])
+
