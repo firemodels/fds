b073fb73d5b3a4de54726b0be3cecc43e9292fd7
gforney <gforney@gmail.com>
2010-03-18 18:18:02 +0000

    bundle: update smokeview bundle scripts
    Former-commit-id: 8c5b9a6e27740b6c0ac6cef7aabadc6116e1a066 [formerly 5fb11ea1f7b340e6e2dc8e01b2f88724080aa242]
    Former-commit-id: cd1276153aaa36f704376b45e3b0f055b7c77907
    Former-commit-id: c8b2015620068652c6b23b2d2cf5c3e4ffd57adb

--- SMV_5/scripts/MAKEdistlinux32.bat
+++ SMV_5/scripts/MAKEdistlinux32.bat
@@ -28,6 +28,6 @@ echo making Linux archives
 plink %svn_logon% %scriptdir%/make_linux_dist.csh %version% %smv_revision%
 
 echo downloading Linux Smokeview files
-pscp %svn_logon%:%bundledir%/smv_%version%_%smv_revision%_linux.tar.gz ..\for_bundle\to_google\.
+pscp %svn_logon%:%bundledir%/smv_%version%_linux32.tar.gz ..\for_bundle\to_google\.
 
 pause
