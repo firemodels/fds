578dcb3d3fc000748c2555cf24ad8a3896c4b0c5
gforney <gforney@gmail.com>
2010-03-18 00:34:56 +0000

    smokeview source: bump version number to 1.2.6
    Former-commit-id: 4895ed0422bff183f77a95683b6c52dbd0033da4 [formerly 7f1219a0de5793af5367bb14ddf53345664dae56]
    Former-commit-id: 3500350642a7fae15e06a89595443727bde0608e
    Former-commit-id: 94c92197d3bdc7d8c5d990228d9d8c4ee37b3021

--- SMV_5/source/smokezip/main.c
+++ SMV_5/source/smokezip/main.c
@@ -17,7 +17,7 @@
 
 #define FORTREAD(read) fseek(BOUNDARYFILE,4,SEEK_CUR);returncode=read;fseek(BOUNDARYFILE,4,SEEK_CUR);
 
-//dummy change to bump version number to 1.2.5
+//dummy change to bump version number to 1.2.6
 
 // svn revision character string
 char main_revision[]="$Revision$";
