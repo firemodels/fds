0d95d1761987a4c831880bbde2ac269efb33664f
shostikk <simo.hostikka@aalto.fi>
2010-03-25 10:20:11 +0000

    FDS Verification: Fix verification scripts.
    Former-commit-id: d33b7a3668882a82ca6bcbbe6e451b46066222ce [formerly c392df92fc9e02aad04c722ef884dfedb64b14cc]
    Former-commit-id: 391ba9e72669a796fc314a49720f84eee6ac36ee
    Former-commit-id: 1819948db92cc35f3be776e67161a53a0a88412e

--- Verification/Run_All.sh
+++ Verification/Run_All.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -f
 export SVNROOT=~/FDS-SMV
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds5_intel_linux_64
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds_sge.csh
 export BASEDIR=`pwd`
 
 ./FDS_Cases.csh
 
 echo FDS cases submitted
+
+cd $BASEDIR/SMV_Scripts
+
+./SMV_Cases.csh
+
+echo SMV cases submitted
