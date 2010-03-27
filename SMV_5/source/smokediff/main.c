96b5838be8435b6954b2394461cdf00711e6845d
gforney <gforney@gmail.com>
2010-03-27 15:57:56 +0000

    smokediff source: bump version number to 1.0.3

--- SMV_5/source/smokediff/main.c
+++ SMV_5/source/smokediff/main.c
@@ -11,7 +11,7 @@
 #include "svdiff.h"
 #include "MALLOC.h"
 
-//dummy change to bump version number to 1.0.2
+//dummy change to bump version number to 1.0.3
 
 // svn revision character string
 char main_revision[]="$Revision$";
