6caab750e46cea1b3362ab0c3e610f599d8b5914
gforney <gforney@gmail.com>
2010-04-10 21:48:57 +0000

    fds2ascii: make fds2ascii names consistent
    Former-commit-id: e6d877b3a6927ea6d5864d521042289f812b7f79 [formerly 8b43f708f223e942d38b5e362bc80b2731a8589e]
    Former-commit-id: cf8a30aa81ba1b44d27892e1d0b2de9256b70b38
    Former-commit-id: 2b518535148ae4c1ce7226bcc4693709d5052d09

--- Utilities/Scripts/BUNDLE_win_generic.bat
+++ Utilities/Scripts/BUNDLE_win_generic.bat
@@ -50,7 +50,8 @@ if "%platform%"=="64" copy %in_smv%\smokediff%platform%_release.exe   %out_bin%\
 if "%platform%"=="32" copy %in_smv%\smokezip%platform%_release.exe   %out_bin%\smokezip.exe
 if "%platform%"=="64" copy %in_smv%\smokezip%platform%_release.exe   %out_bin%\smokezip_win_64.exe
 
-copy %in_fds2ascii%\fds2ascii.exe     %out_bin%\.
+if "%platform%"=="32" copy %in_fds2ascii%\fds2ascii_win32\fds2ascii_win_32.exe     %out_bin%\fds2ascii.exe
+if "%platform%"=="64" copy %in_fds2ascii%\fds2ascii_win64\fds2ascii_win_64.exe     %out_bin%\fds2ascii_win_64.exe
 
 echo.
 echo Copying auxillary files to the bin directory
--- Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
+++ Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
@@ -1,4 +1,4 @@
 call "%IFORT_COMPILER11%"\bin\ifortvars ia32_intel64
 
-ifort -o fds2ascii_win64.exe ..\..\Data_Processing\fds2ascii.f90
+ifort -o fds2ascii_win_64.exe ..\..\Data_Processing\fds2ascii.f90
 pause
