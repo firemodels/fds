14be4c4e766678989e12d249274336410f78bcc2
gforney <gforney@gmail.com>
2010-04-10 21:23:46 +0000

    fds2ascii: update scripts to build fds2ascii
    Former-commit-id: 4781191f188aa1acb660d59437effcb17f64b0b3 [formerly 013a2e0fedc4e2c6587eb1391a54b914503ba037]
    Former-commit-id: 313d85426f51d543358d728e6044bfb0a4f912b5
    Former-commit-id: d5dd155e6d092e9066391abcd8c7ad99c6907c40

---
+++ Utilities/fds2ascii/intel_win_32/make_fds2ascii.bat
@@ -0,0 +1,4 @@
+call "%IFORT_COMPILER11%"\bin\ifortvars ia32
+
+ifort -o fds2ascii_win32.exe ..\..\Data_Processing\fds2ascii.f90
+pause
--- Utilities/fds2ascii/intel_win_32/make_fds2ascii_win32.bat
+++
@@ -1,6 +0,0 @@
-call "%IFORT_COMPILER11%"\bin\ifortvars ia32
-
-erase *.obj
-erase *.mod
-ifort -o ../fds2ascii_win32.exe ..\..\Data_Processing\fds2ascii.f90
-pause
