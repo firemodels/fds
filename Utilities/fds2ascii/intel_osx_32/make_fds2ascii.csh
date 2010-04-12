6094461011f5b1f0ddce8c1bcad53845151f0b5a
gforney <gforney@gmail.com>
2010-04-12 03:20:10 +0000

    fds2ascii: use a makefile to build fds2ascii
    Former-commit-id: 196835acd653fe580613e0b07d216c3f1225e441 [formerly 528ebe282e0bfab3caf12132dc679da50e3289ee]
    Former-commit-id: eb8adcd724a9174326cc6c003834008a18e1ae33
    Former-commit-id: cd2bde3f59876617a3d89edcc6bb411cbd568ccf

--- Utilities/fds2ascii/intel_osx_32/make_fds2ascii.csh
+++ Utilities/fds2ascii/intel_osx_32/make_fds2ascii.csh
@@ -2,4 +2,4 @@
 
 source $IFORT_COMPILER11/bin/ifortvars.csh ia32
 
-ifort -o fds2ascii_osx_32 ../../Data_Processing/fds2ascii.f90
+make -f ../Makefile intel_osx_32
--- Utilities/fds2ascii/intel_win_32/make_fds2ascii.bat
+++ Utilities/fds2ascii/intel_win_32/make_fds2ascii.bat
@@ -1,4 +1,4 @@
 call "%IFORT_COMPILER11%"\bin\ifortvars ia32
 
-ifort -o fds2ascii_win_32.exe ..\..\Data_Processing\fds2ascii.f90
+make -f ..\Makefile intel_win_32
 pause
