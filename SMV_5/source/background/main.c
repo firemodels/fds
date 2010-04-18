526b8d097d06ec7c01c1e019d2b829d03725ee25
gforney <gforney@gmail.com>
2010-04-18 01:04:19 +0000

    background source: initial revision
    Former-commit-id: 5c3e3368348f960809a979f5aa67ce98de110cbf [formerly edf40d1791a6a74921ede599d9c39b2bcccfd94c]
    Former-commit-id: b46ce980ebff2c7299f1a654b61711ad791ddd43
    Former-commit-id: c3ac15fcad58cae00acfd58b3b949884672f74eb

---
+++ SMV_5/source/background/main.c
@@ -0,0 +1,113 @@
+// $Date: 2010-04-11 20:48:06 -0400 (Sun, 11 Apr 2010) $ 
+// $Revision: 6048 $
+// $Author: gforney $
+
+#define INMAIN
+#include "options.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <process.h>
+#include <windows.h>
+#include "svn_revision.h"
+#include "background.h"
+
+//dummy change to bump version number to 0.9
+
+// svn revision character string
+char main_revision[]="$Revision: 6048 $";
+void run_command(void);
+
+void usage(char *prog);
+
+/* ------------------ main ------------------------ */
+
+int main(int argc, char **argv){
+  char *prog;
+  int i;
+  int argstart=-1;
+  float delay_time=0.0;
+  int itime;
+  char *arg;
+  char *command;
+  char *command_arg;
+
+  prog=argv[0];
+
+  if(argc==1){
+    version();
+    return 1;
+  }
+
+  for(i=1;i<argc;i++){
+    int lenarg;
+
+    arg=argv[i];
+    lenarg=strlen(arg);
+    if(arg[0]=='-'){
+      if(lenarg>1){
+        switch(arg[1]){
+	        case 'd':
+            i++;
+            if(i<argc){
+              arg=argv[i];
+              sscanf(arg,"%f",&delay_time);
+              if(delay_time<0.0)delay_time=0.0;
+            }
+		        break;
+          case 'h':
+            usage(prog);
+            return 1;
+            break;
+          default:
+            printf("Unknown option: %s\n",arg);
+            usage(prog);
+            return 1;
+            break;
+	      }
+      }
+    }
+    else{
+      argstart=i;
+      break;
+
+    }
+  }
+
+  if(argstart<0)return 0;
+
+  itime = delay_time*1000;
+  command=argv[argstart];
+  if(delay_time>0.0){
+    printf("In %f seconds, executing command: %s\n",delay_time,command);
+  }
+  else{
+    printf("Executing command: %s\n",command);
+  }
+
+  Sleep(itime);
+  printf("before spawn\n");
+
+  _spawnvp(_P_NOWAIT,command, argv+argstart);
+
+
+  printf("background exiting\n");
+
+  return 0;
+}
+
+void usage(char *prog){
+  char prog_version[100];
+  int svn_num;
+
+  getPROGversion(prog_version);  // get version (ie 5.x.z)
+  svn_num=getmaxrevision();    // get svn revision number
+
+  printf("\n");
+  printf("  background %s(%i) - %s\n\n",prog_version,svn_num,__DATE__);
+  printf("  Runs a windows job in the background\n\n");
+  printf("  %s",prog);
+  printf(" [-d delay time (s) ] prog prog_arguments\n\n");
+
+  printf("  -d dtime - wait dtime seconds before running program prog in the background\n");
+}
