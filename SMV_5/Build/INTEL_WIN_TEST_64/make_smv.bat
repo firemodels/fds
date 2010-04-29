281b72c12c3bb6096eec7b22c87917668065b4f4
gforney <gforney@gmail.com>
2010-04-29 02:24:47 +0000

    build scripts: update scripts for building windowsversions of smokeview from the command line

--- SMV_5/Build/INTEL_WIN_TEST_64/make_smv.bat
+++ SMV_5/Build/INTEL_WIN_TEST_64/make_smv.bat
@@ -7,8 +7,8 @@ IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
 set SETUP_IFORT_COMPILER11=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER11%\bin\ifortvars" ia32
-call "%IFORT_COMPILER11%\bin\iclvars" ia32
+call "%IFORT_COMPILER11%\bin\ifortvars" intel64
+call "%IFORT_COMPILER11%\bin\iclvars" intel64
 :envexist
-make -j4 -f ..\Makefile intel_win_32
+make -j4 -f ..\Makefile intel_win_test_64
 pause
