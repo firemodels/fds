6b7443da62643dd24c216628d0fd157d61012333
gforney <gforney@gmail.com>
2010-04-29 15:27:26 +0000

    build scripts: update wndows smokeview build scripts to build a 64 bit smokeview on a 32 bit platform
    Former-commit-id: bb3ee48348b643be3d172d0454c54d434aec327d [formerly 00113196888e7b596cc59f8ef871bc1167a8ce28]
    Former-commit-id: 94a8e1335d7dd140232fb1ca0a8fc1d1da567531
    Former-commit-id: 813f3d3333d2ba3b3b5bbd6c0574d741275bc0af

---
+++ SMV_5/scripts/MAKEtestsmvwin64on32.bat
@@ -0,0 +1,34 @@
+@echo off
+
+Rem  Windows batch file to build a test Smokeview for Windows 64
+
+Rem setup environment variables (defining where repository resides etc) 
+
+set envfile="%homedrive%\%homepath%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+echo Using the environment variables:
+echo.
+echo Using SVN revision %smv_revision% to build a test 64 bit Windows Smokeview
+
+%svn_drive%
+cd %svn_root%\smv_5\source\smokeview
+svn -r %smv_revision% update
+
+cd %svn_root%\smv_5\Build\INTEL_WIN_TEST_64
+erase *.obj
+make_smv64on32
+
+echo.
+echo compilation complete
+pause
