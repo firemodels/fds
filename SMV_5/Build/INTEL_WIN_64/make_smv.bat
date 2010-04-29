fbed765c800ccd5c6f48f093cc1eca91a2175ea3
gforney <gforney@gmail.com>
2010-04-29 02:24:47 +0000

    build scripts: update scripts for building windowsversions of smokeview from the command line
    Former-commit-id: aa763cf0561bbe69e4b833c9552a1793d6e6a3bd [formerly 092a95c6a3f8af99f82557bf2fe13ffbd639df59]
    Former-commit-id: 85cf93e26369c9d9fa2f23da586b83ea31257efa
    Former-commit-id: 876b4e879a110486908d79c1c9d59fa696a3e09a

--- SMV_5/Build/INTEL_WIN_64/make_smv.bat
+++ SMV_5/Build/INTEL_WIN_64/make_smv.bat
@@ -10,5 +10,5 @@ echo Setting up compiler environment
 call "%IFORT_COMPILER11%\bin\ifortvars" intel64
 call "%IFORT_COMPILER11%\bin\iclvars" intel64
 :envexist
-make -f ..\Makefile intel_win_64
+make -j4 -f ..\Makefile intel_win_64
 pause
--- SMV_5/Build/INTEL_WIN_TEST_32/make_smv.bat
+++ SMV_5/Build/INTEL_WIN_TEST_32/make_smv.bat
@@ -10,5 +10,5 @@ echo Setting up compiler environment
 call "%IFORT_COMPILER11%\bin\ifortvars" ia32
 call "%IFORT_COMPILER11%\bin\iclvars" ia32
 :envexist
-make -j4 -f ..\Makefile intel_win_32
+make -j4 -f ..\Makefile intel_win_test_32
 pause
