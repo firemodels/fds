9fa2433b57af4b5335901b03512e8ddf65862bb7
gforney <gforney@gmail.com>
2010-05-12 00:30:25 +0000

    build scripts: minor edits to several make_fds2 batch files
    Former-commit-id: d968ceb58e96b19e28b328180ad1dc3f5a8763b3 [formerly 0a3e1f56da958a404bc5fd452160d4af1b34955b]
    Former-commit-id: 31787234f6346e16ff72c5b9f037e4f79566eebd
    Former-commit-id: 7edb31bb32f10d1a5275c4f83c895ca7e0007afb

--- FDS_Compilation/intel_win_32/make_fds2.bat
+++ FDS_Compilation/intel_win_32/make_fds2.bat
@@ -2,5 +2,5 @@ set intelbin="%IFORT_COMPILER11%\bin"
 
 call %intelbin%\ifortvars ia32
 
-make -j2 VPATH="../../FDS_Source" -f ..\makefile intel_win_32
+make -j4 VPATH="../../FDS_Source" -f ..\makefile intel_win_32
 pause
