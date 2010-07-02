43fe0ca9b860868b7e3a3d6643ed56a01d60276a
gforney <gforney@gmail.com>
2010-07-02 18:32:51 +0000

    background source: update background source to enable remote use
    Former-commit-id: c1a5c63fcd5ded84d997fb571c862da11daf56df [formerly 58986c07bb2a19315f1a1b01ccf1dc3b5e242da2]
    Former-commit-id: ec4d30a541c554562f353d30312f13235ef78be1
    Former-commit-id: 7985d34a0fa4a736769023079e67169717ac20ff

--- SMV_5/source/background/background.h
+++ SMV_5/source/background/background.h
@@ -11,8 +11,13 @@ void getPROGversion(char *PROGversion);
 int getmaxrevision(void);
 void version(void);
 int getrevision(char *svn);
+char *hostlistfile;
+char *host;
+int nhostinfo;
 
+typedef struct {
+  char *hostname;
+  int ncores;
+} hostdata;
 
-
-
-
+hostdata *hostinfo;
