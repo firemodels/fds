772793fe41c1d4b69002b43f0ad249fcd82e4174
gforney <gforney@gmail.com>
2010-07-22 17:48:55 +0000

    build scripts: clean up fds_smv_env script and add entry for OSX hostname
    Former-commit-id: 20a585828814992d5b02f07686ed7ef63f6dfe0b [formerly ba4c4d4c99f5ab2a006aea79aa7b86bf2e9844f6]
    Former-commit-id: a4224b949aac26574d51bc682dd2f53ca2949e21
    Former-commit-id: c22aaa08f093a31759c8cf70da314ce662c42597

--- SMV_5/scripts/fds_smv_env.bat
+++ SMV_5/scripts/fds_smv_env.bat
@@ -1,12 +1,10 @@
 @echo off
-Rem ---------- SVN repository locations ------------
+Rem ---------- FDS-Smokeview repository settings ------------
 
 set svn_root=d:\fds-smv
 set svn_drive=d:
 set linux_svn_root=FDS-SMV
 
-Rem put google password into gc.password in directory below
-set google_password_dir=d:\gpf_home\
 
 Rem ------ FDS/Smokeview version and revision numbers ---------
 
@@ -15,15 +13,17 @@ set smv_revision=5868
 set fds_version=5.5
 set fds_revision=5545
 
-Rem VVVVVVVVVVVV  should not need to change settings below VVVVVVVVVVVVVVVVVVVVV
-
 Rem ---------- User/Host names -----------------
 
 set linux_username=%username%
 set google_username=%username%
 set linux_hostname=acrux.cfr.nist.gov
+set OSXHOST=tiger.cfr.nist.gov
 
 Rem ----------- for uploading to Google Code -----------------
+
+Rem google password file
+set google_password_dir=d:\gpf_home\
 
 set fds_google_level=Release-3_Maintenance
 set smv_google_level=Release-3_Maintenance
