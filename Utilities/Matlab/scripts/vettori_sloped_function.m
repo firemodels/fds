914e733ecebff669c3ca4ccfc1142c79bd5ade04
Taylor Myers <tmacksmyers@gmail.com>
2010-07-26 15:53:50 +0000

    FDS Validation: Fixed Vettori Sloped scripts

--- Utilities/Matlab/scripts/vettori_sloped_function.m
+++ Utilities/Matlab/scripts/vettori_sloped_function.m
@@ -1,10 +1,10 @@
-function[D] = vettori_sloped_function(file)
+function[T] = vettori_sloped_function(file)
 text=fileread(file);
 for j =1:4
     name = ['TC_',num2str(j),'_1'];
     f1=findstr(name , text);
-    D=textscan(text(f1(end)+30:f1(end)+40),'%[^ ]');
+    C=textscan(text(f1(end)+30:f1(end)+40),'%[^ ]');
     format ('short', 'g');
-    T(j,1)=str2num(D{1}{1});
+    T(j,1)=str2num(C{1}{1});
 end
 format
