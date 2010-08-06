6b243d43be87c78daed3609e4f806992fff7484d
mcgratta <mcgratta@gmail.com>
2010-08-06 14:43:37 +0000

    FDS Validation: Minor fix to Vettori_Sloped files.
    Former-commit-id: 8531a3ad49d042baf405cc5cf43537f6369294f0 [formerly 44d3b7651b32c920cb4faea425454916f7b18adc]
    Former-commit-id: f16bcccf196a94c8be6cc12b40e92ed1ba661c71
    Former-commit-id: 0c6b59037033afd853100b0db39fe9a95a183880

--- Utilities/Matlab/scripts/vettori_sloped_function.m
+++ Utilities/Matlab/scripts/vettori_sloped_function.m
@@ -1,9 +1,9 @@
 function[T] = vettori_sloped_function(file)
 text=fileread(file);
 for j =1:4
-    name = ['TC_',num2str(j),'_1'];
+    name = ['SPK_',num2str(j)];
     f1=findstr(name , text);
-    C=textscan(text(f1(end)+30:f1(end)+40),'%[^ ]');
+    C=textscan(text(f1(end)+25:f1(end)+40),'%[^ ]');
     format ('short', 'g');
     T(j,1)=str2num(C{1}{1});
 end
