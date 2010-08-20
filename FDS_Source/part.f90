094bcfa976b757c0b76b5b18e7f2f4b1692e0f6c
rmcdermo <randy.mcdermott@gmail.com>
2010-08-20 18:23:20 +0000

    FDS Source: more mods to NEW_FLUID_PARTICLE, corrections to gravity term
    Former-commit-id: 6b29ec77d239b49dfa274817e821403d3f7c5dee [formerly cce7b32ee4dc9937b633ce6e909735602bb1ea4f]
    Former-commit-id: e43f15bc532f900d3bbc58df6153596623c407a2
    Former-commit-id: c087d35c51cdce42b5b58ac89587e9391551b83f

--- FDS_Source/part.f90
+++ FDS_Source/part.f90
@@ -1016,24 +1016,24 @@ DROPLET_LOOP: DO I=1,NLP
                ALPHA = FP_MASS/DR_MASS
                OPA   = 1._EB+ALPHA
                BDTOA = BETA*DT/OPA
+               
+               X_OLD = DR%X
+               Y_OLD = DR%Y
+               Z_OLD = DR%Z
 
                U_OLD = DR%U
                V_OLD = DR%V
                W_OLD = DR%W
             
-               DR%U = ( U_OLD + (U_OLD+ALPHA*UBAR)*BDTOA )/OBDT
-               DR%V = ( V_OLD + (V_OLD+ALPHA*VBAR)*BDTOA )/OBDT
-               DR%W = ( W_OLD + (W_OLD+ALPHA*WBAR)*BDTOA )/OBDT
-               
-               X_OLD = DR%X
-               Y_OLD = DR%Y
-               Z_OLD = DR%Z
+               DR%U = ( U_OLD + (U_OLD+ALPHA*UBAR)*BDTOA )/OBDT + GVEC(1)*DT
+               DR%V = ( V_OLD + (V_OLD+ALPHA*VBAR)*BDTOA )/OBDT + GVEC(2)*DT
+               DR%W = ( W_OLD + (W_OLD+ALPHA*WBAR)*BDTOA )/OBDT + GVEC(3)*DT
                
                IF (BETA>1.E-9_EB) THEN
-                  ! analytical solution for droplet position based on drag only
-                  DR%X = DR%X + (U_OLD+ALPHA*UBAR)/OPA*DT + ALPHA/BETA*(U_OLD-UBAR)/OPA*LOG(OBDT)
-                  DR%Y = DR%Y + (V_OLD+ALPHA*VBAR)/OPA*DT + ALPHA/BETA*(V_OLD-VBAR)/OPA*LOG(OBDT)
-                  DR%Z = DR%Z + (W_OLD+ALPHA*WBAR)/OPA*DT + ALPHA/BETA*(W_OLD-WBAR)/OPA*LOG(OBDT)
+                  ! analytical solution for droplet position
+                  DR%X = X_OLD + (U_OLD+ALPHA*UBAR)/OPA*DT + ALPHA/BETA*(U_OLD-UBAR)/OPA*LOG(OBDT) + GVEC(1)*DT*DT
+                  DR%Y = Y_OLD + (V_OLD+ALPHA*VBAR)/OPA*DT + ALPHA/BETA*(V_OLD-VBAR)/OPA*LOG(OBDT) + GVEC(2)*DT*DT
+                  DR%Z = Z_OLD + (W_OLD+ALPHA*WBAR)/OPA*DT + ALPHA/BETA*(W_OLD-WBAR)/OPA*LOG(OBDT) + GVEC(3)*DT*DT
                   
                   ! fluid momentum source term
                   MPOM = DR%PWT*DR_MASS/RHO_G/RVC
@@ -1042,26 +1042,14 @@ DROPLET_LOOP: DO I=1,NLP
                   DR%A_Z = DR%A_Z + MPOM*(W_OLD-DR%W)/DT
                ELSE
                   ! no drag
-                  DR%X = DR%X + DR%U*DT
-                  DR%Y = DR%Y + DR%V*DT
-                  DR%Z = DR%Z + DR%W*DT
+                  DR%X = X_OLD + (U_OLD + GVEC(1)*DT)*DT
+                  DR%Y = Y_OLD + (V_OLD + GVEC(2)*DT)*DT
+                  DR%Z = Z_OLD + (W_OLD + GVEC(3)*DT)*DT
+                  
                   DR%A_X  = 0._EB
                   DR%A_Y  = 0._EB
                   DR%A_Z  = 0._EB
                ENDIF
-               
-               ! gravitational acceleration
-               U_OLD = DR%U
-               V_OLD = DR%V
-               W_OLD = DR%W
-               
-               DR%U = DR%U + GVEC(1)*DT
-               DR%V = DR%V + GVEC(2)*DT
-               DR%W = DR%W + GVEC(3)*DT
-               
-               DR%X = DR%X + 0.5_EB*(DR%U+U_OLD)*DT
-               DR%Y = DR%Y + 0.5_EB*(DR%V+V_OLD)*DT
-               DR%Z = DR%Z + 0.5_EB*(DR%W+W_OLD)*DT
                
             ELSE NEW_FP_STATIC_IF ! particle velocity is zero
 
