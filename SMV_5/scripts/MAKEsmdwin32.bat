34fbbbda17c7fbda423d8798a8731c638648c1f5
gforney <gforney@gmail.com>
2010-09-04 20:26:59 +0000

    bundle: create scripts and add links to them for buliding fds/smokeview utilities (fds2ascii, smokediff, smokezip, background) to build web page
    Former-commit-id: b8c3fda4bc121dce68740cb4c5d6afa0f919839d [formerly 198ebf21b06dd9db68149e7f3fffc1164eac2671]
    Former-commit-id: 0efb7287089287bd6385b6248deecfa3a2347ef8
    Former-commit-id: bb4f9290dee750d263edebc7223496b64eda5b71

---
+++ SMV_5/scripts/MAKEsmdwin32.bat
@@ -0,0 +1,26 @@
+@echo off
+
+Rem  Windows batch file to copy smokediffset SVNROOT=~/FDS-SMV
+
+Rem setup environment variables (defining where repository resides etc) 
+
+set envfile="%homedrive%\%homepath%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+
+cd %svn_root%\Utilities\smokediff\INTEL_WIN_32
+erase *.obj
+make_diff.bat
+pause
