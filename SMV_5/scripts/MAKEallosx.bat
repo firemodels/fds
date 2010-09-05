00a1a4c6d980346e456d690ec8db1d630eceff4b
gforney <gforney@gmail.com>
2010-09-05 02:39:43 +0000

    bundle scripts: add build all option for linux and osx utility builds

---
+++ SMV_5/scripts/MAKEallosx.bat
@@ -0,0 +1,25 @@
+@echo off
+
+Rem  Windows batch file to copy smokediffset SVNROOT=~/FDS-SMV
+
+Rem setup environment variables (defining where repository resides etc) 
+
+set envfile="%homedrive%\%homepath%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV_5/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+
+call %svn_root%\SMV_5\scripts\MAKEsmdosux.bat
+call %svn_root%\SMV_5\scripts\MAKEsmzosx.bat
+call %svn_root%\SMV_5\scripts\MAKEf2aosx.bat
