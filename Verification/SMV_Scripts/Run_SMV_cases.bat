e1c2a7b46252e6c39d198c78a902c5588715b0d0
gforney <gforney@gmail.com>
2010-09-06 02:16:20 +0000

    smv verification: add WUI tree bat file to SMV verification script
    Former-commit-id: 25c6cafe578c22266d764ecf6bc5e283cdc5db15 [formerly f07dae7d88ee105599aeb77521684b6d4d74c6ec]
    Former-commit-id: d9e221faa8ccb9776aa8562c990e4feb91843264
    Former-commit-id: 309885ef52fb3c72c0c5e5b402fe0c54485ca1dd

--- Verification/SMV_Scripts/Run_SMV_cases.bat
+++ Verification/SMV_Scripts/Run_SMV_cases.bat
@@ -5,8 +5,8 @@ set SCRIPT_DIR=%CD%
 set BASEDIR=%CD%\..
 set SVNROOT=%BASEDIR%\..\
 
-set FDS=%SVNROOT%\FDS_Compilation\intel_win_32\fds5_win_32
-Rem set FDS=background -d 1 -u 90 %SVNROOT%\FDS_Compilation\intel_win_64\fds5_win_64
+Rem set FDS=%SVNROOT%\FDS_Compilation\intel_win_32\fds5_win_32
+set FDS=background -d 1 -u 90 %SVNROOT%\FDS_Compilation\intel_win_64\fds5_win_64
 
 set RUNFDS=call %SVNROOT%\Utilities\Scripts\runfds_win32.bat
 
@@ -29,6 +29,9 @@ echo | %FDS% 2> "%smvug%\figures\fds5.version"
 call %SCRIPT_DIR%\SMV_Cases.bat
 
 erase %SCRIPT_DIR%\SMV_Cases.bat
+
+cd %BASEDIR%
+call run_wui_tree_test
 
 cd %BASEDIR%
 echo "smokeview test cases end" > smokeview.end
