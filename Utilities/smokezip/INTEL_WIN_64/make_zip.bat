a36489792c625e9a73ed8a885667ad90141d3fc5
gforney <gforney@gmail.com>
2010-09-21 19:38:10 +0000

    smokezip build: update batch file to build 64 bit windows smokezip

--- Utilities/smokezip/INTEL_WIN_64/make_zip.bat
+++ Utilities/smokezip/INTEL_WIN_64/make_zip.bat
@@ -1,6 +1,16 @@
+@echo off
+
+Rem windows batch file to build smokezip from the command line
+
+IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER11=1
+
+echo Setting up compiler environment
 call "%IFORT_COMPILER11%\bin\ifortvars" intel64
 call "%IFORT_COMPILER11%\bin\iclvars" intel64
-Rem erase *.obj
-Rem erase *.mod
+:envexist
+erase *.obj
+erase *.mod
 make -f ..\Makefile intel_win_64
 pause
