e99dbbd25348fc26e7303248bc7ebb9a6b85bedd
rmcdermo <randy.mcdermott@gmail.com>
2010-09-22 14:55:21 +0000

    Matlab: add check_hrr for Flame_Height validation cases
    Former-commit-id: 6da6dc3f892cb2f47eb69f22c475bba012303c31 [formerly 7a697541eee57d9e93afea54facd65887296ace2]
    Former-commit-id: 201bd6caf5faec00604ce3a0dfb821d69ee59d0e
    Former-commit-id: 1031b6e5589f8fc4fb5d546eff9a705cdf95d543

---
+++ Utilities/Matlab/scripts/check_hrr.m
@@ -0,0 +1,57 @@
+% McDermott
+% 9-20-10
+% check_hrr.m
+
+close all
+clear all
+
+% set the plot style parameters
+
+plot_style
+set(gcf,'DefaultLineLineWidth',Line_Width)
+set(gca,'FontName',Font_Name)
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
+
+DDIR = '../../Validation/Flame_Height/FDS_Output_Files/';
+
+M = csvread([DDIR,'box_height.csv'],1,0);
+Qs = M(:,1);
+Q = M(:,2);
+RI = {'_RI=05','','_RI=20'};
+QI = {'p1','p2','p5','1','2','5','10','20','50','100','200','500','1000','2000','5000','10000'};
+
+K(4)=loglog(Qs,Q,'k-'); hold on
+
+for j=1:length(Qs)
+    for i=1:3
+        filename = ['Qs=',QI{j},RI{i},'_hrr.csv'];
+        A = csvread([DDIR,filename],2,0);
+        n = length(A(:,1));
+        Q_fds = mean(A(round(n/2):n,2));
+        if (i==1); K(1)=loglog(Qs(j),Q_fds,'ksq'); end
+        if (i==2); K(2)=loglog(Qs(j),Q_fds,'r^'); end
+        if (i==3); K(3)=loglog(Qs(j),Q_fds,'go'); end
+    end
+end
+
+set(gca,'FontName',Font_Name)
+set(gca,'FontSize',Label_Font_Size)
+xlabel('Q*','Interpreter','LaTeX','FontSize',Label_Font_Size)
+ylabel('FDS Q (kW)','Interpreter','LaTeX','FontSize',Label_Font_Size)
+hh=legend(K,'$D^*/\delta x=5$','$D^*/\delta x=10$','$D^*/\delta x=20$','correct','Location','Southeast');
+set(hh,'Interpreter','LaTeX','FontSize',Key_Font_Size)
+
+% print to pdf
+
+plotdir = '../../Manuals/FDS_5_Validation_Guide/FIGURES/Heskestad/';
+Plot_Filename = 'Flame_Height_check_hrr';
+
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+display(['Printing plot Flame_Height_check_hrr.pdf ...'])
+print(gcf,'-dpdf',[plotdir,Plot_Filename])
+
+
+
