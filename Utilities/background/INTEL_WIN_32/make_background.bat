cde596ad592d62d9b65330d9c3c771fe876f6c19
gforney <gforney@gmail.com>
2010-09-24 17:40:04 +0000

    build scripts: add scripts to build 64 bit smokeview and utilities apps (windows) on a win32 PC
    Former-commit-id: ea7897f5087a36ffca9bfa3e0aa949b2f8f51a4f [formerly ac3b414ad321059e8e356d430bcd7168e9d8b75c]
    Former-commit-id: dcef1c657c3d2022ae86a02ea0e621dcb52aa0ec
    Former-commit-id: e7ad0ac800b5624ea9928375f8b54ea7beb7ecee

--- Utilities/background/INTEL_WIN_32/make_background.bat
+++ Utilities/background/INTEL_WIN_32/make_background.bat
@@ -1,5 +1,17 @@
+@echo off
+
+Rem windows batch file to build background from the command line
+
+IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER11=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" ia32
 call "%IFORT_COMPILER11%\bin\iclvars" ia32
-Rem erase *.obj
-Rem erase *.mod
+
+:envexist
+erase *.obj
+erase *.mod
 make -f ..\Makefile intel_win_32
 pause
---
+++ Utilities/set_fds_path/INTEL_WIN_64/make_setfdspath64on32.bat
@@ -0,0 +1,17 @@
+@echo off
+
+Rem windows batch file to build setfdspath
+
+IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER11=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" ia32_intel64
+call "%IFORT_COMPILER11%\bin\iclvars" ia32_intel64
+
+:envexist
+erase *.obj
+erase *.mod
+make VPATH="../../../SMV_5/source/set_path" -f ../Makefile intel_win_64
+pause
