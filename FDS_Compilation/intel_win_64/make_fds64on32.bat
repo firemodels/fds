f50fa202a20999fcc2158f6425c086c8747bcf03
gforney <gforney@gmail.com>
2010-09-24 19:18:38 +0000

    build scripts: add script to build a win64 fds on a win32 bit system
    Former-commit-id: da24436421fc06233ee8205555c1fbac50f1188c [formerly 5772d56cd1d0db47a4ce1d7bdfbab3382c06e73d]
    Former-commit-id: 8802a4a13aacc675e21e64e6728c64b84d17ca7a
    Former-commit-id: 380b1283b811dd37e2c780b16095278729358004

---
+++ FDS_Compilation/intel_win_64/make_fds64on32.bat
@@ -0,0 +1,15 @@
+@echo off
+
+Rem windows batch file to build smokeview from the command line
+
+IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER11=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" ia32_intel64
+:envexist
+erase *.obj
+erase *.mod
+make VPATH="../../FDS_Source" -f ..\makefile intel_win_64
+pause
