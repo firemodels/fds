f1f647182dc47ec4f8d7bdab20e67f2f7face879
gforney <gforney@gmail.com>
2010-10-01 21:04:55 +0000

    smokezip source: simple program to test whether pthreads is working on win64

---
+++ Utilities/thread/INTEL_WIN_64/make_thread.bat
@@ -0,0 +1,17 @@
+@echo off
+
+Rem windows batch file to build smokediff from the command line
+
+IF "%SETUP_IFORT_COMPILER_INTEL64%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER_INTEL64=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" intel64
+call "%IFORT_COMPILER11%\bin\iclvars" intel64
+
+:envexist
+erase *.obj
+erase *.mod
+make -f ..\Makefile intel_win_64
+pause
