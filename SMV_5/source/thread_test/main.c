44549786d5a72894d3f6e93e57afaa3800063f1d
gforney <gforney@gmail.com>
2010-10-01 21:19:43 +0000

    smokezip source: simple program to test whether pthreads works on win64
    Former-commit-id: 98574620b4c82d466a68b9db80ec57bf63e16741 [formerly 405e09d4b1b517b2be2e2dac883cd241f838c14b]
    Former-commit-id: 213e453f6e2af63bf86883a748709f10a2cf4fae
    Former-commit-id: 42971640440cd5ca48495c942f00ed638954d89a

---
+++ SMV_5/source/thread_test/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NTHREADS 10
+void *thread_function(void *);
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+int  counter = 0;
+
+int main(int argc, char **argv){
+   pthread_t thread_id[NTHREADS];
+   int i, j;
+
+   for(i=0; i < NTHREADS; i++)
+   {
+     printf("starting thread: %i\n",i);
+      pthread_create( &thread_id[i], NULL, thread_function, NULL );
+   }
+
+   for(j=0; j < NTHREADS; j++)
+   {
+      printf("joining thread %i\n",j);
+      pthread_join( thread_id[j], NULL); 
+   }
+  
+   /* Now that all threads are complete I can print the final result.     */
+   /* Without the join I could be printing a value before all the threads */
+   /* have been completed.                                                */
+
+   printf("Final counter value: %d\n", counter);
+   return 0;
+}
+
+void *thread_function(void *dummyPtr){
+   printf("Thread number %ld\n", pthread_self());
+   pthread_mutex_lock( &mutex1 );
+   counter++;
+   pthread_mutex_unlock( &mutex1 );
+   return NULL;
+}
+\c
