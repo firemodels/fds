5ad2ab0bdf3d3bbeb780698631a2333cdf56e797
gforney <gforney@gmail.com>
2010-10-02 05:10:36 +0000

    smokezip source: add revision checking for threader.c routine
    Former-commit-id: e826c311ee1b9425cfa97ca0c44ddc2cbb98b044 [formerly d2991531462d013337d5a0a63333acbf07b91e87]
    Former-commit-id: 45c1c2e73b5b0b09c723a449a95d52f1b46fcb5e
    Former-commit-id: d84925bc8c534fbad12d766cc5932d2246b9959e

--- SMV_5/source/smokezip/threader.c
+++ SMV_5/source/smokezip/threader.c
@@ -40,16 +40,10 @@ void *MT_part2iso(void *arg){
 
 void *MT_convert_3dsmoke(void *arg){
   smoke3d *smoke3di;
-  int smoke_index;
 
   smoke3di = (smoke3d *)arg;
-  smoke_index=smoke3di-smoke3dinfo;
-  //LOCK_SMOKE;
   convert_3dsmoke(smoke3di);
-  //UNLOCK_SMOKE;
- // printf("smoke3d compression for file: %i\n",smoke_index);
   return NULL;
-
 }
 #endif
 
