f3c896ae2bbdeed706b5b49e3acdeec773bdce03
gforney <gforney@gmail.com>
2010-10-31 18:25:35 +0000

    smokeview source: update to boundary file min/max multi-threading code

--- SMV_5/source/smokeview/threader.h
+++ SMV_5/source/smokeview/threader.h
@@ -17,8 +17,8 @@
 // setup LOCKS
 
 #ifdef pp_THREAD
-#define LOCK_COMPRESS if(mt_compress==1)pthread_mutex_lock(&mutexCOMPRESS);
-#define UNLOCK_COMPRESS if(mt_compress==1)pthread_mutex_unlock(&mutexCOMPRESS);
+#define LOCK_COMPRESS pthread_mutex_lock(&mutexCOMPRESS);
+#define UNLOCK_COMPRESS pthread_mutex_unlock(&mutexCOMPRESS);
 #else
 #define LOCK_COMPRESS
 #define UNLOCK_COMPRESS
@@ -26,12 +26,11 @@
 
 // define mutex's and thread_ids
 
-MT_EXTERN int mt_compress;
-
 #ifndef CPP
 #ifdef pp_THREAD
 MT_EXTERN pthread_mutex_t mutexCOMPRESS;
 MT_EXTERN pthread_t smooth_block_thread_id;
 MT_EXTERN pthread_t compress_thread_id;
+MT_EXTERN pthread_t update_all_patch_bounds_id;
 #endif
 #endif
