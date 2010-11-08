ce334c65e59665d9934b998e6ec842fe1a7fa714
gforney <gforney@gmail.com>
2010-11-08 01:33:04 +0000

    setfds build: update scripts to build setfds (the program that adds entries to the user path), also test commits on an x64 system
    Former-commit-id: 6197f86f4c3a851560ec6e74f00065f04d1cb001 [formerly 0ab1f908057f2311249b81b2e503ea280d4fdc99]
    Former-commit-id: 37347e58bbf12129f59a3b0ddba9cfe4f5ceed33
    Former-commit-id: 76b602605d017c69ef7fc83b2c65750dbcdf666f

--- Utilities/set_fds_path/INTEL_WIN_64/make_setfdspath64.bat
+++ Utilities/set_fds_path/INTEL_WIN_64/make_setfdspath64.bat
@@ -1,9 +1,15 @@
-set intelbin=c:\bin
+@echo off
 
-call %intelbin%\iclvars intel64
-call %intelbin%\ifortvars intel64
+Rem windows batch file to build smokezip from the command line
 
-Rem erase *.obj
-Rem erase *.mod
+IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER11=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" intel64
+call "%IFORT_COMPILER11%\bin\iclvars" intel64
+:envexist
+erase *.obj
 make VPATH="../../../SMV_5/source/set_path" -f ../Makefile intel_win_64
 pause
