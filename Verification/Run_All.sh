4a55858a990da2ae323a06462f65ae301311c767
gforney <gforney@gmail.com>
2010-11-22 18:03:57 +0000

    verification: update scripts to run wui case (which also requires a smokezip run after fds is finishted) for Linux

--- Verification/Run_All.sh
+++ Verification/Run_All.sh
@@ -2,6 +2,8 @@
 export SVNROOT=~/FDS-SMV
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds5_intel_linux_64
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds.sh
+export SMOKEZIP=$SVNROOT/Utilities/smokezip/INTEL_LINUX_32/smokezip_linux_32
 export BASEDIR=`pwd`
 
 ./FDS_Cases.csh
@@ -13,3 +15,9 @@ cd $BASEDIR/scripts
 ./SMV_Cases.csh
 
 echo SMV cases submitted
+
+cd $BASEDIR/scripts
+
+./run_wui_tree_test.csh
+
+echo WUI case submitted
