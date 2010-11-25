d776745957b8a11aed77c2326510e80ccb066ff0
gforney <gforney@gmail.com>
2010-11-25 22:47:26 +0000

    bundle scripts: refine script to verify web script tools are installed

--- SMV/scripts/SHOW_setup.bat
+++ SMV/scripts/SHOW_setup.bat
@@ -26,12 +26,20 @@ echo fds_version=%fds_version%
 echo fds_revision=%fds_revision%
 
 echo.
+echo Press any key to continue
+pause>NUL
+
+echo.
 echo *** FDS-Smokeview repository settings
 
 echo.
 echo svn_root=%svn_root%
 echo svn_drive=%svn_drive%
 echo linux_svn_root=%linux_svn_root%
+
+echo.
+echo Press any key to continue
+pause>NUL
 
 echo.
 echo *** Linux/OSX User/Host names
@@ -44,6 +52,10 @@ echo linux_username=%linux_username%
 echo svn_logon=%svn_logon%
 
 echo.
+echo Press any key to continue
+pause>NUL
+
+echo.
 echo *** Google Code upload settings
 
 echo.
@@ -53,4 +65,8 @@ echo upload=%upload%
 echo fds_google_level=%fds_google_level%
 echo smv_google_level=%smv_google_level%
 
-pause
+echo.
+echo variable display complete.
+echo Press any key to continue.
+pause>NUL
+
---
+++ SMV/scripts/TEST_setup.bat
@@ -0,0 +1,62 @@
+@echo off
+
+echo Testing that software and environment variables used by 
+echo the FDS/Smokeview web scripts are setup.  
+echo.
+echo Press any key to begin test.
+pause >NUL
+
+set envfile="%homedrive%\%homepath%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo The file fds_smv_env.bat was not found in your
+echo home directory: %homedrive%\%homepath%
+echo.
+echo 1.  Copy it from SMV\for_bundle in the repository to %homedrive%\%homepath%
+echo 2.  Then edit it to match your configuration.
+echo 3.  Then repeat this test.
+echo. Press any key to continue.
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+echo fds_smv_env.bat was found in your home directory.
+echo Press any key to view settings.
+pause >NUL
+
+%svn_drive%
+call %envfile%
+
+call %svn_root%\SMV\scripts\SHOW_setup.bat
+
+echo.
+echo Press any key to test putty installation
+pause>NUL
+plink -V
+
+echo.
+echo Press any key to test Compiler installation
+pause>NUL
+IF DEFINED IFORT_COMPILER12 echo IFORT_COMPILER12 variable is defined.
+IF DEFINED IFORT_COMPILER12 echo Fortran compiler located at %IFORT_COMPILER12%
+IF NOT DEFINED IFORT_COMPILER12 echo IFORT_COMPILER12 variable not defined
+echo.
+IF DEFINED IFORT_COMPILER11 echo IFORT_COMPILER11 variable is defined.
+IF DEFINED IFORT_COMPILER11 echo Fortran compiler located at %IFORT_COMPILER11%
+IF NOT DEFINED IFORT_COMPILER11 echo IFORT_COMPILER11 variable not defined
+
+echo.
+echo Winzip test
+echo First press any key to test command line winzip (wzzip)
+pause>NUL
+wzzip
+
+echo.
+echo Next press any key to test the self extractor
+pause>NUL
+winzipse
+
+echo.
+echo Testing complete
+pause
+
