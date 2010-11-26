ac1a9d30f649abb826b9de9440fe439b0e572d19
mcgratta <mcgratta@gmail.com>
2010-11-26 21:22:19 +0000

    FDS Source: Issue 1272. Set T_IGN to HUGE initially for objects that heat up and then burn.

--- FDS_Source/read.f90
+++ FDS_Source/read.f90
@@ -5205,8 +5205,8 @@ PROCESS_SURF_LOOP: DO N=0,N_SURF
    ! Ignition Time
 
    SF%T_IGN = T_BEGIN 
-   IF (SF%TMP_IGN<5000._EB)                    SF%T_IGN = T_END
-   IF (SF%PYROLYSIS_MODEL==PYROLYSIS_MATERIAL) SF%T_IGN = T_END
+   IF (SF%TMP_IGN<5000._EB)                    SF%T_IGN = HUGE(T_END)
+   IF (SF%PYROLYSIS_MODEL==PYROLYSIS_MATERIAL) SF%T_IGN = HUGE(T_END)
 
    ! Species Arrays and Method of Mass Transfer (SPECIES_BC_INDEX)
 
