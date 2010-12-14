7644cfef32d5f4a33f525e0c784952bfb6f3132b
gforney <gforney@gmail.com>
2010-12-14 18:13:47 +0000

    build scripts: modify scripts to allow 32 bit builds on 64 bit PCs
    Former-commit-id: 5fd2fce0b5419732d5a40c03668be99921ebaab1 [formerly 79ea83371f77227c8ebb3aa5bbf856b81f3a4ebb]
    Former-commit-id: d0c2f4901cf1b5236284c474edf1a5e861451ddc
    Former-commit-id: 089db2eeb1d472a067e95c4e7945a75a56188bc9

--- Utilities/smokezip/INTEL_WIN_32/make_zip.bat
+++ Utilities/smokezip/INTEL_WIN_32/make_zip.bat
@@ -9,6 +9,7 @@ set SETUP_IFORT_COMPILER_IA32=1
 echo Setting up compiler environment
 call "%IFORT_COMPILER11%\bin\ifortvars" ia32
 call "%IFORT_COMPILER11%\bin\iclvars" ia32
+if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
 :envexist
 erase *.obj
 erase *.mod
