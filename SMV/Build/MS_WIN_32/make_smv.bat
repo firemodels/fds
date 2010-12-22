47b901527aff305cff31248061429662709c53dc
gforney <gforney@gmail.com>
2010-12-22 04:46:14 +0000

    smv build: add entries to makefile and corresponding directories to bulid windows smokeview using MS Visual Studio rather than the Intel C/C++ compiler

---
+++ SMV/Build/MS_WIN_32/make_smv.bat
@@ -0,0 +1,14 @@
+@echo off
+
+Rem windows batch file to build smokeview from the command line
+
+IF "%SETUP_IFORT_COMPILER_IA32%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER_IA32=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" ia32
+if exist "%VS_COMPILER%\..\vcvars32.bat" call "%VS_COMPILER%\..\vcvars32.bat"
+:envexist
+make -j4 -f ..\Makefile ms_win_32
+pause
---
+++ SMV/Build/MS_WIN_TEST_64/make_smv.bat
@@ -0,0 +1,14 @@
+@echo off
+
+Rem windows batch file to build smokeview from the command line
+
+IF "%SETUP_IFORT_COMPILER_IA32%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER_IA32=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" intel64
+if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
+:envexist
+make -j4 -f ..\Makefile ms_win_test_64
+pause
