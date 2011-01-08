16a3b546405a9d5443d5077ad608f27c07e06c36
gforney <gforney@gmail.com>
2011-01-08 23:03:42 +0000

    FDS verification: remove Smokeview cases from FDS verificaiton windows batch file, also rename Run_All_Cases scripts to Run_FDS_Cases scripts
    Former-commit-id: 434e810c0023d3649c7b42e107045494fa1421ea [formerly 1952e6276f9852c0794b02f3c9b7f674ff717855]
    Former-commit-id: da73b60ee654c7bcd325f1d0efde9b7fe28002f7
    Former-commit-id: e2b3a856f07e35221e0bb79c574455f4917cdc41

--- Verification/Run_FDSl_Cases.bat
+++ Verification/Run_FDSl_Cases.bat
@@ -37,27 +37,4 @@ call FDS_Cases.bat
 
 erase FDS_Cases.bat
 
-cd %BASEDIR%
-
-Rem -----------------------------------------------------------------------
-Rem create a text file containing the FDS5 version used to run these tests.
-Rem This file is included in the smokeview user's guide
-Rem -----------------------------------------------------------------------
-
-set smvug="%CD%\..\Manuals\SMV_5_User_Guide\"
-echo | %FDSEXE% 2> "%smvug%\figures\fds5.version"
-
-Rem -------------------------
-Rem run FDS veriication cases
-Rem -------------------------
-cd %BASEDIR%\scripts
-echo creating FDS case list from SMV_Cases.csh
-..\..\Utilities\Data_Processing\csh2bat SMV_Cases.csh SMV_Cases.bat
-
-call SMV_Cases.bat
-erase SMV_Cases.bat
-
-cd %BASEDIR%\scripts
-call run_wui_tree_test
-
 pause
