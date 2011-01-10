d1366df77f15db00866e7057e9126f23739c7b69
mcgratta <mcgratta@gmail.com>
2011-01-10 16:18:01 +0000

    FDS Source: Change CHANGE_ONLY to RELATIVE on the DEVC line.
    Former-commit-id: bdd424fb97cf93c099a032437f8a083ac2d0b711 [formerly c28832c15fe3f881557f7f3727277da806acc378]
    Former-commit-id: b8e82bf21f24577121a67b92f44e841f626f7e5b
    Former-commit-id: 96ea2e3a260d6bd6a713f977ba456dcba65325f6

--- FDS_Source/dump.f90
+++ FDS_Source/dump.f90
@@ -4014,7 +4014,7 @@ DEVICE_LOOP: DO N=1,N_DEVC
    ! Record initial value and then subtract from computed value
 
    IF (DV%INITIAL_VALUE<-1.E9_EB) THEN
-      IF (DV%CHANGE_ONLY) THEN
+      IF (DV%RELATIVE) THEN
          DV%INITIAL_VALUE = VALUE
       ELSE
          DV%INITIAL_VALUE = 0._EB
