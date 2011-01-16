ebd12686a6a14a5c5ade3adc335d9bcf57750e6c
mcgratta <mcgratta@gmail.com>
2011-01-16 18:54:00 +0000

    FDS Compilation: Add intel_linux_64_db.
    Former-commit-id: 193590af3d2d12b1d213c051c0d2fd9eb99b1fd3 [formerly 23858f7d29928d1cfd8163ee42134a909ae1184a]
    Former-commit-id: a5f27a5bf02d66cfd6f46648f3787f4d20979968
    Former-commit-id: 0fffacac5def3520778c52d828ccb20b9a27c5fd

--- Verification/Run_All.sh
+++ Verification/Run_All.sh
@@ -1,23 +1,15 @@
 #!/bin/bash -f
+
+# This script runs the FDS Verification Cases on a linux machine with
+# a batch queuing system
+
 export SVNROOT=~/FDS-SMV
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds5_intel_linux_64
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
 export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds.sh
-export SMOKEZIP=$SVNROOT/Utilities/smokezip/INTEL_LINUX_32/smokezip_linux_32
 export BASEDIR=`pwd`
 
 ./FDS_Cases.csh
 
 echo FDS cases submitted
 
-cd $BASEDIR/scripts
-
-./SMV_Cases.csh
-
-echo SMV cases submitted
-
-cd $BASEDIR/scripts
-
-./run_wui_tree_test.csh
-
-echo WUI case submitted
