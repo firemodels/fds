1ac3a1931f2ecc355b332164ea57fd7cb54300b7
gforney <gforney@gmail.com>
2011-01-27 17:31:46 +0000

    Verification: add two SH scripts to the repository.  First, qmake.sh for creating a PBS/SGE script for later use and and second qfdsmpi2.sh for specifying both the number of processes and the number of processes per node (eg. will let you run 8 processes on one node, 4 processes each on 2 nodes 2 processes each on 4 nodes etc.

--- Utilities/Scripts/bundle_setup/qmakescript.sh
+++ Utilities/Scripts/bundle_setup/qmakescript.sh
@@ -1,23 +1,28 @@
 #!/bin/bash -f
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: qfdsmpi.sh nthreads fds_command casename.fds"
+  echo "Usage: qmakescript.sh nthreads nprocs fds_command casename.fds"
   echo ""
-  echo "Runs a parallel FDS case on a Linux cluster using the "
-  echo "PBS/SGE qsub batch queuing command"
+  echo "Creates a script used by a batch queuing system to run"
+  echo "FDS on a Linux cluster"
   echo ""
   echo "    nthreads - number of threads (usually number of &mesh lines)"
+  echo "      nprocs - number of processes/threads per node"
   echo " fds_command - full path to fds command name"
   echo "casename.fds - fds case"
   echo 
   exit
 fi
 nthreads=$1
-fds=$2
-in=$3
+nprocs=$2
+fds=$3
+in=$4
+if test $nprocs -gt 8
+then
 nprocs=8
+fi
 nnodes=$(echo "($nthreads-1)/$nprocs+1" | bc)
 if test $nnodes -le 0
 then
@@ -30,20 +35,7 @@ fulldir=`pwd`
 out=$fulldir/$infile.err
 outlog=$fulldir/$infile.log
 
-scriptfile=/tmp/script.$$
-if ! [ -e $fulldir/$in ]; then
-  echo "The fds input file, $fulldir/$in, does not exit. Run aborted."
-  exit
-fi
-if ! [ -e $fds ]; then
-  echo "The FDS program name, $fds, does not exit. Run aborted."
-  exit
-fi
-if ! [ -e $outlog ]; then
-  rm $outlog
-fi
-
-cat << EOF > $scriptfile
+cat << EOF 
 #!/bin/bash -f
 #PBS -N $infile(MPI)
 #PBS -e $out
@@ -61,7 +53,3 @@ echo Directory: \`pwd\`
 
 mpirun -np $nthreads $fds $in
 EOF
-chmod +x $scriptfile
-echo Running $in 
-#qsub $scriptfile
-#rm $scriptfile
