35314400dbf965f8a03c5deb52a7b04d12911815
mcgratta <mcgratta@gmail.com>
2011-01-28 22:54:34 +0000

    FDS Validation: Change .csh scripts to .sh (bash).
    Former-commit-id: bf97d71d7a8d02edf494296125b29c0ac313bdf4 [formerly 2dd3de87e886582d8434a6c0e5b05059b8cd4329]
    Former-commit-id: 30dc53452b5dd9d7e9988936e69cad0f764f7c5e
    Former-commit-id: c886d3a8e7eba28a2d44ae808b2208b5df6e7243

---
+++ Validation/NIST_NRC/Run_All.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR
+
+$RUNFDS $INDIR NIST_NRC_01 
+$RUNFDS $INDIR NIST_NRC_02 
+$RUNFDS $INDIR NIST_NRC_03 
+$RUNFDS $INDIR NIST_NRC_04 
+$RUNFDS $INDIR NIST_NRC_05 
+$RUNFDS $INDIR NIST_NRC_07 
+$RUNFDS $INDIR NIST_NRC_08 
+$RUNFDS $INDIR NIST_NRC_09 
+$RUNFDS $INDIR NIST_NRC_10 
+$RUNFDS $INDIR NIST_NRC_13 
+$RUNFDS $INDIR NIST_NRC_14 
+$RUNFDS $INDIR NIST_NRC_15 
+$RUNFDS $INDIR NIST_NRC_16 
+$RUNFDS $INDIR NIST_NRC_17 
+$RUNFDS $INDIR NIST_NRC_18 
+
+echo FDS cases submitted
