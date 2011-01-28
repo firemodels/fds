4cc7dd4dcab42e7720f6b54a8036b5d3f32ccb9b
mcgratta <mcgratta@gmail.com>
2011-01-28 21:33:17 +0000

    FDS Validation: Change .csh scripts to .sh (bash).

---
+++ Validation/Hamins_CH4/Run_All.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR
+
+$RUNFDS $INDIR Hamins_CH4_01 
+$RUNFDS $INDIR Hamins_CH4_05 
+$RUNFDS $INDIR Hamins_CH4_07 
+$RUNFDS $INDIR Hamins_CH4_19 
+$RUNFDS $INDIR Hamins_CH4_21 
+$RUNFDS $INDIR Hamins_CH4_23 
+
+echo FDS cases submitted
