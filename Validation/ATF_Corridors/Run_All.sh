3ffe4a8384faa4659c2b4c1756efc3d45f18e13c
mcgratta <mcgratta@gmail.com>
2011-01-28 20:36:27 +0000

    FDS Validation: Change .csh scripts to .sh (bash).
    Former-commit-id: 6b92e1567203de1dd857a013da098c2766f5101c [formerly bc23010e99d47c0befd0aec06d5498ab4aa420fd]
    Former-commit-id: 322b7754e7835cbd187399c8534c4d0558b775cb
    Former-commit-id: 209808ad3c80f64e998353bb3ce745bd86d46438

--- Validation/ATF_Corridors/Run_All.sh
+++ Validation/ATF_Corridors/Run_All.sh
@@ -7,9 +7,12 @@ export SVNROOT=`pwd`/../..
 export FDS=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
-export INDIR=FDS_Input_Files
+export INDIR=Current_Results
+
 # uncomment following line to stop all cases
 # export STOPFDS=1
+
+cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR
 
 $RUNFDSMPI 3 $INDIR ATF_Corridors_050_kW
 $RUNFDSMPI 3 $INDIR ATF_Corridors_100_kW
---
+++ Validation/BRE_Spray/Run_All.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR
+
+$RUNFDS $INDIR BRE_Spray_A_1 
+$RUNFDS $INDIR BRE_Spray_A_2
+$RUNFDS $INDIR BRE_Spray_A_3 
+$RUNFDS $INDIR BRE_Spray_A_4 
+$RUNFDS $INDIR BRE_Spray_A_5 
+$RUNFDS $INDIR BRE_Spray_A_6 
+$RUNFDS $INDIR BRE_Spray_A_7 
+$RUNFDS $INDIR BRE_Spray_A_8
+$RUNFDS $INDIR BRE_Spray_B_1 
+$RUNFDS $INDIR BRE_Spray_B_2
+$RUNFDS $INDIR BRE_Spray_B_3 
+$RUNFDS $INDIR BRE_Spray_B_4 
+$RUNFDS $INDIR BRE_Spray_B_5 
+$RUNFDS $INDIR BRE_Spray_B_6 
+$RUNFDS $INDIR BRE_Spray_B_7 
+$RUNFDS $INDIR BRE_Spray_B_8
+$RUNFDS $INDIR BRE_Spray_D_1 
+$RUNFDS $INDIR BRE_Spray_D_2
+$RUNFDS $INDIR BRE_Spray_D_3 
+$RUNFDS $INDIR BRE_Spray_D_4 
+$RUNFDS $INDIR BRE_Spray_D_5 
+$RUNFDS $INDIR BRE_Spray_D_6 
+$RUNFDS $INDIR BRE_Spray_D_7 
+$RUNFDS $INDIR BRE_Spray_D_8
+ 
+echo FDS cases submitted
