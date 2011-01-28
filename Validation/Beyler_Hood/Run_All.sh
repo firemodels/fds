978c55c0da68e9d51698d603d8ba237a4ff59ab3
gforney <gforney@gmail.com>
2011-01-28 16:33:48 +0000

    Validation:  add some example sh scripts that use batch queues to run Validation cases (ATF_Corridors, Beyler_Hood, Sandia_Plumes)
    Former-commit-id: d5a4473d1188b9798a4148153554cc5e2c22dedc [formerly 47c4ec5e0eacce168ac89acfde862e016804ff54]
    Former-commit-id: a643aa7f6ea01543c3ccb80387888918e5106c65
    Former-commit-id: 2f2417fe8308ba901b24010fe2c2ee61330ce1dd

---
+++ Validation/Beyler_Hood/Run_All.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=FDS_Input_Files
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+$RUNFDS $INDIR Beyler_Hood_propane-1353-19-0
+$RUNFDS $INDIR Beyler_Hood_propane-1353-19--10
+$RUNFDS $INDIR Beyler_Hood_propane-1353-19-5
+$RUNFDS $INDIR Beyler_Hood_propane-1825-19-0
+$RUNFDS $INDIR Beyler_Hood_propane-1825-19--10
+$RUNFDS $INDIR Beyler_Hood_propane-1825-19-5
+$RUNFDS $INDIR Beyler_Hood_propane-2430-19--10
+$RUNFDS $INDIR Beyler_Hood_propane-2430-19-5
+$RUNFDS $INDIR Beyler_Hood_propane-3152-19-0
+$RUNFDS $INDIR Beyler_Hood_propane-792-19-5
+$RUNFDS $INDIR Beyler_Hood_propane-821-19-0
+$RUNFDS $INDIR Beyler_Hood_propane-821-19--10
+
+
