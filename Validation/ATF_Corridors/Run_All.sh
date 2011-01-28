dac7b7b3b6f44a8e3eade688ad6702bb6a4f5720
gforney <gforney@gmail.com>
2011-01-28 16:33:48 +0000

    Validation:  add some example sh scripts that use batch queues to run Validation cases (ATF_Corridors, Beyler_Hood, Sandia_Plumes)

---
+++ Validation/ATF_Corridors/Run_All.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -f
+
+# This script runs the a set of Validation Cases on a linux machine with
+# a batch queuing system
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+export BASEDIR=`pwd`
+export INDIR=FDS_Input_Files
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+$RUNFDSMPI 3 $INDIR ATF_Corridors_050_kW
+$RUNFDSMPI 3 $INDIR ATF_Corridors_100_kW
+$RUNFDSMPI 3 $INDIR ATF_Corridors_240_kW
+$RUNFDSMPI 3 $INDIR ATF_Corridors_250_kW
+$RUNFDSMPI 3 $INDIR ATF_Corridors_500_kW
+$RUNFDSMPI 3 $INDIR ATF_Corridors_Mix_kW
+
+
