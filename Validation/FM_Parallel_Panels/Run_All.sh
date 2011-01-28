fe4dedc985631f6a0060694da08d1bdc3e74649a
mcgratta <mcgratta@gmail.com>
2011-01-28 21:33:17 +0000

    FDS Validation: Change .csh scripts to .sh (bash).
    Former-commit-id: 47ab2b632b8b07105d7e9f812278b46942fc13fc [formerly 514b56adb46ec8c969e961b60a1301d4300d0548]
    Former-commit-id: cd4619f541c81b037d2c5a47c933357db2297f8f
    Former-commit-id: 2b5ea260a1ba85b9aa62561e43c25ac4a1e9e6eb

---
+++ Validation/FM_Parallel_Panels/Run_All.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR
+
+$RUNFDS $INDIR FM_Parallel_Panel_1 
+$RUNFDS $INDIR FM_Parallel_Panel_2 
+$RUNFDS $INDIR FM_Parallel_Panel_3 
+$RUNFDS $INDIR FM_Parallel_Panel_4 
+$RUNFDS $INDIR FM_Parallel_Panel_5 
+$RUNFDS $INDIR FM_Parallel_Panel_6 
+
+echo FDS cases submitted
