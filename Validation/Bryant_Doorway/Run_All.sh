4c0b3c8659090b60ec914f9cbda3e1e81a48c993
mcgratta <mcgratta@gmail.com>
2011-01-28 20:36:27 +0000

    FDS Validation: Change .csh scripts to .sh (bash).
    Former-commit-id: 6b92e1567203de1dd857a013da098c2766f5101c [formerly bc23010e99d47c0befd0aec06d5498ab4aa420fd]
    Former-commit-id: 322b7754e7835cbd187399c8534c4d0558b775cb
    Former-commit-id: 209808ad3c80f64e998353bb3ce745bd86d46438

---
+++ Validation/Bryant_Doorway/Run_All.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR
+
+$RUNFDS $INDIR Bryant_034_kW
+$RUNFDS $INDIR Bryant_065_kW
+$RUNFDS $INDIR Bryant_096_kW
+$RUNFDS $INDIR Bryant_128_kW
+$RUNFDS $INDIR Bryant_160_kW
+$RUNFDS $INDIR Bryant_320_kW
+$RUNFDS $INDIR Bryant_511_kW
+ 
+echo FDS cases submitted
