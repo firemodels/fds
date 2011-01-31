81f1f87e75f354147e3d9ad8668de1839940fa15
mcgratta <mcgratta@gmail.com>
2011-01-31 17:08:32 +0000

    FDS Validation: Change Process_All script.

--- Validation/Process_All_Output.sh
+++ Validation/Process_All_Output.sh
@@ -1,22 +1,24 @@
-#!/bin/csh -f
-setenv SVNROOT ~/FDS-SMV
-set VDIR=$SVNROOT/Validation
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/..
+export VDIR=$SVNROOT/Validation
+
 $VDIR/ATF_Corridors/FDS_Output_Files/Process_Output.csh
 $VDIR/Beyler_Hood/FDS_Output_Files/Process_Output.csh
 $VDIR/BRE_Spray/FDS_Output_Files/Process_Output.csh
 $VDIR/Bryant_Doorway/FDS_Output_Files/Process_Output.csh
 $VDIR/CAROLFIRE/FDS_Output_Files/Process_Output.csh
-$VDIR/Decaying_Isotropic_Turbulence/FDS_Output_Files/Process_Output.csh
 $VDIR/Flame_Height/FDS_Output_Files/Process_Output.csh
 $VDIR/FM_Parallel_Panels/FDS_Output_Files/Process_Output.csh
 $VDIR/FM_SNL/FDS_Output_Files/Process_Output.csh
 $VDIR/Hamins_CH4/FDS_Output_Files/Process_Output.csh
+$VDIR/LLNL_Enclosure/FDS_Output_Files/Process_Output.csh
 $VDIR/McCaffrey_Plume/FDS_Output_Files/Process_Output.csh
 $VDIR/Moody_Chart/FDS_Output_Files/Process_Output.csh
 $VDIR/NBS_Multi-Room/FDS_Output_Files/Process_Output.csh
-$VDIR/NIST_FSE_2008/FDS_Output_Files/Process_Output.csh
 $VDIR/NIST_NRC/FDS_Output_Files/Process_Output.csh
 $VDIR/NIST_RSE_1994/FDS_Output_Files/Process_Output.csh
+$VDIR/NRCC_Facade/FDS_Output_Files/Process_Output.csh
 $VDIR/NRL_HAI/FDS_Output_Files/Process_Output.csh
 $VDIR/PRISME/FDS_Output_Files/Process_Output.csh
 $VDIR/Restivo_Experiment/FDS_Output_Files/Process_Output.csh
@@ -24,8 +26,11 @@ $VDIR/Sandia_Plumes/FDS_Output_Files/Process_Output.csh
 $VDIR/Smyth_Slot_Burner/FDS_Output_Files/Process_Output.csh
 $VDIR/SP2009_AST/FDS_Output_Files/Process_Output.csh
 $VDIR/Steckler_Compartment/FDS_Output_Files/Process_Output.csh
+$VDIR/UL_Commodity/FDS_Output_Files/Process_Output.csh
 $VDIR/UL_NFPRF/FDS_Output_Files/Process_Output.csh
 $VDIR/Ulster_SBI/FDS_Output_Files/Process_Output.csh
+$VDIR/Vettori_Flat_Ceiling/FDS_Output_Files/Process_Output.csh
+$VDIR/Vettori_Sloped_Ceiling/FDS_Output_Files/Process_Output.csh
 $VDIR/VTT/FDS_Output_Files/Process_Output.csh
 $VDIR/WTC/FDS_Output_Files/Process_Output.csh
 
