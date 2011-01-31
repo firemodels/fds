ce12613c3a19c7d92c9caad52ced8f3c70bb407d
gforney <gforney@gmail.com>
2011-01-31 18:14:16 +0000

    build/VV scripts: change FDS Linux mpi build scripts and VV scripts to use a consistent OpenMPI environment
    Former-commit-id: 4e02aac48d622ba32c69c6d60417d328891d3403 [formerly a22d681e66faa56f57e4fbed1dc89ed26de5c0f4]
    Former-commit-id: a8342e0be6ee1acc3b2c66840f4bb196bb5f842b
    Former-commit-id: 2613b42e80217daf446358a58f9b4be625a8dd18

---
+++ Validation/Scripts/FDS_SETUP.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FDS=$SVNROOT/FDS_Compilation/mpi_intel_linux_32/fds_mpi_intel_linux_32
+export FORTLIB=/opt/LIB32
+export MPIDIST=/shared/openmpi_32/
+export PATH=$MPIDIST/bin:$PATH
