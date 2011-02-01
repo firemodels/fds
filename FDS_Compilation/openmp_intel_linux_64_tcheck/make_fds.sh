ce0063b0204bd02acf7861156bb012add1def055
gforney <gforney@gmail.com>
2011-02-01 04:37:31 +0000

    build/VV scripts:  update scripts to build and run fds to use a single unified environment.
    Former-commit-id: e49bc897cc82d6a78804c703db6dd37e989753af [formerly 09a2477d98814248738a7c58eaadb1f00f4ae3f3]
    Former-commit-id: 771488d8a4ece4265f9f243da7d394c753604465
    Former-commit-id: 7434178873f59481e426676789bd800ed0288e97

---
+++ FDS_Compilation/openmp_intel_linux_64_tcheck/make_fds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+platform=intel64
+dir=`pwd`
+target=${dir##*/}
+
+source ../SET_MYFDSENV.sh $platform
+#source /exports/intel/itt/tcheck/bin/32/tcheckvars.csh
+
+echo Building $target
+make -j4 VPATH="../../FDS_Source" -f ../makefile_openmp $target
