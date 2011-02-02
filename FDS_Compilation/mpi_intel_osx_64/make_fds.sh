eb47a2f445e2d7c60934e943042f484712e3099c
gforney <gforney@gmail.com>
2011-02-02 15:25:25 +0000

    build/VV scripts: update Mac build scripts to use new "sh scheme"
    Former-commit-id: 6040af40af5e22db98a6a9426bd8beac294cb4ab [formerly d1c1a110619520ba48111f795862521459a7db48]
    Former-commit-id: 679c5a1daa57cf165b69f9190869e900052605dd
    Former-commit-id: 8fe24c66704288bb24d584722b207fd2847fc0e1

---
+++ FDS_Compilation/mpi_intel_osx_64/make_fds.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+platform=intel64
+dir=`pwd`
+target=${dir##*/}
+
+source ../SET_MYFDSENV.sh $platform build
+
+echo Building $target
+make -j4 VPATH="../../FDS_Source" -f ../makefile $target
