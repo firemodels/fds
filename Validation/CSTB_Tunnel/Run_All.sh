c1ab86c0df27c0885a4cd69768789c958d858df0
mcgratta <mcgratta@gmail.com>
2011-02-04 18:08:06 +0000

    FDS Validation: Edit CSTB_Tunnel input files and commit Run_All.sh.

---
+++ Validation/CSTB_Tunnel/Run_All.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+source ../../FDS_Compilation/SET_MYFDSENV.sh intel64 run
+
+# to override FDSMPI defined in above script, remove comment
+# from  line below and define your own FDSMPI location
+#export FDSMPI=override FDSMPI defined in above script
+
+# uncomment following line to stop all cases
+#export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$RUNFDS $INDIR CSTB_Tunnel_Test_2
+$RUNFDS $INDIR CSTB_Tunnel_Test_27
+
+echo FDS cases submitted
+
