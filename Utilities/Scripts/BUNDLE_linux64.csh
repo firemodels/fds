7a759f55478f242b0201fe1b46b10ac775b180ae
gforney <gforney@gmail.com>
2011-02-06 04:34:00 +0000

    bundle scripts: add script that creates a Linux installer (self-extracting tar file)
    Former-commit-id: 981e25c108e4cbb33c3c56f366050ff915a76bcd [formerly 3a828b4f0e91881de98cd76531cbe4cf70ccd9d4]
    Former-commit-id: f832670e6e5a0dbfba33363b820a7526d73411b5
    Former-commit-id: c118297c6c279f202b1818b79eac84f5bf87b24b

--- Utilities/Scripts/BUNDLE_linux64.csh
+++ Utilities/Scripts/BUNDLE_linux64.csh
@@ -10,7 +10,7 @@ setenv smvhost acrux.cfr.nist.gov
 
 setenv manifest manifest_linux_64.html
 
-setenv INTELLIB lib64
+setenv INTELLIB /shared/LIB64
 setenv smokezipdir INTEL_LINUX_64
 setenv smokediffdir INTEL_LINUX_64
 setenv fdsdir intel_linux_64
---
+++ Utilities/Scripts/make_installer.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+LIB=$1
+FDSTAR=$2
+INSTALLER=$3
+
+cat << EOF > $INSTALLER
+#!/bin/bash
+FDS_root=~/FDS6
+
+#
+# get FDS root directory
+#
+echo ""
+echo "*** FDS 6 Installer ***"
+echo ""
+echo "Enter FDS installation directory (default: \$FDS_root)"
+read answer
+if [ "\$answer" != "" ]; then
+FDS_root=\$answer
+fi
+ 
+# make the FDS root directory
+ 
+if [ ! -d \$FDS_root ]
+then
+echo "creating directory \$FDS_root"
+mkdir \$FDS_root>&/dev/null
+else
+while true; do
+    echo "The directory, \$FDS_root, already exists."
+    read -p "Do you wish to overwrite it? (yes/no)" yn
+    case \$yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Installation cancelled";exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+fi
+  
+# did we succeed?
+if [ ! -d \$FDS_root ]
+then
+echo "\`whoami\` does not have permission to create \$FDS_root."
+echo "FDS installation aborted."
+exit 0
+else
+echo "\$FDS_root has been created."
+fi
+
+# can we write to the FDS root directory?
+
+touch \$FDS_root/temp.\$\$>&/dev/null
+if ! [ -e \$FDS_root/temp.\$\$ ]
+then
+echo "\`whoami\` does not have permission to write to \$FDS_root."
+echo "FDS installation aborted."
+exit 0
+fi
+rm \$FDS_root/temp.\$\$
+
+# Find the beginning of the included FDS tar file
+# so it can be subsequently un-tar'd
+ 
+SKIP=\`awk '/^__TARFILE_FOLLOWS__/ { print NR + 1; exit 0; }' \$0\`
+
+# record the name of this script and the name of the directory it will be run in
+
+THIS=\`pwd\`/\$0
+THISDIR=\`pwd\`
+echo "Preliminaries complete."
+while true; do
+    read -p "Do you wish to proceed with the installation? (yes/no)" yn
+    case \$yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Installation cancelled";exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# now copy installation files into the FDS_root directory
+
+echo "Copying FDS installation files to"  \$FDS_root
+cd \$FDS_root
+tail -n +\$SKIP \$THIS | tar -xzv
+echo "Copy complete."
+
+echo "Setting path and LD_LIBRARY_PATH environment variables."
+
+echo "" > ~/.cshrc_fds
+echo "# Setting PATH and LD_LIBRARY_PATH environment" >> ~/.cshrc_fds
+echo "# variables for use by FDS" >> ~/.cshrc_fds
+echo "" >> ~/.cshrc_fds
+echo "" > ~/.bashrc_fds
+echo "# Setting PATH and LD_LIBRARY_PATH environment" >> ~/.bashrc_fds
+echo "# variables for use by FDS" >> ~/.bashrc_fds
+echo "" >> ~/.bashrc_fds
+if [ "a\$LD_LIBRARY_PATH" = "a" ]
+then
+echo "setenv LD_LIBRARY_PATH \`pwd\`/bin/$LIB" >> ~/.cshrc_fds
+echo "export LD_LIBRARY_PATH=\`pwd\`/bin/$LIB" >> ~/.bashrc_fds
+else
+echo "setenv LD_LIBRARY_PATH \`pwd\`/bin/$LIB:\\\$LD_LIBRARY_PATH" >> ~/.cshrc_fds
+echo "export LD_LIBRARY_PATH=\`pwd\`/bin/$LIB:\\\$LD_LIBRARY_PATH" >> ~/.bashrc_fds
+fi
+
+# add FDS bin to path
+
+echo "set PATH (\`pwd\`/bin \\\$PATH)" >> ~/.cshrc_fds
+echo "export PATH=\\\$PATH:\`pwd\`/bin" >> ~/.bashrc_fds
+
+cd \$THISDIR
+echo ""
+echo "Add the following line to your .cshrc file"
+echo "source ~/.cshrc_fds"
+echo ""
+echo "Add the following line to your .bashrc file"
+echo "source ~/.bashrc_fds"
+echo ""
+echo "Installation complete."
+exit 0
+
+
+__TARFILE_FOLLOWS__
+EOF
+chmod +x $INSTALLER
+cat $FDSTAR >> $INSTALLER
+echo "The installer, $INSTALLER, has been created."
