7c468fcb9aee9deab06bda7a0e22cd468b6fa2b1
gforney <gforney@gmail.com>
2011-02-24 20:58:19 +0000

    VV scripts: add scripts to create smokeview figures for smokeview guides  on a remote linux system.  Also, make various script names consistent.
    Former-commit-id: fbb75b79bab101bb830ce559c38ea49a3237a8d2 [formerly d60db2d010f387b0e824adbc11357b11e4602cb4]
    Former-commit-id: 542aca6e2ddb9de7731565798d789cb8d64e0006
    Former-commit-id: d824e251b1bfffa4ca27d3fc15161e24cd555c9b

---
+++ Verification/SMV_Cases.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+$RUNSMV Visualization capecod
+$RUNSMV Visualization colorbar
+$RUNSMV Visualization colorconv
+$RUNSMV Visualization objects_elem
+$RUNSMV Visualization objects_static
+$RUNSMV Visualization objects_dynamic
+$RUNSMV Visualization plume5a
+$RUNSMV Visualization plume5b
+$RUNSMV Visualization plume5c
+$RUNSMV Visualization plume5c_bounddef
+$RUNSMV Visualization sillytexture
+$RUNSMV Visualization smoke_sensor
+$RUNSMV Visualization smoke_test
+$RUNSMV Visualization smoke_test2
+$RUnSMV Visualization thouse5
+$RUNSMV Visualization script_test
+$RUNSMV Visualization transparency
+$RUNSMV Visualization sprinkler_many
+
+$RUNSMV Wui fire_line
+$RUNSMV Wui onetree_surf_1mesh
