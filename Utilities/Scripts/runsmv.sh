bcd3ea6792a01766c8e36cd810ed2a9d19277883
gforney <gforney@gmail.com>
2011-02-25 03:18:49 +0000

    scripts: update script that make smokeview figures
    Former-commit-id: 33a88a6e59e96e9971278be418eab6b0b58d6450 [formerly a094412432ea198e02bf0efa744ccf74c82f2841]
    Former-commit-id: d1c261ce01cef5787b844f1eeefd0bb7a6a97212
    Former-commit-id: 36712ceb8f3b70e71011dc69a7d885cb00e04be8

--- Utilities/Scripts/runsmv.sh
+++ Utilities/Scripts/runsmv.sh
@@ -14,7 +14,7 @@ if ! [ -d $fulldir ]; then
   exit
 fi
 if ! [ -e $fulldir/$in.smv ]; then
-  echo "The smokeview input file, $fulldir/$in.smv, does not exit. Run aborted."
+  echo "The smokeview file, $fulldir/$in.smv, does not exit. Run aborted."
   exit
 fi
 
@@ -22,13 +22,13 @@ disp=`id -u`
 Xvfb :$disp &
 rxs=$!
 
-#sleep 8
+sleep 8
 
 export DISPLAY=:$disp
 source ~/.bashrc_fds intel64
 cd $fulldir
 $SMV -runscript $in
 
-#sleep 8
+sleep 8
 
 kill $rxs
