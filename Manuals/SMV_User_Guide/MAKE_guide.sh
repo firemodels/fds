66a4519fec2b6d6226f33caee02c06085c584e9a
gforney <gforney@gmail.com>
2011-02-25 03:16:50 +0000

    scripts: add scripts that compile the smokeview guides

---
+++ Manuals/SMV_User_Guide/MAKE_guide.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pdflatex SMV_User_Guide
+bibttex SMV_User_Guide
+pdflatex SMV_User_Guide
+pdflatex SMV_User_Guide
---
+++ Manuals/SMV_Verification_Guide/MAKE_guide.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pdflatex SMV_Verification_Guide
+bibtex SMV_Verification_Guide
+pdflatex SMV_Verification_Guide
+pdflatex SMV_Verification_Guide
