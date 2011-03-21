da4c0b3e35541d59b11db90e9cb6b56bae42e5bf
mcgratta <mcgratta@gmail.com>
2011-03-21 13:03:06 +0000

    FDS Validation: Minor edit to check_hrr.
    Former-commit-id: 614a92f4ce34cac397ed3dcc9a2edb89296ec8b2 [formerly 7566b78fbddb944865826bc00f794e5789bee8fb]
    Former-commit-id: 7b695b7248cc4d9f7b3bf95d0fe7b5c68c29e9cd
    Former-commit-id: c4a95c83e70402cb0449eb395a726987c540c142

--- Utilities/Matlab/master_validation_script.m
+++ Utilities/Matlab/master_validation_script.m
@@ -1,22 +1,36 @@
 % McGrattan
-% 7-8-2009
+% March 21, 2011
 % master_validation_script.m
-%
-% This script processes the plots that are specified in the file 
-% validation_data_config_matlab.csv.
-%
-% See also: dataplot, scatplot
+
+% This script creates the plots that are included in the FDS Validation 
+% Guide. It consists of calls to other scripts contained within the
+% subfolder called "scripts". 
+
+% The most important script is called dataplot. It reads the file called
+% validation_data_config_matlab.csv and generates 1000+ plots. If you
+% want to process only some of these plots, comment out the other 
+% scripts and change the data plot line as follows:
+
+% [saved_data,drange] = dataplot('validation',[a:b]);
+
+% where a and b are the lines in the .csv file you want to process.
 
 close all
 clear all
-
 addpath 'scripts'
+
+% Scripts that run prior to dataplot
 
 flame_height
 
-[saved_data,drange] = dataplot('validation');
+% dataplot creates most of the plots for the Validation Guide. It must
+% be run before scatplot, which makes the scatter plots.
 
+[saved_data,drange] = dataplot('validation');
 scatplot(saved_data,drange)
+
+% Miscellaneous other scripts for special cases
+
 beyler_hood
 check_hrr
 sandia_helium_plume
