087e5d76f1a17c6246b45350a36dd07d103ab68d
mcgratta <mcgratta@gmail.com>
2011-03-23 20:43:31 +0000

    FDS Validation: Modify Run_All scripts for parallel cases.

--- Validation/VTT/Run_All.sh
+++ Validation/VTT/Run_All.sh
@@ -4,11 +4,11 @@
 # a batch queuing system
 
 export SVNROOT=`pwd`/../..
-export FDS=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
-source ../../FDS_Compilation/SET_MYFDSENV.sh intel64
+source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
 #export STOPFDS=1
