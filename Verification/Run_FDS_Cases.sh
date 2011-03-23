85257e1250aa7c46c0261221248e183248d98989
mcgratta <mcgratta@gmail.com>
2011-03-23 20:23:23 +0000

    FDS Verification: Restore Run_FDS_Cases.sh to its original form.
    Former-commit-id: 1828127b300e127e7b40105b04f5cb8687389771 [formerly 3cdf213d51d09c6d4aa13fccff166917098c87bc]
    Former-commit-id: 9199c4a3b4c920db22975d1d4df94c7545ff6074
    Former-commit-id: 848cd32187d7da31bec48a551e4e75d469427107

--- Verification/Run_FDS_Cases.sh
+++ Verification/Run_FDS_Cases.sh
@@ -7,8 +7,9 @@ export SVNROOT=`pwd`/..
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
 export BASEDIR=`pwd`
+
 # uncomment following line to stop all cases
-export STOPFDS=1
+#export STOPFDS=1
 
 ./FDS_Cases.sh
 
