1d959f91a7e807b7f88c5a6d83909eacf1cc25bf
gforney <gforney@gmail.com>
2011-03-24 18:34:31 +0000

    smv build: fix typo in build for 64 bit linux smokeview

--- SMV/Build/INTEL_LINUX_64/make_smv.sh
+++ SMV/Build/INTEL_LINUX_64/make_smv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -f
-source $IFORT_COMIPLER/bin/iccvars.sh intel64
-source $IFORT_COMIPLER/bin/ifortars.sh intel64
+source $IFORT_COMPILER/bin/iccvars.sh intel64
+source $IFORT_COMPILER/bin/ifortvars.sh intel64
 make -j4 -f ../Makefile intel_linux_64
