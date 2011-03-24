a3a97bf1741acce14e1c171ca14e89a3372eab4e
gforney <gforney@gmail.com>
2011-03-24 13:17:56 +0000

    bundle scripts: update configuration file

--- SMV/scripts/fds_smv_env.bat
+++ SMV/scripts/fds_smv_env.bat
@@ -1,10 +1,10 @@
 @echo off
 Rem ------ FDS/Smokeview version and revision numbers ---------
 
-set smv_version=5.6.3a
-set smv_revision=7457
-set fds_version=fds6_test
-set fds_revision=7687
+set smv_version=v6test_7951
+set smv_revision=7951
+set fds_version=v6test_7950
+set fds_revision=7950
 set fds_edition=FDS6
 
 Rem ---------- FDS-Smokeview repository settings ------------
@@ -16,7 +16,7 @@ set linux_svn_root=FDS-SMV
 Rem ---------- User/Host names -----------------
 
 set linux_hostname=acrux.cfr.nist.gov
-set OSXHOST=bluesky.cfr.nist.gov
+set osx_hostname=bluesky.cfr.nist.gov
 set LINUXCOMPILE=fire79
 
 set linux_username=%username%
