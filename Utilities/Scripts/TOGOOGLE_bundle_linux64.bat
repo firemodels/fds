6dd7d0d583ee53e0bf6e9c5310c507c1e171016c
gforney <gforney@gmail.com>
2011-03-24 02:08:57 +0000

    bundle scripts: change linux bundle scripts to handle .sh installer (rather than .tar.gz)
    Former-commit-id: 4606c3f35f82667edc480f3dbe2ea59116f30ed6 [formerly 823c72b59f4af9ef94fe727ef28e425fe37da9e4]
    Former-commit-id: 699a7fa96e32ebb86f108c51ed1bf32f9a349b57
    Former-commit-id: 58374195872514c4f809c3f6d599a8197f61de32

--- Utilities/Scripts/TOGOOGLE_bundle_linux64.bat
+++ Utilities/Scripts/TOGOOGLE_bundle_linux64.bat
@@ -22,7 +22,7 @@ cd %svn_root%\Utilities\to_google
 set glabels=Type-Installer,Opsys-Linux_64,%fds_google_level%
 set dplatform=64 bit Linux
 set summary=Bundled FDS and Smokeview for %dplatform% (SVN r%fds_revision%,%smv_revision%)
-set exe=FDS_%fds_version%-SMV_%smv_version%_linux_64.tar.gz
+set exe=FDS_%fds_version%-SMV_%smv_version%_linux_64.sh
 
 
 echo Uploading %exe%
