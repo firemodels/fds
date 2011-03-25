5711296bae6e55a2ab74954f71959553f0cd63d3
gforney <gforney@gmail.com>
2011-03-25 17:08:20 +0000

    fds bundle: remove SET_FORT.sh, call configuration script directrly from various make_fds.sh scripts
    Former-commit-id: d8e688786f0dffb7fac3643ace56e5d5c875260b [formerly 2290c5d700536889d839f6e5b30ecd2f8b8089b6]
    Former-commit-id: 4fe2a53c0911110ae490c88588812258a39a3074
    Former-commit-id: 46868b3bec210633dd3f9fd4876cea13aec81f00

--- FDS_Compilation/mpi_intel_osx_32/make_fds.sh
+++ FDS_Compilation/mpi_intel_osx_32/make_fds.sh
@@ -3,7 +3,7 @@ platform=ia32
 dir=`pwd`
 target=${dir##*/}
 
-source ../SET_FORT.sh $platform
+source $IFORT_COMPILER/bin/ifortvars.sh $platform
 source ~/.bashrc_fds $platform
 
 echo Building $target
--- FDS_Compilation/openmp_intel_linux_64/make_fds.sh
+++ FDS_Compilation/openmp_intel_linux_64/make_fds.sh
@@ -3,7 +3,8 @@ platform=intel64
 dir=`pwd`
 target=${dir##*/}
 
-source ../SET_FORT.sh $platform
+source $IFORT_COMPILER/bin/ifortvars.sh $platform
+
 
 echo Building $target
 make -j4 VPATH="../../FDS_Source" -f ../makefile_openmp $target
