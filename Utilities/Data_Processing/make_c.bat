6987c69da4731778e2dabeb936009c7a361bc4a4
gforney <gforney@gmail.com>
2011-04-13 11:33:19 +0000

    bundle scripts: update script to build sh2bat.exe
    Former-commit-id: 8529dff87d14dabdb98116d1488266b3a25e8b28 [formerly d17579708285b329dd9ae1183467c4f4adb46a97]
    Former-commit-id: c233ccba409c7dd9400bce16576253a497768f79
    Former-commit-id: 5cc9fea40c37df1649f417004d52a07557905d3c

--- Utilities/Data_Processing/make_c.bat
+++ Utilities/Data_Processing/make_c.bat
@@ -1,6 +1,7 @@
 set intelbin="%IFORT_COMPILER11%\bin"
 
 call %intelbin%\ifortvars ia32
+if exist "%VS_COMPILER%\..\..\bin\vcvars32.bat" call "%VS_COMPILER%\..\..\bin\vcvars32.bat"
 
 icl -o sh2bat sh2bat.c
 pause
