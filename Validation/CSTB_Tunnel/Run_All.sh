366cabc68d069d953782c492c507f629ef688c24
mcgratta <mcgratta@gmail.com>
2011-04-26 19:11:31 +0000

    FDS Validation: Minor changes for CSTB Tunnel.
    Former-commit-id: 86b51d7bea91aecf8f68d0941bca15f497662fc5 [formerly 4ea2fe065174d15e373c678e78fff976ddefd713]
    Former-commit-id: 39f5b4d5ab65ad8812a9e9bbe6e690ad0387edbe
    Former-commit-id: 28df322c4b45a9dbdd10da47b8c3aecd2f68e92f

--- Validation/CSTB_Tunnel/Run_All.sh
+++ Validation/CSTB_Tunnel/Run_All.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -f
 
 export SVNROOT=`pwd`/../..
-export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
-export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
-source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
 #export STOPFDS=1
