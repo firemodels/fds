c1f2b58afb59689855797d5b881ee4b3494fe9dc
gforney <gforney@gmail.com>
2011-04-27 04:24:19 +0000

    VV scripts: update scripts that make smokeview movies
    Former-commit-id: fea0c049a666802202f78ca6843e051ded5cafa2 [formerly f79c90a5e75410bce338e1795df37cef43cf54a9]
    Former-commit-id: af61583d5a1c8bcf541ff4502e02a0a4db2cc1c9
    Former-commit-id: 7b3301dcd26115f07ea3689c1af9bdbec11d0433

--- Utilities/Scripts/makemovie.sh
+++ Utilities/Scripts/makemovie.sh
@@ -3,3 +3,4 @@ base=$1
 out=$2
 #jpeg2yuv -f 25 -I p -j $base%04d.jpg | mpeg2enc -o $out.m1v
 png2yuv -f 25 -I p -j $base%04d.png | mpeg2enc -o $out.m1v
+echo The movie file $out.m1v has been created.
