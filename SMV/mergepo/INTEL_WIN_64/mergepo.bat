ca0cdb1496d277ec67e5310a0ddd765c79b3b05e
gforney <gforney@gmail.com>
2011-06-04 13:43:33 +0000

    build: add directories for building makepo and mergepo from the command line for WIN_64

---
+++ SMV/mergepo/INTEL_WIN_64/mergepo.bat
@@ -0,0 +1,14 @@
+@echo off
+
+Rem windows batch file to build smokeview from the command line
+
+IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER11=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER11%\bin\ifortvars" intel64
+call "%IFORT_COMPILER11%\bin\iclvars" intel64
+:envexist
+make -j4 -f ..\Makefile intel_win_64
+pause
