e0681e58abc88fe59ed464315f96ff82f804b1b7
gforney <gforney@gmail.com>
2011-07-07 17:24:41 +0000

    fds build: update fds mpi windows build batch files
    Former-commit-id: 1bd7cacc8e72c0ba60a95961cef793f67e00d942 [formerly 289d7ce19a18800e55aa3c702027444fa23ba118]
    Former-commit-id: 3bf7f8dd9c9346a363c6e97d52f72ebfa558fbcf
    Former-commit-id: 35787ffb515a6926c1606a8a9bdc22ad8e504984

--- FDS_Compilation/mpi_intel_win_32/make_fds_from_webpage.bat
+++ FDS_Compilation/mpi_intel_win_32/make_fds_from_webpage.bat
@@ -2,5 +2,5 @@ set intelbin="%IFORT_COMPILER11%\bin"
 
 call %intelbin%\ifortvars ia32
 
-make -j2 MPIINCLUDE="d:\mpich\mpich2_32\include" MPILIB="d:\mpich\mpich2_32\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_32
+make -j2 MPIINCLUDE="c:\mpich\mpich2_32\include" MPILIB="c:\mpich\mpich2_32\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_32
 pause
