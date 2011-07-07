21ae7c549b4f58e246922823aa235b07791efc2c
rmcdermo <randy.mcdermott@gmail.com>
2011-07-07 15:37:44 +0000

    FDS Source: account for FREEZE_VELOCITY in particle drag solution
    Former-commit-id: b7060410eca649179b5f3c6be9f74940a8b92b9c [formerly 339253e2ffb549628141103063c954a1ce479045]
    Former-commit-id: 67665bdecf06452e680857523d02dc57a8c79bec
    Former-commit-id: 693a1239b802d022dd8e913684bd1b80d3ca3fe8

--- FDS_Source/part.f90
+++ FDS_Source/part.f90
@@ -1007,8 +1007,9 @@ DROPLET_LOOP: DO I=1,NLP
          
    PARTICLE_NON_STATIC_IF: IF (.NOT.PC%STATIC .AND. DR%IOR==0) THEN
 
-      FP_MASS = (RHO_G/RVC)/NDPC(II,JJ,KK) ! fluid parcel mass
       DR_MASS = PC%DENSITY*FOTH*PI*RDC     ! droplet mass
+      FP_MASS = (RHO_G/RVC)/NDPC(II,JJ,KK) ! fluid parcel mass
+      IF (FREEZE_VELOCITY) FP_MASS = HUGE(1._EB)
                
       BETA  = 0.5_EB*RHO_G*C_DRAG*PI*RDS*(1._EB/DR_MASS+1._EB/FP_MASS)*QREL
       OBDT  = 1._EB+BETA*DT
