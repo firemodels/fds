a22a4108b69a6b9b4fdc239588033da9a135260b
mcgratta <mcgratta@gmail.com>
2011-07-25 19:09:27 +0000

    FDS Validation: New McCaffrey results.
    Former-commit-id: 623e34c97d8f7981d9db71ae5919bb7e66e719cc [formerly 855289a6dbdace216755f58de97b4cbf1cc8828a]
    Former-commit-id: 4e9013a49fb2125a64c5184559fcd6e1ed41474a
    Former-commit-id: 865f9e44594b0689fdce057247f048e7ceb1bbc2

--- Validation/McCaffrey_Plume/Run_All.sh
+++ Validation/McCaffrey_Plume/Run_All.sh
@@ -3,8 +3,11 @@
 export SVNROOT=`pwd`/../..
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
+source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
 # export STOPFDS=1
@@ -16,5 +19,8 @@ $RUNFDS $INDIR McCaffrey_22_kW
 $RUNFDS $INDIR McCaffrey_33_kW 
 $RUNFDS $INDIR McCaffrey_45_kW 
 $RUNFDS $INDIR McCaffrey_57_kW 
+
+$RUNFDS $INDIR McCaffrey_14_kW_coarse
+$RUNFDSMPI 18 $INDIR McCaffrey_14_kW_fine
 
 echo FDS cases submitted
