8d85abc61abf2dd67abbacc4f3c458ae5579757e
mcgratta <mcgratta@gmail.com>
2011-08-26 13:11:35 +0000

    FDS Verification: Add dancing_eddies to Users Guide.

--- Verification/FDS_Pictures.sh
+++ Verification/FDS_Pictures.sh
@@ -1,28 +1,20 @@
 $RUNSMV Controls activate_vents
 $RUNSMV Detectors beam_detector
 $RUNSMV Fires room_fire
-
 $RUNSMV Flowfields helium_2d
 $RUNSMV Flowfields sawtooth
 $RUNSMV Flowfields symmetry_test
-
 $RUNSMV HVAC HVAC_mass_conservation
 $RUNSMV HVAC HVAC_energy_pressure
 $RUNSMV HVAC leak_test_2
-
 $RUNSMV Miscellaneous pyramid
-
 $RUNSMV NS_Analytical_Solution ns2d_64
-
 $RUNSMV Pressure_Effects pressure_boundary
-
+$RUNSMV Pressure_Solver dancing_eddies
 $RUNSMV Scalar_Analytical_Solution  move_slug
 $RUNSMV Scalar_Analytical_Solution  move_slug_fl1
-
 $RUNSMV Sprinklers_and_Sprays cascade
-
 $RUNSMV Turbulence csmag_64
 $RUNSMV Turbulence dsmag_64
-
 $RUNSMV Visualization objects_static
 $RUNSMV Visualization objects_dynamic
