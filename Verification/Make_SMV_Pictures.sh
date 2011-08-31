ee52c3507527e7f4ff875c2344e3cce34e5c2ba6
gforney <gforney@gmail.com>
2011-08-31 13:23:37 +0000

    SMV VV: update scripts and the smokezip makefile

--- Verification/Make_SMV_Pictures.sh
+++ Verification/Make_SMV_Pictures.sh
@@ -5,10 +5,12 @@
 PLATFORM=64
 IPLATFORM=intel64
 export SVNROOT=`pwd`/..
-export SMV=$SVNROOT/SMV/Build/INTEL_LINUX_$PLATFORM/smokeview_linux_$PLATFORM
-export SMOKEZIP=$SVNROOT/Utilities/smokezip/INTEL_LINUX_$PLATFORM/smokezip_linux_$PLATFORM
-export SMOKEDIFF=$SVNROOT/Utilities/smokediff/INTEL_LINUX_$PLATFORM/smokediff_linux_$PLATFORM
-export BACKGROUND=$SVNROOT/Utilities/background/INTEL_LINUX_32/background
+#export SMV=$SVNROOT/SMV/Build/intel_linux__$PLATFORM/smokeview_linux_$PLATFORM
+export SMV=$SVNROOT/SMV/Build/intel_linux_test_$PLATFORM/smokeview_linux_test_$PLATFORM
+#export SMV=~/FDS/FDS5/bin/smv5_linux_64
+export SMOKEZIP=$SVNROOT/Utilities/smokezip/intel_linux_$PLATFORM/smokezip_linux_$PLATFORM
+export SMOKEDIFF=$SVNROOT/Utilities/smokediff/intel_linux_$PLATFORM/smokediff_linux_$PLATFORM
+export BACKGROUND=$SVNROOT/Utilities/background/intel_linux_32/background
 export RUNSMV=$SVNROOT/Utilities/Scripts/runsmv.sh
 export BASEDIR=`pwd`
 
