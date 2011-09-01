20ab1b9334369247794c7b66dd2a048bb7677a1e
rmcdermo <randy.mcdermott@gmail.com>
2011-09-01 16:52:11 +0000

    FDS Verification: update Make_FDS_Pictures.sh for FDS 6
    Former-commit-id: 2019cec26e6bd4f3d0e429c709db44b7fbd6d5de [formerly a53ad0c3f5af7e98c1eb818e73bd9d6eea7ecf67]
    Former-commit-id: d5a8c37de9dd7532742e7e5db7d88dc7a20900dc
    Former-commit-id: b57d2ee265930cf771f6a9ff6e026c8b3ad6e55c

--- Verification/Make_FDS_Pictures.sh
+++ Verification/Make_FDS_Pictures.sh
@@ -4,7 +4,7 @@
 # a batch queuing system
 
 export SVNROOT=`pwd`/..
-export SMV=~/FDS/FDS5/bin/smv5_linux_64
+export SMV=~/FDS/FDS6/bin/smokeview_linux_64
 export RUNSMV=$SVNROOT/Utilities/Scripts/runsmv.sh
 export BASEDIR=`pwd`
 
