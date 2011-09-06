17c417aaabef59d0e64dca8c152b81b42127d578
Topi Sikanen <Topi.Sikanen@gmail.com>
2011-09-06 07:43:58 +0000

    FDS Technical Reference Guide: Clean up in the absorption coefficient appendix directory. Replace the Data directory with one csv file. Delete unneeded script files.
    Former-commit-id: 09f01db43b1dca09c6b6db5f5c5e67d49b6c0fa7 [formerly 0c375f6b8cb9976f9cec85439e0c0e639d3d589c]
    Former-commit-id: e6aea73ed94ec914081cff79ab46866cfbf302f1
    Former-commit-id: 12ef4bf85df500ba4e11f47568a5613021e7d5c9

--- Manuals/FDS_Technical_Reference_Guide/FIGURES/Absorption_Coefficients_Appendix/Data/toluene/Keefe_toluene_2/readme.txt
+++
@@ -1,2 +0,0 @@
-Toluene (30 - 105°C) J. Mol. Struct., 737, 207-219 (2005). 
-http://keefelab.cbu.ca/dkeefe/?page_id=19
--- Manuals/FDS_Technical_Reference_Guide/FIGURES/Absorption_Coefficients_Appendix/readyspec.m
+++
@@ -1,9 +0,0 @@
-% luetaan spektri .y ASCII tiedostosta
-function [a,v]=readyspec(filename)
-    temp=importdata(filename); % in L /molcm
-    vstart=temp(1)*10^2; % oletetaan että v:n yksiköt 1/cm ja 1/cm->1/m
-    vend=temp(2)*10^2;
-    npt=temp(3);
-    v=linspace(vstart,vend,npt);
-    a=temp(4:end)*0.1; % L/mol/cm -> m^3/mol/m
-end
