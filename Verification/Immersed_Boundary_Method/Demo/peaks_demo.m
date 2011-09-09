9bad7824934c4267e2390216dadfeb2b158c941c
rmcdermo <randy.mcdermott@gmail.com>
2011-09-09 19:59:02 +0000

    FDS Verification: add peaks_demo, simple test of surface triangulation
    Former-commit-id: 3a99a63b1be98c29a7b544909cddd773b5243fb9 [formerly 43ce22f20a69055aa7e20685ce3dbe3040b4dbba]
    Former-commit-id: 280e3597b9b74be7d5022988355e788cd07bb6df
    Former-commit-id: 03c0a48f75d5ec16f9ef9d7aeb392e0310b5860a

---
+++ Verification/Immersed_Boundary_Method/Demo/peaks_demo.m
@@ -0,0 +1,62 @@
+% McDermott
+% 9-9-11
+% peaks_demo.m
+
+close all
+clear all
+
+[x,y] = meshgrid(1:15,1:15);
+tri = delaunay(x,y);
+z = peaks(15);
+%trimesh(tri,x,y,z)
+
+% write FDS input file
+
+fid = fopen('peaks_demo.fds','wt');
+
+head = ['&HEAD CHID=''peaks_demo'', TITLE=''Matlab demo of delaunay'' /']; fprintf(fid,'%s\n',head); fprintf(fid,'%s\n','  ');
+
+mesh = ['&MESH IJK=30,30,30, XB=0,15,0,15,-7.5,7.5/']; fprintf(fid,'%s\n',mesh); fprintf(fid,'%s\n','  ');
+
+time = ['&TIME T_END=0./']; fprintf(fid,'%s\n',time); fprintf(fid,'%s\n','  '); % blank line
+
+surf = ['&SURF ID=''terrain'', COLOR=''GREEN''/']; fprintf(fid,'%s\n',surf); fprintf(fid,'%s\n','  ');
+
+vent = ['&VENT MB=''XMIN'', SURF_ID=''OPEN''/']; fprintf(fid,'%s\n',vent);
+vent = ['&VENT MB=''XMAX'', SURF_ID=''OPEN''/']; fprintf(fid,'%s\n',vent);
+vent = ['&VENT MB=''YMIN'', SURF_ID=''OPEN''/']; fprintf(fid,'%s\n',vent);
+vent = ['&VENT MB=''YMAX'', SURF_ID=''OPEN''/']; fprintf(fid,'%s\n',vent);
+vent = ['&VENT MB=''ZMIN'', SURF_ID=''OPEN''/']; fprintf(fid,'%s\n',vent);
+vent = ['&VENT MB=''ZMAX'', SURF_ID=''OPEN''/']; fprintf(fid,'%s\n',vent);
+
+fprintf(fid,'%s\n','  '); % blank line
+
+% write NODE lines
+
+s = size(x);
+
+for i=1:s(1)
+    for j=1:s(2)
+       node = ['&NODE XYZ=',num2str(x(i,j)),',',num2str(y(i,j)),',',num2str(z(i,j)),' /'];
+       fprintf(fid,'%s\n',node);
+    end
+end
+
+fprintf(fid,'%s\n','  '); % blank line
+
+% write TRIS lines
+
+for i=1:length(tri(:,1))
+    tris = ['&TRIS NODES=',num2str(tri(i,1)),',',num2str(tri(i,3)),',',num2str(tri(i,2)),', SURF_ID=''terrain'' /'];
+    fprintf(fid,'%s\n',tris);
+end
+
+fprintf(fid,'%s\n','  '); % blank line
+
+tail = ['&TAIL /']; fprintf(fid,'%s\n',tail);
+
+fclose(fid);
+
+
+
+    
