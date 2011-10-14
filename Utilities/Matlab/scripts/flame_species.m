9044d898da395d92d7a3aa62d35598c7346045f5
cweinschenk <CraigWeinschenk@gmail.com>
2011-10-14 22:39:30 +0000

    FDS Verification: Update to reaction input test cases - added pre-process script to make singular plot
    Former-commit-id: 100de03e5f732c5887b4397626e9dc5030ae8b5d [formerly 3e3d42e22bd80522e2e8a51df2ca6e8e39d87296]
    Former-commit-id: 8ce9fd4bf843fec22f63e19515dbb09b11a8cb87
    Former-commit-id: ede2d8788f7a45c3a6df534321fe6e4e67fa04fc

---
+++ Utilities/Matlab/scripts/flame_species.m
@@ -0,0 +1,41 @@
+%------------------------------------------------
+% C Weinschenk
+% 10-2011
+% Combine outputs from Methane_flame_lumped and
+% Methane_flame_primitive into 1 file
+%------------------------------------------------
+
+%-----------------------
+% Write directory
+%-----------------------
+
+FDS_Output_Files = '../../Verification/Species/';
+
+%----------------------
+% Import files
+%----------------------
+
+primitive_struct=importdata([FDS_Output_Files,'Methane_flame_primitive_devc.csv']);
+primitive_data=primitive_struct.data;
+primitive_text=primitive_struct.textdata;
+
+lumped_struct=importdata([FDS_Output_Files,'Methane_flame_lumped_devc.csv']);
+lumped_data=lumped_struct.data;
+lumped_text=lumped_struct.textdata;
+
+header1=cat(2,primitive_text(1),lumped_text(1,2:4));
+header2=cat(2,primitive_text(2,:),lumped_text(2,2:4));
+data=cat(2,primitive_data,lumped_data(:,2:4));
+
+%-----------------------
+% Write new files
+%-----------------------
+
+fid = fopen([FDS_Output_Files,'Methane_flame_lumpedprimitive.csv'],'wt','n');
+
+fprintf(fid,'%s, %s, %s, %s, %s, %s, %s, %s\n',header1{1,:});
+fprintf(fid,'\n%s, %s, %s, %s, %s, %s, %s, %s\n',header2{1,:});
+for i=1:length(primitive_data)
+    fprintf(fid,'\n%f, %f, %f, %f, %f, %f, %f, %f\n',data(i,:));
+end
+fclose(fid);
