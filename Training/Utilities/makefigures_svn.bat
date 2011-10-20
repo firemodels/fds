be4c410483dba821cdb91ea5a7287687ba834ec5
gforney <gforney@gmail.com>
2011-10-20 23:55:28 +0000

    training: remove various training directories, both FDS cases and manual
    Former-commit-id: 7c2f75ce86adda8b72498b4939551f682c081437 [formerly cc304400c6add9a59b78f00f8949b7320c16dbcb]
    Former-commit-id: f43918d555b3e1d9810fc59fd841126131fcad6f
    Former-commit-id: 3d8f65c6b92d4ddda6430ed22a659415efbdba16

--- Training/Utilities/makefigures_svn.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-set RUNSMV=.\runsmv.bat
-set curdir=%CD%
-
-call %RUNSMV Demonstrations\2Room_Ranch ranch_01 
-call %RUNSMV Demonstrations\2Room_Ranch ranch_02 
-call %RUNSMV Demonstrations\2Room_Ranch ranch_03 
-call %RUNSMV Demonstrations\2Room_Ranch ranch_04 
-
