62ca54fa785234d3c204f7fcc1470a5420a667f7
mcgratta <mcgratta@gmail.com>
2011-10-21 19:27:20 +0000

    FDS Compilation: Update Intel compiler for Windows.
    Former-commit-id: 1320a48b9e38b471bcf56bc0355019f0568b46f6 [formerly 1aa741e072a2ffc213ad061bd83dd638c570b64e]
    Former-commit-id: 5aebe52a38d38cc7376232196b295b34d9596079
    Former-commit-id: 5c4e79b508dd285cb3798dd2257d206001ebe00b

--- FDS_Compilation/intel_win_64/make_fds64on32.bat
+++ FDS_Compilation/intel_win_64/make_fds64on32.bat
@@ -2,12 +2,12 @@
 
 Rem windows batch file to build smokeview from the command line
 
-IF "%SETUP_IFORT_COMPILER11%"=="1" GOTO envexist
+IF "%SETUP_IFORT_COMPILER12%"=="1" GOTO envexist
 
-set SETUP_IFORT_COMPILER11=1
+set SETUP_IFORT_COMPILER12=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER11%\bin\ifortvars" ia32_intel64
+call "%IFORT_COMPILER12%\bin\ifortvars" ia32_intel64
 :envexist
 erase *.obj
 erase *.mod
