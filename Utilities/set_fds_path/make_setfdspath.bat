33879e080d80decd010486c5cb381646091ed6ab
gforney <gforney@gmail.com>
2011-11-07 18:37:40 +0000

    build scripts: update makefile and build scripts for the set_fds_path utility

--- Utilities/set_fds_path/make_setfdspath.bat
+++
@@ -1,9 +0,0 @@
-set intelbin=c:\bin
-
-call %intelbin%\iclvars ia32
-call %intelbin%\ifortvars ia32
-
-Rem erase *.obj
-Rem erase *.mod
-make VPATH="../../SMV_5/source/set_path" -f Makefile intel_win_32
-pause
