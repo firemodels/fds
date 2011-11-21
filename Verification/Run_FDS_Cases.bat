143b6f3ed5b8307a5bc7e8c36553327cfbbc6f22
gforney <gforney@gmail.com>
2011-11-21 12:34:32 +0000

    FDS verification: output timing info when running the FDS verification suiite on Windows
    Former-commit-id: d2e7c4cde4507a50edb1129d02c4d73d7328f7bc [formerly 56bf4d45b47c9042efa241776a645701cec3bc3b]
    Former-commit-id: 0f7da660f8ddfecae83fee1713e6d3dcc5531e39
    Former-commit-id: e4f81d492f6b3f1ad13f1a9a057a43ff2af0de74

--- Verification/Run_FDS_Cases.bat
+++ Verification/Run_FDS_Cases.bat
@@ -3,6 +3,7 @@ set svn_drive=d:
 
 set BASEDIR="%CD%"
 set SVNROOT=%BASEDIR%\..\
+set TIME_FILE="%BASEDIR%\fds_case_times.txt"
 
 Rem Choose one of the following four FDS "definitions" by commenting all lines but one.
 
@@ -31,7 +32,16 @@ echo creating FDS case list from FDS_Cases.csh
 ..\Utilities\Data_Processing\sh2bat FDS_Cases.sh FDS_Cases.bat
 
 echo running FDS cases
+
+echo "FDS test cases begin" > %TIME_FILE%
+date /t >> %TIME_FILE%
+time /t >> %TIME_FILE%
+
 call FDS_Cases.bat
+
+echo "FDS test cases end" > %TIME_FILE%
+date /t >> %TIME_FILE%
+time /t >> %TIME_FILE%
 
 erase FDS_Cases.bat
 
