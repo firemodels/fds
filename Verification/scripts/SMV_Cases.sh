caac313bd5cddf380eaa4369faf1c5dda7a4af8d
gforney <gforney@gmail.com>
2011-11-23 13:57:44 +0000

    smv vv: convert smokeview vv scripts to run from Verification\scripts directory
    Former-commit-id: 9f05f476b0e499ebaa1f09e09faf3cfdb74704be [formerly 5fc96dc3ffa3eb839bfa2b4d412ae36e74e16c68]
    Former-commit-id: 74d37c1201fc2f24c7b317e3b02b1158f05f63f3
    Former-commit-id: 34c2b95bdb4cd1fe39509845c96b288b0893202c

--- Verification/scripts/SMV_Cases.sh
+++ Verification/scripts/SMV_Cases.sh
@@ -6,7 +6,6 @@ $RUNFDS Visualization colorconv
 $RUNFDS Visualization objects_elem
 $RUNFDS Visualization objects_static
 $RUNFDS Visualization objects_dynamic
-$RUNFDS Visualization objects_dynamic3
 $RUNFDS Visualization plume5a
 $RUNFDS Visualization plume5b
 $RUNFDS Visualization plume5c
@@ -19,7 +18,6 @@ $RUNFDS Visualization smoke_test2
 $RUNFDS Visualization sprinkler_many
 $RUNFDS Visualization thouse5
 $RUNFDS Visualization transparency
-$RUNFDS Visualization vis_test2
 
 $RUNFDS WUI fire_line
 $RUNFDS WUI onetree_surf_1mesh
