60573b882601b4d9b1ec6781b104bf5e638c9855
mcgratta <mcgratta@gmail.com>
2011-11-23 21:35:46 +0000

    FDS Utilities: Remove unused programs.

--- Utilities/Data_Processing/make_fortran.sh
+++ Utilities/Data_Processing/make_fortran.sh
@@ -11,8 +11,4 @@ ifort -o flame_height flame_height.f90
 ifort -o Hamins_CH4_Average Hamins_CH4_Average.f90
 ifort -o layer_height layer_height.f
 ifort -o NIST_RSE_1994 NIST_RSE_1994.f90
-ifort -o ns2d ns2d.f90
-ifort -o radiation_box radiation_box.f90
-ifort -o radiation_plane_layer radiation_plane_layer.f90
-ifort -o wall_internal_radiation wall_internal_radiation.f90
 
