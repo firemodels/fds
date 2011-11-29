e6c4915dd94941438ebed8f0302d86abb8074b31
mcgratta <mcgratta@gmail.com>
2011-11-29 19:52:07 +0000

    FDS Utilities: remove unneeded scripts.
    Former-commit-id: 2f73fef883e4fbdd5b48356a02c597d2851ec8e2 [formerly 324dc8973db438855c556b7157596c7efd7611df]
    Former-commit-id: a559c0bf82b5ba200a1a85b6267e539675437125
    Former-commit-id: 72e207fa5a1179e54aab8beae8aaa9f60c8c4b76

--- Utilities/Data_Processing/make_fortran.bat
+++ Utilities/Data_Processing/make_fortran.bat
@@ -3,7 +3,6 @@ set intelbin="%IFORT_COMPILER11%\bin"
 call %intelbin%\ifortvars ia32
 
 ifort -o fds2ascii fds2ascii.f90
-ifort -o flame_height flame_height.f90
 ifort -o Hamins_CH4_Average Hamins_CH4_Average.f90
 ifort -o layer_height layer_height.f
 ifort -o NIST_RSE_1994 NIST_RSE_1994.f90
--- Utilities/Data_Processing/make_fortran.sh
+++ Utilities/Data_Processing/make_fortran.sh
@@ -7,7 +7,6 @@ source $IFORT_COMPILER/bin/ifortvars.sh intel64
 
 ifort -o Beyler_Hood Beyler_Hood.f90
 ifort -o fds2ascii fds2ascii.f90
-ifort -o flame_height flame_height.f90
 ifort -o Hamins_CH4_Average Hamins_CH4_Average.f90
 ifort -o layer_height layer_height.f
 ifort -o NIST_RSE_1994 NIST_RSE_1994.f90
