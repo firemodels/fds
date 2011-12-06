6487296676552a57c102606e299d03e69f4436ca
rmcdermo <randy.mcdermott@gmail.com>
2011-12-06 15:42:01 +0000

    FDS Validation: add Lattimer tilted wall case
    Former-commit-id: 0ba45e99db52524de86886b400d7e86e5f5e7219 [formerly 59175c6ed43de6e03d94e85e4c6af0cf92296f8a]
    Former-commit-id: 81754902356de22452aeea88a87144fbb5838793
    Former-commit-id: b5154f46c3a4551474da7d3c44ecd209e168948c

---
+++ Validation/Lattimer_Tilted_Wall/Run_All.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+source ~/.bashrc_fds intel64
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$RUNFDS $INDIR Lattimer_20_kW_0_degree_coarse
+
+#$RUNFDSMPI 36 $INDIR Lattimer_20_kW_0_degree_fine
+
+echo FDS cases submitted
