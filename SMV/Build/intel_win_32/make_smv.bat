46cc93eb6578558bc621a55e884858c404324af1
gforney <gforney@gmail.com>
2011-12-09 23:20:19 +0000

    smokeview build: update build scripts to use version 12 environment variables

--- SMV/Build/intel_win_32/make_smv.bat
+++ SMV/Build/intel_win_32/make_smv.bat
@@ -7,10 +7,10 @@ IF "%SETUP_IFORT_COMPILER_IA32%"=="1" GOTO envexist
 set SETUP_IFORT_COMPILER_IA32=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER11%\bin\ifortvars" intel64 vs2008
-call "%IFORT_COMPILER11%\bin\iclvars" intel64 vs2008
-call "%IFORT_COMPILER11%\bin\ifortvars" ia32 vs2008
-call "%IFORT_COMPILER11%\bin\iclvars" ia32 vs2008
+call "%IFORT_COMPILER12%\bin\ifortvars" intel64 vs2008
+call "%IFORT_COMPILER12%\bin\iclvars" intel64 vs2008
+call "%IFORT_COMPILER12%\bin\ifortvars" ia32 vs2008
+call "%IFORT_COMPILER12%\bin\iclvars" ia32 vs2008
 :envexist
 make -f ..\Makefile intel_win_32
 pause
