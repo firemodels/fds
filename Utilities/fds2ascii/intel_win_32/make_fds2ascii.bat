b1b384cbc9fc738bff2e92e78d73f456ff9893ea
gforney <gforney@gmail.com>
2011-12-10 13:41:58 +0000

    utilitiy build: convert build scripts to use version 12 intel compiler environment  variables

--- Utilities/fds2ascii/intel_win_32/make_fds2ascii.bat
+++ Utilities/fds2ascii/intel_win_32/make_fds2ascii.bat
@@ -1,4 +1,4 @@
-call "%IFORT_COMPILER11%"\bin\ifortvars ia32
+call "%IFORT_COMPILER12%"\bin\ifortvars ia32
 erase *.obj
 make -f ..\Makefile intel_win_32
 pause
--- Utilities/smokediff/intel_win_32/make_diff.bat
+++ Utilities/smokediff/intel_win_32/make_diff.bat
@@ -7,8 +7,8 @@ IF "%SETUP_IFORT_COMPILER_IA32%"=="1" GOTO envexist
 set SETUP_IFORT_COMPILER_IA32=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER11%\bin\ifortvars" ia32
-call "%IFORT_COMPILER11%\bin\iclvars" ia32
+call "%IFORT_COMPILER12%\bin\ifortvars" ia32
+call "%IFORT_COMPILER12%\bin\iclvars" ia32
 if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
 :envexist
 erase *.obj
