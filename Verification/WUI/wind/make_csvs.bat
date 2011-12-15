ae404e8b9133073a2bb65f93118ecb20fc1e7172
gforney <gforney@gmail.com>
2011-12-15 18:02:13 +0000

    wui verification: add batch file that converts spreadsheet data
    Former-commit-id: 455e030ee9580b9dd4b194cecbf1ae2e5f858d1c [formerly b5e3be88039125bb82b7c63f87ca427c8bbcbf5e]
    Former-commit-id: 189fbc49cd14c0b5779a4acaaa06110c1cdbc68a
    Former-commit-id: 5d5aaa6e64b40bf1c68098ff8b6e50b8f142f7db

---
+++ Verification/WUI/wind/make_csvs.bat
@@ -0,0 +1,6 @@
+set convert=..\..\..\utilities\sodar2fds\intel_win_64\sodar2fds_win_64
+
+%convert% -prefix sd11 -offset " 50.0  50.0 0.0" a111024.csv
+%convert% -prefix sd12 -offset " 50.0 150.0 0.0" a111025.csv
+%convert% -prefix sd21 -offset "150.0  50.0 0.0" a111026.csv
+%convert% -prefix sd22 -offset "150.0 150.0 0.0" a111027.csv
