35e6f56d576d5692fffa329d1f33cd2b1a30ef65
gforney <gforney@gmail.com>
2012-01-01 00:38:56 +0000

    verification scripts: set up Run_FDS_Cases.sh to handle MPI cases - put MPI cases in FDS_MPI_Cases.sh file
    Former-commit-id: 6ba387b6e4dcb862dc99ade3ca68b57ecf9483eb [formerly 0aeb7d3bff45293f687a6d38e93a9ddb74993e2c]
    Former-commit-id: 302a01572d46368f813760a5b2b864ba2b56a92e
    Former-commit-id: 6d3443dc7159c242b1ede4a2b055581168a33457

--- Verification/Run_FDS_Cases.sh
+++ Verification/Run_FDS_Cases.sh
@@ -5,13 +5,16 @@
 
 export SVNROOT=`pwd`/..
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
 
 # uncomment following line to stop all cases
 # export STOPFDS=1
 
 ./FDS_Cases.sh
+./FDS_MPI_Cases.sh
 
 echo FDS cases submitted
 
