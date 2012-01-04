0445756a7b2fb9c2e6189a2578f4f333eac9a94f
rmcdermo <randy.mcdermott@gmail.com>
2012-01-04 22:15:16 +0000

    Matlab: add to flame_height script output of FDS results for Tamanini HRR profiles
    Former-commit-id: 54e06ede542ad96bd2d79044b3b89855def5821b [formerly 68310422fd77864f22880693675d52d980dea237]
    Former-commit-id: 10657141bcdc71b27fa04c3fa6664a82f3239f1e
    Former-commit-id: df6f6bc457e2501f7e636b91b3d621547f96ed30

--- Utilities/Matlab/scripts/flame_height.m
+++ Utilities/Matlab/scripts/flame_height.m
@@ -36,6 +36,7 @@ T_inf = 293;
 g = 9.81;
 D = 1.13;
 f=0.99;
+%Q* =  .1  .2  .5    1    2    5    10    20    50    100    200    500    1000    2000    5000    10000
 Qdot=[151 303 756 1513 3025 7564 15127 30255 75636 151273 302545 756363 1512725 3025450 7563625 15127250];
 
 for i=1:16 % hrr loop
@@ -57,7 +58,7 @@ for i=1:16 % hrr loop
             L(j) = z(k-1)+dz*(f*Qdot(i)-hrr(k-1))/(hrr(k)-hrr(k-1));
         else
             L(j) = dz*f*Qdot(i)/hrr(k);
-        end
+		end
         
     end % resolution loop
     
@@ -85,6 +86,73 @@ for i=1:16
     fprintf(fid,'%f, %f\n',H(i,:));
 end
 fclose(fid);
+
+% Generate FDS results for Tamanini cases
+
+close all
+clear all
+
+filename{1} = '../../Validation/Heskestad_Flame_Height/FDS_Output_Files/FDS_Tamanini_RI=05.csv';
+filename{2} = '../../Validation/Heskestad_Flame_Height/FDS_Output_Files/FDS_Tamanini_RI=10.csv';
+filename{3} = '../../Validation/Heskestad_Flame_Height/FDS_Output_Files/FDS_Tamanini_RI=20.csv';
+
+fds_line_file = {'Qs=2000_RI=05_line.csv', 'Qs=p5_RI=05_line.csv', 'Qs=p2_RI=05_line.csv'; ...
+	             'Qs=2000_RI=10_line.csv', 'Qs=p5_RI=10_line.csv', 'Qs=p2_RI=10_line.csv'; ...
+				 'Qs=2000_RI=20_line.csv', 'Qs=p5_RI=20_line.csv', 'Qs=p2_RI=20_line.csv'};
+			 
+Qstar = [2000 .5 .2];
+header = {'z/L jet','Q jet','z/L 62','Q 62','z/L 31','Q 31'};
+
+for j=1:3 % resolution loop
+	
+	clear z hrr
+	
+	fid = fopen(filename{j},'wt');
+	
+	fprintf(fid,'%s, %s, %s, %s, %s, %s\n',header{:});
+	A = [];
+	
+	for k=1:3 % hrr loop
+		
+		M = importdata(fds_line_file{j,k},',',2);
+		z = M.data(:,1);
+		dz = z(2)-z(1);
+		hrrpul = M.data(:,2);
+		Qdot_line = sum(hrrpul)*dz;
+		L(k) = 1.13*(3.7*Qstar(k)^(2/5)-1.02); % Heskestad correlation, D=1.13
+		
+		% determine flame height
+		for n=1:length(z)
+			hrr(n) = sum(hrrpul(1:n))*dz/Qdot_line; % cummulative heat release
+		end
+		
+		A=[A,(z+dz/2)/L(k),hrr'];
+		
+	end % hrr loop
+	
+	for i=1:length(z)
+		fprintf(fid,'%f, %f, %f, %f, %f, %f\n',A(i,:));
+	end
+	fclose(fid);
+	
+end % resolution loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
