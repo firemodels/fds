f095fdd7edb9a727ed6f82f2d8fdcf9287d8395f
gforney <gforney@gmail.com>
2012-01-15 00:30:09 +0000

    cluster scripts: add optional parameter (-q ) used to specify queues when submitting jobs on the cluster
    Former-commit-id: 71a4b2b98ab45696abe6a53d05d487a917b8576b [formerly 24f63714fc2b6ba3c19d010aaec80962a54bf2e2]
    Former-commit-id: 573d654970dec94ed1f1465747c8c9976ca367c1
    Former-commit-id: 90bc08c306180953267ba201ae24c7bae294bc79

--- Utilities/Scripts/bundle_setup/qfdsmpi.sh
+++ Utilities/Scripts/bundle_setup/qfdsmpi.sh
@@ -1,76 +1,92 @@
 #!/bin/bash -f
-EXPECTED_ARGS=4
+EXPECTED_ARGS=3
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: qfdsmpi.sh nthreads platform fds_command casename.fds"
+  echo "Usage: qfdsmpi.sh [-q queue] nthreads fds_command casename.fds"
   echo ""
   echo "Runs a parallel FDS case on a Linux cluster using the "
-  echo "PBS/SGE qsub batch queuing command (SVN $Revision$')"
+  echo "PBS/SGE qsub batch queuing command"
   echo ""
+  echo " -q queue     - optional parameter used to specify the name of the queue"
+  echo                  choices: batch (default), vis,fire60s, fire70s"
   echo "    nthreads - number of threads (usually number of &mesh lines)"
-  echo "    platform - ia32 or intel64"
   echo " fds_command - full path to fds command name"
-  echo "casename.fds - fds case"
+  echo "casename.fds - FDS input file"
   echo 
   exit
 fi
-nthreads=$1
-platform=$2
-fds=$3
-in=$4
-if test $nthreads -le 0
+queue=batch
+ARGS=("$@")
+for ((argi=0,i=0;i<$ARGC;i++)) ; do
+arg=${ARGS[$i]}
+case $arg in
+ -q)
+   let i=$i+1
+   queue=${ARGS[$i]}
+  ;;
+  *)
+   if [ $argi -eq 0 ] ; then
+     nthreads=$arg
+     let argi=$argi+1
+   elif [ $argi -eq 1 ] ; then
+     fds=$arg
+     let argi=$argi+1
+   elif [ $argi -eq 1 ] ; then
+     in=$arg
+     let argi=$argi+1
+   fi
+  ;;
+esac
+done
+
+
+nprocs=8
+nnodes=$(echo "($nthreads-1)/$nprocs+1" | bc)
+if test $nnodes -le 0
 then
-echo "Number of threads specified is $nthreads . Must be bigger than 0."
-exit
+nnodes=1
 fi
-nnodes=$(echo "($nthreads-1)/8+1" | bc)
-nprocs=$(echo "($nthreads-1)/$nnodes+1" | bc)
 
 infile=${in%.*}
 fulldir=`pwd`
 
-err=$fulldir/$infile.err
-err2=$fulldir/$infile.err2
+out=$fulldir/$infile.err
 outlog=$fulldir/$infile.log
 
 scriptfile=/tmp/script.$$
 if ! [ -e $fulldir/$in ]; then
-  echo "The fds input file, $fulldir/$in, does not exist. Run aborted."
+  echo "The fds input file, $fulldir/$in, does not exit. Run aborted."
   exit
 fi
 if ! [ -e $fds ]; then
-  echo "The FDS program name, $fds, does not exist. Run aborted."
+  echo "The FDS program name, $fds, does not exit. Run aborted."
   exit
 fi
-if ! [ -e ~/.bashrc_fds ]; then
-  echo "The environment file, ~/.bashrc_fds, does not exist. Run aborted."
-  exit
-fi
-source ~/.bashrc_fds $platform
 if [ -e $outlog ]; then
   echo "Removing log file: $outlog"
   rm $outlog
 fi
 
 cat << EOF > $scriptfile
-#!/bin/bash
+#!/bin/bash -f
 #PBS -N $infile(MPI)
-#PBS -e $out2
+#PBS -e $out
 #PBS -o $outlog
 #PBS -l nodes=$nnodes:ppn=$nprocs
 #\$ -N $infile(MPI)
-#\$ -e $out2
+#\$ -e $out
 #\$ -o $outlog
 #\$ -l nodes=$nnodes:ppn=$nprocs
+
 cd $fulldir
 echo Start time: \`date\`
 echo Running $infile on \`hostname\`
 echo Directory: \`pwd\`
 
-$MPIDIST/bin/mpirun -np $nthreads $fds $in 2>$err
+mpirun -np $nthreads $fds $in
 EOF
 chmod +x $scriptfile
 echo Running $in 
-qsub -V $scriptfile
+qsub -q $queue $scriptfile
 rm $scriptfile
