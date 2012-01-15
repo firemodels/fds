a1a9700f9924d13b6d287ee6fff5a00840998983
gforney <gforney@gmail.com>
2012-01-15 00:30:09 +0000

    cluster scripts: add optional parameter (-q ) used to specify queues when submitting jobs on the cluster
    Former-commit-id: 71a4b2b98ab45696abe6a53d05d487a917b8576b [formerly 24f63714fc2b6ba3c19d010aaec80962a54bf2e2]
    Former-commit-id: 573d654970dec94ed1f1465747c8c9976ca367c1
    Former-commit-id: 90bc08c306180953267ba201ae24c7bae294bc79

--- Utilities/Scripts/bundle_setup/qfds.sh
+++ Utilities/Scripts/bundle_setup/qfds.sh
@@ -1,20 +1,40 @@
 #!/bin/bash -f
 EXPECTED_ARGS=2
 
-if [ $# -ne $EXPECTED_ARGS ]
+if [ $# -lt $EXPECTED_ARGS ]
 then
-  echo "Usage: qfds.sh fds_command casename.fds"
+  echo "Usage: qfds.sh [-q queue] fds_command casename.fds"
   echo ""
   echo "Runs a serial FDS case on a Linux cluster using the "
   echo "PBS/SGE qsub batch queuing command (SVN $Revision$')"
   echo ""
+  echo " -q queue     - optional parameter used to specify the name of the queue"
+  echo                  choices: batch (default), vis, fire60s, fire70s"
   echo "  fds_command - full path to fds command name"
-  echo " casename.fds - fds case"
-  echo 
+  echo " casename.fds - FDS input file"
+  echo
   exit
 fi
-fds=$1
-in=$2
+queue=batch
+ARGS=("$@")
+for ((argi=0,i=0;i<$ARGC;i++)) ; do
+arg=${ARGS[$i]}
+case $arg in
+ -q)
+   let i=$i+1
+   queue=${ARGS[$i]}
+  ;;
+  *)
+   if [ $argi -eq 0 ] ; then
+     fds=$arg
+     let argi=$argi+1
+   elif [ $argi -eq 1 ] ; then
+     in=$arg
+     let argi=$argi+1
+   fi
+  ;;
+esac
+done
 
 infile=${in%.*}
 fulldir=`pwd`
@@ -65,5 +85,5 @@ $fds $in 2>$err
 EOF
 chmod +x $scriptfile
 echo Running $in 
-qsub $scriptfile
+qsub -q $queue $scriptfile
 rm $scriptfile
