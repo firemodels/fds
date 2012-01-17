f7527935e93cb5925b9341f7770642903caf9dc7
gforney <gforney@gmail.com>
2012-01-17 03:06:06 +0000

    cluster scripts: edits to qfds.sh to improve usability

--- Utilities/Scripts/bundle_setup/qfds.sh
+++ Utilities/Scripts/bundle_setup/qfds.sh
@@ -8,44 +8,53 @@ EXPECTED_ARGS=1
 if [ $# -lt $EXPECTED_ARGS ]
 then
   echo "Usage: qfds.sh [-f repository root] [-n processes per node] [-q queue]"
-  echo "               [-r] [-t nthreads] [fds_command] casename.fds"
+  echo "               [-r] [-p nprocesses] [fds_command] casename.fds"
   echo ""
-  echo "Runs an FDS case on a Linux cluster using the PBS/SGE qsub command"
-  echo "This script can use the FDS executable specified on the command line"
-  echo "or located in the users FDS repository (if the -r or -f option is "
-  echo "specified.  Alternate queues may be specified using the -q option."
+  echo "This script runs FDS on a Linux cluster using an executable specified" 
+  echo "on the command line or if the -r option is specified an executable"
+  echo "located in the repository. The parallel version of FDS may be run by" 
+  echo "using -p to specifying multiple processes. Alternate queues (vis, "
+  echo "fire60s or fire70s) are set using the -q option."
   echo ""
-  echo " -n procs per node -  maximum processes assigned per node [default: 8]"
-  echo " -t nthreads - number of threads used to run case [default: 1] "
-  echo " -q queue - name of the queue. choices: batch (default), vis, fire60s,"
-  echo "    fire70s"
-  echo " -r - use repository executable"
-  echo " -f repository root - repository where FDS executable"
+  echo " -n processes per node - maximum number of processes assigned per"
+  echo "    node [default: 8, (4 for the fire60s queue)]"
+  echo " -p nprocesses - number of processes used to run a case [default: 1] "
+  echo " -q queue - name of the queue. choices: [default: batch (other choices:"  
+  echo "    vis, fire60s and fire70s)"
+  echo " -r - use FDS located in repository"
+  echo " -f repository root - name and location of repository where FDS "
   echo "    is located  [default: ~/FDS-SMV]"
-  echo " fds_command - full path to fds command name (not used if -f or "
-  echo "    -r options are specified)"
+  echo " fds_command - full path to fds command name (not needed if either"
+  echo "    -f or -r options are specified)"
   echo "casename.fds - FDS input file"
   echo ""
   echo "Examples:"
   echo "qfds.sh -r casename.fds"
-  echo "    run casename.fds using "
-  echo "    ~/FDS-SMV/FDS_Compilation/intel_linux_64/fds_intel_linux_64"
+  echo "  run casename.fds using FDS located in the repository at"
+  echo "  ~/FDS-SMV/FDS_Compilation/intel_linux_64/fds_intel_linux_64"
   echo "qfds.sh ~/bin/fds_linux_64 casename.fds"
-  echo "    run casename.fds using ~/bin/fds_linux_64"
-  echo "qfds.sh -r -t 8 -n 2 casename.fds"
+  echo "  run casename.fds using FDS located at ~/bin/fds_linux_64"
+  echo "qfds.sh -r -p 8 -n 2 casename.fds"
   echo "  run casename.fds using 8 processes, 2 processes per node (4 nodes)"
-  echo "  and ~/FDS-SMV/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64"
+  echo "  and FDS located in the repository at"
+  echo "  ~/FDS-SMV/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64"
   echo ""
   exit
 fi
+
+# default parameter settings
+
 queue=batch
-nthreads=1
+nprocesses=1
 use_repository=0
-nprocs=8
+nprocesses_per_node=8
 FDSROOT=~/FDS-SMV
 MPIRUN=
-ndefined=0
-while getopts 'f:n:q:rt:' OPTION
+nprocesses_per_node_defined=0
+
+# read in parameters from command line
+
+while getopts 'f:n:p:q:r' OPTION
 do
 case $OPTION  in
   f)
@@ -53,8 +62,11 @@ case $OPTION  in
    use_repository=1
    ;;
   n)
-   nprocs="$OPTARG"
-   ndefined=1
+   nprocesses_per_node="$OPTARG"
+   nprocesses_per_node_defined=1
+  ;;
+  p)
+   nprocesses="$OPTARG"
   ;;
   q)
    queue="$OPTARG"
@@ -62,26 +74,30 @@ case $OPTION  in
   r)
    use_repository=1
   ;;
-  t)
-   nthreads="$OPTARG"
-  ;;
 esac
 done
 shift $(($OPTIND-1))
 
+# set number of processes per node  to 4 if the fire60s queue is being used
+# (the fire60s only have 4 cores)
+
 if [ "$queue" == "fire60s" ]
 then
-  if [ $ndefined -eq 0 ]
-  then
-    nprocs=4
-  fi
+if test $nprocesses_per_node_defined -eq 0 
+then
+  nprocesses_per_node=4
 fi
+fi
+
 if [ $use_repository -eq 0 ]
 then
+#set fds and the input file using the command line
   fdsexe=$1
   in=$2
 else
- if [ $nthreads -gt 1 ]
+ if [ $nprocesses -gt 1 ]
+# only set the input file using the command line, the fds exe is defined
+# using the repository (serial if nprocesses==1 parallel otherwise)
   then
   fdsexe=$FDSROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
  else
@@ -89,22 +105,15 @@ else
  fi
  in=$1
 fi
-if [ $nthreads -gt 1 ]
+
+# if there is more than 1 process then use the mpirun command
+
+if [ $nprocesses -gt 1 ]
 then
-MPIRUN="mpirun -np $nthreads"
+MPIRUN="mpirun -np $nprocesses"
 fi
-echo MPIRUN=$MPIRUN
 
-echo fdsexe=$fdsexe
-echo input=$in
-echo queue=$queue
-echo nthreads=$nthreads
-echo nprocs=$nprocs
-echo use_repository=$use_repository
-
-
-nprocs=8
-nnodes=$(echo "($nthreads-1)/$nprocs+1" | bc)
+nnodes=$(echo "($nprocesses-1)/$nprocesses_per_node+1" | bc)
 if test $nnodes -le 0
 then
 nnodes=1
@@ -116,7 +125,8 @@ fulldir=`pwd`
 out=$fulldir/$infile.err
 outlog=$fulldir/$infile.log
 
-scriptfile=/tmp/script.$$
+# make sure files that are needed exist
+
 if ! [ -e $fulldir/$in ]; then
   echo "The FDS input file, $fulldir/$in, does not exist. Run aborted."
 fi
@@ -134,25 +144,33 @@ if ! [ -e $fdsexe ]; then
   exit
 fi
 
+scriptfile=/tmp/script.$$
 cat << EOF > $scriptfile
 #!/bin/bash -f
 #PBS -N $infile(MPI)
 #PBS -e $out
 #PBS -o $outlog
-#PBS -l nodes=$nnodes:ppn=$nprocs
+#PBS -l nodes=$nnodes:ppn=$nprocesses_per_node
 #\$ -N $infile(MPI)
 #\$ -e $out
 #\$ -o $outlog
-#\$ -l nodes=$nnodes:ppn=$nprocs
+#\$ -l nodes=$nnodes:ppn=$nprocesses_per_node
 
 cd $fulldir
 echo Start time: \`date\`
 echo Running $infile on \`hostname\`
 echo Directory: \`pwd\`
-
 $MPIRUN $fdsexe $in
 EOF
+echo "        Input file:$in"
+echo "        Executable:$fdsexe"
+echo "             Queue:$queue"
+echo "         Processes:$nprocesses"
+if test $nprocesses -gt 1
+then
+echo "             Nodes:$nnodes"
+echo "Processes per node:$nprocesses_per_node"
+fi
 chmod +x $scriptfile
-echo Running $in using $queue queue
-#qsub -q $queue $scriptfile
-#rm $scriptfile
+qsub -q $queue $scriptfile
+rm $scriptfile
