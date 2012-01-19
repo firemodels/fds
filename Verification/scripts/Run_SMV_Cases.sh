d236da08e42566c7c76fcc833b7d87cde85712f7
gforney <gforney@gmail.com>
2012-01-19 03:52:54 +0000

    VV scripts: add option to use fire70s batch queue when running verification suite
    Former-commit-id: f7117945bfffdfc14f6a3500f664a16e1f1dded0 [formerly 663947d53ea973f6d9e2ffd32aa9784bee797185]
    Former-commit-id: 657ba9fdf02562715db4a8c691ed0d184185b6b2
    Former-commit-id: d38fd207aac5bbec4ad411ee239e0d29fb66b9a8

--- Verification/scripts/Run_SMV_Cases.sh
+++ Verification/scripts/Run_SMV_Cases.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -f
+#/bin/bash -f
 
 # This script runs the FDS Verification Cases on a linux machine with
 # a batch queuing system
@@ -7,8 +7,13 @@ CURDIR=`pwd`$a
 cd ..
 export SVNROOT=`pwd`/..
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
-export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds.sh
+
+#export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+#export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds.sh
+# to run on fire60s comment above two lines and uncomment below two lines
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds6.sh
+export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds6.sh
+
 export BASEDIR=`pwd`
 # uncomment following line to stop all cases
 #export STOPFDS=1
