c07d10c0b9fb3d4d8b99af80e0fe45bdcf531ca1
mcgratta <mcgratta@gmail.com>
2012-02-09 17:27:58 +0000

    FDS Validation: Fix bad path name in NIST_RSE.m
    Former-commit-id: a8492acb7b985ac2ceaaa4594969d6ee5625c739 [formerly 7992dbf0df1856170212de89e8cd2ea2dbc542a1]
    Former-commit-id: ead105dd9a78b3bf03fdb08f650c30da0d6a5544
    Former-commit-id: 8e1bdaa27c53bacfd7b28a58554b560d17257afc

--- Utilities/Matlab/scripts/NIST_RSE.m
+++ Utilities/Matlab/scripts/NIST_RSE.m
@@ -11,7 +11,7 @@ close all;
 % and post processing scripts
 %------------------------------------------------
 
-addpath('/../../Validation/NIST_RSE_1994/FDS_Output_Files')
+addpath('../../Validation/NIST_RSE_1994/FDS_Output_Files')
 
 %------------------------------------------------
 % Read in FDS output files
