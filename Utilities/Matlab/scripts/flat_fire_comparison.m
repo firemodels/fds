4a71536c2f2114a25edce78d410c9083f49c5f7c
rmcdermo <randy.mcdermott@gmail.com>
2012-02-23 21:58:45 +0000

    Matlab: change flat_fire_comparison to use 4 byte reals to synch with currently committed flat_fire.fds
    Former-commit-id: 485a53ae56d431ff707936fe1b32a2d6faeba9fe [formerly 9076cf8e3674bcd533c80ed1ce53f87c4e6f002e]
    Former-commit-id: 1712b1285fd9cad21855a986d744f8ab466ba8ea
    Former-commit-id: 60ffbb6668fa162a51c6768fa51b9e774ecc953b

--- Utilities/Matlab/scripts/flat_fire_comparison.m
+++ Utilities/Matlab/scripts/flat_fire_comparison.m
@@ -32,7 +32,7 @@ vexact = g ./ (2 * K * V_0 * (K * V_0 * tvec + 1))...
 
 repository = '../../../Verification/Sprinklers_and_Sprays/';
 
-[STIME, XP, YP, ZP, QP] = read_prt5([repository, 'flat_fire.prt5'],'real*8');
+[STIME, XP, YP, ZP, QP] = read_prt5([repository, 'flat_fire.prt5'],'real*4');
 
 H(1) = plot(xexact, yexact, '-k');
 hold on
