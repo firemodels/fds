cae1703406e14352a4b8603340abbe1f213d6a01
Taylor Myers <tmacksmyers@gmail.com>
2012-02-24 16:02:51 +0000

    Training: DuPont Cases
    Former-commit-id: 3113375423a56a4e912babf0e8afab773d0c0267 [formerly 09f149b341fe0006ce836430563826db9767b3c5]
    Former-commit-id: dc8d411725687226c25840ac38187161ab69b0fd
    Former-commit-id: 6511d9a29d443b2a4e940cd99613919d34ba371f

---
+++ Training/DuPont/Run_All.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$RUNFDS $INDIR ASTM_E_648_01
+$RUNFDS $INDIR ASTM_E_648_02
+$RUNFDS $INDIR ASTM_E_648_03 
+$RUNFDS $INDIR ASTM_E_648_04 
+$RUNFDS $INDIR ASTM_E_648_05 
+
+echo FDS cases submitted
