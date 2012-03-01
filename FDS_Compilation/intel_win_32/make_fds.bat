77023d4bc33c2b0d9571a1904a82f6764006d4ef
gforney <gforney@gmail.com>
2012-03-01 01:42:30 +0000

    FDS build: add check to 32 bit win build scripts to prevent environment overflow
    Former-commit-id: 1f69029fe1531390adb40acdd819825b57fc5e7d [formerly 853735e538d6b9efc16937e7f22d22c62fcc789d]
    Former-commit-id: ae689bd623e39c8c95e2f6de7ea6c9d147d2a30c
    Former-commit-id: bead78e6890d03758c5e8c591ff373e5466f7b73

--- FDS_Compilation/intel_win_32/make_fds.bat
+++ FDS_Compilation/intel_win_32/make_fds.bat
@@ -1,6 +1,12 @@
 set intelbin="%IFORT_COMPILER12%\bin"
 
+IF "%SETUP_IFORT_COMPILER_32%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER_32=1
+
+echo Setting up compiler environment
 call %intelbin%\ifortvars ia32
 
+:envexist
 make VPATH="../../FDS_Source" -f ..\makefile intel_win_32
 pause
