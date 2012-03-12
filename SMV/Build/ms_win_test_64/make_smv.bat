28fdf20c02bdf65af0aa8ffbff13e506bef78a46
gforney <gforney@gmail.com>
2012-03-12 02:27:01 +0000

    smv build: add makefile entries for the visual studio C compiler

--- SMV/Build/ms_win_test_64/make_smv.bat
+++ SMV/Build/ms_win_test_64/make_smv.bat
@@ -7,7 +7,7 @@ IF "%SETUP_IFORT_COMPILER_IA32%"=="1" GOTO envexist
 set SETUP_IFORT_COMPILER_IA32=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER11%\bin\ifortvars" intel64
+call "%IFORT_COMPILER12%\bin\ifortvars" intel64
 if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
 :envexist
 make -j4 -f ..\Makefile ms_win_test_64
