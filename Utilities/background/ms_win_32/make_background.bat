8dec14011c1db2cc90e2ff1e7b288329dc86a54a
gforney <gforney@gmail.com>
2012-03-12 02:27:43 +0000

    background build: add makefile entries for the visual studio C compiler
    Former-commit-id: 273530c5373fa452fa7d3e7847a6877babec6a5c [formerly 6e6b6a37ec9b61fc4aef3307e2534d9c77ee0284]
    Former-commit-id: f03cb85b74301dec154acadf495857d6a38ff0f6
    Former-commit-id: 9cff5edfafc648b2af430c283b6fe13aebd36807

---
+++ Utilities/background/ms_win_32/make_background.bat
@@ -0,0 +1,18 @@
+@echo off
+
+Rem windows batch file to build background from the command line
+
+IF "%SETUP_IFORT_COMPILER12%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER12=1
+
+echo Setting up compiler environment
+Rem call "%IFORT_COMPILER12%\bin\ifortvars" ia32
+Rem call "%IFORT_COMPILER12%\bin\iclvars" ia32
+
+:envexist
+if exist "%VS_COMPILER%\..\vcvars32.bat" call "%VS_COMPILER%\..\vcvars32.bat"
+erase *.obj
+erase *.mod
+make -f ..\Makefile ms_win_32
+pause
