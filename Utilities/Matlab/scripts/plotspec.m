a7a89c2b5c40d12074455de7387651065e6bf416
rmcdermo <randy.mcdermott@gmail.com>
2012-03-19 13:21:30 +0000

    Matlab: mods to plotspec due to new CBC clock
    Former-commit-id: c8a0d9ccdfcbe3fe4b62115310d0f819ce0a41d2 [formerly 2a31649c6132685dba148374b9fa17b050531703]
    Former-commit-id: 708520df6cfc4fe7363fc808db04c084b699422c
    Former-commit-id: 3dbd9bb81cc0aaf85d9b7f67ed20163fcf5134f3

--- Utilities/Matlab/scripts/plotspec.m
+++ Utilities/Matlab/scripts/plotspec.m
@@ -26,9 +26,9 @@ L = 9*2*pi/100; % box length (m)
 k0 = 2*pi/L;
 kc = 1/2*N*k0;
 
-uvw_file1 = [chid,'_uvw_000.csv'];
-uvw_file2 = [chid,'_uvw_001.csv'];
-uvw_file3 = [chid,'_uvw_002.csv'];
+uvw_file1 = [chid,'_uvw_001.csv'];
+uvw_file2 = [chid,'_uvw_002.csv'];
+uvw_file3 = [chid,'_uvw_003.csv'];
 
 % Plot the FDS data
 H(1) = plotspec_uvw(uvw_file1,'k.-'); hold on
