e4a8c832287a768bedc1e6aea45e03b4d7cef54e
gforney <gforney@gmail.com>
2012-04-10 04:31:29 +0000

    FDS build: do not set up compiler environment if it has already been set up (for mpi_intel_win_64)
    Former-commit-id: 8bd97993746cff1a349f0f6adcb14d537879eb9c [formerly e0d3b450d7fdbdd3fa3cbd412e28ebea8f5ad806]
    Former-commit-id: c88437e1113522f58e2b41e96a4e5683dc008cf9
    Former-commit-id: c200794f507626dbea0c77667b646282c18ac531

--- FDS_Compilation/mpi_intel_win_64/make_fds.bat
+++ FDS_Compilation/mpi_intel_win_64/make_fds.bat
@@ -1,5 +1,10 @@
 set intelbin="%IFORT_COMPILER12%\bin"
 
+IF "%SETUP_IFORT_COMPILER12%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER12=1
+
+echo Setting up compiler environment
 call %intelbin%\ifortvars intel64
 
 make VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_64
