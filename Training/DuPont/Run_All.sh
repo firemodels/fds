7c0022698a4c9d811d92445ebbc2cfa2358e1d5b
Taylor Myers <tmacksmyers@gmail.com>
2012-04-26 16:36:00 +0000

    FDS Training: DuPont cases.

--- Training/DuPont/Run_All.sh
+++ Training/DuPont/Run_All.sh
@@ -2,18 +2,22 @@
 
 export SVNROOT=`pwd`/../..
 export FDS=~/FDS/FDS5/bin/fds5_linux_64
-#export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64_7031/fds5_mpi_intel_linux_64
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
+source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
 # export STOPFDS=1
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
+$RUNFDS $INDIR ASTM_E_648_22
+$RUNFDS $INDIR ASTM_E_648_23
+$RUNFDS $INDIR ASTM_E_648_24
 
-$RUNFDS $INDIR ASTM_E_648_20
-$RUNFDS $INDIR ASTM_E_648_21
+$RUNFDSMPI 16 $INDIR ASTM_E_648_25
 
 echo FDS cases submitted
