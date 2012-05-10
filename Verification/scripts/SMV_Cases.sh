8735b826c1e9ab94b52a87ec7357d9fb321447d0
gforney <gforney@gmail.com>
2012-05-10 01:06:21 +0000

    smv ug/tg: update manuals using new test cases

--- Verification/scripts/SMV_Cases.sh
+++ Verification/scripts/SMV_Cases.sh
@@ -6,8 +6,6 @@ $RUNFDS Visualization colorconv
 $RUNFDS Visualization objects_elem
 $RUNFDS Visualization objects_static
 $RUNFDS Visualization objects_dynamic
-$RUNFDS Visualization plume5a
-$RUNFDS Visualization plume5b
 $RUNFDS Visualization plume5c
 $RUNFDS Visualization plume5c_bounddef
 $RUNFDS Visualization sillytexture
