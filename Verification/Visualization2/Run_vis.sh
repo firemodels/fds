da2f01cd9d9efb98e279758639cf73647fdd04b2
gforney <gforney@gmail.com>
2012-05-10 01:40:53 +0000

    smv  vv: minor edit to runvis.sh script

--- Verification/Visualization2/Run_vis.sh
+++ Verification/Visualization2/Run_vis.sh
@@ -7,7 +7,7 @@ export SVNROOT=`pwd`/../..
 export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
-export INDIR=../Visualization
+export INDIR=../Visualization2
 source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
