9a3cd39dd463da0e5d8da8ca8af1bde0a6f1a0ed
gforney <gforney@gmail.com>
2012-05-19 00:42:09 +0000

    smv vv: update smokeview vv script

--- Verification/scripts/Run_SMV_Cases.sh
+++ Verification/scripts/Run_SMV_Cases.sh
@@ -6,13 +6,24 @@
 CURDIR=`pwd`$a
 cd ..
 export SVNROOT=`pwd`/..
-export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
 
+# for Linux (with queing)
+#export FDSEXE=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+
+# for OSX (without queuing)
+export BACKGROUND=$SVNROOT/Utilities/background/intel_osx_32/background
+export FDSEXE=$SVNROOT/FDS_Compilation/intel_osx_64/fds_intel_osx_64
+export FDS=$FDSEXE
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds_noq.sh
+
+# default queue
 #export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
 #export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds.sh
 # to run on fire60s comment above two lines and uncomment below two lines
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds6.sh
-export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds6.sh
+
+# fire60s queue
+#export RUNFDS=$SVNROOT/Utilities/Scripts/runfds6.sh
+#export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds6.sh
 
 export BASEDIR=`pwd`
 # uncomment following line to stop all cases
