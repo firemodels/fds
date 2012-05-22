72cfbc83ffa7a06252cef1889e05d56a86f76764
Susan Kilian <kiliansusan@gmail.com>
2012-05-22 19:19:45 +0000

    FDS Verification: temporarily comment out ScaRC MPI cases in FDS_MPI_Cases.sh
    Former-commit-id: 906a8ad5ca37877d2c6ab30f8b5f0e81de91abfc [formerly 04b58ada9957952b4ec560914a116ddaf956e88e]
    Former-commit-id: 32e83fdb80d6af552282ab62c9007df7bafca019
    Former-commit-id: 86218bfa81f2ac6ee0c52a5e8010b83cdb63f4f1

--- Verification/FDS_MPI_Cases.sh
+++ Verification/FDS_MPI_Cases.sh
@@ -3,7 +3,7 @@
 $RUNFDSMPI 5 Flowfields simple_duct
 $RUNFDSMPI 8 Pressure_Solver duct_flow
 $RUNFDSMPI 5 Pressure_Solver hallways
-$RUNFDSMPI 64 Pressure_Solver scarc2d_bicg_64mesh
-$RUNFDSMPI 64 Pressure_Solver scarc2d_cggmg_64mesh
-$RUNFDSMPI 64 Pressure_Solver scarc2d_bicggmg_64mesh
+#$RUNFDSMPI 64 Pressure_Solver scarc2d_bicg_64mesh
+#$RUNFDSMPI 64 Pressure_Solver scarc2d_cggmg_64mesh
+#$RUNFDSMPI 64 Pressure_Solver scarc2d_bicggmg_64mesh
 
