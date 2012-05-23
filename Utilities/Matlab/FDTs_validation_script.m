55a10a1166a52790479c15f830aa5a85c05a80cd
mcgratta <mcgratta@gmail.com>
2012-05-23 13:16:48 +0000

    FDTs: Add processing scripts.
    Former-commit-id: 4134f3560694882540649227ccb3d19d9eb46217 [formerly 623f027f5a05aab90d6fab3e1b243d58c69940c4]
    Former-commit-id: fd868a1d9814a016edcdd63981023d40a94e983e
    Former-commit-id: f03f35cd660e391431672eae65090e846537ac8a

---
+++ Utilities/Matlab/FDTs_validation_script.m
@@ -0,0 +1,38 @@
+% McGrattan
+% March 21, 2011
+% master_validation_script.m
+%
+% This script creates the plots that are included in the FDS Validation 
+% Guide. It consists of calls to other scripts contained within the
+% subfolder called "scripts". 
+%
+% The most important script is called dataplot. It reads the file called
+% validation_data_config_matlab.csv and generates 1000+ plots. If you
+% want to process only some of these plots, comment out the other 
+% scripts and change the data plot line as follows:
+%
+% [saved_data,drange] = dataplot(cfil,vdir,plotdir,[a:b]);
+%
+% where a and b are the lines in the .csv file you want to process.
+% Alternatively, you can specify the "Dataname" you want:
+%
+% [saved_data,drange] = dataplot(cfil,vdir,plotdir,'WTC');
+%
+% In this case, all the WTC results will be plotted. 
+
+close all
+clear all
+
+addpath 'scripts'
+
+% dataplot creates most of the plots for the Validation Guide. It must be run before scatplot, which makes the scatter plots.
+
+cfil = [pwd,'/FDTs_validation_dataplot_inputs.csv'];
+vdir = [pwd,'/../../Validation/'];
+plotdir = [pwd,'/../../Manuals/'];
+qfil = [pwd,'/FDTs_validation_scatterplot_inputs.csv'];
+
+[saved_data,drange] = dataplot(cfil,vdir,plotdir);
+scatplot(saved_data,drange,qfil)
+ 
+display('validation scripts completed successfully!')
