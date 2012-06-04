897a48be8f61082e380b6ddbe7cce2f51afd4288
Kristopher Overholt <koverholt@gmail.com>
2012-06-04 16:18:49 +0000

    FDTs: Add shell script for running all FDTs cases
    Former-commit-id: d80d8a6a7237295ac75ab848d0899f6a8e3f588d [formerly 8bbfd07347b0610702b65d7627302e82a018aecd]
    Former-commit-id: 9279dfc9765d76200c1ead02eeb28c5c50a66782
    Former-commit-id: f39d2df102e9a8a60245cb58398e87cc6329258c

---
+++ Validation/Run_FDTs_Cases.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -f
+
+# This script runs the FDTs cases on a linux machine with
+# a batch queuing system
+
+export SVNROOT=`pwd`/..
+export FDTs=$SVNROOT/Utilities/Data_Processing/FDTs
+export VDIR=$SVNROOT/Validation
+
+cd $VDIR/CAROLFIRE/FDTs
+$FDTs CAROLFIRE_THIEF_Inputs.txt
+
+cd $VDIR/Fleury_Heat_Flux/FDTs
+$FDTs Fleury_Heat_Flux_FDTs_Input.txt
+
+cd $VDIR/FM_SNL/FDTs
+$FDTs FM_SNL_FDTs_Input.txt
+
+cd $VDIR/LLNL_Enclosure/FDTs
+$FDTs LLNL_FDTs_Input.txt
+
+cd $VDIR/NIST_NRC/FDTs
+$FDTs NIST_NRC_FDTs_Input.txt
+
+cd $VDIR/Steckler_Compartment/FDTs
+$FDTs Steckler_MQH_Inputs.txt
+
+cd $VDIR/UL_NFPRF/FDTs
+$FDTs UL_NFPRF_FDTs_Input.txt
+
+cd $VDIR/Vettori_Flat_Ceiling/FDTs
+$FDTs Vettori_FDTs_Input.txt
+
+echo FDTs cases complete
