3d39052e8a8d149f09615c4f99a0caa7c4027635
rmcdermo <randy.mcdermott@gmail.com>
2012-06-07 16:22:53 +0000

    FDS Validation: add Wind directory
    Former-commit-id: 3bdefe9627161e9008cdd2239b9a8e6fdf2138b6 [formerly 961f41245cb68ac969d142b9a9ea6998fc8e10ae]
    Former-commit-id: 243cf51d7f1a9af4b2f44f19b44c371fbf771bd8
    Former-commit-id: 76e717fd9c780154c4b0df58c1cb5dbbe71d5b08

---
+++ Validation/Wind/Run_All.sh
@@ -0,0 +1,20 @@
+#/bin/bash -f
+
+# This script runs the a set of Validation Cases on a linux machine with
+# a batch queuing system
+
+export SVNROOT=`pwd`/../..
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+source ~/.bashrc_fds intel64
+
+# uncomment following line to stop all cases
+#export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$RUNFDSMPI 27 $INDIR wind_cube
+
+echo FDS cases submitted
