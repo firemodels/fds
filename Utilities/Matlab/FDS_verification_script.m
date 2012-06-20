a2ffdb23bbca49c2af066de98cb95b3c29251d50
Max Gould <max.gould@gmail.com>
2012-06-20 17:45:59 +0000

    FDS Matlab Utilities - Added Periodic Test 6 to verification script

--- Utilities/Matlab/FDS_verification_script.m
+++ Utilities/Matlab/FDS_verification_script.m
@@ -27,6 +27,7 @@ addpath 'scripts'
 radiation_box;            display('radiation_box complete...')
 radiation_plane_layer;    display('radiation_plane_layer complete...')
 ns2d;                     display('ns2d complete...')
+vort2d;                   display('vort2d complete...')
 wall_internal_radiation;  display('wall_internal_radiation complete...')
 ashrae_7;                 display('ashrae_7 complete...')
 flame_species;            display('flame_species complete...')
