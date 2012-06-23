4c5f32849703d2248f3e217e613b3c3f08e1ae18
gforney <gforney@gmail.com>
2012-06-23 21:19:41 +0000

    bundle scripts: update bundle scripts

--- Utilities/Scripts/BUNDLE_linux64.bat
+++ Utilities/Scripts/BUNDLE_linux64.bat
@@ -19,7 +19,7 @@ Rem location of batch files used to set up Intel compilation environment
 call %envfile%
 
 set bundledir=FDS_%fds_version%-SMV_%smv_version%_linux64
-plink %svn_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_linux64.csh %linux_svn_root% %bundledir% %linux_hostname% %fds_edition%
+plink %svn_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_linux64.csh %linux_svn_root% %bundledir% %linux_hostname% %fds_edition%  %fds_version% %smv_version%
 
 set manifest=%svn_root%\Utilities\to_google\manifest_linux_64.html
 echo Downloading manifest
