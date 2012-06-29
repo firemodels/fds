699c9bdc4dfa41834262598472a5e9308318a740
gforney <gforney@gmail.com>
2012-06-29 18:00:59 +0000

    vv scripts: edit run_fds verification script to give options to run on other cluster nodes (fire70s or firevis/smokevis)

--- Verification/Run_FDS_Cases.sh
+++ Verification/Run_FDS_Cases.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -f
+#!/bin/bash
 
 # This script runs the FDS Verification Cases on a linux machine with
 # a batch queuing system
@@ -7,11 +7,21 @@ export SVNROOT=`pwd`/..
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
 export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 
+# VVVVVVVVVVVVVVVVV select which group of nodes to run on VVVVVVVV
+
+# new cluster
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
 export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
-# to run on fire70s comment two lines above and uncomment two lines below
+
+# fire70s
 #export RUNFDS=$SVNROOT/Utilities/Scripts/runfds7.sh
 #export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi7.sh
+
+# firevis/smokevis
+#export RUNFDS=$SVNROOT/Utilities/Scripts/runfdsv.sh
+#export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpiv.sh
+
+# ^^^^^^^^^^^^^^^^^ select nodes to run on ^^^^^^^^^^^^^^^^^^^^^^
 
 export BASEDIR=`pwd`
 
