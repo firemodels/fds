409f8ca3c733d95b93baedabff19b89c4a67c789
rmcdermo <randy.mcdermott@gmail.com>
2012-07-04 19:47:59 +0000

    Matlab scripts: add yplus.m
    Former-commit-id: 658d2738b96f06a040aa641beeca09846f41408e [formerly f8d7388fd942ad0edbdd558f05227addea567ed4]
    Former-commit-id: b72da842604c784ae711b65d6a84865a3cc885e1
    Former-commit-id: 3dc3a4fb7cfea3a344df881442de5cbc995f7371

---
+++ Utilities/Matlab/scripts/yplus.m
@@ -0,0 +1,59 @@
+% Abbas
+% 7-3-12
+% yplus.m
+
+close all
+clear all
+
+dir = '../../../Verification/Turbulence/';
+M_8  = importdata([dir,'yplus_8_devc.csv'],',',2);
+M_16 = importdata([dir,'yplus_16_devc.csv'],',',2);
+M_32 = importdata([dir,'yplus_32_devc.csv'],',',2);
+
+yp = [M_8.data(end,2),M_16.data(end,2),M_32.data(end,2)];
+
+n = [8 16 32];
+y = 0.5*(1./n);
+mu = 1;
+rho = 1.199;
+tau_w = 0.5;
+u_tau = sqrt(tau_w/rho);
+d_nu = (mu/rho)/u_tau;
+yp_exact = y/d_nu;
+
+plot(yp_exact,yp_exact,'ro'); hold on
+plot(yp_exact,yp,'b--')
+
+plot_style
+set(gca,'Units',Plot_Units)
+set(gca,'FontName',Font_Name)
+set(gca,'FontSize',Title_Font_Size)
+set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
+set(gcf,'DefaultLineLineWidth',Line_Width)
+
+xlabel('$y^+$ specified','Interpreter',Font_Interpreter,'Fontname',Font_Name),
+ylabel('$y^+$ predicted','Interpreter',Font_Interpreter,'Fontname',Font_Name),
+leg = legend('exact','FDS');
+set(leg,'Location','SouthEast')
+set(leg,'Interpreter',Font_Interpreter)
+set(leg,'Fontname',Font_Name)
+
+% add SVN if file is available
+
+SVN_Filename = [dir,'yplus_8_svn.txt'];
+if exist(SVN_Filename,'file')
+    SVN = importdata(SVN_Filename);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
+
+% print pdf
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf','../../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/yplus')
+
