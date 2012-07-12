52bb5ed52a6fa3401dd1791871154b757068807b
gforney <gforney@gmail.com>
2012-07-12 20:41:23 +0000

    vv scripts: change vv scripts to assume that sh2bat.exe is installed from a bundle rather than in the repository
    Former-commit-id: 4a8e1409975b3f85008043a05a130064fdb421a4 [formerly c29caf608e7a2c1cbe47cb9e193e93e903a76553]
    Former-commit-id: 0d62796706f141ddee44288384b24db80dabd4a2
    Former-commit-id: d470fea70046629fecebb036a9f53a0be0632f8a

--- Verification/scripts/Make_SMV_pictures.bat
+++ Verification/scripts/Make_SMV_pictures.bat
@@ -18,7 +18,7 @@ set smvug="%SVNROOT%\Manuals\SMV_User_Guide"
 set smvvg="%SVNROOT%\Manuals\SMV_Verification_Guide"
 set RUNFDS=call "%SCRIPT_DIR%\runsmv.bat"
 set RUNCFAST=call "%SCRIPT_DIR%\runsmv.bat"
-set SH2BAT="%SVNROOT%\Utilities\Data_Processing\sh2bat"
+set SH2BAT="sh2bat"
 
 cd %smvug%
 
