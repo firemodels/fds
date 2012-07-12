6eb19cfcc8b7f9845ca72d2a8e8096e6b85a350a
gforney <gforney@gmail.com>
2012-07-12 14:22:44 +0000

    fds build: add a windows 64 bit dv makefile entry and directory

---
+++ FDS_Compilation/intel_win_64_dv/make_fds.bat
@@ -0,0 +1,12 @@
+set intelbin="%IFORT_COMPILER12%\bin"
+
+IF "%SETUP_IFORT_COMPILER12%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER12=1
+
+echo Setting up compiler environment
+call %intelbin%\ifortvars intel64
+
+:envexist
+make VPATH="../../FDS_Source" -f ..\makefile intel_win_64_dv
+pause
