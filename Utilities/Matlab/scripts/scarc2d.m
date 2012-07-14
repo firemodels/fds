e9477a497ebcf1a6e187e1c8898e59f0f1ecc226
Kristopher Overholt <koverholt@gmail.com>
2012-07-14 18:15:00 +0000

    Matlab: Update scarc2d.m for reduced 4x4 scarc2d cases
    Former-commit-id: b8610f5cf6a56a562ce67016d89eedf0a06bd955 [formerly 471f5f6b0dee0b023366ada857adb6f0c0afc154]
    Former-commit-id: 07de20be48bb7357f2c231d7567a5196a5aef1f6
    Former-commit-id: edf7487590bbbe895c362c6393c410dad18704df

--- Utilities/Matlab/scripts/scarc2d.m
+++ Utilities/Matlab/scripts/scarc2d.m
@@ -17,27 +17,27 @@ FFT_1MESH  = csvread([data_dir,'scarc2d_fft_1mesh_devc.csv'],2,0);
 FFT_64MESH = csvread([data_dir,'scarc2d_fft_64mesh_devc.csv'],2,0);
 
 SCARC_CG_64MESH    = csvread([data_dir,'scarc2d_cg_64mesh_devc.csv'],2,0);
-SCARC_BICG_64MESH  = csvread([data_dir,'scarc2d_bicg_64mesh_devc.csv'],2,0);
+SCARC_BICG_16MESH  = csvread([data_dir,'scarc2d_bicg_16mesh_devc.csv'],2,0);
 SCARC_GMG_64MESH   = csvread([data_dir,'scarc2d_gmg_64mesh_devc.csv'],2,0);
 
 time_fft_1mesh  = FFT_1MESH(:,1);  pres_fft_1mesh  = FFT_1MESH(:,5);
 time_fft_64mesh = FFT_64MESH(:,1); pres_fft_64mesh = FFT_64MESH(:,5);
 
 time_scarc_cg_64mesh    = SCARC_CG_64MESH(:,1)  ; pres_scarc_cg_64mesh   = SCARC_CG_64MESH(:,5);
-time_scarc_bicg_64mesh  = SCARC_BICG_64MESH(:,1); pres_scarc_bicg_64mesh = SCARC_BICG_64MESH(:,5);
+time_scarc_bicg_16mesh  = SCARC_BICG_16MESH(:,1); pres_scarc_bicg_16mesh = SCARC_BICG_16MESH(:,5);
 time_scarc_gmg_64mesh   = SCARC_GMG_64MESH(:,1) ; pres_scarc_gmg_64mesh  = SCARC_GMG_64MESH(:,5);
 
 H(1)=plot(time_fft_1mesh ,pres_fft_1mesh ,'k-','LineWidth',Line_Width); hold on
 H(2)=plot(time_fft_64mesh,pres_fft_64mesh,'k--','LineWidth',Line_Width); hold on
 
 H(3)=plot(time_scarc_cg_64mesh(1:10:end) ,pres_scarc_cg_64mesh(1:10:end)   ,'ro','LineWidth',Line_Width); hold on
-H(4)=plot(time_scarc_bicg_64mesh(1:10:end),pres_scarc_bicg_64mesh(1:10:end) ,'g^','LineWidth',Line_Width); hold on
+H(4)=plot(time_scarc_bicg_16mesh(1:10:end),pres_scarc_bicg_16mesh(1:10:end) ,'g^','LineWidth',Line_Width); hold on
 H(5)=plot(time_scarc_gmg_64mesh(1:10:end) ,pres_scarc_gmg_64mesh(1:10:end)  ,'b+','LineWidth',Line_Width); hold on
 
 xlabel('Time (s)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 ylabel('Pressure (Pa)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 axis([0 1 -2 2])
-legend_handle=legend(H,'FFT 1 mesh','FFT 64 meshes','ScaRC-CG 64 meshes','ScaRC-BICG 64 meshes','ScaRC-GMG 64 meshes','Location','EastOutside');
+legend_handle=legend(H,'FFT 1 mesh','FFT 64 meshes','ScaRC-CG 64 meshes','ScaRC-BICG 16 meshes','ScaRC-GMG 64 meshes','Location','EastOutside');
 legend boxoff
 set(legend_handle,'FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 set(legend_handle,'Position',[6.3 1.75 3.7 1.5])
