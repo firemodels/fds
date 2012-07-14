948dc4158040247a517de5b78f750f1b8e03e6e1
Kristopher Overholt <koverholt@gmail.com>
2012-07-14 18:46:47 +0000

    FDS Verification: Convert all scarc2d cases to 8 meshes (4x2), update Matlab script, update Run_FDS scripts

--- Utilities/Matlab/scripts/scarc2d.m
+++ Utilities/Matlab/scripts/scarc2d.m
@@ -14,37 +14,37 @@ data_dir = '../../../Verification/Pressure_Solver/';
 plot_dir = '../../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/';
 
 FFT_1MESH  = csvread([data_dir,'scarc2d_fft_1mesh_devc.csv'],2,0);
-FFT_64MESH = csvread([data_dir,'scarc2d_fft_64mesh_devc.csv'],2,0);
+FFT_8MESH = csvread([data_dir,'scarc2d_fft_8mesh_devc.csv'],2,0);
 
-SCARC_CG_64MESH    = csvread([data_dir,'scarc2d_cg_64mesh_devc.csv'],2,0);
-SCARC_BICG_16MESH  = csvread([data_dir,'scarc2d_bicg_16mesh_devc.csv'],2,0);
-SCARC_GMG_64MESH   = csvread([data_dir,'scarc2d_gmg_64mesh_devc.csv'],2,0);
+SCARC_CG_8MESH    = csvread([data_dir,'scarc2d_cg_8mesh_devc.csv'],2,0);
+SCARC_BICG_8MESH  = csvread([data_dir,'scarc2d_bicg_8mesh_devc.csv'],2,0);
+SCARC_GMG_8MESH   = csvread([data_dir,'scarc2d_gmg_8mesh_devc.csv'],2,0);
 
 time_fft_1mesh  = FFT_1MESH(:,1);  pres_fft_1mesh  = FFT_1MESH(:,5);
-time_fft_64mesh = FFT_64MESH(:,1); pres_fft_64mesh = FFT_64MESH(:,5);
+time_fft_8mesh = FFT_8MESH(:,1); pres_fft_8mesh = FFT_8MESH(:,5);
 
-time_scarc_cg_64mesh    = SCARC_CG_64MESH(:,1)  ; pres_scarc_cg_64mesh   = SCARC_CG_64MESH(:,5);
-time_scarc_bicg_16mesh  = SCARC_BICG_16MESH(:,1); pres_scarc_bicg_16mesh = SCARC_BICG_16MESH(:,5);
-time_scarc_gmg_64mesh   = SCARC_GMG_64MESH(:,1) ; pres_scarc_gmg_64mesh  = SCARC_GMG_64MESH(:,5);
+time_scarc_cg_8mesh    = SCARC_CG_8MESH(:,1)  ; pres_scarc_cg_8mesh   = SCARC_CG_8MESH(:,5);
+time_scarc_bicg_8mesh  = SCARC_BICG_8MESH(:,1); pres_scarc_bicg_8mesh = SCARC_BICG_8MESH(:,5);
+time_scarc_gmg_8mesh   = SCARC_GMG_8MESH(:,1) ; pres_scarc_gmg_8mesh  = SCARC_GMG_8MESH(:,5);
 
 H(1)=plot(time_fft_1mesh ,pres_fft_1mesh ,'k-','LineWidth',Line_Width); hold on
-H(2)=plot(time_fft_64mesh,pres_fft_64mesh,'k--','LineWidth',Line_Width); hold on
+H(2)=plot(time_fft_8mesh,pres_fft_8mesh,'k--','LineWidth',Line_Width); hold on
 
-H(3)=plot(time_scarc_cg_64mesh(1:10:end) ,pres_scarc_cg_64mesh(1:10:end)   ,'ro','LineWidth',Line_Width); hold on
-H(4)=plot(time_scarc_bicg_16mesh(1:10:end),pres_scarc_bicg_16mesh(1:10:end) ,'g^','LineWidth',Line_Width); hold on
-H(5)=plot(time_scarc_gmg_64mesh(1:10:end) ,pres_scarc_gmg_64mesh(1:10:end)  ,'b+','LineWidth',Line_Width); hold on
+H(3)=plot(time_scarc_cg_8mesh(1:10:end) ,pres_scarc_cg_8mesh(1:10:end)   ,'ro','LineWidth',Line_Width); hold on
+H(4)=plot(time_scarc_bicg_8mesh(1:10:end),pres_scarc_bicg_8mesh(1:10:end) ,'g^','LineWidth',Line_Width); hold on
+H(5)=plot(time_scarc_gmg_8mesh(1:10:end) ,pres_scarc_gmg_8mesh(1:10:end)  ,'b+','LineWidth',Line_Width); hold on
 
 xlabel('Time (s)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 ylabel('Pressure (Pa)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 axis([0 1 -2 2])
-legend_handle=legend(H,'FFT 1 mesh','FFT 64 meshes','ScaRC-CG 64 meshes','ScaRC-BICG 16 meshes','ScaRC-GMG 64 meshes','Location','EastOutside');
+legend_handle=legend(H,'FFT 1 mesh','FFT 8 meshes','ScaRC-CG 8 meshes','ScaRC-BICG 8 meshes','ScaRC-GMG 8 meshes','Location','EastOutside');
 legend boxoff
 set(legend_handle,'FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 set(legend_handle,'Position',[6.3 1.75 3.7 1.5])
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Title_Font_Size)
 
-SVN_Filename = [data_dir,'scarc2d_cg_64mesh_svn.txt'];
+SVN_Filename = [data_dir,'scarc2d_cg_8mesh_svn.txt'];
 if exist(SVN_Filename,'file')
     SVN = importdata(SVN_Filename);
     x_lim = get(gca,'XLim');
--- Verification/FDS_MPI_Cases.sh
+++ Verification/FDS_MPI_Cases.sh
@@ -3,7 +3,7 @@
 $RUNFDSMPI 5 Flowfields simple_duct
 $RUNFDSMPI 8 Pressure_Solver duct_flow
 $RUNFDSMPI 5 Pressure_Solver hallways
-$RUNFDSMPI 16 Pressure_Solver scarc2d_bicg_16mesh
-$RUNFDSMPI 16 Pressure_Solver scarc2d_cggmg_16mesh
-$RUNFDSMPI 16 Pressure_Solver scarc2d_bicggmg_16mesh
+$RUNFDSMPI 8 Pressure_Solver scarc2d_bicg_8mesh
+$RUNFDSMPI 8 Pressure_Solver scarc2d_cggmg_8mesh
+$RUNFDSMPI 8 Pressure_Solver scarc2d_bicggmg_8mesh
 
