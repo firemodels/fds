e397aeac1801edb0bc6a1d4ecd9050ea28850027
Kristopher Overholt <koverholt@gmail.com>
2012-07-18 20:18:32 +0000

    FDS Verification: Add optional argument (-q queue_name) to Run_FDS_Cases.sh and Run_SMV_Cases.sh to specify which queue to run verification cases on
    Former-commit-id: f4947a1ad145f3caa2c929075f98885ae20b4fd3 [formerly b25a887c49bf30013c863ea49a981c78cf7996f1]
    Former-commit-id: 8a17ad49e0f3b96185b54c593d2c4eff3dd3dec6
    Former-commit-id: 0054f40764efab4d78eb5f5beb693ee718fd4822

--- Verification/Run_FDS_Cases.sh
+++ Verification/Run_FDS_Cases.sh
@@ -11,29 +11,49 @@ export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
 export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 
 # Otherwise, if -d (debug) option is specified, then run FDS DB version.
-while getopts 'd' OPTION
+while getopts 'dq:' OPTION
 do
 case $OPTION in
   d)
    export FDS=$SVNROOT/FDS_Compilation/intel_linux_64_db/fds_intel_linux_64_db
    export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64_db/fds_mpi_intel_linux_64_db
    ;;
+  q)
+   queue="$OPTARG"
+   ;;
 esac
 done
 
 # VVVVVVVVVVVVVVVVV select which group of nodes to run on VVVVVVVV
 
-# new cluster
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
-export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+# Set queue to submit cases to
+# If no argument is specfied, then run cases on the default queue (batch).
 
-# fire70s
-#export RUNFDS=$SVNROOT/Utilities/Scripts/runfds7.sh
-#export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi7.sh
+   # blaze queue (default)
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+   export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 
-# firevis/smokevis
-#export RUNFDS=$SVNROOT/Utilities/Scripts/runfdsv.sh
-#export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpiv.sh
+# Otherwise, if -q (queue) option is specified, then run cases on the specified queue.
+if [ "$queue" == "fire60s" ]
+then
+   # fire60s queue
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfds6.sh
+   export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi6.sh
+fi
+
+if [ "$queue" == "fire70s" ]
+then
+   # fire70s queue
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfds7.sh
+   export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi7.sh
+fi
+
+if [ "$queue" == "vis" ]
+then
+   # firevis/smokevis queue
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfdsv.sh
+   export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpiv.sh
+fi
 
 # ^^^^^^^^^^^^^^^^^ select nodes to run on ^^^^^^^^^^^^^^^^^^^^^^
 
--- Verification/scripts/Run_SMV_Cases.sh
+++ Verification/scripts/Run_SMV_Cases.sh
@@ -21,34 +21,50 @@ export FDS=$FDSEXE
 export CFAST=~/cfast/CFAST/intel_linux_64/cfast6_linux_64
 
 # Otherwise, if -d (debug) option is specified, then run FDS DB version.
-while getopts 'd' OPTION
+while getopts 'dq:' OPTION
 do
 case $OPTION in
   d)
    export FDSEXE=$SVNROOT/FDS_Compilation/intel_linux_64_db/fds_intel_linux_64_db
    export FDS=$FDSEXE
    ;;
+  q)
+   queue="$OPTARG"
+   ;;
 esac
 done
 
+# Set queue to submit cases to
+# If no argument is specfied, then run cases on the default queue (batch).
 # blaze queue (default)
-export RUNCFAST=$SVNROOT/Utilities/Scripts/runcfast.sh
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
-export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds.sh
+   export RUNCFAST=$SVNROOT/Utilities/Scripts/runcfast.sh
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+   export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds.sh
 
-# fire60s queue
-#export RUNFDS=$SVNROOT/Utilities/Scripts/runfds6.sh
-#export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds6.sh
+# Otherwise, if -q (queue) option is specified, then run cases on the specified queue.
+if [ "$queue" == "fire60s" ]
+then
+   # fire60s queue
+   export RUNCFAST=$SVNROOT/Utilities/Scripts/runcfast6.sh
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfds6.sh
+   export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds6.sh
+fi
 
-# fire70s queue
-#export RUNCFAST=$SVNROOT/Utilities/Scripts/runcfast7.sh
-#export RUNFDS=$SVNROOT/Utilities/Scripts/runfds7.sh
-#export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds7.sh
+if [ "$queue" == "fire70s" ]
+then
+   # fire70s queue
+   export RUNCFAST=$SVNROOT/Utilities/Scripts/runcfast7.sh
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfds7.sh
+   export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfds7.sh
+fi
 
-# vis queue
-#export RUNCFAST=$SVNROOT/Utilities/Scripts/runcfastv.sh
-#export RUNFDS=$SVNROOT/Utilities/Scripts/runfdsv.sh
-#export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfdsv.sh
+if [ "$queue" == "vis" ]
+then
+   # vis queue
+   export RUNCFAST=$SVNROOT/Utilities/Scripts/runcfastv.sh
+   export RUNFDS=$SVNROOT/Utilities/Scripts/runfdsv.sh
+   export RUNFDSFG=$SVNROOT/Utilities/Scripts/runfdsv.sh
+fi
 
 export BASEDIR=`pwd`
 # uncomment following line to stop all cases
