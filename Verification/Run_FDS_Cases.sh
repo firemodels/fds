1cbe1124d01c10eb3cc3e78bbc8e6322a0c24a22
Kristopher Overholt <koverholt@gmail.com>
2012-07-19 13:53:51 +0000

    VV Scripts: Add options to select set of cases (all, serial, or mpi) and to stop FDS cases

--- Verification/Run_FDS_Cases.sh
+++ Verification/Run_FDS_Cases.sh
@@ -4,17 +4,22 @@
 # a batch queuing system
 
 queue=batch
+cases=all
 
 function usage {
-echo "Run_FDS_Cases.sh [-d -h -q queue_name ]"
+echo "Run_FDS_Cases.sh [ -c cases -d -h -q queue_name -s ]"
 echo "Runs FDS verification suite"
 echo ""
 echo "Options"
+echo "-c cases - select set of cases to run"
+echo "     default: $cases"
+echo "     other options: serial, mpi"
 echo "-d - use debug version of FDS"
 echo "-h - display this message"
 echo "-q queue_name - run cases using the queue queue_name"
 echo "     default: $queue"
 echo "     other options: fire60s, fire70s, vis"
+echo "-s - stop FDS runs"
 exit
 }
 
@@ -26,9 +31,12 @@ export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
 export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 
 # Otherwise, if -d (debug) option is specified, then run FDS DB version.
-while getopts 'dhq:' OPTION
+while getopts 'c:dhq:s' OPTION
 do
 case $OPTION in
+  c)
+   cases="$OPTARG"
+   ;;
   d)
    export FDS=$SVNROOT/FDS_Compilation/intel_linux_64_db/fds_intel_linux_64_db
    export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64_db/fds_mpi_intel_linux_64_db
@@ -39,6 +47,9 @@ case $OPTION in
   q)
    queue="$OPTARG"
    ;;
+  s)
+   export STOPFDS=1
+   ;;   
 esac
 done
 
@@ -47,11 +58,19 @@ export RUNFDSMPI="$SVNROOT/Utilities/Scripts/runfdsmpi.sh -q $queue"
 
 export BASEDIR=`pwd`
 
-# uncomment following line to stop all cases
-# export STOPFDS=1
-
-./FDS_Cases.sh
-./FDS_MPI_Cases.sh
+# Run appropriate set of cases depending on user specified set (-c option)
+case "$cases" in
+  all)
+    ./FDS_Cases.sh
+    ./FDS_MPI_Cases.sh
+    ;;
+  serial)
+    ./FDS_Cases.sh
+    ;;
+  mpi)
+    ./FDS_MPI_Cases.sh
+    ;;
+esac
 
 echo FDS cases submitted
 
