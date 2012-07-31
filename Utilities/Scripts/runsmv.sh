491fa3a6e873040cda036869bfc252c27263c630
gforney <gforney@gmail.com>
2012-07-31 13:26:09 +0000

    vv scripts: make changes to allow runsmv.sh to run on the Mac
    Former-commit-id: 4e803e5b1dd9398ea5b6b1dcafafa9ffd934a9d6 [formerly c9d74cd435dc6d5d7e4cbbfa17a57c756970bf30]
    Former-commit-id: a273631d7a5b7db98c8d5076d1434743c18e9407
    Former-commit-id: f45908f6c588f0ee38be6f9ba350833362439db0

--- Utilities/Scripts/runsmv.sh
+++ Utilities/Scripts/runsmv.sh
@@ -1,4 +1,28 @@
 #!/bin/bash -f
+
+function smv_linux {
+disp=`id -u`
+Xvfb :$disp -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
+rxs=$!
+echo pausing for 8 seconds
+sleep 8
+
+export DISPLAY=:$disp
+source ~/.bashrc_fds intel64
+cd $fulldir
+$SMV $SMVBINDIR -runscript $in
+echo pausing for 8 seconds
+sleep 8
+
+kill $rxs
+}
+
+function smv_osx {
+source ~/.bashrc_fds intel64
+cd $fulldir
+$SMV $SMVBINDIR -runscript $in
+}
+
 dir=$1
 in=$2
 
@@ -18,17 +42,15 @@ if ! [ -e $fulldir/$in.smv ]; then
   exit
 fi
 
-disp=`id -u`
-Xvfb :$disp -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
-rxs=$!
-echo pausing for 8 seconds
-sleep 8
+OS=`uname`
+if [ "$OS" == "Darwin" ]; then
+  OSSYSTEM=osx
+else
+  OSSYSTEM=linux
+fi
 
-export DISPLAY=:$disp
-source ~/.bashrc_fds intel64
-cd $fulldir
-$SMV $SMVBINDIR -runscript $in
-echo pausing for 8 seconds
-sleep 8
-
-kill $rxs
+if [ "$OSSYSTEM" == "linux" ]; then
+  smv_linux;
+else
+  smv_osx;
+fi
