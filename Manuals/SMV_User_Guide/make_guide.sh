60729480f61cb0f2c7dca203f53dc48755c36500
Kristopher Overholt <koverholt@gmail.com>
2012-08-02 23:57:20 +0000

    SMV Manuals: Add ./make_guide.sh and .bat scripts to compile guides and check for errors/warnings
    Former-commit-id: dc6a25ad3479d0787ea559c03b99ca88de30523b [formerly 91daf285fdf05ca7f7b0ecfd9a163962806ef0f4]
    Former-commit-id: a51e1981c9c5fe203307fb4cb3d5d002886c2fda
    Former-commit-id: 586b6b87a5c45d53780c4e19e1a75d293e120989

---
+++ Manuals/SMV_User_Guide/make_guide.sh
@@ -0,0 +1,36 @@
+clean_build=1
+
+# Build SMV User Guide
+pdflatex -interaction nonstopmode SMV_User_Guide &> SMV_User_Guide.err
+bibtex SMV_User_Guide &> SMV_User_Guide.err
+pdflatex -interaction nonstopmode SMV_User_Guide &> SMV_User_Guide.err
+pdflatex -interaction nonstopmode SMV_User_Guide &> SMV_User_Guide.err
+
+# Scan and report any errors in the LaTeX build process
+if [[ `grep "! LaTeX Error:" -I SMV_User_Guide.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX errors detected:"
+      grep "! LaTeX Error:" -I SMV_User_Guide.err
+      clean_build=0
+fi
+
+# Check for LaTeX warnings (undefined references or duplicate labels)
+if [[ `grep -E "undefined|multiply defined|multiply-defined" -I SMV_User_Guide.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX warnings detected:"
+      grep -E "undefined|multiply defined|multiply-defined" -I SMV_User_Guide.err
+      clean_build=0
+fi
+
+if [[ $clean_build == 0 ]]
+   then
+      :
+   else
+      echo "SMV User Guide built successfully!"
+fi    
