86dee2a59a543552d858ca1c34714ba02f10f954
Kristopher Overholt <koverholt@gmail.com>
2012-08-02 15:24:56 +0000

    FDS Manuals: Add ./make_guide.sh scripts to compile guides and check for errors/warnings

---
+++ Manuals/FDS_Validation_Guide/make_guide.sh
@@ -0,0 +1,36 @@
+clean_build=1
+
+# Build FDS Validation Guide
+pdflatex -interaction nonstopmode FDS_Validation_Guide &> FDS_Validation_Guide.err
+bibtex FDS_Validation_Guide &> FDS_Validation_Guide.err
+pdflatex -interaction nonstopmode FDS_Validation_Guide &> FDS_Validation_Guide.err
+pdflatex -interaction nonstopmode FDS_Validation_Guide &> FDS_Validation_Guide.err
+
+# Scan and report any errors in the LaTeX build process
+if [[ `grep "! LaTeX Error:" -I FDS_Validation_Guide.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX errors detected:"
+      grep "! LaTeX Error:" -I FDS_Validation_Guide.err
+      clean_build=0
+fi
+
+# Check for LaTeX warnings (undefined references or duplicate labels)
+if [[ `grep -E "undefined|multiply defined|multiply-defined" -I FDS_Validation_Guide.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX warnings detected:"
+      grep -E "undefined|multiply defined|multiply-defined" -I FDS_Validation_Guide.err
+      clean_build=0
+fi
+
+if [[ $clean_build == 0 ]]
+   then
+      :
+   else
+      echo "FDS Validation Guide built successfully!"
+fi    
