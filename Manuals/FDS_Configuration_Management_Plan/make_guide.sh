619ccec285c361fea14fddd7ffe06c737b1a19cc
Kristopher Overholt <koverholt@gmail.com>
2012-08-02 15:24:56 +0000

    FDS Manuals: Add ./make_guide.sh scripts to compile guides and check for errors/warnings
    Former-commit-id: b404f61113e8075ab6ca44381d8f95233d680aff [formerly d7ffd0713cb7c3c243b5c48e5baf24032e47c386]
    Former-commit-id: 53ab0a0fb096979457c9eb539f8120c42ce663a5
    Former-commit-id: 6d2e68fa7483cd352074584b19f4aac557dd7c75

---
+++ Manuals/FDS_Configuration_Management_Plan/make_guide.sh
@@ -0,0 +1,36 @@
+clean_build=1
+
+# Build FDS Configuration Management Plan
+pdflatex -interaction nonstopmode FDS_Configuration_Management_Plan &> FDS_Configuration_Management_Plan.err
+bibtex FDS_Configuration_Management_Plan &> FDS_Configuration_Management_Plan.err
+pdflatex -interaction nonstopmode FDS_Configuration_Management_Plan &> FDS_Configuration_Management_Plan.err
+pdflatex -interaction nonstopmode FDS_Configuration_Management_Plan &> FDS_Configuration_Management_Plan.err
+
+# Scan and report any errors in the LaTeX build process
+if [[ `grep "! LaTeX Error:" -I FDS_Configuration_Management_Plan.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX errors detected:"
+      grep "! LaTeX Error:" -I FDS_Configuration_Management_Plan.err
+      clean_build=0
+fi
+
+# Check for LaTeX warnings (undefined references or duplicate labels)
+if [[ `grep -E "undefined|multiply defined|multiply-defined" -I FDS_Configuration_Management_Plan.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX warnings detected:"
+      grep -E "undefined|multiply defined|multiply-defined" -I FDS_Configuration_Management_Plan.err
+      clean_build=0
+fi
+
+if [[ $clean_build == 0 ]]
+   then
+      :
+   else
+      echo "FDS Configuration Management Plan built successfully!"
+fi    
