a9690d17bba82d99f4d6833d42f09878a373c28f
Kristopher Overholt <koverholt@gmail.com>
2012-08-09 14:53:40 +0000

    FDTs Validation Guide: Initial commit of guide and figures

---
+++ Manuals/FDTs_Validation_Guide/make_guide.sh
@@ -0,0 +1,36 @@
+clean_build=1
+
+# Build FDTs Validation Guide
+pdflatex -interaction nonstopmode FDTs_Validation_Guide &> FDTs_Validation_Guide.err
+bibtex FDTs_Validation_Guide &> FDTs_Validation_Guide.err
+pdflatex -interaction nonstopmode FDTs_Validation_Guide &> FDTs_Validation_Guide.err
+pdflatex -interaction nonstopmode FDTs_Validation_Guide &> FDTs_Validation_Guide.err
+
+# Scan and report any errors in the LaTeX build process
+if [[ `grep "! LaTeX Error:" -I FDTs_Validation_Guide.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX errors detected:"
+      grep "! LaTeX Error:" -I FDTs_Validation_Guide.err
+      clean_build=0
+fi
+
+# Check for LaTeX warnings (undefined references or duplicate labels)
+if [[ `grep -E "undefined|multiply defined|multiply-defined" -I FDTs_Validation_Guide.err` == "" ]]
+   then
+      # Continue along
+      :
+   else
+      echo "LaTeX warnings detected:"
+      grep -E "undefined|multiply defined|multiply-defined" -I FDTs_Validation_Guide.err
+      clean_build=0
+fi
+
+if [[ $clean_build == 0 ]]
+   then
+      :
+   else
+      echo "FDTs Validation Guide built successfully!"
+fi    
