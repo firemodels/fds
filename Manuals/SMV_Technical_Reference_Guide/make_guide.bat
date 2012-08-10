de58a17134eed1cab1e705a9d396ea36e920497c
gforney <gforney@gmail.com>
2012-08-10 13:15:35 +0000

    smv guides: check for errors when building smv guides on windows (similar to corresponding .sh scripts)
    Former-commit-id: 318056b60d7af43f526db43ab0d550720819e2c2 [formerly 911c2b1f1a54ecaf0a41156a3d8df78b09a7c1bd]
    Former-commit-id: 3c5621b7ea43bc585044dcff27d8e4699f265e85
    Former-commit-id: 8b660bf96ca5f1d6904a0f23cf2c102d2e6cef9e

--- Manuals/SMV_Technical_Reference_Guide/make_guide.bat
+++ Manuals/SMV_Technical_Reference_Guide/make_guide.bat
@@ -1,5 +1,15 @@
 @echo off
-pdflatex SMV_Technical_Reference_Guide
-bibtex SMV_Technical_Reference_Guide
-pdflatex SMV_Technical_Reference_Guide
-pdflatex SMV_Technical_Reference_Guide
+set paper=SMV_Technical_Reference_Guide
+
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+
+echo %paper% build complete
+pause
--- Manuals/SMV_Verification_Guide/make_guide.bat
+++ Manuals/SMV_Verification_Guide/make_guide.bat
@@ -1,5 +1,15 @@
 @echo off
-pdflatex SMV_Verification_Guide
-bibtex SMV_Verification_Guide
-pdflatex SMV_Verification_Guide
-pdflatex SMV_Verification_Guide
+set paper=SMV_Verification_Guide
+
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+
+echo %paper% build complete
+pause
