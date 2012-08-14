f40e5e1452e5369e54a04bac056c4d923296fea2
Kristopher Overholt <koverholt@gmail.com>
2012-08-14 19:27:25 +0000

    FDTs: Rename input files for consistency
    Former-commit-id: 2dac02f4ddb00dac4bb9cdac2542e46244a6d025 [formerly 83e514835cfeb43b463add60bccae1bfc160bd80]
    Former-commit-id: bc2388d85cd68935790e89df78687520d286bf42
    Former-commit-id: 2365d59f6244f866d004c264cc1bdff1de751230

--- Validation/Run_FDTs_Cases.sh
+++ Validation/Run_FDTs_Cases.sh
@@ -15,54 +15,54 @@ ifort FDTs.f90 -o FDTs
 # Then, run all FDTs cases
 
 cd $VDIR/CAROLFIRE/FDTs
-$FDTs CAROLFIRE_THIEF_Inputs.txt
+$FDTs CAROLFIRE_FDTs_Inputs.txt
 
 cd $VDIR/Fleury_Heat_Flux/FDTs
-$FDTs Fleury_Heat_Flux_FDTs_Input.txt
+$FDTs Fleury_Heat_Flux_FDTs_Inputs.txt
 
 cd $VDIR/FM_SNL/FDTs
-$FDTs FM_SNL_FDTs_Input.txt
+$FDTs FM_SNL_FDTs_Inputs.txt
 
 cd $VDIR/Hamins_CH4/FDTs
-$FDTs Hamins_FDTs_Input.txt
+$FDTs Hamins_FDTs_Inputs.txt
 
 cd $VDIR/LLNL_Enclosure/FDTs
-$FDTs LLNL_FDTs_Input.txt
+$FDTs LLNL_FDTs_Inputs.txt
 
 cd $VDIR/NBS_Multi-Room/FDTs
-$FDTs NBS_Multi-Room_FDTs_Input.txt
+$FDTs NBS_Multi-Room_FDTs_Inputs.txt
 
 cd $VDIR/NIST_Dunes_2000/FDTs
-$FDTs NIST_Dunes_2000_FDTs_Input.txt
+$FDTs NIST_Dunes_2000_FDTs_Inputs.txt
 
 cd $VDIR/NIST_NRC/FDTs
-$FDTs NIST_NRC_FDTs_Input.txt
+$FDTs NIST_NRC_FDTs_Inputs.txt
 
 cd $VDIR/NRL_HAI/FDTs
-$FDTs NRL_HAI_FDTs_Input.txt
+$FDTs NRL_HAI_FDTs_Inputs.txt
 
 cd $VDIR/SP_AST/FDTs
-$FDTs SP_AST_FDTs_Input.txt
+$FDTs SP_AST_FDTs_Inputs.txt
 
 cd $VDIR/Steckler_Compartment/FDTs
-$FDTs Steckler_MQH_Inputs.txt
+$FDTs Steckler_FDTs_Inputs.txt
 
 cd $VDIR/UL_NFPRF/FDTs
-$FDTs UL_NFPRF_FDTs_Input.txt
+$FDTs UL_NFPRF_FDTs_Inputs.txt
 
 cd $VDIR/Ulster_SBI/FDTs
-$FDTs Ulster_SBI_FDTs_Input.txt
+$FDTs Ulster_SBI_FDTs_Inputs.txt
 
 cd $VDIR/USN_Hangars/FDTs
-$FDTs USN_Hangars_FDTs_Input.txt
+$FDTs USN_Hangars_FDTs_Inputs.txt
 
 cd $VDIR/Vettori_Flat_Ceiling/FDTs
-$FDTs Vettori_FDTs_Input.txt
+$FDTs Vettori_FDTs_Inputs.txt
 
 cd $VDIR/VTT/FDTs
-$FDTs VTT_FDTs_Input.txt
+$FDTs VTT_FDTs_Inputs.txt
 
 cd $VDIR/WTC/FDTs
-$FDTs WTC_FDTs_Input.txt
+$FDTs WTC_FDTs_Inputs.txt
 
 echo FDTs cases complete
