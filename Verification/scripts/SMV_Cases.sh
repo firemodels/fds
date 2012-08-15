f80adca042395bd398eb28d3a2760080656b04a2
firesmokebot <firesmokebot@gmail.com>
2012-08-15 13:35:33 +0000

    vv scripts: remove entries from various .ssf files so that orphan images are not created
    Former-commit-id: a0205e55d5f944c6ee5a45cd4a14ea60f9581c5e [formerly 33992d1d8ea968d4f46bb61ece22aecc0b045b9a]
    Former-commit-id: 3bc9caeda2cd65867d19ae2bf53fa8d2bc1cc7f5
    Former-commit-id: e230d1993d83be1184284ebb5bf96a8295ec26ef

--- Verification/scripts/SMV_Cases.sh
+++ Verification/scripts/SMV_Cases.sh
@@ -9,7 +9,6 @@ $RUNFDS Visualization objects_dynamic
 $RUNFDS Visualization objects_elem
 $RUNFDS Visualization objects_static
 $RUNFDS Visualization plume5c
-$RUNFDS Visualization plume_fed
 $RUNFDS Visualization plume5cdelta
 $RUNFDS Visualization plumeiso
 $RUNFDS Visualization plume5c_bounddef
