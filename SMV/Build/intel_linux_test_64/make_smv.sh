3eb3e2358c9f2e7e037fed5416b50740ffb1132b
gforney <gforney@gmail.com>
2012-08-17 13:12:29 +0000

    smv build: remove comment

--- SMV/Build/intel_linux_test_64/make_smv.sh
+++ SMV/Build/intel_linux_test_64/make_smv.sh
@@ -2,4 +2,4 @@
 ../clean.sh $*
 source $IFORT_COMPILER/bin/iccvars.sh intel64
 source $IFORT_COMPILER/bin/ifortvars.sh intel64
-#make -j4 -f ../Makefile intel_linux_test_64
+make -j4 -f ../Makefile intel_linux_test_64
