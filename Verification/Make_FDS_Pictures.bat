ee7e3db119637143bdc161332fb4c92624c44c1f
mcgratta <mcgratta@gmail.com>
2012-08-17 20:42:27 +0000

    FDS Source: Remove SAWTOOTH.
    Former-commit-id: 6540f4d5556194686cda2f4b8c5933e35d5993a4 [formerly 453b719ff5318719ab5502b6cf6a56bbab0807d0]
    Former-commit-id: 3d990fa1eaa44e015983066213f21e4c0c2aede8
    Former-commit-id: 0cfbebe6c8e6aab36eb0e1bc2a7d3efb4aeb9bbe

--- Verification/Make_FDS_Pictures.bat
+++ Verification/Make_FDS_Pictures.bat
@@ -19,7 +19,6 @@ smokeview -runscript room_fire
 
 cd ..\Flowfields
 smokeview -runscript helium_2d
-smokeview -runscript sawtooth
 smokeview -runscript symmetry_test
 smokeview -runscript jet_fan
 
