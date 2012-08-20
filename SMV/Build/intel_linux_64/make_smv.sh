2302d7492370b57c306ad3ee421cdfba92ec517c
gforney <gforney@gmail.com>
2012-08-20 19:46:45 +0000

    smv build: work around for 64 bit smokeview build

--- SMV/Build/intel_linux_64/make_smv.sh
+++ SMV/Build/intel_linux_64/make_smv.sh
@@ -3,4 +3,6 @@ source ../setopts.sh $*
 source $IFORT_COMPILER/bin/iccvars.sh intel64
 source $IFORT_COMPILER/bin/ifortvars.sh intel64
 make -f ../Makefile clean
-make -j4 SMV_TESTFLAG="$SMV_TESTFLAG" SMV_TESTSTRING="$SMV_TESTSTRING" SMV_PROFILEFLAG="$SMV_PROFILEFLAG" SMV_PROFILESTRING="$SMV_PROFILESTRING" -f ../Makefile intel_linux_64
+echo *** $SMV_MAKE_OPTS ***
+#make -j4 ${SMV_MAKE_OPTS} -f ../Makefile intel_linux_64
+make -j4 -f ../Makefile intel_linux_64
