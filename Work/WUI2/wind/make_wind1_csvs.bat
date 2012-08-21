e8dc97052b60425795b589142b423fe0c958a8c0
gforney <gforney@gmail.com>
2012-08-21 17:56:27 +0000

    vv: add files removed from previsou revision
    Former-commit-id: da3df08b1a8a8ef4d96bac07e3ee9df42daa59aa [formerly b15b64e809426873235199f817601b922fe8e15d]
    Former-commit-id: 2b46eacc7f715c89ca46cba8106f0415080c244b
    Former-commit-id: 9f4a20dfc795252eedf024fb95bcb3b51c15fe11

---
+++ Work/WUI2/wind/make_wind1_csvs.bat
@@ -0,0 +1,7 @@
+set convert=..\..\..\utilities\wind2fds\intel_win_64\wind2fds_win_64
+
+%convert% -prefix sd11 -offset " 50.0  50.0 0.0" a111024.csv
+%convert% -prefix sd12 -offset " 50.0 150.0 0.0" a111025.csv
+%convert% -prefix sd21 -offset "150.0  50.0 0.0" a111026.csv
+%convert% -prefix sd22 -offset "150.0 150.0 0.0" a111027.csv
+%convert% -mindate "12/25/2011 15:37:32" -maxdate "1/1/2012 10:01:30" -wv -prefix wv11 -offset "100.0 100.0 50.0" site17_1214_0102c_ORIG wv11.csv
