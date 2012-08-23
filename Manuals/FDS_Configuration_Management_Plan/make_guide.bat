c37800f47c21f525eea49bc86cac0bb713e1e877
gforney <gforney@gmail.com>
2012-08-23 16:50:34 +0000

    guide scripts: add batch files with error checking to build FDS guides

---
+++ Manuals/FDS_Configuration_Management_Plan/make_guide.bat
@@ -0,0 +1,16 @@
+@echo off
+set paper=FDS_Configuration_Management_Plan
+
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+
+echo %paper% build complete
+pause
+
