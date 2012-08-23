dc34dc221397c870483b8d30b0f655959587fd4b
Kristopher Overholt <koverholt@gmail.com>
2012-08-23 19:55:02 +0000

    FDS Compilation: Add mpi_intel_osx_64_db target directory
    Former-commit-id: 7463b2aa3cd70359a76345e40e28125c0cac1148 [formerly 2e29f44312037c1612347c6980e3a4580a79e14c]
    Former-commit-id: f15df4b7cc3ed3528f44030c9247cc7f112f9040
    Former-commit-id: b82a7d3e984503230f0f431bb583076f83398303

---
+++ FDS_Compilation/mpi_intel_osx_64_db/make_fds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+platform=intel64
+dir=`pwd`
+target=${dir##*/}
+
+source $IFORT_COMPILER/bin/ifortvars.sh $platform
+source ~/.bashrc_fds $platform
+
+echo Building $target
+make -j4 VPATH="../../FDS_Source" -f ../makefile $target
