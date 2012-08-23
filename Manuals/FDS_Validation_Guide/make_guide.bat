7235b4c399206c0b9199d22ae287ebb900a95ca0
gforney <gforney@gmail.com>
2012-08-23 16:50:34 +0000

    guide scripts: add batch files with error checking to build FDS guides
    Former-commit-id: 33b1933d759a8d94baefc7155527f89e7ae52ec7 [formerly 32f05fa1be3d6ed6cebcb4efe0bb550972b11ae9]
    Former-commit-id: 1d85601ba7a0525ed9bb0199c80b0a4844af4f1e
    Former-commit-id: c8f2fbf450a5b3aedbc3e116b8f2d2d53f27cd31

---
+++ Manuals/FDS_Validation_Guide/make_guide.bat
@@ -0,0 +1,15 @@
+@echo off
+set paper=FDS_Validation_Guide
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+
+echo %paper% build complete
+pause
+
---
+++ Manuals/FDTs_Validation_Guide/make_guide.bat
@@ -0,0 +1,16 @@
+@echo off
+set paper=FDTs_Validation_Guide
+
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+
+echo %paper% build complete
+pause
+
