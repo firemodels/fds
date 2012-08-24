42ff80e6c0857d5a919a52be0ba0b721b74deca2
rmcdermo <randy.mcdermott@gmail.com>
2012-08-24 17:48:05 +0000

    Matlab: mods to blasius and heated_channel
    Former-commit-id: 512adb5c929a349ac5acf52b9dba498f13fdab92 [formerly cc0b1e1e5a100bb9cf1f3df21d85fda56a3e15a8]
    Former-commit-id: 055ed76c133dbe871f04a11d833dda53695706bc
    Former-commit-id: 7efdb7538afb26f349744f08a0eadf7ee3cf874b

--- Utilities/Matlab/scripts/blasius.m
+++ Utilities/Matlab/scripts/blasius.m
@@ -5,6 +5,8 @@
 close all
 clear all
 
+plot_style
+
 repository = '../../../Verification/Flowfields/';
 
 
@@ -27,36 +29,23 @@ z_32 = M(:,1);
 
 
 %gather FDS result(n=64)
-filename=[repository,'blasius_32_line.csv'];
+filename=[repository,'blasius_64_line.csv'];
 
 M = csvread(filename,2,0);
 
 u_64 = M(:,2);
 z_64 = M(:,1);
 
-
-%gather FDS result(n=128)
-filename=[repository,'blasius_32_line.csv'];
-
-M = csvread(filename,2,0);
-
-u_128 = M(:,2);
-z_128 = M(:,1);
-
-
-% %gather FDS result(u0)
-% filename=[repository,'blasius_128_devc.csv'];
-% 
-% M = csvread(filename,2,0);
-
-
 %gather blasius profile
 
-u0= max(u_128(:));
+u0= max(u_64(:));
 zmax=0.3;
 [eta,fp] = blasius_analytic(u0, zmax);
 
-z_blasius=eta*sqrt(1E-3/1.199*0.05/u0);
+mu = 0.001;
+rho = 1.19987607036;
+xc = 0.05;
+z_blasius=eta*sqrt(mu/rho*xc/u0);
 u_blasius=fp*u0;
 
 
@@ -64,27 +53,36 @@ u_blasius=fp*u0;
 range = 1:4:length(u_blasius);
 H(1)=plot(u_blasius(range),z_blasius(range),'bo');
 hold on
+H(2)=plot(u_16,z_16,'r--');
+H(3)=plot(u_32,z_32,'c-.');
+H(4)=plot(u_64,z_64,'g-');
 
-plot_style
+axis([0 1.1 0 0.15])
+
 set(gca,'Units',Plot_Units)
 set(gca,'FontName',Font_Name)
+set(gca,'FontSize',Title_Font_Size)
 set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
-set(gcf,'DefaultLineLineWidth',Line_Width)
 
-axis([0 1.1 0 0.15]) 
-xlabel('$u$','Interpreter',Font_Interpreter,'FontSize',Title_Font_Size)
-ylabel('$z$','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size,'Rotation',0.0)
+xlabel('$u$ (m/s)','Interpreter',Font_Interpreter,'FontName',Font_Name)
+ylabel('$z$ (m)','Interpreter',Font_Interpreter,'FontName',Font_Name)
 
-
-H(2)=plot(u_16,z_16,'-.r');
-H(3)=plot(u_32,z_32,'--c');
-H(4)=plot(u_64,z_64,'-.g');
-H(5)=plot(u_128,z_128,'-k');
-
-
-h = legend(H,'Blasius','$N_z=16$','$N_z=32$','$N_z=64$','$N_z=128$','Location','northwest');
+h = legend(H,'Blasius','$N_z=16$','$N_z=32$','$N_z=64$','Location','northwest');
 legend boxoff
 set(h,'FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
+
+% add SVN if file is available
+
+SVN_Filename = [repository,'blasius_16_svn.txt'];
+if exist(SVN_Filename,'file')
+    SVN = importdata(SVN_Filename);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
 
 % print to pdf for whole velocity profile
 set(gcf,'Visible',Figure_Visibility);
@@ -101,7 +99,6 @@ clear H
 err(1) = 0;
 err(2) = 0;
 err(3) = 0;
-err(4) = 0;
 
 % get error(n=16)
 for i=1:length(u_16)   
@@ -124,35 +121,40 @@ end
 err(3)=err(3)/64;
 err(3)=sqrt(err(3));
 
-% get error(n=128)
-for i=1:length(u_128)   
-    err(4)=err(4)+(abs(u_128(i)-u_blasius(2+(i-1)*2)))^2;    
-end   
-err(4)=err(4)/length(u_128);
-err(4)=sqrt(err(4));
-
 
 dz(1)=abs(z_16(10)-z_16(9));
 dz(2)=abs(z_32(10)-z_32(9));
 dz(3)=abs(z_64(10)-z_64(9));
-dz(4)=abs(z_128(10)-z_128(9));
 
 plot_style
 set(gca,'Units',Plot_Units)
 set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
 
 H(1)=loglog(dz, err,'b*-','LineWidth',Line_Width); hold on
-H(2)=loglog(dz, dz,'k--','LineWidth',Line_Width);
-H(3)=loglog(dz, dz.^2,'k-','LineWidth',Line_Width);
+H(2)=loglog(dz, 10*dz,'k--','LineWidth',Line_Width);
+H(3)=loglog(dz, 100*dz.^2,'k-','LineWidth',Line_Width);
 
 xlabel('Grid Spacing, $\delta z$ (m)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
-ylabel('L2 Error (m/s)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
-%axis([1e-3 1e-1 1e-3 1e-0])
+ylabel('RMS Error (m/s)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
+axis([1e-3 1e-1 1e-3 1e-0])
 legend_handle=legend(H,'FDS','${\cal O}(\delta z)$','${\cal O}(\delta z^2)$','Location','Northwest');
 legend boxoff
 set(legend_handle,'FontSize',Label_Font_Size,'Interpreter',Font_Interpreter)
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Title_Font_Size)
+
+% add SVN if file is available
+
+SVN_Filename = [repository,'blasius_16_svn.txt'];
+if exist(SVN_Filename,'file')
+    SVN = importdata(SVN_Filename);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = 10^( log10(x_lim(1))+ SVN_Scale_X*( log10(x_lim(2)) - log10(x_lim(1)) ) );
+    Y_SVN_Position = 10^( log10(y_lim(1))+ SVN_Scale_Y*( log10(y_lim(2)) - log10(y_lim(1)) ) );
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
 
 % print to pdf
 set(gcf,'Visible',Figure_Visibility);
