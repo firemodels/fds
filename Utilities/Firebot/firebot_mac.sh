e15ffd8930d160611d04fe1c3d627cfbfb0a39e9
Kristopher Overholt <koverholt@gmail.com>
2012-08-24 03:50:51 +0000

    Firebot: Fix typo in firebot_mac.sh
    Former-commit-id: 111be374fac7e8e719ace463980627aaaa5342bc [formerly 3298f305d6d6d09609ab24c56ad81cc064ae0499]
    Former-commit-id: 5672447f6beabc834e5773247f528b83234cb874
    Former-commit-id: e28fd93e8ff9b83f12996102b0176d7f0325df39

--- Utilities/Firebot/firebot_mac.sh
+++ Utilities/Firebot/firebot_mac.sh
@@ -389,8 +389,8 @@ wait_verification_cases_long_end()
 {
    # Scans qstat and waits for verification cases to end
    while [[ `ps x | grep intel_osx_64 | wc -l` -gt 1 ]]; do
-      JOBS_REMAINING=`ps x | grep intel_osx_64 | wc -l`
-      echo "${JOBS_REMAINING} verification cases currently running." >> $FIREBOT_DIR/output/stage5
+      JOBS_RUNNING=`ps x | grep intel_osx_64 | wc -l`
+      echo "${JOBS_RUNNING} verification cases currently running." >> $FIREBOT_DIR/output/stage5
       TIME_LIMIT_STAGE="5"
       check_time_limit
       sleep 60
