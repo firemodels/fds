d60d6736a075532fb09370ba9d99c98b787e2d2e
Kristopher Overholt <koverholt@gmail.com>
2012-08-27 05:20:56 +0000

    VV Scripts: Add performance metric to fds_timing_stats.sh
    Former-commit-id: 40707c0a5da988d0cf5fd438a7475fbaaeb51a4e [formerly 5d41c576e32ed68e05569f6d45b055f8c8556a99]
    Former-commit-id: a5ae791d631b0b893c7e8c4bfc9f7e774e25ee71
    Former-commit-id: 340dc4b955f8e868fc2c17cc9c99b722fc43f24f

--- Utilities/Scripts/fds_timing_stats.sh
+++ Utilities/Scripts/fds_timing_stats.sh
@@ -7,22 +7,61 @@
 # This script checks the .out files for the FDS Verification Suite
 # and generates a .csv file of the CPU time called fds_timing_stats.csv
 
+# cd to Verification directory
 SVNROOT=`pwd`/../..
 cd $SVNROOT/Verification
 
-echo 'FDS Case,CPU Time,Number of Cells,Number of Time Steps' > $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
+# Write header information to fds_timing_stats.csv file
+echo 'FDS Case,CPU Time (s),Number of Cells,Number of Time Steps,Performance Metric' > $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
+
+# Loop over all .out files in the Verification directory
 for i in */*.out
 do
+   # Get current file name
    FILE=$i
-   CPU_TIME=`grep -H "Total CPU:" "$i" | tail -n 1 | awk -F' ' '{print $(NF-1), $NF}'`
+
+   # Grep for CPU time and units
+   CPU_TIME_VALUE=`grep -H "Total CPU:" "$i" | tail -n 1 | awk -F' ' '{print $(NF-1)}'`
+   CPU_TIME_UNITS=`grep -H "Total CPU:" "$i" | tail -n 1 | awk -F' ' '{print $NF}'`
+
+   # Convert min to s
+   if [[ $CPU_TIME_UNITS = 'min' ]]
+   then
+      CPU_TIME=`echo "$CPU_TIME_VALUE*60" | bc`
+   fi
+
+   # Convert hr to s
+   if [[ $CPU_TIME_UNITS = 'h' ]]
+   then
+      CPU_TIME=`echo "$CPU_TIME_VALUE*3600" | bc`
+   fi
+
+   # Grep for number of cells in each dimension
    X_CELLS=`grep -H "Cells in the X" "$i" | awk -F' ' '{print $NF}'`
    Y_CELLS=`grep -H "Cells in the Y" "$i" | awk -F' ' '{print $NF}'`
    Z_CELLS=`grep -H "Cells in the Z" "$i" | awk -F' ' '{print $NF}'`
-   NUM_TIME_STEPS=`grep -H "Time Step     " "$i" | tail -n 1 | awk -F' ' '{print $(NF-4)}'`
-   NUM_TOTAL_CELLS=0
+
+   # Sum over the number of cells (for multi-mesh cases)
    for j in $X_CELLS
    do
-       let NUM_TOTAL_CELLS+=`echo "$X_CELLS * $Y_CELLS * $Z_CELLS" | bc`
+      let NUM_TOTAL_CELLS+=`echo "$X_CELLS * $Y_CELLS * $Z_CELLS" | bc`
    done
-   echo "$FILE,$CPU_TIME,$NUM_TOTAL_CELLS,$NUM_TIME_STEPS" >> $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
+
+   # Grep for number of time steps
+   NUM_TIME_STEPS=`grep -H "Time Step     " "$i" | tail -n 1 | awk -F' ' '{print $(NF-4)}'`
+
+   # Calculate nondimensional performance metric
+   # Skip over cases with no time steps
+   if [[ $NUM_TIME_STEPS -eq 0 ]]
+   then
+      PERFORMANCE=0
+   else
+      let PERFORMANCE=`echo "($CPU_TIME*$NUM_TOTAL_CELLS)/$NUM_TIME_STEPS" | bc`
+   fi
+
+   # Write results to fds_timing_stats.csv file
+   echo "$FILE,$CPU_TIME,$NUM_TOTAL_CELLS,$NUM_TIME_STEPS,$PERFORMANCE" >> $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
+
+   # Reset variables
+   NUM_TOTAL_CELLS=0
 done
