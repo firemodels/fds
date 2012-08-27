89b9bc6233df4b5e2b76c8639fb3d01d82191e33
Kristopher Overholt <koverholt@gmail.com>
2012-08-27 04:52:09 +0000

    VV Scripts: Add number of cells and number of time steps to fds_timing_stats.sh script
    Former-commit-id: ef9d324cac70d6f1f059978c88f66cbb8ed20985 [formerly e38b1fe0001b93bd5dfbfa4ad7bd32e194cff4fd]
    Former-commit-id: 85b11b79b3414dc3d2100e3fb7c3c20b60f8f732
    Former-commit-id: d0a95f5972ba499a28dcbabf768124d4b302d900

--- Utilities/Scripts/fds_timing_stats.sh
+++ Utilities/Scripts/fds_timing_stats.sh
@@ -10,10 +10,19 @@
 SVNROOT=`pwd`/../..
 cd $SVNROOT/Verification
 
-echo 'FDS Case,CPU Time' > $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
+echo 'FDS Case,CPU Time,Number of Cells,Number of Time Steps' > $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
 for i in */*.out
 do
    FILE=$i
    CPU_TIME=`grep -H "Total CPU:" "$i" | tail -n 1 | awk -F' ' '{print $(NF-1), $NF}'`
-   echo "$FILE,$CPU_TIME" >> $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
+   X_CELLS=`grep -H "Cells in the X" "$i" | awk -F' ' '{print $NF}'`
+   Y_CELLS=`grep -H "Cells in the Y" "$i" | awk -F' ' '{print $NF}'`
+   Z_CELLS=`grep -H "Cells in the Z" "$i" | awk -F' ' '{print $NF}'`
+   NUM_TIME_STEPS=`grep -H "Time Step     " "$i" | tail -n 1 | awk -F' ' '{print $(NF-4)}'`
+   NUM_TOTAL_CELLS=0
+   for j in $X_CELLS
+   do
+       let NUM_TOTAL_CELLS+=`echo "$X_CELLS * $Y_CELLS * $Z_CELLS" | bc`
+   done
+   echo "$FILE,$CPU_TIME,$NUM_TOTAL_CELLS,$NUM_TIME_STEPS" >> $SVNROOT/Utilities/Scripts/fds_timing_stats.csv
 done
