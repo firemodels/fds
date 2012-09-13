279df6c994c5190a48ed7923d983f2dc02542ec1
Kristopher Overholt <koverholt@gmail.com>
2012-09-13 03:49:43 +0000

    Manuals: Check for more LaTeX errors in make_guide.sh scripts
    Former-commit-id: a0e98984a84a9d8b936c505ae2f7297aeac327c5 [formerly 1bffba6e06767891d8219573b780b3dd551e0578]
    Former-commit-id: 6eba242ae55b0916932fc1b7f5404c5d78356c59
    Former-commit-id: 5fccb8b588132069b4acccc44edd050f39dd0ba9

--- Manuals/FDS_User_Guide/make_guide.sh
+++ Manuals/FDS_User_Guide/make_guide.sh
@@ -13,7 +13,7 @@ if [[ `grep -E "Error: pdflatex|Fatal error|! LaTeX Error:" -I FDS_User_Guide.er
       :
    else
       echo "LaTeX errors detected:"
-      grep "! LaTeX Error:" -I FDS_User_Guide.err
+      grep -E "Error: pdflatex|Fatal error|! LaTeX Error:" -I FDS_User_Guide.err
       clean_build=0
 fi
 
--- Manuals/SMV_Verification_Guide/make_guide.sh
+++ Manuals/SMV_Verification_Guide/make_guide.sh
@@ -13,7 +13,7 @@ if [[ `grep -E "Error: pdflatex|Fatal error|! LaTeX Error:" -I SMV_Verification_
       :
    else
       echo "LaTeX errors detected:"
-      grep "! LaTeX Error:" -I SMV_Verification_Guide.err
+      grep -E "Error: pdflatex|Fatal error|! LaTeX Error:" -I SMV_Verification_Guide.err
       clean_build=0
 fi
 
