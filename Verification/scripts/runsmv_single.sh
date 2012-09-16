28af04239105c8714700b8308cd89851b105759c
gforney <gforney@gmail.com>
2012-09-16 03:35:18 +0000

    smv vv: add script to run smokeview on linux system using Xvfb
    Former-commit-id: d820234aa4538b728faceb6147f7781121bdae07 [formerly c3ff0efc85aa41ac2d6fa8ff1d2707c087f7a862]
    Former-commit-id: ab42d6f9678132dca917b19c1a8eb8127b1c2e9c
    Former-commit-id: 30c1dfc4fb7d5f8435b511c5e80f14eb257eaa6a

---
+++ Verification/scripts/runsmv_single.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+function usage {
+echo "Make_SMV_Pictures.sh [-d -h -r -s size ]"
+echo "Generates figures for Smokeview verification suite"
+echo ""
+echo "Options"
+echo "-d - use debug version of smokeview"
+echo "-h - display this message"
+echo "-t - use test version of smokeview"
+echo "-s size - use 32 or 64 bit (default) version of smokeview"
+exit
+}
+
+OS=`uname`
+if [ "$OS" == "Darwin" ]; then
+  PLATFORM=osx
+else
+  PLATFORM=linux
+fi
+
+SIZE=_64
+DEBUG=
+TEST=
+
+while getopts 'dhts:' OPTION
+do
+case $OPTION  in
+  d)
+   DEBUG=_db
+   ;;
+  h)
+   usage;
+   ;;
+  t)
+   TEST=_test
+  ;;
+  s)
+   SIZE="$OPTARG"
+   if [ $SIZE -eq 64 ] ; then
+     SIZE=_64
+   else
+     SIZE=_32
+   fi
+  ;;
+esac
+done
+shift $(($OPTIND-1))
+
+
+VERSION=$PLATFORM$TEST$SIZE$DEBUG
+VERSION2=$PLATFORM$SIZE$DEBUG
+
+export SVNROOT=~/FDS-SMV/
+
+export SMV=$SVNROOT/SMV/Build/intel_$VERSION2/smokeview_$VERSION
+export SMVBINDIR="-bindir $SVNROOT/SMV/for_bundle"
+
+export STARTX=$SVNROOT/Utilities/Scripts/startXserver.sh
+export STOPX=$SVNROOT/Utilities/Scripts/stopXserver.sh
+
+source $STARTX
+$SMV -runscript $1
+source $STOPX
