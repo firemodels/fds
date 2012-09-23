a3678408eb2d7a4d8cd2f91342aa9873cdf8fbb1
Kristopher Overholt <koverholt@gmail.com>
2012-09-23 05:18:09 +0000

    Manuals: Fix LaTeX error scanning in make_guide scripts

--- Manuals/SMV_Verification_Guide/make_guide.sh
+++ Manuals/SMV_Verification_Guide/make_guide.sh
@@ -7,13 +7,13 @@ pdflatex -interaction nonstopmode SMV_Verification_Guide &> SMV_Verification_Gui
 pdflatex -interaction nonstopmode SMV_Verification_Guide &> SMV_Verification_Guide.err
 
 # Scan and report any errors in the LaTeX build process
-if [[ `grep -E "Error: pdflatex|Fatal error|! LaTeX Error:" -I SMV_Verification_Guide.err` == "" ]]
+if [[ `grep -E "Error:|Fatal error|! LaTeX Error:" -I SMV_Verification_Guide.err` == "" ]]
    then
       # Continue along
       :
    else
       echo "LaTeX errors detected:"
-      grep -E "Error: pdflatex|Fatal error|! LaTeX Error:" -I SMV_Verification_Guide.err
+      grep -E "Error:|Fatal error|! LaTeX Error:" -I SMV_Verification_Guide.err
       clean_build=0
 fi
 
