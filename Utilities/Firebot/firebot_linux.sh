fc0d2b397d08a2432fcc328b82f4497c52dc8b04
rmcdermo <randy.mcdermott@gmail.com>
2012-09-26 13:07:37 +0000

    Firebot: add email accounts
    Former-commit-id: 23ae96fd678827d55a4ad08cc09b52fa9af8dfcd [formerly 8a33d4faf8beca090d4e4d49f649c7364fcf8ce3]
    Former-commit-id: 41dbf5ffb0244652930ece290702b7c41a1c33e0
    Former-commit-id: be2ed92e5677783a162b6c16bb003d1b0ff5e815

--- Utilities/Firebot/firebot_linux.sh
+++ Utilities/Firebot/firebot_linux.sh
@@ -14,7 +14,7 @@
 #  ===================
 
 #mailTo="kevin.mcgrattan@nist.gov, randall.mcdermott@nist.gov, glenn.forney@nist.gov, craig.weinschenk@nist.gov, jfloyd@haifire.com, koverholt@gmail.com"
-mailTo="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, jfloyd@haifire.com, koverholt@gmail.com"
+mailTo="kevin.mcgrattan@nist.gov, mcgratta@gmail.com, randall.mcdermott@nist.gov, randy.mcdermott@gmail.com, gforney@gmail.com, craig.weinschenk@nist.gov, CraigWeinschenk@gmail.com, jfloyd@haifire.com, koverholt@gmail.com"
 FIREBOT_USERNAME="firebot"
 
 FIREBOT_HOME_DIR="/home/$FIREBOT_USERNAME"
