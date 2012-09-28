8d7dc01f6c77d45a9207c193514877bcf22eda3c
gforney <gforney@gmail.com>
2012-09-28 20:54:27 +0000

    smv vv: simplify paths in run cases and generate pictures batch files
    Former-commit-id: a399e2bae8711a4fe247b7b36f8aa1c05fb3c215 [formerly 3df87209dee16e145257970d328cb72c23048f45]
    Former-commit-id: 2afdb991b56b08cfad8204aec6cc39d43d6d72ff
    Former-commit-id: 0917ee188aa08f175248145a621217dd9df50317

--- Verification/scripts/Make_SMV_pictures.bat
+++ Verification/scripts/Make_SMV_pictures.bat
@@ -4,31 +4,39 @@ echo Creating figures for the Smokeview User's and Verification guides
 
 
 set SCRIPT_DIR=%CD%
-set BASEDIR=%CD%\..
-set SVNROOT=%BASEDIR%\..\
+
+cd %CD%\..
+set BASEDIR=%CD%
+
+cd %BASEDIR%\..\
+set SVNROOT=%CD%
 
 set SMOKEVIEW="smokeview"
 set SMOKEZIP="smokezip"
 set SMOKEDIFF="smokediff"
 set BACKGROUND="background"
 
-set vis="%BASEDIR%\Visualization"
-set wui="%BASEDIR%\Wui"
+set vis="%SVNROOT%\Verification\Visualization"
+set wui="%SVNROOT%\Verification\Wui"
 set smvug="%SVNROOT%\Manuals\SMV_User_Guide"
 set smvvg="%SVNROOT%\Manuals\SMV_Verification_Guide"
 
 set RUNFDS=call "%SCRIPT_DIR%\runsmv.bat"
 set RUNCFAST=call "%SCRIPT_DIR%\runsmv.bat"
-set SH2BAT=..\..\Utilities\Data_Processing\sh2bat
+set SH2BAT=%SVNROOT%\Utilities\Data_Processing\sh2bat
 
 cd %smvug%
 
+echo.
 echo erasing Smokeview User guide scripted figures and info files
+
 erase SCRIPT_FIGURES\*.png
 erase SCRIPT_FIGURES\*.help
 erase SCRIPT_FIGURES\*.version
 
+echo.
 echo Creating Smokeview User guide info files
+
 %SMOKEVIEW% -help > SCRIPT_FIGURES\smokeview.help
 %SMOKEVIEW% -version > SCRIPT_FIGURES\smokeview.version
 %SMOKEZIP% -help > SCRIPT_FIGURES\smokezip.help
@@ -38,13 +46,20 @@ echo Creating Smokeview User guide info files
 %BACKGROUND% -version > SCRIPT_FIGURES\background.version
 
 cd %smvvg%
+
+echo.
 echo erasing Smokeview Verification guide scripted figures and info files
+
 erase SCRIPT_FIGURES\*.version
 erase SCRIPT_FIGURES\*.png
+
+echo.
 echo Creating Smokeview Verification guide info file
 %SMOKEVIEW% -version > SCRIPT_FIGURES\smokeview.version
 
+echo.
 echo converting SMV_Cases.sh case list to SMV_Pictures_Cases.bat
+
 cd %SCRIPT_DIR%
 %SH2BAT% SMV_Cases.sh SMV_Pictures_Cases.bat
 %SH2BAT% SMV_DIFF_Cases.sh SMV_DIFF_Pictures_Cases.bat
@@ -55,7 +70,9 @@ cd %SVNROOT%\Verification\Visualization
 %SMOKEDIFF% plume5c plume5cdelta
 %SMOKEDIFF% thouse5 thouse5delta
 
+echo.
 echo Generating images
+
 cd %BASEDIR%
 call %SCRIPT_DIR%\SMV_Pictures_Cases.bat
 
