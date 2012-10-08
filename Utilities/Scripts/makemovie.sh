9e19adb4bd8c3e11317fd32ef35e221a8ada7473
gforney <gforney@gmail.com>
2012-10-08 19:19:38 +0000

    smv vv: update scripts (both smokeview and bash) for generating movies.

--- Utilities/Scripts/makemovie.sh
+++ Utilities/Scripts/makemovie.sh
@@ -2,6 +2,8 @@
 base=$1
 outdir=$2
 underscore=_
+echoerr() { echo "$@" 1>&2; }
+echoerr Creating the movie file $outdir/$base.m1v
 #jpeg2yuv -f 25 -I p -j $base$underscore%04d.jpg | mpeg2enc -o $outdir/$base.m1v
-png2yuv -f 25 -I p -j $base$underscore%04d.png | mpeg2enc -o $outdir/$base.m1v
-echo The movie file $outdir/$base.m1v has been created.
+png2yuv -f 25 -I p -j $base$underscore%04d.png | mpeg2enc -v 0 -o $outdir/$base.m1v
+echoerr The movie file $outdir/$base.m1v has been created.
