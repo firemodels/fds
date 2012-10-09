a319426b7ec23c2a3cb23eb41909e98b13701896
cweinschenk <CraigWeinschenk@gmail.com>
2012-10-09 20:30:09 +0000

    FDS Source: Major Commit - remove obsolete fire routines
    Former-commit-id: 13011d415639fb713a61e8271cbc1aa5e48c5abc [formerly eccd7675ab634975b527e166e69bc60b04929604]
    Former-commit-id: 2f5df69fa96b9a325c5c56a31ff59dc87b544bf4
    Former-commit-id: 6287f201a2a4807dd9deb1acd51cc0ed46f261b4

--- FDS_Source/dump.f90
+++ FDS_Source/dump.f90
@@ -2487,133 +2487,50 @@ IF (N_REACTIONS>0) WRITE(LU_OUTPUT,'(//A)') ' Gas Phase Reaction Information'
 
 REACTION_LOOP: DO N=1,N_REACTIONS
    RN => REACTION(N)
-   EDC_IF: IF (.NOT. EDC) THEN
-   
-      SELECT CASE (COMBUSTION_ODE)
-         CASE (SINGLE_EXACT)
-            ODE_SOLVER = 'SINGLE EXACT'      
-         CASE (EXPLICIT_EULER)
-            ODE_SOLVER = 'EXPLICIT EULER'
-         CASE (RUNGE_KUTTA_2)
-            ODE_SOLVER = 'RUNGE-KUTTA 2'
-         CASE (RK2_RICHARDSON)
-               ODE_SOLVER = 'RK2 RICHARDSON'
-         CASE (EDCM_RK2)
-               ODE_SOLVER = 'EDCM RK2'
-      END SELECT
-   
-      IF (RN%FYI/='null') WRITE(LU_OUTPUT,'(/3X,A)') RN%FYI
-      IF (RN%ID/='null')  WRITE(LU_OUTPUT,'(/3X,A)') RN%ID
-   
-      SELECT CASE(RN%MODE)
-         CASE(FINITE_RATE)
-            WRITE(LU_OUTPUT,'(/3X,A)')  'Finite Rate Reaction'
-            WRITE(LU_OUTPUT,'(3X,A,A)')  'ODE Solver:  ', ODE_SOLVER
-            WRITE(LU_OUTPUT,'(/3X,A)') 'Tracked Species'
-            WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'         
-            DO NN=0,N_TRACKED_SPECIES
-               IF (ABS(RN%NU(NN)) <=TWO_EPSILON_EB) CYCLE
-               WRITE(LU_OUTPUT,'(3X,A,1X,F12.6)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
-            ENDDO
-            WRITE(LU_OUTPUT,'(/3X,A)') 'Detailed Species'
-            WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'
-            DO NN=1,N_SPECIES
-               IF (ABS(RN%NU_SPECIES(NN))>=TWO_EPSILON_EB) WRITE(LU_OUTPUT,'(3X,A,1X,F9.4)') SPECIES(NN)%ID,RN%NU_SPECIES(NN)
-            ENDDO
-            WRITE(LU_OUTPUT,'(/A)') '   Species ID                     Rate Exponent'
-            DO NN=1,N_SPECIES
-               IF (RN%N_S(NN) <=-998._EB) CYCLE
-               WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') SPECIES(NN)%ID,RN%N_S(NN) 
-            ENDDO
-            WRITE(LU_OUTPUT,'(/A)') '   Arrhenius Constants'
-            WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Pre-exponential:  ',RN%A_IN
-            WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Activation Energy:',RN%E_IN
-            WRITE(LU_OUTPUT,'(/A)') '   Fuel                           Heat of Combustion (kJ/kg)'    
-            WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') RN%FUEL,RN%HEAT_OF_COMBUSTION/1000._EB
-         CASE (EDDY_DISSIPATION)
-            WRITE(LU_OUTPUT,'(/3X,A)')  'Eddy Dissipation Reaction'
-            WRITE(LU_OUTPUT,'(3X,A,A)')  'ODE Solver:  ', ODE_SOLVER
-            WRITE(LU_OUTPUT,'(/3X,A)') 'Tracked Species'
-            WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'
-            DO NN=0,N_TRACKED_SPECIES
-               IF (ABS(RN%NU(NN))>TWO_EPSILON_EB) WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
-            ENDDO        
-            WRITE(LU_OUTPUT,'(/3X,A)') 'Detailed Species'
-            WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'
-            DO NN=1,N_SPECIES
-               IF (ABS(RN%NU_SPECIES(NN))>TWO_EPSILON_EB) WRITE(LU_OUTPUT,'(3X,A,1X,F9.4)') SPECIES(NN)%ID,RN%NU_SPECIES(NN)
-            ENDDO
-            WRITE(LU_OUTPUT,'(A,F12.3)')  '   Heat of Combustion (kJ/kg)  ',RN%HEAT_OF_COMBUSTION/1000._EB
-         CASE (EDDY_DISSIPATION_CONCEPT)
-            WRITE(LU_OUTPUT,'(/3X,A)')  'Eddy Dissipation Concept Reaction'
-            WRITE(LU_OUTPUT,'(3X,A,A)')  'ODE Solver:  ', ODE_SOLVER
-            WRITE(LU_OUTPUT,'(/3X,A)') 'Tracked Species'
-            WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'         
-            DO NN=0,N_TRACKED_SPECIES
-               IF (ABS(RN%NU(NN)) <=TWO_EPSILON_EB) CYCLE
-               WRITE(LU_OUTPUT,'(3X,A,1X,F12.6)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
-            ENDDO
-            WRITE(LU_OUTPUT,'(/3X,A)') 'Detailed Species'
-            WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'
-            DO NN=1,N_SPECIES
-               IF (ABS(RN%NU_SPECIES(NN))>=TWO_EPSILON_EB) WRITE(LU_OUTPUT,'(3X,A,1X,F9.4)') SPECIES(NN)%ID,RN%NU_SPECIES(NN)
-            ENDDO
-            WRITE(LU_OUTPUT,'(/A)') '   Species ID                     Rate Exponent'
-            DO NN=1,N_SPECIES
-               IF (RN%N_S(NN) <=-998._EB) CYCLE
-               WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') SPECIES(NN)%ID,RN%N_S(NN) 
-            ENDDO
-            WRITE(LU_OUTPUT,'(/A)') '   Arrhenius Constants'
-            WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Pre-exponential:  ',RN%A_IN
-            WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Activation Energy:',RN%E_IN
-            WRITE(LU_OUTPUT,'(/A)') '   Fuel                           Heat of Combustion (kJ/kg)'    
-            WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') RN%FUEL,RN%HEAT_OF_COMBUSTION/1000._EB
-      END SELECT
-   ELSE EDC_IF
-      SELECT CASE (COMBUSTION_ODE)     
-         CASE (EXPLICIT_EULER)
-            ODE_SOLVER = 'EXPLICIT EULER'
-         CASE (EDCM_RK2)
-               ODE_SOLVER = 'EDCM RK2'
-      END SELECT
-      SELECT CASE (EXTINCT_MOD)     
-         CASE (EXTINCTION_1)
-            EXTINCTION_MODEL = 'EXTINCTION 1'
-         CASE (EXTINCTION_2)
-            EXTINCTION_MODEL = 'EXTINCTION 2'
-      END SELECT    
+   SELECT CASE (COMBUSTION_ODE)     
+      CASE (EXPLICIT_EULER)
+         ODE_SOLVER = 'EXPLICIT EULER'
+      CASE (EDC_RK2)
+            ODE_SOLVER = 'EDC RK2'
+   END SELECT
+   SELECT CASE (EXTINCT_MOD)     
+      CASE (EXTINCTION_1)
+         EXTINCTION_MODEL = 'EXTINCTION 1'
+      CASE (EXTINCTION_2)
+         EXTINCTION_MODEL = 'EXTINCTION 2'
+   END SELECT    
       
-      IF (RN%FYI/='null') WRITE(LU_OUTPUT,'(/3X,A)') RN%FYI
-      IF (RN%ID/='null')  WRITE(LU_OUTPUT,'(/3X,A)') RN%ID   
+   IF (RN%FYI/='null') WRITE(LU_OUTPUT,'(/3X,A)') RN%FYI
+   IF (RN%ID/='null')  WRITE(LU_OUTPUT,'(/3X,A)') RN%ID   
    
-      WRITE(LU_OUTPUT,'(/3X,A)')  'Eddy Dissipation Concept Reaction'
-      WRITE(LU_OUTPUT,'(3X,A,A)')  'ODE Solver:  ', ODE_SOLVER
-      IF (SUPPRESSION) THEN
-         WRITE(LU_OUTPUT,'(3X,A,A)')  'Extinction Model:  ', EXTINCTION_MODEL
-         WRITE(LU_OUTPUT,'(3X,A,F8.1)')  'Critical Flame Temperature (K):  ', RN%CRIT_FLAME_TMP
-      ENDIF
-      WRITE(LU_OUTPUT,'(/3X,A)') 'Tracked Species'
-      WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'         
-      DO NN=0,N_TRACKED_SPECIES
-         IF (ABS(RN%NU(NN)) <=TWO_EPSILON_EB) CYCLE
-         WRITE(LU_OUTPUT,'(3X,A,1X,F12.6)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
-      ENDDO
-      WRITE(LU_OUTPUT,'(/3X,A)') 'Detailed Species'
-      WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'
-      DO NN=1,N_SPECIES
-         IF (ABS(RN%NU_SPECIES(NN))>=TWO_EPSILON_EB) WRITE(LU_OUTPUT,'(3X,A,1X,F9.4)') SPECIES(NN)%ID,RN%NU_SPECIES(NN)
-      ENDDO
-      WRITE(LU_OUTPUT,'(/A)') '   Species ID                     Rate Exponent'
-      DO NN=1,N_SPECIES
-        IF (RN%N_S(NN) <=-998._EB) CYCLE
-        WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') SPECIES(NN)%ID,RN%N_S(NN) 
-      ENDDO
-      WRITE(LU_OUTPUT,'(/A)') '   Arrhenius Constants'
-      WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Pre-exponential:  ',RN%A_IN
-      WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Activation Energy:',RN%E_IN
-      WRITE(LU_OUTPUT,'(/A)') '   Fuel                           Heat of Reaction (kJ/kg)'            
-      WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') RN%FUEL,RN%HEAT_OF_COMBUSTION/1000._EB
-   ENDIF EDC_IF    
+   WRITE(LU_OUTPUT,'(/3X,A)')  'Eddy Dissipation Concept Reaction'
+   WRITE(LU_OUTPUT,'(3X,A,A)')  'ODE Solver:  ', ODE_SOLVER
+   IF (SUPPRESSION) THEN
+      WRITE(LU_OUTPUT,'(3X,A,A)')  'Extinction Model:  ', EXTINCTION_MODEL
+      WRITE(LU_OUTPUT,'(3X,A,F8.1)')  'Critical Flame Temperature (K):  ', RN%CRIT_FLAME_TMP
+   ENDIF
+   WRITE(LU_OUTPUT,'(/3X,A)') 'Tracked Species'
+   WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'         
+   DO NN=0,N_TRACKED_SPECIES
+      IF (ABS(RN%NU(NN)) <=TWO_EPSILON_EB) CYCLE
+      WRITE(LU_OUTPUT,'(3X,A,1X,F12.6)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
+   ENDDO
+   WRITE(LU_OUTPUT,'(/3X,A)') 'Detailed Species'
+   WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'
+   DO NN=1,N_SPECIES
+      IF (ABS(RN%NU_SPECIES(NN))>=TWO_EPSILON_EB) WRITE(LU_OUTPUT,'(3X,A,1X,F9.4)') SPECIES(NN)%ID,RN%NU_SPECIES(NN)
+   ENDDO
+   WRITE(LU_OUTPUT,'(/A)') '   Species ID                     Rate Exponent'
+   DO NN=1,N_SPECIES
+     IF (RN%N_S(NN) <=-998._EB) CYCLE
+     WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') SPECIES(NN)%ID,RN%N_S(NN) 
+   ENDDO
+   WRITE(LU_OUTPUT,'(/A)') '   Arrhenius Constants'
+   WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Pre-exponential:  ',RN%A_IN
+   WRITE(LU_OUTPUT,'(A,1X,ES13.6)')  '   Activation Energy:',RN%E_IN
+   WRITE(LU_OUTPUT,'(/A)') '   Fuel                           Heat of Combustion (kJ/kg)'            
+   WRITE(LU_OUTPUT,'(3X,A,1X,F11.5)') RN%FUEL,RN%HEAT_OF_COMBUSTION/1000._EB
+ 
 ENDDO REACTION_LOOP
 
 ! Print out information about materials
