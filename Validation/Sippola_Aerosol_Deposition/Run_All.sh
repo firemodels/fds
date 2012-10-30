e4c47a6a6708903aa8c6ab7420fed499e05e506f
Kristopher Overholt <koverholt@gmail.com>
2012-10-30 19:53:55 +0000

    FDS Validation: Add additional Sippola aerosol deposition in duct cases
    Former-commit-id: 616eaa27f1345afe254206b81ad4e62d07159d34 [formerly 2db20d97d67db258848d60b4c747d76f3f86e811]
    Former-commit-id: 557c970cf3902efd9730c4d50b3651a26bc9be3f
    Former-commit-id: d0b63f21016661eb1ce98665ef35c2b848a9081b

--- Validation/Sippola_Aerosol_Deposition/Run_All.sh
+++ Validation/Sippola_Aerosol_Deposition/Run_All.sh
@@ -11,6 +11,21 @@ export INDIR=Current_Results
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
-$RUNFDS $INDIR square_duct_2_2_vel_1_um
+$RUNFDS $INDIR Sippola_Test_01
+$RUNFDS $INDIR Sippola_Test_02
+$RUNFDS $INDIR Sippola_Test_03
+$RUNFDS $INDIR Sippola_Test_04
+$RUNFDS $INDIR Sippola_Test_05
+$RUNFDS $INDIR Sippola_Test_06
+$RUNFDS $INDIR Sippola_Test_07
+$RUNFDS $INDIR Sippola_Test_08
+$RUNFDS $INDIR Sippola_Test_09
+$RUNFDS $INDIR Sippola_Test_10
+$RUNFDS $INDIR Sippola_Test_11
+$RUNFDS $INDIR Sippola_Test_12
+$RUNFDS $INDIR Sippola_Test_13
+$RUNFDS $INDIR Sippola_Test_14
+$RUNFDS $INDIR Sippola_Test_15
+$RUNFDS $INDIR Sippola_Test_16
 
 echo FDS cases submitted
