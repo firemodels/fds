0d23bc0fa0a5dc6a2a6279044693ce1c8b570c47
gforney <gforney@gmail.com>
2012-11-02 19:07:04 +0000

    fds vv: add alternate run_all script to NIST_RSE directory

---
+++ Validation/NIST_RSE_1994/Run_All2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export QFDS=/usr/local/bin/qfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+source ~/.bashrc_fds intel64
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_100_RI=5.fds
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_150_RI=5.fds
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_200_RI=5.fds
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_300_RI=5.fds
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_400_RI=5.fds 
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_500_RI=5.fds 
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_50_RI=5.fds
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_600_RI=5.fds
+$QFDS -r -p 2 -d $INDIR NIST_RSE_1994_75_RI=5.fds  
+
+echo FDS cases submitted
