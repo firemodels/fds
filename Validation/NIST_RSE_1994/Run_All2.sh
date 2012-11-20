8db769fa838ca908899294387aa8b3199e878b46
cweinschenk <CraigWeinschenk@gmail.com>
2012-11-20 15:25:36 +0000

    FDS Validation: update Run_All scripts for NIST_RSE cases
    Former-commit-id: cd85d404869cbc56a1679c0b41a7eefcc59c61f1 [formerly 1189378d0a508edbaf4efdd70e2428c4de3f2d6f]
    Former-commit-id: d5dfc149366b301d73cf347bf065032554cfd129
    Former-commit-id: d5f97d8a427c3c933c40d294a0a6bd6f97ec58a5

--- Validation/NIST_RSE_1994/Run_All2.sh
+++ Validation/NIST_RSE_1994/Run_All2.sh
@@ -4,8 +4,8 @@ export SVNROOT=`pwd`/../..
 export QFDS=/usr/local/bin/qfds.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
-qq="-q fire80s"
-#qq=
+# qq="-q fire80s"
+qq=
 source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
@@ -22,5 +22,26 @@ $QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_500_RI=5.fds
 $QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_50_RI=5.fds
 $QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_600_RI=5.fds
 $QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_75_RI=5.fds  
+
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_100_RI=10.fds
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_150_RI=10.fds
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_200_RI=10.fds
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_300_RI=10.fds
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_400_RI=10.fds 
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_500_RI=10.fds 
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_50_RI=10.fds
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_600_RI=10.fds
+$QFDS -r -p 2 $qq -d $INDIR NIST_RSE_1994_75_RI=10.fds  
+
+#runs jobs on four nodes - 2 processors per node
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_100_RI=20.fds
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_150_RI=20.fds
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_200_RI=20.fds
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_300_RI=20.fds
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_400_RI=20.fds 
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_500_RI=20.fds 
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_50_RI=20.fds
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_600_RI=20.fds
+$QFDS -r -p 8 -n 2 $qq -d $INDIR NIST_RSE_1994_75_RI=20.fds  
 
 echo FDS cases submitted
