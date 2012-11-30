1ec211963ac5d57c2ec9c6e66e994ebdeb6330e6
gforney <gforney@gmail.com>
2012-11-30 00:57:11 +0000

    smv vv: add script to run smokebot whenever smokeview source changes (script is run from cron)
    Former-commit-id: f0f2fc7b3620c481b7d753dc0aa7d90ab1941b5e [formerly d20023569b422a1ffd7a71d718535415ca9ee572]
    Former-commit-id: 25807b0239eab217d35bf7ce00c532542a02f8a9
    Former-commit-id: 0f135292d421061c875893ee90f21a9f143bb071

---
+++ Utilities/Firebot/auto_smokebot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SVNROOT=~/FDS-SMV
+mailTo="gforney@gmail.com"
+
+SVNSOURCE=$SVNROOT/SMV/source
+SVNFILE=$SVNROOT/svnversion
+SMOKEBOTDIR=~/SMOKEBOT/
+SMOKEBOTEXE=./run_smokebot.sh
+
+cd $SVNSOURCE
+svn update
+THISSVN=`svn info | tail -3 | head -1 | awk '{print $4}'`
+LASTSVN=`cat $SVNFILE`
+if [ $THISSVN != $LASTSVN ] ; then
+  echo Smokebot run initiated
+  echo $THISSVN>$SVNFILE
+  echo -e "smokeview source has changed\n (Old revision: $LASTSVN, New revision: $THISSVN)\n Smokebot run initiated.\n" | mail -s "smokebot run initiated" $mailTo > /dev/null
+  cd $SMOKEBOTDIR
+  $SMOKEBOTEXE
+fi
