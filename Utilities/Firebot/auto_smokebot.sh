647cf1788e11e8862b4dfc07bb1e8ea230eb0710
gforney <gforney@gmail.com>
2012-11-30 02:35:13 +0000

    fds/smv vv: update auto_smokebot to run if either smokeview or fds source changes

--- Utilities/Firebot/auto_smokebot.sh
+++ Utilities/Firebot/auto_smokebot.sh
@@ -2,18 +2,39 @@
 SVNROOT=~/FDS-SMV
 mailTo="gforney@gmail.com koverholt@gmail.com"
 
-SVNSOURCE=$SVNROOT/SMV/source
-SVNFILE=$SVNROOT/svnversion
+SMV_SOURCE=$SVNROOT/SMV/source
+SVN_SMVFILE=$SVNROOT/smv_revision
+
+FDS_SOURCE=$SVNROOT/FDS_Source
+SVN_FDSFILE=$SVNROOT/fds_revision
+
 SMOKEBOTDIR=~/SMOKEBOT/
 SMOKEBOTEXE=./run_smokebot.sh
 
-cd $SVNSOURCE
-svn update
-THISSVN=`svn info | tail -3 | head -1 | awk '{print $4}'`
-LASTSVN=`cat $SVNFILE`
-if [ $THISSVN != $LASTSVN ] ; then
-  echo $THISSVN>$SVNFILE
-  echo -e "smokeview source has changed\n (Old revision: $LASTSVN, New revision: $THISSVN)\n Smokebot run initiated.\n" | mail -s "smokebot run initiated" $mailTo > /dev/null
+MESSAGE_FILE=$SVNROOT/message
+
+cd $SMV_SOURCE
+svn update > /dev/null
+THIS_SMVSVN=`svn info | tail -3 | head -1 | awk '{print $4}'`
+LAST_SMVSVN=`cat $SVN_SMVFILE`
+
+cd $FDS_SOURCE
+svn update > /dev/null
+THIS_FDSSVN=`svn info | tail -3 | head -1 | awk '{print $4}'`
+LAST_FDSSVN=`cat $SVN_FDSFILE`
+
+rm -f $MESSAGE_FILE
+if [[ $THIS_SMVSVN != $LAST_SMVSVN || $THIS_FDSSVN != $LAST_FDSSVN ]] ; then
+  if [[ $THIS_SMVSVN != $LAST_SMVSVN ]] ; then
+    echo -e "smokeview source has changed. $LAST_SMVSVN->$THIS_SMVSVN" >> $MESSAGE_FILE
+  fi
+  if [[ $THIS_FDSSVN != $LAST_FDSSVN ]] ; then
+    echo -e "FDS source has changed. $LAST_FDSSVN->$THIS_FDSSVN" >> $MESSAGE_FILE
+  fi
+  echo -e "Smokebot run initiated." >> $MESSAGE_FILE
+  echo $THIS_SMVSVN>$SVN_SMVFILE
+  echo $THIS_FDSSVN>$SVN_FDSFILE
+  cat $MESSAGE_FILE | mail -s "smokebot run initiated" $mailTo > /dev/null
   cd $SMOKEBOTDIR
   $SMOKEBOTEXE
 fi
