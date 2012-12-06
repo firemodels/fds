6625dba632a36bb6178e519b09d2cf60b7104e7f
gforney <gforney@gmail.com>
2012-12-06 21:11:04 +0000

    smv vv: update run script to use new way of submitting jobs

--- SMV/vistest/Visualization2/Run_vis2.sh
+++ SMV/vistest/Visualization2/Run_vis2.sh
@@ -3,34 +3,31 @@ CURDIR=`pwd`
 cd ..
 export SVNROOT=`pwd`/..
 
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
-export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
-export BASEDIR=`pwd`
+QFDS=/usr/local/bin/qfds.sh
 # uncomment following line to stop all cases
 #export STOPFDS=1
 
-
-$RUNFDS Visualization2 bound_test
-$RUNFDS Visualization2 cell_test
-$RUNFDS Visualization2 diff2a
-$RUNFDS Visualization2 diff2b
-$RUNFDS Visualization2 fed_test2
-$RUNFDS Visualization2 fed_test
-$RUNFDS Visualization2 grid_test
-$RUNFDS Visualization2 lava_lamp
-$RUNFDS Visualization2 mplume5c2
-$RUNFDS Visualization2 mplume5c2_large
-$RUNFDS Visualization2 objects_dynamic2
-$RUNFDS Visualization2 objects_dynamic3
-$RUNFDS Visualization2 objects_dynamic4
-$RUNFDS Visualization2 objects_include
-$RUNFDS Visualization2 objects_movingbox
-$RUNFDS Visualization2 objects_moving_slice
-$RUNFDS Visualization2 objects_movingsphere2
-$RUNFDS Visualization2 objects_rack
-$RUNFDS Visualization2 objects_wui
-$RUNFDS Visualization2 slice3d_test2
-$RUNFDS Visualization2 slice3d_test
-$RUNFDS Visualization2 smoke_test
-$RUNFDS Visualization2 smv_reload
-$RUNFDS Visualization2 vector_cell_test
+$QFDS -r -d Visualization2 bound_test.fds
+$QFDS -r -d Visualization2 cell_test.fds
+$QFDS -r -d Visualization2 diff2a.fds
+$QFDS -r -d Visualization2 diff2b.fds
+$QFDS -r -d Visualization2 fed_test2.fds
+$QFDS -r -d Visualization2 fed_test.fds
+$QFDS -r -d Visualization2 grid_test.fds
+$QFDS -r -d Visualization2 lava_lamp.fds
+$QFDS -r -d Visualization2 mplume5c2.fds
+$QFDS -r -d Visualization2 mplume5c2_large.fds
+$QFDS -r -d Visualization2 objects_dynamic2.fds
+$QFDS -r -d Visualization2 objects_dynamic3.fds
+$QFDS -r -d Visualization2 objects_dynamic4.fds
+$QFDS -r -d Visualization2 objects_include.fds
+$QFDS -r -d Visualization2 objects_movingbox.fds
+$QFDS -r -d Visualization2 objects_moving_slice.fds
+$QFDS -r -d Visualization2 objects_movingsphere2.fds
+$QFDS -r -d Visualization2 objects_rack.fds
+$QFDS -r -d Visualization2 objects_wui.fds
+$QFDS -r -d Visualization2 slice3d_test2.fds
+$QFDS -r -d Visualization2 slice3d_test.fds
+$QFDS -r -d Visualization2 smoke_test.fds
+$QFDS -r -d Visualization2 smv_reload.fds
+$QFDS -r -d Visualization2 vector_cell_test.fds
