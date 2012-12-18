f4bb494a78004ee3ec79efe69fee916b583f38b5
gforney <gforney@gmail.com>
2012-12-18 17:42:54 +0000

    smv vv: add options in  qfds.sh to handle smokeview, add qsmokeview.sh script to invoke multiple smokeviews (using qfds.sh)
    Former-commit-id: 634c3fea3b821b16d3eb70d8b90c081c7452b1b7 [formerly 8b6aa0ae0d09cc497dd3517cb8425a1fc28bcfac]
    Former-commit-id: 9a3201723cb11cf77a000b997ccb9991ef07e835
    Former-commit-id: 3236da9c4f5137ea9c6fd8cffdef252744c56138

---
+++ Utilities/Scripts/bundle_setup/qsmokeview.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# $Date: 2012-11-28 11:04:24 -0500 (Wed, 28 Nov 2012) $ 
+# $Revision: 13952 $
+# $Author: gforney $
+
+PROG=$0
+
+# setup default queue name
+
+progname=qsmokeview.sh
+
+nprocesses=1
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: $progname [-p nprocesses] casename "
+  echo ""
+  echo " -p nprocesses - number of processes used to run a case [default: 1] "
+  echo "input_file - input file"
+  echo ""
+  exit
+fi
+
+# default parameter settings
+
+while getopts 'p:' OPTION
+do
+case $OPTION  in
+  p)
+   nprocesses="$OPTARG"
+  ;;
+esac
+done
+shift $(($OPTIND-1))
+input=$1
+
+i=0
+while [ $i -lt $nprocesses ]; do
+  qfds.sh -r -s -x -y $i -y $nprocesses -q fire70s $input
+  let i=i+1
+done
