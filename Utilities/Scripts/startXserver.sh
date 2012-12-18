219db7720cef869e9ffe9cefb46dcd6d92bb6030
gforney <gforney@gmail.com>
2012-12-18 15:17:43 +0000

    vv: recommit startXserver.sh

--- Utilities/Scripts/startXserver.sh
+++ Utilities/Scripts/startXserver.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -f
+GETNEWPORT () 
+{
+  display_port=`id -u`
+  nmatches=`ps a | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
+  while [ $nmatches -ne 0 ] ; do
+    display_port=`expr $display_port + 1`
+    nmatches=`ps a | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
+  done
+}
 
 OS=`uname`
 if [ "$OS" != "Darwin" ]; then
-  disp=`id -u`
-  Xvfb :$disp -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
+  GETNEWPORT 
+  Xvfb :$display_port -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
   export SMV_ID=$!
-  export DISPLAY=:$disp
+  export DISPLAY=:$display_port
   sleep 8
 fi
