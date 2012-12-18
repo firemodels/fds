f090814bfb41beadc2a43406a1229765b72609cb
gforney <gforney@gmail.com>
2012-12-18 17:46:55 +0000

    smv vv: update script to run smokeview in batch mode
    Former-commit-id: 54e9da1f98ee9817a6175f61e785d98f3f3b2896 [formerly 19bce900966fc604ec979e0674005be92f31f387]
    Former-commit-id: 38819ce7ac9472d01fda7f3d67e26c47fb2a5798
    Former-commit-id: b584d5bea8f4d2d3c03e83c35b2bf12c9052c600

--- Verification/scripts/runsmv_single.sh
+++ Verification/scripts/runsmv_single.sh
@@ -7,9 +7,13 @@ echo ""
 echo "Options"
 echo "-d - use debug version of smokeview"
 echo "-h - display this message"
+echo "-m script - run smokeview script named script"
 echo "-r - run-time directory"
 echo "-t - use test version of smokeview"
 echo "-s size - use 32 or 64 bit (default) version of smokeview"
+echo "-x - invoke -volrender option in smokeview"
+echo "-y startframe - invoke -startframe option in smokeview"
+echo "-z skipframe - invoke -skipframe option in smokeview"
 exit
 }
 
@@ -23,9 +27,13 @@ fi
 SIZE=_64
 DEBUG=
 TEST=
+SCRIPT=
 RUNDIR=
+SKIPFRAME=1
+STARTFRAME=0
+VOLRENDER=
 
-while getopts 'dhtr:s:' OPTION
+while getopts 'dhm:r:s:txy:z:' OPTION
 do
 case $OPTION  in
   d)
@@ -34,26 +42,59 @@ case $OPTION  in
   h)
    usage;
    ;;
+  m)
+   SCRIPT="$OPTARG"
+   ;;
   r)
    RUNDIR="$OPTARG"
    ;;
   t)
    TEST=_test
-  ;;
+   ;;
   s)
    SIZE="$OPTARG"
-   if [ $SIZE -eq 64 ] ; then
+   if [ $SIZE -eq 64 ]; then
      SIZE=_64
    else
      SIZE=_32
    fi
-  ;;
+   ;;
+  x)
+   VOLRENDER=y
+   ;;
+  y)
+   STARTFRAME="$OPTARG"
+   ;;
+  z)
+   SKIPFRAME="$OPTARG"
+   ;;
 esac
 done
 shift $(($OPTIND-1))
 
+if [ $# == 0 ] ; then
+  usage;
+  exit
+fi
+
+CASE=$1
+if [ "$SCRIPT" != "" ]; then
+  SCRIPT="-script $SCRIPT"
+fi
 if [ "$RUNDIR" != "" ]; then
   RUNDIR="-bindir $RUNDIR"
+fi
+if [ "$VOLRENDER" == "y" ]; then
+  VOLRENDER="-volrender"
+  if [ "$STARTFRAME" != "" ]; then
+    STARTFRAME="-startframe $STARTFRAME"
+  fi
+  if [ "$SKIPFRAME" != "" ]; then
+    SKIPFRAME="-skipframe $SKIPFRAME"
+  fi
+else
+  STARTFRAME=
+  SKIPFRAME=
 fi
 VERSION=$PLATFORM$TEST$SIZE$DEBUG
 VERSION2=$PLATFORM$SIZE$DEBUG
@@ -66,10 +107,11 @@ if [ "$RUNDIR" != "" ]; then
   SMVDIR="-bindir $RUNDIR"
 fi
 
-export STARTX=$SVNROOT/Utilities/Scripts/startXserver.sh
-export STOPX=$SVNROOT/Utilities/Scripts/stopXserver.sh
+STARTX=$SVNROOT/Utilities/Scripts/startXserver.sh
+STOPX=$SVNROOT/Utilities/Scripts/stopXserver.sh
 
+echo running:
+echo $SMV $RUNDIR $SMVBINDIR $SCRIPT $VOLRENDER $STARTFRAME $SKIPFRAME $CASE
 source $STARTX
-$SMV $RUNDIR $SMVBINDIR -redirect -runscript $1
-echo command used: $SMV $SMVBINDIR -redirect -runscript $1
+$SMV $RUNDIR $SMVBINDIR $SCRIPT $VOLRENDER $STARTFRAME $SKIPFRAME $CASE
 source $STOPX
