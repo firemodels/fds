9260079c081e656655998c22904a3697ef4f158e
gforney <gforney@gmail.com>
2012-12-18 02:50:49 +0000

    vv: revert 14239
    Former-commit-id: 03b3832df8ed88b96977168d2e2daf16dd14b1f5 [formerly ec54f78b97597bdc9b659c152abab6a1f199175b]
    Former-commit-id: f477f59dcbd4627a62c1786d34aaecb90321cac3
    Former-commit-id: 7ea8f8b30e32127b36b4dd6ce037e1b56b1203da

--- Utilities/Scripts/startXserver.sh
+++ Utilities/Scripts/startXserver.sh
@@ -1,19 +1,10 @@
 #!/bin/bash -f
-GETNEWPORT () 
-{
-  display_port=`id -u`
-  nmatches=`ps a | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
-  while [ $nmatches -ne 0 ] ; do
-    display_port=`expr $display_port + 1`
-    nmatches=`ps a | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
-  done
-}
 
 OS=`uname`
 if [ "$OS" != "Darwin" ]; then
-  GETNEWPORT 
-  Xvfb :$display_port -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
+  disp=`id -u`
+  Xvfb :$disp -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
   export SMV_ID=$!
-  export DISPLAY=:$display_port
+  export DISPLAY=:$disp
   sleep 8
 fi
