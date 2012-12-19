776dc5fd8b0b1282607a7014a23d2b7a112dfb3c
gforney <gforney@gmail.com>
2012-12-19 15:43:12 +0000

    vv: update movie making scripts
    Former-commit-id: d4925edca61b4a2e6ce6d02edd580da39a3d1e96 [formerly 5b3b0ca7a8bde8015601cb40c352a7a7666a4cc3]
    Former-commit-id: 0fbf2a85004dabaf1c2901d649bace0216d192b5
    Former-commit-id: 21b8ba0b72a64bb0039834ca35ef874e5ee419a4

--- Utilities/Scripts/bundle_setup/qsmokeview.sh
+++ Utilities/Scripts/bundle_setup/qsmokeview.sh
@@ -10,11 +10,15 @@ PROG=$0
 progname=qsmokeview.sh
 
 nprocesses=1
+dir=.
+queue=fire70s
 
 if [ $# -lt 1 ]
 then
   echo "Usage: $progname [-p nprocesses] casename "
   echo ""
+  echo " -d directory - directory containing case"
+  echo " -q queue - queue used to run cases (only fire60s, fire70s or vis)"
   echo " -p nprocesses - number of processes used to run a case [default: 1] "
   echo "input_file - input file"
   echo ""
@@ -23,20 +27,38 @@ fi
 
 # default parameter settings
 
-while getopts 'p:' OPTION
+while getopts 'd:p:q:' OPTION
 do
 case $OPTION  in
+  d)
+  dir="$OPTARG"
+  ;;
   p)
    nprocesses="$OPTARG"
+  ;;
+  q)
+  queue="$OPTARG"
   ;;
 esac
 done
 shift $(($OPTIND-1))
 input=$1
 
+stopfile=$dir/$input.stop
+
+if [ $STOPFDS ]; then
+ echo "stopping case: $input"
+ touch $stopfile
+ exit
+fi
+
+if [ "$queue" == "batch" ] ; then
+  queue=fire70s
+fi
+
 i=0
 while [ $i -lt $nprocesses ]; do
-  qfds.sh -r -s -x -y $i -z $nprocesses -q fire70s $input
+  qfds.sh -d $dir -r -s -x -y $i -z $nprocesses -q $queue $input
   sleep 3
   let i=i+1
 done
