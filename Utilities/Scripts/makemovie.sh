ca76656adc93c7afc4606ec50d54f783c3b66fc5
gforney <gforney@gmail.com>
2012-12-19 01:16:40 +0000

    vv: update movie making script
    Former-commit-id: 94b57cbb4b34a35cc8c38d62e74a1941e5d5421f [formerly d99f7c28da43dabec958fe06893e6d2e2e75a41e]
    Former-commit-id: e32638b1be63a89ab4539198b99ce18241377fa2
    Former-commit-id: 961280aec34ccf5af9d9467a244476f339e646c2

--- Utilities/Scripts/makemovie.sh
+++ Utilities/Scripts/makemovie.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
+if [ $# -lt 1 ] ; then
+  echo "Usage: makemovie.sh [-d output_directory] [-m movie_name] input_base"
+  echo ""
+  echo "This script generates a movie from a sequence of "
+  echo "png image files.  Each image file has the form basexxxx.png"
+  echo "where xxxx is a frame number."
+  echo ""
+  echo "-d dir - directory where movie will be placed (default: .)"
+  echo "-m movie name - name of movie generated (default: input_base.m1v)"
+  echo ""
+  exit
+fi
+
+outdir=.
+moviename=
+while getopts 'd:m:' OPTION
+do
+case $OPTION in
+  d)
+  outdir="$OPTARG"
+  ;;
+  m)
+  moviename="$OPTARG"
+  ;;
+esac
+done
+shift $((OPTIND-1))
+
 base=$1
-outdir=$2
 underscore=_
+if [ "$moviename" == "" ] ; then
+  moviename=$base.m1v
+else
+  moviename=$moviename.m1v
+fi
+echo base=$base dir=$outdir moviename=$moviename
+exit
+
 echoerr() { echo "$@" 1>&2; }
-echoerr Creating the movie file $outdir/$base.m1v
-#jpeg2yuv -f 25 -I p -j $base$underscore%04d.jpg | mpeg2enc -o $outdir/$base.m1v
-png2yuv -f 25 -I p -j $base$underscore%04d.png | mpeg2enc -v 0 -o $outdir/$base.m1v
-echoerr The movie file $outdir/$base.m1v has been created.
+echoerr Creating the movie file $outdir/$moviename
+png2yuv -f 25 -I p -j $base$underscore%04d.png | mpeg2enc -v 0 -o $outdir/$moviename
+echoerr The movie file $outdir/$moviename has been created.
