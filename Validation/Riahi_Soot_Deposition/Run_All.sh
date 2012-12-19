6ae606008056821988e9c4decad964acf51b4fb9
Kristopher Overholt <koverholt@gmail.com>
2012-12-19 19:20:40 +0000

    FDS Validation: Initial commit of Riahi Soot Deposition validation folders

---
+++ Validation/Riahi_Soot_Deposition/Run_All.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$RUNFDS $INDIR Riahi_Gasoline
+
+echo FDS cases submitted
