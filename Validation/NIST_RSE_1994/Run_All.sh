8463bd8cb50650dfa674b700fa6ea575d6463982
mcgratta <mcgratta@gmail.com>
2012-12-28 20:42:47 +0000

    FDS Validation: Edit run script.
    Former-commit-id: bdc3b85b0ca6c002460b67f8761265745b60ed88 [formerly 7fed9d39a7831c4f18bf7e2043bd3a2716452477]
    Former-commit-id: 49ca1b271fe69b19929cb522433e21608d0d16a0
    Former-commit-id: b684141152a8101fee5da8e0c523ff43868129c9

--- Validation/NIST_RSE_1994/Run_All.sh
+++ Validation/NIST_RSE_1994/Run_All.sh
@@ -20,7 +20,7 @@ export SVNROOT=`pwd`/../..
 export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
 export RUNFDSMPI="$SVNROOT/Utilities/Scripts/runfdsmpi.sh $queue"
 export BASEDIR=`pwd`
-export INDIR=Current_Results_ex
+export INDIR=Current_Results
 source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
