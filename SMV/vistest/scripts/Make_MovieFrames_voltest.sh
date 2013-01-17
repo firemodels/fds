d0bf3c64fd46546ce17fd6eba2eac4d532454dda
gforney <gforney@gmail.com>
2013-01-17 14:42:41 +0000

    smv vv: update movie making script

--- SMV/vistest/scripts/Make_MovieFrames_voltest.sh
+++ SMV/vistest/scripts/Make_MovieFrames_voltest.sh
@@ -10,8 +10,12 @@ QSMV=/usr/local/bin/qsmokeview.sh
 # uncomment following line to stop all cases
 #export STOPFDS=1
 
-rm -f Voltest/frames/mplumeB8n*.png
+#rm -f Voltest/frames/mplume8n*.png
+#rm -f Voltest/frames/mplumeB8n*.png
+rm -f Voltest/frames/voltest2*.png
+#rm -f Voltest/frames/voltest3*.png
 
 #$QSMV -d Voltest -p 20 mplume8n
-$QSMV -d Voltest -p 50 -q fire7080s  mplumeB8n
-#$QSMV -d Voltest -p 30 voltest2
+#$QSMV -d Voltest -p 30 -q fire70s  mplumeB8n
+$QSMV -d Voltest -p 50 voltest2
+#$QSMV -d Voltest -p 30 voltest3
