3fe9e841de668ac6e22a1b19a0ddc36735df4deb
gforney <gforney@gmail.com>
2013-01-18 15:06:22 +0000

    smv vv: update voltest movie making scripts

--- SMV/vistest/scripts/Make_Movies_voltest.sh
+++ SMV/vistest/scripts/Make_Movies_voltest.sh
@@ -12,5 +12,5 @@ makemovie=/usr/local/bin/make_movie.sh
 cd $CURDIR/..
 
 #$makemovie -i Voltest/frames -o movies mplume8n
-$makemovie -i Voltest/frames -o movies mplumeB8n
-#$makemovie -i Voltest/frames -o movies voltest2
+#$makemovie -i Voltest/frames -o movies mplumeB8n
+$makemovie -i Voltest/frames -o movies voltest2
