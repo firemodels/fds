27a9fa9f80ce30eee9319a78194c77eb808724f8
gforney <gforney@gmail.com>
2013-01-18 15:26:25 +0000

    smv vv: add lockfile to startXserver to prevent more than one person from running this script at a time
    Former-commit-id: a187a7f0b7fd40e23f4b79320ce34a04825ad4d8 [formerly ad695371f5d41f4a4ddadf5c41bd0e37d692e747]
    Former-commit-id: d39ba2af9501dd3319d625d5b51ba187d6719990
    Former-commit-id: 91421654d7401db7085ba3709584898f1506b3c0

--- Utilities/Scripts/startXserver.sh
+++ Utilities/Scripts/startXserver.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -f
+lockfile=/tmp/startXlock
 GETNEWPORT () 
 {
+  while [ -e $lockfile ] ; do
+    sleep 5
+  done
+  touch $lockfile
+  chmod 777 $lockfile
   display_port=`id -u`
   nmatches=`ps a -e | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
-  echo display_port=$display_port nmatches=$nmatches 
   while [ $nmatches -ne 0 ] ; do
-    sleep 1
     display_port=`expr $display_port + 1`
     nmatches=`ps a -e | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
-    echo display_port=$display_port nmatches=$nmatches 
   done
 }
 
@@ -18,5 +21,5 @@ if [ "$OS" != "Darwin" ]; then
   Xvfb :$display_port -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
   export SMV_ID=$!
   export DISPLAY=:$display_port
-  sleep 8
+  rm $lockfile
 fi
