d504f7451b69d0befca0abf6087b4d1b5ec12508
drjfloyd <drjfloyd@gmail.com>
2013-01-31 13:42:44 +0000

    FDS Source: Fix output formatting issue arising from changes to Y_O2_INFTY in r14550
    Former-commit-id: bd391888e05d2b3433cd71a359f8780640538c63 [formerly 96a4030a44a4cd41f028d4f33b18e1c0c520a98e]
    Former-commit-id: 39e8c65a31b5fda932caaaa4fafa35485d564504
    Former-commit-id: 91e08b297f2808738fff2f94920c087bffa90f74

--- FDS_Source/dump.f90
+++ FDS_Source/dump.f90
@@ -2497,7 +2497,8 @@ REACTION_LOOP: DO N=1,N_REACTIONS
    WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'         
    DO NN=0,N_TRACKED_SPECIES
       IF (ABS(RN%NU(NN)) <=TWO_EPSILON_EB) CYCLE
-      WRITE(LU_OUTPUT,'(3X,A,1X,F12.6)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
+      IF (ABS(RN%NU(NN)) < 10000._EB) WRITE(LU_OUTPUT,'(3X,A,1X,F12.6)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
+      IF (ABS(RN%NU(NN)) > 10000._EB) WRITE(LU_OUTPUT,'(3X,A,1X,E12.5)') SPECIES_MIXTURE(NN)%ID,RN%NU(NN) 
    ENDDO
    WRITE(LU_OUTPUT,'(/3X,A)') 'Detailed Species'
    WRITE(LU_OUTPUT,'(A)') '   Species ID                     Stoich. Coeff.'
