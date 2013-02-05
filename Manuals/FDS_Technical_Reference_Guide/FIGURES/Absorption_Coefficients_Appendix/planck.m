b2e81d1dfb261de93427add0939cbca8b334397f
mcgratta <mcgratta@gmail.com>
2013-02-05 18:57:03 +0000

    FDS Tech Guide: Edit Appendix I, Liquid Absorption Coefficients.
    Former-commit-id: 77c2f9e293ba45ad49dc7c18f3b8d84ce8e980f7 [formerly 622995e75b9bde448255c967dbc6640995832f82]
    Former-commit-id: a435ba915938e1987e06497358ce980929bc0daf
    Former-commit-id: b7ee1ae322c9ba5cb5b56ccde0706f400262e986

--- Manuals/FDS_Technical_Reference_Guide/FIGURES/Absorption_Coefficients_Appendix/planck.m
+++
@@ -1,9 +0,0 @@
-function I=planck(Temp,eta)
-    if nargin<2
-           eta=linspace(1,1.2e6,1000);
-    end
-    % Radiation constants
-    C1=1.1910428220e-16; % W* m^2/sr
-    C2=1.438775225e-2; % m*K
-    I=((C1*eta.^3)./(exp((C2*eta)/Temp)-1)).';
-end
