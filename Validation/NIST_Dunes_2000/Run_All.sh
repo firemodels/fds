55c0b6d386959746f3b7e577b969147a6e3e615c
Kristopher Overholt <koverholt@gmail.com>
2013-02-12 21:52:28 +0000

    FDS Validation: Update HRR ramps for NIST Dunes 2000 cases based on calibration to fire room TC (t-squared ramps)
    Former-commit-id: 5fe397a8ba2620c27ae81d2c1655debee2e00053 [formerly e472328358328a643c7e5f5f1d98e33393b7a8cc]
    Former-commit-id: 14abcd7be0956c49bf4b6cc88d5ffe53e90a57ff
    Former-commit-id: afa2c7b27e982b177c0daf39bdf11473c5fa7af9

--- Validation/NIST_Dunes_2000/Run_All.sh
+++ Validation/NIST_Dunes_2000/Run_All.sh
@@ -3,25 +3,24 @@
 # This script runs the a set of Validation Cases on a linux machine with
 # a batch queuing system
 
-#!/bin/bash -f
-
 export SVNROOT=`pwd`/../..
-export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
+source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
 #export STOPFDS=1
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
-$RUNFDS $INDIR NIST_Dunes_2000_SDC02
-$RUNFDS $INDIR NIST_Dunes_2000_SDC05
-$RUNFDS $INDIR NIST_Dunes_2000_SDC07
-$RUNFDS $INDIR NIST_Dunes_2000_SDC10
-$RUNFDS $INDIR NIST_Dunes_2000_SDC15_Calibration
-$RUNFDS $INDIR NIST_Dunes_2000_SDC33
-$RUNFDS $INDIR NIST_Dunes_2000_SDC35
-$RUNFDS $INDIR NIST_Dunes_2000_SDC38_Calibration
-$RUNFDS $INDIR NIST_Dunes_2000_SDC39
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC02
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC05
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC07
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC10
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC15
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC33
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC35
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC38
+$RUNFDSMPI 4 $INDIR NIST_Dunes_2000_SDC39
