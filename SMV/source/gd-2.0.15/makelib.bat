989809ebf0fa6a2b4205d7b3e4f35edf71438eac
gforney <gforney@gmail.com>
2013-02-22 20:35:24 +0000

    smv build: corrections to gd makefile and build script

--- SMV/source/gd-2.0.15/makelib.bat
+++ SMV/source/gd-2.0.15/makelib.bat
@@ -3,4 +3,4 @@ call ..\setopts %OPTS%
 erase *.o *.obj libgd.a libgd.lib
 set target=libgd.lib
 if %COMPILER% == gcc set target=libgd.a
-make CFLAGS="-g  -DHAVE_LIBPNG -DHAVE_LIBZ -DHAVE_LIBJPEG " COMPILER=%COMPILER% SIZE=%SIZE% RM=erase -f ./makefile %target%
+make CFLAGS="-g  -DWIN32 -DHAVE_LIBPNG -DHAVE_LIBZ -DHAVE_LIBJPEG " COMPILER=%COMPILER% SIZE=%SIZE% RM=erase -f ./makefile %target%
