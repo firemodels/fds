e1f2cad854323c867df72ce6badb55be7d668e6d
gforney <gforney@gmail.com>
2013-02-22 15:42:32 +0000

    smv build: update makefile and build script for the command line windows build of jpeg library

--- SMV/source/jpeg-6b/makelib.bat
+++ SMV/source/jpeg-6b/makelib.bat
@@ -1,5 +1,6 @@
 @echo off
-
 call ..\setopts %OPTS%
-erase *.o
-make COMPILER=%COMPILER% SIZE=%SIZE% RM=erase libjpeg.a
+erase *.o *.obj libjpeg.a libjpeg.lib
+set target=libjpeg.lib
+if %COMPILER% == gcc set target=libjpeg.a
+make COMPILER=%COMPILER% SIZE=%SIZE% RM=erase -f ./makefile %target%
