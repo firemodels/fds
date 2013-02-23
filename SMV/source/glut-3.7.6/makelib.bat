42c14d71fb69982e308036302216c987288f830b
gforney <gforney@gmail.com>
2013-02-23 21:19:58 +0000

    smv build: add set _WIN32 directive for glut build

--- SMV/source/glut-3.7.6/makelib.bat
+++ SMV/source/glut-3.7.6/makelib.bat
@@ -3,4 +3,4 @@ call ..\setopts %OPTS%
 erase *.o *.obj libglutwin.a libglutwin.lib
 set target=libglutwin.lib
 if %COMPILER% == gcc set target=libglutwin.a
-make COMPILER=%COMPILER% SIZE=%SIZE% FILTERC="-D WIN32" -f ./makefile %target%
+make COMPILER=%COMPILER% SIZE=%SIZE% FILTERC="-D WIN32 -D _WIN32" -f ./makefile %target%
