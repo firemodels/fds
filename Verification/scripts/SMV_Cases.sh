4f08e62bdc74a1a371db560de0377f570f031aab
gforney <gforney@gmail.com>
2013-03-16 00:47:44 +0000

    smv vv: remove cfast test case from the case list
    Former-commit-id: 5cca072ce22a2c750b2cf778c11071405a8de157 [formerly 71c4358cf0773feb9d2beb7d306e773514d5405f]
    Former-commit-id: 326dc0e30842e3b5e9098805cec5253e00512114
    Former-commit-id: 5b91baf7c0f2067a7620c9a80839909c2367dc51

--- Verification/scripts/SMV_Cases.sh
+++ Verification/scripts/SMV_Cases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 $RUNFDS Visualization cell_test
-$RUNCFAST Visualization cfast_test
+#$RUNCFAST Visualization cfast_test
 $RUNFDS Visualization colorbar
 $RUNFDS Visualization colorconv
 $RUNFDS Visualization fed_test
