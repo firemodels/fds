15983ef6ebd0da5171049e25c441dc22340e0a4e
Kristopher Overholt <koverholt@gmail.com>
2013-03-21 05:37:02 +0000

    FDS VV Scripts: Change warning text in synthetic_eddy_method.m
    Former-commit-id: 3a2efd81453c84dcc97930dc478414ea46259687 [formerly 28fee08feeabeba0e16d65b7214d6f753050b4f8]
    Former-commit-id: 11d0cf3d24e42635d4c5d0c0e68de57b991a44b4
    Former-commit-id: e48bef289ef56c75367705c1a3b0610198a13b41

--- Utilities/Matlab/scripts/synthetic_eddy_method.m
+++ Utilities/Matlab/scripts/synthetic_eddy_method.m
@@ -62,12 +62,12 @@ print(gcf,'-dpdf',[plotdir,'sem_flat_leddy_p2'])
 % compute error
 umean_error = norm(umean-uprof)/u0/length(umean);
 if umean_error>error_tolerance
-    display(['Warning: sem_flat_leddy_p2.fds umean_error = ',umean_error])
+    display(['Matlab Warning: sem_flat_leddy_p2.fds umean_error = ',umean_error])
 end
     
 urms_error = norm(umean+urms-1.1*uprof)/u0/length(umean);
 if urms_error>error_tolerance
-    display(['Warning: sem_flat_leddy_p2.fds urms_error = ',urms_error])
+    display(['Matlab Warning: sem_flat_leddy_p2.fds urms_error = ',urms_error])
 end
 
 
@@ -123,12 +123,12 @@ print(gcf,'-dpdf',[plotdir,'sem_par_leddy_p2'])
 % compute error
 umean_error = norm(umean-uprof)/umax/length(umean);
 if umean_error>error_tolerance
-    display(['Warning: sem_par_leddy_p2.fds umean_error = ',umean_error])
+    display(['Matlab Warning: sem_par_leddy_p2.fds umean_error = ',umean_error])
 end
 
 urms_error = norm(umean+urms-1.1*uprof)/umax/length(umean);
 if urms_error>error_tolerance
-    display(['Warning: sem_par_leddy_p2.fds urms_error = ',urms_error])
+    display(['Matlab Warning: sem_par_leddy_p2.fds urms_error = ',urms_error])
 end
 
 % Atmospheric profile
@@ -183,12 +183,12 @@ print(gcf,'-dpdf',[plotdir,'sem_atm_leddy_p2'])
 % compute error
 umean_error = norm(umean-uprof)/u0/length(umean);
 if umean_error>error_tolerance
-    display(['Warning: sem_atm_leddy_p2.fds umean_error = ',umean_error])
+    display(['Matlab Warning: sem_atm_leddy_p2.fds umean_error = ',umean_error])
 end
 
 urms_error = norm(umean+urms-1.1*uprof)/u0/length(umean);
 if urms_error>error_tolerance
-    display(['Warning: sem_atm_leddy_p2.fds urms_error = ',urms_error])
+    display(['Matlab Warning: sem_atm_leddy_p2.fds urms_error = ',urms_error])
 end
 
 
