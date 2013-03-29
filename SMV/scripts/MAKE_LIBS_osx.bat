611a51f086e3b4aaa41146bd40565e7a4063399d
gforney <gforney@gmail.com>
2013-03-29 17:46:21 +0000

    smv build: add scripts to build smokeviewi libraries from the build web page

---
+++ SMV/scripts/MAKE_LIBS_osx.bat
@@ -0,0 +1,34 @@
+@echo off
+Title Building Smokeview for 32 bit Linux
+
+Rem  Windows batch file to build a release Smokeview for Linux 32.
+
+Rem setup environment variables (defining where repository resides etc) 
+
+set envfile="%userprofile%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+cd %svn_root%\smv\scripts
+
+set scriptdir=FDS-SMV/SMV/scripts
+set LIBDIR=FDS-SMV/SMV/Build/LIBS
+
+plink %svn_logon% %scriptdir%/ssh_command2.csh %osx_hostname% %LIBDIR%/lib_osx_intel_32 makelibs.sh
+
+plink %svn_logon% %scriptdir%/ssh_command2.csh %osx_hostname% %LIBDIR%/lib_osx_intel_64 makelibs.sh
+
+echo.
+echo build complete
+pause
