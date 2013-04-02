a7772df68d5dd4d154ddb3cd04a5890aa3fbcdd1
gforney <gforney@gmail.com>
2013-04-02 14:37:48 +0000

    smv build scripts: update scripts to use IFORT_COMPILER13 variable
    Former-commit-id: 2b87d1ed09bcd4bdea7cfb0d7be32504a6d88d64 [formerly 2e1aab0e6cee8ac74c178e43e38f633f84ffe589]
    Former-commit-id: 3e9590612946635e49c657427509e478d0e335ea
    Former-commit-id: a5fc6d69f3f804dd845bae3299f2c04d486f443b

--- Utilities/set_fds_path/intel_win_32/make_setfdspath.bat
+++ Utilities/set_fds_path/intel_win_32/make_setfdspath.bat
@@ -2,13 +2,12 @@
 
 Rem windows batch file to build smokeview from the command line
 
-IF "%SETUP_IFORT_COMPILER12%"=="1" GOTO envexist
+IF "%SETUP_IFORT_COMPILER%"=="1" GOTO envexist
 
-set SETUP_IFORT_COMPILER12=1
+set SETUP_IFORT_COMPILER=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER12%\bin\ifortvars" ia32
-call "%IFORT_COMPILER12%\bin\iclvars" ia32
+call "%IFORT_COMPILER13%\bin\compilervars" ia32
 if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
 :envexist
 
