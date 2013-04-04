ad54f3f4f9ac157226cc24da661b5c0f9125153f
rmcdermo <randy.mcdermott@gmail.com>
2013-04-04 22:54:51 +0000

    FDS Validation: add Purdue Run_All.sh
    Former-commit-id: 720e36a6e29f221fc60e7b71129c035c9b69b65a [formerly 01d90274e1383f6db714a33818efa165ff1d8339]
    Former-commit-id: 3d5b07026bdd20f0f2c3c94027c3a11c9af30e9b
    Former-commit-id: f58b68acad154fdeab77c3389a781f6f3f8d7046

---
+++ Validation/Purdue_Flames/Run_All.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$RUNFDS $INDIR 7p1_cm_methane_4mm
+$RUNFDS $INDIR 7p1_cm_methane_2mm  
+
+echo FDS cases submitted
