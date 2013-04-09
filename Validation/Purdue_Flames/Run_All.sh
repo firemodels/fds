17a0ccdd32ab12507c92600ad1edfc8454f36bd7
rmcdermo <randy.mcdermott@gmail.com>
2013-04-09 00:04:39 +0000

    FDS Validation: add multi-mesh Purdue cases (commented out)
    Former-commit-id: 835a1a1920265c40e035a048eb2b80846e64f5ee [formerly 972d0107d7e8cbb4654bab010b02e459b8279bae]
    Former-commit-id: 7b311312543d355d7c2433e59352138bcf602c81
    Former-commit-id: e84dbedb757895f4cfa63934c1f5ebb5093dd034

--- Validation/Purdue_Flames/Run_All.sh
+++ Validation/Purdue_Flames/Run_All.sh
@@ -3,6 +3,8 @@
 export SVNROOT=`pwd`/../..
 export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
 export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
+export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
 
@@ -12,6 +14,8 @@ export INDIR=Current_Results
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
 $RUNFDS $INDIR 7p1_cm_methane_4mm
-$RUNFDS $INDIR 7p1_cm_methane_2mm  
+$RUNFDS $INDIR 7p1_cm_methane_2mm
+#$RUNFDSMPI 16 $INDIR 7p1_cm_methane_2mm_16mesh
+#$RUNFDSMPI 128 $INDIR 7p1_cm_methane_1mm_128mesh
 
 echo FDS cases submitted
