4f00b5089b8bb24cb40dd0b3c279a3c7d9e78e77
Kristopher Overholt <koverholt@gmail.com>
2013-04-19 16:40:18 +0000

    FDS Compilation: Consolidate OpenMP targets into primary Makefile; remove deprecated tcheck target

--- FDS_Compilation/openmp_intel_linux_64/make_fds.sh
+++ FDS_Compilation/openmp_intel_linux_64/make_fds.sh
@@ -6,4 +6,4 @@ target=${dir##*/}
 source $IFORT_COMPILER/bin/ifortvars.sh $platform
 
 echo Building $target
-make -j4 VPATH="../../FDS_Source" -f ../makefile_openmp $target
+make -j4 VPATH="../../FDS_Source" -f ../makefile $target
