fdb9f72a66114ffdee722bed8e5f6122e295c1ea
rmcdermo <randy.mcdermott@gmail.com>
2013-04-24 21:11:35 +0000

    FDS Validation: migrate Lattimer Run_All.sh to qfds.sh and add ibm cases
    Former-commit-id: 5f0085c63d57045caab7ef24528c94fd0aff4c52 [formerly 1c25c483964770c3f5c3569e57fd74aa38493cf7]
    Former-commit-id: ca07691fe54b45b12c12d98c1617e5eb07ce1f9f
    Former-commit-id: 75282cc276886bdeacc0ecce3eca1e2e1f44d3bd

--- Validation/Lattimer_Tilted_Wall/Run_All.sh
+++ Validation/Lattimer_Tilted_Wall/Run_All.sh
@@ -1,23 +1,24 @@
 #!/bin/bash -f
 
 export SVNROOT=`pwd`/../..
-export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
-export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
-export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+export QFDS=/usr/local/bin/qfds.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
+# qq="-q fire80s"
+qq=
 source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
-#export STOPFDS=1
+# export STOPFDS=1
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
-#$RUNFDS $INDIR Lattimer_20_kW_0_degree_coarse
+$QFDS -r -p 27 $qq -d $INDIR Lattimer_20_kW_0_degree_coarse.fds
+$QFDS -r -p 27 $qq -d $INDIR Lattimer_20_kW_0_degree.fds
+$QFDS -r -p 27 $qq -d $INDIR Lattimer_20_kW_0_degree_fine.fds
 
-$RUNFDSMPI 27 $INDIR Lattimer_20_kW_0_degree_coarse
-$RUNFDSMPI 27 $INDIR Lattimer_20_kW_0_degree
-$RUNFDSMPI 27 $INDIR Lattimer_20_kW_0_degree_fine
+$QFDS -r $qq -d $INDIR Lattimer_20_kW_0_degree_ibm.fds
+$QFDS -r $qq -d $INDIR Lattimer_20_kW_10_degree_ibm.fds
+$QFDS -r $qq -d $INDIR Lattimer_20_kW_20_degree_ibm.fds
 
 echo FDS cases submitted
