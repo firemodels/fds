8693f653c1b0f2a9484f9a098a7535c65cd5844e
Charlies Luo <mrctkg@gmail.com>
2013-04-25 15:28:13 +0000

    FDS Verification: edits (mainly on particles) to the arch beam input files

--- Verification/Immersed_Boundary_Method/Abaqus/Arch_demo.m
+++ Verification/Immersed_Boundary_Method/Abaqus/Arch_demo.m
@@ -8,7 +8,7 @@ clear all
 
 % write FDS input file
 
-fid = fopen('Arch_demo.fds','wt');
+fid = fopen('Arch_demo2.fds','wt');
 
 head = ['&HEAD CHID=''Arch_demo'', TITLE=''Demo of Abaqus >> Matlab freeBoundary, created by Arch_demo.m'' /']; fprintf(fid,'%s\n',head);
 
@@ -16,14 +16,22 @@ fprintf(fid,'%s\n','  '); % blank line
 
 mesh = ['&MESH IJK=32,32,32, XB=-1.,1.,-1.,1.,0,1/']; fprintf(fid,'%s\n',mesh); fprintf(fid,'%s\n','  ');
 
-time = ['&TIME T_END=10./']; fprintf(fid,'%s\n',time); fprintf(fid,'%s\n','  '); % blank line
+time = ['&TIME T_END=60./']; fprintf(fid,'%s\n',time); fprintf(fid,'%s\n','  '); % blank line
 
-misc = ['&MISC IMMERSED_BOUNDARY_METHOD=1 /']; fprintf(fid,'%s\n',misc);
+misc = ['&MISC IMMERSED_BOUNDARY_METHOD=1 /']; fprintf(fid,'%s\n',misc); fprintf(fid,'%s\n','  ');
+
+dump = ['&DUMP CUTCELL_DATA_FILE=.TRUE. /']; fprintf(fid,'%s\n',dump);
 
 fprintf(fid,'%s\n','  '); % blank line
 
-surf = ['&SURF ID=''hot vent'', COLOR=''RED'', CONVECTIVE_HEAT_FLUX=100./'];  fprintf(fid,'%s\n',surf);
-surf = ['&SURF ID=''cold'', COLOR=''BLUE''/'];  fprintf(fid,'%s\n',surf);
+surf = ['&SURF ID=''hot vent'', COLOR=''RED'', CONVECTIVE_HEAT_FLUX=10./'];  fprintf(fid,'%s\n',surf);
+part = ['&PART ID=''p1'', SURF_ID=''arch surf'', STATIC=.TRUE., PROP_ID=''ball'', QUANTITIES(1)=''PARTICLE TEMPERATURE'' /']; fprintf(fid,'%s\n',part);
+prop = ['&PROP ID=''ball'', SMOKEVIEW_ID=''SPHERE'', SMOKEVIEW_PARAMETERS(1)=''D=0.01'' /']; fprintf(fid,'%s\n',prop);
+surf = ['&SURF ID=''arch surf'', COLOR=''BLUE'', EMISSIVITY=0.95, RADIUS=0.005, GEOMETRY=''SPHERICAL'' /',];  fprintf(fid,'%s\n',surf);
+
+fprintf(fid,'%s\n','  '); % blank line
+
+geom = ['&GEOM ID=''arch'', SHAPE=''COMPLEX'', SURF_ID=''cold'',DT_GEOC=1., GEOC_FILENAME=''Arch_demo_bending_01.gc'' /']; fprintf(fid,'%s\n',geom);
 
 fprintf(fid,'%s\n','  '); % blank line
 
@@ -45,7 +53,7 @@ slcf = ['&SLCF PBX=0, QUANTITY=''TEMPERATURE'', VECTOR=.TRUE. /'];  fprintf(fid,
 
 fprintf(fid,'%s\n','  '); % blank line
 
-bnde = ['&BNDE QUANTITY=''GAS TEMPERATURE'' /'];  fprintf(fid,'%s\n',bnde);  fprintf(fid,'%s\n','  '); % blank line
+bnde = ['&BNDE QUANTITY=''WALL TEMPERATURE'' /'];  fprintf(fid,'%s\n',bnde);  fprintf(fid,'%s\n','  '); % blank line
 
 % write VERT lines
 
@@ -61,8 +69,9 @@ TR = TriRep(Tet,X);
 FF = freeBoundary(TR); % freeBoundary uses the same ordering convention as FDS: right-hand rule for outward normal
 
 for i=1:length(FF(:,1))
-    surf_id='''cold''';
-    face = ['&FACE N=',num2str(FF(i,1)),',',num2str(FF(i,2)),',',num2str(FF(i,3)),', SURF_ID=',surf_id,' /']; fprintf(fid,'%s\n',face);
+    surf_id='''arch surf''';
+    part_id='''p1''';
+    face = ['&FACE N=',num2str(FF(i,1)),',',num2str(FF(i,2)),',',num2str(FF(i,3)),', SURF_ID=',surf_id, ',PART_ID=', part_id, ' /' ]; fprintf(fid,'%s\n',face);
 end
 
 fprintf(fid,'%s\n','  '); % blank line
