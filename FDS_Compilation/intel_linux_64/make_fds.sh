38d3f04756ec578fae6bb40672a0fa52db021139
gforney <gforney@gmail.com>
2013-04-30 20:10:11 +0000

    fds build: change ifortvars.sh to compilervars.sh in various make_fds.sh scripts to be compatible with the new version of the Intel compilers

--- FDS_Compilation/intel_linux_64/make_fds.sh
+++ FDS_Compilation/intel_linux_64/make_fds.sh
@@ -3,7 +3,7 @@ platform=intel64
 dir=`pwd`
 target=${dir##*/}
 
-source $IFORT_COMPILER/bin/ifortvars.sh $platform
+source $IFORT_COMPILER/bin/compilervars.sh $platform
 
 echo Building $target
 make -j4 VPATH="../../FDS_Source" -f ../makefile $target
