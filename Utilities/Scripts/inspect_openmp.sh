1059421b4a03ee2ab0df8a07c86df7558a200f4c
Kristopher Overholt <koverholt@gmail.com>
2013-04-30 23:50:26 +0000

    VV Scripts: Save inspect_openmp results to Scripts dir
    Former-commit-id: c8177cdb489ee3ac5975dbe871c8a5c04331cad0 [formerly c81aab0f891668286a9ad4e33396785af126cc6b]
    Former-commit-id: 1fde3bc9e99c828b62aaa52d32a6001256da3cb1
    Former-commit-id: c138d9c72709ba4ec905861f389aaac4065a3682

--- Utilities/Scripts/inspect_openmp.sh
+++ Utilities/Scripts/inspect_openmp.sh
@@ -10,4 +10,4 @@ export SVNROOT=`pwd`/../..
 source /opt/intel/inspector_xe/inspxe-vars.sh quiet
 
 cd $SVNROOT/Verification/Timing_Benchmarks
-inspxe-cl -collect ti3 -result-dir r000ti3 -- $SVNROOT/FDS_Compilation/openmp_intel_linux_64_db/fds_openmp_intel_linux_64_db bench2.fds
+inspxe-cl -collect ti3 -result-dir $SVNROOT/Utilities/Scripts/inspect_openmp_ti3 -- $SVNROOT/FDS_Compilation/openmp_intel_linux_64_db/fds_openmp_intel_linux_64_db bench2.fds
