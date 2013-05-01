697220b5714b448723eb79bd26e03c7bc5cd49bf
Kristopher Overholt <koverholt@gmail.com>
2013-05-01 19:37:54 +0000

    VV Scripts: Add inspector reporting script; edits to Firebot Stage 2c OpenMP checking
    Former-commit-id: 23795a2d3f1617e25c1656b747887d666c7cfd97 [formerly aa931c4a4030cb3cade9f6699a6700cb2aac5925]
    Former-commit-id: 66cbfc79678114cf6d643dc18e8845e73e889d02
    Former-commit-id: 15d9f462de7ec6e0d13d2b93dd2bce3c8cfaa7ae

---
+++ Utilities/Scripts/inspect_report.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# inspect_report.sh
+# Kristopher Overholt
+# 5/1/2013
+
+# Report results from thread checker
+
+RESULT_DIR=$SVNROOT/Utilities/Scripts/inspect_openmp_ti3
+REPORT_TYPE=problems
+
+function usage {
+echo "inspect_report.sh [ -d result-dir -r report-type ]"
+echo "Report results from thread checker"
+echo ""
+echo "Options"
+echo "-d result-dir - directory that contains thread checker results"
+echo "-r report-type - type of report: problems [or] observations"
+exit
+}
+
+while getopts 'd:hr:' OPTION
+do
+case $OPTION in
+  d)
+   cases="$OPTARG"
+   ;;
+  h)
+   usage;
+   ;;
+  r)
+   cases="$OPTARG"
+   ;;
+esac
+done
+
+export SVNROOT=`pwd`/../..
+source /opt/intel/inspector_xe/inspxe-vars.sh quiet
+
+inspxe-cl -report $REPORT_TYPE -result-dir $RESULT_DIR
