1ea35c78574c79a91daaa926fd868e86814cd2b0
rmcdermo <randy.mcdermott@gmail.com>
2013-05-07 15:33:03 +0000

    FDS Validation: clean up Sandia input files
    Former-commit-id: 603491e5b501b6a7206c629701ffe95a68e2e4a8 [formerly 41895714c287db9c55e15d71d7b89135cb44d1ae]
    Former-commit-id: 15d5d27c3bab2835948ed95bca3d92d2d0fbaa6d
    Former-commit-id: 35abb82dd3d0ee49042c381c5a147a297a4beafd

--- Validation/Sandia_Plumes/Run_All.sh
+++ Validation/Sandia_Plumes/Run_All.sh
@@ -1,34 +1,32 @@
 #!/bin/bash -f
 
-# This script runs the a set of Validation Cases on a linux machine with
-# a batch queuing system
-
 export SVNROOT=`pwd`/../..
-export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
-export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+export QFDS=/usr/local/bin/qfds.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
+# qq="-q fire80s"
+qq=
 source ~/.bashrc_fds intel64
 
 # uncomment following line to stop all cases
-#export STOPFDS=1
+# export STOPFDS=1
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
-$RUNFDSMPI 16 $INDIR Sandia_He_1m_dx6cm
-$RUNFDSMPI 16 $INDIR Sandia_He_1m_dx3cm
-$RUNFDSMPI 16 $INDIR Sandia_He_1m_dx1p5cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test14_dx6cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test14_dx3cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test14_dx1p5cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test17_dx6cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test17_dx3cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test17_dx1p5cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test24_dx6cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test24_dx3cm
-$RUNFDSMPI 16 $INDIR Sandia_CH4_1m_Test24_dx1p5cm
-$RUNFDSMPI 16 $INDIR Sandia_H2_1m_Test35_dx6cm
-$RUNFDSMPI 16 $INDIR Sandia_H2_1m_Test35_dx3cm
-$RUNFDSMPI 16 $INDIR Sandia_H2_1m_Test35_dx1p5cm
+$QFDS -r -p 16 $qq -d $INDIR Sandia_He_1m_dx6cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_He_1m_dx3cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_He_1m_dx1p5cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test14_dx6cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test14_dx3cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test14_dx1p5cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test17_dx6cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test17_dx3cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test17_dx1p5cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test24_dx6cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test24_dx3cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_CH4_1m_Test24_dx1p5cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_H2_1m_Test35_dx6cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_H2_1m_Test35_dx3cm.fds
+$QFDS -r -p 16 $qq -d $INDIR Sandia_H2_1m_Test35_dx1p5cm.fds
 
 echo FDS cases submitted
