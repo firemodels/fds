b540979dd1488fbf14d79e2432f82b3910686cb8
drjfloyd <drjfloyd@gmail.com>
2013-05-09 18:01:15 +0000

    FDS Source: Fixing line length
    Former-commit-id: 0549346fdaaa142e774d99880a7ad5375bd54335 [formerly 0b9fbc607216bcde3ac61434e7bc07207efd3592]
    Former-commit-id: 8fdc9ca1e75679048c9fc889819af7dc25db9973
    Former-commit-id: 6c0006cdc6b90fca67f144e0e6cbab81ef3efd90

--- FDS_Source/read.f90
+++ FDS_Source/read.f90
@@ -8855,7 +8855,8 @@ READ_DEVC_LOOP: DO NN=1,N_DEVC_READ
 
       ! Determine if the DEVC is a TIME or LINE device
 
-      IF (QUANTITY=='TIME' .AND. NO_UPDATE_DEVC_ID=='null' .AND. NO_UPDATE_CTRL_ID=='null' ) OUTPUT = .FALSE. ! Don't print out clocks
+      ! Don't print out clocks
+      IF (QUANTITY=='TIME' .AND. NO_UPDATE_DEVC_ID=='null' .AND. NO_UPDATE_CTRL_ID=='null' ) OUTPUT = .FALSE.
 
       IF (POINTS==1 .AND. OUTPUT)      N_DEVC_TIME = N_DEVC_TIME + 1
       IF (POINTS>1 .AND. I_POINT==1)   N_DEVC_LINE = N_DEVC_LINE + 1
