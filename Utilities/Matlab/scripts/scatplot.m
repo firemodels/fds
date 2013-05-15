1ece58e976ad3b063d47b77788627cbe0cc5ad3b
Kristopher Overholt <koverholt@gmail.com>
2013-05-15 23:49:35 +0000

    VV Scripts: More edits to histogram output in scatplot.m

--- Utilities/Matlab/scripts/scatplot.m
+++ Utilities/Matlab/scripts/scatplot.m
@@ -322,30 +322,48 @@ for j=2:length(Q);
         display(['Printing scatter plot ',num2str(j),'...'])
         print(gcf,'-dpdf',[plotdir,Plot_Filename])
         
-        % Print histogram of ln(M/E) and normal distribution
-        [normality,p] = lillietest(log(nonzeros(Predicted_Metric))-log(nonzeros(Measured_Metric)));
-        if normality == 0
-            normality_test = 'Pass';
-        else
-            normality_test = 'Fail';
+        %%% Print histogram of ln(M/E) and normal distribution
+        % Normality test
+        ln_M_E = log(nonzeros(Predicted_Metric))-log(nonzeros(Measured_Metric));
+        % Normality test requires at least 4 observations
+        if size(ln_M_E,1) >= 4
+            [normality,p] = lillietest(ln_M_E);
+            if normality == 0
+                normality_test = 'Pass';
+            else
+                normality_test = 'Fail';
+            end
+            % Plot histogram
+            figure
+            box on
+            hold on
+            [n,xout] = hist(log(Predicted_Values)-log(Measured_Values),10);
+            bar(xout,n,'LineWidth',1,'FaceColor',[0.7,0.7,0.7])
+            % Plot normal distribution
+            ix = xout(1):1e-3:xout(end);
+            mu = mean(log(Predicted_Values)-log(Measured_Values));
+            sd = std(log(Predicted_Values)-log(Measured_Values));
+            iy = pdf('normal', ix, mu, sd);
+            plot(ix,iy*trapz(xout,n),'k','LineWidth',2);
+            % Additional plot content
+            y_lim = get(gca,'YLim') * 1.25;
+            set(gca,'YLim',y_lim);
+            xlabel('Interval Number','Interpreter',Font_Interpreter,'FontSize',Scat_Label_Font_Size,'FontName',Font_Name)
+            ylabel('Number of Data Points','Interpreter',Font_Interpreter,'FontSize',Scat_Label_Font_Size,'FontName',Font_Name)
+            set(gca,'Units','inches')
+            set(gca,'FontName','Times')
+            set(gca,'FontSize',12)
+            set(gca,'XTick',xout,'XTickLabel',{'1','2','3','4','5','6','7','8','9','10'})
+            set(gca,'Position',[Scat_Plot_X,Scat_Plot_Y,Scat_Plot_Width,Scat_Plot_Height])
+            text(0.03, 0.95,Scatter_Plot_Title,'FontSize',Scat_Title_Font_Size,'FontName','Times','Interpreter',Font_Interpreter,'Units','normalized')
+            text(0.03, 0.90,['Normality Test: ',normality_test],'FontSize',Scat_Title_Font_Size,'FontName','Times','Interpreter',Font_Interpreter,'Units','normalized')
+            set(gcf,'Visible','on');
+            set(gcf,'PaperUnits','inches');
+            set(gcf,'PaperSize',[PDF_Paper_Width Scat_Paper_Height]);
+            set(gcf,'PaperPosition',[0 0 PDF_Paper_Width Scat_Paper_Height]);
+            print(gcf,'-dpdf',[plotdir,[Plot_Filename, '_Histogram']])
+            hold off
         end
-        figure
-        [n,xout] = hist(log(Predicted_Values)-log(Measured_Values),10);
-        bar(xout,n)
-        xlabel('Interval Number','Interpreter',Font_Interpreter,'FontSize',Scat_Label_Font_Size,'FontName',Font_Name)
-        ylabel('Number of Data Points','Interpreter',Font_Interpreter,'FontSize',Scat_Label_Font_Size,'FontName',Font_Name)
-        set(gca,'Units','inches')
-        set(gca,'FontName','Times')
-        set(gca,'FontSize',12)
-        set(gca,'XTick',xout,'XTickLabel',{'1','2','3','4','5','6','7','8','9','10'})
-        set(gca,'Position',[Scat_Plot_X,Scat_Plot_Y,Scat_Plot_Width,Scat_Plot_Height])
-        text(0.03, 0.95,Scatter_Plot_Title,'FontSize',Scat_Title_Font_Size,'FontName','Times','Interpreter',Font_Interpreter,'Units','normalized')
-        text(0.03, 0.90,['Normality Test: ',normality_test],'FontSize',Scat_Title_Font_Size,'FontName','Times','Interpreter',Font_Interpreter,'Units','normalized')
-        set(gcf,'Visible','on');
-        set(gcf,'PaperUnits','inches');
-        set(gcf,'PaperSize',[PDF_Paper_Width Scat_Paper_Height]);
-        set(gcf,'PaperPosition',[0 0 PDF_Paper_Width Scat_Paper_Height]);
-        print(gcf,'-dpdf',[plotdir,[Plot_Filename, '_Histogram']])
         
         % Perform this code block for FDS validation scatterplot output
         if stats_output == 2
