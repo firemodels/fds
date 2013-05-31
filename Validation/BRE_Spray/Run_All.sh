a92bcaed7e0d3ab7c8ad69875b23c0cbb6675e33
gforney <gforney@gmail.com>
2013-05-31 17:49:09 +0000

    FDS vv: update BRE_Spray Run_All script to use qfds.sh
    Former-commit-id: 031eaf3c45347baa6883ab70001901ecb0ef8ec7 [formerly d926c75b5ad1c14ed8e364c6c44fb54b78daff3a]
    Former-commit-id: 94606163589021c0a61be965bf83d2eabe6be4ac
    Former-commit-id: f785d8de7182b35de94be1da9b87b6f8dba5d7fc

--- Validation/BRE_Spray/Run_All.sh
+++ Validation/BRE_Spray/Run_All.sh
@@ -1,39 +1,40 @@
 #!/bin/bash -f
 
 export SVNROOT=`pwd`/../..
-export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
-export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export QFDS=/usr/local/bin/qfds.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
+# qq="-q fire80s"
+#qq=
 
 # uncomment following line to stop all cases
-# export STOPFDS=1
+export STOPFDS=1
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
-$RUNFDS $INDIR BRE_Spray_A_1 
-$RUNFDS $INDIR BRE_Spray_A_2
-$RUNFDS $INDIR BRE_Spray_A_3 
-$RUNFDS $INDIR BRE_Spray_A_4 
-$RUNFDS $INDIR BRE_Spray_A_5 
-$RUNFDS $INDIR BRE_Spray_A_6 
-$RUNFDS $INDIR BRE_Spray_A_7 
-$RUNFDS $INDIR BRE_Spray_A_8
-$RUNFDS $INDIR BRE_Spray_B_1 
-$RUNFDS $INDIR BRE_Spray_B_2
-$RUNFDS $INDIR BRE_Spray_B_3 
-$RUNFDS $INDIR BRE_Spray_B_4 
-$RUNFDS $INDIR BRE_Spray_B_5 
-$RUNFDS $INDIR BRE_Spray_B_6 
-$RUNFDS $INDIR BRE_Spray_B_7 
-$RUNFDS $INDIR BRE_Spray_B_8
-$RUNFDS $INDIR BRE_Spray_D_1 
-$RUNFDS $INDIR BRE_Spray_D_2
-$RUNFDS $INDIR BRE_Spray_D_3 
-$RUNFDS $INDIR BRE_Spray_D_4 
-$RUNFDS $INDIR BRE_Spray_D_5 
-$RUNFDS $INDIR BRE_Spray_D_6 
-$RUNFDS $INDIR BRE_Spray_D_7 
-$RUNFDS $INDIR BRE_Spray_D_8
+$QFDS -r $qq -d $INDIR BRE_Spray_A_1.fds
+$QFDS -r $qq -d $INDIR BRE_Spray_A_2.fds
+$QFDS -r $qq -d $INDIR BRE_Spray_A_3.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_A_4.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_A_5.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_A_6.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_A_7.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_A_8.fds
+$QFDS -r $qq -d $INDIR BRE_Spray_B_1.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_B_2.fds
+$QFDS -r $qq -d $INDIR BRE_Spray_B_3.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_B_4.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_B_5.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_B_6.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_B_7.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_B_8.fds
+$QFDS -r $qq -d $INDIR BRE_Spray_D_1.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_D_2.fds
+$QFDS -r $qq -d $INDIR BRE_Spray_D_3.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_D_4.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_D_5.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_D_6.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_D_7.fds 
+$QFDS -r $qq -d $INDIR BRE_Spray_D_8.fds
  
 echo FDS cases submitted
