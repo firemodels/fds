d9eb7367bff183908e453ae8e5c9039b34287fdc
gforney <gforney@gmail.com>
2013-05-31 17:26:14 +0000

    FDS vv: update ATF Run_All script to use qfds.sh

--- Validation/ATF_Corridors/Run_All.sh
+++ Validation/ATF_Corridors/Run_All.sh
@@ -4,20 +4,20 @@
 # a batch queuing system
 
 export SVNROOT=`pwd`/../..
-export FDSMPI=$SVNROOT/FDS_Compilation/mpi_intel_linux_64/fds_mpi_intel_linux_64
-export RUNFDSMPI=$SVNROOT/Utilities/Scripts/runfdsmpi.sh
+export QFDS=/usr/local/bin/qfds.sh
 export BASEDIR=`pwd`
 export INDIR=Current_Results
-source ~/.bashrc_fds intel64
+# qq="-q fire80s"
+qq=
 
 # uncomment following line to stop all cases
 #export STOPFDS=1
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
-$RUNFDSMPI 3 $INDIR ATF_Corridors_050_kW
-$RUNFDSMPI 3 $INDIR ATF_Corridors_100_kW
-$RUNFDSMPI 3 $INDIR ATF_Corridors_240_kW
-$RUNFDSMPI 3 $INDIR ATF_Corridors_250_kW
-$RUNFDSMPI 3 $INDIR ATF_Corridors_500_kW
-$RUNFDSMPI 3 $INDIR ATF_Corridors_Mix_kW
+$QFDS -r -p 3 -d $INDIR ATF_Corridors_050_kW.fds
+$QFDS -r -p 3 -d $INDIR ATF_Corridors_100_kW.fds
+$QFDS -r -p 3 -d $INDIR ATF_Corridors_240_kW.fds
+$QFDS -r -p 3 -d $INDIR ATF_Corridors_250_kW.fds
+$QFDS -r -p 3 -d $INDIR ATF_Corridors_500_kW.fds
+$QFDS -r -p 3 -d $INDIR ATF_Corridors_Mix_kW.fds
