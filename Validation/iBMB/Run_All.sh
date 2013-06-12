6d29b7e280299c3a30b0379e5ef887e7a2518448
mcgratta <mcgratta@gmail.com>
2013-06-12 19:11:06 +0000

    FDS Validation: Add iBMB data.
    Former-commit-id: 56052aa6303c4d728579b63a44f472a485963a6d [formerly eae04a731a9558e21291761a37e4217931aa5154]
    Former-commit-id: 87aa9fb7efe9b6ca375a720ba80f63c059cab1fe
    Former-commit-id: f55568137f8d5543f53dd6905b2f4facea37283c

---
+++ Validation/iBMB/Run_All.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -f
+
+export SVNROOT=`pwd`/../..
+export FDS=$SVNROOT/FDS_Compilation/intel_linux_64/fds_intel_linux_64
+export RUNFDS=$SVNROOT/Utilities/Scripts/runfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$RUNFDS $INDIR ICFMP4_01
+$RUNFDS $INDIR ICFMP5_04
+
+echo FDS cases submitted
