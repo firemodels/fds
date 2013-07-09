791912e97a70fec9c6dd704506b85c72250738c0
Kristopher Overholt <koverholt@gmail.com>
2013-07-09 23:02:58 +0000

    FDS Guides: Add check for "Misplaced" LaTeX error to all guides
    Former-commit-id: 72482f9b84d67131b36614f41ac2c3b2890a54fb [formerly 1d9d243af8e9df3c887f80d0e7318309a6251274]
    Former-commit-id: 337cb4692aa4107eacc58d37f4d0ea5e29aadda6
    Former-commit-id: 12283fd91ee75ce4156e2a79fe159674553950fc

--- Manuals/FDS_Verification_Guide/make_guide.sh
+++ Manuals/FDS_Verification_Guide/make_guide.sh
@@ -12,13 +12,13 @@ pdflatex -interaction nonstopmode FDS_Verification_Guide &> FDS_Verification_Gui
 pdflatex -interaction nonstopmode FDS_Verification_Guide &> FDS_Verification_Guide.err
 
 # Scan and report any errors in the LaTeX build process
-if [[ `grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted" -I FDS_Verification_Guide.err | grep -v "xpdf supports version 1.5"` == "" ]]
+if [[ `grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted|Misplaced" -I FDS_Verification_Guide.err | grep -v "xpdf supports version 1.5"` == "" ]]
    then
       # Continue along
       :
    else
       echo "LaTeX errors detected:"
-      grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted" -I FDS_Verification_Guide.err | grep -v "xpdf supports version 1.5"
+      grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted|Misplaced" -I FDS_Verification_Guide.err | grep -v "xpdf supports version 1.5"
       clean_build=0
 fi
 
--- Manuals/SMV_Verification_Guide/make_guide.sh
+++ Manuals/SMV_Verification_Guide/make_guide.sh
@@ -12,13 +12,13 @@ pdflatex -interaction nonstopmode SMV_Verification_Guide &> SMV_Verification_Gui
 pdflatex -interaction nonstopmode SMV_Verification_Guide &> SMV_Verification_Guide.err
 
 # Scan and report any errors in the LaTeX build process
-if [[ `grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted" -I SMV_Verification_Guide.err | grep -v "xpdf supports version 1.5"` == "" ]]
+if [[ `grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted|Misplaced" -I SMV_Verification_Guide.err | grep -v "xpdf supports version 1.5"` == "" ]]
    then
       # Continue along
       :
    else
       echo "LaTeX errors detected:"
-      grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted" -I SMV_Verification_Guide.err | grep -v "xpdf supports version 1.5"
+      grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted|Misplaced" -I SMV_Verification_Guide.err | grep -v "xpdf supports version 1.5"
       clean_build=0
 fi
 
