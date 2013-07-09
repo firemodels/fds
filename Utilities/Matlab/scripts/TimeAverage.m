6f5b6aa4975e2301f43596ae22d21f764fae02e0
Anna Matala <anskima@gmail.com>
2013-07-09 11:36:54 +0000

    FDS Validation: CHRISTIFIRE: Time and temperature averaged experimental results (5 s and 5 K) and a script to plot the results.
    Former-commit-id: ac7e72749875c4d5a0f69bc6f743720d75859d1b [formerly 0657aa9c4b33e99c1b1ee2279e01db16f425a39f]
    Former-commit-id: 1f5c51f06f595ad7c17087265bf3675de9097c2c
    Former-commit-id: c98aaaa4c6c5c07114ce8d0bb74e0e93bc6520c9

--- Utilities/Matlab/scripts/TimeAverage.m
+++ Utilities/Matlab/scripts/TimeAverage.m
@@ -1,5 +1,6 @@
 %T. Myers
-%6-07-2010
+%6-07-2010 
+%(Modified by A. Matala 07-09-2013)
 %TimeAverage.m
 %
 %Input: in = file_name.csv; Intended to be used with a csv format file. The
@@ -30,17 +31,52 @@ f = input('Please specify the factor by which you would like to reduce the data.
 %Sizes the data file, and creates a new matrix to populate with the time
 %averaged data
 [v,u]=size(M);
-N=zeros(((v./f)+1),u);
 
+%Interpolate to make dt = 1 s
+%First, make sure that the values of time column are distinct
+d_indx = find(diff(M(:,1))>0);
+
+M2(:,1) = [ceil(M(d_indx(1),1)):floor(M(d_indx(length(d_indx)),1))];
+for i = 2:u
+   M2(:,i) = interp1(M(d_indx,1),M(d_indx,i),M2(:,1));
+end
+
+N=zeros(floor(length(M2(:,1))/f)-1,u);
 %Time averages the data, first creating the time row, then creating a zero
 %point, then running the averages.
+N(1,:) = M2(1,:);
+t_min = N(1,1);
 for m =2:u
-for n =1:(v./f)
-    N(1,1)=0;
-    N(n+1,1)=((f)*(n));
-    N(1,m)=M(1,m);
-    N(n+1,m)=mean(M((1+(f)*(n-1)):((f)*n),m));
+for n =1:length(N(:,1))
+%     N(1,1)=0;
+%     N(n+1,1)=((f)*(n));
+%     N(1,m)=M2(1,m);
+%     N(n+1,m)=mean(M2((1+(f)*(n-1)):((f)*n),m));
+    N(n,1) = t_min + f*n;
+    
+    if isequal(rem(f,2),0) %even numbers
+        N(n,m) = mean(M2((f*n-f/2+1):(f*n+f/2),m));
+    
+    else % odd numbers
+        N(n,m) = mean(M2((f*n-floor(f/2)):(f*n+floor(f/2)),m));
+    end
 end
+end
+
+%round the results to requested presision
+p = input('Please specify the presision of the results.\n If the columns have different presision, give them in brackets []. \n :');
+if isequal(length(p),1)
+    N = round(N./p).*p;
+else
+    for i = 1:length(p)
+       N(:,i) =  round(N(:,i)./p(i)).*p(i);
+    end
+end
+
+
+for i = 2:u
+    figure;
+    plot(M(:,1),M(:,i),N(:,1),N(:,i))
 end
 
 %Re-reads the data file, this time to acquire the text data. 
