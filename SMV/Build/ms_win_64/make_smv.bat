c92766433ea09576ee9dbb07f2b3da9b06943b86
gforney <gforney@gmail.com>
2013-07-20 20:46:23 +0000

    smv build: add a ms_win_64 makefile entry for a 64  bit windows smokeview build that uses the MS C/C++ compilers (available with VIsual Studio)

---
+++ SMV/Build/ms_win_64/make_smv.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set SMV_TESTFLAG=
+set SMV_TESTSTRING=
+
+IF "%COMPILERS_DEFINED%"=="1" GOTO envexist
+
+  set %COMPILERS_DEFINED%=1
+
+  call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall" x86_amd64
+  call "%IFORT_COMPILER13%\bin\compilervars" intel64
+:envexist
+
+if "%1" NEQ "-t" goto endif
+  set SMV_TESTFLAG=-D pp_BETA
+  set SMV_TESTSTRING=test_
+:endif
+
+Rem erase *.obj
+make -j4 SMV_TESTFLAG="%SMV_TESTFLAG%" SMV_TESTSTRING="%SMV_TESTSTRING%" -f ..\Makefile ms_win_64
+pause
