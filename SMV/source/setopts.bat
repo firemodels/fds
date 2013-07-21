ed751ba4865067d79f4c7111f99eef9886431293
gforney <gforney@gmail.com>
2013-07-21 23:08:43 +0000

    smv build: add option to build smokeview required libraries using the MS C/C++ compilers (available with VIsual Studio)

--- SMV/source/setopts.bat
+++ SMV/source/setopts.bat
@@ -1,24 +1,38 @@
 @echo off
-
 set SIZE=-m32
 set SIZE2=ia32
+
 set COMPILER=gcc
 set COMPILER2=g++
 
+set MSCOMPILER=X86
+
 IF "%1" NEQ "g" SET COMPILER=icl
 IF "%1" NEQ "g" SET COMPILER2=icl
+
+IF "%1" EQU "m" SET COMPILER=cl
+IF "%1" EQU "m" SET COMPILER2=cl
+
 IF "%2" NEQ "3" SET SIZE=-m64
 IF "%2" NEQ "3" SET SIZE2=intel64
 
+IF "%2" NEQ "3" SET MSCOMPILER=x86_amd64
+
 IF "%COMPILER%" == "icl" SET SIZE=
-IF "%COMPILER%" NEQ "icl" GOTO label1
+IF "%COMPILER%" == "cl" SET SIZE=
 
-IF "%SETUP_IFORT_COMPILER%" == "1" GOTO envexist
+IF "%COMPILER%" NEQ "cl" GOTO MSenvexist
 
-set SETUP_IFORT_COMPILER=1
+IF "%MSCOMPILERS_DEFINED%" EQU "1" GOTO MSenvexist
+echo Setting up MS compiler environment
+set MSCOMPILERS_DEFINED=1
+call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall" %MSCOMPILER3%
+GOTO Ienvexist
+:MSenvexist
 
-echo Setting up compiler environment
+IF "%COMPILER%" NEQ "icl" GOTO Ienvexist
+IF "%ICOMPILERS_DEFINED%" EQU "1" GOTO Ienvexist
+echo Setting up Intel compiler environment
+set ICOMPILERS_DEFINED=1
 call "%IFORT_COMPILER13%\bin\compilervars" %SIZE2%
-:envexist
-
-:label1
+:Ienvexist
