e9b5c642fc5559acb97857ffc965fa1209ff495a
gforney <gforney@gmail.com>
2013-07-29 19:27:47 +0000

    smokeview source:  bump version number to 6.1.1

--- SMV/source/smokeview/options.h
+++ SMV/source/smokeview/options.h
@@ -18,10 +18,10 @@
 #undef pp_release
 #endif
 // comment the following line when building an unofficial release
-//#define pp_OFFICIAL_RELEASE
+#define pp_OFFICIAL_RELEASE
 #ifdef pp_release
 #ifdef pp_OFFICIAL_RELEASE
-#define PROGVERSION "6.1.0"
+#define PROGVERSION "6.1.1"
 #else
 #define PROGVERSION "Unofficial release"
 #endif
