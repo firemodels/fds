7f8c259663bd17dfd32399244b855991dafe5083
gforney <gforney@gmail.com>
2013-07-30 13:03:14 +0000

    FDS vv: add a couple of validation cases from the USFS
    Former-commit-id: faf1f1d3a9f2d3968d8604b305eed437ec661c02 [formerly bd5a49565a8f702e530ac875a3e2334c14d48577]
    Former-commit-id: 853c8c6293952735a19394d16da1b0372bd30211
    Former-commit-id: 8509ea5af56c517533ca9075ce7a23f53c46cead

---
+++ Validation/USFS_Stick/Run_All.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export SVNROOT=`pwd`/../..
+export QFDS=/usr/local/bin/qfds.sh
+export BASEDIR=`pwd`
+export INDIR=Current_Results
+# qq="-q fire80s"
+qq=
+
+# uncomment following line to stop all cases
+# export STOPFDS=1
+
+/bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
+
+$QFDS -r $qq -d $INDIR thermthin_sphere.fds 
+$QFDS -r $qq -d $INDIR thermthick_sphere.fds 
+
+echo FDS cases submitted
