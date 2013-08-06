41bbdc23bb840c01c71ab75e5b1468837b691a96
Kristopher Overholt <koverholt@gmail.com>
2013-08-06 19:46:40 +0000

    VV Scripts: Refactor and generalize scatplot and dataplot scripts; add NRC style options for scatter plots

--- Utilities/Matlab/FDS_validation_script.m
+++ Utilities/Matlab/FDS_validation_script.m
@@ -1,22 +1,22 @@
 % McGrattan
 % March 21, 2011
-% master_validation_script.m
+% FDS_validation_script.m
 %
 % This script creates the plots that are included in the FDS Validation 
 % Guide. It consists of calls to other scripts contained within the
 % subfolder called "scripts". 
 %
 % The most important script is called dataplot. It reads the file called
-% validation_data_config_matlab.csv and generates 1000+ plots. If you
+% FDS_validation_dataplot_inputs.csv and generates 1000+ plots. If you
 % want to process only some of these plots, comment out the other 
 % scripts and change the data plot line as follows:
 %
-% [saved_data,drange] = dataplot(cfil,vdir,plotdir,[a:b]);
+% [saved_data,drange] = dataplot(Dataplot_Inputs_File,Validation_Dir,Manuals_Dir,[a:b]);
 %
 % where a and b are the lines in the .csv file you want to process.
 % Alternatively, you can specify the "Dataname" you want:
 %
-% [saved_data,drange] = dataplot(cfil,vdir,plotdir,'WTC');
+% [saved_data,drange] = dataplot(Dataplot_Inputs_File,Validation_Dir,Manuals_Dir,'WTC');
 %
 % In this case, all the WTC results will be plotted. 
 
@@ -34,15 +34,29 @@ sippola_aerosol_deposition
 
 % dataplot creates most of the plots for the Validation Guide. It must be run before scatplot, which makes the scatter plots.
 
-cfil = [pwd,'/FDS_validation_dataplot_inputs.csv'];
-vdir = [pwd,'/../../Validation/'];
-plotdir = [pwd,'/../../Manuals/'];
-qfil = [pwd,'/FDS_validation_scatterplot_inputs.csv'];
-output_file = [pwd,'/FDS_validation_scatterplot_output.csv'];
+Dataplot_Inputs_File = [pwd,'/FDS_validation_dataplot_inputs.csv'];
+Working_Dir = [pwd, '/../../Validation/'];
+Manuals_Dir = [pwd, '/../../Manuals/'];
+Scatterplot_Inputs_File = [pwd, '/FDS_validation_scatterplot_inputs.csv'];
+Output_File = [pwd, '/FDS_validation_scatterplot_output.csv'];
+Stats_Output = 2;
+Statistics_Tex_Output = [pwd, '../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_statistics.tex'];
+Histogram_Tex_Output = [pwd, '../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_histograms.tex'];
 
-[saved_data,drange] = dataplot(cfil,vdir,plotdir);
-scatplot(saved_data,drange,qfil,plotdir,output_file)
+NRC_Options = true;
+Append_To_Scatterplot_Title = '';
 
+[saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir);
+scatplot(saved_data, drange, ...
+         'Scatterplot_Inputs_File', Scatterplot_Inputs_File, ...
+         'Manuals_Dir', Manuals_Dir, ...
+         'Output_File', Output_File, ...
+         'Stats_Output', Stats_Output, ...
+         'Statistics_Tex_Output', Statistics_Tex_Output, ...
+         'Histogram_Tex_Output', Histogram_Tex_Output, ...
+         'NRC_Options', NRC_Options, ...
+         'Append_To_Scatterplot_Title', Append_To_Scatterplot_Title)
+     
 % Miscellaneous other scripts for special cases
 
 beyler_hood
--- Utilities/Matlab/FDTs_validation_script.m
+++ Utilities/Matlab/FDTs_validation_script.m
@@ -1,22 +1,22 @@
 % McGrattan
 % March 21, 2011
-% master_validation_script.m
+% FDTs_validation_script.m
 %
-% This script creates the plots that are included in the FDS Validation 
+% This script creates the plots that are included in the FDTs Validation 
 % Guide. It consists of calls to other scripts contained within the
 % subfolder called "scripts". 
 %
 % The most important script is called dataplot. It reads the file called
-% validation_data_config_matlab.csv and generates 1000+ plots. If you
+% FDTs_validation_dataplot_inputs.csv and generates 1000+ plots. If you
 % want to process only some of these plots, comment out the other 
 % scripts and change the data plot line as follows:
 %
-% [saved_data,drange] = dataplot(cfil,vdir,plotdir,[a:b]);
+% [saved_data,drange] = dataplot(Dataplot_Inputs_File,Validation_Dir,Manuals_Dir,[a:b]);
 %
 % where a and b are the lines in the .csv file you want to process.
 % Alternatively, you can specify the "Dataname" you want:
 %
-% [saved_data,drange] = dataplot(cfil,vdir,plotdir,'WTC');
+% [saved_data,drange] = dataplot(Dataplot_Inputs_File,Validation_Dir,Manuals_Dir,'WTC');
 %
 % In this case, all the WTC results will be plotted. 
 
@@ -27,13 +27,27 @@ addpath 'scripts'
 
 % dataplot creates most of the plots for the Validation Guide. It must be run before scatplot, which makes the scatter plots.
 
-cfil = [pwd,'/FDTs_validation_dataplot_inputs.csv'];
-vdir = [pwd,'/../../Validation/'];
-plotdir = [pwd,'/../../Manuals/'];
-qfil = [pwd,'/FDTs_validation_scatterplot_inputs.csv'];
-output_file = [pwd,'/FDTs_validation_scatterplot_output.csv'];
+Dataplot_Inputs_File = [pwd, '/FDTs_validation_dataplot_inputs.csv'];
+Working_Dir = [pwd, '/../../Validation/'];
+Manuals_Dir = [pwd, '/../../Manuals/'];
+Scatterplot_Inputs_File = [pwd, '/FDTs_validation_scatterplot_inputs.csv'];
+Output_File = [pwd, '/FDTs_validation_scatterplot_output.csv'];
+Stats_Output = 2;
+Statistics_Tex_Output = [pwd, '../../Manuals/FDTs_Validation_Guide/SCRIPT_FIGURES/Scatterplots/validation_statistics.tex'];
+Histogram_Tex_Output = [pwd, '../../Manuals/FDTs_Validation_Guide/SCRIPT_FIGURES/Scatterplots/validation_histograms.tex'];
 
-[saved_data,drange] = dataplot(cfil,vdir,plotdir);
-scatplot(saved_data,drange,qfil,plotdir,output_file)
+NRC_Options = true;
+Append_To_Scatterplot_Title = '';
+
+[saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir);
+scatplot(saved_data, drange, ...
+         'Scatterplot_Inputs_File', Scatterplot_Inputs_File, ...
+         'Manuals_Dir', Manuals_Dir, ...
+         'Output_File', Output_File, ...
+         'Stats_Output', Stats_Output, ...
+         'Statistics_Tex_Output', Statistics_Tex_Output, ...
+         'Histogram_Tex_Output', Histogram_Tex_Output, ...
+         'NRC_Options', NRC_Options, ...
+         'Append_To_Scatterplot_Title', Append_To_Scatterplot_Title)
  
 display('validation scripts completed successfully!')
