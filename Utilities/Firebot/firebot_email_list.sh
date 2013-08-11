04cb7cb8384e3225e42e60507a9b91e9c628af1a
Kristopher Overholt <koverholt@gmail.com>
2013-08-11 18:02:20 +0000

    Firebot: Edits to troubleshoot failed Firebot status email

--- Utilities/Firebot/firebot_email_list.sh
+++ Utilities/Firebot/firebot_email_list.sh
@@ -5,3 +5,4 @@ mailToSMV="gforney@gmail.com, koverholt@gmail.com"
 
 # Mailing list for Smokebot/Firebot status report
 mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com"
+
