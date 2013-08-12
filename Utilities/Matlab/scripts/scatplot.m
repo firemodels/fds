bc0f49dbfd1cdc1d02ab785a98c9ea64e0ba3f42
Kristopher Overholt <koverholt@gmail.com>
2013-08-12 00:21:34 +0000

    VV Scripts: Add separate histogram script to clean up scatplot script
    Former-commit-id: aba64422188499ad78724dbe8b40540338e00549 [formerly 75f172819931b90976512e9b09946dbf954ba342]
    Former-commit-id: 6134991180c5416e1e37d82fa0f36f87d93ac0f7
    Former-commit-id: c37080b376f452f6d63865e986c8a5e8da83417b

--- Utilities/Matlab/scripts/scatplot.m
+++ Utilities/Matlab/scripts/scatplot.m
@@ -24,6 +24,7 @@
 
 function [] = scatplot(saved_data,drange,varargin)
 
+% Parse input arguments using ('Key', Value) syntax
 for k=1:2:length(varargin);
     switch (varargin{k})
     case {'Scatterplot_Inputs_File'}
@@ -333,66 +334,9 @@ for j=2:length(Q);
         set(gcf,'PaperPosition',[0 0 PDF_Paper_Width Scat_Paper_Height]);
         display(['Printing scatter plot ',num2str(j),'...'])
         print(gcf,Image_File_Type,[Manuals_Dir,Plot_Filename])
-
+        
         % Print histogram of ln(M/E) and normal distribution
-        if Stats_Output == 2 % (Validation runs only)
-            % Wrap histogram routine in try loop
-            % Skips case upon any Matlab error
-            try
-                ln_M_E = log(Predicted_Metric)-log(Measured_Metric);
-                % Normality test (requires at least 4 observations)
-                if length(ln_M_E) >= 4
-
-                    pval = spiegel_test(ln_M_E);
-                    
-                    % Plot histogram
-                    figure
-                    box on
-                    hold on
-                    [n,xout] = hist(ln_M_E,10);
-                    bar(xout,n,1,'LineWidth',1,'FaceColor',[0.7,0.7,0.7])
-                    
-                    % Plot normal distribution
-                    x_lim = [xout(1)-(xout(2)-xout(1)),xout(end)+(xout(2)-xout(1))];
-                    ix = x_lim(1):1e-3:x_lim(2);
-                    mu = mean(ln_M_E);
-                    sd = std(ln_M_E);
-                    % Generate PDF of normal distribution (overlaid on results)
-                    iy = 1/(sd*sqrt(2*pi))*exp(-(ix-mu).^2/(2*sd^2));
-                    plot(ix,iy*trapz(xout,n),'k','LineWidth',2);
-
-                    % Additional plot content
-                    set(gca,'XLim',[x_lim(1),x_lim(2)]);
-                    y_lim = get(gca,'YLim') * 1.25;
-                    set(gca,'YLim',y_lim);
-                    xlabel('Interval Number','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size,'FontName',Font_Name)
-                    ylabel('Number of Data Points','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size,'FontName',Font_Name)
-                    set(gca,'Units','inches')
-                    set(gca,'FontName',Font_Name)
-                    set(gca,'FontSize',12)
-                    set(gca,'XTick',xout,'XTickLabel',{'1','2','3','4','5','6','7','8','9','10'})
-                    set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
-                    text(0.03, 0.90,Scatter_Plot_Title,'FontSize',Title_Font_Size,'FontName',Font_Name,'Interpreter',Font_Interpreter,'Units','normalized')
-                    text(0.03, 0.82,'Normality Test','FontSize',Title_Font_Size,'FontName',Font_Name,'Interpreter',Font_Interpreter,'Units','normalized')
-                    text(0.03, 0.74,['p-value = ',num2str(pval,'%4.2f')],'FontSize',Title_Font_Size,'FontName',Font_Name,'Interpreter',Font_Interpreter,'Units','normalized')
-                    
-                    PDF_Paper_Width = Paper_Width;
-                    
-                    set(gcf,'Visible','on');
-                    set(gcf,'PaperUnits','inches');
-                    set(gcf,'PaperSize',[PDF_Paper_Width Paper_Height]);
-                    set(gcf,'PaperPosition',[0 0 PDF_Paper_Width Paper_Height]);
-                    print(gcf,Image_File_Type,[Manuals_Dir,[Plot_Filename, '_Histogram']])
-                    hold off
-                    % Add histogram name to array for LaTeX output later
-                    [~, filename, ~] = fileparts(Plot_Filename);
-                    Output_Histograms{end+1} = [filename, '_Histogram'];
-                end
-            catch
-                display(['Error: Problem with histogram routine for scatter plot ', Scatter_Plot_Title, '; Skipping histogram.'])
-                continue
-            end
-        end
+        statistics_histogram
         
         % Perform this code block for FDS validation scatterplot output
         if Stats_Output == 2
