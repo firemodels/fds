2c6bfc44b6626b37e75d57c4a790b6cf8ed83db8
gforney <gforney@gmail.com>
2013-08-13 15:20:33 +0000

    fds build: remove -j2 option from 32 bit mpi build script
    Former-commit-id: 4590b4a072cf1b15d4e0b1623e2957cf63dd3166 [formerly 72c1ed7eee41ee622673313eecd1973e4826c693]
    Former-commit-id: 70cef40a0e981230c66dc2bce28e019c3a7840f5
    Former-commit-id: 3d853af8883141644690d25e7d17380d11c580b8

--- FDS_Compilation/mpi_intel_win_32/make_fds_from_webpage.bat
+++ FDS_Compilation/mpi_intel_win_32/make_fds_from_webpage.bat
@@ -2,5 +2,5 @@ set intelbin="%IFORT_COMPILER13%\bin"
 
 call %intelbin%\ifortvars ia32
 
-make -j2 MPIINCLUDE="c:\mpich\mpich2_32\include" MPILIB="c:\mpich\mpich2_32\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_32
+make MPIINCLUDE="c:\mpich\mpich2_32\include" MPILIB="c:\mpich\mpich2_32\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_32
 pause
