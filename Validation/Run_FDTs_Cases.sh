642bd1415a8080648706e243311afcfec800bece
Kristopher Overholt <koverholt@gmail.com>
2013-08-19 19:50:56 +0000

    FDTs: Add verification directory/case placeholder

--- Validation/Run_FDTs_Cases.sh
+++ Validation/Run_FDTs_Cases.sh
@@ -1,74 +1,76 @@
 #!/bin/bash -f
 
-# This script runs the FDTs cases on a linux machine with
-# a batch queuing system
+# This script runs all of the FDTs cases
 
 export SVNROOT=`pwd`/..
 export FDTs=$SVNROOT/Utilities/Data_Processing/FDTs
-export VDIR=$SVNROOT/Validation
+export VERIFICATION_DIR=$SVNROOT/Verification
+export VALIDATION_DIR=$SVNROOT/Validation
 
 # First, compile latest version of FDTs.f90
 
 cd $SVNROOT/Utilities/Data_Processing
 ifort FDTs.f90 -o FDTs
 
-# Then, run all FDTs cases
+# Run all FDTs verification cases
 
-cd $VDIR/Arup_Tunnel/FDTs
-$FDTs Arup_Tunnel_FDTs_Inputs.txt
+cd $VERIFICATION_DIR/FDTs
+$FDTs Verification.txt
 
-cd $VDIR/ATF_Corridors/FDTs
+# Run all FDTs validation cases
+
+cd $VALIDATION_DIR/ATF_Corridors/FDTs
 $FDTs ATF_Corridors_FDTs_Inputs.txt
 
-cd $VDIR/CAROLFIRE/FDTs
+cd $VALIDATION_DIR/CAROLFIRE/FDTs
 $FDTs CAROLFIRE_FDTs_Inputs.txt
 
-cd $VDIR/Fleury_Heat_Flux/FDTs
+cd $VALIDATION_DIR/Fleury_Heat_Flux/FDTs
 $FDTs Fleury_Heat_Flux_FDTs_Inputs.txt
 
-cd $VDIR/FM_SNL/FDTs
+cd $VALIDATION_DIR/FM_SNL/FDTs
 $FDTs FM_SNL_FDTs_Inputs.txt
 
-cd $VDIR/Hamins_CH4/FDTs
+cd $VALIDATION_DIR/Hamins_CH4/FDTs
 $FDTs Hamins_FDTs_Inputs.txt
 
-cd $VDIR/LLNL_Enclosure/FDTs
+cd $VALIDATION_DIR/LLNL_Enclosure/FDTs
 $FDTs LLNL_FDTs_Inputs.txt
 
-cd $VDIR/NBS_Multi-Room/FDTs
+cd $VALIDATION_DIR/NBS_Multi-Room/FDTs
 $FDTs NBS_Multi-Room_FDTs_Inputs.txt
 
-cd $VDIR/NIST_NRC/FDTs
+cd $VALIDATION_DIR/NIST_NRC/FDTs
 $FDTs NIST_NRC_FDTs_Inputs.txt
 
-cd $VDIR/NIST_Dunes_2000/FDTs
+cd $VALIDATION_DIR/NIST_Dunes_2000/FDTs
 $FDTs NIST_Dunes_2000_FDTs_Inputs.txt
 
-cd $VDIR/NRL_HAI/FDTs
+cd $VALIDATION_DIR/NRL_HAI/FDTs
 $FDTs NRL_HAI_FDTs_Inputs.txt
 
-cd $VDIR/SP_AST/FDTs
+cd $VALIDATION_DIR/SP_AST/FDTs
 $FDTs SP_AST_FDTs_Inputs.txt
 
-cd $VDIR/Steckler_Compartment/FDTs
+cd $VALIDATION_DIR/Steckler_Compartment/FDTs
 $FDTs Steckler_FDTs_Inputs.txt
 
-cd $VDIR/UL_NFPRF/FDTs
+cd $VALIDATION_DIR/UL_NFPRF/FDTs
 $FDTs UL_NFPRF_FDTs_Inputs.txt
 
-cd $VDIR/Ulster_SBI/FDTs
+cd $VALIDATION_DIR/Ulster_SBI/FDTs
 $FDTs Ulster_SBI_FDTs_Inputs.txt
 
-cd $VDIR/USN_Hangars/FDTs
+cd $VALIDATION_DIR/USN_Hangars/FDTs
 $FDTs USN_Hangars_FDTs_Inputs.txt
 
-cd $VDIR/Vettori_Flat_Ceiling/FDTs
+cd $VALIDATION_DIR/Vettori_Flat_Ceiling/FDTs
 $FDTs Vettori_FDTs_Inputs.txt
 
-cd $VDIR/VTT/FDTs
+cd $VALIDATION_DIR/VTT/FDTs
 $FDTs VTT_FDTs_Inputs.txt
 
-cd $VDIR/WTC/FDTs
+cd $VALIDATION_DIR/WTC/FDTs
 $FDTs WTC_FDTs_Inputs.txt
 
 echo FDTs cases complete
