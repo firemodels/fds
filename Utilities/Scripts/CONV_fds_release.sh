5fca33353c7b23ce891eaf433e5b67bdef5e7d79
gforney <gforney@gmail.com>
2013-09-17 14:42:28 +0000

    bundle scripts: add Linux python script to convert FDS notes from wiki to html format
    Former-commit-id: 28451315dcdfcde094f8d48e27498d2a1d2f9570 [formerly 7ce61c6694a52ab2a4c4e0cb818451c1faba2dbf]
    Former-commit-id: bee578589cbb57e3affaea16df0fd6c4c236713d
    Former-commit-id: ffe65a00054b9cb7e8c460830bcc98d9cdb65afa

---
+++ Utilities/Scripts/CONV_fds_release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bundleinfo=./bundle_setup
+wikify=./wikify.py
+
+
+echo Converting the FDS release notes from wiki to html format
+$wikify -r $bundleinfo/FDS_Release_Notes.wiki > $bundleinfo/FDS_Release_Notes.htm
