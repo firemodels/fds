f40988222c2c9ad15b7e8721ec6d00551f0087b7
gforney <gforney@gmail.com>
2013-09-17 14:46:20 +0000

    fds build: update web build scripts to use version 14 parameters
    Former-commit-id: 50fc99015ad53cd28b0802275d669fc3ecabbcd0 [formerly 8cea3a4bd689593337f1a6489f6ee1e138a5c07f]
    Former-commit-id: 8bc0d442cd58d2bd35ced8c2ddc8cd1ce172d9a0
    Former-commit-id: e22639c261c6639ecfbed6e2f16f8f7172a50591

--- FDS_Compilation/mpi_intel_win_32/make_fds_from_webpage.bat
+++ FDS_Compilation/mpi_intel_win_32/make_fds_from_webpage.bat
@@ -1,6 +1,6 @@
-set intelbin="%IFORT_COMPILER13%\bin"
+set intelbin="%IFORT_COMPILER14%\bin"
 
-call %intelbin%\ifortvars ia32
+call %intelbin%\compilervars ia32
 
 make MPIINCLUDE="c:\mpich\mpich2_32\include" MPILIB="c:\mpich\mpich2_32\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_32
 pause
