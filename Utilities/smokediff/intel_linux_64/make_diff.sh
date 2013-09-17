539b910d4fff8e07b2694af1619f6b82476f3b2e
gforney <gforney@gmail.com>
2013-09-17 19:59:01 +0000

    build scripts: update various utility build scripts to use current Intel setup scripts

--- Utilities/smokediff/intel_linux_64/make_diff.sh
+++ Utilities/smokediff/intel_linux_64/make_diff.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source $IFORT_COMPILER/bin/iccvars.sh intel64
-source $IFORT_COMPILER/bin/ifortvars.sh intel64
+source $IFORT_COMPILER/bin/compilervars.sh intel64
 rm *.o
 make -j4 -f ../Makefile intel_linux_64
