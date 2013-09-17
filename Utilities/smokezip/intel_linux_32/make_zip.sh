15b4cfd8fe3272a78d6dd4aa040ab26f7ebc1acf
gforney <gforney@gmail.com>
2013-09-17 19:59:01 +0000

    build scripts: update various utility build scripts to use current Intel setup scripts
    Former-commit-id: cf03895c2801115ce13f1fb317100b26a282bb37 [formerly e0ebf08fa9b7199538f3eef49cbb631304d5629d]
    Former-commit-id: 3552a67f58471dc50ce5efb7efc55ff0375943ee
    Former-commit-id: d949188611e7c647dd622ed3c49556d9be730ea4

--- Utilities/smokezip/intel_linux_32/make_zip.sh
+++ Utilities/smokezip/intel_linux_32/make_zip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -f
-source $IFORT_COMPILER/bin/iccvars.sh ia32
-source $IFORT_COMPILER/bin/ifortvars.sh ia32
+source $IFORT_COMPILER/bin/compilervars.sh ia32
 rm *.o
 make -j4 -f ../Makefile intel_linux_32
