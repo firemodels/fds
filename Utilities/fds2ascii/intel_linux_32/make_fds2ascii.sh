48be9767cd867cea00f79857371f0fb61dc22c1a
gforney <gforney@gmail.com>
2013-09-17 19:49:59 +0000

    fds2ascii build: update build scripts to use correct intel environment setting scripts

--- Utilities/fds2ascii/intel_linux_32/make_fds2ascii.sh
+++ Utilities/fds2ascii/intel_linux_32/make_fds2ascii.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm *.o
-source $IFORT_COMPILER/bin/ifortvars.sh ia32
+source $IFORT_COMPILER/bin/compilervars.sh ia32
 make -f ../Makefile intel_linux_32
--- Utilities/fds2ascii/intel_linux_64/make_fds2ascii.sh
+++ Utilities/fds2ascii/intel_linux_64/make_fds2ascii.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm *.o
-source $IFORT_COMPILER/bin/ifortvars.sh intel64
+source $IFORT_COMPILER/bin/compilervars.sh intel64
 make -f ../Makefile intel_linux_64
