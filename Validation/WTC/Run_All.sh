703294cfafa8154b576468d0f9c4e84412cfab37
rmcdermo <randy.mcdermott@gmail.com>
2013-09-19 20:20:14 +0000

    FDS Validation: fix spaces in Run_All.sh
    Former-commit-id: 2ebbb77eb50875c9fcbc520e4dc6641b3efa9dc5 [formerly eabddd766b071c902c8d6dfc0623b6a4f9507f78]
    Former-commit-id: 827b61d2513286e6e6d6d35190d14caf955548aa
    Former-commit-id: 3bce236fac08522a0c45d91865501d96280d82e1

--- Validation/WTC/Run_All.sh
+++ Validation/WTC/Run_All.sh
@@ -13,10 +13,10 @@ qq=
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
 $QFDS -r $qq -d $INDIR WTC_01.fds 
-$QFDS -r $qq  -d $INDIR WTC_02.fds
+$QFDS -r $qq -d $INDIR WTC_02.fds
 $QFDS -r $qq -d $INDIR WTC_03.fds 
-$QFDS -r $qq  -d $INDIR WTC_04.fds 
-$QFDS -r $qq  -d $INDIR WTC_05.fds 
+$QFDS -r $qq -d $INDIR WTC_04.fds 
+$QFDS -r $qq -d $INDIR WTC_05.fds 
 $QFDS -r $qq -d $INDIR WTC_06.fds 
 
 echo FDS cases submitted
