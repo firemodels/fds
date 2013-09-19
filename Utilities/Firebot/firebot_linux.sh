a068f99b951f4dfc9808312ef2d21e6617c94df6
Kristopher Overholt <koverholt@gmail.com>
2013-09-19 11:23:44 +0000

    Firebot: Fix verification case error tolerance reporting
    Former-commit-id: 92d078757ac539fc0c97369da453625a50dc9ab1 [formerly 6bd68d6345cc2497342b0cd48f228139cd60e44c]
    Former-commit-id: 1d0d2f9c979de73bf38deeff96455d4c86b268b6
    Former-commit-id: 48aea7c598f8a909d9349f483393c8ee1e3b1f28

--- Utilities/Firebot/firebot_linux.sh
+++ Utilities/Firebot/firebot_linux.sh
@@ -1028,7 +1028,7 @@ check_verification_stats()
    else
       echo "Warnings from Stage 7a - Matlab plotting and statistics (verification):" >> $WARNING_LOG
       echo "The following cases are outside of their specified error tolerance:" >> $WARNING_LOG
-      grep ",No," FDS_verification_scatterplot_output.csv >> $WARNING_LOG
+      grep "Out of Tolerance" FDS_verification_scatterplot_output.csv >> $WARNING_LOG
       echo "" >> $WARNING_LOG
    fi
 
