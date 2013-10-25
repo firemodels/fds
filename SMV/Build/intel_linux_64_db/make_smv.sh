241c2b172a502dcfa3ad0309b532c4c59eb0e4e5
gforney <gforney@gmail.com>
2013-10-25 13:38:28 +0000

    smv build: convert a smokeview build script to be compatible with version 14 compilers

--- SMV/Build/intel_linux_64_db/make_smv.sh
+++ SMV/Build/intel_linux_64_db/make_smv.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source $IFORT_COMPILER/bin/iccvars.sh intel64
-source $IFORT_COMPILER/bin/ifortvars.sh intel64
+source $IFORT_COMPILER/bin/compilervars.sh intel64
 make -f ../Makefile clean
 make -j4 -f ../Makefile intel_linux_64_db
