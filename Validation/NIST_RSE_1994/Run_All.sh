a87eef452177f41bdc5906c0e1894182c6082f1a
cweinschenk <CraigWeinschenk@gmail.com>
2013-10-28 17:47:20 +0000

    FDS Validation: update NIST_RSE input files, latest NIST_RSE results
    Former-commit-id: bd68987798638261746d2586f378740733ac1fc4 [formerly f0bfb4ac80c640c8f077d9c25858c4be0f549b8f]
    Former-commit-id: ce301b5f210a86a110bd6f4ecd13189bfe12f08e
    Former-commit-id: 2f82f0fafe637307bde9927b546846de14f291c2

--- Validation/NIST_RSE_1994/Run_All.sh
+++ Validation/NIST_RSE_1994/Run_All.sh
@@ -8,7 +8,7 @@ export INDIR=Current_Results
 qq=
 
 # uncomment following line to stop all cases
-export STOPFDS=1
+# export STOPFDS=1
 
 /bin/sh -c "cp $BASEDIR/FDS_Input_Files/*.fds $BASEDIR/$INDIR"
 
