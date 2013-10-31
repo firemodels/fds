55beb18f8f470f5bd0cc9725cbc8152a74c4683a
gforney <gforney@gmail.com>
2013-10-31 17:35:56 +0000

    smv bundle: make some corrections to smokeview installation wrap up scripts

--- SMV/for_bundle/wrapup_smv_install_32.bat
+++ SMV/for_bundle/wrapup_smv_install_32.bat
@@ -16,7 +16,11 @@ echo Associating the .smv file extension with smokeview.exe
 ftype smvDoc="%CD%\smokeview.exe" "%%1" >Nul
 assoc .smv=smvDoc>Nul
 
+Rem adding path
+echo.
+echo Adding %CD% to the system path 
+call "%CD%\set_path.exe" -s -m -a "%CD%"
+
+erase "%CD%\set_path.exe"
 echo Press any key to complete update
 pause>NUL
-erase "%CD%"\wrapup_smv_install.bat
-
--- SMV/for_bundle/wrapup_smv_install_64.bat
+++ SMV/for_bundle/wrapup_smv_install_64.bat
@@ -18,12 +18,9 @@ assoc .smv=smvDoc>Nul
 
 Rem adding path
 echo.
-echo Adding %CD% to the system path 
+echo Adding %CD% to the system path
 call "%CD%\set_path.exe" -s -m -a "%CD%"
 
-
-echo Press any key to complete update.
+erase "%CD%\set_path.exe"
+echo Press any key to complete update
 pause>NUL
-erase "%CD%|\set_path.exe"
-erase "%CD%"\wrapup_smv_install.bat
-
