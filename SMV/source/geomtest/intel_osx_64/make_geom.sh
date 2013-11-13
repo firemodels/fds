22454a39262c62a53f841c92f854d58c06b37568
gforney <gforney@gmail.com>
2013-11-13 15:13:08 +0000

    geomtest: add osx and linux build entries

---
+++ SMV/source/geomtest/intel_osx_64/make_geom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+platform=intel64
+dir=`pwd`
+target=${dir##*/}
+
+source $IFORT_COMPILER/bin/compilervars.sh $platform
+
+echo Building $target
+make -j4 VPATH=".." -f ../Makefile $target
