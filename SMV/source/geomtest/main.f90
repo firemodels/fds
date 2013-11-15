2213db237e712343cdc52e79c44fcb14206f9f58
gforney <gforney@gmail.com>
2013-11-15 15:40:21 +0000

    geomtest: initial test working

--- SMV/source/geomtest/main.f90
+++ SMV/source/geomtest/main.f90
@@ -5,6 +5,7 @@ implicit none
 CHARACTER(250) :: FN_INPUT='null'
 
 CALL GET_COMMAND_ARGUMENT(1,FN_INPUT)
+open(5,FILE=FN_INPUT)
 
 write(6,*)"test &GEOM input parsing"
 write(6,*)"input file: ",trim(FN_INPUT)
