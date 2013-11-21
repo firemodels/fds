2b3d30e739833f96bcea795305f6bf624074b951
Kristopher Overholt <koverholt@gmail.com>
2013-11-21 14:46:43 +0000

    Firebot: Add OS identifier to Firebot status email
    Former-commit-id: 4b6f654ca71fde56717d6c85f5b2cbcb52a8544d [formerly dea2bde15575818c4b1b91000925f64704ca8fef]
    Former-commit-id: 9414079a21a2020df20fcee8d98ff566c2724472
    Former-commit-id: c81f9a4cc54aae1bdfe2182ec3f7a12e2972de4b

--- Utilities/Firebot/firebot_mac.sh
+++ Utilities/Firebot/firebot_mac.sh
@@ -548,7 +548,7 @@ email_build_status()
       # Send success message with links to nightly manuals
       stop_time=`date`
       echo "-------------------------------" >> $TIME_LOG
-      echo "Host: $hostname " >> $TIME_LOG
+      echo "Mac Host: $hostname " >> $TIME_LOG
       echo "Start Time: $start_time " >> $TIME_LOG
       echo "Stop Time: $stop_time " >> $TIME_LOG
       echo "-------------------------------" >> $TIME_LOG
