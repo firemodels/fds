8b67b0b6dd6efe924faeceed8a1e990edbe7bee6
Timo Korhonen <tkorhon1@gmail.com>
2013-11-22 07:52:52 +0000

    FDS Source: ieva.f90 no 'save' anymore (gfortran warning). evac.f90 crowbar project stuff added (reads in 3d-surveillance data and uses this to 'correct' the agent trajectories + creates new agents when they enter the area, if needed). read.f90/type.f90/cons.f90 changes are minor, they just have a couple of new definitions and initializations for new variables needed by the surveillance data stuff.
    Former-commit-id: 9966ef992214e38e3af04dccf191a1b99e2557a3 [formerly 1bdd0b446c6618130987c95169ca6452221cb4cc]
    Former-commit-id: 089b1df6952ccbf4d8b86fd98bd45441dae7dac8
    Former-commit-id: 34e6fff63dd38cdcc9860e55537779efca89eea9

--- FDS_Source/type.f90
+++ FDS_Source/type.f90
@@ -360,10 +360,11 @@ TYPE HUMAN_TYPE
    CHARACTER(60) :: NODE_NAME='null'
    CHARACTER(30) :: FFIELD_NAME='null'
    REAL(EB) :: X=0._EB,Y=0._EB,Z=0._EB,U=0._EB,V=0._EB,W=0._EB,F_X=0._EB,F_Y=0._EB,&
-               X_old=0._EB,Y_old=0._EB,X_group=0._EB,Y_group=0._EB
-   REAL(EB) :: UBAR=0._EB, VBAR=0._EB, UBAR_Center=0._EB, VBAR_Center=0._EB
+               X_old=0._EB,Y_old=0._EB,X_group=0._EB,Y_group=0._EB, U_CB=0.0_EB, V_CB=0.0_EB
+   REAL(EB) :: UBAR=0._EB, VBAR=0._EB, UBAR_Center=0._EB, VBAR_Center=0._EB, T_LastRead_CB=0.0_EB
    REAL(EB) :: Speed=1.25_EB, Radius=0.255_EB, Mass=80.0_EB, Tpre=1._EB, Tau=1._EB, &
-               Eta=0._EB, Ksi=0._EB, Tdet=0._EB, Speed_ave=0._EB, T_Speed_ave=0._EB
+               Eta=0._EB, Ksi=0._EB, Tdet=0._EB, Speed_ave=0._EB, T_Speed_ave=0._EB, &
+               X_CB=0.0_EB, Y_CB=0.0_EB, X2_CB=0.0_EB, Y2_CB=0.0_EB, UAVE_CB=0.0_EB, VAVE_CB=0.0_EB
    REAL(EB) :: r_torso=0.15_EB, r_shoulder=0.095_EB, d_shoulder=0.055_EB, angle=0._EB, &
                torque=0._EB, m_iner=4._EB
    REAL(EB) :: tau_iner=0.2_EB, angle_old=0._EB, omega=0._EB
@@ -373,10 +374,10 @@ TYPE HUMAN_TYPE
    REAL(EB) :: TempMax1=0._EB, FluxMax1=0._EB, TempMax2=0._EB, FluxMax2=0._EB, Density=0._EB, DensityR=0._EB, DensityL=0._EB
    REAL(EB) :: P_detect_tot=0._EB, v0_fac=1._EB, D_Walls=0._EB
    REAL(EB) :: T_FallenDown=0._EB, F_FallDown=0._EB, Angle_FallenDown=0._EB, SizeFac_FallenDown=0._EB, T_CheckFallDown=0._EB
-   INTEGER  :: IOR=-1, ILABEL=0, COLOR_INDEX=0, INODE=0, IMESH=-1, IPC=0, IEL=0, I_FFIELD=0, I_Target2=0
+   INTEGER  :: IOR=-1, ILABEL=0, COLOR_INDEX=0, INODE=0, IMESH=-1, IPC=0, IEL=0, I_FFIELD=0, I_Target2=0, ID_CB=-1
    INTEGER  :: GROUP_ID=0, DETECT1=0, GROUP_SIZE=0, I_Target=0, I_DoorAlgo=0, I_Door_Mode=0, STRS_Direction = 1
    INTEGER  :: STR_SUB_INDX, SKIP_WALL_FORCE_IOR
-   LOGICAL  :: SHOW=.TRUE., NewRnd=.TRUE.
+   LOGICAL  :: SHOW=.TRUE., NewRnd=.TRUE., CROWBAR_READ_IN=.FALSE., CROWBAR_UPDATE_V0=.FALSE.
    LOGICAL  :: SeeDoorXB1=.FALSE., SeeDoorXB2=.FALSE., SeeDoorXYZ1=.FALSE., SeeDoorXYZ2=.FALSE.
 END TYPE HUMAN_TYPE
 
