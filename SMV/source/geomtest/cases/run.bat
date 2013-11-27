0630d31582c6b4a8d5b92a99565a4b52aceee248
gforney <gforney@gmail.com>
2013-11-27 19:22:12 +0000

    geomtest vv: add test cases to test component_only keyword and groups

--- SMV/source/geomtest/cases/run.bat
+++ SMV/source/geomtest/cases/run.bat
@@ -1,7 +1,8 @@
 @echo off
-..\intel_win_64\geomtest test.fds
 ..\intel_win_64\geomtest test_azim.fds
+..\intel_win_64\geomtest test_component.fds
 ..\intel_win_64\geomtest test_elev.fds
-..\intel_win_64\geomtest test_scale.fds
 ..\intel_win_64\geomtest test_group.fds
 ..\intel_win_64\geomtest test_group2.fds
+..\intel_win_64\geomtest test_group3.fds
+..\intel_win_64\geomtest test_scale.fds
--- SMV/source/geomtest/cases/run_osx.sh
+++ SMV/source/geomtest/cases/run_osx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 GEOMTEST=../intel_osx_64/geomtest
-$GEOMTEST test.fds
 $GEOMTEST test_azim.fds
+$GEOMTEST test_component.fds
 $GEOMTEST test_elev.fds
-$GEOMTEST test_scale.fds
 $GEOMTEST test_group.fds
+$GEOMTEST test_group2.fds
+$GEOMTEST test_group3.fds
+$GEOMTEST test_scale.fds
