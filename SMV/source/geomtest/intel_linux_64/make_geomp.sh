e50c1e5a5cc3d0ba1ab81aa9b388b49ae9bf23b9
gforney <gforney@gmail.com>
2013-12-01 00:20:55 +0000

    geomtest: add a profile target to the makefile

---
+++ SMV/source/geomtest/intel_linux_64/make_geomp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+platform=intel64
+dir=`pwd`
+target=intel_linux_64p
+
+source $IFORT_COMPILER/bin/compilervars.sh $platform
+
+echo Building $target
+rm *.o *.mod
+make VPATH=".." -f ../Makefile $target
