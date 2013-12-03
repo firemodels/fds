8d2ac4162e237d059dc6b5d4e87d661425cc9278
Kristopher Overholt <koverholt@gmail.com>
2013-12-03 02:15:18 +0000

    Validationbot: Update email list

--- Utilities/Firebot/firebot_email_list.sh
+++ Utilities/Firebot/firebot_email_list.sh
@@ -6,3 +6,7 @@ mailToSMV="gforney@gmail.com, koverholt@gmail.com"
 # Mailing list for Smokebot/Firebot status report
 mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com"
 
+# Mailing list for Validationbot
+# mailtoFDS_validationbot="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, koverholt@gmail.com"
+mailtoFDS_validationbot="koverholt@gmail.com"
+
