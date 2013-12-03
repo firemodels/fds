99def79d93e73377e8909135c659f2016ecf881a
gforney <gforney@gmail.com>
2013-12-03 21:58:05 +0000

    geomtest vv: modify test_sphere2 to test new rotation keyword

--- SMV/source/geomtest/cases/run.bat
+++ SMV/source/geomtest/cases/run.bat
@@ -1,11 +1,15 @@
 @echo off
+..\intel_win_64\geomtest test_arch.fds
 ..\intel_win_64\geomtest test_azim.fds
 ..\intel_win_64\geomtest test_component.fds
 ..\intel_win_64\geomtest test_elev.fds
 ..\intel_win_64\geomtest test_group.fds
 ..\intel_win_64\geomtest test_group2.fds
 ..\intel_win_64\geomtest test_group3.fds
+..\intel_win_64\geomtest test_obst.fds
 ..\intel_win_64\geomtest test_scale.fds
+..\intel_win_64\geomtest test_sphere.fds
+..\intel_win_64\geomtest test_sphere2.fds
 ..\intel_win_64\geomtest test_time.fds
 ..\intel_win_64\geomtest test_time2.fds
 ..\intel_win_64\geomtest test_time3.fds
