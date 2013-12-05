df150ed1522522ee9df0c2142c1feccfa656c520
Kristopher Overholt <koverholt@gmail.com>
2013-12-05 15:49:46 +0000

    Validationbot: Edit email list; fix email notification for Validationbot
    Former-commit-id: daba6b1d5eba5264fd5d66c7d0bc3bc9b55a224e [formerly fe8d9e3f54904e3f8c8208891648b864a92737a6]
    Former-commit-id: 2f1231a8e971b4df3fc1ac8b2cf1facc9a9d2563
    Former-commit-id: cf9a681667d363288a75df40424e31f72ea81883

--- Utilities/Firebot/firebot_email_list.sh
+++ Utilities/Firebot/firebot_email_list.sh
@@ -3,9 +3,9 @@
 # Mailing list for Smokebot status report
 mailToSMV="gforney@gmail.com, koverholt@gmail.com"
 
-# Mailing list for Smokebot/Firebot status report
+# Mailing list for Firebot status report
 mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com"
 
-# Mailing list for Validationbot
-mailtoFDS_validationbot="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, koverholt@gmail.com"
+# Mailing list for Validationbot, Macbot, etc.
+mailtoFDS_nist="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, koverholt@gmail.com"
 
