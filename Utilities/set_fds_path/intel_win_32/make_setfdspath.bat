83e6a157ca1e551c46cb8b808daf86621c71dd6d
gforney <gforney@gmail.com>
2013-12-15 17:26:25 +0000

    set_path: make some fixes to include file and build scripts

--- Utilities/set_fds_path/intel_win_32/make_setfdspath.bat
+++ Utilities/set_fds_path/intel_win_32/make_setfdspath.bat
@@ -7,7 +7,7 @@ IF "%SETUP_IFORT_COMPILER%"=="1" GOTO envexist
 set SETUP_IFORT_COMPILER=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER14%\bin\compilervars" ia32
+call "%IFORT_COMPILER14%\bin\compilervars" ia32 %VS_VERSION%
 if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
 :envexist
 
