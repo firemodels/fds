4a78d4603209c34b8fd6ab82d8a06180eed160ee
gforney <gforney@gmail.com>
2013-12-15 17:26:25 +0000

    set_path: make some fixes to include file and build scripts
    Former-commit-id: 3b6d9b1c669d728868fe2b9609685c3f4db1579f [formerly d816c634d53db1d1b241d007c4b001610d2cdf40]
    Former-commit-id: 53c3f9561f5fd4eeb088aeb738e8f65149954fb3
    Former-commit-id: e2cdd71b4b04945061174c0c9e969c92d31958af

--- Utilities/set_fds_path/intel_win_64/make_setfdspath.bat
+++ Utilities/set_fds_path/intel_win_64/make_setfdspath.bat
@@ -7,8 +7,8 @@ IF "%SETUP_IFORT_COMPILER%"=="1" GOTO envexist
 set SETUP_IFORT_COMPILER=1
 
 echo Setting up compiler environment
-call "%IFORT_COMPILER14%\bin\compilervars" ia32
+call "%IFORT_COMPILER14%\bin\compilervars" intel64
 :envexist
 erase *.obj
-make VPATH="../../../SMV/source/set_path" -f ../Makefile intel_win_64
+make -f ../Makefile intel_win_64
 pause
