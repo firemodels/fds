ffd5925cbcdbb827a101ad4f5c14e8eec88ad076
gforney <gforney@gmail.com>
2013-12-23 23:38:12 +0000

    fds/smv vv: add ruddy to the firebot/smokebot email list

--- Utilities/Firebot/firebot_email_list.sh
+++ Utilities/Firebot/firebot_email_list.sh
@@ -4,7 +4,7 @@
 mailToSMV="gforney@gmail.com, koverholt@gmail.com"
 
 # Mailing list for Firebot status report
-mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com"
+mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com ruddymell@gmail.com"
 
 # Mailing list for Validationbot, Macbot, etc.
 mailToFDS_nist="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, koverholt@gmail.com"
