9f97dd0865badb4352db41c4a57de8805f3a6c9f
Kristopher Overholt <koverholt@gmail.com>
2014-01-07 12:12:52 +0000

    VV Scripts: Edit Run_All script for iBMB cases
    Former-commit-id: 0fa639b72cb368518f46bb9e7df5ca610e53e4ef [formerly 7eb711adbda32efc72ebdc8e125f4a4c848fc008]
    Former-commit-id: c494c1971d64bad5139e990ea9b0796ace753c81
    Former-commit-id: 27ceecf6d4544d598dc35c3b60eb2ca6b0c87b8c

--- Validation/iBMB/Run_All.sh
+++ Validation/iBMB/Run_All.sh
@@ -5,7 +5,7 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-$QFDS $INDIR ICFMP4_01
-$QFDS $INDIR ICFMP5_04
+$QFDS $DEBUG -r $QUEUE -d $INDIR ICFMP4_01.fds
+$QFDS $DEBUG -r $QUEUE -d $INDIR ICFMP5_04.fds
 
 echo FDS cases submitted
