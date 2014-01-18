f9768212d9a5cedfd1edc02848872662a2cc12aa
Kristopher Overholt <koverholt@gmail.com>
2014-01-18 16:17:19 +0000

    Validationbot: Minor edit to stage 3

--- Utilities/Firebot/firebot_linux.sh
+++ Utilities/Firebot/firebot_linux.sh
@@ -637,7 +637,7 @@ run_validation_cases_debug()
    wait_cases_debug_start 'validation'
 
    # Wait some additional time for all cases to start
-   sleep 300
+   sleep 60
 
    #  ==================
    #  = Stop all cases =
