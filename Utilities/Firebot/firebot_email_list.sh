5ce8a1541b228fabf6b618bb4cbf98e890eafc61
Kristopher Overholt <koverholt@gmail.com>
2014-01-19 23:18:09 +0000

    Firebot: Edit to verbose email list
    Former-commit-id: d40573335b686f4eb02ecc7e7a1d4a944bd19301 [formerly 1f1e11cb7ec001ecddd454e01fcfd9b5d640b954]
    Former-commit-id: f2eabebd20fbcbcab631b5c4c424e880b06e035f
    Former-commit-id: 33f986c404f94fae7a4691aa2030cb046848de61

--- Utilities/Firebot/firebot_email_list.sh
+++ Utilities/Firebot/firebot_email_list.sh
@@ -3,9 +3,9 @@
 # Mailing list for Smokebot status report
 mailToSMV="gforney@gmail.com, koverholt@gmail.com"
 
-# Mailing list for Firebot status report
+# General mailing list for Firebot status report
 mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com"
 
-# Mailing list for Validationbot, Macbot, etc.
-mailToFDS_nist="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, koverholt@gmail.com"
+# Verbose mailing list for Validationbot, Macbot, etc.
+mailToFDS_verbose="mcgratta@gmail.com, randy.mcdermott@gmail.com, drjfloyd@gmail.com, gforney@gmail.com, koverholt@gmail.com, CraigWeinschenk@gmail.com"
 
