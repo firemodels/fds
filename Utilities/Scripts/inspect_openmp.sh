6563b940b940a2ba6ff37c6719d003be8d017170
Daniel H-Hoff <daniel.haarhoff@gmail.com>
2014-01-21 09:19:08 +0000

    Scripts: expand inspect_openmp.sh
    Delete any existing results directory.
    Add source folder to search path of collect.
    Compile the inspect target prior to inspection.
    Switch from _db to _inspect target.
    
    This fixes the issue of false positive data race detections.
    
    
    Former-commit-id: 8d3ff9de4fdb20b9fc4be0a1e63e78602a9b96ab [formerly c641bbfa102984910486ccca589d259b00f8f805]
    Former-commit-id: 9b3378e59205f1c9d0e73cc7bb74c1eb6a4fc1c4
    Former-commit-id: 941f0e4db190359476f5409e2fa0c6db437ca64f

--- Utilities/Scripts/inspect_openmp.sh
+++ Utilities/Scripts/inspect_openmp.sh
@@ -9,5 +9,20 @@
 export SVNROOT=`pwd`/../..
 source /opt/intel/inspector_xe/inspxe-vars.sh quiet
 
+TARGET=$SVNROOT/FDS_Compilation/openmp_intel_linux_64_inspect
+
+if [ -d inspect_openmp_ti3 ]
+then
+    rm -r inspect_openmp_ti3
+fi
+
+cd $TARGET
+make -f ../makefile clean
+./make_fds.sh
+
 cd $SVNROOT/Verification/Timing_Benchmarks
-inspxe-cl -collect ti3 -result-dir $SVNROOT/Utilities/Scripts/inspect_openmp_ti3 -- $SVNROOT/FDS_Compilation/openmp_intel_linux_64_db/fds_openmp_intel_linux_64_db bench2.fds
+export OMP_NUM_THREADS=2
+inspxe-cl -collect ti3 -knob scope=normal \
+          -result-dir $SVNROOT/Utilities/Scripts/inspect_openmp_ti3 \
+          -search-dir src=$SVNROOT/FDS_Source \
+          -- $SVNROOT/FDS_Compilation/openmp_intel_linux_64_inspect/fds_openmp_intel_linux_64_inspect bench2.fds
