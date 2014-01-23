c3652e2befa8a7fecd6fe583a3d16e6451d57289
mcgratta <mcgratta@gmail.com>
2014-01-23 19:29:46 +0000

    FDS Validation: Put FM/SNL back in validationbot queue.
    Former-commit-id: a55bf887cf86dececeda399c4c6287ea4d07b20d [formerly e294912ec506e6f19493dade255ef6fb11278a30]
    Former-commit-id: 19ef39f35373b8c19c329868526ae01e33f3bb1a
    Former-commit-id: 77fdb8f4953924f0c581c780047448b21a8cbf7f

--- Validation/Process_All_Output.sh
+++ Validation/Process_All_Output.sh
@@ -19,7 +19,7 @@ $VDIR/FAA_Cargo_Compartments/FDS_Output_Files/Process_Output.csh
 $VDIR/FAA_Polymers/FDS_Output_Files/Process_Output.csh
 $VDIR/Fleury_Heat_Flux/FDS_Output_Files/Process_Output.csh
 $VDIR/FM_Parallel_Panels/FDS_Output_Files/Process_Output.csh
-#$VDIR/FM_SNL/FDS_Output_Files/Process_Output.csh
+$VDIR/FM_SNL/FDS_Output_Files/Process_Output.csh
 #$VDIR/FPRF_HAI_Corridor/FDS_Output_Files/Process_Output.csh
 $VDIR/Hamins_CH4/FDS_Output_Files/Process_Output.csh
 $VDIR/Harrison_Spill_Plumes/FDS_Output_Files/Process_Output.csh
