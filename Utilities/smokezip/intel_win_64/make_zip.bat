2ff08cb2d384076135e869ecce29a4e1616f25fd
gforney <gforney@gmail.com>
2014-01-23 21:48:45 +0000

    smv vv: update windows smokebot
    Former-commit-id: f235866d0375f6c1548bf7698f5a0cd98a636a94 [formerly 77a2ea514190426c7257fd589548ea4b0e7b49ca]
    Former-commit-id: 35b153265be73da64eefeefd5d6a9a2e9239c548
    Former-commit-id: 74bdd771ccf95d2fbe681760f7f7c8a0330d68b7

--- Utilities/smokezip/intel_win_64/make_zip.bat
+++ Utilities/smokezip/intel_win_64/make_zip.bat
@@ -9,7 +9,8 @@ set SETUP_IFORT_COMPILER=1
 echo Setting up compiler environment
 call "%IFORT_COMPILER14%\bin\compilervars" intel64
 :envexist
-erase *.obj
-erase *.mod
+
+erase *.obj *.mod
 make -f ..\Makefile intel_win_64
+pause+
 
