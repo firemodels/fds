501820cd4a0e6b001b0584df4706bb1940b83d76
gforney <gforney@gmail.com>
2014-01-27 13:02:10 +0000

    smv vv: add error checking to windows smokebot

--- Utilities/Firebot/smokebot_win.bat
+++ Utilities/Firebot/smokebot_win.bat
@@ -39,6 +39,9 @@ set warningfile=%OUTDIR%\warnings.txt
 set infofile=%OUTDIR%\info.txt
 set revisionfile=%OUTDIR%\revision.txt
 set havewarnings=0
+set haveCC=1
+
+set emailexe=%userprofile%\bin\mailsend.exe
 
 call "%IFORT_COMPILER14%\bin\compilervars" %compile_platform% 1> Nul 2>&1
 call %svnroot%\Utilities\Firebot\firebot_email_list.bat
@@ -54,7 +57,6 @@ echo Stage 0 - Preliminaries
 echo "" > %errorfile%
 echo "" > %warningfile%
 
-echo           checking compilers
 ifort 1> stage0a.txt 2>&1
 type stage0a.txt | find /i /c "not recognized" > count0a.txt
 set /p nothaveFORTRAN=<count0a.txt
@@ -62,25 +64,44 @@ if %nothaveFORTRAN% == 1 (
   echo "***Fatal error: Fortran compiler not present"
   echo "***Fatal error: Fortran compiler not present" > %errorfile%
   echo "smokebot run aborted"
-  call :abort
+  call :output_abort_message
+  exit /b 1
 )
+echo             found Fortran
 
 icl 1> stage0b.txt 2>&1
 type stage0b.txt | find /i /c "not recognized" > count0b.txt
 set /p nothaveCC=<count0b.txt
+if %nothaveCC% == 1 (
+  set haveCC=0
+  echo "***Warning: C/C++ compiler not found - using installed Smokeview to generate images"
+) else (
+  echo             found C/C++
+)
+
+if NOT exist %emailexe% (
+  echo ***warning: email client not found.   
+  echo             Smokebot messages will only be sent to the console.
+) else (
+  echo             found a command line email client
+)
+
+call :is_file_installed pdflatex|| exit /b 1
+echo             found pdflatex
 
 :: update cfast repository
 
-echo           updating cfast repository
+echo             updating cfast repository
 cd %cfastroot%
 svn update  1> %OUTDIR%\stage0.txt 2>&1
 
 :: build cfast
 
-echo           building cfast
+echo             building cfast
 cd %cfastroot%\CFAST\intel_win_%size%
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage0.txt 2>&1
 make VPATH="../Source:../Include" INCLUDE="../Include" -f ..\makefile intel_win_%size% 1>> %OUTDIR%\stage0.txt 2>&1
+call :file_not_found cfast6_win_%size%.exe %OUTDIR%\stage0.txt|| exit /b 1
 
 :: -------
 :: Stage 1
@@ -99,20 +120,20 @@ set /p revision=<%revisionfile%
 :: -------
 
 echo Stage 2 - Building FDS (debug version)
-echo           serial
+echo             serial
 cd %svnroot%\FDS_Compilation\intel_win_%size%_db
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage2a.txt 2>&1
 make VPATH="../../FDS_Source" -f ..\makefile intel_win_%size%_db 1>> %OUTDIR%\stage2a.txt 2>&1
 
-call :file_not_found fds_win_%size%_db.exe %OUTDIR%\stage2a.txt
+call :file_not_found fds_win_%size%_db.exe %OUTDIR%\stage2a.txt|| exit /b 1
 call :find_string "remark warning" %OUTDIR%\stage2a.txt
 
-echo           parallel
+echo             parallel
 cd %svnroot%\FDS_Compilation\mpi_intel_win_%size%_db
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage2b.txt 2>&1
 make MPIINCLUDE="c:\mpich\mpich2_%size%\include" MPILIB="c:\mpich\mpich2_%size%\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_%size%_db 1>> %OUTDIR%\stage2b.txt 2>&1
 
-call :file_not_found fds_mpi_win_%size%_db.exe %OUTDIR%\stage2b.txt
+call :file_not_found fds_mpi_win_%size%_db.exe %OUTDIR%\stage2b.txt|| exit /b 1
 call :find_string "remark warning" %OUTDIR%\stage2b.txt
 
 :: -------
@@ -127,20 +148,20 @@ echo Stage 3 - Running verification cases (debug) (not implemented)
 
 echo Stage 4 - Building FDS (release version)
 
-echo           serial
+echo             serial
 cd %svnroot%\FDS_Compilation\intel_win_%size%
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage4a.txt 2>&1
 make VPATH="../../FDS_Source" -f ..\makefile intel_win_%size% 1>> %OUTDIR%\stage4a.txt 2>&1
 
-call :file_not_found fds_win_%size%.exe %OUTDIR%\stage4a.txt
+call :file_not_found fds_win_%size%.exe %OUTDIR%\stage4a.txt|| exit /b 1
 call :find_string "remark warning" %OUTDIR%\stage4a.txt
 
-echo           parallel
+echo             parallel
 cd %svnroot%\FDS_Compilation\mpi_intel_win_%size%
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage4b.txt 2>&1
 make MPIINCLUDE="c:\mpich\mpich2_%size%\include" MPILIB="c:\mpich\mpich2_%size%\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_%size%  1>> %OUTDIR%\stage4b.txt 2>&1
 
-call :file_not_found fds_mpi_win_%size%.exe %OUTDIR%\stage4b.txt
+call :file_not_found fds_mpi_win_%size%.exe %OUTDIR%\stage4b.txt|| exit /b 1
 call :find_string "remark warning" %OUTDIR%\stage4b.txt
 
 :: ----------
@@ -149,25 +170,38 @@ call :find_string "remark warning" %OUTDIR%\stage4b.txt
 
 echo Stage 5pre - Building Smokeview utilities
 
-echo              smokezip
-cd %svnroot%\Utilities\smokezip\intel_win_%size%
+echo                fds2ascii
+cd %svnroot%\Utilities\fds2ascii\intel_win_%size%
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage5pre.txt 2>&1
 make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
+call :file_not_found fds2ascii_win_%size%.exe %OUTDIR%\stage5pre.txt|| exit /b 1
 
-echo              smokediff
-cd %svnroot%\Utilities\smokediff\intel_win_%size%
-erase *.obj *.mod *.exe 1>> %OUTDIR%\stage5pre.txt 2>&1
-make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
+if %haveCC% == 1 (
+  echo                smokediff
+  cd %svnroot%\Utilities\smokediff\intel_win_%size%
+  erase *.obj *.mod *.exe 1>> %OUTDIR%\stage5pre.txt 2>&1
+  make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
+  call :file_not_found smokediff_win_%size%.exe %OUTDIR%\stage5pre.txt
 
-echo              wind2fds
-cd %svnroot%\Utilities\wind2fds\intel_win_%size%
-erase *.obj *.mod *.exe 1>> %OUTDIR%\stage5pre.txt 2>&1
-make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
+  echo                smokezip
+  cd %svnroot%\Utilities\smokezip\intel_win_%size%
+  erase *.obj *.mod *.exe 1>> %OUTDIR%\stage5pre.txt 2>&1
+  make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
+  call :file_not_found smokezip_win_%size%.exe %OUTDIR%\stage5pre.txt|| exit /b 1
 
-echo              fds2ascii
-cd %svnroot%\Utilities\fds2ascii\intel_win_%size%
-erase *.obj *.mod *.exe 1>> %OUTDIR%\stage5pre.txt 2>&1
-make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
+  echo                wind2fds
+  cd %svnroot%\Utilities\wind2fds\intel_win_%size%
+  erase *.obj *.mod *.exe 1>> %OUTDIR%\stage5pre.txt 2>&1
+  make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
+  call :file_not_found wind2fds_win_%size%.exe %OUTDIR%\stage5pre.txt|| exit /b 1
+) else (
+  call :is_file_installed smokediff|| exit /b 1
+  echo                smokediff not built, is installed
+  call :is_file_installed smokezip|| exit /b 1
+  echo                smokezip not built, is installed
+  call :is_file_installed wind2fds|| exit /b 1
+  echo                wind2fds not built, is installed
+)
 
 :: -------
 :: Stage 5
@@ -176,7 +210,8 @@ make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage5pre.txt 2>&1
 echo Stage 5 - Running verification cases (release)
 
 cd %svnroot%\Verification\scripts
-call run_smv_cases 1> %OUTDIR%\stage5.txt 2>&1
+echo call Run_SMV_cases %size%
+call Run_SMV_cases %size% 1> %OUTDIR%\stage5.txt 2>&1
 
 :: --------
 :: Stage 6a
@@ -188,7 +223,7 @@ cd %svnroot%\SMV\Build\intel_win_%size%_db
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage6a.txt 2>&1
 make -f ..\Makefile intel_win_%size%_db 1>> %OUTDIR%\stage6a.txt 2>&1
 
-call :file_not_found smokeview_win_%size%_db.exe %OUTDIR%\stage6a.txt
+call :file_not_found smokeview_win_%size%_db.exe %OUTDIR%\stage6a.txt|| exit /b 1
 call :find_string "remark warning" %OUTDIR%\stage6a.txt
 
 :: --------
@@ -207,7 +242,7 @@ cd %svnroot%\SMV\Build\intel_win_%size%
 erase *.obj *.mod *.exe 1> %OUTDIR%\stage6c.txt 2>&1
 make -f ..\Makefile intel_win_%size% 1>> %OUTDIR%\stage6c.txt 2>&1
 
-call :file_not_found smokeview_win_%size%.exe %OUTDIR%\stage6c.txt
+call :file_not_found smokeview_win_%size%.exe %OUTDIR%\stage6c.txt|| exit /b 1
 call :find_string "remark warning" %OUTDIR%\stage6c.txt
 
 :: --------
@@ -217,7 +252,7 @@ call :find_string "remark warning" %OUTDIR%\stage6c.txt
 echo Stage 6d - Making Smokeview pictures (release mode)
 
 cd %svnroot%\Verification\scripts
-call MAKE_SMV_pictures 1> %OUTDIR%\stage6d.txt 2>&1
+call MAKE_SMV_pictures %size% 1> %OUTDIR%\stage6d.txt 2>&1
 
 call :find_string "error" %OUTDIR%\stage6d.txt
 
@@ -228,15 +263,15 @@ call :find_string "error" %OUTDIR%\stage6d.txt
 echo Stage 8 - Building Smokeview guides
 
 cd %svnroot%\Manuals\SMV_User_Guide
-echo                 User
+echo             User
 call make_guide 1> %OUTDIR%\stage8.txt 2>&1
 
 cd %svnroot%\Manuals\SMV_Technical_Reference_Guide
-echo                 Technical Reference
+echo             Technical Reference
 call make_guide 1>> %OUTDIR%\stage8.txt 2>&1
 
 cd %svnroot%\Manuals\SMV_Verification_Guide
-echo                 Verification
+echo             Verification
 call make_guide 1>> %OUTDIR%\stage8.txt 2>&1
 
 echo smokebot build success on %COMPUTERNAME% > %infofile%
@@ -247,19 +282,41 @@ cd %CURDIR%
 :: Wrap up
 :: -------
 
-if %havewarnings% == 0 (
-  call %email% %mailToSMV% "smokebot build success on %COMPUTERNAME% %revision%" %infofile%
-)
-if %havewarnings% GTR 0 (
-  %email% %mailToSMV% "smokebot build success with warnings on %COMPUTERNAME% %revision%" %warningfile%
+if exist %emailexe% (
+  if %havewarnings% == 0 (
+    call %email% %mailToSMV% "smokebot build success on %COMPUTERNAME% %revision%" %infofile%  
+  )
+  if %havewarnings% GTR 0 (
+    %email% %mailToSMV% "smokebot build success with warnings on %COMPUTERNAME% %revision%" %warningfile%
+  )
 )
 
 echo smokebot_win completed
 goto eof
 
-:abort
-  call %email% %mailToSMV% "smokebot build failure on %COMPUTERNAME% %revision%" %errorfile%
-goto eof
+:output_abort_message
+  echo "***Fatal error: smokebot build failure on %COMPUTERNAME% %revision%"
+  if exist %email% (
+    call %email% %mailToSMV% "smokebot build failure on %COMPUTERNAME% %revision%" %errorfile%
+  )
+exit /b
+
+:: -----------------------------------------
+:is_file_installed
+:: -----------------------------------------
+
+  set program=%1
+  %program% -help 1>> exist.txt 2>&1
+  type exist.txt | find /i /c "not recognized" > count.txt
+  set /p nothave=<count.txt
+  if %nothave% == 1 (
+    echo "***Fatal error: %program% not present"
+    echo "***Fatal error: %program% not present" > %errorfile%
+    echo "smokebot run aborted"
+    call :output_abort_message
+    exit /b 1
+  )
+  exit /b 0
 
 :: -----------------------------------------
   :file_not_found
@@ -271,9 +328,10 @@ set outputfile=%2
 if NOT exist %file% (
   echo ***fatal error: problems with compilation, aborting smokebot
   type %outputfile% >> %errorfile%
-  call :abort
+  call :output_abort_message
+  exit /b 1
 )
-exit /b
+exit /b 0
 
 :: -----------------------------------------
   :find_string
