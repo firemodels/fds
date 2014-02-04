097f972492cd5497f4b3c9e18f09f73ffc5fbf64
gforney <gforney@gmail.com>
2014-02-04 01:00:58 +0000

    smv vv: add batchfile to run smokebot

---
+++ SMV/scripts/RUNwindowssmokebot.bat
@@ -0,0 +1,27 @@
+@echo off
+Title Running Windows Smokebot
+
+
+Rem  Windows batch file to package a test Linux Smokeview
+
+Rem setup environment variables (defining where repository resides etc) 
+
+set envfile="%userprofile%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use SMV/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+cd %userprofile%\SMOKEBOT
+smokebot.lnk 1
+
+pause
