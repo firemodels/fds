8095445545f066b8e00506475bc52d09bac553fc
Kristopher Overholt <koverholt@gmail.com>
2014-02-06 21:52:34 +0000

    FDS Validation: Add NIST_He_2009 back to active validation cases
    Former-commit-id: 6c02d116c0038542650b9512863380830ed9604c [formerly d4bf0b61cdd1de3737df010f8aa729f38dd53e4c]
    Former-commit-id: 4f560e49ad87b9f8cf706cbcf1ac87e31941d936
    Former-commit-id: 5061f570e3f82bc463262046ee9a3493a830d840

--- Validation/Process_All_Output.sh
+++ Validation/Process_All_Output.sh
@@ -31,7 +31,7 @@ $VDIR/McCaffrey_Plume/FDS_Output_Files/Process_Output.csh
 $VDIR/Moody_Chart/FDS_Output_Files/Process_Output.csh
 $VDIR/NBS_Multi-Room/FDS_Output_Files/Process_Output.csh
 #$VDIR/NIJ_HAI_Pool/FDS_Output_Files/Process_Output.csh
-#$VDIR/NIST_He_2009/FDS_Output_Files/Process_Output.csh
+$VDIR/NIST_He_2009/FDS_Output_Files/Process_Output.csh
 $VDIR/NIST_Smoke_Alarms/FDS_Output_Files/Process_Output.csh
 $VDIR/NIST_NRC/FDS_Output_Files/Process_Output.csh
 $VDIR/NIST_RSE_1994/FDS_Output_Files/Process_Output.csh
