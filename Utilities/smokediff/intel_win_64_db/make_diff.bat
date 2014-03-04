d6366a24ded93ed03742e1c75d8a2bdd2cfa7a45
gforney <gforney@gmail.com>
2014-03-04 21:41:24 +0000

    smokediff: implement debug makefile entry
    Former-commit-id: e548714373b2f1c30d0c836e89bb30a51a4be219 [formerly d82665f02f686191555460c661cb7fe7dee443dd]
    Former-commit-id: 2d3e93cbe93e512c164e17f6333c02bf4373e249
    Former-commit-id: ac9a76287ff8c9f23dc137b33116275cd70e106a

---
+++ Utilities/smokediff/intel_win_64_db/make_diff.bat
@@ -0,0 +1,16 @@
+@echo off
+
+Rem windows batch file to build smokediff from the command line
+
+IF "%SETUP_IFORT_COMPILER_INTEL64%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER_INTEL64=1
+
+echo Setting up compiler environment
+call "%IFORT_COMPILER14%\bin\compilervars" intel64
+
+:envexist
+erase *.obj *.mod
+make -f ..\Makefile intel_win_64_db
+pause
+
