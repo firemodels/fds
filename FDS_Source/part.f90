422356221812f5e386d4d893c7e2327d94e494ed
drjfloyd <drjfloyd@gmail.com>
2014-03-06 19:19:52 +0000

    FDS Source: Remove old code from part.f90
    Former-commit-id: de0c45643abc1cf380fa5c75c041be683e7d16bc [formerly 79c6064866d64c01e05eb22fda04fbb9920e6989]
    Former-commit-id: 19108b3d4e52b2be822dd21a6ccc11e08d294c6b
    Former-commit-id: 298dde53ba53941a8bbe09535703ecba1e2ef5f9

--- FDS_Source/part.f90
+++ FDS_Source/part.f90
@@ -2253,7 +2253,7 @@ SPECIES_LOOP: DO Z_INDEX = 1,N_TRACKED_SPECIES
          TMP_G_NEW = TMP_G
          ITMP     = INT(TMP_DROP_NEW)
          TMP_WGT  = TMP_DROP_NEW - AINT(TMP_DROP_NEW)
-         H_NEW = H_G_OLD -Q_CON_GAS*WGT+WGT*M_VAP*(H_V+H_L)!+ H_D_OLD*WGT + Q_RAD - M_DROP*H_L*WGT + Q_CON_WALL
+         H_NEW = H_G_OLD -Q_CON_GAS*WGT+WGT*M_VAP*(H_V+H_L)
          IF (H_NEW > 0._EB) THEN
             ZZ_GET2 = ZZ_GET * M_GAS/M_GAS_NEW               
             ZZ_GET2(Z_INDEX) = ZZ_GET2(Z_INDEX) + WGT*M_VAP/M_GAS_NEW
