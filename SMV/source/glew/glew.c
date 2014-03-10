beb8166e0b7c1f08b95296b49bc354b7cd29a52e
gforney <gforney@gmail.com>
2014-03-10 19:37:47 +0000

    smokeview source: fix glew headers for Linux

--- SMV/source/glew/glew.c
+++ SMV/source/glew/glew.c
@@ -35,7 +35,7 @@
 #if defined(_WIN32)
 #  include "wglew.h"
 #elif !defined(__ANDROID__) && !defined(__native_client__) && (!defined(__APPLE__) || defined(GLEW_APPLE_GLX))
-#  include <GL/glxew.h>
+#  include "glxew.h"
 #endif
 
 /*
