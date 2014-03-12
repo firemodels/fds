d8de7ec12b71d11002c48b7a5381254e4109b262
gforney <gforney@gmail.com>
2014-03-12 00:35:47 +0000

    glui2p36 source: comment out two template class lines causing problems for library build (onlyi needed to build dll)

--- SMV/source/glui_v2_36/GL/glui.h
+++ SMV/source/glui_v2_36/GL/glui.h
@@ -1715,8 +1715,8 @@ public:
 
     #ifdef _MSC_VER
     // Explicit template instantiation needed for dll
-    template class GLUIAPI std::allocator<GLUI_String>;
-    template class GLUIAPI std::vector<GLUI_String, std::allocator<GLUI_String> >;
+    //template class GLUIAPI std::allocator<GLUI_String>;
+    //template class GLUIAPI std::vector<GLUI_String, std::allocator<GLUI_String> >;
     #endif
 
     std::vector<GLUI_String> hist_list;
