933b64de887948c9d159c0420287df58bea08fd2
gforney <gforney@gmail.com>
2014-03-14 01:44:58 +0000

    smv vv: add support for OSX in startXserver (preliminary - not tested)
    Former-commit-id: 1c3b0ec8e9452801278069a38e338871cce3ecbf [formerly 9c39ab9115a5a111c25b0359f99c3359b489cc68]
    Former-commit-id: 4e50bc27a3c39b43fcba7e997881a392092af9e5
    Former-commit-id: 8d6aab649120762330496c9cbb0e573f89c625d9

--- Utilities/Scripts/startXserver.sh
+++ Utilities/Scripts/startXserver.sh
@@ -15,11 +15,13 @@ GETNEWPORT ()
   done
 }
 
+XVFB=Xvfb
 OS=`uname`
-if [ "$OS" != "Darwin" ]; then
-  GETNEWPORT 
-  Xvfb :$display_port -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
-  export SMV_ID=$!
-  export DISPLAY=:$display_port
-  rm $lockfile
+if [ "$OS" == "Darwin" ]; then
+  XVFB=xvfb
 fi
+GETNEWPORT 
+$XVFB :$display_port -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
+export SMV_ID=$!
+export DISPLAY=:$display_port
+rm $lockfile
