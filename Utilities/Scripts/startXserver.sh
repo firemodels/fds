4671a27f948be40d0aba5c5f8704fec6116f9d11
gforney <gforney@gmail.com>
2014-03-14 17:54:18 +0000

    smv vv: modify startXserver for OSX, create killXservers to kill all Xvfb instances (for OSX)
    Former-commit-id: 6d67e57f5d677e6d0f787d7b56e0cbda02a218b3 [formerly 0a527735ab8e0a283de41d525f3df0ecfa73c7e1]
    Former-commit-id: 2bc06028d138bfd4dbde24e972072c62dd67e9d2
    Former-commit-id: 91165641318b093db80f2e63e70ab4a129d5dff5

--- Utilities/Scripts/startXserver.sh
+++ Utilities/Scripts/startXserver.sh
@@ -8,20 +8,20 @@ GETNEWPORT ()
   touch $lockfile
   chmod 777 $lockfile
   display_port=`id -u`
-  nmatches=`ps a -e | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
+  nmatches=`ps a -e | grep $XVFB | grep $display_port | grep -v grep | wc | awk '{print $1}'`
   while [ $nmatches -ne 0 ] ; do
     display_port=`expr $display_port + 1`
-    nmatches=`ps a -e | grep Xvfb | grep $display_port | grep -v grep | wc | awk '{print $1}'`
+    nmatches=`ps a -e | grep $XVFB | grep $display_port | grep -v grep | wc | awk '{print $1}'`
   done
 }
 
-XVFB=Xvfb
 OS=`uname`
-if [ "$OS" == "Darwin" ]; then
-  XVFB=xvfb
+if [ "`uname`" == "Darwin" ]; then
+   XVFB=/usr/X11/bin/Xvfb
 fi
 GETNEWPORT 
 $XVFB :$display_port -fp /usr/share/X11/fonts/misc -screen 0 1280x1024x24 &
 export SMV_ID=$!
 export DISPLAY=:$display_port
+echo $SMV_ID > /tmp/test.out
 rm $lockfile
