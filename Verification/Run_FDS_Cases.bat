24f465a581b5be32572e1a54b87812ba0d1eb9d9
gforney <gforney@gmail.com>
2014-03-20 14:18:30 +0000

    FDS vv: check that FDS cases have completed before finishing script
    Former-commit-id: 82fa405242ad4df97091dfe97f9eabfd338c73a8 [formerly 05c1050c37fec23e62015a1137a1e8b9c8067c9f]
    Former-commit-id: 1e62ac31a9b6f6d960329c4e29810b1273195794
    Former-commit-id: e9709d544f2bae70cf6b2e19630707e6c352c322

--- Verification/Run_FDS_Cases.bat
+++ Verification/Run_FDS_Cases.bat
@@ -3,6 +3,7 @@
 set OPENMP=
 
 :: to use openmp (assuming you have compiled the openmp exe's), uncomment the follwoing two "set" lines
+:: and set the number of threads to what you want (or leave commented to use number of cores available)
 :: set OPENMP=openmp_
 :: set OMP_NUM_THREADS=2
 
@@ -10,11 +11,20 @@ set BASEDIR="%CD%"
 set SVNROOT=%BASEDIR%\..\
 set TIME_FILE="%BASEDIR%\fds_case_times.txt"
 
-:: DO NOT erase or comment following line when committing to repository
+:: VVVVVVVVVVVVVVVVVVVVVV define FDS exe location VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+:: DO NOT erase or comment the following two line when committing to repository
+
+set FDSBASE=fds_%OPENMP%win_64.exe
 set FDSEXE=%SVNROOT%\FDS_Compilation\%OPENMP%intel_win_64\fds_%OPENMP%win_64.exe
 
-:: if you wish to use a custom FDS, comment the above "set" and uncomment and customize the below "set"
+:: if you wish to use a custom FDS, comment the above two "set" lines and uncomment
+:: and customize the below two "set" lines
+
+:: set FDSBASE=fds_win_64.exe
 :: set FDSEXE=C:\PROJECTS\FDS_DEVEL\6.0a\fds_win_64
+
+:: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 set BACKGROUNDEXE=%SVNROOT%\Utilities\background\intel_win_32\background.exe
 
@@ -50,6 +60,23 @@ call FDS_Cases.bat
 
 call FDS_MPI_Cases.bat
 
+:: loop until all FDS cases have finished
+
+:loop1
+tasklist | find /i /c "%FDSBASE%" > temp.out
+set /p numexe=<temp.out
+echo Number of cases running - %numexe%
+if %numexe% == 0 goto finished
+Timeout /t 30 >nul 
+goto loop1
+
+:finished
+echo "FDS cases completed"
+goto eof
+
+::  for possible future edits, put any batch subroutines added between here and eof statement below
+
+:eof
 echo "FDS test cases end" >> %TIME_FILE%
 date /t >> %TIME_FILE%
 time /t >> %TIME_FILE%
