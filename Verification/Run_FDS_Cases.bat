3b1046c7acf1b10b27ce7d112b7c3264ee4ec480
gforney <gforney@gmail.com>
2014-03-21 01:28:39 +0000

    FDS vv: add some error checking to FDS verification test batch file
    Former-commit-id: 1d2016521493bd21152c470fe4b986b7ed38f31b [formerly f7a18afc0a37b8b9b2167373d4bfe8885eeea0c2]
    Former-commit-id: b9d16b89c8ccba97bed68a34e4f1e3dd734cce9b
    Former-commit-id: 05d39ce3b5d2bcc6f837864a8dd599edc91fcfdc

--- Verification/Run_FDS_Cases.bat
+++ Verification/Run_FDS_Cases.bat
@@ -1,40 +1,35 @@
 @echo off
 
 set OPENMP=
+set BASEDIR="%CD%"
+cd ..
+set SVNROOT="%CD%"
+cd %BASEDIR%
+set TIME_FILE="%BASEDIR%\fds_case_times.txt"
 
-:: to use openmp (assuming you have compiled the openmp exe's), uncomment the follwoing two "set" lines
-:: and set the number of threads to what you want (or leave commented to use number of cores available)
+::*** uncomment following two lines to use openmp
+
 :: set OPENMP=openmp_
 :: set OMP_NUM_THREADS=2
 
-set BASEDIR="%CD%"
-set SVNROOT=%BASEDIR%\..\
-set TIME_FILE="%BASEDIR%\fds_case_times.txt"
+:: default FDS location
 
-:: VVVVVVVVVVVVVVVVVVVVVV define FDS exe location VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-
-:: DO NOT erase or comment the following two line when committing to repository
-
-set FDSBASE=fds_%OPENMP%win_64.exe
 set FDSEXE=%SVNROOT%\FDS_Compilation\%OPENMP%intel_win_64\fds_%OPENMP%win_64.exe
 
-:: if you wish to use a custom FDS, comment the above two "set" lines and uncomment
-:: and customize the below two "set" lines
+::*** uncomment following line to define a custom FDS location - (Do not commit un-commented)
 
-:: set FDSBASE=fds_win_64.exe
 :: set FDSEXE=C:\PROJECTS\FDS_DEVEL\6.0a\fds_win_64
 
-:: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+if not exist %FDSEXE%  (
+  echo "***error: The program, %FDSEXE% , was not found.  Verification test runs aborted."
+  goto eof2
+)
+
+call :getfilename %FDSEXE% 
+set FDSBASE=%file%
 
 set BACKGROUNDEXE=%SVNROOT%\Utilities\background\intel_win_32\background.exe
-
-Rem Choose one of the following run options by commenting the line you don't want to use.
-Rem Use the first "FDS" definition to run one case at a time in the forground.
-Rem Use the second "FDS" definition to execute more than one case at a time in the background
-
-Rem set FDS=%FDSEXE%
 set FDS=%BACKGROUNDEXE% -u 75 -d 10 %FDSEXE%
-
 set RUNFDS=call %SVNROOT%\Utilities\Scripts\runfds_win32.bat
 set RUNFDSMPI=call %SVNROOT%\Utilities\Scripts\runfdsmpi_win32.bat
 
@@ -74,7 +69,9 @@ goto loop1
 echo "FDS cases completed"
 goto eof
 
-::  for possible future edits, put any batch subroutines added between here and eof statement below
+:getfilename
+set file=%~nx1
+exit /b
 
 :eof
 echo "FDS test cases end" >> %TIME_FILE%
@@ -83,5 +80,7 @@ time /t >> %TIME_FILE%
 
 erase FDS_Cases.bat
 erase FDS_MPI_Cases.bat
+
+:eof2
 
 pause
