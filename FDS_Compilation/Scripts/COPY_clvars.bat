89cafa1f5227e14490b9dda51514bc80b42ed30c
gforney <gforney@gmail.com>
2014-03-24 02:44:58 +0000

    build scripts remove unused scripts
    Former-commit-id: 29b72017f44c2e49cbcd95c50d67a82fc4868b65 [formerly 281be0656fb7206ec3d38517484be101245f93ba]
    Former-commit-id: 9021470c8333cae8c44a0ee6d4c3145712d39a69
    Former-commit-id: 5b34a2d7f6d206e70bfa13b8edc1341134da6a06

--- FDS_Compilation/Scripts/COPY_clvars.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo copying clvars.bat to c:\bin
-echo press any key to proceed or ^<CTRL^> C to abort
-pause > NUL
-copy /-Y clvars.bat c:\bin
-echo copy complete.  press any key to finish.
-pause > NUL
--- FDS_Compilation/Scripts/clvars.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\Microsoft Visual Studio 8\VC\"vcvarsall x86
