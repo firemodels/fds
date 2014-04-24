8fb5802da00e2cbd6634c664e901479c7d4db0ac
gforney <gforney@gmail.com>
2014-04-24 18:47:41 +0000

    fds vv: remove geomtest build from firebot
    Former-commit-id: 09024b6590d1b8f2cb949fa9d303cee594a91fb9 [formerly 401604ad4e564f99bbe637d01905c16ab878fec7]
    Former-commit-id: 2de4b4d6166688e4f6c428cee45831f7866d0797
    Former-commit-id: e1dec79046cd16a0b774865ae885c67c6027007d

--- Utilities/Firebot/firebot_mac.sh
+++ Utilities/Firebot/firebot_mac.sh
@@ -236,14 +236,6 @@ compile_fds_db()
    cd $FDS_SVNROOT/FDS_Compilation/intel_osx_64_db
    make -f ../makefile clean &> /dev/null
    ./make_fds.sh &> $FIREBOT_DIR/output/stage2a
-
-   # geomtest: (these lines will be removed, once geomtest capabilities
-   #            have been added to FDS)
-   cd $FDS_SVNROOT/SMV/source/geomtest/intel_osx_64
-   rm -f *.o geomtest
-   echo 'Compiling geomtest:' >> $FIREBOT_DIR/output/stage2a 2>&1
-   ./make_geom.sh >> $FIREBOT_DIR/output/stage2a 2>&1
-   echo "" >> $FIREBOT_DIR/stage2a 2>&1
 }
 
 check_compile_fds_db()
