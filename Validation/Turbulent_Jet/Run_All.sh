9bc49bfc3922e10b30aa12ed46c7f91647833f1b
mcgratta <mcgratta@gmail.com>
2014-05-17 19:03:35 +0000

    FDS Validation: More results for 6.1.0

--- Validation/Turbulent_Jet/Run_All.sh
+++ Validation/Turbulent_Jet/Run_All.sh
@@ -10,9 +10,9 @@ $QFDS $DEBUG -r $QUEUE -d $INDIR jet_dsmag_dx10cm.fds
 $QFDS $DEBUG -r $QUEUE -d $INDIR jet_deardorff_dx10cm.fds
 $QFDS $DEBUG -r $QUEUE -d $INDIR jet_vreman_dx10cm.fds
 
-$QFDS $DEBUG -p 73 -r $QUEUE -d $INDIR jet_csmag_dx5cm.fds
-$QFDS $DEBUG -p 73 -r $QUEUE -d $INDIR jet_dsmag_dx5cm.fds
-$QFDS $DEBUG -p 73 -r $QUEUE -d $INDIR jet_deardorff_dx5cm.fds
-$QFDS $DEBUG -p 73 -r $QUEUE -d $INDIR jet_vreman_dx5cm.fds
+$QFDS $DEBUG -p 37 -r $QUEUE -d $INDIR jet_csmag_dx5cm.fds
+$QFDS $DEBUG -p 37 -r $QUEUE -d $INDIR jet_dsmag_dx5cm.fds
+$QFDS $DEBUG -p 37 -r $QUEUE -d $INDIR jet_deardorff_dx5cm.fds
+$QFDS $DEBUG -p 37 -r $QUEUE -d $INDIR jet_vreman_dx5cm.fds
 
 echo FDS cases submitted
