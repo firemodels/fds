6a6215283e11beeef8ce77a27c8aa21d016de9cf
gforney <gforney@gmail.com>
2014-05-21 13:35:26 +0000

    FDS vv: add 128x128x128 timing test cases to vv suite
    Former-commit-id: d9a6c556bb3d281f2de71999f69277c646ffce1c [formerly e7c7200d13b8f7f97182cab1fa4f97d7fe58a318]
    Former-commit-id: 941cec408f7f7364b26f0fd806e8a0fc145148a8
    Former-commit-id: d0bfb40b87af1f164585a6f08b90f8cd07a5d331

--- Verification/Timing_Benchmarks/makecases.sh
+++ Verification/Timing_Benchmarks/makecases.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-./makecase.sh test1
-./makecase.sh test2
-./makecase.sh test3
-./makecase.sh test4
-./makecase.sh test5
-./makecase.sh test6
-./makecase.sh test7
-./makecase.sh test8
+#  1.  after making changes to the template input file in
+#      makecase.sh, run this script
+#  2.  then commit the updated test...fds input files to the repository
+
+./makecase.sh 64 test64a
+./makecase.sh 64 test64b
+./makecase.sh 64 test64c
+./makecase.sh 64 test64d
+./makecase.sh 64 test64e
+./makecase.sh 64 test64f
+./makecase.sh 64 test64g
+./makecase.sh 64 test64h
+./makecase.sh 128 test128a
+./makecase.sh 128 test128b
+./makecase.sh 128 test128c
+./makecase.sh 128 test128d
+./makecase.sh 128 test128e
+./makecase.sh 128 test128f
+./makecase.sh 128 test128g
+./makecase.sh 128 test128h
