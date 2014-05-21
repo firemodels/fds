8a4589590360e53f341ab45bb10644af7d40d8f3
gforney <gforney@gmail.com>
2014-05-21 15:30:47 +0000

    FDS vv rename openmp timing test cases
    Former-commit-id: 6ff7b9b33d6d3a5c5d31ce80a93f1483698d0196 [formerly 595cc81b393a7e98765835edff252bdcbc11d49d]
    Former-commit-id: c3625b244dcc1813e840c847f6f5c837b1422cca
    Former-commit-id: de277cbfb22d55ad07867d738da3ac4d00744943

--- Verification/Timing_Benchmarks/makecases.sh
+++ Verification/Timing_Benchmarks/makecases.sh
@@ -3,19 +3,19 @@
 #      makecase.sh, run this script
 #  2.  then commit the updated test...fds input files to the repository
 
-./makecase.sh 64 test64a
-./makecase.sh 64 test64b
-./makecase.sh 64 test64c
-./makecase.sh 64 test64d
-./makecase.sh 64 test64e
-./makecase.sh 64 test64f
-./makecase.sh 64 test64g
-./makecase.sh 64 test64h
-./makecase.sh 128 test128a
-./makecase.sh 128 test128b
-./makecase.sh 128 test128c
-./makecase.sh 128 test128d
-./makecase.sh 128 test128e
-./makecase.sh 128 test128f
-./makecase.sh 128 test128g
-./makecase.sh 128 test128h
+./makecase.sh 64 openmp_test64a
+./makecase.sh 64 openmp_test64b
+./makecase.sh 64 openmp_test64c
+./makecase.sh 64 openmp_test64d
+./makecase.sh 64 openmp_test64e
+./makecase.sh 64 openmp_test64f
+./makecase.sh 64 openmp_test64g
+./makecase.sh 64 openmp_test64h
+./makecase.sh 128 openmp_test128a
+./makecase.sh 128 openmp_test128b
+./makecase.sh 128 openmp_test128c
+./makecase.sh 128 openmp_test128d
+./makecase.sh 128 openmp_test128e
+./makecase.sh 128 openmp_test128f
+./makecase.sh 128 openmp_test128g
+./makecase.sh 128 openmp_test128h
