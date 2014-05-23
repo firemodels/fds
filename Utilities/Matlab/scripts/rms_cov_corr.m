fb143c24b75d5208a5bf5c5bb0f3d04e547a4285
drjfloyd <drjfloyd@gmail.com>
2014-05-23 17:37:02 +0000

    FDS Verification: Matlab verification script for rms_cov_corr
    Former-commit-id: 034d35c78d0c9373859384775aea7daf62abb537 [formerly edfe53ec7fa5efda6683e343c792cba6bf4556f2]
    Former-commit-id: 3082dcf79ba778f6d608d54a99ac706975862aae
    Former-commit-id: f4131c65b550e0e6b60ddbc835bb17f8548e1ff4

---
+++ Utilities/Matlab/scripts/rms_cov_corr.m
@@ -0,0 +1,165 @@
+% Floyd
+% 5-23-2014
+% rms_cov_corr_hood.m
+
+close all
+clear all
+
+addpath('../../Verification/Controls');
+
+% load experimental data and FDS prediction
+[fds_data] = csvread('rms_cov_corr_devc.csv',2);
+
+startrow=60/0.02+1;
+endrow=size(fds_data,1);
+
+umean=mean(fds_data(startrow:endrow,2));
+wmean=mean(fds_data(startrow:endrow,3));
+
+udiff=fds_data(startrow:endrow,2)-umean;
+wdiff=fds_data(startrow:endrow,3)-wmean;
+
+udiff2=udiff;
+wdiff2=wdiff;
+uwcova=udiff;
+for i=1:endrow-startrow+1
+    udiff2(i)=udiff(i)*udiff(i);
+    wdiff2(i)=wdiff(i)*wdiff(i);
+    uwcova(i)=udiff(i)*wdiff(i);
+    drawnow;
+end
+
+urms=sqrt(mean(udiff2));
+wrms=sqrt(mean(wdiff2));
+uwcov=mean(uwcova);
+uwcorr=uwcov/urms/wrms;
+
+startrow2=500/0.02+1;
+urms_fds=mean(fds_data(startrow2:endrow,4));
+uwcov_fds=mean(fds_data(startrow2:endrow,5));
+uwcorr_fds=mean(fds_data(startrow2:endrow,6));
+
+xcalc(1)=60;
+xcalc(2)=1000;
+ycalc(1)=urms;
+ycalc(2)=urms;
+maxval= max(fds_data(:,4));
+maxval=ceil(maxval*10)/10;
+
+h=plot(xcalc,ycalc,'r-',fds_data(:,1),fds_data(:,4),'k-','LineWidth',1.5);
+hold on
+axis([0 1000 0 maxval])
+plot_style
+set(gca,'Units',Plot_Units)
+set(gca,'FontName',Font_Name)
+set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
+xlabel('Time(s)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+ylabel('urms (m/s)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+legend('Analytic','FDS','Location','NorthEast')
+
+svn_file = '../../Verification/Controls/rms_cov_corr_svn.txt';
+
+if exist(svn_file,'file')
+    SVN = importdata(svn_file);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
+
+% print to pdf
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+plotname = ['../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/rms_cov_corr_rms'];
+print(gcf,'-dpdf',plotname);
+
+hold off
+clear h
+
+ycalc(1)=uwcov;
+ycalc(2)=uwcov;
+maxval= max(fds_data(:,5));
+maxval=ceil(maxval*10)/10;
+
+h=plot(xcalc,ycalc,'r-',fds_data(:,1),fds_data(:,5),'k-','LineWidth',1.5);
+hold on
+axis([0 1000 0 maxval])
+plot_style
+set(gca,'Units',Plot_Units)
+set(gca,'FontName',Font_Name)
+set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
+xlabel('Time(s)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+ylabel('u-w convariance (m^2/s^2)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+legend('Analytic','FDS','Location','NorthEast')
+
+if exist(svn_file,'file')
+    SVN = importdata(svn_file);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
+
+% print to pdf
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+plotname = ['../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/rms_cov_corr_cov'];
+print(gcf,'-dpdf',plotname);
+
+hold off
+clear h
+
+ycalc(1)=uwcorr;
+ycalc(2)=uwcorr;
+maxval= max(fds_data(:,6));
+maxval=ceil(maxval*10)/10;
+
+h=plot(xcalc,ycalc,'r-',fds_data(:,1),fds_data(:,6),'k-','LineWidth',1.5);
+hold on
+axis([0 1000 0 maxval])
+plot_style
+set(gca,'Units',Plot_Units)
+set(gca,'FontName',Font_Name)
+set(gca,'Position',[Plot_X,Plot_Y,Plot_Width,Plot_Height])
+xlabel('Time(s)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+ylabel('u-w cross correlation','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+legend('Analytic','FDS','Location','NorthEast')
+
+if exist(svn_file,'file')
+    SVN = importdata(svn_file);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
+
+% print to pdf
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+plotname = ['../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/rms_cov_corr_corr'];
+print(gcf,'-dpdf',plotname);
+
+clear h
+
+% check errors
+if abs((urms-urms_fds)/urms) > 0.05
+   display(['Matlab Warning: urms in rms_cov_corr is out of tolerance.'])
+end
+if abs((uwcov-uwcov_fds)/urms) > 0.05
+   display(['Matlab Warning: uwcov in rms_cov_corr is out of tolerance.'])
+end
+if abs((uwcorr-uwcorr_fds)/urms) > 0.05
+   display(['Matlab Warning: uwcorr in rms_cov_corr is out of tolerance.'])
+end
