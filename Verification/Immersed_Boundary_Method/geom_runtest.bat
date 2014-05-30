4ccc4b2580bc8677deaec3a9095e1d7ce590d3d1
gforney <gforney@gmail.com>
2014-05-30 15:07:00 +0000

    smv vv: add batch files for running geom tests

---
+++ Verification/Immersed_Boundary_Method/geom_runtest.bat
@@ -0,0 +1,20 @@
+@echo off
+set fds=call ..\..\..\bin\fds_db
+%fds% geom_arch.fds
+%fds% geom_azim.fds
+%fds% geom_elev.fds
+%fds% geom_obst.fds
+%fds% geom_scale.fds
+%fds% geom_simple.fds
+%fds% geom_sphere.fds
+%fds% geom_sphere2.fds
+%fds% geom_terrain.fds
+%fds% geom_texture.fds
+%fds% geom_texture2.fds
+%fds% geom_texture3.fds
+%fds% geom_time.fds
+%fds% geom_time2.fds
+%fds% geom_time3.fds
+%fds% geom_time4.fds
+%fds% geom_time5.fds
+%fds% geom_volume.fds
