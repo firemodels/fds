540d29cb78e8c16333790645fd52f8f312dba356
gforney <gforney@gmail.com>
2014-05-30 15:20:03 +0000

    smv vv: add bash scripts for running geometry tests
    Former-commit-id: ef8ab5480addd58cecb183692d5ebde6fcdf79ad [formerly e722e5e3b0839cdbcc4ef4293d0457f38cf0e4f6]
    Former-commit-id: a5f0e4d2e896c9825ca57915f1a37cd1be81b5aa
    Former-commit-id: 7f3de282263db322ea511d0c07c895d700f141f0

---
+++ Verification/Immersed_Boundary_Method/geom_vistest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+smv=smokeview
+$smv geom_arch
+$smv geom_azim
+$smv geom_elev
+$smv geom_obst
+$smv geom_scale
+$smv geom_simple
+$smv geom_sphere
+$smv geom_sphere2
+$smv geom_terrain
+$smv geom_texture
+$smv geom_texture2
+$smv geom_texture3
+$smv geom_time
+$smv geom_time2
+$smv geom_time3
+$smv geom_time4
+$smv geom_time5
+$smv geom_volume
