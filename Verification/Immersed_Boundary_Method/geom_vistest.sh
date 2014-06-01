48bc567c71b4becb42c231f0de6588534eee4a5e
gforney <gforney@gmail.com>
2014-06-01 17:02:46 +0000

    geom notes/vv: update sphere texture figure

--- Verification/Immersed_Boundary_Method/geom_vistest.sh
+++ Verification/Immersed_Boundary_Method/geom_vistest.sh
@@ -11,7 +11,8 @@ $smv geom_sphere2
 $smv geom_terrain
 $smv geom_texture
 $smv geom_texture2
-$smv geom_texture3
+$smv geom_texture3a
+$smv geom_texture3b
 $smv geom_time
 $smv geom_time2
 $smv geom_time3
