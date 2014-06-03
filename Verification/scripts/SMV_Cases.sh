2f0f47af75c90625bb01b5e9cf3dd7bd100781d7
gforney <gforney@gmail.com>
2014-06-03 15:07:59 +0000

    geom vv: add -g option to run/visualize only geometry cases

--- Verification/scripts/SMV_Cases.sh
+++ Verification/scripts/SMV_Cases.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
-$RUNFDS Immersed_Boundary_Method geom_arch
-$RUNFDS Immersed_Boundary_Method geom_azim
-$RUNFDS Immersed_Boundary_Method geom_elev
-$RUNFDS Immersed_Boundary_Method geom_obst
-$RUNFDS Immersed_Boundary_Method geom_scale
-$RUNFDS Immersed_Boundary_Method geom_simple
-$RUNFDS Immersed_Boundary_Method geom_sphere1a
-$RUNFDS Immersed_Boundary_Method geom_sphere1b
-$RUNFDS Immersed_Boundary_Method geom_sphere1c
-$RUNFDS Immersed_Boundary_Method geom_sphere1d
-$RUNFDS Immersed_Boundary_Method geom_sphere1e
-$RUNFDS Immersed_Boundary_Method geom_sphere1f
-$RUNFDS Immersed_Boundary_Method geom_sphere2
-$RUNFDS Immersed_Boundary_Method geom_sphere3a
-$RUNFDS Immersed_Boundary_Method geom_sphere3b
-$RUNFDS Immersed_Boundary_Method geom_sphere3c
-$RUNFDS Immersed_Boundary_Method geom_sphere3d
-$RUNFDS Immersed_Boundary_Method geom_sphere3e
-$RUNFDS Immersed_Boundary_Method geom_sphere3f
-$RUNFDS Immersed_Boundary_Method geom_sphere_fire
-$RUNFDS Immersed_Boundary_Method geom_terrain
-$RUNFDS Immersed_Boundary_Method geom_texture
-$RUNFDS Immersed_Boundary_Method geom_texture2
-$RUNFDS Immersed_Boundary_Method geom_texture3a
-$RUNFDS Immersed_Boundary_Method geom_texture3b
-$RUNFDS Immersed_Boundary_Method geom_texture4a
-$RUNFDS Immersed_Boundary_Method geom_texture4b
-$RUNFDS Immersed_Boundary_Method geom_time
-$RUNFDS Immersed_Boundary_Method geom_time2
-$RUNFDS Immersed_Boundary_Method geom_time3
-$RUNFDS Immersed_Boundary_Method geom_time4
-$RUNFDS Immersed_Boundary_Method geom_time5
-
 $RUNFDS Visualization cell_test
 $RUNCFAST Visualization cfast_test
 $RUNFDS Visualization colorbar
