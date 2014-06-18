3608f05fcf6cde3bf4e11b3211af7cd8487e3c30
gforney <gforney@gmail.com>
2014-06-18 14:22:11 +0000

    smokebot (windows): update email list
    Former-commit-id: c62e7b8decd7929213009e10886cd55af6dcfb8b [formerly 85773c67a5f2adc2e1f225cf858d4fdb02045eed]
    Former-commit-id: 85f221254f6e7f4c6125e30ee1ed891776d88445
    Former-commit-id: 211b4f7880a68fa9e5fb2721324ea72f28e26a3c

--- Utilities/Firebot/firebot_email_list.bat
+++ Utilities/Firebot/firebot_email_list.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Mailing list for Smokebot status report
-set mailToSMV="gforney@gmail.com, koverholt@gmail.com"
+set mailToSMV="gforney@gmail.com"
 
 :: General mailing list for Firebot status report
 set mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com"
