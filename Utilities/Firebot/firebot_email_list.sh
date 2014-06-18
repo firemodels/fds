5ffcfd3501f1ece6b7b638204fa628f73ce4ba04
Kristopher Overholt <koverholt@gmail.com>
2014-06-18 07:49:06 +0000

    Smokebot: Update email list
    Former-commit-id: 22f24b0fe7df61baf41fd3f458694d53ba7d770a [formerly 33e9de9dc8094739b2d338783637f74f8c5e15ab]
    Former-commit-id: 86e0caa4a87b4311a9bd2d3c00ba37304e525c57
    Former-commit-id: 1d1590090a1a4048ca31a1708cdefdaf0fb267f0

--- Utilities/Firebot/firebot_email_list.sh
+++ Utilities/Firebot/firebot_email_list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Mailing list for Smokebot status report
-mailToSMV="gforney@gmail.com, koverholt@gmail.com"
+mailToSMV="gforney@gmail.com"
 
 # General mailing list for Firebot status report
 mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, mrctkg@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com, daniel.haarhoff@gmail.com, arquitetojulio@gmail.com"
