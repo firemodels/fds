f247791d93de33177db71746d70d441e097dcc6d
mcgratta <mcgratta@gmail.com>
2014-07-09 18:10:27 +0000

    FDS Utilities: Remove some unused programs.
    Former-commit-id: 7207f865831e87e7b3a7b9c10a50856e317ce359 [formerly d7f09830c73bf83ba0b8cb7240f3fbe72503f2f2]
    Former-commit-id: 4adf77a0557daf47b5e9ceaa026eccb7f4e65b5b
    Former-commit-id: ed0d4597d4fc68aed12a1ebc70e542a34515039c

--- Utilities/Data_Processing/make_fortran.bat
+++ Utilities/Data_Processing/make_fortran.bat
@@ -1,8 +1,7 @@
-set intelbin="%IFORT_COMPILER11%\bin"
+set intelbin="%IFORT_COMPILER14%\bin"
 
 call %intelbin%\ifortvars ia32
 
 ifort -o fds2ascii fds2ascii.f90
-ifort -o Hamins_CH4_Average Hamins_CH4_Average.f90
 ifort -o layer_height layer_height.f
 pause
