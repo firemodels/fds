41ef9dde9221a69526803358f528203307efd1a3
mcgratta <mcgratta@gmail.com>
2014-07-09 14:41:38 +0000

    FDS Verification: Remove unused scarc cases from verification scripts.

--- Verification/FDS_MPI_Cases.sh
+++ Verification/FDS_MPI_Cases.sh
@@ -7,6 +7,8 @@ $RUNFDSMPI 4 Pressure_Solver dancing_eddies_default
 $RUNFDSMPI 8 Pressure_Solver duct_flow
 $RUNFDSMPI 5 Pressure_Solver hallways
 $RUNFDSMPI 8 Pressure_Solver scarc2d_bicg_8mesh
-$RUNFDSMPI 8 Pressure_Solver scarc2d_cggmg_8mesh
-$RUNFDSMPI 8 Pressure_Solver scarc2d_bicggmg_8mesh
+$RUNFDSMPI 8 Pressure_Solver scarc2d_fft_8mesh
+$RUNFDSMPI 8 Pressure_Solver scarc2d_cg_8mesh
+$RUNFDSMPI 8 Pressure_Solver scarc2d_gmg_8mesh
+
 
