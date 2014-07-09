c2e2a7697c971b8b7036463c5d9ea1c6904264c7
mcgratta <mcgratta@gmail.com>
2014-07-09 14:41:38 +0000

    FDS Verification: Remove unused scarc cases from verification scripts.
    Former-commit-id: c1aae84c6710493c08c1706dd90c4c1f15fac553 [formerly cb104b8d1e45cd21283f5243a0a98822fbb0cee9]
    Former-commit-id: 89ee802229c5bb78753ae62d66c3f0425d0fad15
    Former-commit-id: 3bc29969be0eec8662894e4268fa381ccc63831d

--- Verification/FDS_Cases.sh
+++ Verification/FDS_Cases.sh
@@ -161,9 +161,6 @@ $RUNFDS Pressure_Effects zone_shape
 
 $RUNFDS Pressure_Solver dancing_eddies_1mesh
 $RUNFDS Pressure_Solver scarc2d_fft_1mesh
-$RUNFDS Pressure_Solver scarc2d_fft_8mesh
-$RUNFDS Pressure_Solver scarc2d_cg_8mesh
-$RUNFDS Pressure_Solver scarc2d_gmg_8mesh
 
 $RUNFDS Pyrolysis cable_11_insulation_mcc
 $RUNFDS Pyrolysis cable_23_insulation_mcc
