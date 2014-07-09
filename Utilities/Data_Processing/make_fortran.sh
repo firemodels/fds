6b3106449289ef596c6b412f8fe8d497e8cf7240
mcgratta <mcgratta@gmail.com>
2014-07-09 18:10:27 +0000

    FDS Utilities: Remove some unused programs.

--- Utilities/Data_Processing/make_fortran.sh
+++ Utilities/Data_Processing/make_fortran.sh
@@ -2,11 +2,8 @@
 
 #  This script compiles a few utility programs.
  
-#source $IFORT_COMPILER/bin/compilervars.sh ia32
 source $IFORT_COMPILER/bin/compilervars.sh intel64
 
-ifort -o Beyler_Hood Beyler_Hood.f90
 ifort -o fds2ascii fds2ascii.f90
-ifort -o Hamins_CH4_Average Hamins_CH4_Average.f90
 ifort -o layer_height layer_height.f
 
