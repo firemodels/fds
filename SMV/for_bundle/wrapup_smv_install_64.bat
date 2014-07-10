2af267a74703d7085675dbb9944d94a1932ce4de
gforney <gforney@gmail.com>
2014-07-10 17:54:46 +0000

    fds/smv bundle: add commands (commented) for adding firewall rules when running windows/mpi version of fds (thank you pavlo!)

--- SMV/for_bundle/wrapup_smv_install_64.bat
+++ SMV/for_bundle/wrapup_smv_install_64.bat
@@ -16,10 +16,19 @@ echo Associating the .smv file extension with smokeview.exe
 ftype smvDoc="%CD%\smokeview.exe" "%%1" >Nul
 assoc .smv=smvDoc>Nul
 
-Rem adding path
+:: adding path
+
 echo.
 echo Adding %CD% to the system path
 call "%CD%\set_path.exe" -s -m -a "%CD%"
+
+:: add firewall rules
+
+:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-MPIEXEC.SMPD" program="%CD%\mpiexec.smpd.exe" profile=domain protocol=tcp
+:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-MPIEXEC.SMPD" program="%CD%\mpiexec.smpd.exe" profile=domain protocol=udp
+
+:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-SMPD" program="%CD%\smpd.exe" profile=domain protocol=tcp
+:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-SMPD" program="%CD%\smpd.exe" profile=domain protocol=udp
 
 erase "%CD%\set_path.exe"
 echo Press any key to complete update
