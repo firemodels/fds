15ec00db5e4f7cdf0bdef0b42a753dfb62c07c42
gforney <gforney@gmail.com>
2014-07-14 17:47:45 +0000

    build scripts: add titles to batch files for building various apps
    Former-commit-id: 3e2097c7970f897d235774e534977c1107ef28f9 [formerly ea2cbea4aba72efd1ee44f7b111cc436af4f7429]
    Former-commit-id: 148c2675684c581c5229c14f5df34d8da7840ade
    Former-commit-id: 60cb44995f67929ca7e7744a49df5ab56564fde3

--- Utilities/smokediff/intel_win_32/make_diff.bat
+++ Utilities/smokediff/intel_win_32/make_diff.bat
@@ -1,6 +1,6 @@
 "@echo off
 
-Rem windows batch file to build smokeview from the command line
+:: windows batch file to build smokeview from the command line
 
 IF "%SETUP_IFORT_COMPILER_IA32%"=="1" GOTO envexist
 
@@ -10,6 +10,7 @@ echo Setting up compiler environment
 call "%IFORT_COMPILER14%\bin\compilervars" ia32
 if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
 :envexist
+Title Building smokediff for 32 bit Windows
 erase *.obj
 erase *.mod
 make -f ..\Makefile intel_win_32
--- Utilities/smokediff/intel_win_64/make_diff.bat
+++ Utilities/smokediff/intel_win_64/make_diff.bat
@@ -1,6 +1,6 @@
 @echo off
 
-Rem windows batch file to build smokediff from the command line
+:: windows batch file to build smokediff from the command line
 
 IF "%SETUP_IFORT_COMPILER_INTEL64%"=="1" GOTO envexist
 
@@ -10,6 +10,7 @@ echo Setting up compiler environment
 call "%IFORT_COMPILER14%\bin\compilervars" intel64
 
 :envexist
+Title Building smokediff for 64 bit Windows
 erase *.obj *.mod
 make -f ..\Makefile intel_win_64
 pause
