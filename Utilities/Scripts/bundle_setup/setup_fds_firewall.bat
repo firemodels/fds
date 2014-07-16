605e4925df9715ce8466b68c9bfb56ea1424519f
pavapil <unknown@unknown.com>
2014-07-16 18:10:10 +0000

    FDS Scripts: Updated bundle firewall script for Intel MPI port setup

--- Utilities/Scripts/bundle_setup/setup_fds_firewall.bat
+++ Utilities/Scripts/bundle_setup/setup_fds_firewall.bat
@@ -1,12 +1,21 @@
+:: This script deletes previous Intel MPI firewall exceptions
+:: and creates 2 new rules opening ports equivalent to PORT_VAL.
+:: It then sets env variable MPICH_PORT_RANGE to match PORT_VAL.
+
+
 @echo off
 echo in setup_fds_fireall
 pause
 
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-MPIEXEC.SMPD" program="%CD%\mpiexec.smpd.exe" profile=domain protocol=tcp
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-MPIEXEC.SMPD" program="%CD%\mpiexec.smpd.exe" profile=domain protocol=udp
+set PORT_VAL=8670-8690
+netsh advfirewall firewall delete rule name="Intel MPI Port for FDS"
+netsh advfirewall firewall add rule dir=in action=allow name="Intel MPI Port for FDS" profile=domain protocol=tcp localport=%PORT_VAL%
+netsh advfirewall firewall add rule dir=in action=allow name="Intel MPI Port for FDS" profile=domain protocol=udp localport=%PORT_VAL%
 
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-SMPD" program="%CD%\smpd.exe" profile=domain protocol=tcp
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-SMPD" program="%CD%\smpd.exe" profile=domain protocol=udp
+setx MPIEXEC_PORT_RANGE 8670:8690
+setx MPICH_PORT_RANGE 8670:8690
+
+smpd -install
 
 echo FDS directory=%CD%\bin
 
