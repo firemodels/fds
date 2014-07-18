482b2bc4903ba31e676be956885a37bb40c04439
gforney <gforney@gmail.com>
2014-07-18 02:36:02 +0000

    bundle scripts:  for windows install: add uninstall batch file to start menu, put firewall script in "right" place
    Former-commit-id: bce24911280cc2d929aa485705424882ce3cb21a [formerly 9f1985933c152b0252cc2109c65fc5bf2a606f6f]
    Former-commit-id: 2ac69e709e7be15cec0e27ac56a164b8cfa0657a
    Former-commit-id: d16bc1c0915b7c6565d54738d06e5371368c5a60

--- Utilities/Scripts/INSTALL_win64.bat
+++ Utilities/Scripts/INSTALL_win64.bat
@@ -1,5 +1,6 @@
+@echo off
 
-Title Bundle FDS and Smokeview for 64 bit Windows
+Title Install FDS and Smokeview for 64 bit Windows
 
 Rem Windows batch file to upload 64 bit windows bundle to the google download site
 
