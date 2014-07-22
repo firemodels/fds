9db912226fd2639cd717fc92691f0c8908049bfc
Kristopher Overholt <koverholt@gmail.com>
2014-07-22 20:14:12 +0000

    Bundle Scripts: Add private networks to MPI firewall exceptions (based on Pavlo's debugging)
    Former-commit-id: 8d8ff9ea78c68317145a0467eed0a4c38d660e7b [formerly f1815c0f8283fccda275582988adfbb704f91186]
    Former-commit-id: 5806db04549b84a2c8c86dae80ff8a081caf57eb
    Former-commit-id: 200a3b230d3737dbcb0d59a85ec139c56905fbde

--- Utilities/Scripts/bundle_setup/setup_fds_firewall.bat
+++ Utilities/Scripts/bundle_setup/setup_fds_firewall.bat
@@ -6,8 +6,8 @@ set bindir=%1
 
 set PORT_VAL=8670-8690
 netsh advfirewall firewall delete rule name="Intel MPI Port for FDS" > Nul
-netsh advfirewall firewall add rule dir=in action=allow name="Intel MPI Port for FDS" profile=domain protocol=tcp localport=%PORT_VAL% > Nul
-netsh advfirewall firewall add rule dir=in action=allow name="Intel MPI Port for FDS" profile=domain protocol=udp localport=%PORT_VAL% > Nul
+netsh advfirewall firewall add rule dir=in action=allow name="Intel MPI Port for FDS" profile=domain,private protocol=tcp localport=%PORT_VAL% > Nul
+netsh advfirewall firewall add rule dir=in action=allow name="Intel MPI Port for FDS" profile=domain,private protocol=udp localport=%PORT_VAL% > Nul
 
 setx MPIEXEC_PORT_RANGE 8670:8690 > Nul
 setx MPICH_PORT_RANGE 8670:8690 > Nul
