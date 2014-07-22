17ae1bc9cd94287e12e7cbfd3ccd1a4ddf007f99
gforney <gforney@gmail.com>
2014-07-22 16:03:06 +0000

    bundle scripts: obtain FDS release notes wiki from repository before converting to html
    Former-commit-id: 6a6d82e5bd5e80cee96f2e93ba203b9f7306fb96 [formerly 4b49954c65e133b0c2839973f3a077236094211c]
    Former-commit-id: c6de2298a879bed5ace4b7e159b0467daf524be3
    Former-commit-id: 790d4e9d090ebd6cdb3c97d475aa754e45dbe34a

--- Utilities/Scripts/CONV_fds_release.sh
+++ Utilities/Scripts/CONV_fds_release.sh
@@ -4,5 +4,7 @@ bundleinfo=./bundle_setup
 wikify=./wikify.py
 
 
-echo Converting the FDS release notes from wiki to html format
-$wikify -r $bundleinfo/FDS_Release_Notes.wiki > $bundleinfo/release_notes.htm
+echo obtaining FDS release notes wiki from the repository
+svn export --quiet --force https://fds-smv.googlecode.com/svn/wiki/FDS_Release_Notes.wiki $bundleinfo/fds.wiki
+echo Converting the wiki to html format
+$wikify -r $bundleinfo/fds.wiki > $bundleinfo/release_notes.htm
