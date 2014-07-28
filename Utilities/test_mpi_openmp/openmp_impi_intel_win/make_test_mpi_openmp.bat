a098a2ce1240cce1ce4b700e1e9a772d185d9e86
mcgratta <mcgratta@gmail.com>
2014-07-28 13:11:43 +0000

    FDS Compilation: Add Windows directory for test_mpi_openmp

---
+++ Utilities/test_mpi_openmp/openmp_impi_intel_win/make_test_mpi_openmp.bat
@@ -0,0 +1,12 @@
+@echo on
+
+set intelbin=%IFORT_COMPILER14%\bin
+set impiversion=5.0.0.028
+
+set SETUP_IFORT_COMPILER64=1
+echo Setting up compiler environment
+call "%intelbin%\ifortvars" intel64
+call "%intelbin%\..\..\MPI\%impiversion%\intel64\bin\mpivars.bat"
+
+make VPATH=".." -f "..\makefile" openmp_impi_intel_win
+pause
