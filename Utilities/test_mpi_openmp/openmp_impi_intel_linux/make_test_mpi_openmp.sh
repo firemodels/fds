b57fbed12ac98a9f44c573400ac772b901f11c52
mcgratta <mcgratta@gmail.com>
2014-07-30 15:58:35 +0000

    FDS Compilation: Put -openmp on compile line of test_mpi_open.
    Former-commit-id: c2dd4646cddbcac203dc0ad075f0dd48c331a761 [formerly 3bfa1d0d31f0a551d280bbedcf6b2575c314db76]
    Former-commit-id: 8e03096cac42dbb0dea4b022fbc56e1baa7eedcd
    Former-commit-id: e9d06e564b88a82336df3105a275445968620710

--- Utilities/test_mpi_openmp/openmp_impi_intel_linux/make_test_mpi_openmp.sh
+++ Utilities/test_mpi_openmp/openmp_impi_intel_linux/make_test_mpi_openmp.sh
@@ -7,3 +7,4 @@ source $IFORT_COMPILER/bin/compilervars.sh $platform
 source $IFORT_COMPILER/../impi/5.0.0.028/intel64/bin/mpivars.sh
 echo Building $target
 make VPATH="../" -f ../makefile $target
+#mpiifort -o test_mpi_openmp -openmp ../test_mpi_openmp.f90
