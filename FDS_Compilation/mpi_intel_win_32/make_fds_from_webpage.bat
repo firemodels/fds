a8dbd121fa0993c02a5b5d3e3cfe4f0c5b828ce7
mcgratta <mcgratta@gmail.com>
2014-07-30 18:18:37 +0000

    FDS Compilation: Remove all 32 bit build folders.
    Former-commit-id: 5ab4e11411f62c90c8519c07e9e2932af51f35ce [formerly f78e260d8e1af6ba0e7f62919aba8fff05956220]
    Former-commit-id: b8493387987d3777455bd5d0f35a41aaafec587a
    Former-commit-id: 1a11b79805bdd14c9f229ef3c96ba984d0c58fc6

--- FDS_Compilation/mpi_intel_win_32/make_fds_from_webpage.bat
+++
@@ -1,6 +0,0 @@
-set intelbin="%IFORT_COMPILER14%\bin"
-
-call %intelbin%\compilervars ia32
-
-make MPIINCLUDE="c:\mpich\mpich2_32\include" MPILIB="c:\mpich\mpich2_32\lib\fmpich2.lib" VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_32
-pause
