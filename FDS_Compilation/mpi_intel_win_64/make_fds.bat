f69a946e9bb63ba77c964098f9a9bcf768a1a7e1
pavapil <unknown@unknown.com>
2014-07-31 18:50:30 +0000

    FDS Compilation: Updated make scripts to properly source ifortvars and mpivars
    Former-commit-id: 911fbefa27584f467473866d3862909398ca495b [formerly ab4ae9d3f5269cd80b698ccc815eac5be2b0eeca]
    Former-commit-id: 527f111fbc9e917504209cc0500bf9736a4bdf13
    Former-commit-id: 4126870132a872e12bc646c783187162697d3bfa

--- FDS_Compilation/mpi_intel_win_64/make_fds.bat
+++ FDS_Compilation/mpi_intel_win_64/make_fds.bat
@@ -1,5 +1,7 @@
 @echo off
-set intelbin="%IFORT_COMPILER13%\bin"
+set intelbin="%IFORT_COMPILER14%\bin"
+
+set mpiversion=5.0.0.028
 
 IF "%SETUP_IFORT_COMPILER64%"=="1" GOTO envexist
 
@@ -7,6 +9,7 @@ set SETUP_IFORT_COMPILER64=1
 
 echo Setting up compiler environment
 call %intelbin%\ifortvars intel64
+call %intelbin%\..\..\MPI\%mpiversion%\intel64\bin\mpivars.bat
 
 :envexist
 Title Building FDS (mpi) for 64 bit Windows
--- FDS_Compilation/mpi_intel_win_64_db/make_fds.bat
+++ FDS_Compilation/mpi_intel_win_64_db/make_fds.bat
@@ -1,7 +1,17 @@
 @echo off
 set intelbin="%IFORT_COMPILER14%\bin"
 
-call %intelbin%\ifortvars intel64
+set mpiversion=5.0.0.028
 
+IF "%SETUP_IFORT_COMPILER64%"=="1" GOTO envexist
+
+set SETUP_IFORT_COMPILER64=1
+
+echo Setting up compiler environment
+call %intelbin%\ifortvars intel64
+call %intelbin%\..\..\MPI\%mpiversion%\intel64\bin\mpivars.bat
+
+:envexist
+Title Building FDS (mpi) for 64 bit Windows
 make VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_64_db
 pause
