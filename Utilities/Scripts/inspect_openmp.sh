2ef5732947ea77ebf46606ed2a1ba6ae8bc6df4f
Kristopher Overholt <koverholt@gmail.com>
2014-08-01 02:38:37 +0000

    VV Scripts: Remove openmp_ from dir/binary names in inspect_openmp script
    Former-commit-id: 0274e254eb10f5661cb731ab7051e07dd1f936ea [formerly 173d5ca4f3912c75cd3557088f98b449eb9e2607]
    Former-commit-id: 1eead13e14ccd3fa579a649bbacadd8b0f263e63
    Former-commit-id: bad7eed72de174425d31c05d8d218931faf56e11

--- Utilities/Scripts/inspect_openmp.sh
+++ Utilities/Scripts/inspect_openmp.sh
@@ -9,7 +9,7 @@
 export SVNROOT=`pwd`/../..
 source /opt/intel/inspector_xe/inspxe-vars.sh quiet
 
-TARGET=$SVNROOT/FDS_Compilation/openmp_intel_linux_64_inspect
+TARGET=$SVNROOT/FDS_Compilation/intel_linux_64_inspect
 
 if [ -d inspect_openmp_ti3 ]
 then
@@ -25,4 +25,4 @@ export OMP_NUM_THREADS=2
 inspxe-cl -collect ti3 -knob scope=normal \
           -result-dir $SVNROOT/Utilities/Scripts/inspect_openmp_ti3 \
           -search-dir src=$SVNROOT/FDS_Source \
-          -- $SVNROOT/FDS_Compilation/openmp_intel_linux_64_inspect/fds_openmp_intel_linux_64_inspect bench2.fds
+          -- $SVNROOT/FDS_Compilation/intel_linux_64_inspect/fds_intel_linux_64_inspect bench2.fds
