199b0295434d74a1d93c3447627bc15615fb2e6e
pavapil <unknown@unknown.com>
2014-08-04 19:08:44 +0000

    FDS Compilation: Re-added Intel MPI for Linux to makefile.
    Former-commit-id: 62893261674db15675b67485d521968e5ebaa4bb [formerly 3917be8bd1949f874764305300754abd3867a2ce]
    Former-commit-id: 5bdaac1109eff705a6f19894e2b2078310478473
    Former-commit-id: 5c74e1fd2ac2b7dbb97c4a66a4335914710d7591

---
+++ FDS_Compilation/impi_intel_linux_64/make_fds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+platform=intel64
+dir=`pwd`
+target=${dir##*/}
+
+source $IFORT_COMPILER/bin/compilervars.sh $platform
+source $I_MPI_ROOT/bin/mpivars.sh
+source ~/.bashrc_fds $platform
+
+echo Building $target
+make -j4 VPATH="../../FDS_Source" -f ../makefile $target
