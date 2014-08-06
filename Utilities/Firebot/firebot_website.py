110e9646e13b17a49bcf106ae5cf9f266ebc60b7
Kristopher Overholt <koverholt@gmail.com>
2014-08-06 15:57:42 +0000

    Firebot: Remove unused stages from docs
    Former-commit-id: b772f35841ecc569bc7dbc22252a3add2847c04a [formerly fad9284bd8de2f766c4ea0d2f059744921af073d]
    Former-commit-id: fc6e5e173ebdb178d88dda7b98b9624335662704
    Former-commit-id: f0289a615a61ed5e684f582ab1cb8b099a5937c3

--- Utilities/Firebot/firebot_website.py
+++ Utilities/Firebot/firebot_website.py
@@ -92,9 +92,6 @@ To view a detailed error log, mouse over a fail or warning status.<br>
 <b>Stage 7b:</b> Matlab plotting (validation) <br>
 <b>Stage 7c:</b> FDS run time statistics <br>
 <b>Stage 8:</b> Build FDS-SMV guides <br>
-<b>Stage 9a:</b> Run correlations cases <br>
-<b>Stage 9b:</b> Matlab plotting (correlations) <br>
-<b>Stage 9c:</b> Build correlations guide <br>
 </span></span>"""
 
 print "<br><hr align='left'><br>"
