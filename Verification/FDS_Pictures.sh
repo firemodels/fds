412470d6135c69c11177d7315489624726f476b9
gforney <gforney@gmail.com>
2014-08-08 01:11:39 +0000

    FDS vv: add -d before directory names in FDS_Pictures script

--- Verification/FDS_Pictures.sh
+++ Verification/FDS_Pictures.sh
@@ -1,36 +1,36 @@
-$RUNSMV Controls activate_vents
-$RUNSMV Detectors beam_detector
-$RUNSMV Fires room_fire
-$RUNSMV Flowfields helium_2d_isothermal
-$RUNSMV Flowfields symmetry_test
-$RUNSMV Flowfields jet_fan
-$RUNSMV Flowfields velocity_bc_test
-$RUNSMV HVAC HVAC_mass_conservation
-$RUNSMV HVAC HVAC_energy_pressure
-$RUNSMV HVAC leak_test_2
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_01
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_02
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_03
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_04
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_05
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_06
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_07
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_08
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_09
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_10
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_11
-$RUNSMV Immersed_Boundary_Method tri_cube_cut_cell_test_12
-$RUNSMV Miscellaneous pyramid
-$RUNSMV NS_Analytical_Solution ns2d_64
-$RUNSMV Pressure_Effects pressure_boundary
-$RUNSMV Pressure_Solver dancing_eddies_tight
-$RUNSMV Scalar_Analytical_Solution move_slug
-$RUNSMV Scalar_Analytical_Solution move_slug_fl1
-$RUNSMV Scalar_Analytical_Solution shunn3_256
-$RUNSMV Species propane_flame_deposition
-$RUNSMV Sprinklers_and_Sprays cascade
-$RUNSMV Turbulence csmag_64
-$RUNSMV Turbulence dsmag_64
-$RUNSMV Turbulence ribbed_channel_80
-$RUNSMV Visualization objects_static
-$RUNSMV Visualization objects_dynamic
+$RUNSMV -d  Controls activate_vents
+$RUNSMV -d  Detectors beam_detector
+$RUNSMV -d  Fires room_fire
+$RUNSMV -d  Flowfields helium_2d_isothermal
+$RUNSMV -d  Flowfields symmetry_test
+$RUNSMV -d  Flowfields jet_fan
+$RUNSMV -d  Flowfields velocity_bc_test
+$RUNSMV -d  HVAC HVAC_mass_conservation
+$RUNSMV -d  HVAC HVAC_energy_pressure
+$RUNSMV -d  HVAC leak_test_2
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_01
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_02
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_03
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_04
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_05
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_06
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_07
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_08
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_09
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_10
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_11
+$RUNSMV -d  Immersed_Boundary_Method tri_cube_cut_cell_test_12
+$RUNSMV -d  Miscellaneous pyramid
+$RUNSMV -d  NS_Analytical_Solution ns2d_64
+$RUNSMV -d  Pressure_Effects pressure_boundary
+$RUNSMV -d  Pressure_Solver dancing_eddies_tight
+$RUNSMV -d  Scalar_Analytical_Solution move_slug
+$RUNSMV -d  Scalar_Analytical_Solution move_slug_fl1
+$RUNSMV -d  Scalar_Analytical_Solution shunn3_256
+$RUNSMV -d  Species propane_flame_deposition
+$RUNSMV -d  Sprinklers_and_Sprays cascade
+$RUNSMV -d  Turbulence csmag_64
+$RUNSMV -d  Turbulence dsmag_64
+$RUNSMV -d  Turbulence ribbed_channel_80
+$RUNSMV -d  Visualization objects_static
+$RUNSMV -d  Visualization objects_dynamic
