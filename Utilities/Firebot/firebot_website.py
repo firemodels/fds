daec9f2b9568939b71f48272f29c194ae2762b7b
Kristopher Overholt <koverholt@gmail.com>
2014-08-18 02:06:58 +0000

    Firebot: Minor edit to Stage 2a name
    Former-commit-id: 1972344b4650603ef0ad159cc8a9e102fb5f5d39 [formerly 3f85b656ad980214c1865b4e76283825e6747c35]
    Former-commit-id: 7b7c2f64c999257484f3491667441dbeeb44c414
    Former-commit-id: 49dc47a2eb0b091b7a75bb32a086d5bfabb8061e

--- Utilities/Firebot/firebot_website.py
+++ Utilities/Firebot/firebot_website.py
@@ -76,7 +76,7 @@ To view a detailed error log, mouse over a fail or warning status.<br>
 <span class="dropt">View a list of build stages<br>
 <span style="width:700px;">
 <b>Stage 1:</b> SVN operations <br>
-<b>Stage 2a:</b> Compile FDS debug <br>
+<b>Stage 2a:</b> Compile and inspect FDS debug <br>
 <b>Stage 2b:</b> Compile FDS MPI debug <br>
 <b>Stage 3:</b> Run verification cases (debug mode) <br>
 <b>Stage 4a:</b> Compile FDS release <br>
