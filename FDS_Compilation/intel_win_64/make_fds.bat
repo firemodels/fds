0a624cfecc9247d6b5044b78b80f8419d8e44dbf
gforney <gforney@gmail.com>
2014-08-31 17:53:01 +0000

    FDS/SMV build: use a centralized batch file for setting the Intel compiler environment (so we only need to make changes in one place when compiler changes).
    Former-commit-id: 7ffcc2c9dbe0dc57b537781027b83c51849cf0ff [formerly a0d0401757a0cef84e8fc440f6b1a099b56a1dfa]
    Former-commit-id: c7abd95cec602980192bae2d45da99910582c9c5
    Former-commit-id: d9dce33e6a1e160a0f006225250144bc1aadd14b

--- FDS_Compilation/intel_win_64/make_fds.bat
+++ FDS_Compilation/intel_win_64/make_fds.bat
@@ -1,15 +1,10 @@
 @echo off
-set intelbin="%IFORT_COMPILER15%\bin"
 
-IF "%SETUP_IFORT_COMPILER_64%"=="1" GOTO envexist
+:: setup compiler environment
+call ..\..\Utilities\Scripts\setup_intel_compilers.bat
 
-set SETUP_IFORT_COMPILER_64=1
-
-echo Setting up compiler environment
-call %intelbin%\ifortvars intel64
-
-:envexist
 Title Building FDS for 64 bit Windows
+
 make VPATH="../../FDS_Source" -f ..\makefile intel_win_64
 pause
 
--- Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
+++ Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
@@ -1,4 +1,9 @@
-call "%IFORT_COMPILER14%\bin\compilervars" intel64
-erase *.obj
+@echo off
+:: setup compiler environment
+call ..\..\Scripts\setup_intel_compilers.bat
+
+Title Building fds2ascii for 64 bit Windows
+
+erase *.obj *.mod
 make -f ..\Makefile intel_win_64
 pause
