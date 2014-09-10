a670884d1dcf290befc0c1e4b0c28234589089ef
rmcdermo <randy.mcdermott@gmail.com>
2014-09-10 00:47:31 +0000

    FDS Verification: comment wrf cases until I sort out mpi with nudging
    Former-commit-id: 32e9f84c9cd4a3a3c501c836516bb34be307bef8 [formerly 64e4aa867b206e247f2e1b07447117454e237b0f]
    Former-commit-id: 0def3aa4e2a0a7d227e2ca50ae0108df032f807b
    Former-commit-id: 98d0e00d412af710a6937cfad8c9c5512b2d5f5b

--- Verification/FDS_MPI_Cases.sh
+++ Verification/FDS_MPI_Cases.sh
@@ -7,6 +7,6 @@ $QFDS -p 4 -d Pressure_Solver dancing_eddies_default.fds
 $QFDS -p 8 -d Pressure_Solver duct_flow.fds
 $QFDS -p 5 -d Pressure_Solver hallways.fds
 
-$QFDS -p 4 -d WRF wrf_time_ramp.fds
-$QFDS -p 4 -d WRF wrf_prof_ramp.fds
-$QFDS -p 4 -d WRF wrf_time_prof_ramp.fds
+#$QFDS -p 4 -d WRF wrf_time_ramp.fds
+#$QFDS -p 4 -d WRF wrf_prof_ramp.fds
+#$QFDS -p 4 -d WRF wrf_time_prof_ramp.fds
