d326302d27050d74aa4424eecf0dee1aef7cdf55
gforney <gforney@gmail.com>
2014-09-12 20:09:57 +0000

    bundle scripts: correct side effects of recent qfds.sh changes
    Former-commit-id: 2aecd4a27f5ec350413850444ae30c06a7930d12 [formerly 3993e841b6e6e6f25e05ede051685c87808cca11]
    Former-commit-id: cab3f32c88a9631036d46064f166e1a9a8c779da
    Former-commit-id: f0ccc4b8e7ff072a825b8a62df216eb9d20ea5c5

---
+++ Utilities/Scripts/copyfdscase.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+while getopts 'd:p:o:t' OPTION
+do
+case $OPTION  in
+  d)
+   dir="$OPTARG"
+   ;;
+  o)
+   dummy="$OPTARG"
+   ;;
+  p)
+   dummy2="$OPTARG"
+   ;;
+  t)
+   dummy3="xxx"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
+in=$1
+infile=${in%.*}
+
+cd $dir
+if ! [ -d $OUTDIR/$dir ]; then
+  mkdir $OUTDIR/$dir
+fi
+
+cp $infile.fds $OUTDIR/$dir/.
+if [ -e $infile.ini ]; then
+  cp $infile.ini $OUTDIR/$dir/.
+fi
+
+if [ -e $infile.ssf ]; then
+  cp $infile.ssf $OUTDIR/$dir/.
+fi
