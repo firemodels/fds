fc981908c63a2eb9722f4b600af04939fd2baf72
gforney <gforney@gmail.com>
2014-10-01 20:37:48 +0000

    geom notes: add scripts to generate geometry figures and build geometry notes
    Former-commit-id: c88dd6c47c718d6a89989488c2d677bd8fa27298 [formerly db2cc57e4883c1923d103b7fb9f97609ab2f1d12]
    Former-commit-id: d4cba4ed6c259e336b4b152d53f454064fb6719a
    Former-commit-id: b7f401207ec283c1f890821f019e7c52a82d4020

---
+++ Manuals/FDS_User_Guide/make_geom_notes.bat
@@ -0,0 +1,18 @@
+@echo off
+set paper=geom_notes
+
+echo pass 1
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+bibtex %paper% > %paper%.err
+echo pass 2
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+echo pass 3
+pdflatex -interaction nonstopmode %paper% > %paper%.err
+
+find "! LaTeX Error:" %paper%.err
+find "Fatal error" %paper%.err
+find "Error:" %paper%.err
+
+echo %paper% build complete
+pause
+
