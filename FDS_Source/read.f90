ce79639ab7bff574579ab9c578ee235a39a79a8d
drjfloyd <drjfloyd@gmail.com>
2014-10-06 13:42:52 +0000

    FDS Source: Fix line length
    Former-commit-id: 8b4705ff93b52945b7bed24c3b0fa210954cada1 [formerly 1d1f863f681a64db296faa3a6a71bacf89933779]
    Former-commit-id: 090c771dfb3a51e7272d4c38ef1b6047db666882
    Former-commit-id: 90ffb1079b82cad269b78414c9e76c1cd968e552

--- FDS_Source/read.f90
+++ FDS_Source/read.f90
@@ -624,7 +624,8 @@ IF (USE_MPI) THEN
    DO NM=1,NMESHES
       IF (NM==1) CYCLE
       IF (PROCESS(NM) < PROCESS(NM-1)) THEN
-         WRITE(MESSAGE,'(A,I3,A,I3,A)') 'ERROR: MPI_PROCESS for MESH ', NM, 'is less than MPI_PROCESS for MESH ',NM-1,'. Reorder MESH lines.'
+         WRITE(MESSAGE,'(A,I3,A,I3,A)') 'ERROR: MPI_PROCESS for MESH ', NM, 'is less than MPI_PROCESS for MESH ',NM-1,&
+	                                             '. Reorder MESH lines.'
          CALL SHUTDOWN(MESSAGE) ; RETURN         
       ENDIF
    ENDDO
