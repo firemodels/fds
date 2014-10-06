de350fabf69f9dcc5c45a59342a8772bb66f455a
gforney <gforney@gmail.com>
2014-10-06 19:22:44 +0000

    smv vv: corrections to scripts used by linux smokebot
    Former-commit-id: 83a31c8d8dc8db729fbfbacb48cdef6cab822305 [formerly 7e72508fcd12f9a08f827592090d5bc6dc75c491]
    Former-commit-id: 074484951306222063236b593a6455b15f27a3bd
    Former-commit-id: 59e4d3a5b01690e20a059d3956e18dc1fc62298c

--- Verification/scripts/copyout.sh
+++ Verification/scripts/copyout.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-outdir=$FDS_SVNROOT/Verification/scripts/Outfiles
-dir=$1
-infile=$2
+# parse options
+DIR=.
+while getopts 'bd:e:im:n:o:p:q:st' OPTION
+do
+case $OPTION  in
+  b)
+   dummy=1
+   ;;
+  d)
+   DIR="$OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
 
-fulldir=$FDS_SVNROOT/Verification/$dir
+
+outdir=$FDS_SVNROOT/Verification/scripts/Outfiles
+infile=$1
+
+fulldir=$FDS_SVNROOT/Verification/$dDIR
 outfile=$infile.out
 
 # ensure that various files and directories exist
