9f0b60b8d7d9cdeed38e039d13614a81f166565b
gforney <gforney@gmail.com>
2014-10-07 20:25:52 +0000

    coupling build: update batch file that builds coupling emulator to use windows preprocessing dircetive
    Former-commit-id: ba860b4b1909bb72ba914ae9c4be402c4ee38c60 [formerly c8e28b6fd8fc157e7c263715de6ed46119addc23]
    Former-commit-id: 8ab89862f06ec4e72a4c6a57222b9cd31d4f4dcf
    Former-commit-id: 1153f37b99c45a66b065c1ccb62232eccc791401

--- Utilities/Data_Processing/make_fortran.bat
+++ Utilities/Data_Processing/make_fortran.bat
@@ -4,5 +4,5 @@ call %intelbin%\ifortvars intel64
 
 ifort -o fds2ascii fds2ascii.f90
 ifort -o layer_height layer_height.f
-ifort -o coupling_emulator coupling_emulator.f90
+ifort -o coupling_emulator -fpp -D pp_WINDOWS coupling_emulator.f90
 pause
