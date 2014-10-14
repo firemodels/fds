041ca7e478561802ede1bdabbde48d88ba4104bc
mcgratta <mcgratta@gmail.com>
2014-10-14 19:27:33 +0000

    FDS Verification: Add an example of mesh overlap to dancing_eddies suite.
    Former-commit-id: 9aabe89ee46707eae2a16263095d30f1ae390b24 [formerly 54cf03e1fdc9c9c04bae08d0eb8ae388c3a19c5a]
    Former-commit-id: c2e76fcb34d220d588d38c20e175df432ea3474f
    Former-commit-id: a5470990555328443912f6d1012475018452dff0

--- Verification/FDS_MPI_Cases.sh
+++ Verification/FDS_MPI_Cases.sh
@@ -11,6 +11,7 @@ $QFDS -p 8 -d Flowfields symmetry_test_mpi.fds
 $QFDS -p 4 -d Heat_Transfer back_wall_test.fds
 
 $QFDS -p 4 -d Pressure_Solver dancing_eddies_tight.fds
+$QFDS -p 4 -d Pressure_Solver dancing_eddies_tight_overlap.fds
 $QFDS -p 4 -d Pressure_Solver dancing_eddies_default.fds
 $QFDS -p 8 -d Pressure_Solver duct_flow.fds
 $QFDS -p 5 -d Pressure_Solver hallways.fds
