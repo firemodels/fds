d8a9b75de8e1302818cf275175a74deab4bc9ede
rmcdermo <randy.mcdermott@gmail.com>
2014-10-31 19:05:18 +0000

    Matlab: add radial profiles of vertical velocity for mccaffrey_plume.m (under construction)
    Former-commit-id: a6155c0ae4c1986147432ae55e80daca8086e665 [formerly a184ada68d7bed17261782905b3a5cbc008d8952]
    Former-commit-id: 1fc08d52730cf4667bf1dd68285b561a85a80228
    Former-commit-id: 0382c11b53c4cc965c4466588ea5fceb74403869

--- Utilities/Matlab/scripts/mccaffrey_plume.m
+++ Utilities/Matlab/scripts/mccaffrey_plume.m
@@ -16,12 +16,13 @@ T0 = 273.15 + 20;
 
 DS = (Q/(rho*cp*T0*sqrt(g))).^(2/5); % m
 
-datadir = '../../Validation/McCaffrey_Plume/FDS_Output_Files/';
+%datadir = '../../Validation/McCaffrey_Plume/FDS_Output_Files/';
+datadir = '../../Validation/McCaffrey_Plume/Test2/';
 plotdir = '../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/McCaffrey_Plume/';
 
-%chid = {'McCaffrey_14_kW_10','McCaffrey_22_kW_10','McCaffrey_33_kW_10','McCaffrey_45_kW_10','McCaffrey_57_kW_10'};
-%chid = {'McCaffrey_14_kW_20','McCaffrey_22_kW_20','McCaffrey_33_kW_20','McCaffrey_45_kW_20','McCaffrey_57_kW_20'};
-chid = {'McCaffrey_14_kW_40','McCaffrey_22_kW_40','McCaffrey_33_kW_40','McCaffrey_45_kW_40','McCaffrey_57_kW_40'};
+chid = {'McCaffrey_14_kW_11','McCaffrey_22_kW_11','McCaffrey_33_kW_11','McCaffrey_45_kW_11','McCaffrey_57_kW_11'};
+%chid = {'McCaffrey_14_kW_21','McCaffrey_22_kW_21','McCaffrey_33_kW_21','McCaffrey_45_kW_21','McCaffrey_57_kW_21'};
+%chid = {'McCaffrey_14_kW_45','McCaffrey_22_kW_45','McCaffrey_33_kW_45','McCaffrey_45_kW_45','McCaffrey_57_kW_45'};
 mark = {'ko','k+','k^','ksq','kd'};
 n_chid = length(chid);
 
@@ -78,8 +79,10 @@ ylabel('$v^* = v/\sqrt{g D^*} $','FontSize',Label_Font_Size,'Interpreter',Font_I
 
 for i=1:n_chid
     M = importdata([datadir,chid{i},'_line.csv'],',',2);
-    z = M.data(:,1);
-    v = M.data(:,3);
+    z = M.data(:,find(strcmp('Height',M.colheaders)));
+    dz = z(2)-z(1);
+    z = z + dz/2; % use with "wvel" for staggered storage location
+    v = M.data(:,find(strcmp('wvel',M.colheaders)));
 
     %zq_fds = z./Q(i)^(2/5);
     %vq_fds = v./Q(i)^(1/5);
@@ -98,7 +101,7 @@ legend boxoff
 
 % add SVN if file is available
 
-SVN_Filename = [datadir,'McCaffrey_14_kW_10_svn.txt'];
+SVN_Filename = [datadir,'McCaffrey_14_kW_11_svn.txt'];
 if exist(SVN_Filename,'file')
     SVN = importdata(SVN_Filename);
     x_lim = get(gca,'XLim');
@@ -137,8 +140,8 @@ ylabel('$\Theta^* = (T-T_0)/T_0$','FontSize',Label_Font_Size,'Interpreter',Font_
 
 for i=1:n_chid
     M = importdata([datadir,chid{i},'_line.csv'],',',2);
-    z = M.data(:,1);
-    T = M.data(:,2) + 273.15;
+    z = M.data(:,find(strcmp('Height',M.colheaders)));
+    T = M.data(:,find(strcmp('tmp',M.colheaders))) + 273.15;
     %zq_fds = z./Q(i)^(2/5);
     %hh(i)=loglog(zq_fds,T,mark{i});
     hh(i)=loglog(z./DS(i),(T-T0)/T0,mark{i});
@@ -152,7 +155,7 @@ legend boxoff
 
 % add SVN if file is available
 
-SVN_Filename = [datadir,'McCaffrey_14_kW_10_svn.txt'];
+SVN_Filename = [datadir,'McCaffrey_14_kW_11_svn.txt'];
 if exist(SVN_Filename,'file')
     SVN = importdata(SVN_Filename);
     x_lim = get(gca,'XLim');
@@ -172,5 +175,218 @@ set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
 print(gcf,'-dpdf',[plotdir,'McCaffrey_Temperature_Correlation'])
 
 
+% Velocity profile in the plume
 
+figure
+
+z = 1;
+chid = {'McCaffrey_22_kW_11','McCaffrey_22_kW_21','McCaffrey_22_kW_45'};
+mark = {'kd','k^','ksq'};
+for i=1:length(chid)
+    M = importdata([datadir,chid{i},'_line.csv'],',',2);
+    x = M.data(:,find(strcmp('Radius',M.colheaders)));
+    V = M.data(:,find(strcmp('wx_p',M.colheaders)));
+    V0 = V(find(x==0));
+    hh(i)=plot(x/z,V/V0,mark{i}); hold on
+end
+
+z = 1.25;
+chid = {'McCaffrey_45_kW_11','McCaffrey_45_kW_21','McCaffrey_45_kW_45'};
+mark = {'kd','k^','ksq'};
+for i=1:length(chid)
+    M = importdata([datadir,chid{i},'_line.csv'],',',2);
+    x = M.data(:,find(strcmp('Radius',M.colheaders)));
+    V = M.data(:,find(strcmp('wx_p',M.colheaders)));
+    V0 = V(find(x==0));
+    hh(length(chid)+i)=plot(x/z,V/V0,mark{i},'MarkerFaceColor','k'); hold on
+end
+
+x = linspace(0,.3,100);
+a1 = 0.16; % see Fig. 6, McCaffrey (1979)
+a2 = 0.14;
+a3 = 0.18;
+v1 = exp(-(5/6/a1*x).^2);
+v2 = exp(-(5/6/a2*x).^2);
+v3 = exp(-(5/6/a3*x).^2);
+hh(2*length(chid)+1)=plot(x,v1,'k-');
+hh(2*length(chid)+2)=plot(x,v2,'k--');
+hh(2*length(chid)+3)=plot(x,v3,'k-.');
+
+axis([0 .35 0 1.2])
+xlabel('$x/z$','FontSize',Label_Font_Size,'Interpreter',Font_Interpreter)
+ylabel('$V/V_0$','FontSize',Label_Font_Size,'Interpreter',Font_Interpreter)
+
+leg_key = {'22 kW 11','22 kW 21','22 kW 45',...
+           '45 kW 11','45 kW 21','45 kW 45',...,
+           '$\alpha=0.14$','$\alpha=0.16$','$\alpha=0.18$'};
+lh=legend(hh,leg_key,'location','northeast');
+set(lh,'Interpreter',Font_Interpreter)
+legend boxoff
+
+xt = .05;
+yt = 1.1;
+text(xt,yt,'Plume','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter,'FontName',Font_Name)
+
+% add SVN if file is available
+
+SVN_Filename = [datadir,'McCaffrey_14_kW_11_svn.txt'];
+if exist(SVN_Filename,'file')
+    SVN = importdata(SVN_Filename);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
+
+% print to pdf
+
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf',[plotdir,'McCaffrey_Velocity_Profile_Plume'])
+
+
+% Velocity profile in the intermittent region
+
+figure
+clear hh
+
+z = 0.5;
+x_1oe = 0.46*(z/Q(2)^(2/5)) + 0.013*sqrt(Q(2));
+chid = {'McCaffrey_22_kW_11','McCaffrey_22_kW_21','McCaffrey_22_kW_45'};
+mark = {'kd','k^','ksq'};
+for i=1:length(chid)
+    M = importdata([datadir,chid{i},'_line.csv'],',',2);
+    x = M.data(:,find(strcmp('Radius',M.colheaders)));
+    V = M.data(:,find(strcmp('wx_i',M.colheaders)));
+    V0 = V(find(x==0));
+    hh(i)=plot(x/x_1oe,V/V0,mark{i}); hold on
+end
+
+z = 0.75;
+x_1oe = 0.46*(z/Q(4)^(2/5)) + 0.013*sqrt(Q(4));
+chid = {'McCaffrey_45_kW_11','McCaffrey_45_kW_21','McCaffrey_45_kW_45'};
+mark = {'kd','k^','ksq'};
+for i=1:length(chid)
+    M = importdata([datadir,chid{i},'_line.csv'],',',2);
+    x = M.data(:,find(strcmp('Radius',M.colheaders)));
+    V = M.data(:,find(strcmp('wx_i',M.colheaders)));
+    V0 = V(find(x==0));
+    hh(length(chid)+i)=plot(x/x_1oe,V/V0,mark{i},'MarkerFaceColor','k'); hold on
+end
+
+x = linspace(0,2,100);
+v1 = exp(-x.^1.5);
+hh(2*length(chid)+1)=plot(x,v1,'k-');
+
+axis([0 3 0 1.2])
+xlabel('$x/x_{1/e}$','FontSize',Label_Font_Size,'Interpreter',Font_Interpreter)
+ylabel('$V/V_0$','FontSize',Label_Font_Size,'Interpreter',Font_Interpreter)
+
+leg_key = {'22 kW 11','22 kW 21','22 kW 45',...
+           '45 kW 11','45 kW 21','45 kW 45',...
+           '$n=3/2$'};
+lh=legend(hh,leg_key,'location','northeast');
+set(lh,'Interpreter',Font_Interpreter)
+legend boxoff
+
+xt = .5;
+yt = 1.1;
+text(xt,yt,'Intermittent','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter,'FontName',Font_Name)
+
+% add SVN if file is available
+
+SVN_Filename = [datadir,'McCaffrey_14_kW_11_svn.txt'];
+if exist(SVN_Filename,'file')
+    SVN = importdata(SVN_Filename);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
+
+% print to pdf
+
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf',[plotdir,'McCaffrey_Velocity_Profile_Intermittent'])
+
+
+% Velocity profile in the flame region
+
+figure
+clear hh
+
+z = 0.125;
+x_1oe = 0.46*(z/Q(2)^(2/5)) + 0.013*sqrt(Q(2));
+chid = {'McCaffrey_22_kW_11','McCaffrey_22_kW_21','McCaffrey_22_kW_45'};
+mark = {'kd','k^','ksq'};
+for i=1:length(chid)
+    M = importdata([datadir,chid{i},'_line.csv'],',',2);
+    x = M.data(:,find(strcmp('Radius',M.colheaders)));
+    V = M.data(:,find(strcmp('wx_f',M.colheaders)));
+    V0 = V(find(x==0));
+    hh(i)=plot(x/x_1oe,V/V0,mark{i}); hold on
+end
+
+z = 0.25;
+x_1oe = 0.46*(z/Q(4)^(2/5)) + 0.013*sqrt(Q(4));
+chid = {'McCaffrey_45_kW_11','McCaffrey_45_kW_21','McCaffrey_45_kW_45'};
+mark = {'kd','k^','ksq'};
+for i=1:length(chid)
+    M = importdata([datadir,chid{i},'_line.csv'],',',2);
+    x = M.data(:,find(strcmp('Radius',M.colheaders)));
+    V = M.data(:,find(strcmp('wx_f',M.colheaders)));
+    V0 = V(find(x==0));
+    hh(length(chid)+i)=plot(x/x_1oe,V/V0,mark{i},'MarkerFaceColor','k'); hold on
+end
+
+x = linspace(0,2,100);
+v1 = exp(-x.^1.5);
+v2 = exp(-x.^2);
+hh(2*length(chid)+1)=plot(x,v1,'k-');
+hh(2*length(chid)+2)=plot(x,v2,'k--');
+
+axis([0 3 0 1.2])
+xlabel('$x/x_{1/e}$','FontSize',Label_Font_Size,'Interpreter',Font_Interpreter)
+ylabel('$V/V_0$','FontSize',Label_Font_Size,'Interpreter',Font_Interpreter)
+
+leg_key = {'22 kW 11','22 kW 21','22 kW 45',...
+           '45 kW 11','45 kW 21','45 kW 45',...
+           '$n=3/2$','$n=2$'};
+lh=legend(hh,leg_key,'location','northeast');
+set(lh,'Interpreter',Font_Interpreter)
+legend boxoff
+
+xt = .5;
+yt = 1.1;
+text(xt,yt,'Flame','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter,'FontName',Font_Name)
+
+% add SVN if file is available
+
+SVN_Filename = [datadir,'McCaffrey_14_kW_11_svn.txt'];
+if exist(SVN_Filename,'file')
+    SVN = importdata(SVN_Filename);
+    x_lim = get(gca,'XLim');
+    y_lim = get(gca,'YLim');
+    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+end
+
+% print to pdf
+
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'PaperUnits',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf',[plotdir,'McCaffrey_Velocity_Profile_Flame'])
 
