2a711b81c939998f58f32e21e5a280e6b4c49848
mcgratta <mcgratta@gmail.com>
2014-11-03 15:28:28 +0000

    FDS Validation: Add CSIRO Grassland Fires.
    Former-commit-id: a8708931548ed04d851aac60a4688e52e504871b [formerly 02679a0da9cc1a2ed0045b7616846eed7a7021bb]
    Former-commit-id: dc9d7e30e4a7cfbd54ac35553b02df2e902312b4
    Former-commit-id: 566232d53549cd2cf4642d5d3fccbb132d475432

---
+++ Validation/CSIRO_Grassland_Fires/Run_All.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script runs a set of Validation Cases on a Linux machine with a batch queuing system.
+# See the file Validation/Common_Run_All.sh for more information.
+export SVNROOT=`pwd`/../..
+source $SVNROOT/Validation/Common_Run_All.sh
+
+$QFDS -p 36 -n 4 $DEBUG $QUEUE -d $INDIR Case_C064.fds
+$QFDS -p 36 -n 4 $DEBUG $QUEUE -d $INDIR Case_F19.fds
+
+echo FDS cases submitted
