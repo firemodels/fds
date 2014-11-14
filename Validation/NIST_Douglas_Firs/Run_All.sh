02358a683aab680607bee1911e7c3c976e74ba6f
mcgratta <mcgratta@gmail.com>
2014-11-14 21:19:45 +0000

    FDS Validation: Add NIST_Douglas_Firs.
    Former-commit-id: 1615d4e9b28cf6b91066406e930a5526971cb32d [formerly 12a50c7c89e251233160b514c43c0eb2b2836de8]
    Former-commit-id: 91c79aa0fbc3f43cb2a906f2c56cb3dc274bbfdf
    Former-commit-id: 5ae6aa02f556e8ff6a7f191eee84cce91678c4b7

---
+++ Validation/NIST_Douglas_Firs/Run_All.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script runs a set of Validation Cases on a Linux machine with a batch queuing system.
+# See the file Validation/Common_Run_All.sh for more information.
+export SVNROOT=`pwd`/../..
+source $SVNROOT/Validation/Common_Run_All.sh
+
+$QFDS -p 1 $DEBUG $QUEUE -d $INDIR tree_2_m_14_pc.fds
+$QFDS -p 1 $DEBUG $QUEUE -d $INDIR tree_2_m_49_pc.fds
+$QFDS -p 2 $DEBUG $QUEUE -d $INDIR tree_5_m_26_pc.fds
+
+echo FDS cases submitted
