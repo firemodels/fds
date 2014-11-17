4bc79246b8f9fe4506068517a49c755fbe405d75
gforney <gforney@gmail.com>
2014-11-17 19:02:27 +0000

    build script: update script to build fds2ascii
    Former-commit-id: 07d6011f8e01456cd6adc49d2e835ab20176140f [formerly 4584c21e2b974dd37e9dec2b519ea6e5d16a2b36]
    Former-commit-id: e78532c19d09cffdc8fc0fe10663166d390094fc
    Former-commit-id: e890ffce61ab9195292762343e3df6c44b6a8a7a

--- Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
+++ Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
@@ -4,6 +4,6 @@ call ..\..\Scripts\setup_intel_compilers.bat
 
 Title Building fds2ascii for 64 bit Windows
 
-erase *.obj *.mod
-make -f ..\Makefile intel_win_64
+erase *.obj *.exe
+ifort -o fds2ascii_win_64.exe -O2 /nologo ..\..\Data_processing\fds2ascii.f90
 pause
