c2ab727d0a38518319394986d6c3f914e16b08ef
gforney <gforney@gmail.com>
2014-11-19 21:55:13 +0000

    build scripts: update various windows build scripts to use latest compiler setup script
    Former-commit-id: a3adbc9c285de08dd0d436595e0d26b35af91aed [formerly 574e200ee981fb2d06872119701ee475ca5c5196]
    Former-commit-id: 4882bdfe24a8abd2e8cb696c1c590a5a03445ed3
    Former-commit-id: daebac65fa4d0ef87e64b1bbf171ebc7e2f0e790

--- Utilities/set_fds_path/intel_win_32/make_setfdspath.bat
+++ Utilities/set_fds_path/intel_win_32/make_setfdspath.bat
@@ -1,15 +1,7 @@
 @echo off
-
-Rem windows batch file to build smokeview from the command line
-
-IF "%SETUP_IFORT_COMPILER%"=="1" GOTO envexist
-
-set SETUP_IFORT_COMPILER=1
-
-echo Setting up compiler environment
-call "%IFORT_COMPILER14%\bin\compilervars" ia32 %VS_VERSION%
+:: setup compiler environment
+call ..\..\Scripts\setup_intel_compilers32.bat
 if exist "%VS_COMPILER%\vcvars32x86_amd64.bat" call "%VS_COMPILER%\vcvars32x86_amd64"
-:envexist
 
 erase *.obj
 erase *.mod
