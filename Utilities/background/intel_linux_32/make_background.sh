1abca48f11b69fa20df6b85c37b28ee77331b516
gforney <gforney@gmail.com>
2014-11-21 15:04:44 +0000

    build scripts: add option to various rm lines to prevent bogus error messages
    Former-commit-id: f9804d926e94dc637bdad23e696d77cc80b63495 [formerly 309253358c9e7ce1b87882c9b012c7732193ddc7]
    Former-commit-id: de2f5f96090ff84645b77fd5236b8d2a52c6a41f
    Former-commit-id: 4b3e559b8aae1010034b7307d30312159f30942b

--- Utilities/background/intel_linux_32/make_background.sh
+++ Utilities/background/intel_linux_32/make_background.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $IFORT_COMPILER/bin/compilervars.sh ia32
-rm *.o
+rm -f *.o
 make -j4 -f ../Makefile intel_linux_32
--- Utilities/smokediff/intel_linux_64/make_diff.sh
+++ Utilities/smokediff/intel_linux_64/make_diff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $IFORT_COMPILER/bin/compilervars.sh intel64
-rm *.o
+rm -f *.o
 make -f ../Makefile intel_linux_64
