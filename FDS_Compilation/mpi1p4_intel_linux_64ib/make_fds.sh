b6862789d6e5c3ddd50c0512c907db102ff0a1a1
Kristopher Overholt <koverholt@gmail.com>
2014-12-03 20:08:32 +0000

    FDS Compilation: Remove unused compile target dir

--- FDS_Compilation/mpi1p4_intel_linux_64ib/make_fds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dir=`pwd`
-target=${dir##*/}
-
-source $IFORT_COMPILER/bin/compilervars.sh intel64
-source ~/.bashrc_fds intel64ib1p4p3
-
-echo Building $target 
-make -j4 VPATH="../../FDS_Source" -f ../makefile $target
