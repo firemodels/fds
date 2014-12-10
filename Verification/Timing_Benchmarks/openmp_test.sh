9dda02c1249aec18a1f71e489ccc64b7453e37ac
gforney <gforney@gmail.com>
2014-12-10 14:07:32 +0000

    FDS vv: update openmp test script
    Former-commit-id: 8c08b3104212ffa8b91605abc4ebebafa7dce882 [formerly e8dcaf4b65f261ac2961bdd22ac88734f1ae2665]
    Former-commit-id: bbdd6a57bead56841349a3ed4f9cfca042f7e0ea
    Former-commit-id: 93b901242d7c9186e3f8784d314be6ef9a9e7fec

--- Verification/Timing_Benchmarks/openmp_test.sh
+++ Verification/Timing_Benchmarks/openmp_test.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 QFDS=../../Utilities/Scripts/qfds.sh
 
-file=openmp64_test0
-./makecase.sh 64 $file
-$QFDS -t -o 1 -d . $file.fds
-for index in {1..20}
+for index in {1..10}
 do
-  file=openmp64_test$index
+  file=openmp64_test1_$index
   ./makecase.sh 64 $file
-  $QFDS -t -o 4 -d . $file.fds
+  $QFDS -t -o 1 -d . $file.fds
 done
 
-file=openmp128_test0
-./makecase.sh 128 $file
-$QFDS -t -o 1 -d . $file.fds
-for index in {1..20}
+for index in {1..10}
 do
-  file=openmp128_test$index
-  ./makecase.sh 128 $file
+  file=openmp64_test4_$index
+  ./makecase.sh 64 $file
   $QFDS -t -o 4 -d . $file.fds
 done
