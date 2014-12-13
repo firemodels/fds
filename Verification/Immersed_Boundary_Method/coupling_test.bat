f37ef3077b55443c8b4352bf210752531ed3a6c8
gforney <gforney@gmail.com>
2014-12-13 03:52:33 +0000

    VV: add/update scripts for testing coupling emulator

--- Verification/Immersed_Boundary_Method/coupling_test.bat
+++ Verification/Immersed_Boundary_Method/coupling_test.bat
@@ -1,4 +1,4 @@
 @echo off
-set coupler=..\..\Utilities\Data_Processing\coupling_emulator.exe
+set coupler=..\..\Utilities\coupling_emulator\intel_win_64\coupling_emulator_win_64.exe
 set fds=..\..\FDS_Compilation\intel_win_64\fds_win_64.exe
 %coupler% %fds%
--- Verification/Immersed_Boundary_Method/coupling_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-coupler=../../Utilities/Data_Processing/coupling_emulator
-fds=../../FDS_Compilation/intel_linux_64/fds_intel_linux_64
-$coupler $fds
