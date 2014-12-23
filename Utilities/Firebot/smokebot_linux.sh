0fc25ee552f3af28798ad4e9afdf64f1bb1b47ef
gforney <gforney@gmail.com>
2014-12-23 14:53:53 +0000

    smv vv: add geom notes to smokebot

--- Utilities/Firebot/smokebot_linux.sh
+++ Utilities/Firebot/smokebot_linux.sh
@@ -97,6 +97,7 @@ GUIDE_DIR=$SMOKEBOT_DIR/guides
 STAGE_STATUS=$OUTPUT_DIR/stage_status
 SMV_VG_GUIDE=$FDS_SVNROOT/Manuals/SMV_Verification_Guide/SMV_Verification_Guide.pdf
 SMV_UG_GUIDE=$FDS_SVNROOT/Manuals/SMV_User_Guide/SMV_User_Guide.pdf
+GEOM_NOTES=$FDS_SVNROOT/Manuals/FDS_User_Guide/geom_notes.pdf
 
 THIS_FDS_AUTHOR=
 THIS_FDS_FAILED=0
@@ -1122,7 +1123,7 @@ email_build_status()
       if [ "$MUTT_MISSING" == "1" ] ; then
         mail -s "smokebot build success on ${hostname}! Revision ${SVN_REVISION}." $mailTo < $TIME_LOG > /dev/null
       else
-        mutt -a $SMV_VG_GUIDE -a $SMV_UG_GUIDE -s "smokebot build success on ${hostname}! Revision ${SVN_REVISION}." $mailTo < $TIME_LOG > /dev/null
+        mutt -a $SMV_VG_GUIDE -a $SMV_UG_GUIDE -a $GEOM_NOTES -s "smokebot build success on ${hostname}! Revision ${SVN_REVISION}." $mailTo < $TIME_LOG > /dev/null
       fi
    fi
 }
