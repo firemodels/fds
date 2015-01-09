6c08478a98294c59b4c72394c1f48a2dbe6504fb
Kristopher Overholt <koverholt@gmail.com>
2015-01-09 13:43:16 +0000

    Firebot: Remove self from email list; farewell Firebot - go forth and conquer

--- Utilities/Firebot/firebot_email_list.bat
+++ Utilities/Firebot/firebot_email_list.bat
@@ -4,4 +4,4 @@
 set mailToSMV="gforney@gmail.com"
 
 :: General mailing list for Firebot status report
-set mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com"
+set mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com"
