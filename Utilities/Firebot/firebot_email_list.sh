41d8f78ab68ad75292360f8d5abf01d9c9e0eb88
Kristopher Overholt <koverholt@gmail.com>
2015-01-09 13:43:16 +0000

    Firebot: Remove self from email list; farewell Firebot - go forth and conquer
    Former-commit-id: 3db4c8d66bf28725e0c6c07f566936cda6db9cd5 [formerly 82d3bc244eddc005b2f7b6dbe72470605ae3095a]
    Former-commit-id: 8182a9a8735d0ce7896f4953436fec45e30f40d1
    Former-commit-id: a2165bf724c7f0580a694a480f24df49229ee1cd

--- Utilities/Firebot/firebot_email_list.sh
+++ Utilities/Firebot/firebot_email_list.sh
@@ -4,8 +4,8 @@
 mailToSMV="gforney@gmail.com"
 
 # General mailing list for Firebot status report
-mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, koverholt@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com, daniel.haarhoff@gmail.com, arquitetojulio@gmail.com"
+mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com, daniel.haarhoff@gmail.com, arquitetojulio@gmail.com"
 
 # Verbose mailing list for Validationbot, Macbot, etc.
-mailToFDS_verbose="mcgratta@gmail.com, randy.mcdermott@gmail.com, drjfloyd@gmail.com, gforney@gmail.com, koverholt@gmail.com, CraigWeinschenk@gmail.com"
+mailToFDS_verbose="mcgratta@gmail.com, randy.mcdermott@gmail.com, drjfloyd@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com"
 
