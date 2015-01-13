0c4ddbbe8cdc3434d442bb71b27d8b40c3b4d7b4
gforney <gforney@gmail.com>
2015-01-13 21:49:19 +0000

    FDS vv: update email list for windows firebot
    Former-commit-id: 0811719495cc6cb59bf1f87eb9b2c9f13e87f2d9 [formerly 254f9637d67288360d8f8eb3301816a65f267488]
    Former-commit-id: 448130a1dcc4ce877fec97b4263050774f3fce03
    Former-commit-id: c43dfc5d798ae50c264c20d9e7495e15bed81da4

--- Utilities/Firebot/firebot_email_list.bat
+++ Utilities/Firebot/firebot_email_list.bat
@@ -4,4 +4,6 @@
 set mailToSMV="gforney@gmail.com"
 
 :: General mailing list for Firebot status report
-set mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com"
+set mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, drjfloyd@gmail.com"
+
+set mailToFDSAll="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com, Topi.Sikanen@gmail.com, shostikk@gmail.com, ben.trettel@gmail.com, kiliansusan@gmail.com, ruddymell@gmail.com"
