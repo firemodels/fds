e9b43f6d71cb140c71ffd026862a706ba9cec291
gforney <gforney@gmail.com>
2015-01-16 19:36:44 +0000

    FDS vv: add option to mac firebot to not revert repo (so normal user can use it)
    Former-commit-id: 9ca61ebb298124f7bf1e65cf74ebce7f939d7921 [formerly 635b5b3138ab85959dfa8eb0acd9a6d7955284cc]
    Former-commit-id: 90c49b7946a695f79498eb9c778676e2132112e5
    Former-commit-id: 6ad0aae150611994e6ae41ce0f572c22c95a690f

--- Utilities/Firebot/firebot_mac_wrapper.sh
+++ Utilities/Firebot/firebot_mac_wrapper.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+REVERT=
+while getopts 'n' OPTION
+do
+case $OPTION  in
+  n)
+  REVERT=-n
+  ;;
+esac
+done
+shift $(($OPTIND-1))
+
 running=~/firebot/firebot_running
 if [ -e $running ] ; then
   exit
 fi
 touch $running
-~/firebot/firebot_mac.sh "$@"
+~/firebot/firebot_mac.sh $REVERT "$@"
 rm $running
