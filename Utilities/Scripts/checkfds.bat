37d826db9d001857bc7c3e757306143b05791609
gforney <gforney@gmail.com>
2015-01-18 03:42:24 +0000

    FDS vv: improve error detection and reporting in windows firebot
    Former-commit-id: 081bb76b2f9720c3ca5cdb3e42b4d2083117414a [formerly 3a0e0f050db44ae18dc84c476b5a44aeb5687209]
    Former-commit-id: 51b157e5778fe82e5f18cdb6e0c3325775e222d6
    Former-commit-id: 5b9f6631985255e99f2e41c11d961791b658b8c0

---
+++ Utilities/Scripts/checkfds.bat
@@ -0,0 +1,24 @@
+@echo off
+
+:: $Date$ 
+:: $Revision$
+:: $Author$
+
+call %SVNROOT%\Utilities\Scripts\getopts.bat %*
+
+set fulldir=%BASEDIR%/%dir%
+
+set errfile=%infile%.err
+set err2file=%infile%.err2
+
+cd %fulldir%
+
+grep -v "commands for target" %errfile% > %OUTDIR%\stage_error0.txt
+grep -v "mpif.h" %OUTDIR%\stage_error0.txt > %err2file%
+grep -H -i -A 5 -B 5 "error"           %err2file% >> %OUTDIR%\stage_error.txt
+grep -H -i -A 5 -B 5 "forrtl"          %err2file% >> %OUTDIR%\stage_error.txt
+grep -H -i -A 5 -B 5 "Run aborted"     %err2file% >> %OUTDIR%\stage_error.txt
+grep -H -i -A 5 -B 5 "STOP: Numerical" %err2file% >> %OUTDIR%\stage_error.txt
+grep -H -i -A 5 -B 5 "Segmentation"    %err2file% >> %OUTDIR%\stage_error.txt
+
+cd %BASEDIR%
