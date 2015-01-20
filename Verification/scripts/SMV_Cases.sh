a30d7ca9440ad1d5ffa2b719b66073f580762cf1
gforney <gforney@gmail.com>
2015-01-20 12:48:30 +0000

    smv vv: update windows firebot and smokebot
    Former-commit-id: 1ce54cb2ed3c174383137e825a745bc967d056a3 [formerly cc0971fc59289e72c72654d51bdcb554448d0df7]
    Former-commit-id: 9a2bf29e83691736154aa08e420b214363bbfdc3
    Former-commit-id: 6648d5c7a50285ac0d1e5428df6d86bcb40f2e8d

--- Verification/scripts/SMV_Cases.sh
+++ Verification/scripts/SMV_Cases.sh
@@ -28,13 +28,11 @@ $QFDS -d Visualization vcirctest.fds
 $QFDS -d Visualization vcirctest2.fds
 $RUNTFDS -d Visualization version.fds
 
-$QFDS -d WUI levelset1.fds
-$QFDS -d WUI pine_tree.fds
-#$QFDS -d WUI fire_line.fds
 $QFDS -d WUI BT10m_2x2km_LS.fds
-$QFDS -d WUI wind_test1.fds
-$QFDS -d WUI tree_test2.fds
+#$QFDS -d WUI fire_line.fds
 $QFDS -d WUI hill_structure.fds
-#$QFDS -d WUI pine_tree.fds
-
-# $QFDS -d WUI onetree_surf_1mesh.fds
+$QFDS -d WUI levelset1.fds
+#$QFDS -d WUI onetree_surf_1mesh.fds
+$QFDS -d WUI pine_tree.fds
+$QFDS -d WUI tree_test2.fds
+$QFDS -d WUI wind_test1.fds
