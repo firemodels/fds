e6cfefba45ee41297748d30bbb52064f7d30529b
gforney <gforney@gmail.com>
2015-02-07 14:52:44 +0000

    smv vv: add script for running smokeview using Xvfb (preliminary)
    Former-commit-id: 43a3935b27512cd6b02a99e7e6aac67db84067a1 [formerly 094176022e881d61e83f6f1dd02cb1d0e9f22a95]
    Former-commit-id: efd7242d2bd1148d43a3dda37e23e7f35c26e22d
    Former-commit-id: cbd8f02014c91b6b2c2db1331412469e23cf48f6

---
+++ Utilities/Scripts/smokeview.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RUNSCRIPT=
+ssffile=
+SMV=smokeview
+
+while getopts 'd:' OPTION
+do
+case $OPTION in
+  d)
+   dir="$OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
+in=$1
+in=${in%*.*}
+
+RUNSCRIPT=-runscript
+ssffile=$in.ssf
+
+notfound=`$SMV -help 2>&1 | tail -1 | grep "not found" | wc -l`
+if [ "$notfound" == "1" ];  then
+  echo "*** Error (fatal): The program $SMV is not available. Run aborted."
+  exit
+fi
+
+if ! [ -e $in.smv ]; then
+  echo "*** Error (fatal): The smokeview file, $in.smv, does not exist. Run aborted."
+  exit
+fi
+if ! [ -e $ssffile ]; then
+  echo "*** Error (fatal): The smokeview script file, $ssffile, does not exist. Run aborted."
+  exit
+fi
+
+
+echo $SMV $RUNSCRIPT $in
+source startXserver.sh
+$SMV $RUNSCRIPT $in
+source stopXserver.sh
