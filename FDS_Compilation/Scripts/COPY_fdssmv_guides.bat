7da77ba030b0b8a519b0379055ea4f15108c16b8
gforney <gforney@gmail.com>
2015-02-19 19:59:39 +0000

    scripts: update script that downloads manuals
    Former-commit-id: 4425160c59a6efec67089fd13a4b451e1445fdc0 [formerly 044c677fffd744538208754bdf6557fd20e12d73]
    Former-commit-id: bb14ff5917abbc349135963a71b044ba493ab48d
    Former-commit-id: f6c5a08f312cd9bc3f0785ed8f157e417d4389f0

--- FDS_Compilation/Scripts/COPY_fdssmv_guides.bat
+++ FDS_Compilation/Scripts/COPY_fdssmv_guides.bat
@@ -1,39 +1,31 @@
 @echo off
 
-Rem  Windows batch file to create an achive for a 64 bit Linux smokeview
-
-Rem setup environment variables (defining where repository resides etc) 
-
-set envfile="%userprofile%"\fds_smv_env.bat
-IF EXIST %envfile% GOTO endif_envexist
-echo ***Fatal error.  The environment setup file %envfile% does not exist. 
-echo Create a file named %envfile% and use SMV/scripts/fds_smv_env_template.bat
-echo as an example.
-echo.
-echo Aborting now...
-pause>NUL
-goto:eof
-
-:endif_envexist
-
-call %envfile%
-
-%svn_drive%
-
 echo.
 echo ---downloading guides
 echo.
+
 set fromdir=/var/www/html/firebot/manuals
-set todir="%userprofile%\Google Drive"
+set login=%username%@blaze.nist.gov
 
-pscp %svn_logon%:%fromdir%/FDS_User_Guide.pdf  %todir%\.
-pscp %svn_logon%:%fromdir%/FDS_Verification_Guide.pdf  %todir%\.
-pscp %svn_logon%:%fromdir%/FDS_Technical_Reference_Guide.pdf  %todir%\.
-pscp %svn_logon%:%fromdir%/FDS_Validation_Guide.pdf %todir%\.
-pscp %svn_logon%:%fromdir%/FDS_Configuration_Management_Plan.pdf %todir%\.
-pscp %svn_logon%:%fromdir%/SMV_Technical_Reference_Guide.pdf %todir%\.
-pscp %svn_logon%:%fromdir%/SMV_User_Guide.pdf %todir%\.
-pscp %svn_logon%:%fromdir%/SMV_Verification_Guide.pdf %todir%\.
+cd "%userprofile%
+if not exist "Google Drive" (
+  mkdir "Google Drive"
+)
+cd "Google Drive"
 
+if not exist "FDS-SMV Newest Manuals" (
+  mkdir "FDS-SMV Newest Manuals"
+)
+cd "FDS-SMV Newest Manuals"
+
+
+pscp %login%:%fromdir%/FDS_User_Guide.pdf  .
+pscp %login%:%fromdir%/FDS_Verification_Guide.pdf  .
+pscp %login%:%fromdir%/FDS_Technical_Reference_Guide.pdf  .
+pscp %login%:%fromdir%/FDS_Validation_Guide.pdf .
+pscp %login%:%fromdir%/FDS_Configuration_Management_Plan.pdf .
+pscp %login%:%fromdir%/SMV_Technical_Reference_Guide.pdf .
+pscp %login%:%fromdir%/SMV_User_Guide.pdf .
+pscp %login%:%fromdir%/SMV_Verification_Guide.pdf .
 
 pause
