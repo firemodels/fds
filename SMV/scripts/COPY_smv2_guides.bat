f57157499bf125f99140a8b6c08cdc77bdb0aa47
gforney <gforney@gmail.com>
2015-03-07 13:56:48 +0000

    vv scripts: update guide upload scripts to be consistent with recent changes to firebot

---
+++ SMV/scripts/COPY_smv2_guides.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo.
+echo ---downloading guides
+echo.
+
+set fromdir=/var/www/html/firebot/manuals
+set login=%username%@blaze.nist.gov
+
+cd "%userprofile%
+if not exist "Google Drive" (
+  mkdir "Google Drive"
+)
+cd "Google Drive"
+
+if not exist "FDS-SMV Newest Manuals" (
+  mkdir "FDS-SMV Newest Manuals"
+)
+cd "FDS-SMV Newest Manuals"
+
+
+pscp %login%:%fromdir%/SMV_Technical_Reference_Guide.pdf .
+pscp %login%:%fromdir%/SMV_User_Guide.pdf .
+pscp %login%:%fromdir%/SMV_Verification_Guide.pdf .
+
+pause
