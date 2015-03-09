25f3607a22115a547d4439257b9295f56aae2ca4
gforney <gforney@gmail.com>
2015-03-09 22:46:05 +0000

    FDS/smv bots: minor corrections to bots

--- Utilities/Firebot/fds_guides2GD.sh
+++ Utilities/Firebot/fds_guides2GD.sh
@@ -5,7 +5,7 @@ BASEDIR=Newest_FDS_Guides
 UPDATE_GUIDES=$FROMDIR/$BASEDIR/update_guides
 if [ -e $UPDATE_GUIDES ] ; then
   cd $FROMDIR
-  gdrive upload -i $BASEDIR
+  drive upload -i $BASEDIR
   rm $UPDATE_GUIDES
   cd $CURDIR
 fi
