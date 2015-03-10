a8f2064ecef42f238c06f7f0596f4a967920f017
gforney <gforney@gmail.com>
2015-03-10 00:56:19 +0000

    FDS/smv vv: corrections to guides->google scripts
    Former-commit-id: 0058c06f91d52f007f0d6d3504fb1aa5322af38d [formerly 2e1acbccca7d6dc081f9c75c1b6f306168273245]
    Former-commit-id: 786d3fbbe4f970e23790f886191c235714c0c3ee
    Former-commit-id: 77b5ce32f50c921c3ea1c1009bb328c0f431b663

--- Utilities/Firebot/fds_guides2GD.sh
+++ Utilities/Firebot/fds_guides2GD.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+
 CURDIR=`pwd`
+PARENT_ID=0B-W-dkXwdHWNeU41UkZiNV93Mmc
 FROMDIR=~firebot/firebot/output
 BASEDIR=Newest_FDS_Guides
 UPDATE_GUIDES=$FROMDIR/$BASEDIR/update_guides
+
+UPLOAD ()
+{
+  FILE=$1
+  FILEnew=${FILE}_new.pdf
+  cp $FILE.pdf /tmp/$FILEnew
+  drive list  | grep $FILEnew | awk '{ system("drive delete -i " $1)} '
+  drive upload -p $PARENT_ID -f /tmp/$FILEnew
+}
+
 if [ -e $UPDATE_GUIDES ] ; then
-  cd $FROMDIR
-  drive upload -i $BASEDIR
-  rm $UPDATE_GUIDES
+  cd $FROMDIR/$BASEDIR
+  rm -f $UPDATE_GUIDES
+  UPLOAD FDS_Configuration_Management_Plan
+  UPLOAD FDS_Technical_Reference_Guide
+  UPLOAD FDS_User_Guide
+  UPLOAD FDS_Validation_Guide
+  UPLOAD FDS_Verification_Guide
   cd $CURDIR
 fi
--- Utilities/Firebot/smv_guides2GD.sh
+++ Utilities/Firebot/smv_guides2GD.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+
 CURDIR=`pwd`
+PARENT_ID=0B-W-dkXwdHWNfjRyMjFWSzVBRDV3YWpmUzNyZ0pvMmRfVGhoTzdkTEFjdzN3RENWcmQ2UUk
 FROMDIR=~smokebot/smokebot/output
 BASEDIR=Newest_Smokeview_Guides
 UPDATE_GUIDES=$FROMDIR/$BASEDIR/update_guides
+
+UPLOAD ()
+{
+  FILE=$1
+  FILEnew=${FILE}_new.pdf
+  cp $FILE.pdf /tmp/$FILEnew
+  drive list  | grep $FILEnew | awk '{ system("drive delete -i " $1)} '
+  drive upload -p $PARENT_ID -f /tmp/$FILEnew
+}
+
 if [ -e $UPDATE_GUIDES ] ; then
-  cd $FROMDIR
-  drive upload -i $BASEDIR
-  rm $UPDATE_GUIDES
+  cd $FROMDIR/$BASEDIR
+  rm -f $UPDATE_GUIDES
+  UPLOAD SMV_User_Guide
+  UPLOAD SMV_Technical_Reference_Guide
+  UPLOAD SMV_Verification_Guide
   cd $CURDIR
 fi
