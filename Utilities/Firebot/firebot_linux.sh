a7dc8215be641c21d92e1cc4f7ffda06a9707c92
gforney <gforney@gmail.com>
2015-03-10 18:58:50 +0000

    vv scripts: update web links

--- Utilities/Firebot/firebot_linux.sh
+++ Utilities/Firebot/firebot_linux.sh
@@ -1218,7 +1218,7 @@ email_build_status()
       echo "Stop Time: $stop_time " >> $TIME_LOG
       echo "-------------------------------" >> $TIME_LOG
       echo "Nightly Manuals (private):  http://blaze.nist.gov/firebot" >> $TIME_LOG
-      echo "Nightly Manuals  (public):  https://drive.google.com/folderview?id=0B_wB1pJL2bFQUlJwMmNfaHlqME0&usp=sharing" >> $TIME_LOG
+      echo "Nightly Manuals  (public):  http://goo.gl/n1Q3WH" >> $TIME_LOG
       echo "-------------------------------" >> $TIME_LOG
       mail -s "[${1}@$hostname] ${2} success! Revision ${SVN_REVISION} passed all build tests." $mailToFDS < $TIME_LOG > /dev/null
    fi
