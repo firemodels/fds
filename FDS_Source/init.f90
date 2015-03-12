96c42bacc945002d645516ecf246b6bcb7baff61
rmcdermo <randy.mcdermott@gmail.com>
2015-03-12 16:43:35 +0000

    FDS Source: check for thin obstructions only at internal wall cells

--- FDS_Source/init.f90
+++ FDS_Source/init.f90
@@ -919,7 +919,9 @@ WALL_LOOP_0: DO IW=1,M%N_EXTERNAL_WALL_CELLS+M%N_INTERNAL_WALL_CELLS
       SOLID_CELL = M%SOLID(IC)
    ENDIF
 
-   IF (.NOT.SOLID_CELL) THEN
+   ! Check internal wall cells for thin obstructions with specified velocity
+
+   IF (IW>M%N_EXTERNAL_WALL_CELLS .AND. .NOT.SOLID_CELL) THEN
       IF (ABS(WC%UW0)>TWO_EPSILON_EB .OR. ANY(SF%LEAK_PATH>=0) .OR.  SF%PYROLYSIS_MODEL/=PYROLYSIS_NONE) THEN
          WRITE(LU_ERR,'(A,A,A,I4)') 'ERROR: SURF ',TRIM(SF%ID),' cannot be applied to a thin obstruction, OBST #',&
                                     M%OBSTRUCTION(WC%OBST_INDEX)%ORDINAL
