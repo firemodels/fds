32ce7b00bd5f118915b84838305993445e4d9c00
gforney <gforney@gmail.com>
2015-03-16 17:15:23 +0000

    smv build: remove glui_v2_36 - not used

--- SMV/source/glui_v2_36/makelib.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call ..\setopts %OPTS%
-erase *.o *.obj libglui.a libglui.lib
-set target=libglui.lib
-if %COMPILER% == gcc set target=libglui.a
-make COMPILER=%COMPILER% COMPILER2=%COMPILER2% SIZE=%SIZE% RM=erase -f ./makefile %target%
--- SMV/source/glui_v2_36/makelib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -f *.o
-source ../setopts.sh $*
-
-rm -f *.o
-eval make COMPILER=${COMPILER} COMPILER2=${COMPILER2} SIZE=${SIZE} PLATFORM=\"${PLATFORM}\"
