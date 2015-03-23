40b29640fd2ab46dc7fa8af62c8b0a1734a38d97
gforney <gforney@gmail.com>
2015-03-23 19:10:32 +0000

    FDS valg: rewrite scripts to output FDS build date and revision number for each validation case in Table 15.2 - addresses issue 2387
    Former-commit-id: 0d10b618f3629fcd569b5e8c859a6b9d0cb57fd4 [formerly 391435544252cdcf047d5401ccdcd738cfd0dcf3]
    Former-commit-id: e44267ed49f8caf0efb00a8b73db928e5b97b920
    Former-commit-id: 59b84d308a369bd0c5ef669f286e321843948eac

---
+++ Utilities/Scripts/makesvnentry.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR=$1
+curdir=`pwd`
+svnnumdata=/tmp/svnnumdata.$$
+cat ~/FDS-SMV/Validation/$DIR/FDS_Output_Files/${DIR}*svn.txt > $svnnumdata 2> /dev/null
+svnnum=`tail -1 $svnnumdata`
+if [ "$svnnum" != "" ] ; then
+  cd ~/FDS-SMV/FDS_Source
+  svndate=`svn -r $svnnum info | grep Date | awk '{print $4}'`
+  echo "${DIR//_/\_}  & $svndate & $svnnum \\\\ \hline"
+  cd $curdir
+fi
+rm $svnnumdata
