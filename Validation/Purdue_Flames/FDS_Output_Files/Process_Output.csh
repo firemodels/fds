99b755457a8f5654784b4cecb7487a11aca7dc9f
mcgratta <mcgratta@gmail.com>
2015-03-24 16:03:17 +0000

    FDS Validation: More results for 6.2.0.

--- Validation/Purdue_Flames/FDS_Output_Files/Process_Output.csh
+++ Validation/Purdue_Flames/FDS_Output_Files/Process_Output.csh
@@ -2,7 +2,6 @@
 setenv SVNROOT ~/FDS-SMV
 set DDIR=$SVNROOT/Validation/Purdue_Flames/Current_Results
 set WDIR=$SVNROOT/Validation/Purdue_Flames/FDS_Output_Files
-#cp $DDIR/*devc.csv $WDIR
 cp $DDIR/*line.csv $WDIR
 cp $DDIR/*svn.txt $WDIR
 
--- Validation/Purdue_Flames/Run_All.sh
+++ Validation/Purdue_Flames/Run_All.sh
@@ -5,10 +5,7 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-$QFDS $DEBUG $QUEUE -d $INDIR 7p1_cm_methane_4mm.fds
-#$QFDS $DEBUG $QUEUE -d $INDIR 7p1_cm_methane_2mm.fds
+$QFDS $DEBUG       $QUEUE -d $INDIR 7p1_cm_methane_4mm.fds
 $QFDS $DEBUG -p 16 $QUEUE -d $INDIR 7p1_cm_methane_2mm_16mesh.fds
-#$QFDS $DEBUG -p 128 $QUEUE -d $INDIR 7p1_cm_methane_1mm_128mesh_les.fds
-#$QFDS $DEBUG -p 128 $QUEUE -d $INDIR 7p1_cm_methane_1mm_128mesh_dns.fds
 
 echo FDS cases submitted
