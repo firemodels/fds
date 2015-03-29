ea41cc515b13f5142f335bcdf29bb67a611c8ad8
gforney <gforney@gmail.com>
2015-03-29 13:38:46 +0000

    smv vv: update movie testing scripts
    Former-commit-id: 70f608a45232b11a48c68a5343a480d4abc93337 [formerly 140e444d8a7c9d09587388de653e7ab3e4ff740b]
    Former-commit-id: 37706f8527e03f33b15bdb01a34738fbe01a5704
    Former-commit-id: 698f16cc7ab6702a8767bedfe4dc4c8185e595ea

--- Verification/Visualization/makemovie_test.bat
+++ Verification/Visualization/makemovie_test.bat
@@ -1,4 +1,6 @@
 @echo off
 erase movie_test*.png 2>Nul
-smokeview -runscript movie_test 
+echo making images
+smokeview -runscript movie_test > Nul 2>Nul
+echo making movie
 ffmpeg -y -i movie_test_%%04d.png movie_test.avi
