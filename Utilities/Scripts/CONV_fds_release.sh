e43dedc13ee8644ae3bac12c751359e795095358
gforney <gforney@gmail.com>
2015-04-05 03:39:09 +0000

    bundle scripts: update script to convert readme wikis to html

--- Utilities/Scripts/CONV_fds_release.sh
+++ Utilities/Scripts/CONV_fds_release.sh
@@ -10,11 +10,11 @@ echo Converting the wiki to html format
 $wikify -r $bundleinfo/fds.wiki > $bundleinfo/release_notes.htm
 
 echo obtaining FDS MPI Linux release notes wiki from the repository
-svn export --quiet --force https://code.google.com/p/fds-smv/source/browse/wiki/Running_FDS_MPI_on_Linux.wiki $bundleinfo/fds_linux.wiki
+svn export --quiet --force https://fds-smv.googlecode.com/svn/wiki/Running_FDS_MPI_on_Linux.wiki $bundleinfo/fds_linux.wiki
 echo Converting the FDS MPI Linux wiki to html format
 $wikify -r $bundleinfo/fds_linux.wiki > $bundleinfo/fds_linux.html
 
 echo obtaining FDS MPI OX X release notes wiki from the repository
-svn export --quiet --force https://code.google.com/p/fds-smv/source/browse/wiki/Running_FDS_MPI_on_OSX.wiki $bundleinfo/fds_osx.wiki
+svn export --quiet --force https://fds-smv.googlecode.com/svn/wiki/Running_FDS_MPI_on_OSX.wiki $bundleinfo/fds_osx.wiki
 echo Converting the FDS MPI OSX wiki to html format
 $wikify -r $bundleinfo/fds_osx.wiki > $bundleinfo/fds_osx.html
