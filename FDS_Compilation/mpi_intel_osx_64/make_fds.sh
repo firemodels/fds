79672dd39dfb72c117d7028568b89f7ab7f1d279
gforney <gforney@gmail.com>
2015-04-10 23:04:56 +0000

    FDS build: correction to osx 64 build script
    Former-commit-id: 440a5a564671dd3c18f64c2c55f21d0ca28b1163 [formerly 73fc8dedef6d1a6a68bdd327b559e226dc4f25c7]
    Former-commit-id: ad40d31a82ee3bdbe4b6d85fcad8c8641a45fb31
    Former-commit-id: a69fd1416f2323d378676f308ae06412af459b77

--- FDS_Compilation/mpi_intel_osx_64/make_fds.sh
+++ FDS_Compilation/mpi_intel_osx_64/make_fds.sh
@@ -4,7 +4,6 @@ dir=`pwd`
 target=${dir##*/}
 
 source $IFORT_COMPILER/bin/compilervars.sh $platform
-source ~/.bashrc_fds $platform
 
 echo Building $target
 make -j4 VPATH="../../FDS_Source" -f ../makefile $target
