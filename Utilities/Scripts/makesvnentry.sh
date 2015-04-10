edd8971ce755ce7f79603922324e576bbd0c3d17
gforney <gforney@gmail.com>
2015-04-10 11:52:58 +0000

    FDS source: output svn number and REVISION_DATE to a file (on one line), modify makesvnentry.sh script to handle this change

--- Utilities/Scripts/makesvnentry.sh
+++ Utilities/Scripts/makesvnentry.sh
@@ -2,15 +2,16 @@
 DIR=$1
 curdir=`pwd`
 svnnumdata=/tmp/svnnumdata.$$
+svndatedata=/tmp/svndatedata.$$
 ncfile=/tmp/ncfile.$$
-cat ~/FDS-SMV/Validation/$DIR/FDS_Output_Files/*svn.txt 2> /dev/null | sort -u > $svnnumdata
+cat ~/FDS-SMV/Validation/$DIR/FDS_Output_Files/*svn.txt 2> /dev/null | awk '{print $1}' | sort -u > $svnnumdata
+cat ~/FDS-SMV/Validation/$DIR/FDS_Output_Files/*svn.txt 2> /dev/null | awk '{print $3}' | sort -u > $svndatedata
 svnnum=`head -1 $svnnumdata`
 svnnum2=`tail -1 $svnnumdata`
 cat $svnnumdata | wc -l > $ncfile 2> /dev/null
 nc=`cat $ncfile`
 if [ "$svnnum" != "" ] ; then
-  cd ~/FDS-SMV/FDS_Source
-  svndate=`svn -r $svnnum info | grep Date | awk '{print $4}'`
+  svndate=`head -1 $svndatedata`
   if [[ "$nc" -gt 1 ]] ; then
     svnnum=$svnnum-$svnnum2
   fi
@@ -18,4 +19,5 @@ if [ "$svnnum" != "" ] ; then
   cd $curdir
 fi
 rm $svnnumdata
+rm $svndatedata
 rm $ncfile
