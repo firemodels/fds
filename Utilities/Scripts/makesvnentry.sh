f443629b719da98463fbd3a241289320cd89b903
gforney <gforney@gmail.com>
2015-04-11 19:09:47 +0000

    bundle scripts: revert makesvnentry.sh script to revision 22315

--- Utilities/Scripts/makesvnentry.sh
+++ Utilities/Scripts/makesvnentry.sh
@@ -2,16 +2,15 @@
 DIR=$1
 curdir=`pwd`
 svnnumdata=/tmp/svnnumdata.$$
-svndatedata=/tmp/svndatedata.$$
 ncfile=/tmp/ncfile.$$
-cat ~/FDS-SMV/Validation/$DIR/FDS_Output_Files/*svn.txt 2> /dev/null | awk '{print $1}' | sort -u > $svnnumdata
-cat ~/FDS-SMV/Validation/$DIR/FDS_Output_Files/*svn.txt 2> /dev/null | awk '{print $3}' | sort -u > $svndatedata
+cat ~/FDS-SMV/Validation/$DIR/FDS_Output_Files/*svn.txt 2> /dev/null | sort -u > $svnnumdata
 svnnum=`head -1 $svnnumdata`
 svnnum2=`tail -1 $svnnumdata`
 cat $svnnumdata | wc -l > $ncfile 2> /dev/null
 nc=`cat $ncfile`
 if [ "$svnnum" != "" ] ; then
-  svndate=`head -1 $svndatedata`
+  cd ~/FDS-SMV/FDS_Source
+  svndate=`svn -r $svnnum info | grep Date | awk '{print $4}'`
   if [[ "$nc" -gt 1 ]] ; then
     svnnum=$svnnum-$svnnum2
   fi
@@ -19,5 +18,4 @@ if [ "$svnnum" != "" ] ; then
   cd $curdir
 fi
 rm $svnnumdata
-rm $svndatedata
 rm $ncfile
