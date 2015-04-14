856c9103edc97fde1ef9ddd00f558577c5853f80
gforney <gforney@gmail.com>
2015-04-14 20:07:11 +0000

    FDS build: when building the Linux/mpi version of FDS set the appropriate (ethernet or infiniband) MPI library required (which may not be the same as what was set in the users startup files

--- FDS_Compilation/mpi_intel_linux_64ib_db/make_fds.sh
+++ FDS_Compilation/mpi_intel_linux_64ib_db/make_fds.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+platform=intel64
 dir=`pwd`
 target=${dir##*/}
 
-source $IFORT_COMPILER/bin/compilervars.sh intel64
-source ~/.bashrc_fds intel64ib
+source $IFORT_COMPILER/bin/compilervars.sh $platform
+source ../Scripts/set_mpidist.sh /shared/openmpi_64ib
+if [ "$MPIDIST" == "" ]; then
+  exit
+fi
 
 echo Building $target 
 make -j4 VPATH="../../FDS_Source" -f ../makefile $target
