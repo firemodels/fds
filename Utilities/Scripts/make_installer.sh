1787c040a6fd6e40af13928d53a9c5b4b5c576c3
gforney <gforney@gmail.com>
2015-04-14 18:48:10 +0000

    build scripts: update bundle generating script
    Former-commit-id: 57aab61dcc889a103297bc673fff5d9324b162e5 [formerly 9e0c6e4c0f101ded88d9a84197405392f02b84c1]
    Former-commit-id: 2a468aa80b8a04f3c4e4f1aaa33f2675f00768e6
    Former-commit-id: 6ab19731697d2ffff2f07ca3ea1708f6640d5272

--- Utilities/Scripts/make_installer.sh
+++ Utilities/Scripts/make_installer.sh
@@ -338,7 +338,7 @@ cat << BASH > \$BASHFDS
 
 # MPI distribution location
 
-export MPIDIST=\$mpipath
+export MPIDIST=\\\$1
 
 # unalias application names used by FDS
 
@@ -405,7 +405,7 @@ cat << EOF >> $INSTALLER
   grep -v bashrc_fds ~/.bash_profile | grep -v "#FDS" > \$BASHSTARTUP
   echo "#FDS " >> \$BASHSTARTUP
   echo "#FDS Setting the environment for FDS and Smokeview. "   >> \$BASHSTARTUP
-  echo "source ~/.bashrc_fds" >> \$BASHSTARTUP
+  echo "source ~/.bashrc_fds \$mpipath" >> \$BASHSTARTUP
   cp \$BASHSTARTUP ~/.bash_profile
   rm \$BASHSTARTUP
 EOF
@@ -420,7 +420,7 @@ cat << EOF >> $INSTALLER
   grep -v bashrc_fds ~/.bashrc | grep -v "#FDS" > \$BASHSTARTUP
   echo "#FDS " >> \$BASHSTARTUP
   echo "#FDS Setting the environment for FDS and Smokeview. "   >> \$BASHSTARTUP
-  echo "source ~/.bashrc_fds" >> \$BASHSTARTUP
+  echo "source ~/.bashrc_fds \$mpipath" >> \$BASHSTARTUP
   cp \$BASHSTARTUP ~/.bashrc
   rm \$BASHSTARTUP
 EOF
