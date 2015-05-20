c342a647588cfac1cb7accdf71aa07b2962a57a8
gforney <gforney@gmail.com>
2015-05-20 19:10:21 +0000

    build scripts: update keyword expansion scripts

--- SMV/Build/intel_linux_64/make_smv.sh
+++ SMV/Build/intel_linux_64/make_smv.sh
@@ -4,7 +4,11 @@ source ../test_ifort.sh
 source $IFORT_COMPILER/bin/compilervars.sh intel64
 LIBDIR=../LIBS/lib_linux_intel_64/
 source ../test_libs.sh
+KWDIR=../../../Utilities/keyword
+SDIR=../../source
 
+source $KWDIR/expand_file.sh $KWDIR $SDIR/smokeview $SDIR/shared/str_util.c
 make -f ../Makefile clean
 eval make ${SMV_MAKE_OPTS} -f ../Makefile intel_linux_64
+source $KWDIR/contract_file.sh $KWDIR $SDIR/smokeview/main.c
 
