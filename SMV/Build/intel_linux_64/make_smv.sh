a40e34a3cffd29373d04933c3d7afe958a157240
gforney <gforney@gmail.com>
2015-05-20 23:36:27 +0000

    smv build: corrections to smokeview build scripts

--- SMV/Build/intel_linux_64/make_smv.sh
+++ SMV/Build/intel_linux_64/make_smv.sh
@@ -7,7 +7,7 @@ source ../test_libs.sh
 KWDIR=../../../Utilities/keyword
 SDIR=../../source
 
-source $KWDIR/expand_file.sh $KWDIR $SDIR/smokeview $SDIR/shared/str_util.c
+source $KWDIR/expand_file.sh $KWDIR $SDIR/smokeview $SDIR/shared/string_util.c
 make -f ../Makefile clean
 eval make ${SMV_MAKE_OPTS} -f ../Makefile intel_linux_64
 source $KWDIR/contract_file.sh $KWDIR $SDIR/smokeview/main.c
--- SMV/Build/intel_linux_64/make_smv_db.sh
+++ SMV/Build/intel_linux_64/make_smv_db.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source $IFORT_COMPILER/bin/compilervars.sh intel64
 make -f ../Makefile clean
+source $KWDIR/expand_file.sh $KWDIR $SDIR/smokeview $SDIR/shared/string_util.c
 make -f ../Makefile intel_linux_64_db
+source $KWDIR/contract_file.sh $KWDIR $SDIR/smokeview/main.c
+
