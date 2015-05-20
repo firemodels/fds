ca31efa2b1479051cdf6bf9ee97b4c9e459e6d7c
gforney <gforney@gmail.com>
2015-05-20 23:58:15 +0000

    smv util build: add keyword expansion scripts to smokeview utility bild scripts
    Former-commit-id: 0a764f204a1b50bb5227a5db0015c141d6827caf [formerly 4780be7a033673bd461b65db4f93375440af6a8a]
    Former-commit-id: e18eaedaf723e2b4f1d6cf609aae03757083cdd9
    Former-commit-id: b3399403fa95a3dc9f97dc350de1ab5e03faf1a8

--- Utilities/smokezip/intel_linux_64/make_zip.sh
+++ Utilities/smokezip/intel_linux_64/make_zip.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 source $IFORT_COMPILER/bin/compilervars.sh intel64
+KWDIR=../../keyword
+SDIR=../../../SMV/source
+
 rm -f *.o
+$KWDIR/expand_file.sh $KWDIR $SDIR/smokezip $SDIR/shared/string_util.c
 make -f ../Makefile intel_linux_64
+$KWDIR/contract_file.sh $KWDIR $SDIR/shared/string_util.c
