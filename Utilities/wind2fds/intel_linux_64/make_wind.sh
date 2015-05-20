0d11df9b2d087d6d2a3da04e24d7e8605aaf784b
gforney <gforney@gmail.com>
2015-05-20 23:58:15 +0000

    smv util build: add keyword expansion scripts to smokeview utility bild scripts
    Former-commit-id: 0a764f204a1b50bb5227a5db0015c141d6827caf [formerly 4780be7a033673bd461b65db4f93375440af6a8a]
    Former-commit-id: e18eaedaf723e2b4f1d6cf609aae03757083cdd9
    Former-commit-id: b3399403fa95a3dc9f97dc350de1ab5e03faf1a8

--- Utilities/wind2fds/intel_linux_64/make_wind.sh
+++ Utilities/wind2fds/intel_linux_64/make_wind.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -f
 source $IFORT_COMPILER/bin/compilervars.sh intel64
+KWDIR=../../keyword
+SDIR=../../../SMV/source
+
 rm -f *.o
+$KWDIR/expand_file.sh $KWDIR $SDIR/wind2dfs $SDIR/shared/string_util.c
 make -j4 -f ../Makefile intel_linux_64
+$KWDIR/contract_file.sh $KWDIR $SDIR/shared/string_util.c
