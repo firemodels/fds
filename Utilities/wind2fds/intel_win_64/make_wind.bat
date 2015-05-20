35d22c142f6f98830e08a292a79660832fede448
gforney <gforney@gmail.com>
2015-05-20 20:50:12 +0000

    smv source: use keyword expansion routine to determine revision string rather than svn keyword expansion (in order to be compatible with git)
    Former-commit-id: dd1c5c9d7b1fe42ef408dcf0c34f8a57a40d02e7 [formerly 2fc0fb4b6145cf4ccc6c7e592662e9443e1fc2ae]
    Former-commit-id: 7f3f045b5e270e0db031a7bfe9cd1cd45322d0d1
    Former-commit-id: 4a7cf014b9048ba3d6e34f12e3c7ce9035fd59e6

--- Utilities/wind2fds/intel_win_64/make_wind.bat
+++ Utilities/wind2fds/intel_win_64/make_wind.bat
@@ -1,10 +1,14 @@
 @echo off
 :: setup compiler environment
 call ..\..\Scripts\setup_intel_compilers.bat
+set KWDIR=..\..\keyword
+set SDIR=..\..\..\SMV\source
 
 Title Building wind2fds for 64 bit Windows
 
 erase *.obj *.mod
+call %KWDIR%\expand_file %SDIR%\wind2fds %SDIR%\shared\string_util.c
 make -f ..\Makefile intel_win_64
+call %KWDIR%\contract_file %SDIR%\shared\string_util.c
 pause
 
