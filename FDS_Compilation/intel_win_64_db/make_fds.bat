99a067e228ddd3ae7cd3efbf8c3e166d0ad8b1d8
gforney <gforney@gmail.com>
2015-05-21 18:56:56 +0000

    FDS source: perform keyword substitution using scripts rather than svn (since git which will be used shortly does not support keyword substitution).
    Former-commit-id: 96278c57972be6b5bcec9638accb09879fbd7c67 [formerly f4f1042c21211387662df4e0f6fa04b1dceae439]
    Former-commit-id: 4a7c61f4d11c340f508f6c6cb7877f986e57af36
    Former-commit-id: 8153c668bc903dd61a31ba8baa078e3df15cbf7a

--- FDS_Compilation/intel_win_64_db/make_fds.bat
+++ FDS_Compilation/intel_win_64_db/make_fds.bat
@@ -1,8 +1,12 @@
 @echo off
 :: setup compiler environment
 call ..\..\Utilities\Scripts\setup_intel_compilers.bat
+set KWDIR=..\..\Utilities\keyword
+set SDIR=..\..\FDS_Source
 
 Title Building debug FDS for 64 bit Windows
 
+call %KWDIR%\expand_file %SDIR% %SDIR%\main.f90
 make VPATH="../../FDS_Source" -f ..\makefile intel_win_64_db
+call %KWDIR%\contract_file %SDIR%\main.f90
 pause
--- FDS_Compilation/mpi_intel_win_64_db/make_fds.bat
+++ FDS_Compilation/mpi_intel_win_64_db/make_fds.bat
@@ -1,8 +1,12 @@
 @echo off
 :: setup compiler environment
 call ..\..\Utilities\Scripts\setup_intel_compilers.bat
+set KWDIR=..\..\Utilities\keyword
+set SDIR=..\..\FDS_Source
 
 Title Building debug FDS (mpi) for 64 bit Windows
 
+call %KWDIR%\expand_file %SDIR% %SDIR%\main.f90
 make VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_64_db
+call %KWDIR%\contract_file %SDIR%\main.f90
 pause
