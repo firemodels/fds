a9ea5f568e73672dcbd208c7efcc70ec7e0dfd38
gforney <gforney@gmail.com>
2015-05-21 19:31:01 +0000

    smv ug: expand Revision keyword in script that builds smv ug guide
    Former-commit-id: 9c1cd0fd016173b3e8e2b613882a9b3e5fa43424 [formerly b35b249526afae7a88ffe68050fc6d517fa6a668]
    Former-commit-id: 23f778b5e82af2f148ccec6729b0cf1f6ca47c8e
    Former-commit-id: cbfba22edddd94f02a45a330e9f62e5a60fa0855

--- Manuals/SMV_User_Guide/make_guide.sh
+++ Manuals/SMV_User_Guide/make_guide.sh
@@ -6,10 +6,15 @@ export TEXINPUTS=".:../LaTeX_Style_Files:"
 clean_build=1
 
 # Build SMV User Guide
+KWDIR=../../Utilities/keyword
+SDIR=.
+
+source $KWDIR/expand_file.sh $KWDIR $SDIR $SDIR/SMV_User_Guide.tex
 pdflatex -interaction nonstopmode SMV_User_Guide &> SMV_User_Guide.err
 bibtex SMV_User_Guide &> SMV_User_Guide.err
 pdflatex -interaction nonstopmode SMV_User_Guide &> SMV_User_Guide.err
 pdflatex -interaction nonstopmode SMV_User_Guide &> SMV_User_Guide.err
+source $KWDIR/contract_file.sh $KWDIR $SDIR/SMV_User_Guide.tex
 
 # Scan and report any errors in the LaTeX build process
 if [[ `grep -E "Error:|Fatal error|! LaTeX Error:|Paragraph ended before|Missing \\\$ inserted|Misplaced" -I SMV_User_Guide.err | grep -v "xpdf supports version 1.5"` == "" ]]
