d33ac0d002655a04b492476cc52b45190c4f20f2
gforney <gforney@gmail.com>
2015-05-21 20:38:59 +0000

    build scripts: add some error checking to windows keyword expansion/contraction scripts
    Former-commit-id: daa56fcae13073aba40f3963bad2bd65fca27897 [formerly e646b35cbbeb7c325e106e0c24051bc1ac638bee]
    Former-commit-id: 556b256c09d99f70ede570fca0725db85181aaa2
    Former-commit-id: 22610e3f3f052c80c5d3371c55e01a5b0ff20651

--- Utilities/keyword/expand_keyword.bat
+++ Utilities/keyword/expand_keyword.bat
@@ -17,6 +17,20 @@ set file=%3
 if NOT exist %file% (
   goto eof
 )
+
+:: looking for sed
+
+set temp1="%temp%\temp.txt"
+set temp1c="%temp%\tempc.txt"
+
+sed 1> %temp1% 2>&1
+type %temp1% | find /i /c "not recognized" > %temp1c%
+set flag=
+set /p flag=<%temp1c%
+if %flag% == 1 (
+  goto eof
+)
+
 sed -e "s/$%keyword%:.*\$/$%keyword%: %newvalue% $/g" %file% | sed "s/$/\r/" > %temp%\temp.txt
 copy %temp%\temp.txt %file% 1> Nul 2>&1
 
