cae77714caa84ee95907be2c8d635474ca3f9133
gforney <gforney@gmail.com>
2015-05-21 00:24:13 +0000

    build scripts: add keyword expansion scripts to osx build scripts

--- SMV/Build/intel_osx_64/make_smv.sh
+++ SMV/Build/intel_osx_64/make_smv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 source ../setopts.sh $*
-#source ../test_ifort.sh
 LIBDIR=../LIBS/lib_osx_intel_64/
 source ../test_libs.sh
+KWDIR=../../../Utilities/keyword
+SDIR=../../source
 
+source $KWDIR/expand_file.sh $KWDIR $SDIR/smokeview $SDIR/shared/string_util.c
 make -f ../Makefile clean
 eval make ${SMV_MAKE_OPTS}-f ../Makefile intel_osx_64
+source $KWDIR/contract_file.sh $KWDIR $SDIR/smokeview/main.c
--- Utilities/smokezip/intel_osx_64/make_zip.sh
+++ Utilities/smokezip/intel_osx_64/make_zip.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+KWDIR=../../keyword
+SDIR=../../../SMV/source
+
+$KWDIR/expand_file.sh $KWDIR $SDIR/smokezip $SDIR/shared/string_util.c
 rm -f *.o
 make -f ../Makefile intel_osx_64
+$KWDIR/contract_file.sh $KWDIR $SDIR/shared/string_util.c
