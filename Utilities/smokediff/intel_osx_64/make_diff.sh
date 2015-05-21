ce18a186a04af8b7f723fba43baf1d428bc2419a
gforney <gforney@gmail.com>
2015-05-21 00:24:13 +0000

    build scripts: add keyword expansion scripts to osx build scripts
    Former-commit-id: 37862e6d5cd8dd35b5774ee01e994fc4323ffd0f [formerly c1cba52c586a8981b86e0879d8d92be50ae8613a]
    Former-commit-id: 37c89f3c4af42d355397b48634455e4ae9e4a364
    Former-commit-id: 711d48ff88592ae19c14dab2ca4c00bffb9accc7

--- Utilities/smokediff/intel_osx_64/make_diff.sh
+++ Utilities/smokediff/intel_osx_64/make_diff.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 rm -f *.o
+KWDIR=../../keyword
+SDIR=../../../SMV/source
+
+$KWDIR/expand_file.sh $KWDIR $SDIR/smokediff $SDIR/shared/string_util.c
 make -j4 -f ../Makefile intel_osx_64
+$KWDIR/contract_file.sh $KWDIR $SDIR/shared/string_util.c
