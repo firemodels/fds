a6339a4367ce6ee9ad663d15b72bf0e384adfd22
gforney <gforney@gmail.com>
2015-05-22 03:46:35 +0000

    getdate: preliminary windows routine to output date/time to stdout

---
+++ SMV/source/getdate/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//dummy change to force revision update
+
+/* ------------------ main ------------------------ */
+
+int main(int argc, char **argv){
+  struct tm *tm;
+  time_t t;
+  char str_time[100];
+ char str_date[100];
+
+  t = time(NULL);
+  tm = localtime(&t);
+
+  strftime(str_time, sizeof(str_time), "%H:%M:%S", tm);
+  strftime(str_date, sizeof(str_date), "%b %d, %Y", tm);
+  printf("%s %s\n",str_date,str_time);
+}
---
+++ SMV/source/getdate/options.h
@@ -0,0 +1,118 @@
+#ifndef OPTIONS_H_DEFINED
+#define OPTIONS_H_DEFINED
+
+#include "lint.h"
+#ifdef CPP
+#define CCC "C"
+#define EXTERNCPP extern "C"
+#else
+#define CCC
+#define EXTERNCPP
+#endif
+
+
+ /* ------------------ options ------------------------ */
+#ifdef WIN32
+#ifdef _DEBUG
+#pragma float_control( precise, on)
+#pragma float_control( except, on )
+#endif
+#pragma warning (disable:193)		
+#pragma warning (disable:695)		
+#pragma warning (disable:2557)		
+#pragma warning (disable:4996)		
+#pragma warning (disable:4701)		
+#pragma warning (disable:4310)		
+#pragma warning (disable:4127)		
+#pragma warning (disable:4244)		
+#pragma warning (disable:1478)
+#pragma warning (disable:1786)
+
+#pragma warning (disable:810)		/* conversion from xx to yy may lose sig bits */
+#pragma warning (disable:869)		/* parameer was never referenced */
+#pragma warning (disable:4018)		/* signied/unsigned match */
+#pragma warning (disable:4206)		/* translation unit empty */
+#pragma warning (disable:4267)		/* size_t to int possible loss of data */
+#pragma warning (disable:4389)		/* signed/unsigned mis-match */
+#pragma warning (disable:1418)		/* external function definition with no prior declaration */
+#pragma warning (disable:1599)		/* declaration hides variable */
+#pragma warning (disable:981)		/* operands are evaluated in unspecified order */
+#pragma warning (disable:1419)		/* external declaration in primary source file */
+#pragma warning (disable:1572)		/* floating-point equality and inequality comparisons are unreliable */
+#pragma warning (disable:494)		/* omission of "class"is nonstandard */
+#pragma warning (disable:444)		/* destructor for base class */
+#pragma warning (disable:2259)		/* non-pointer conversion from "double" to "float" ma lose significant bits */
+
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#ifdef pp_LINUX64
+#define pp_LINUX
+#endif
+
+#ifdef pp_OSX64
+#define pp_OSX
+#endif
+
+//#define pp_KDTEST
+#define pp_PART
+#ifndef pp_OSX
+#define pp_THREAD
+#endif
+
+#undef pp_release
+#define pp_release
+
+#ifdef _DEBUG
+#define pp_MEMDEBUG
+#endif
+
+#ifdef pp_BETA
+#undef pp_release
+#define PROGVERSION "test"
+#define pp_PART2
+#endif
+
+#ifdef WIN32
+#define pp_noappend
+#define pp_cvf
+#endif
+
+#ifdef pp_release
+#define PROGVERSION "1.0.0"
+#endif
+
+#ifdef X64
+#undef BIT64
+#define BIT64
+#endif
+
+#ifdef pp_LINUX64
+#undef BIT64
+#define BIT64
+#endif
+
+#ifdef BIT64
+#define FILE_SIZE unsigned long long
+#else
+#define FILE_SIZE unsigned int
+#endif
+
+#define LINT long int
+#ifdef X64
+#undef LINT
+#ifdef WIN32
+#define LINT __int64
+#else
+#define LINT long long int
+#endif
+#endif
+
+#ifdef X64
+#define STRUCTSTAT struct __stat64
+#define STAT _stat64
+#else
+#define STRUCTSTAT struct stat
+#define STAT stat
+#endif
+#endif
