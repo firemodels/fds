e27229fb1bf89c0c946e8a18c9143656d2eba4d1
gforney <gforney@gmail.com>
2015-05-22 15:01:47 +0000

    FDS build: update FDS build scripts to use keyword expansion scripts

--- FDS_Compilation/intel_linux_64_db/make_fds.sh
+++ FDS_Compilation/intel_linux_64_db/make_fds.sh
@@ -2,8 +2,12 @@
 platform=intel64
 dir=`pwd`
 target=${dir##*/}
+KWDIR=../../Utilities/keyword
+SDIR=../../FDS_Source
 
 source $IFORT_COMPILER/bin/compilervars.sh $platform
 
 echo Building $target
+source $KWDIR/expand_file.sh $KWDIR $SDIR $SDIR/main.f90
 make -j4 VPATH="../../FDS_Source" -f ../makefile $target
+source $KWDIR/contract_file.sh $KWDIR $SDIR/main.f90
--- FDS_Compilation/mpi_intel_linux_64ib_db/make_fds.sh
+++ FDS_Compilation/mpi_intel_linux_64ib_db/make_fds.sh
@@ -2,6 +2,8 @@
 platform=intel64
 dir=`pwd`
 target=${dir##*/}
+KWDIR=../../Utilities/keyword
+SDIR=../../FDS_Source
 
 source $IFORT_COMPILER/bin/compilervars.sh $platform
 source ../Scripts/set_mpidist.sh /shared/openmpi_64ib
@@ -10,4 +12,7 @@ if [ "$MPIDIST" == "" ]; then
 fi
 
 echo Building $target 
+source $KWDIR/expand_file.sh $KWDIR $SDIR $SDIR/main.f90
 make -j4 VPATH="../../FDS_Source" -f ../makefile $target
+source $KWDIR/contract_file.sh $KWDIR $SDIR/main.f90
+
