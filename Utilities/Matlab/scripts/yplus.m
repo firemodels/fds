425c8890a216811ddcd65b0bb73831f8a5a39def
rmcdermo <randy.mcdermott@gmail.com>
2015-06-23 21:12:16 +0000

    Matlab: addverstr to yplus.m
    Former-commit-id: 3d713b0f65c4b9147cda22982927c3b47a9fdea1 [formerly 6adfbbef5f8fcf55fb300ca296cc6cd97450b0f7]
    Former-commit-id: 64249dc3278f95f8925383314b4c9d0ad856c691
    Former-commit-id: 0bef97a0733424285677c53e484e6c942fccf947

--- Utilities/Matlab/scripts/yplus.m
+++ Utilities/Matlab/scripts/yplus.m
@@ -56,18 +56,10 @@ set(leg,'Location','SouthEast')
 set(leg,'Interpreter',Font_Interpreter)
 set(leg,'Fontname',Font_Name)
 
-% add SVN if file is available
+% add git version if file is available
 
-SVN_Filename = [dir,'yplus_8_svn.txt'];
-if exist(SVN_Filename,'file')
-    SVN = importdata(SVN_Filename);
-    x_lim = get(gca,'XLim');
-    y_lim = get(gca,'YLim');
-    X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
-    Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
-    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
-        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
-end
+git_file = [dir,'yplus_8_git.txt'];
+addverstr(gca,git_file,'linear')
 
 % print pdf
 set(gcf,'Visible',Figure_Visibility);
