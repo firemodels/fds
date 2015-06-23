79d6d2544b8e1ebbee03f54a2365ab1fae756ae9
rmcdermo <randy.mcdermott@gmail.com>
2015-06-23 16:28:38 +0000

    Matlab: addverstr to flame_height2.m
    Former-commit-id: e226da0a524237fb5c1195f78bed9366b9ddbdab [formerly 35059431c3da6add373c92fc3e76049f3de112a7]
    Former-commit-id: 36744bdb9ab9c2157aade6ea68849ae9ee09968b
    Former-commit-id: 106cc318ac7ea9e57d4e1a151f6290f475dc899d

--- Utilities/Matlab/scripts/flame_height2.m
+++ Utilities/Matlab/scripts/flame_height2.m
@@ -158,17 +158,18 @@ set(plot_handle,'Position',plot_position)
 
 % add SVN if file is available
 
-svn_file = '../../Validation/Heskestad_Flame_Height/FDS_Output_Files/Qs=10000_RI=05_svn.txt';
+svn_file = '../../Validation/Heskestad_Flame_Height/FDS_Output_Files/Qs=10000_RI=05_git.txt';
+addverstr(gca,svn_file,'loglog')
 
-if exist(svn_file,'file')
-    SVN = importdata(svn_file);
-    x_lim = get(gca,'XLim');
-    y_lim = get(gca,'YLim');
-    X_SVN_Position = 10^( log10(x_lim(1))+ SVN_Scale_X*( log10(x_lim(2)) - log10(x_lim(1)) ) );
-    Y_SVN_Position = 10^( log10(y_lim(1))+ SVN_Scale_Y*( log10(y_lim(2)) - log10(y_lim(1)) ) );
-    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
-        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
-end
+% if exist(svn_file,'file')
+%     SVN = importdata(svn_file);
+%     x_lim = get(gca,'XLim');
+%     y_lim = get(gca,'YLim');
+%     X_SVN_Position = 10^( log10(x_lim(1))+ SVN_Scale_X*( log10(x_lim(2)) - log10(x_lim(1)) ) );
+%     Y_SVN_Position = 10^( log10(y_lim(1))+ SVN_Scale_Y*( log10(y_lim(2)) - log10(y_lim(1)) ) );
+%     text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+%         'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+% end
 
 % print to pdf
 
