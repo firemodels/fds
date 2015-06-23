5297f20f319a66acd8c55727cd23ee72d39b3cb1
rmcdermo <randy.mcdermott@gmail.com>
2015-06-23 20:53:06 +0000

    Matlab: addverstr to power_spectrum.m

--- Utilities/Matlab/scripts/power_spectrum.m
+++ Utilities/Matlab/scripts/power_spectrum.m
@@ -73,15 +73,16 @@ text(1e1,4e-2,'-5/3','FontSize',Label_Font_Size,'Interpreter',Font_Interpreter,'
 
 % add SVN if file is available
 
-if exist(svn_file,'file')
-    SVN = importdata(svn_file);
-    x_lim = get(gca,'XLim');
-    y_lim = get(gca,'YLim');
-    X_SVN_Position = x_lim(1)+0.10*(x_lim(2)-x_lim(1));
-    Y_SVN_Position = y_lim(1)+1.90*(y_lim(2)-y_lim(1));
-    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
-        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
-end
+addverstr(gca,svn_file,'linear')
+% if exist(svn_file,'file')
+%     SVN = importdata(svn_file);
+%     x_lim = get(gca,'XLim');
+%     y_lim = get(gca,'YLim');
+%     X_SVN_Position = x_lim(1)+0.10*(x_lim(2)-x_lim(1));
+%     Y_SVN_Position = y_lim(1)+1.90*(y_lim(2)-y_lim(1));
+%     text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+%         'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+% end
 
 % print to pdf
 set(gcf,'PaperUnits',Paper_Units);
