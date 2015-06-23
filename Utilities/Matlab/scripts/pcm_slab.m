18c71e09a6bd514e6c51e1948938f4797c673222
rmcdermo <randy.mcdermott@gmail.com>
2015-06-23 20:46:37 +0000

    Matlab: addverstr to pcm_slab.m
    Former-commit-id: d9830358a55e73a34e0ad8f3c0e613e5d03c8453 [formerly 2ead8a543b4c631c7330c2d6aa2507360829a94f]
    Former-commit-id: 7c921181d8f23e0f59cd5fdd349f0bb3918127a1
    Former-commit-id: e276b3cdf33a24f47a8e64915163af6ca64e07bc

--- Utilities/Matlab/scripts/pcm_slab.m
+++ Utilities/Matlab/scripts/pcm_slab.m
@@ -112,16 +112,17 @@ for i_plot=1:2
     
     % add SVN if file is available
     
-    SVN_Filename = ['pcm_slab_svn.txt'];
-    if exist(SVN_Filename,'file')
-        SVN = importdata(SVN_Filename);
-        x_lim = get(gca,'XLim');
-        y_lim = get(gca,'YLim');
-        X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
-        Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
-        text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
-            'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
-    end
+    SVN_Filename = ['pcm_slab_git.txt'];
+    addverstr(gca,SVN_Filename,'linear')
+    % if exist(SVN_Filename,'file')
+    %     SVN = importdata(SVN_Filename);
+    %     x_lim = get(gca,'XLim');
+    %     y_lim = get(gca,'YLim');
+    %     X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
+    %     Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
+    %     text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+    %         'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+    % end
     
     % Create the PDF files
     
