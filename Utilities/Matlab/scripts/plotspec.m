efd85d915cad790936844f9ace9a7b9afd88a31a
rmcdermo <randy.mcdermott@gmail.com>
2015-06-23 20:48:59 +0000

    Matlab: addverstr to plotspec.m
    Former-commit-id: d9c9b4116e18dbccd42e5deeae1c93afb9e60731 [formerly bdac61cbc4d19e30e0f633f1628959b1eef68069]
    Former-commit-id: b8e41545f3521e0295ffe2b298704cb92b73170c
    Former-commit-id: 281ab7d1191a8844b61793042c213f9e11fb5c80

--- Utilities/Matlab/scripts/plotspec.m
+++ Utilities/Matlab/scripts/plotspec.m
@@ -14,7 +14,7 @@
 function [] = plotspec(chid,N)
 
 close all
-addpath('../../Verification/Turbulence')
+%addpath('../../Verification/Turbulence/cbc')
 
 % set FDS standard plot format
 plot_style
@@ -52,6 +52,32 @@ H(1) = plotspec_uvw(uvw_file1,'k.-'); hold on
 H(2) = plotspec_uvw(uvw_file2,'r.-');
 H(3) = plotspec_uvw(uvw_file3,'b.-');
 
+% spec_file1 = [chid,'_spec_001.csv'];
+% spec_file2 = [chid,'_spec_002.csv'];
+% spec_file3 = [chid,'_spec_003.csv'];
+
+% skip_case = 0;
+% if ~exist(spec_file1)
+%     display(['Error: File ' spec_file1 ' does not exist. Skipping case.'])
+%     skip_case = 1;
+% end
+% if ~exist(spec_file2)
+%     display(['Error: File ' spec_file2 ' does not exist. Skipping case.'])
+%     skip_case = 1;
+% end
+% if ~exist(spec_file3)
+%     display(['Error: File ' spec_file3 ' does not exist. Skipping case.'])
+%     skip_case = 1;
+% end
+% if skip_case
+%     return
+% end
+
+% % Plot the FDS data
+% H(1) = plotspec_fft(spec_file1,'k.-'); hold on
+% H(2) = plotspec_fft(spec_file2,'r.-');
+% H(3) = plotspec_fft(spec_file3,'b.-');
+
 % Gather the Comte-Bellot/Corrsin data
 CBC = load('cbcdata.txt');
 k  = CBC(:,1)*1e2;
@@ -67,7 +93,7 @@ loglog(k,E3,'k-');
 % format axes
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Key_Font_Size)
-axis([10 1000 1/1e6 1000/1e6])
+axis([1e1 2e3 1e-7 1e-3])
 ylabel('{\it E(k)} (m^3/s^2)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 xlabel('{\it k} (1/m)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter)
 
@@ -101,21 +127,22 @@ set(anno_obj_handle,'FontName',Font_Name)
 
 % add SVN if file is available
 
-SVN_Filename = [chid,'_svn.txt'];
-if exist(SVN_Filename,'file')
-    SVN = importdata(SVN_Filename);
-    x_lim = get(gca,'XLim');
-    y_lim = get(gca,'YLim');
-    X_SVN_Position = 10^( log10(x_lim(1))+ SVN_Scale_X*( log10(x_lim(2)) - log10(x_lim(1)) ) );
-    Y_SVN_Position = 10^( log10(y_lim(1))+ SVN_Scale_Y*( log10(y_lim(2)) - log10(y_lim(1)) ) );
-    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
-        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
-end
+SVN_Filename = [chid,'_git.txt'];
+addverstr(gca,SVN_Filename,'loglog')
+% if exist(SVN_Filename,'file')
+%     SVN = importdata(SVN_Filename);
+%     x_lim = get(gca,'XLim');
+%     y_lim = get(gca,'YLim');
+%     X_SVN_Position = 10^( log10(x_lim(1))+ SVN_Scale_X*( log10(x_lim(2)) - log10(x_lim(1)) ) );
+%     Y_SVN_Position = 10^( log10(y_lim(1))+ SVN_Scale_Y*( log10(y_lim(2)) - log10(y_lim(1)) ) );
+%     text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+%         'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+% end
 
 % print to pdf
 set(gcf,'Visible',Figure_Visibility);
 set(gcf,'PaperUnits',Paper_Units);
 set(gcf,'PaperSize',[Paper_Width Paper_Height]);
 set(gcf,'PaperPosition',[0 0 Paper_Width Paper_Height]);
-print(gcf,'-dpdf',['../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/',chid,'_spectra'])
+print(gcf,'-dpdf',[chid,'_spectra'])
     
