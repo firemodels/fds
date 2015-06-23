3690e0d60e440a0779129f0e6ef07617ababb9c5
rmcdermo <randy.mcdermott@gmail.com>
2015-06-23 16:27:17 +0000

    Matlab: addverstr to fds_moody_chart.m
    Former-commit-id: 1decead5efa5ab00d6c37c9778b2790ff14829d2 [formerly 0ac18af9bb701d8c39abac6f70b843a7012f9938]
    Former-commit-id: 87c651eced9bb2996ddf03327ef9e24cb9ab4382
    Former-commit-id: 3c345f26bd92303a843a4e4cb58a7d2ed6abd3fb

--- Utilities/Matlab/scripts/fds_moody_chart.m
+++ Utilities/Matlab/scripts/fds_moody_chart.m
@@ -122,16 +122,17 @@ set(h,'Interpreter',Font_Interpreter)
 
 % add SVN if file is available
 
-SVN_Filename = [repository,'moody_dpdx=-0p01_N8_svn.txt'];
-if exist(SVN_Filename,'file')
-    SVN = importdata(SVN_Filename);
-    x_lim = get(gca,'XLim');
-    y_lim = get(gca,'YLim');
-    X_SVN_Position = 10^( log10(x_lim(1))+ SVN_Scale_X*( log10(x_lim(2)) - log10(x_lim(1)) ) );
-    Y_SVN_Position = 10^( log10(y_lim(1))+ SVN_Scale_Y*( log10(y_lim(2)) - log10(y_lim(1)) ) );
-    text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
-        'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
-end
+SVN_Filename = [repository,'moody_dpdx=-0p01_N8_git.txt'];
+addverstr(gca,SVN_Filename,'loglog')
+% if exist(SVN_Filename,'file')
+%     SVN = importdata(SVN_Filename);
+%     x_lim = get(gca,'XLim');
+%     y_lim = get(gca,'YLim');
+%     X_SVN_Position = 10^( log10(x_lim(1))+ SVN_Scale_X*( log10(x_lim(2)) - log10(x_lim(1)) ) );
+%     Y_SVN_Position = 10^( log10(y_lim(1))+ SVN_Scale_Y*( log10(y_lim(2)) - log10(y_lim(1)) ) );
+%     text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
+%         'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
+% end
 
 % print to pdf
 set(gcf,'Visible',Figure_Visibility);
