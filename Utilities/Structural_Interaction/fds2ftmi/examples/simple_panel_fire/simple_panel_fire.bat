99b4dd6a5191543b2da713d15b9453bdecab3961
RIO4FDS <arquitetojulio@gmail.com>
2015-06-26 13:44:45 +0000

    adding time average output option in fds2ftmi
    Former-commit-id: 5dc15651c0763808677ff026a3ec39a780d2c265 [formerly 529a4b1f44e02663a9e7f9e27feec309ca8a260e]
    Former-commit-id: 1918d2faa591e4dde5293efaeebe1da5ef8ac3b5
    Former-commit-id: 07fb43af0830abcdb386367886993096607f1cc9

--- Utilities/Structural_Interaction/fds2ftmi/examples/simple_panel_fire/simple_panel_fire.bat
+++ Utilities/Structural_Interaction/fds2ftmi/examples/simple_panel_fire/simple_panel_fire.bat
@@ -1,12 +1,12 @@
-..\..\..\..\..\FDS_Compilation\intel_win_64\fds_win_64 simple_panel_hot.fds 
-ansys150 -b nolist -j simple_panel_hot_ansys -i read_geometry_hot.ans -o simple_panel_hot_ansys 
-..\..\intel_win_64\fds2ftmi_win_64 simple_panel_hot 0.05 2 0 600 2 0 1 simple_panel_hot_to_ansys 
-ansys150 -b nolist -j simple_panel_hot_ansys -i run_simple_panel_hot.ans -o simple_panel_hot_ansys  
-ansys150 -b nolist -j simple_panel_hot_ansys -i plot_hot.ans -o simple_panel_hot_ansys  
+..\..\..\..\..\FDS_Compilation\intel_win_64\fds_win_64 simple_panel_fire.fds 
+ansys150 -b nolist -j simple_panel_fire_ansys -i read_geometry_fire.ans -o simple_panel_fire_ansys 
+..\..\intel_win_64\fds2ftmi_win_64 simple_panel_fire 0.05 2 0 600 2 0 0 1 simple_panel_fire_to_ansys 
+ansys150 -b nolist -j simple_panel_fire_ansys -i run_simple_panel_fire.ans -o simple_panel_fire_ansys  
+ansys150 -b nolist -j simple_panel_fire_ansys -i plot_fire.ans -o simple_panel_fire_ansys  
 set DISPLAY=localhost:0.0 
-displayw -d POSTSCRIPT -j simple_panel_hot_ansys 
-epstopdf --outfile=simple_panel_hot_ansys.pdf pscr000.eps  
-move simple_panel_hot_ansys.pdf ..\..\scripts\SCRIPT_FIGURES\simple_panel_hot_ansys.pdf 
+displayw -d POSTSCRIPT -j simple_panel_fire_ansys 
+epstopdf --outfile=simple_panel_fire_ansys.pdf pscr000.eps  
+move simple_panel_fire_ansys.pdf ..\..\scripts\SCRIPT_FIGURES\simple_panel_fire_ansys.pdf 
 del *.loc 
 del *.dat 
 del *_ansys* 
