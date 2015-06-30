6ac8fdb6378f7be06beaaa375706dc739a665561
Glenn Forney <gforney@gmail.com>
2015-06-30 16:09:12 -0400

    FDS build: generate git revision string in make file for all windows makefile entries

--- FDS_Compilation/mpi_intel_win_64/make_fds.bat
+++ FDS_Compilation/mpi_intel_win_64/make_fds.bat
@@ -4,12 +4,8 @@ set fds_build_debug=0
 
 :: setup compiler environment
 call ..\..\Utilities\Scripts\setup_intel_compilers.bat
-set KWDIR=..\..\Utilities\keyword
-set SDIR=..\..\FDS_Source
 
 Title Building FDS (mpi) for 64 bit Windows
 
-call %KWDIR%\expand_file %SDIR% %SDIR%\main.f90
 make VPATH="../../FDS_Source" -f ..\makefile mpi_intel_win_64
-call %KWDIR%\contract_file %SDIR%\main.f90
 pause
