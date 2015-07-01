3f1db1bdc64c66074f3c018e1fe311b8c38205d9
shostikk <simo.hostikka@aalto.fi>
2015-07-01 10:13:36 +0300

    shostikk: Add validation files for Tokyo University of Science facade tests.

---
+++ Validation/TUS_Facade/Run_All.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script runs a set of Validation Cases on a Linux machine with a batch queuing system.
+# See the file Validation/Common_Run_All.sh for more information.
+export SVNROOT=`pwd`/../..
+source $SVNROOT/Validation/Common_Run_All.sh
+
+$QFDS $DEBUG $QUEUE -p 2 -n 3 -d $INDIR TUS_facade_I-1-2.fds
+$QFDS $DEBUG $QUEUE -p 2 -n 3 -d $INDIR TUS_facade_1-1-2-3.fds
+$QFDS $DEBUG $QUEUE -p 2 -n 3 -d $INDIR TUS_facade_2-1-2-3.fds
