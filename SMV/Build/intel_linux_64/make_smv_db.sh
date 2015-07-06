210beabd717ab9419eca1acda905c2a6e549435f
Glenn Forney <gforney@gmail.com>
2015-07-06 15:56:09 -0400

    smokeview: remove unused variable, unused script call

--- SMV/Build/intel_linux_64/make_smv_db.sh
+++ SMV/Build/intel_linux_64/make_smv_db.sh
@@ -5,6 +5,4 @@ KWDIR=../../../Utilities/keyword
 SDIR=../../source
 
 make -f ../Makefile clean
-source $KWDIR/expand_file.sh $KWDIR $SDIR/smokeview $SDIR/shared/string_util.c
 make -f ../Makefile intel_linux_64_db
-source $KWDIR/contract_file.sh $KWDIR $SDIR/smokeview/main.c
