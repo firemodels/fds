163b12e93b58f9847e3ec2449624ce7454c2cd08
Glenn Forney <gforney@gmail.com>
2015-07-09 09:21:37 -0400

    vv: cleanup bot run scripts

--- Utilities/Firebot/run_smokebot.sh
+++ Utilities/Firebot/run_smokebot.sh
@@ -1,11 +1,48 @@
-#!/bin/bash  
+#!/bin/bash
 
-MAKEMOVIES=
-while getopts 'm' OPTION
-do case $OPTION in
-  m)
-   MAKEMOVIES="-m"
-  ;;
+running=bot_running
+if [ -e bot_running ] ; then
+  echo Smokebot is already running.
+  echo Erase the file $running if this is not the case.
+  exit
+fi
+
+CURDIR=`pwd`
+FDS_GITbase=FDS-SMVgitclean
+BRANCH=development
+botscript=smokebot_linux.sh
+cFDS_GITbase=
+cBRANCH=
+UPDATEREPO=
+while getopts 'b:d:u' OPTION
+do
+case $OPTION  in
+  b)
+   BRANCH="$OPTARG"
+   ;;
+  d)
+   FDS_GITbase="$OPTARG"
+   ;;
+  u)
+   UPDATEREPO=1
+   ;;
 esac
 done
-bash -lc "./smokebot_linux_wrapper.sh -q smokebot $MAKEMOVIES" &
+shift $(($OPTIND-1))
+
+if [[ "$FDS_GITbase" != "" ]]; then
+   cFDS_GITbase="-d $FDS_GITbase"
+fi 
+if [[ "$BRANCH" != "" ]]; then
+   cFDS_GITbase="-b $BRANCH"
+fi 
+if [[ "$UPDATEREPO" == "1" ]]; then
+   cd ~/$FDS_GITbase
+   git checkout $BRANCH
+   git pull
+   cp Utilities/Firebot/$botscript $CURDIR/.
+   cd $CURDIR
+fi
+touch $running
+./$botscript $cBRANCH $cFDS_GITbase "$@"
+rm $running
