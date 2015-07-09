7f26f98299bcae8fbe70c733cccf368e22b70a07
gforney <gforney@gmail.com>
2015-07-08 21:50:35 -0400

    vv: correct email batch file
    Former-commit-id: 123b285050d81a1b3bccabeacac000acb7032dee [formerly 7104bde835f214c48aefe37aeb0a1e315ebe5e19]
    Former-commit-id: 49aa089a52cbd4126be0234815a7d159493d5578
    Former-commit-id: c0fc82c3040c8f2714c15df2a9dd7524c3bd5990

--- Utilities/Firebot/firebot_email_list.bat
+++ Utilities/Firebot/firebot_email_list.bat
@@ -4,8 +4,8 @@
 set mailToSMV="gforney@gmail.com"
 
 :: Mailing lists for Firebot status report
-set mailToFDS="gforney@gmail.com"
+set mailToFDS="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, drjfloyd@gmail.com"
 
-set mailToFDSAll="gforney@gmail.com"
+set mailToFDSAll="mcgratta@gmail.com, randy.mcdermott@gmail.com, gforney@gmail.com, CraigWeinschenk@gmail.com, drjfloyd@gmail.com"
 
 set mailToFDSDebug="gforney@gmail.com"
