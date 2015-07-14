9377af8092e019ed14d031faf860246dd833de70
gforney <gforney@gmail.com>
2015-07-14 19:32:48 -0400

    smokeview source: add error message

--- SMV/source/smokeview/IOslice.c
+++ SMV/source/smokeview/IOslice.c
@@ -1092,7 +1092,8 @@ void readslice(char *file, int ifile, int flag, int *errorcode){
       return;
     }
     if(settmax_s==0&&settmin_s==0&&sd->compression_type==0){
-      if(framesize == 0){
+      if(framesize <= 0){
+        fprintf(stderr,"*** Error: frame size is 0 in slice file %s . \n",file);
         error = 1;
       }
       else{
