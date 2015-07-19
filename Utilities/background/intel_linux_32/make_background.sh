477b4645b210a69901407e7df5c096842e2a81a3
gforney <gforney@gmail.com>
2015-07-19 17:22:30 -0400

    smv/util build: use preprocessing directives for setting revision strings
    Former-commit-id: 910a0bf3f3accba69e2048726d37a382e55b3222 [formerly fffd9d9696a0194ac0b7ae4ea8250fee9d35fdbf]
    Former-commit-id: 5097e04e8a939dc0b3c57ffae22cfef7df72f53e
    Former-commit-id: fb90bdb4c087df7599fe3f4dc92d9fce8056e1e6

--- Utilities/background/intel_linux_32/make_background.sh
+++ Utilities/background/intel_linux_32/make_background.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 source $IFORT_COMPILER/bin/compilervars.sh ia32
-KWDIR=../../keyword
-SDIR=../../../SMV/source
 
 rm -f *.o
-$KWDIR/expand_file.sh $KWDIR $SDIR/background $SDIR/shared/string_util.c
 make -j4 -f ../Makefile intel_linux_32
-$KWDIR/contract_file.sh $KWDIR $SDIR/shared/string_util.c
--- Utilities/getdate/intel_win_64/make_getdate.bat
+++ Utilities/getdate/intel_win_64/make_getdate.bat
@@ -1,14 +1,10 @@
 @echo off
 :: setup compiler environment
 call ..\..\Scripts\setup_intel_compilers.bat
-set KWDIR=..\..\keyword
-set SDIR=..\..\..\SMV\source
 
 Title Building getdate for 64 bit Windows
 
 erase *.obj
-call %KWDIR%\expand_file %SDIR%\getdate %SDIR%\getdate\main.c
 make -f ..\Makefile intel_win_64
-call %KWDIR%\contract_file %SDIR%\getdate\main.c
 pause
 
