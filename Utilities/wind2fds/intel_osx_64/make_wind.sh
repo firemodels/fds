1c92f7463546f6b45d7d9bf24471adfc7f02f0c2
gforney <gforney@gmail.com>
2015-07-19 17:22:30 -0400

    smv/util build: use preprocessing directives for setting revision strings

--- Utilities/wind2fds/intel_osx_64/make_wind.sh
+++ Utilities/wind2fds/intel_osx_64/make_wind.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-KWDIR=../../keyword
-SDIR=../../../SMV/source
 
 rm -f *.o
-$KWDIR/expand_file.sh $KWDIR $SDIR/wind2dfs $SDIR/shared/string_util.c
 make -f ../Makefile intel_osx_64
-$KWDIR/contract_file.sh $KWDIR $SDIR/shared/string_util.c
