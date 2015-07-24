f593bfb0b107f8a2a277c1453eaaa5c38589de9d
gforney <gforney@gmail.com>
2015-07-24 13:20:09 -0400

    SMV manuals: add git keyword substition when building smokeview guides on windows
    Former-commit-id: 6a586a8b24cd2cdf57b7b3bc3ab1827fa993a87e [formerly aea21addc678d933217d9640b81c3c998448ece4]
    Former-commit-id: e3a2bad526a40dcdb7a9597ea1ec49afb9c45aea
    Former-commit-id: 5efca2324dc8fdfeee63df0324aecf5dd0db02f8

--- Manuals/SMV_Verification_Guide/make_guide.bat
+++ Manuals/SMV_Verification_Guide/make_guide.bat
@@ -1,6 +1,10 @@
 @echo off
 set paper=SMV_Verification_Guide
 
+git describe --long --dirty > gitinfo.txt
+set /p gitrevision=<gitinfo.txt
+echo \newcommand^{\gitrevision^}^{%gitrevision%^} > ..\Bibliography\gitrevision.tex
+
 echo pass 1
 pdflatex -interaction nonstopmode %paper% > %paper%.err
 bibtex %paper% > %paper%.err
@@ -13,6 +17,7 @@ find "! LaTeX Error:" %paper%.err
 find "Fatal error" %paper%.err
 find "Error:" %paper%.err
 
+if exist ..\Bibliography\gitrevision.tex erase ..\Bibliography\gitrevision.tex
 echo %paper% build complete
 
 
