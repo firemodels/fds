350436aeb6f6ef6550a771cea6d826c5de466069
rmcdermo <randy.mcdermott@gmail.com>
2015-07-29 12:57:15 -0400

    Matlab: uncomment my verfication script
    Former-commit-id: c57ab273b6267f1093530c19e1ee0ad2b466e1b4 [formerly 198fdfe0d10648c88aa97e90738c2bffb1eb876b]
    Former-commit-id: b6d102ffb5809deb748ecd54ea9262cf9ee1974a
    Former-commit-id: 407f1f046cc6587eb734d585bc3b6217d80f1416

--- Utilities/Matlab/FDS_verification_script.m
+++ Utilities/Matlab/FDS_verification_script.m
@@ -22,75 +22,74 @@ clear all
 
 addpath 'scripts'
 
-% % Scripts to run prior to dataplot
+% Scripts to run prior to dataplot
 
-% radiation_box
-% radiation_plane_layer
-% ns2d
-% vort2d
-% wall_internal_radiation
-% ashrae_7
-% flame_species
-% EDC_species
-% cat_propane_depo
-% burke_schumann
-% convective_cooling_convergence
-% random_walk_soln
+radiation_box
+radiation_plane_layer
+ns2d
+vort2d
+wall_internal_radiation
+ashrae_7
+flame_species
+EDC_species
+cat_propane_depo
+burke_schumann
+convective_cooling_convergence
+random_walk_soln
 
-% % Dataplot and scatplot options
+% Dataplot and scatplot options
 
-% Dataplot_Inputs_File = [pwd, '/FDS_verification_dataplot_inputs.csv'];
-% Working_Dir = [pwd, '/../../Verification/'];
-% Manuals_Dir = [pwd, '/../../Manuals/'];
-% Scatterplot_Inputs_File = [pwd, '/FDS_verification_scatterplot_inputs.csv'];
+Dataplot_Inputs_File = [pwd, '/FDS_verification_dataplot_inputs.csv'];
+Working_Dir = [pwd, '/../../Verification/'];
+Manuals_Dir = [pwd, '/../../Manuals/'];
+Scatterplot_Inputs_File = [pwd, '/FDS_verification_scatterplot_inputs.csv'];
 
-% % Statistics output options
+% Statistics output options
 
-% Stats_Output = 'Verification';
-% Output_File = [pwd, '/FDS_verification_scatterplot_output.csv'];
-% Statistics_Tex_Output = [pwd, '/../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/Scatterplots/verification_statistics.tex'];
+Stats_Output = 'Verification';
+Output_File = [pwd, '/FDS_verification_scatterplot_output.csv'];
+Statistics_Tex_Output = [pwd, '/../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/Scatterplots/verification_statistics.tex'];
 
-% % Override the plot style options with NRC 1824 plot options
+% Override the plot style options with NRC 1824 plot options
 
-% NRC_Options = false;
-% Append_To_Scatterplot_Title = '';
+NRC_Options = false;
+Append_To_Scatterplot_Title = '';
 
-% % Run dataplot and scatplot scripts
+% Run dataplot and scatplot scripts
 
-% [saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir);
-% scatplot(saved_data, drange, ...
-%          'Manuals_Dir', Manuals_Dir, ...
-%          'Scatterplot_Inputs_File', Scatterplot_Inputs_File, ...
-%          'Stats_Output', Stats_Output, ...
-%          'Output_File', Output_File, ...
-%          'Statistics_Tex_Output', Statistics_Tex_Output, ...
-%          'NRC_Options', NRC_Options, ...
-%          'Append_To_Scatterplot_Title', Append_To_Scatterplot_Title)
+[saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir);
+scatplot(saved_data, drange, ...
+         'Manuals_Dir', Manuals_Dir, ...
+         'Scatterplot_Inputs_File', Scatterplot_Inputs_File, ...
+         'Stats_Output', Stats_Output, ...
+         'Output_File', Output_File, ...
+         'Statistics_Tex_Output', Statistics_Tex_Output, ...
+         'NRC_Options', NRC_Options, ...
+         'Append_To_Scatterplot_Title', Append_To_Scatterplot_Title)
 
-% % Special cases
+% Special cases
  
-% turb_model
-% jet_decay
-% wall_model
-% pyrolysis
-% birch_tga
-% water_ice_water
-% pcm_slab
-% pulsating
-% compression_wave
-% plate_view_factor
-% terminal_velocity_convergence
-% flat_fire_comparison
-% fluid_part
-% extinction
-% fan_curve
-% mesh_transformation
-% synthetic_eddy_method
+turb_model
+jet_decay
+wall_model
+pyrolysis
+birch_tga
+water_ice_water
+pcm_slab
+pulsating
+compression_wave
+plate_view_factor
+terminal_velocity_convergence
+flat_fire_comparison
+fluid_part
+extinction
+fan_curve
+mesh_transformation
+synthetic_eddy_method
 shunn_mms_error
-% openmp_timing_benchmarks
-% rms_cov_corr
-% hot_layer_collapse
-% radiating_polygon
-% saad_mms_error
+openmp_timing_benchmarks
+rms_cov_corr
+hot_layer_collapse
+radiating_polygon
 
 display('verification scripts completed successfully!')
