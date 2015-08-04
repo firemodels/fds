090dad858462992b1bfd9a8f41a498d9a2b9990c
drjfloyd <drjfloyd@gmail.com>
2015-08-04 17:34:46 -0400

    FDS Source:  Ensure C_DROP2 is defined
    Former-commit-id: a92fff4ac3ec3519070d3ae88f19b3785dbd8f04 [formerly db95478524f799c355c8366bfcc4c3a58e145cbe]
    Former-commit-id: 258553a09d8e66f48a3fa471848085c4388d4470
    Former-commit-id: 34f2ae88a61087f17cc5308e04686a8411f6cfac

--- FDS_Source/part.f90
+++ FDS_Source/part.f90
@@ -2241,6 +2241,7 @@ SPECIES_LOOP: DO Z_INDEX = 1,N_TRACKED_SPECIES
                TMP_DROP_NEW = TMP_DROP + (Q_TOT - M_VAP * H_V)/(C_DROP * (M_DROP - M_VAP))
                ITMP = NINT(TMP_DROP)
                ITMP2 = MIN(I_BOIL,MAX(I_MELT,NINT(TMP_DROP_NEW)))
+               C_DROP2 = SS%C_P_L(ITMP2)
                IF (ITMP/=ITMP2) THEN
                   C_DROP2 = SUM(SS%C_P_L(MIN(ITMP,ITMP2):MAX(ITMP,ITMP2)))/REAL(ABS(ITMP2-ITMP)+1,EB)
                   TMP_DROP_NEW = TMP_DROP + (Q_TOT - M_VAP * H_V)/(C_DROP2 * (M_DROP - M_VAP))
