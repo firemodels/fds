201b6211c362f370d9b87d502cec4c49b1f0045c
rmcdermo <randy.mcdermott@gmail.com>
2015-08-05 17:47:57 -0400

    FDS Verification Guide: add table of Shunn temporal error norms
    Former-commit-id: a01566b322b321328fe28ef8f0e51f52fa4663f3 [formerly a783df36477c6ab791b73fa50f53ca5da93fcb69]
    Former-commit-id: 74ad86fa0113e5464a2d7bc7c3db7adde39bcbe7
    Former-commit-id: 649d601fb6ce2dc8e2023cb3ec4def68a3889a2f

---
+++ Manuals/Copy_Firebot_Figures.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export FIREBOTROOT=/home2/smokevis2/firebot/FDS-SMVgitclean/
+export FIREBOTMANS=$FIREBOTROOT/Manuals/
+export FBUG=$FIREBOTMANS/FDS_User_Guide/
+export FBVG=$FIREBOTMANS/FDS_Verification_Guide/
+export FBVAL=$FIREBOTMANS/FDS_Validation_Guide/
+export BASEDIR=`pwd`
+
+# Copy User's Guide Figures
+cp $FBUG/SCRIPT_FIGURES/* $BASEDIR/FDS_User_Guide/SCRIPT_FIGURES/
+echo Users Guide Figures Copied
+
+# Copy Verification Guide Figures
+cp $FBVG/SCRIPT_FIGURES/* $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/
+echo Verification Figures Copied
+
+# Copy Validation Guide Figures
+#cp -R $FBVAL/SCRIPT_FIGURES/* $BASEDIR/FDS_Validation_Guide/SCRIPT_FIGURES/ &> /dev/null
+rsync -r --exclude=*.svn $FBVAL/SCRIPT_FIGURES/* $BASEDIR/FDS_Validation_Guide/SCRIPT_FIGURES/
+echo Validation Guide Figures Copied
