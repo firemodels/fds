55c7673cb6d46f7c4838595fa5b6d535f40cfb87
gforney <gforney@gmail.com>
2015-08-06 16:48:25 -0400

    firbot: add manuals to private status page

--- Utilities/Firebot/status_privtop.sh
+++ Utilities/Firebot/status_privtop.sh
@@ -8,8 +8,15 @@ cat << EOF
 <body>
 <h2>FDS Automatic Verification Summary Page</h2>
 
-<hr align='left'>
+<h3>FDS Manuals</h3>
+EOF
 
+curdir=`pwd`
+cd /var/www/html/firebot/manuals
+ls -l *.pdf | awk '{printf("<li><a href=\"http://blaze.nist.gov/firebot/manuals/%s\">%s</a> <em>%s %s %s</em>\n",$9,$9,$6,$7,$8)}'
+cd $curdir
+
+cat << EOF
 <h3>Firebot Status</h3>
 
 This page displays the status for up to 30 of the most recent build/test cycles.<br>
