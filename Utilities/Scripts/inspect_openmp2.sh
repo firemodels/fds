28a4f60dcea5803373295da6b51bc4927d215ffa
gforney <gforney@gmail.com>
2015-08-11 16:41:29 -0400

    FDS vv: add scripts to do thread checking outside of firebot
    Former-commit-id: 172c6801c7ba0a3ea43c7782457e56160571e6b1 [formerly c321ebbb110ac3e022dbe84a4742314ab321d50b]
    Former-commit-id: 123baf3e7608911f7841f0674bad19ebe274ebb5
    Former-commit-id: ef92c9afe2c0dd4807e067c0e4c440b1c4ce8d06

---
+++ Utilities/Scripts/inspect_openmp2.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+CURDIR=`pwd`
+
+GITROOT=~/FDS-SMVgitclean
+if [ "$FDSSMV" != "" ] ; then
+  GITROOT=$FDSSMV
+fi
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: inspect_openmp2.sh [-r repository root] [-v] casename.dfs"
+  echo ""
+  echo " -r repository root - name and location of repository where FDS is located"
+  echo "    [default: $GITROOT]"
+  echo " -v   - list command used to thread check"
+  echo "input_file - input file"
+  echo ""
+  exit
+fi
+
+showinput=
+while getopts 'r:v' OPTION
+do
+case $OPTION  in
+  r)
+   GITROOT="$OPTARG"
+   ;;
+  v)
+   showinput=1
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+case=$1
+
+INSPECT_DIR=$GITROOT/Utilities/Scripts/inspect_openmp_ti3
+
+# Perform OpenMP thread checking (locate deadlocks and data races)
+
+source /opt/intel/inspector_xe/inspxe-vars.sh quiet
+
+TARGET=$GITROOT/FDS_Compilation/intel_linux_64_inspect
+
+if [ "$showinput" == "" ] ; then
+  if [ -d $INSPECT_DIR ] ; then
+    rm -r $INSPECT_DIR
+  fi
+  cd $TARGET
+  make -f ../makefile clean
+  ./make_fds.sh
+fi
+
+export OMP_NUM_THREADS=2
+
+if [ "$showinput" == "1" ] ; then
+  echo inspxe-cl -collect ti3 -knob scope=normal \
+          -result-dir $INSPECT_DIR \
+          -search-dir src=$GITROOT/FDS_Source \
+          -- $GITROOT/FDS_Compilation/intel_linux_64_inspect/fds_intel_linux_64_inspect $case
+  exit
+fi
+cd $CURDIR
+inspxe-cl -collect ti3 -knob scope=normal \
+          -result-dir $INSPECT_DIR \
+          -search-dir src=$GITROOT/FDS_Source \
+          -- $GITROOT/FDS_Compilation/intel_linux_64_inspect/fds_intel_linux_64_inspect $case
