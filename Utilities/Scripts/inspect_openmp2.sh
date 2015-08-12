4a6b35ed56ec7d772416effdccbeb2587a2eac78
gforney <gforney@gmail.com>
2015-08-12 09:28:56 -0400

    FDS vv: update thread checker scripts
    Former-commit-id: c9049123bd71c1b644ee226d2859a5aae06cd26a [formerly d90220546456ec4965939efdef8fe274cf64735c]
    Former-commit-id: a027747a284eac36230b895d1bbf3fc6a0dbcc25
    Former-commit-id: d110c96368f7f8dac232597435f4057d92d32528

--- Utilities/Scripts/inspect_openmp2.sh
+++ Utilities/Scripts/inspect_openmp2.sh
@@ -5,14 +5,17 @@ GITROOT=~/FDS-SMVgitclean
 if [ "$FDSSMV" != "" ] ; then
   GITROOT=$FDSSMV
 fi
+RESULT_DIR=$GITROOT/Utilities/Scripts/inspect_openmp_ti3
 
 function usage {
   echo "Usage: inspect_openmp.sh [-r repository root] [-v] casename.dfs"
   echo ""
+  echo " -d result-dir - directory containing thread checker results"
+  echo "    [default: $RESULT_DIR"
   echo " -h display this message"
-  echo " -r repository root - name and location of repository where FDS is located"
+  echo " -r repository root - FDS repository root directory"
   echo "    [default: $GITROOT]"
-  echo " -v   - list command used to thread check"
+  echo " -v   - list command that will be used to thread check"
   echo "input_file - input file"
   echo ""
   exit
@@ -24,9 +27,12 @@ then
 fi
 
 showinput=
-while getopts 'hr:v' OPTION
+while getopts 'd:hr:v' OPTION
 do
 case $OPTION  in
+  d)
+   RESULT_DIR="$OPTARG"
+   ;;
   h)
    usage;
    ;;
@@ -41,8 +47,6 @@ done
 shift $(($OPTIND-1))
 case=$1
 
-INSPECT_DIR=$GITROOT/Utilities/Scripts/inspect_openmp_ti3
-
 # Perform OpenMP thread checking (locate deadlocks and data races)
 
 source /opt/intel/inspector_xe/inspxe-vars.sh quiet
@@ -50,8 +54,8 @@ source /opt/intel/inspector_xe/inspxe-vars.sh quiet
 TARGET=$GITROOT/FDS_Compilation/intel_linux_64_inspect
 
 if [ "$showinput" == "" ] ; then
-  if [ -d $INSPECT_DIR ] ; then
-    rm -r $INSPECT_DIR
+  if [ -d $RESULT_DIR ] ; then
+    rm -r $RESULT_DIR
   fi
   cd $TARGET
   make -f ../makefile clean
@@ -62,13 +66,13 @@ export OMP_NUM_THREADS=2
 
 if [ "$showinput" == "1" ] ; then
   echo inspxe-cl -collect ti3 -knob scope=normal \
-          -result-dir $INSPECT_DIR \
+          -result-dir $RESULT_DIR \
           -search-dir src=$GITROOT/FDS_Source \
           -- $GITROOT/FDS_Compilation/intel_linux_64_inspect/fds_intel_linux_64_inspect $case
   exit
 fi
 cd $CURDIR
 inspxe-cl -collect ti3 -knob scope=normal \
-          -result-dir $INSPECT_DIR \
+          -result-dir $RESULT_DIR \
           -search-dir src=$GITROOT/FDS_Source \
           -- $GITROOT/FDS_Compilation/intel_linux_64_inspect/fds_intel_linux_64_inspect $case
