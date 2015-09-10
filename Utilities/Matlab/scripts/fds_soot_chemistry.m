492475bd6df109103052ce50b2dc4c143521c3c7
rmcdermo <randy.mcdermott@gmail.com>
2015-09-10 11:41:03 -0400

    Matlab: add soot chemistry script

--- Utilities/Matlab/scripts/fds_soot_chemistry.m
+++ Utilities/Matlab/scripts/fds_soot_chemistry.m
@@ -1,9 +1,10 @@
 % McDermott
-% 3-31-11
-% fds_simple_chemistry.m
+% 9-4-2015
+% fds_soot_chemistry.m
 %
 % This script is a distilled version of the SIMPLE_CHEMISTRY routine in FDS and may be
-% used as a check on reaction coefficients for primitive and lumped species.
+% used as a check on reaction coefficients for primitive and lumped species. Here we
+% also handle the case where soot is tracked separately.
 
 close all
 clear all
@@ -22,7 +23,7 @@ X_H = 0.1;
 % define CO and soot yields
 
 y_CO = 0;
-y_s  = .01;
+y_s  = 0.01;
 
 % define the element matrix (number of atoms [rows] for each primitive species [columns])
 
@@ -59,11 +60,15 @@ v_2 = zeros(1,n_species);
 % compute what we know so far
 
 v_2(i_carbon_monoxide) = W(i_fuel)/W(i_carbon_monoxide)*y_CO
-v_2(i_soot)            = W(i_fuel)/W(i_soot)*y_s
+
+% soot is its own tracked species
+
+v_3 = [0 0 0 0 0 0 1];
+nu_3 = W(i_fuel)/W(i_soot)*y_s;
 
 % linear system right hand side
 
-b = E*(v_1' - v_2')
+b = E*(v_1' - nu_3*v_3')
 
 % matrix
 
@@ -80,20 +85,21 @@ v_2(i_nitrogen)       = x(4);
 
 nu_1 = -1       % fuel stoich coeff
 nu_2 = sum(v_2) % prod stoich coeff
+nu_3 = nu_3     % soot stoich coeff
 
-v_2 = v_2/nu_2; % normalize volume fractions
+v_2 = v_2/nu_2  % normalized product volume fractions
 
 % check mass balance (should be 0)
 
-nu_0*sum(v_0*W) + nu_1*sum(v_1*W) + nu_2*sum(v_2*W)
+nu_0*sum(v_0*W) + nu_1*sum(v_1*W) + nu_2*sum(v_2*W) + nu_3*sum(v_3*W)
 
 % check primitive reaction coefficients
 
-NU = nu_0*v_0 + nu_1*v_1 + nu_2*v_2;
+NU = nu_0*v_0 + nu_1*v_1 + nu_2*v_2 + nu_3*v_3;
 
 % display fuel properties
 
-Z2Y = [v_0',v_1',v_2']
+Z2Y = [v_0',v_1',v_2',v_3']
 
 
 
