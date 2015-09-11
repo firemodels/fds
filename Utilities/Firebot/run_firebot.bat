9734ce4fbf2e7ba2aeb703d45653ca7cab17b87f
gforney <gforney@gmail.com>
2015-09-11 08:01:48 -0400

    firebot: make windows firebot usage consistent with smokebot and cfastbot
    Former-commit-id: 020d3acad229ce74889febbc2aa94d4370540388 [formerly f6bc1a47b8dd9e43ac7939de0e714d47513e6533]
    Former-commit-id: d9e2fbf2ccf5adb021ada398e5d91b825bf7639e
    Former-commit-id: 390712a2657170764b33618313454905e8003caf

--- Utilities/Firebot/run_firebot.bat
+++ Utilities/Firebot/run_firebot.bat
@@ -1,34 +1,77 @@
 @echo off
+:: usage: 
+::  run_firebot -fdsrepo name -altemail -email address -nomatlab -noupdate
+::  (all command arguments are optional)
 
-set curdir=%CD%
-set running=bot.running
 set altemail=0
+set update=1
 set usematlab=1
 set stopscript=0
-set fdsrepo=FDS-SMVgitclean
-set emailto=
 
+set fdsrepo=%userprofile%\FDS-SMVgitclean
+if exist .fds_git (
+  set fdsrepo=..\..
+)
+if x%FDSGIT% == x goto skip_fdsgit
+  if EXIST %FDSGIT% (
+    set fdsrepo=%FDSGIT%
+  )
+:skip_fdsgit
+
+set emailto=
+if not x%EMAILGIT% == x (
+  set emailto=%EMAILGIT%
+)
+
+:: parse command line arguments
+
+set stopscript=0
 call :getopts %*
 if %stopscript% == 1 (
   exit /b
 )
-  
-set gitrepo=%userprofile%\%fdsrepo%
+
+:: normalize directory paths
+
+call :normalise %CD% curdir
+set curdir=%temparg%
+
+call :normalise %fdsrepo%\Utilities\Firebot
+set fdsbotdir=%temparg%
+
 set running=%curdir%\bot.running
 
-if not exist %running% (
+if exist %running% goto skip_running
+
+:: get latest firebot
+
+    if %update% == 0 goto no_update
+    echo getting latest firebot
+    cd %fdsrepo%
+    git fetch origin
+    git pull 1> Nul 2>&1
+    if not %fdsbotdir% == %curdir% (
+      copy %fdsbotdir%\smokebot_win.bat %curdir%
+    )
+    cd %curdir%
+    :no_update
+
+:: run firebot
+
   echo 1 > %running%
-  call firebot_win.bat %fdsrepo% %altemail% %usematlab% %emailto%
-  cd %curdir%
+  call firebot_win.bat %fdsrepo% %update% %altemail% %usematlab% %emailto%
   erase %running%
-) else (
-  echo A bot is already running.
-  echo Erase the file %running% if this is not the case
-)
+  goto end_running
+:skip_running
+  echo ***Error: firebot is currently running.
+  echo If this is not the case, erase the file:
+  echo %running%
+:end_running
 
 goto eof
 
 :getopts
+ if (%1)==() exit /b
  set valid=0
  set arg=%1
  if /I "%1" EQU "-help" (
@@ -50,6 +93,10 @@ goto eof
    set valid=1
    set altemail=1
  )
+ if /I "%1" EQU "-noupdate" (
+   set valid=1
+   set update=0
+ )
  if /I "%1" EQU "-nomatlab" (
    set valid=1
    set usematlab=0
@@ -70,13 +117,21 @@ exit /b
 :usage  
 echo run_smokebot [options]
 echo. 
-echo -help          - display this message
-echo -fdsrepo name  - specify the FDS repo name (default: FDS-SMVgitclean) 
-echo -altemail      - use alternate email server
-echo -email address - override "to" email addresses specified in repo 
+echo -help           - display this message
+echo -fdsrepo name   - specify the FDS-SMV repository
+echo       (default: %fdsrepo%) 
+echo -altemail        - use an alternate email server
+echo -email address  - override "to" email addresses specified in repo 
+if "%emailto%" NEQ "" (
+echo       (default: %emailto%^)
+)
 echo -nomatlab      - do not use matlab
+echo -noupdate       - do not update repository
+exit /b
+
+:normalise
+set temparg=%~f1
 exit /b
 
 :eof
-
 
