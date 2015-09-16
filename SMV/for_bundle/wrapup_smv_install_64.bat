a92e755f68d1ce49f2d3370ace5ec129c9e2bd2f
gforney <gforney@gmail.com>
2015-09-16 19:46:06 -0400

    bundle scripts: update smokeview bundle scripts to use new path directory structure

--- SMV/for_bundle/wrapup_smv_install_64.bat
+++ SMV/for_bundle/wrapup_smv_install_64.bat
@@ -10,26 +10,20 @@ echo.
 echo Wrapping up 64 bit Smokeview update
 echo.
 
+SMVBINDIR=%CD%
+
 echo.
 echo Associating the .smv file extension with smokeview.exe
 
-ftype smvDoc="%CD%\smokeview.exe" "%%1" >Nul
+ftype smvDoc="%SMVBINDIR%\smokeview.exe" "%%1" >Nul
 assoc .smv=smvDoc>Nul
 
 :: adding path
 
 echo.
-echo Adding %CD% to the system path
-call "%CD%\set_path.exe" -s -m -a "%CD%"
+echo Adding %SMVBINDIR% to the system path
+call "%SMVBINDIR%\set_path.exe" -s -m -a "%SMVBINDIR%"
 
-:: add firewall rules
-
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-MPIEXEC.SMPD" program="%CD%\mpiexec.smpd.exe" profile=domain protocol=tcp
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-MPIEXEC.SMPD" program="%CD%\mpiexec.smpd.exe" profile=domain protocol=udp
-
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-SMPD" program="%CD%\smpd.exe" profile=domain protocol=tcp
-:: netsh advfirewall firewall add rule dir=in action=allow name="IMPI-SMPD" program="%CD%\smpd.exe" profile=domain protocol=udp
-
-erase "%CD%\set_path.exe"
+erase "%SMVBINDIR%\set_path.exe"
 echo Press any key to complete update
 pause>NUL
