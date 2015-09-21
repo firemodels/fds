5b870b3e07395b73f42ca8daaf23a53c41e9a4ec
= <=>
2015-09-21 17:00:31 -0400

    update firebot figures script and update user guide to include chemical subiterations discussion

--- Manuals/Copy_Firebot_Figures.sh
+++ Manuals/Copy_Firebot_Figures.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export FIREBOTROOT=/home2/smokevis2/firebot/FDS-SMVgitclean/
-export FIREBOTMANS=$FIREBOTROOT/Manuals/
-export FBUG=$FIREBOTMANS/FDS_User_Guide/
-export FBVG=$FIREBOTMANS/FDS_Verification_Guide/
-export FBVAL=$FIREBOTMANS/FDS_Validation_Guide/
+export FIREBOTROOT=/home/firebot/SMOKEVIS2/FDS-SMVgitclean
+export FIREBOTMANS=$FIREBOTROOT/Manuals
+export FBUG=$FIREBOTMANS/FDS_User_Guide
+export FBVG=$FIREBOTMANS/FDS_Verification_Guide
+export FBVAL=$FIREBOTMANS/FDS_Validation_Guide
 export BASEDIR=`pwd`
 
 # Copy User's Guide Figures
@@ -16,6 +16,5 @@ cp $FBVG/SCRIPT_FIGURES/* $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/
 echo Verification Figures Copied
 
 # Copy Validation Guide Figures
-#cp -R $FBVAL/SCRIPT_FIGURES/* $BASEDIR/FDS_Validation_Guide/SCRIPT_FIGURES/ &> /dev/null
-rsync -r --exclude=*.svn $FBVAL/SCRIPT_FIGURES/* $BASEDIR/FDS_Validation_Guide/SCRIPT_FIGURES/
+cp -R $FBVAL/SCRIPT_FIGURES/* $BASEDIR/FDS_Validation_Guide/SCRIPT_FIGURES/ &> /dev/null
 echo Validation Guide Figures Copied
