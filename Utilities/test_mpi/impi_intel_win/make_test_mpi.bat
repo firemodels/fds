658d4078f5c3ad48e9fbc423832923139a7299ae
gforney <gforney@gmail.com>
2015-10-01 10:53:09 -0400

    mpi test: update makefile for test_mpi utility to use latest version of fortran and mpi libraries

--- Utilities/test_mpi/impi_intel_win/make_test_mpi.bat
+++ Utilities/test_mpi/impi_intel_win/make_test_mpi.bat
@@ -1,12 +1,10 @@
 @echo on
 
-set intelbin=%IFORT_COMPILER14%\bin
-set impiversion=5.0.0.028
-
-set SETUP_IFORT_COMPILER64=1
 echo Setting up compiler environment
-call "%intelbin%\ifortvars" intel64
-call "%intelbin%\..\..\MPI\%impiversion%\intel64\bin\mpivars.bat"
+call ..\..\..\Utilities\Scripts\setup_intel_compilers.bat
+
+echo Setting up mpi environment
+call "%IFORT_COMPILER%\MPI\intel64\bin\mpivars.bat"
 
 make VPATH=".." -f "..\makefile" impi_intel_win
 pause
