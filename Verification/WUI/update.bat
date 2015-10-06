56f4307ba09f166f9108f7404f2624d4f517c037
gforney <gforney@gmail.com>
2015-10-06 15:12:51 -0400

    smv vv: add batch files to update smokeview script files
    Former-commit-id: 34e1121075f8050d1f57ca907504a8f3720deda4 [formerly 9f52004b54ef7141f02d93d3e4b25142c63bb66a]
    Former-commit-id: bab09adf16767fb34a012620d53e430de3a96f3b
    Former-commit-id: da9a31534fac8588e91911988f91e3d29be9442c

---
+++ Verification/WUI/update.bat
@@ -0,0 +1,6 @@
+smokeview -update_ssf BT10m_2x2km_LS
+smokeview -update_ssf hill_structure
+smokeview -update_ssf levelset1
+smokeview -update_ssf pine_tree
+smokeview -update_ssf tree_test2
+smokeview -update_ssf wind_test1
