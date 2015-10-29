4f839e5bb5fde352026227c56faff0091c2fb545
gforney <gforney@gmail.com>
2015-10-29 11:20:37 -0400

    FDS build: update build scripts to use MPIDISTETH/MPIDISTIB environment variables optinally defined in a startup file

--- FDS_Compilation/Scripts/set_mpidist.sh
+++ FDS_Compilation/Scripts/set_mpidist.sh
@@ -4,7 +4,14 @@
 
 export MPIDIST=$1
 
-if [[ "$MPIDIST" != "" && ! -d $MPIDIST ]]; then
+if [[ "$MPIDIST" == "" ]]; then
+  echo "*** Warning: the MPI distribution location is not defined."
+  echo "Make sure MPIDISTETH and/or MPIDISTIB variables are defined"
+  echo "in your startup file"
+  exit
+fi
+
+if [[ ! -d $MPIDIST ]]; then
   echo "*** Warning: the MPI distribution, $MPIDIST, does not exist"
   echo "*** Compilation aborted"
   MPIDIST=
@@ -12,7 +19,6 @@ if [[ "$MPIDIST" != "" && ! -d $MPIDIST ]]; then
 fi
 
 # Update LD_LIBRARY_PATH and PATH
-echo Building FDS with the MPI distribution: $MPIDIST
 LD_LIBRARY_PATH=$MPIDIST/lib:$LD_LIBRARY_PATH
 PATH=$MPIDIST/bin:$PATH
 export LD_LIBRARY_PATH PATH
