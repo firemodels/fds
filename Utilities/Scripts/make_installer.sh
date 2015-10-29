64f2ebe70998ea273f332f63f6cd6ab9c55e2edb
gforney <gforney@gmail.com>
2015-10-29 13:40:13 -0400

    bundle scripts: update make_installer script
    Former-commit-id: 3048f28593470dcf2ad278340717481f777f5700 [formerly 054d5c616198e3628312b0ea811eba1fc33dfc6d]
    Former-commit-id: 67f24bd6d451882177d31e6f37dda514d717bda1
    Former-commit-id: b89fa777e78eeb480083a4d99f908c79d7e82716

--- Utilities/Scripts/make_installer.sh
+++ Utilities/Scripts/make_installer.sh
@@ -287,11 +287,17 @@ fi
 
 cat << EOF >> $INSTALLER
 mpipath=
+mpipatheth=
+mpipathib=
 if [ -d /shared/openmpi_64 ] ; then
-   mpipath=/shared/openmpi_64
+   mpipatheth=/shared/openmpi_64
+   mpipath=\$MPIDISTETH
+   mpipath2=\\\$MPIDISTETH
 fi
 if [ -d /shared/openmpi_64ib ] ; then
-   mpipath=/shared/openmpi_64ib
+   mpipathib=/shared/openmpi_64ib
+   mpipath=\$MPIDISTIB
+   mpipath2=\\\$MPIDISTIB
 fi
 
 #--- do we want to proceed
@@ -402,10 +408,12 @@ cat << EOF >> $INSTALLER
   BASHSTARTUP=/tmp/.bash_profile_temp_\$\$
   cd \$THISDIR
   echo "Updating .bash_profile"
-  grep -v bashrc_fds ~/.bash_profile | grep -v "#FDS" > \$BASHSTARTUP
-  echo "#FDS " >> \$BASHSTARTUP
-  echo "#FDS Setting the environment for FDS and Smokeview. "   >> \$BASHSTARTUP
-  echo "source ~/.bashrc_fds \$mpipath" >> \$BASHSTARTUP
+  grep -v bashrc_fds ~/.bash_profile | grep -v "#FDS" | grep -v MPIDISTETH | grep -v MPIDISTIB > \$BASHSTARTUP
+  echo "#FDS environment -----------------------------" >> \$BASHSTARTUP
+  echo "export MPIDISTETH=\$mpipatheth"                       >> \$BASHSTARTUP
+  echo "export MPIDISTIB=\$mpipathib"                         >> \$BASHSTARTUP
+  echo "source ~/.bashrc_fds \$mpipath2"                      >> \$BASHSTARTUP
+  echo "# --------------------------------------------"       >> \$BASHSTARTUP
   cp \$BASHSTARTUP ~/.bash_profile
   rm \$BASHSTARTUP
 EOF
@@ -417,10 +425,12 @@ cat << EOF >> $INSTALLER
   BASHSTARTUP=/tmp/.bashrc_temp_\$\$
   cd \$THISDIR
   echo "Updating .bashrc"
-  grep -v bashrc_fds ~/.bashrc | grep -v "#FDS" > \$BASHSTARTUP
-  echo "#FDS " >> \$BASHSTARTUP
-  echo "#FDS Setting the environment for FDS and Smokeview. "   >> \$BASHSTARTUP
-  echo "source ~/.bashrc_fds \$mpipath" >> \$BASHSTARTUP
+  grep -v bashrc_fds ~/.bashrc | grep -v "#FDS" | grep -v MPIDISTETH | grep -v MPIDISTIB > \$BASHSTARTUP
+  echo "#FDS environment -----------------------" >> \$BASHSTARTUP
+  echo "export MPIDISTETH=\$mpipatheth"           >> \$BASHSTARTUP
+  echo "export MPIDISTIB=\$mpipathib"             >> \$BASHSTARTUP
+  echo "source ~/.bashrc_fds \$mpipath2"          >> \$BASHSTARTUP
+  echo "#FDS -----------------------------------" >> \$BASHSTARTUP
   cp \$BASHSTARTUP ~/.bashrc
   rm \$BASHSTARTUP
 EOF
