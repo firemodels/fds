597aa0c69034e425a1962ab42f484d86aa278596
gforney <gforney@gmail.com>
2015-10-30 08:27:50 -0400

    FDS build: add platform option to setmpi script
    Former-commit-id: 670fb01a97000d7e030669c112e71e044bdddb6d [formerly 8d2b64ee8770b465adc48652a6a2ace2a8f83e2e]
    Former-commit-id: 9f46be75bcebf4c0017d523890c9cd49fe9affdc
    Former-commit-id: 7cf139cf8d2429aea0b44d19c6bb339804d3bded

--- FDS_Compilation/Scripts/set_mpidist.sh
+++ FDS_Compilation/Scripts/set_mpidist.sh
@@ -2,17 +2,30 @@
 
 # MPI distribution location
 
-export MPIDIST=$1
+export MPITYPE=$1
+export MPIDIST=$2
+
+if [[ "$MPIDIST" == "" ]]; then
+  if [[ "$MPITYPE" == "ib" ]]; then
+    if [[ -d /shared/openmpi_64ib ]]; then
+      MPIDIST=/shared/openmpi_64ib
+    fi
+  else
+    if [[ -d /shared/openmpi_64 ]]; then
+      MPIDIST=/shared/openmpi_64
+    fi
+  fi
+fi
 
 if [[ "$MPIDIST" == "" ]]; then
   echo "*** Warning: the MPI distribution location is not defined."
-  echo "Make sure the MPIDIST_ETH and/or MPIDIST_IB environment"
+  echo "Make sure that the MPIDIST_ETH and/or MPIDIST_IB environment"
   echo "variables are defined in your startup file"
   exit
 fi
 
 if [[ ! -d $MPIDIST ]]; then
-  echo "*** Warning: the MPI distribution, $MPIDIST, does not exist"
+  echo "*** Warning: the MPI distribution, $MPIDIST, does not exist."
   echo "*** Compilation aborted"
   MPIDIST=
   exit
