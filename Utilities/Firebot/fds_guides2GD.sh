a12ea3e602b0facbba918a3dfc4246f965cc8545
gforney <gforney@gmail.com>
2015-11-03 09:46:41 -0500

    bots: update logic for uploading files
    Former-commit-id: 2228bc4bcf68f6c6a3e16e762c9aaca3c3e6ccaf [formerly 2318f115998df62562bc70cb494f96118f087168]
    Former-commit-id: 7c4cfabb754968ae0296e9804b44d051a3d59d39
    Former-commit-id: 4cf5658377348fdff7653fa215bd08968f80cdfc

--- Utilities/Firebot/fds_guides2GD.sh
+++ Utilities/Firebot/fds_guides2GD.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
+FROMDIR=$1
 
 GDRIVE=~/bin/gdrive
 CURDIR=`pwd`
 # directory containing guides on google drive : FDS-SMV Newest Manuals
 PARENT_ID=0B_wB1pJL2bFQUlJwMmNfaHlqME0
 
-# directory containing guides within firebot account
-FROMDIR=~firebot/firebotgit/output
-BASEDIR=Newest_Guides
-
 UPLOAD ()
 {
   FILE=$1
   FILEnew=${FILE}_new.pdf
-  cp $FILE.pdf /tmp/$FILEnew
+  cp $FILE.pdf $FILEnew
   $GDRIVE list  | grep $FILEnew | awk '{ system("~/bin/gdrive delete -i " $1)} '
-  $GDRIVE upload -p $PARENT_ID -f /tmp/$FILEnew
+  $GDRIVE upload -p $PARENT_ID -f $FILEnew
 }
 
 if [ -e $GDRIVE ] ; then
-  cd $FROMDIR/$BASEDIR
+  cd $FROMDIR
 #  UPLOAD FDS_Configuration_Management_Plan
   UPLOAD FDS_Technical_Reference_Guide
   UPLOAD FDS_User_Guide
