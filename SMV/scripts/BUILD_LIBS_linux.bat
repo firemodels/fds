3444f0f8c843c2c4cd2eee90e7d55716453dfd27
gforney <gforney@gmail.com>
2015-11-09 15:25:15 -0500

    build scripts: correct script that builds libraries for linux smokeview/smokezip/smokediff

--- SMV/scripts/BUILD_LIBS_linux.bat
+++ SMV/scripts/BUILD_LIBS_linux.bat
@@ -20,10 +20,10 @@ call %envfile%
 %svn_drive%
 cd %svn_root%\smv\scripts
 
-set scriptdir=FDS-SMV/Utilities/Scripts
-set LIBDIR=FDS-SMV/SMV/Build/LIBS
+set scriptdir=%linux_svn_root%/Utilities/Scripts
+set LIBDIR=%linux_svn_root%/SMV/Build/LIBS
 
-plink %svn_logon% %scriptdir%/ssh_command2.sh %linux_hostname% %LIBDIR%/lib_linux_intel_64 makelibs.sh
+plink %svn_logon% %scriptdir%/ssh_command2.csh %linux_hostname% %LIBDIR%/lib_linux_intel_64 makelibs.sh
 
 echo.
 echo build complete
