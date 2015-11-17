d7a85b825fcc5e4f37c31c5c330d5b351d3a4024
shostikk <simo.hostikka@aalto.fi>
2015-11-17 19:40:54 +0200

    Use environemnt variable for MPI distribution path

--- FDS_Compilation/mpi_intel_linux_64/make_fds.sh
+++ FDS_Compilation/mpi_intel_linux_64/make_fds.sh
@@ -4,7 +4,7 @@ dir=`pwd`
 target=${dir##*/}
 
 source $IFORT_COMPILER/bin/compilervars.sh $platform
-source ../Scripts/set_mpidist.sh /shared/openmpi_64
+source ../Scripts/set_mpidist.sh $MPIDIST
 if [ "$MPIDIST" == "" ]; then
   exit
 fi
