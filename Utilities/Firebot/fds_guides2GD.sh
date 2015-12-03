6e26c6b996f6fc2497fe5dc29a1df3a251169e99
gforney <gforney@gmail.com>
2015-12-03 08:58:50 -0500

    firebot: upload figures to directory where manuals are uploaded
    Former-commit-id: d7fc7c639add449b2155430a28fa8c33d282b7ea [formerly 8558c5e3f71d5ff125f5e93d5f92054404807fc6]
    Former-commit-id: fa553ae719e69fb38d9d978cb9ea2500e7a22daa
    Former-commit-id: ec7a03685862983c76d4b8712767dc629b8ee41f

--- Utilities/Firebot/fds_guides2GD.sh
+++ Utilities/Firebot/fds_guides2GD.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 FROMDIR=$1
+MANDIR=$2
 
 GDRIVE=~/bin/gdrive
 CURDIR=`pwd`
 # directory containing guides on google drive : FDS-SMV Newest Manuals
 PARENT_ID=0B_wB1pJL2bFQUlJwMmNfaHlqME0
 
-UPLOAD ()
+UPLOADGUIDE ()
 {
+  cd $FROMDIR
   FILE=$1
   FILEnew=${FILE}_new.pdf
   cp $FILE.pdf $FILEnew
   $GDRIVE list  | grep $FILEnew | awk '{ system("~/bin/gdrive delete -i " $1)} '
   $GDRIVE upload -p $PARENT_ID -f $FILEnew
 }
+UPLOADFIGURES ()
+{
+  cd $MANDIR
+  DIRECTORY=$1
+  tarfile=$DIRECTORY.tar
+  cd $DIRECTORY/SCRIPT_FIGURES
+  tar cvf $tarfile .
+  gzip $tarfile
+  $GDRIVE list  | grep $tarfile.gz | awk '{ system("~/bin/gdrive delete -i " $1)} '
+  $GDRIVE upload -p $PARENT_ID -f $tarfile.gz
+}
 
 if [ -e $GDRIVE ] ; then
-  cd $FROMDIR
-  UPLOAD FDS_Config_Management_Plan
-  UPLOAD FDS_Technical_Reference_Guide
-  UPLOAD FDS_User_Guide
-  UPLOAD FDS_Validation_Guide
-  UPLOAD FDS_Verification_Guide
+  UPLOADGUIDE FDS_Config_Management_Plan
+  UPLOADGUIDE FDS_Technical_Reference_Guide
+  UPLOADGUIDE FDS_User_Guide
+  UPLOADGUIDE FDS_Validation_Guide
+  UPLOADGUIDE FDS_Verification_Guide
+  UPLOADFIGURES FDS_Technical_Reference_Guide
+  UPLOADFIGURES FDS_User_Guide
+  UPLOADFIGURES FDS_Validation_Guide
+  UPLOADFIGURES FDS_Verification_Guide
   cd $CURDIR
 fi
