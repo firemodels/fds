a8428e5a6bc0225b117cdaf27babb31121cde99f
rmcdermo <randy.mcdermott@gmail.com>
2015-12-10 11:56:59 -0500

    FDS Source: add comment in mass
    Former-commit-id: e2612f7e9d583c6e13047f4db807f322eed969e7 [formerly f284c65924fc96d2fc793894e9783ed86207acad]
    Former-commit-id: 6f88e44c16253a3ea703199de84256d488ffd3d1
    Former-commit-id: 06efb03911fd152e9dbe0daf7a518de2254e4bd1

--- FDS_Source/mass.f90
+++ FDS_Source/mass.f90
@@ -252,6 +252,7 @@ CASE(.TRUE.) PREDICTOR_STEP
 
       SELECT CASE(WC%BOUNDARY_TYPE)
          CASE DEFAULT; CYCLE WALL_LOOP
+         ! SOLID_BOUNDARY is not currently functional here, but keep for testing
          CASE(SOLID_BOUNDARY);        UN = -SIGN(1._EB,REAL(IOR,EB))*WC%ONE_D%UW
          CASE(INTERPOLATED_BOUNDARY); UN = UVW_SAVE(IW)
       END SELECT
@@ -386,6 +387,7 @@ CASE(.FALSE.) PREDICTOR_STEP
 
       SELECT CASE(WC%BOUNDARY_TYPE)
          CASE DEFAULT; CYCLE WALL_LOOP_2
+         ! SOLID_BOUNDARY is not currently functional here, but keep for testing
          CASE(SOLID_BOUNDARY);        UN = -SIGN(1._EB,REAL(IOR,EB))*WC%ONE_D%UWS
          CASE(INTERPOLATED_BOUNDARY); UN = UVW_SAVE(IW)
       END SELECT
