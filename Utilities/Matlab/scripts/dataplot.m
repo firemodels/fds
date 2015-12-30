56eb79996e78e16f9c1fe5bc6287b6d6b987a13e
rmcdermo <randy.mcdermott@gmail.com>
2015-12-30 12:22:55 -0500

    Matlab: add + operator to dataplot
    Former-commit-id: b1bbdaa7f65b01d59f8c11a8628b375808eb9eae [formerly c22946909ba74f201ece135dc536877ec2b1934d]
    Former-commit-id: cf76e29c3e0d62f5cefdedbd10defd3c7f04876d
    Former-commit-id: 6e36c48fdb15825dfdcfe5b91e825fe3765e1a8b

--- Utilities/Matlab/scripts/dataplot.m
+++ Utilities/Matlab/scripts/dataplot.m
@@ -30,7 +30,8 @@
 % Dependencies:
 %    ../scripts/define_drow_variables.m
 %    dvcread.m
-%    parse.m
+%    parsepipe.m
+%    parseplus.m
 %
 % Example: From the command line within the Matlab/functions/ directory,
 %    type
@@ -139,7 +140,7 @@ for i=2:n_plots
         define_drow_variables
         
         % Save for scatter plots
-        Q1                      = parse(Quantity);
+        Q1                      = parsepipe(Quantity);
         Save_Quantity(i,1:length(Q1))        = Q1;
         Save_Group_Style(i)     = Group_Style;
         Save_Fill_Color(i)      = Fill_Color;
@@ -156,9 +157,9 @@ for i=2:n_plots
            continue
         end
         [H M] = dvcread(d1_Filename,d1_Col_Name_Row);
-        R1 = parse(d1_Ind_Col_Name);
-        S1 = parse(d1_Dep_Col_Name);
-        style = parse(d1_Style);
+        R1 = parsepipe(d1_Ind_Col_Name);
+        S1 = parsepipe(d1_Dep_Col_Name);
+        style = parsepipe(d1_Style);
         % Wrap entire d1 dataplot routine in try loop
         % Skips case upon any Matlab error
         try
@@ -253,59 +254,70 @@ for i=2:n_plots
            continue
         end
         [H M] = dvcread(d2_Filename,d2_Col_Name_Row);
-        R2 = parse(d2_Ind_Col_Name);
-        S2 = parse(d2_Dep_Col_Name);
-        style = parse(d2_Style);
+        R2 = parsepipe(d2_Ind_Col_Name);
+        S2 = parsepipe(d2_Dep_Col_Name);
+        style = parsepipe(d2_Style);
         % Wrap entire d2 dataplot routine in try loop
         % Skips case upon any Matlab error
         try
             for j=1:length(S2)
+                
+                % check for "+" operator on columns 
+                SP = parseplus(S2(j));
+
                 d2_Ind_Col = find(strcmp(H,R2(min(j,length(R2)))));
-                d2_Dep_Col = find(strcmp(H,S2(j)));
+                for jj=1:length(SP)
+                    d2_Dep_Col(jj) = find(strcmp(H,SP(jj)));
+                end
                 clear indices
+
                 % Clear flag for stat_x_y metric
                 using_stat_x_y = 0;
                 using_stat_x_y_check_zero = 0;
                 indices = find(d2_Comp_Start<=M(:,d2_Ind_Col) & M(:,d2_Ind_Col)<=d2_Comp_End);
+
+                M_Ind = M(indices,d2_Ind_Col);
+                M_Dep = sum(M(indices,d2_Dep_Col),2);
+
                 if strcmp(Metric,'max')
-                    Save_Predicted_Metric(i,j,1) = max(M(indices,d2_Dep_Col))-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = max(M_Dep)-d2_Initial_Value;
                 elseif strcmp(Metric,'min')
-                    Save_Predicted_Metric(i,j,1) = d2_Initial_Value-min(M(indices,d2_Dep_Col));
+                    Save_Predicted_Metric(i,j,1) = d2_Initial_Value-min(M_Dep);
                 elseif strcmp(Metric,'maxabs')
-                    Save_Predicted_Metric(i,j,1) = max(abs(M(indices,d2_Dep_Col)-d2_Initial_Value));
+                    Save_Predicted_Metric(i,j,1) = max(abs(M_Dep-d2_Initial_Value));
                 elseif strfind(Metric,'max_')
                     using_stat_x_y = 1;
                     compare_indices = sscanf(Metric, ['max_' '%f' '_' '%f']);
                     if compare_indices(2) == j
-                        Save_Predicted_Metric(i,1,1) = max(M(indices,d2_Dep_Col))-d2_Initial_Value;
+                        Save_Predicted_Metric(i,1,1) = max(M_Dep)-d2_Initial_Value;
                         using_stat_x_y_check_zero = 1;
                     end
                 elseif strcmp(Metric,'mean')
-                    Save_Predicted_Metric(i,j,1) = abs(mean(M(indices,d2_Dep_Col))-d2_Initial_Value);
+                    Save_Predicted_Metric(i,j,1) = abs(mean(M_Dep)-d2_Initial_Value);
                 % If mean_x_y is specified for a plot with multiple curves,
                 % then get the results from curve y only
                 elseif strfind(Metric,'mean_')
                     using_stat_x_y = 1;
                     compare_indices = sscanf(Metric, ['mean_' '%f' '_' '%f']);
                     if compare_indices(2) == j
-                        Save_Predicted_Metric(i,1,1) = abs(mean(M(indices,d2_Dep_Col))-d2_Initial_Value);
+                        Save_Predicted_Metric(i,1,1) = abs(mean(M_Dep)-d2_Initial_Value);
                         using_stat_x_y_check_zero = 1;
                     end
                 elseif strcmp(Metric,'all')
-                    Save_Predicted_Metric(i,j,1:length(indices)) = M(indices,d2_Dep_Col)-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1:length(indices)) = M_Dep-d2_Initial_Value;
                 elseif strcmp(Metric,'threshold')
-                    Save_Predicted_Metric(i,j,1) = min(M(indices,d2_Dep_Col))-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = min(M_Dep)-d2_Initial_Value;
                 elseif strcmp(Metric,'area')
-                    Save_Predicted_Metric(i,j,1) = trapz(M(indices,d2_Ind_Col), M(indices,d2_Dep_Col))-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = trapz(M_Ind,M_Dep)-d2_Initial_Value;
                 elseif strcmp(Metric,'end')
-                    Save_Predicted_Metric(i,j,1) = M(indices(end),d2_Dep_Col)-d2_Initial_Value;
+                    Save_Predicted_Metric(i,j,1) = M_Dep(end)-d2_Initial_Value;
                 % If end_x_y is specified for a plot with multiple curves,
                 % then get the results from curve y only
                 elseif strfind(Metric,'end_')
                     using_stat_x_y = 1;
                     compare_indices = sscanf(Metric, ['end_' '%f' '_' '%f']);
                     if compare_indices(2) == j
-                        Save_Predicted_Metric(i,1,1) = M(indices(end),d2_Dep_Col)-d2_Initial_Value;
+                        Save_Predicted_Metric(i,1,1) = M_Dep(end)-d2_Initial_Value;
                         using_stat_x_y_check_zero = 1;
                     end
                 else
@@ -320,13 +332,15 @@ for i=2:n_plots
                     Save_Predicted_Metric(i,1,1) = 1E-12;
                 end
                 clear indices
+
+                % Plots
                 indices = find(d2_Start<=M(:,d2_Ind_Col) & M(:,d2_Ind_Col)<=d2_End);
                 if strcmp(Flip_Axis,'no')
-                    X = M(indices,d2_Ind_Col)/Scale_Ind;
-                    Y = M(indices,d2_Dep_Col)/Scale_Dep;
+                    X = M_Ind/Scale_Ind;
+                    Y = M_Dep/Scale_Dep;
                 else
-                    X = M(indices,d2_Dep_Col)/Scale_Dep;
-                    Y = M(indices,d2_Ind_Col)/Scale_Ind;
+                    X = M_Dep/Scale_Dep;
+                    Y = M_Ind/Scale_Ind;
                 end
                 if strcmp(Plot_Type,'linear')
                     K(length(S1)+j) = plot(X,Y,char(style(j)));
@@ -337,6 +351,7 @@ for i=2:n_plots
                 elseif strcmp(Plot_Type,'semilogy')
                     K(length(S1)+j) = semilogy(X,Y,char(style(j)));
                 end
+
             end
         catch
             display(['Error: Problem with dataplot row ', num2str(i), ' (', Dataname,...
@@ -392,7 +407,7 @@ for i=2:n_plots
             end
 
             if size(Key_Position)>0
-                legend_handle = legend(K,[parse(d1_Key),parse(d2_Key)],'Location',Key_Position);
+                legend_handle = legend(K,[parsepipe(d1_Key),parsepipe(d2_Key)],'Location',Key_Position);
                 % % The latest version of Matlab (R2015b) apparently get this correct, but I will
                 % % leave this commented code for a bit until we are sure this is working on blaze.
                 % if strcmp(Key_Position,'EastOutside')
