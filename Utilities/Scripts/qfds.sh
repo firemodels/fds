9b84161d2ca88c36f584b89adc4250ec94276b2c
gforney <gforney@gmail.com>
2016-01-11 23:10:14 -0500

    run scripts: minor formatting edit in qfds
    Former-commit-id: 472a000a3320a3f1463ad38f6ceaf9fb03add857 [formerly 18d6e1672931c773d09ec156cf458e11107f1e0a]
    Former-commit-id: 6788113dcc6a1e98d99ab73ec165bfaee688a704
    Former-commit-id: 307c0b1876ab82c752e3a44936a12d13b96664cd

--- Utilities/Scripts/qfds.sh
+++ Utilities/Scripts/qfds.sh
@@ -407,7 +407,7 @@ export OMP_NUM_THREADS=$nopenmp_threads
 cd $fulldir
 echo
 echo \`date\`
-echo Input file: $infile 
+echo Input file: $in
 echo Directory: \`pwd\`
 echo Host: \`hostname\`
 $MPIRUN $exe $in $OUT2ERROR
