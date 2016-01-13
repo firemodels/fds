e6b17dd23e9b3a207f1c8313d8cf47b9d6d6653e
gforney <gforney@gmail.com>
2016-01-13 17:18:44 -0500

    smv vv: add option to Run_SMV_Cases.sh to wait until all cases have completed
    Former-commit-id: 371a60539601ae3c7691d7e33be8b0ebf1587e63 [formerly d589d5865b0ca9c6da6ba48dbfa87433fe2ecaf7]
    Former-commit-id: 53e09c5c96fdc9873eb95ba12579cbab2f1ad74f
    Former-commit-id: 8e2ec6cc18ac743613f6289ae309319afc337501

--- Manuals/FDS_User_Guide/make_geom_figures.sh
+++ Manuals/FDS_User_Guide/make_geom_figures.sh
@@ -2,10 +2,10 @@
 CURDIR=`pwd`
 
 cd ../../Verification/scripts
-./Run_SMV_Cases.sh -g
+./Run_SMV_Cases.sh -g  -w -j GM_
 
 cd $CURDIR
 cd ../../Verification/scripts
-#call Make_SMV_Pictures.sh -g
+./Make_SMV_Pictures.sh -g
 cd $CURDIR
 
