234c7d02cafede70b84dbb8f93cd8ece08860acd
gforney <gforney@gmail.com>
2016-01-14 10:25:27 -0500

    scripts: add option to make_geom_figures script to specify queue
    Former-commit-id: a4a0e8b77d04053aec26ddb7eb68f80ba30ee7a0 [formerly 959559680e1421ea835045d88540cff2febe91bb]
    Former-commit-id: 3b2eae6d9d4717bb0a0e529570c85f1aa6705abd
    Former-commit-id: 81e8672d8167a826dfb914b9e87b1dec7c3b1d3b

--- Manuals/FDS_User_Guide/make_geom_figures.sh
+++ Manuals/FDS_User_Guide/make_geom_figures.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 CURDIR=`pwd`
+QUEUE=
+
+function usage {
+echo "-h - display this message"
+echo "-q queue - specify queue"
+exit
+}
+
+while getopts 'hq:' OPTION
+do
+case $OPTION  in
+  h)
+   usage
+   ;;
+  q)
+   QUEUE="-q $OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
 
 cd ../../Verification/scripts
-./Run_SMV_Cases.sh -g  -w -j GM_
+./Run_SMV_Cases.sh -g  -w -j GM_ $QUEUE
 
 cd $CURDIR
 cd ../../Verification/scripts
