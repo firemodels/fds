9bda07fc91da50e4f07f1139f434d0acf95bdc70
gforney <gforney@gmail.com>
2016-01-14 16:59:14 -0500

    ply2fds: add command line parsing

--- SMV/source/ply2fds/main.c
+++ SMV/source/ply2fds/main.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/* ------------------ usage ------------------------ */
+
+void usage(void){
+  printf(" ply2fds filename.ply\n\n");
+  printf(" -h - display this message\n");
+}
 
 /* ------------------ main ------------------------ */
 
 int main(int argc, char **argv){
+  int i;
+  char *filebase=NULL;
+
+  if(argc==1){
+    usage();
+    return 1;
+  }
+
+  for(i = 1; i<argc; i++){
+    int lenarg;
+    char *arg;
+
+    arg = argv[i];
+    lenarg = strlen(arg);
+    if(arg[0]=='-'&&lenarg>1){
+      switch(arg[1]){
+      case 'h':
+        usage();
+        return 1;
+        break;
+      default:
+        usage();
+        return 1;
+        break;
+      }
+    }
+    else{
+      if(filebase==NULL){
+        filebase = argv[i];
+      }
+    }
+  }
+  if(filebase==NULL){
+    usage();
+    return 1;
+  }
 
 }
--- SMV/source/ply2fds/ply.c
+++ SMV/source/ply2fds/ply.c
@@ -31,6 +31,7 @@ WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
 
 */
 
+#include "options.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
