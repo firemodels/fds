d6e16767ebb8eddfce13352acd09f01a6bbd85bf
gforney <gforney@gmail.com>
2016-01-23 16:49:06 -0500

    util build: add warning about where fds2ascii is built now
    Former-commit-id: 5829e555d378a22899f79ceb8c839a4342619c35 [formerly 61dae05a3e15fd0797b9bd9f23c822173bbf2da9]
    Former-commit-id: cadb0f34eff2bb94f7e2c5c96c884940a2461e2a
    Former-commit-id: a676152a0a20457e65fd16283f241a7a08344c88

--- Utilities/Data_Processing/make_fortran.bat
+++ Utilities/Data_Processing/make_fortran.bat
@@ -2,7 +2,7 @@ set intelbin="%IFORT_COMPILER16%\bin"
 
 call %intelbin%\ifortvars intel64
 
-echo use script in Utilities\fds2ascii\intel_win_64 to build fds2ascii
+echo ***warning: use script in Utilities\fds2ascii\intel_win_64 to build fds2ascii
 ifort -o layer_height layer_height.f
 ifort -o coupling_emulator -fpp -D pp_WINDOWS coupling_emulator.f90
 pause
