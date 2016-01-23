1edd896ec9936784ace0a48225580bf4f88da45e
gforney <gforney@gmail.com>
2016-01-23 16:49:06 -0500

    util build: add warning about where fds2ascii is built now

--- Utilities/Data_Processing/make_fortran.sh
+++ Utilities/Data_Processing/make_fortran.sh
@@ -4,7 +4,7 @@
  
 source $IFORT_COMPILER/bin/compilervars.sh intel64
 
-echo use script in Utilities/fds2ascii/intel_linux_64 (or intel_osx_64 ) to build fds2ascii
+echo ***warning: use script in Utilities/fds2ascii/intel_linux_64 (or intel_osx_64 ) to build fds2ascii
 ifort -o layer_height layer_height.f
 ifort -o coupling_emulator coupling_emulator.f90
 
