22472f3c0dadd9509ef1d11eddc8ee2e9e1db5b0
Jake O'Shannessy <joshannessy@gmail.com>
2016-01-28 00:26:53 -0600

    Added lua dependency into repository.

---
+++ SMV/source/lua-5.3.1/makepo/main.c
@@ -0,0 +1,133 @@
+#include "options.h"
+#include <stdio.h>
+#include <string.h>
+#include "string_util.h"
+     
+int add_msgstring=0;
+int add_comments=0;
+void usage(char *prog);
+
+/* ------------------ main ------------------------ */
+
+int main(int argc, char **argv){
+  char buffer[1024];
+  int ii;
+  char *arg,*prog;
+  FILE *stream;
+
+  //stream=fopen("menus.c","r");
+  stream=stdin;
+  prog=argv[0];
+  for(ii=1;ii<argc;ii++){
+    int lenarg;
+
+    arg=argv[ii];
+    lenarg=strlen(arg);
+    if(arg[0]=='-'&&lenarg>1){
+      switch(arg[1]){
+      case 'a':
+        add_msgstring=1;
+        break;
+      case 'c':
+        add_comments=1;
+        break;
+      default:
+        usage(prog);
+        return 1;
+      }
+    }
+    else{
+      usage(prog);
+      return 1;
+    }
+  }
+  if(add_comments==1){
+    printf("// Translate the phrase after each msgid and place\n");
+    printf("// it on the following line after msgstr, as in:\n");
+    printf("// msgid \"original phrase\"\n");
+    printf("// msgstr \"translated phrase\"\n");
+    printf("// \n");
+    printf("// If you have trouble with some of the terms feel free\n");
+    printf("// to ask.  It is also OK to leave troublesome terms\n");
+    printf("// untranslated, they will be output in English\n");
+    printf("// \n");
+    printf("// Suggested translation priorities:\n");
+    printf("// 1.  translate terms in menus, e.g. Load/Unload, \n");
+    printf("//     Show/Hide etc.\n");
+    printf("// 2.  translate terms in dialog boxes.\n");
+    printf("// 3.  Now go through the following list and translate terms.\n");
+    printf("//     that are left.  \n");
+    printf("// \n");
+  }
+  if(add_msgstring==0){
+    while(!feof(stream)){
+      char *beg,*end, *beg2;
+
+      fgets(buffer,sizeof(buffer),stream);
+      beg=strstr(buffer,"_(\"");
+      if(beg==NULL)continue;
+      beg+=2;
+      for(beg2=beg+1;beg2<buffer+sizeof(buffer);beg2++){
+        char c;
+   
+        c = *beg2;
+        if((c<'a'||c>'z')&&(c<'A'||c>'Z')){
+          if((c=='1'||c=='2'||c=='3')&&(*(beg2+1)=='D'||*(beg2+1)=='d')){
+          }
+          else{
+            continue;
+          }
+        }
+        beg=beg2-1;
+        *beg='"';
+        break;
+      }
+      end=strstr(beg+1,"\"");
+      if(end!=NULL){
+        int i,len;
+
+        end[1]=0;
+        len=strlen(beg);
+        for(i=len-2;i>=0;i--){
+          char c;
+   
+          c = beg[i];
+          if((c<'a'||c>'z')&&(c<'A'||c>'Z'))continue;
+          beg[i+1]='"';
+          beg[i+2]=0;
+          break;
+        }
+        printf("msgid %s\n",beg);
+      }
+    }
+  }
+  else{
+    while(!feof(stream)){
+      char *beg;
+
+      fgets(buffer,sizeof(buffer),stream);
+      trim(buffer);
+      beg=strstr(buffer,"msgid");
+      if(beg!=NULL&&beg==buffer){
+        printf("%s\n",buffer);
+        printf("msgstr \"\"\n");
+        printf("\n");
+      }
+    }
+  }
+}
+
+/* ------------------ usage ------------------------ */
+
+void usage(char *prog){
+  printf("%s [-a] < stdin > stdout \n",prog);
+  printf("Create a .po file by parsing a collection of .c/.h/.cpp files\n");
+  printf("looking for strings of the form _(\"....\") , outputting each\n");
+  printf("string found as \n");
+  printf("MSGID \".....\"\n");
+  printf("If the -a option is  used then the string\n");
+  printf("MSGSTR \"\"\n");
+  printf("is also output\n");
+}
+
+
---
+++ SMV/source/lua-5.3.1/makepo/options.h
@@ -0,0 +1,112 @@
+#define PROGVERSION "1.0"
+
+ /* ------------------ options ------------------------ */
+#ifdef WIN32
+#ifdef _DEBUG
+#pragma float_control( precise, on)
+#pragma float_control( except, on )
+#endif
+#pragma warning (disable:193)		
+#pragma warning (disable:695)		
+#pragma warning (disable:2557)		
+#pragma warning (disable:4996)		
+#pragma warning (disable:4701)		
+#pragma warning (disable:4310)		
+#pragma warning (disable:4127)		
+#pragma warning (disable:4244)		
+#pragma warning (disable:1478)
+#pragma warning (disable:1786)
+
+#pragma warning (disable:810)		/* conversion from xx to yy may lose sig bits */
+#pragma warning (disable:869)		/* parameer was never referenced */
+#pragma warning (disable:4018)		/* signied/unsigned match */
+#pragma warning (disable:4206)		/* translation unit empty */
+#pragma warning (disable:4267)		/* size_t to int possible loss of data */
+#pragma warning (disable:4389)		/* signed/unsigned mis-match */
+#pragma warning (disable:1418)		/* external function definition with no prior declaration */
+#pragma warning (disable:1599)		/* declaration hides variable */
+#pragma warning (disable:981)		/* operands are evaluated in unspecified order */
+#pragma warning (disable:1419)		/* external declaration in primary source file */
+#pragma warning (disable:1572)		/* floating-point equality and inequality comparisons are unreliable */
+#pragma warning (disable:494)		/* omission of "class"is nonstandard */
+#pragma warning (disable:444)		/* destructor for base class */
+#pragma warning (disable:2259)		/* non-pointer conversion from "double" to "float" ma lose significant bits */
+
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#ifdef CPP
+#define CCC "C"
+#else
+#define CCC
+#endif
+
+#ifdef CPP
+#define EXTERNCPP extern "C"
+#else
+#define EXTERNCPP
+#endif
+
+#ifdef INMAIN
+#define SVEXTERN
+#define SVDECL(var,val)  var=val
+#else
+#define SVEXTERN extern CCC
+#define SVDECL(var,val)  var
+#endif
+
+
+#ifdef pp_LINUX64
+#define pp_LINUX
+#endif
+
+#ifdef pp_OSX64
+#define pp_OSX
+#endif
+
+#define pp_PART
+#ifndef pp_OSX
+#define pp_THREAD
+#endif
+
+#undef pp_release
+#define pp_release
+
+#ifdef _DEBUG
+#define pp_MEMDEBUG
+#endif
+
+#ifdef pp_BETA
+#endif
+
+#ifdef WIN32
+#define pp_noappend
+#define pp_cvf
+#endif
+
+#ifdef pp_release
+#endif
+
+#ifdef X64
+#undef BIT64
+#define BIT64
+#endif
+
+#ifdef pp_LINUX64
+#undef BIT64
+#define BIT64
+#endif
+
+#ifdef BIT64
+#define FILE_SIZE unsigned long long
+#else
+#define FILE_SIZE unsigned int
+#endif
+
+#ifdef X64
+#define STRUCTSTAT struct __stat64
+#define STAT _stat64
+#else
+#define STRUCTSTAT struct stat
+#define STAT stat
+#endif
