fa6f9a3f0122ea45c8fa64139823264a1111fb6a
Jake O'Shannessy <joshannessy@gmail.com>
2016-01-31 18:10:58 -0600

    Improved file loading error messages.

--- SMV/source/smvluacore/load.lua
+++ SMV/source/smvluacore/load.lua
@@ -11,18 +11,27 @@ end
 
 function load.datafile(filename)
     local errorcode = loaddatafile(filename)
+    if errorcode == 1 then
+        error("load.datafile: could not load " .. filename)
+    end
     assert(errorcode == 0, string.format("loaddatafile errorcode: %d\n",errorcode))
     return errorcode
 end
 
 function load.vdatafile(filename)
     local errorcode = loadvdatafile(filename)
+    if errorcode == 1 then
+        error("load.vdatafile: could not load " .. filename)
+    end
     assert(errorcode == 0, string.format("loadvdatafile errorcode: %d\n",errorcode))
     return errorcode
 end
 
 function load.tour(name)
     local errorcode = loadtour(name)
+    if errorcode == 1 then
+        error("load.tour: could not load " .. name)
+    end
     assert(errorcode == 0, string.format("loadtour errorcode: %d\n",errorcode))
     return errorcode
 end
