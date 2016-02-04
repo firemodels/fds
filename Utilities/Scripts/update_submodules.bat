8a9923e3e28522839ed67ede01146d075fe54616
gforney <gforney@gmail.com>
2016-02-04 13:03:45 -0500

    git: add a windows batch file for updating submodules in a git repo

---
+++ Utilities/Scripts/update_submodules.bat
@@ -0,0 +1,69 @@
+@echo off
+:: usage: 
+::  update_submodules -fdsrepo reponame -help
+::  (all command arguments are optional)
+
+set fdsrepo=%userprofile%\FDS-SMVgitclean
+if x%FDSGIT% == x goto skip_fdsgit
+  if EXIST %FDSGIT% (
+    set fdsrepo=%FDSGIT%
+  )
+:skip_fdsgit
+
+set stopscript=0
+call :getopts %*
+if %stopscript% == 1 (
+  exit /b
+)
+
+set curdir=%CD%
+cd %fdsrepo%
+echo updating submodules in %fdsrepo%
+git submodule foreach git remote update 
+git submodule foreach git merge origin/master
+cd %curdir%
+
+goto eof
+
+:getopts
+ if (%1)==() exit /b
+ set valid=0
+ set arg=%1
+ if /I "%1" EQU "-repo" (
+   set fdsrepo=%2
+   set valid=1
+   shift
+ )
+ if /I "%1" EQU "-help" (
+   call :usage
+   set stopscript=1
+   exit /b
+ )
+ shift
+ if %valid% == 0 (
+   echo.
+   echo ***Error: the input argument %arg% is invalid
+   echo.
+   echo Usage:
+   call :usage
+   set stopscript=1
+   exit /b
+ )
+if not (%1)==() goto getopts
+exit /b
+
+:usage  
+echo update_submodules [options]
+echo update submodules contained in a git repo
+echo. 
+echo -help           - display this message
+echo -repo name   - specify the git repository
+echo       (default: %fdsrepo%) 
+exit /b
+
+:normalise
+set temparg=%~f1
+exit /b
+
+:eof
+
