5ef530410f5bb5a321509eb403739a844f738082
gforney <gforney@gmail.com>
2016-02-16 22:20:32 -0500

    web scripts: correct osx login
    Former-commit-id: f4ff5d1a981103d443ae071d7e150f1a745963af [formerly fc2c66473dff69d5d99ecec96a22032ede6eae8b]
    Former-commit-id: 0793666f7ed4bc3ce986b6a94335798b82721a43
    Former-commit-id: bc00bf818c76c323d9b18bff1edfc01d30c33675

--- Utilities/Scripts/BUNDLE_osx64.bat
+++ Utilities/Scripts/BUNDLE_osx64.bat
@@ -19,12 +19,12 @@ Rem location of batch files used to set up Intel compilation environment
 call %envfile%
 
 set bundledir=FDS_%fds_version%-SMV_%smv_version%_osx64
-plink %svn_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_osx64.csh %linux_svn_root% %bundledir% %osx_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
+plink %osx_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_osx64.csh %linux_svn_root% %bundledir% %osx_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
 
 set manifest=%svn_root%\Utilities\uploads\manifest_osx_64.html
 echo Downloading manifest
 erase %manifest%
-pscp %svn_logon%:manifest_osx_64.html %manifest%
+pscp %osx_logon%:manifest_osx_64.html %manifest%
 start explorer %manifest%
 
 echo Downloading compressed archive to:
