4d2c448e27b508a04e364a8015bc8c3c8d4ad1b6
gforney <gforney@gmail.com>
2016-02-24 15:57:51 -0500

    bots: create 'dow jones' timing files
    Former-commit-id: b8c421a594ba53c2dba212cfc92deb438443272a [formerly fd0235353d6b2f5a425fc1b52ff66cd2a91574d1]
    Former-commit-id: c3ddfc3bef10414e247a52d38eacfe01ad9ba99e
    Former-commit-id: 4236567aca85ec6271f9823a1a0a35e367a90031

---
+++ Verification/scripts/SMV_Timing_Cases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$QFDS -d Visualization colorconv.fds
+$QFDS -d Visualization mplume5c8.fds
+$QFDS -d Visualization plume5c.fds
+$QFDS -d Visualization thouse5.fds
+$QFDS -d WUI hill_structure.fds
+$QFDS -d WUI wind_test2.fds
