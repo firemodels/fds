d27be62635354c004bb39d1586e4a8e32f472cc8
gforney <gforney@gforney.com>
2016-02-28 16:35:55 -0500

    status scripts: update status web page script
    Former-commit-id: 465d8b23a09bf6f8713e535c0f08550a675f9dbd [formerly 5bdb7ca49fd00b50d8b88cbb616d0273291dbe52]
    Former-commit-id: 53094f9d48b0c994a0e2d25e0180f190bda1dc81
    Former-commit-id: de9327d20e00265437732b172b316cc0a3d3cfe1

--- Utilities/Firebot/status_updatepub.sh
+++ Utilities/Firebot/status_updatepub.sh
@@ -2,10 +2,12 @@
 gitwebrepo=~/FDS-SMVgitweb
 firebotdir=~/FDS-SMVgitclean/Utilities/Firebot
 old=~/.firebot/old
-new=~/.firebot/new
 newpage=~/.firebot/newpage
+olddata=~/.firebot/old_data
+newdata=~/.firebot/fds_times.csv
 running=~/.fdssmvgit/bot_running
 curdir=`pwd`
+EXIT="yes"
 
 # don't update status page if firebot is running
 cd $firebotdir
@@ -16,17 +18,36 @@ fi
 if [ -e $old ]; then
   ndiff=`diff $old $new|wc -l`
   if [ "$ndiff" == "0" ] ; then
-    exit
+    EXIT="no"
+  else
+    cp $new $old
   fi
 fi
+if [ -e $olddata ]; then
+  ndiff=`diff $olddata $newdata|wc -l`
+  if [ "$ndiff" == "0" ] ; then
+     EXIT="no"
+  else
+     cp $newdata $olddata
+  fi
+fi
+if [ "$EXIT" == "yes" ]; then
+   exit
+fi
+
 ./status_pubtop.sh > $newpage
 ./status2html.sh >> $newpage
 ./status_pubbot.sh >> $newpage
-cp $new $old
+
 cd $gitwebrepo
 git remote update
 git merge origin/gh-pages
+
 cp $newpage firebot_status.html
 git add firebot_status.html
+
+cp $newdata fds_times.csv
+git add fds_times.csv
+
 git commit -m "firebot: update firebot status page `date`"
 git push
