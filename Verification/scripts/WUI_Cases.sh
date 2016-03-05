2cb460f78dda1038b047ab980444e2e038f9adc1
gforney <gforney@gmail.com>
2016-03-05 12:52:54 -0500

    smv vv: add -t option to cases with -A parameter (to improve timing stats)

--- Verification/scripts/WUI_Cases.sh
+++ Verification/scripts/WUI_Cases.sh
@@ -4,10 +4,10 @@
 
 $QFDS -d WUI BT10m_2x2km_LS.fds
 #$QFDS -d WUI fire_line.fds
-$QFDS -d WUI -A hill_structure.fds
+$QFDS -d WUI -t -A hill_structure.fds
 $QFDS -d WUI levelset1.fds
 #$QFDS -d WUI onetree_surf_1mesh.fds
 $QFDS -d WUI pine_tree.fds
 $QFDS -d WUI tree_test2.fds
 $QFDS -d WUI wind_test1.fds
-$QFDS -d WUI -A wind_test2.fds
+$QFDS -d WUI -t -A wind_test2.fds
