1c384f46ca37229b800ed0b3c7469717936f5135
gforney <gforney@gmail.com>
2016-03-06 18:20:17 -0500

    firebot: add cpu plot to status web page hosted on blaze
    Former-commit-id: 6e4e46416073e2677189c7033126ffe15b59c5b8 [formerly 6e16c6e2612165e96bd031769b759b2d2eb75dd8]
    Former-commit-id: a7194792c5334cad722c2d6eee45ca2c864e011e
    Former-commit-id: ebd030468e86bb6682c13f02d016a02fe150a53c

--- Utilities/Firebot/status_privtop.sh
+++ Utilities/Firebot/status_privtop.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
+curdir=`pwd`
+cpufrom=~/.firebot/fds_times.csv
+
 cat << EOF
 <!DOCTYPE html>
 <html><head><title>Firebot Build Status</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Days since Jan 1, 2016', 'CPU Time (s)'],
+EOF
+
+sort -n -k 1 -t , $cpufrom | tail -30 | awk -F ',' '{ printf("[%s,%s],\n",$1,$2) }'
+
+cat << EOF
+        ]);
+
+        var options = {
+          title: 'Firebot CPU History',
+          curveType: 'line',
+          legend: { position: 'bottom' },
+          colors: ['black'],
+          pointSize: 5
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
 
 </head>
 <body>
 <h2>FDS Automatic Verification Summary Page</h2>
+<div id="curve_chart" style="width: 500px; height: 300px"></div>
 EOF
 
-curdir=`pwd`
 cd /var/www/html/firebot/manuals
 cat << EOF
 <h3>FDS Manuals</h3>
@@ -33,8 +64,6 @@ EOF
 cd $curdir
 
 cat << EOF
-<h3>Benchmark Time History</h3>
-<IMG width=500 SRC="fds_times.png"><br>
 <h3>Firebot Status</h3>
 
 Firebot status for up to 30 previous build/test cycles.<br>
