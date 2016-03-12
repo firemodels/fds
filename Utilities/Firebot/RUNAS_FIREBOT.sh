a555b5a081dca654c42b40717fb34fe83564436b
gforney <gforney@gforney.com>
2016-03-12 16:45:10 -0500

    firebot: remove debug comment character

--- Utilities/Firebot/RUNAS_FIREBOT.sh
+++ Utilities/Firebot/RUNAS_FIREBOT.sh
@@ -9,7 +9,7 @@ echo You are about to run firebot.
 echo "Press any key to proceed or <CTRL> c to abort."
 read val
 
-#nohup ./run_firebot.sh -u -c -U &
+nohup ./run_firebot.sh -u -c -U &
 echo firebot started.
 echo
 echo Type: 
