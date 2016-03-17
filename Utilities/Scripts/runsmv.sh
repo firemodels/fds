567b9607a817a25d69d68bdf896b9449777b3337
gforney <gforney@gmail.com>
2016-03-17 18:39:37 -0400

    smv vv: remove unused option, -time, from runsmv.sh script
    Former-commit-id: b2d93bf94853e29409c678f1b51e750e63826938 [formerly a4e5ce5c75a64b9adc98bf6245e82844a2d52253]
    Former-commit-id: d8a9a15940e67d491d52b9543ff1cadbf23c4e43
    Former-commit-id: 9017bd7874415b197dfc46c2c95fd79a5c1e1ee1

--- Utilities/Scripts/runsmv.sh
+++ Utilities/Scripts/runsmv.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
 FED=
 MOVIE=
 RUNSCRIPT=
 ssffile=
-TIMEOPTION=
 
-while getopts 'Ad:fmt' OPTION
+while getopts 'Ad:fm' OPTION
 do
 case $OPTION in
   d)
@@ -18,9 +16,6 @@ case $OPTION in
    ;;
   m)
    MOVIE="y"
-   ;;
-  t)
-   TIMEOPTION=-time
    ;;
 esac
 done
@@ -67,4 +62,4 @@ fi
 #source ~/.bashrc_fds default
 cd $fulldir
 echo $SMV $FED $SMVBINDIR $RUNSCRIPT $in
-$SMV $TIMEOPTION $FED $SMVBINDIR -redirect $RUNSCRIPT $in
+$SMV $FED $SMVBINDIR -redirect $RUNSCRIPT $in
