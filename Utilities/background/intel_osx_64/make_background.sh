a4c0452097d3a2328eb4333bea382f399a85942e
gforney <gforney@gmail.com>
2016-03-19 17:16:35 -0400

    repo structure: move smokeview utility build directories to SMV/Build
    Former-commit-id: 789445c6e8377ba1a7fb184e244b967cca154a78 [formerly 2065f40818404a5db0bb25a52fa44eeca58fc767]
    Former-commit-id: 6894de3b9fa193039d85a68f26390ff659016e8d
    Former-commit-id: 484faa4a641fe97d0db8fc78d97245a817398140

--- Utilities/background/intel_osx_64/make_background.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source $IFORT_COMPILER/bin/compilervars.sh intel64
-
-rm -f *.o background
-make -f ../Makefile intel_osx_64
--- Utilities/smokediff/intel_osx_64/make_diff.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -f *.o
-
-make "FORTLIBDIR=$IFORT_COMPILER_LIB" -f ../Makefile intel_osx_64
