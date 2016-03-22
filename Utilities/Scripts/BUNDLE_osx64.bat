36f994123298110b7b6de091d4b3693b4e81e545
gforney <gforney@gmail.com>
2016-03-21 21:21:58 -0400

    bundle scripts: convert bundle scripts to bash
    Former-commit-id: c1c594652d74df7f4561cb708c69ed2699159df4 [formerly 0e56071a6814bd79594e84c49f1ae3dd258d526d]
    Former-commit-id: 065b2d0934e5bc7378ba8f28800600c9c2510c07
    Former-commit-id: fc146a6ee32969e3a9f2d45133a888105f0c555e

--- Utilities/Scripts/BUNDLE_osx64.bat
+++ Utilities/Scripts/BUNDLE_osx64.bat
@@ -19,7 +19,7 @@ Rem location of batch files used to set up Intel compilation environment
 call %envfile%
 
 set bundledir=FDS_%fds_version%-SMV_%smv_version%_osx64
-plink %osx_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_osx64.csh %linux_svn_root% %bundledir% %osx_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
+plink %osx_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_osx64.sh %linux_svn_root% %bundledir% %osx_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
 
 set manifest=%svn_root%\Utilities\uploads\manifest_osx_64.html
 echo Downloading manifest
