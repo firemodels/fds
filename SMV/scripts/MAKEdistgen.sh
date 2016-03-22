bc75d5e32246bdecfb6ea50c2d3b1b0fa751dda4
gforney <gforney@gmail.com>
2016-03-21 21:05:05 -0400

    bundle scripts: add error checking when copying files when generating a smokeview bundle

--- SMV/scripts/MAKEdistgen.sh
+++ SMV/scripts/MAKEdistgen.sh
@@ -6,6 +6,56 @@ HOST=$4
 FDS_EDITION=$5
 SVNROOT=$6
 
+SCP ()
+{
+  HOST=$1
+  FROMDIR=$2
+  FROMFILE=$3
+  TODIR=$4
+  TOFILE=$5
+
+  scp $HOST\:$FROMDIR/$FROMFILE $TODIR/$TOFILE 2>/dev/null
+  if [ -e $TODIR/$TOFILE ]; then
+    echo "$TOFILE copied from $HOST"
+  else
+    echo "***error: the file $TOFILE failed to copy from $HOST"
+  fi
+}
+
+CP ()
+{
+  FROMDIR=$1
+  FROMFILE=$2
+  TODIR=$3
+  TOFILE=$4
+  if [ ! -e $FROMDIR/$FROMFILE ]; then
+    echo "***error: the file $FROMFILE does not exist"
+  else
+    cp $FROMDIR/$FROMFILE $TODIR/$TOFILE
+  fi
+  if [ -e $TODIR/$TOFILE ]; then
+    echo "$TOFILE copied"
+  else
+    echo "***error: the file $TOFILE failed to copy"
+  fi
+}
+
+CPDIR ()
+{
+  FROMDIR=$1
+  TODIR=$2
+  if [ ! -e $FROMDIR ]; then
+    echo "***error: the directory $FROMDIR does not exist"
+  else
+    cp -r $FROMDIR $TODIR
+  fi
+  if [ -e $TODIR ]; then
+    echo "$TODIR copied"
+  else
+    echo "***error: the directory $TODIR failed to copy"
+  fi
+}
+
 platformsize=${platform}_$size
 BACKGROUNDDIR=$SVNROOT/SMV/Build/background/intel_${platform}_64
 SMOKEVIEWDIR=$SVNROOT/SMV/Build/smokeview/intel_$platformsize
@@ -24,15 +74,15 @@ mkdir -p $DIR/bin
 mkdir -p $DIR/Documentation
 cp ~/FDS-SMVwebpages/smv_readme.html $DIR/Documentation/release_notes.html
 
-cp -r $FORBUNDLE/textures $DIR/bin/.
-cp $FORBUNDLE/objects.svo $DIR/bin/.
-cp $FORBUNDLE/smokeview.ini $DIR/bin/.
-cp $FORBUNDLE/volrender.ssf $DIR/bin/.
-scp $HOST\:$BACKGROUNDDIR/background $DIR/bin/.
-scp $HOST\:$SMOKEDIFFDIR/smokediff_$platformsize $DIR/bin/smokediff
-scp $HOST\:$SMOKEVIEWDIR/smokeview_$platformsize $DIR/bin/smokeview
-scp $HOST\:$SMOKEZIPDIR/smokezip_$platformsize $DIR/bin/smokezip
-scp $HOST\:$WINDDIR/wind2fds_$platformsize $DIR/bin/wind2fds
+CPDIR $FORBUNDLE/textures $DIR/bin/textures
+CP $FORBUNDLE objects.svo $DIR/bin objects.svo
+CP $FORBUNDLE smokeview.ini $DIR/bin smokeview.ini
+CP $FORBUNDLE volrender.ssf $DIR/bin volrender.ssf
+SCP $HOST $BACKGROUNDDIR background $DIR/bin background
+SCP $HOST $SMOKEDIFFDIR smokediff_$platformsize $DIR/bin smokediff
+SCP $HOST $SMOKEVIEWDIR smokeview_$platformsize $DIR/bin smokeview
+SCP $HOST $SMOKEZIPDIR smokezip_$platformsize $DIR/bin smokezip
+SCP $HOST $WINDDIR wind2fds_$platformsize $DIR/bin wind2fds
 rm -f $DIR.tar $DIR.tar.gz
 cd $DIR
 tar cvf ../$DIR.tar .
