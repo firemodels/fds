344db4525b7ae66210f8a573ae4c4a74ba5acae5
gforney <gforney@gmail.com>
2016-03-26 22:03:10 -0400

    firebot: change lite option to -L
    Former-commit-id: e9e922bc5e155ad39d4feeae50800369e57194d3 [formerly fad46efad76b987c56bcb17aae921017632505c5]
    Former-commit-id: e68798a12bce476664b3ff3817d85e171bbde5c4
    Former-commit-id: 73d7e68930ad9cf90a61f57c23436b44e040eb23

--- Utilities/Firebot/run_firebot.sh
+++ Utilities/Firebot/run_firebot.sh
@@ -30,11 +30,11 @@ echo ""
 echo "Options:"
 echo "-b - branch_name - run firebot using branch_name [default: $BRANCH]"
 echo "-c - clean repo"
-echo "-D - exit after running debug cases"
 echo "-f - force firebot run"
 echo "-F - skip figure generation and build document stages"
 echo "-h - display this message"
 echo "-i - use installed version of smokeview"
+echo "-L - firebot lite,  exit after running debug cases"
 if [ "$EMAIL" != "" ]; then
 echo "-m email_address [default: $EMAIL]"
 else
@@ -64,7 +64,7 @@ FORCE=
 SKIPMATLAB=
 SKIPFIGURES=
 FIREBOT_LITE=
-while getopts 'b:cDFfhim:q:nr:sS:uUv' OPTION
+while getopts 'b:cFfhiLm:q:nr:sS:uUv' OPTION
 do
 case $OPTION  in
   b)
@@ -72,9 +72,6 @@ case $OPTION  in
    ;;
   c)
    CLEANREPO=1
-   ;;
-  D)
-   FIREBOT_LITE=-D
    ;;
   f)
    FORCE=1
@@ -87,6 +84,9 @@ case $OPTION  in
    ;;
   i)
    USEINSTALL="-i"
+   ;;
+  L)
+   FIREBOT_LITE=-L
    ;;
   m)
    EMAIL="$OPTARG"
