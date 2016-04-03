411d75eff60ce917d5da9bdee35b04f65aff9db8
gforney <gforney@gmail.com>
2016-04-02 21:49:14 -0400

    dem: add script to run dem test cases

---
+++ SMV/Build/dem2fds/data/run_cases.bat
@@ -0,0 +1,15 @@
+@echo off
+set case=blodget
+fds %case%.fds
+
+set case=nist
+fds %case%.fds
+
+set case=sugarloaf
+fds %case%.fds
+
+set case=test
+fds %case%.fds
+
+set case=trails
+fds %case%.fds
