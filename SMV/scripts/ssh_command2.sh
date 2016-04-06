c57d5cbb7d367207e239a2b208dba681a6556fdc
gforney <gforney@gmail.com>
2016-04-06 19:57:21 -0400

    build scripts: remove unused scripts

--- SMV/scripts/ssh_command2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-host=$1
-dir=$2
-command=$3
-arg=$4
-echo copying $dir/$command to $host
-scp $dir/$command $host\:.
-echo running command $command $arg on $host
-ssh $host ./$command $arg
