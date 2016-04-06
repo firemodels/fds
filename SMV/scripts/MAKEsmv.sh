96de5e44340a745166e6acb72c3fd8059691c5f2
gforney <gforney@gmail.com>
2016-04-06 13:35:34 -0400

    build scripts: update smokeview build scripts

--- SMV/scripts/MAKEsmv.sh
+++ SMV/scripts/MAKEsmv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+dir=$1
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
 GITROOT=`pwd`
 
-cd $GITROOT/SMV/Build/smokeview/intel_linux_64
+cd $GITROOT/SMV/Build/smokeview/$dir
 ./make_smv.sh
