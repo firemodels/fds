b391fc9dea8f5ac2cfa009db6e01e473e2c999e0
gforney <gforney@gmail.com>
2016-04-13 10:12:08 -0400

    FDS vv: preliminary revision to consolidate validation table scripts
    Former-commit-id: 3c501e07cda214505c9ce52f7f2e3f2263ebfaf6 [formerly e0b4b2e64803a87e252cfdb33d7bbe559984f7f2]
    Former-commit-id: 9d6f2c642aa8c3149c725d9e512642d3ee0ead69
    Former-commit-id: c6a30e7abc59affa7d1580c081f563d0cec9f826

---
+++ Utilities/Scripts/validation_git_stats2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# This script outputs a LaTeX file with a table of the FDS validation
+# sets and their corresponding GIT information (i.e., when the FDS
+# output files were last commited to the repository). This table
+# is then included in the FDS Validation Guide.
+MAKEGITENTRY(){
+DIR=$1
+gitrevisions=/tmp/gitrevisions.$$
+cat $FDSSMV/Validation/$DIR/FDS_Output_Files/*git.txt 2> /dev/null | sort -u > $gitrevisions
+gitrev=`head -1 $gitrevisions`
+if [ "$gitrev" != "" ] ; then
+  gitrevshort=`echo $gitrev | awk -F - '{print $4}' | sed 's/^.\{1\}//'`
+  gitdate=`git show -s --format=%aD $gitrevshort | head -1 | awk '{print $3,$2",",$4}'`
+  gitdate2=`git show -s --format=%at $gitrevshort | head -1 | awk '{print $1}'`
+  echo "${DIR//_/\_}  & $gitdate & $gitrev & $gitdate2\\\\ \hline"
+fi
+rm $gitrevisions
+}
+
+
+CURRENT_DIR=`pwd`
+if [ "$FDSSMV" == "" ] ; then
+   FDSSMV=~/FDS-SMVgitclean
+fi
+export FDSSMV
+
+while getopts 'r:' OPTION
+do
+case $OPTION  in
+  r)
+   FDSSMV="$OPTARG"
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
+cd $FDSSMV/Utilities/Scripts
+
+# Name and location of output .tex file with validation GIT statistics
+OUTPUT_TEX_FILE=$FDSSMV/Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_git_stats.tex
+
+# Table header
+echo "\begin{longtable}[c]{|l|c|c|}" > $OUTPUT_TEX_FILE
+echo "\caption[Validation Git Statistics]{Validation Git statistics for all data sets}" >> $OUTPUT_TEX_FILE
+echo "\label{validation_git_stats}" >> $OUTPUT_TEX_FILE
+echo "\\\\ \hline" >> $OUTPUT_TEX_FILE
+echo "Dataset  &  FDS Revision Date  &  FDS Revision String\\\\ \hline \hline" >> $OUTPUT_TEX_FILE
+echo "\endfirsthead" >> $OUTPUT_TEX_FILE
+echo "\hline" >> $OUTPUT_TEX_FILE
+echo "Dataset  &  FDS Revision Date  &  FDS Revision String\\\\ \hline \hline" >> $OUTPUT_TEX_FILE
+echo "\endhead" >> $OUTPUT_TEX_FILE
+
+maketable=$FDSSMV/Validation/Process_All_Output.sh
+#grep VDIR $maketable | awk 'BEGIN { FS = "/" } ; { system("./makegitentry.sh  " $2) }'  | sort -n -t '&' -k 4 | awk -F "&" '{ print $1 "&" $2 "&" $3 "\\\\ \\hline"}' >> $OUTPUT_TEX_FILE
+CASEFILE=/tmp/temp.out.$$
+grep VDIR $maketable | awk 'BEGIN { FS = "/" } ; { print $2 }' > $CASEFILE
+while read p; do
+  MAKEGITENTRY   $p | sort -n -t '&' -k 4 | awk -F "&" '{ print $1 "&" $2 "&" $3 "\\\\ \\hline"}' >> $OUTPUT_TEX_FILE
+done <$CASEFILE
+rm $CASEFILE
+
+
+# Table footer
+echo "\end{longtable}" >> $OUTPUT_TEX_FILE
+
+cd $CURRENT_DIR
+
