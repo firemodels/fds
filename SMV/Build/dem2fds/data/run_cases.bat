a7cd7bf94f718cecce38050b934c23ff808ffce6
gforney <gforney@gmail.com>
2016-04-14 15:26:47 -0400

    smv vv: update batch file that generates terrain test cases

--- SMV/Build/dem2fds/data/run_cases.bat
+++ SMV/Build/dem2fds/data/run_cases.bat
@@ -1,12 +1,15 @@
 @echo off
+set fds=fds
+::set fds=..\..\..\..\fds_compilation\mpi_intel_win_64\fds_mpi_win_64
+
 set case=blodget
-fds %case%.fds
+%fds% %case%.fds
 
 set case=nist
-fds %case%.fds
+%fds% %case%.fds
 
 set case=sugarloaf
-fds %case%.fds
+%fds% %case%.fds
 
 set case=trails
-fds %case%.fds
+%fds% %case%.fds
