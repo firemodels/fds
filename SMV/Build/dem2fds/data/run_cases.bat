242db3f6dc6f7c2f3e57ed14df588469bf6bc59a
gforney <gforney@gmail.com>
2016-04-15 09:36:44 -0400

    smv vv: update scripts to run new geometry cases

--- SMV/Build/dem2fds/data/run_cases.bat
+++ SMV/Build/dem2fds/data/run_cases.bat
@@ -1,6 +1,7 @@
 @echo off
 set fds=fds
-::set fds=..\..\..\..\fds_compilation\mpi_intel_win_64\fds_mpi_win_64
+::set fds=..\..\..\..\fds_compilation\intel_win_64_db\fds_win_64_db
+::set fds=..\..\..\..\fds_compilation\intel_win_64\fds_win_64
 
 set case=blodget
 %fds% %case%.fds
@@ -12,4 +13,10 @@ set case=sugarloaf
 %fds% %case%.fds
 
 set case=trails
+%fds% %case%.fds
+
+set case=test1x1
+%fds% %case%.fds
+
+set case=test3x3
 %fds% %case%.fds
