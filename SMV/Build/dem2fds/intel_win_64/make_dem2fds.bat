e461488a28c62a4197452907449b83bae1471407
gforney <gforney@gmail.com>
2016-04-19 08:49:14 -0400

    dem2fds build: fix typo in build script

--- SMV/Build/dem2fds/intel_win_64/make_dem2fds.bat
+++ SMV/Build/dem2fds/intel_win_64/make_dem2fds.bat
@@ -2,7 +2,7 @@
 :: setup compiler environment
 call ..\..\..\..\Utilities\Scripts\setup_intel_compilers.bat
 
-Title Building getdate for 64 bit Windows
+Title Building dem2fds for 64 bit Windows
 
 erase *.obj
 make SHELL="%ComSpec%" -f ..\Makefile intel_win_64
