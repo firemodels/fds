4cf05805ac71b0db974d667736c3a989ac4494d3
gforney <gforney@gmail.com>
2016-04-22 15:27:03 -0400

    smv build: remove png125 library

--- SMV/source/png125/makelib.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-call ..\scripts\setopts %OPTS%
-erase *.o *.obj libpng.a libpng.lib
-set target=libpng.lib
-if %COMPILER% == gcc set target=libpng.a
-make COMPILER=%COMPILER% SIZE=%SIZE% RM=erase -f ./makefile %target%
-endlocal
