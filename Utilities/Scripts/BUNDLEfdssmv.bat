0636b83dc5967be404892da4d4a30d0f84a15afa
gforney <gforney@gmail.com>
2016-04-26 09:16:15 -0400

    scripts: use !var! for script variables within if statements
    Former-commit-id: 239c5298190eb39f815f604049cd72b0b9516811 [formerly c390f895ef6518f1938e2d5f79269b5fba813967]
    Former-commit-id: a36ad0c7a8275eda020d96b219116ba61235715e
    Former-commit-id: 23c8a3c5b48a506ba7c8e2ea54f63ccecda6e602

--- Utilities/Scripts/BUNDLEfdssmv.bat
+++ Utilities/Scripts/BUNDLEfdssmv.bat
@@ -32,7 +32,7 @@ if "%platform%" == "windows" (
 )
 if "%platform%" == "linux" (
   set bundledir=FDS_%fds_version%-SMV_%smv_version%_linux64
-  plink %linux_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_linux64.sh %linux_svn_root% %bundledir% %linux_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
+  plink %linux_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_linux64.sh %linux_svn_root% !bundledir! %linux_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
 
   set manifest=%svn_root%\Utilities\uploads\manifest_linux_64.html
   echo Downloading manifest
@@ -41,13 +41,13 @@ if "%platform%" == "linux" (
   start explorer !manifest!
 
   echo Downloading compressed archive to:
-  echo   %svn_root%\Utilities\uploads\%bundledir%.sh
-  pscp %linux_logon%:%linux_svn_root%/Utilities/uploads/%bundledir%.sh %svn_root%/Utilities/uploads/.
+  echo   %svn_root%\Utilities\uploads\!bundledir!.sh
+  pscp %linux_logon%:%linux_svn_root%/Utilities/uploads/!bundledir!.sh %svn_root%/Utilities/uploads/.
   goto eof
 )
 if "%platform%" == "osx" (
   set bundledir=FDS_%fds_version%-SMV_%smv_version%_osx64
-  plink %osx_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_osx64.sh %linux_svn_root% %bundledir% %osx_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
+  plink %osx_logon% %linux_svn_root%/Utilities/Scripts/BUNDLE_osx64.sh %linux_svn_root% !bundledir! %osx_hostname% %fds_edition%  %fds_version% %smv_version% %fdssmv_major_version%
 
   set manifest=%svn_root%\Utilities\uploads\manifest_osx_64.html
   echo Downloading manifest
@@ -56,8 +56,8 @@ if "%platform%" == "osx" (
   start explorer !manifest!
 
   echo Downloading compressed archive to:
-  echo   %svn_root%\Utilities\uploads\%bundledir%.sh
-  pscp %osx_logon%:%linux_svn_root%/Utilities/uploads/%bundledir%.sh %svn_root%/Utilities/uploads/.
+  echo   %svn_root%\Utilities\uploads\!bundledir!.sh
+  pscp %osx_logon%:%linux_svn_root%/Utilities/uploads/!bundledir!.sh %svn_root%/Utilities/uploads/.
   goto eof
 )
 
