5c5d3d2ee46a87bd6cfbb792f15a6683d73aa953
rmcdermo <randy.mcdermott@gmail.com>
2016-05-17 14:06:29 -0400

    FDS Source: fix some crazy typos
    Former-commit-id: 622a83b7dd4f0440ac29d313c29e9f4fb1bc320e [formerly ae9749f704b5eae8fbd768cc1c444a6884a7fea2]
    Former-commit-id: c5c13d669d3cee36647798d08e6aa4f8eece8bc7
    Former-commit-id: 4742209d9efb59a834d1bd8d07c38202786a58bd

--- FDS_Source/wall.f90
+++ FDS_Source/wall.f90
@@ -681,8 +681,8 @@ SUBROUTINE SOLID_HEAT_TRANSFER_3D
 ! Currently, this is not hooked into PYROLYSIS shell elements,
 ! but this is under development.
 
-REAL(EB) :: DT_SUB,T_LOC,RHO_S,K_S,C_S,TMP_G,TMP_F,TMP_S,RDN,HTC,K_S_M,K_S_P
-INTEGER  :: II,JJ,KK,I,J,K,IOR,IC,ICM,ICP,IIG,JJG,KKG,NR
+REAL(EB) :: DT_SUB,T_LOC,RHO_S,K_S,C_S,TMP_G,TMP_F,TMP_S,RDN,HTC,K_S_M,K_S_P,H_S
+INTEGER  :: II,JJ,KK,I,J,K,IOR,IC,ICM,ICP,IIG,JJG,KKG,NR,ITER
 REAL(EB), POINTER, DIMENSION(:,:,:) :: KDTDX=>NULL(),KDTDY=>NULL(),KDTDZ=>NULL()
 TYPE(OBSTRUCTION_TYPE), POINTER :: OB=>NULL(),OBM=>NULL(),OBP=>NULL()
 TYPE (MATERIAL_TYPE), POINTER :: ML=>NULL(),MLM=>NULL(),MLP=>NULL()
@@ -875,7 +875,6 @@ SUBSTEP_LOOP: DO WHILE ( ABS(T_LOC-DT)>TWO_EPSILON_EB )
                NR = -NINT(ML%C_S)
                C_S = EVALUATE_RAMP(TMP(I,J,K),0._EB,NR)
             ENDIF
-
             TMP(I,J,K) = TMP(I,J,K) + DT_SUB/(RHO_S*C_S) * ( (KDTDX(I,J,K)-KDTDX(I-1,J,K))*RDX(I) + &
                                                              (KDTDY(I,J,K)-KDTDY(I,J-1,K))*RDY(J) + &
                                                              (KDTDZ(I,J,K)-KDTDZ(I,J,K-1))*RDZ(K) )
@@ -2456,3 +2455,4 @@ HEAT_TRANSFER_COEFFICIENT = MAX(H_FORCED,H_NATURAL)
 END FUNCTION HEAT_TRANSFER_COEFFICIENT
 
 END MODULE WALL_ROUTINES
+
