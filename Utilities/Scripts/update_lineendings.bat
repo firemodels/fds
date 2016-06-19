16c8b7bd7d0204bd4072c5cd5ee7567c5f163077
gforney <gforney@gmail.com>
2016-06-19 01:36:07 -0400

    repo: cd to repo root directory before updating line endings

--- Utilities/Scripts/update_lineendings.bat
+++ Utilities/Scripts/update_lineendings.bat
@@ -1,8 +1,11 @@
 @echo off
 
+set CURDIR=%CD%
+cd ..\..
 git add . -u
 git commit -m "saving files before refreshing line endings"
 git rm --cached -r .
 git reset --hard
 git add .
 git commit -m "normalize all the line endings"
+cd %CURDIR%
