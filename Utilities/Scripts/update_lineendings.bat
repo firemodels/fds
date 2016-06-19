998a288ff9255dc076eb526ed4a69b1b2c71865f
gforney <gforney@gmail.com>
2016-06-19 01:22:15 -0400

    normalize all the line endings
    Former-commit-id: b78bfe1e1ab48f0a35b6b42ce31501386e2189c7 [formerly 530cbef54ca6fce54f0a9ca299c2e29bbefc292e]
    Former-commit-id: 62326aaabc69fa57d5d9809edba56b69d72d372c
    Former-commit-id: 8f8bedf64251f2b0eb1d4d8a8ffcebbe516674f2

--- Utilities/Scripts/update_lineendings.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-git add . -u
-git commit -m "saving files before refreshing line endings"
-git rm --cached -r .
-git reset --hard
-git add .
-git commit -m "normalize all the line endings"
--- Utilities/Scripts/validation_git_stats.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Name and location of output .tex file with validation SVN statistics
-set OUTPUT_TEX_FILE=..\..\Manuals\FDS_Validation_Guide\SCRIPT_FIGURES\ScatterPlots\validation_svn_stats.tex
-
-copy stats_table_template.tex %OUTPUT_TEX_FILE%
-
