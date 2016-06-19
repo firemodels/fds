cc836b1ed24d4531f5942cd59e2b0f0216460ffd
gforney <gforney@gmail.com>
2016-06-19 01:36:07 -0400

    repo: cd to repo root directory before updating line endings
    Former-commit-id: 53d584255275f0d75ea43ba040d50a7c027ec313 [formerly dc37cf0cbc9c10332ff10f616cfcd49d77af1846]
    Former-commit-id: be693e5f46c21507dc0cd3d0203bc592acdcccbe
    Former-commit-id: 5780e6530023165a84c0d90360dcad440e79654e

--- Utilities/Scripts/update_lineendings.sh
+++ Utilities/Scripts/update_lineendings.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+CURDIR=`pwd`
+cd ../..
 git add . -u
 git commit -m "saving files before refreshing line endings"
 git rm --cached -r .
 git reset --hard
 git add .
 git commit -m "normalize all the line endings"
+cd $CURDIR
