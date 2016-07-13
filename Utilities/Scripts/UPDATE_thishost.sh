707974009b6ef975cb33b333aa23e599368a9adb
gforney <gforney@gmail.com>
2016-07-13 11:40:57 -0400

    scripts: update update bash script
    Former-commit-id: 13c9beb1bbf963e54303b59e436fa55752f0da07 [formerly 780ad62cc2fe5f9038f1a9bc48683264802571d8]
    Former-commit-id: f0f9a7e8e783ac0e196c23c40327fc81a4ff99d4
    Former-commit-id: 75645d71fdc430f2244cf89642dabbad9ee3e568

--- Utilities/Scripts/UPDATE_thishost.sh
+++ Utilities/Scripts/UPDATE_thishost.sh
@@ -5,9 +5,9 @@ host=$2
 
 echo Updating the GIT repository $directory on $host to the latest revision
 echo
-cd \~/$directory
-git describe --dirty 
+cd ~/$directory
 git checkout development
 git remote update
 git merge origin/development
 git merge firemodels/development 
+git describe --dirty 
