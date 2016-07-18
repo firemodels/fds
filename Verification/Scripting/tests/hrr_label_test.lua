3a400b55b6e582baa9d19d9e1752007da070d4e9
Jake O'Shannessy <joshannessy@gmail.com>
2016-07-18 09:22:23 -0600

    smv lua: Restructure scripting tests.
    Former-commit-id: 8960f60bffc34e756a8dd9e19742a3c1ee7e1936 [formerly 1f6c16801f757b4e2484ab779530358f4b480615]
    Former-commit-id: 88752ab0fa157ddfffa4036a3e1d602d3bb853c9
    Former-commit-id: 4723476c769f384e7a2da60b342249ab745afa8d

---
+++ Verification/Scripting/tests/hrr_label_test.lua
@@ -0,0 +1,8 @@
+smv = require "smv"
+string = require "string"
+
+load.datafile("room_fire_02.sf")
+set_hrrlabel_visibility(true)
+unload.all()
+set_hrrlabel_visibility(true)
+load.datafile("room_fire_02.sf")
