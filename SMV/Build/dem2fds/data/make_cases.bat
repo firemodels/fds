91beaf7689fcb1ff95645474791806ea2692d421
gforney <gforney@gmail.com>
2016-08-05 11:37:27 -0400

    dem2fds vv: update verification cases

--- SMV/Build/dem2fds/data/make_cases.bat
+++ SMV/Build/dem2fds/data/make_cases.bat
@@ -1,21 +1,22 @@
 @echo off
 :: generate terrain with &GEOM
-set option=-e
+:: set option=-g
 
 :: generate terrain with &OBST
-::set option=-o
+set option=-o
 
-::set dem2fds=..\intel_win_64\dem2fds_win_64.exe
-set dem2fds=dem2fds
+set dem2fds=..\intel_win_64\dem2fds_win_64.exe
+::set dem2fds=dem2fds
 
-set case=blodget
-%dem2fds% %option% %case% < %case%_elevs.csv > %case%.fds
+::set case=blodget
+::%dem2fds% %option% %case% < %case%_elevs.csv > %case%.fds
 
-set case=nist
-%dem2fds% %option% %case% < %case%_elevs.csv > %case%.fds
+::set case=nist
+::%dem2fds% %option% %case% < %case%_elevs.csv > %case%.fds
 
-set case=sugarloaf
-%dem2fds% %option% %case% < %case%_elevs.csv > %case%.fds
+%dem2fds% %option% -d %userprofile%\terrain\tower tower.in 
 
-set case=trails
-%dem2fds% %option% %case% < %case%_elevs.csv > %case%.fds
+%dem2fds% %option% -d %userprofile%\terrain\sugarloaf sugarloaf.in 
+
+
+%dem2fds% %option% -d %userprofile%\terrain\trails trails.in 
