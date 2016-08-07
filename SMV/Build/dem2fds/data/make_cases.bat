83e763f63ea2c536fae8467c8a3d6b004d1f5ee7
gforney <gforney@gmail.com>
2016-08-07 10:47:20 -0400

    dem2fds vv: udpate sscripts that make and run dem2fds vv cases

--- SMV/Build/dem2fds/data/make_cases.bat
+++ SMV/Build/dem2fds/data/make_cases.bat
@@ -8,17 +8,27 @@ set option=-o
 set dem2fds=..\intel_win_64\dem2fds_win_64.exe
 ::set dem2fds=dem2fds
 
-::set case=blodget
+echo.
+echo demtest
+%dem2fds% %option% -d %userprofile%\terrain\demtest demtest1.in 
+%dem2fds% %option% -d %userprofile%\terrain\demtest demtest2.in 
+
+echo.
+echo blodget
 %dem2fds% %option% -d %userprofile%\terrain\blodget blodget.in 
 
+echo.
 echo NIST
 %dem2fds% %option% -d %userprofile%\terrain\nist nist.in 
 
+echo.
 echo tower
 %dem2fds% %option% -d %userprofile%\terrain\tower tower.in 
 
+echo.
 echo sugarloaf
 %dem2fds% %option% -d %userprofile%\terrain\sugarloaf sugarloaf.in 
 
+echo.
 echo trails
 %dem2fds% %option% -d %userprofile%\terrain\trails trails.in 
--- SMV/Build/dem2fds/data/run_cases.bat
+++ SMV/Build/dem2fds/data/run_cases.bat
@@ -3,15 +3,27 @@ set fds=fds
 ::set fds=..\..\..\..\FDS_Compilation\intel_win_64_db\fds_win_64_db
 ::set fds=..\..\..\..\FDS_Compilation\mpi_intel_win_64\fds_mpi_win_64
 
-set case=blodget
-%fds% %case%.fds
+echo.
+echo demtest
+%fds% demtest1.fds 
+%fds% demtest2.fds 
 
-set case=nist
-%fds% %case%.fds
+echo.
+echo blodget
+%fds% blodget.fds 
 
-set case=sugarloaf
-%fds% %case%.fds
+echo.
+echo NIST
+%fds% nist.fds 
 
-set case=trails
-%fds% %case%.fds
+echo.
+echo tower
+%fds% tower.fds 
 
+echo.
+echo sugarloaf
+%fds% sugarloaf.fds 
+
+echo.
+echo trails
+%fds% trails.fds 
