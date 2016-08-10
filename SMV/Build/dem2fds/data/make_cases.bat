8627bb02a77aa62e4251369be140c8abfcba1d7e
gforney <gforney@gmail.com>
2016-08-09 21:34:05 -0400

    dem2fds vv: update script that generates input files

--- SMV/Build/dem2fds/data/make_cases.bat
+++ SMV/Build/dem2fds/data/make_cases.bat
@@ -1,12 +1,5 @@
 @echo off
-:: generate terrain with &GEOM
-:: set option=-g
-
-:: generate terrain with &OBST
-set option=-o
-
-:: generate terrain with &OBST and examine map images
-:: set option=-e -o
+set option=%1 %2 
 
 set dem2fds=..\intel_win_64\dem2fds_win_64.exe
 ::set dem2fds=dem2fds
