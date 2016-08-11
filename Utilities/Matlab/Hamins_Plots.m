0a763f28646d22c52afabdd652a081d9aaaf3371
mcgratta <mcgratta@gmail.com>
2016-08-11 09:44:25 -0400

    FDS Validation: Add supplemental files for new Hamins_Gas_Burners case.
    Former-commit-id: f54ae1c8eb5992cd3cd2ac217889d1be3c8856e0 [formerly 5621d5fc881d37d9bc9ee3bf6363fdf7a5caf427]
    Former-commit-id: 7214b66ab0310a9af5101703fe68cee20e6f51b4
    Former-commit-id: 0e5a0fee9dc1e13e59612dcd775fd90e3a7cc161

--- Utilities/Matlab/Hamins_Plots.m
+++ Utilities/Matlab/Hamins_Plots.m
@@ -16,12 +16,32 @@ clear all
 
 addpath 'scripts'
 
+%layer_height
+
 Dataplot_Inputs_File = [pwd,'/FDS_validation_dataplot_inputs.csv'];
 Working_Dir = [pwd, '/../../Validation/'];
 Manuals_Dir = [pwd, '/../../Manuals/'];
 
+Scatterplot_Inputs_File = [pwd, '/FDS_validation_scatterplot_inputs.csv'];
+Stats_Output = 'Validation';
+Output_File = [pwd, '/FDS_validation_scatterplot_output.csv'];
+Statistics_Tex_Output = [pwd, '/../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_statistics.tex'];
+Histogram_Tex_Output = [pwd, '/../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_histograms.tex'];
+NRC_Options = false;
+Append_To_Scatterplot_Title = '';
+
 % The relevant rows from FDS_validation_dataplot_inputs.csv are the last arguments of the following call:
 
-[saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir,[2664:2828]);
+[saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir,'Hamins Gas Burners');
      
+scatplot(saved_data, drange, ...
+         'Manuals_Dir', Manuals_Dir, ...
+         'Scatterplot_Inputs_File', Scatterplot_Inputs_File, ...
+         'Stats_Output', Stats_Output, ...
+         'Output_File', Output_File, ...
+         'Statistics_Tex_Output', Statistics_Tex_Output, ...
+         'Histogram_Tex_Output', Histogram_Tex_Output, ...
+         'NRC_Options', NRC_Options, ...
+         'Append_To_Scatterplot_Title', Append_To_Scatterplot_Title)
+
 display('Hamins_Plots completed successfully!')
