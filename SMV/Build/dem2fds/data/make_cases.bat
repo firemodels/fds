ce28ca0c78d960c9ebe8c51ec3b8a348c60bc1e2
gforney <gforney@gmail.com>
2016-08-11 19:58:18 -0400

    dem2fds vv: update make_cases batch file

--- SMV/Build/dem2fds/data/make_cases.bat
+++ SMV/Build/dem2fds/data/make_cases.bat
@@ -1,33 +1,31 @@
 @echo off
-set option=%1 %2 
-if x%option% NEQ x goto skip
-set option=-obst
-:skip
+set option=%1
+set option2=%2
 
 set dem2fds=..\intel_win_64\dem2fds_win_64.exe
 ::set dem2fds=dem2fds
 
 echo.
 echo demtest
-%dem2fds% %option% -nobuffer -dir %userprofile%\terrain\demtest demtest1.in 
-%dem2fds% %option% -nobuffer -dir %userprofile%\terrain\demtest demtest2.in 
+%dem2fds% %option% %option2% -nobuffer -dir %userprofile%\terrain\demtest demtest1.in 
+%dem2fds% %option% %option2% -nobuffer -dir %userprofile%\terrain\demtest demtest2.in 
 
 echo.
 echo blodget
-%dem2fds% %option% -dir %userprofile%\terrain\blodget blodget.in 
+%dem2fds% %option% %option2% -dir %userprofile%\terrain\blodget blodget.in 
 
 echo.
 echo NIST
-%dem2fds% %option% -nobuffer -dir %userprofile%\terrain\nist nist.in 
+%dem2fds% %option% %option2% -nobuffer -dir %userprofile%\terrain\nist nist.in 
 
 echo.
 echo tower
-%dem2fds% %option% -dir %userprofile%\terrain\tower tower.in 
+%dem2fds% %option% %option2% -dir %userprofile%\terrain\tower tower.in 
 
 echo.
 echo sugarloaf
-%dem2fds% %option% -nobuffer -dir %userprofile%\terrain\sugarloaf sugarloaf.in 
+%dem2fds% %option% %option2% -nobuffer -dir %userprofile%\terrain\sugarloaf sugarloaf.in 
 
 echo.
 echo trails
-%dem2fds% %option% -dir %userprofile%\terrain\trails trails.in 
+%dem2fds% %option% %option2% -dir %userprofile%\terrain\trails trails.in 
