0ac39ee35dfbbd9dd3dc90416fdfbe70a6160cbe
rmcdermo <randy.mcdermott@gmail.com>
2016-08-11 13:48:21 -0400

    Matlab: upgrades to FDS_validation_script
    Former-commit-id: 63c4e00ed3ba348f69bfb798b6e9fcf586da5bee [formerly 81162cfcb692d2696108a56274fa97a5afbb1734]
    Former-commit-id: cbf1d4f2df104ed56af8e48cbecd8f2069f58b58
    Former-commit-id: ec4949a09a3b355a1f251a416eb759c3043e26c5

--- Utilities/Matlab/FDS_validation_script.m
+++ Utilities/Matlab/FDS_validation_script.m
@@ -31,82 +31,22 @@
 
 function [] = FDS_validation_script(varargin)
 
+close all
+clearvars -except varargin
+
 addpath 'scripts'
 
 % Process variable argument list
 
-skip_to_dataplot=false;
+run_subset=false;
+run_dataplot=true;
 if nargin>0
-    skip_to_dataplot=true;
-else
-    close all
-    clear all
+    run_subset=true;
 end
 
-% Scripts that run prior to dataplot
-
-switch varargin
-    case {'Heskestad Flame Height'}
-        flame_height
-    case {'McCaffrey Plume'}
-        cat_mccaffrey
-    case {'NIST RSE 1994'}
-        NIST_RSE
-    case {'Sippola Aerosol Deposition'}
-        sippola_aerosol_deposition
-    case {'ATF Corridors','FM/SNL','NIST FSE 2008','NIST/NRC','NRCC Smoke Tower','PRISME','UL/NIST Vents','VTT','WTC'}
-        layer_height
-    case {'CSIRO Grassland Fires'}
-        combine_csiro
-    case {'FM Datacenter'}
-        fm_datacenter_scatter
-    otherwise
-        flame_height
-        cat_mccaffrey
-        NIST_RSE
-        sippola_aerosol_deposition
-        layer_height
-        combine_csiro
-        fm_datacenter_scatter
-end
-
-% Dataplot and scatplot options
-
-Dataplot_Inputs_File = [pwd,'/FDS_validation_dataplot_inputs.csv'];
-Working_Dir = [pwd, '/../../Validation/'];
-Manuals_Dir = [pwd, '/../../Manuals/'];
-Scatterplot_Inputs_File = [pwd, '/FDS_validation_scatterplot_inputs.csv'];
-
-% Statistics output options
-
-Stats_Output = 'Validation';
-Output_File = [pwd, '/FDS_validation_scatterplot_output.csv'];
-Statistics_Tex_Output = [pwd, '/../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_statistics.tex'];
-Histogram_Tex_Output = [pwd, '/../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_histograms.tex'];
-
-% Override the plot style options with NRC 1824 plot options
-
-NRC_Options = false;
-Append_To_Scatterplot_Title = '';
-
-% Run dataplot and scatplot scripts
-
-varargin
-
-[saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir, varargin);
-scatplot(saved_data, drange, ...
-         'Manuals_Dir', Manuals_Dir, ...
-         'Scatterplot_Inputs_File', Scatterplot_Inputs_File, ...
-         'Stats_Output', Stats_Output, ...
-         'Output_File', Output_File, ...
-         'Statistics_Tex_Output', Statistics_Tex_Output, ...
-         'Histogram_Tex_Output', Histogram_Tex_Output, ...
-         'NRC_Options', NRC_Options, ...
-         'Append_To_Scatterplot_Title', Append_To_Scatterplot_Title)
-     
 % Miscellaneous other scripts for special cases
 
-if ~skip_to_dataplot
+if ~run_subset % subset_if_1
     backward_facing_step
     beyler_hood
     sandia_helium_plume
@@ -120,6 +60,87 @@ if ~skip_to_dataplot
     VTT_Sprays
     fm_datacenter_veltest
     umd_line_burner
-end
+else
+    switch varargin{1}
+        case {'backward_facing_step'};  backward_facing_step;  run_dataplot=false;
+        case {'beyler_hood'}         ;  beyler_hood;           run_dataplot=false;
+        case {'sandia_helium_plume'};   sandia_helium_plume;   run_dataplot=false;
+        case {'sandia_methane_fire'};   sandia_methane_fire;   run_dataplot=false;
+        case {'spray_attenuation'};     spray_attenuation;     run_dataplot=false;
+        case {'Cup_burner'};            Cup_burner;            run_dataplot=false;
+        case {'flame_height2'};         flame_height2;         run_dataplot=false;
+        case {'purdue_flames'};         purdue_flames;         run_dataplot=false;
+        case {'christifire'};           christifire;           run_dataplot=false;
+        case {'pressure_coefficient'};  pressure_coefficient;  run_dataplot=false;
+        case {'VTT_Sprays'};            VTT_Sprays;            run_dataplot=false;
+        case {'fm_datacenter_veltest'}; fm_datacenter_veltest; run_dataplot=false;
+        case {'umd_line_burner'};       umd_line_burner;       run_dataplot=false;
+    end
+end % subset_if_1
+
+if run_dataplot % dataplot_if
+
+    % Scripts that run prior to dataplot
+
+    if ~run_subset % subset_if_2
+        flame_height
+        cat_mccaffrey
+        NIST_RSE
+        sippola_aerosol_deposition
+        layer_height
+        combine_csiro
+        fm_datacenter_scatter
+    else
+        switch varargin{1}
+            case {'Heskestad Flame Height'}
+                flame_height
+            case {'McCaffrey Plume'}
+                cat_mccaffrey
+            case {'NIST RSE 1994'}
+                NIST_RSE
+            case {'Sippola Aerosol Deposition'}
+                sippola_aerosol_deposition
+            case {'ATF Corridors','FM/SNL','NIST FSE 2008','NIST/NRC','NRCC Smoke Tower','PRISME','UL/NIST Vents','VTT','WTC'}
+                layer_height
+            case {'CSIRO Grassland Fires'}
+                combine_csiro
+            case {'FM Datacenter'}
+                fm_datacenter_scatter
+        end
+    end % subset_if_2
+
+    % Dataplot and scatplot options
+
+    Dataplot_Inputs_File = [pwd,'/FDS_validation_dataplot_inputs.csv'];
+    Working_Dir = [pwd, '/../../Validation/'];
+    Manuals_Dir = [pwd, '/../../Manuals/'];
+    Scatterplot_Inputs_File = [pwd, '/FDS_validation_scatterplot_inputs.csv'];
+
+    % Statistics output options
+
+    Stats_Output = 'Validation';
+    Output_File = [pwd, '/FDS_validation_scatterplot_output.csv'];
+    Statistics_Tex_Output = [pwd, '/../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_statistics.tex'];
+    Histogram_Tex_Output = [pwd, '/../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/ScatterPlots/validation_histograms.tex'];
+
+    % Override the plot style options with NRC 1824 plot options
+
+    NRC_Options = false;
+    Append_To_Scatterplot_Title = '';
+
+    % Run dataplot and scatplot scripts
+
+    [saved_data,drange] = dataplot(Dataplot_Inputs_File, Working_Dir, Manuals_Dir, varargin);
+    scatplot(saved_data, drange, ...
+             'Manuals_Dir', Manuals_Dir, ...
+             'Scatterplot_Inputs_File', Scatterplot_Inputs_File, ...
+             'Stats_Output', Stats_Output, ...
+             'Output_File', Output_File, ...
+             'Statistics_Tex_Output', Statistics_Tex_Output, ...
+             'Histogram_Tex_Output', Histogram_Tex_Output, ...
+             'NRC_Options', NRC_Options, ...
+             'Append_To_Scatterplot_Title', Append_To_Scatterplot_Title)
+
+end  % dataplot_if
 
 display('validation scripts completed successfully!')
