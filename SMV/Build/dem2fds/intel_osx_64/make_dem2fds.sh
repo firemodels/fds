355dfbad352afaedf9b3b89ece3c9ec105a73de7
gforney <gforney@gmail.com>
2016-08-25 13:52:19 -0400

    dem2fds build: build libraries if not available

--- SMV/Build/dem2fds/intel_osx_64/make_dem2fds.sh
+++ SMV/Build/dem2fds/intel_osx_64/make_dem2fds.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source $IFORT_COMPILER/bin/compilervars.sh intel64
+source ../../scripts/setopts.sh $*
+LIBDIR=../../LIBS/intel_osx_64/
+source ../../scripts/test_libs.sh
 
 rm -f *.o
 make -f ../Makefile intel_osx_64
