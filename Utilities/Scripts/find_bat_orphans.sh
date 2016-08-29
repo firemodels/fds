ef7c5460cc847dbb2683ac686e76052c5e9bc455
gforney <gforney@gmail.com>
2016-08-29 14:51:34 -0400

    reorg: add a script to look for bat orphans
    Former-commit-id: 2067dce6ba0833187f91f63cc853103b0129ba78

---
+++ Utilities/Scripts/find_bat_orphans.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+script=$1
+
+CURDIR=`pwd`
+SVNROOT=`pwd`/../../..
+cd $SVNROOT
+SVNROOT=`pwd`
+
+cd $SVNROOT/FDS
+FDSROOT=`pwd`
+
+cd $SVNROOT/SMV
+SMVROOT=`pwd`
+
+echo looking for $script in FDS repo
+grep $script $FDSROOT/Build/*.html $FDSROOT/Utilities/*.html $FDSROOT/Build/*/*.bat $FDSROOT/Build/Scripts/*.bat $FDSROOT/Utilities/Firebot/*.bat $FDSROOT/Utilities/Scripts/*.bat $FDSROOT/Verification/scripts/*.bat
+
+echo looking for $script in SMV repo
+grep $script $SMVROOT/Build/*.html $SMVROOT/Build/*/*.bat $SMVROOT/Build/Scripts/*.bat $SMVROOT/Utilities/Firebot/*.bat $SMVROOT/Utilities/Scripts/*.bat $SMVROOT/Verification/scripts/*.bat
