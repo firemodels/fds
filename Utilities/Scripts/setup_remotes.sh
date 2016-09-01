c9d859d28854fd9dc29ace41495818d99b5579c1
gforney <gforney@gmail.com>
2016-09-01 08:30:50 -0400

    reorg: create scripts for downloading and setting up remotes for new repos

--- Utilities/Scripts/setup_remotes.sh
+++ Utilities/Scripts/setup_remotes.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
-cd cor
+
+function usage {
+echo "Setup remote connections with each firemodels repo"
+echo ""
+echo "Options:"
+echo "-h - display this message"
+exit
+}
+
+while getopts 'h' OPTION
+do
+case $OPTION  in
+  h)
+   usage;
+   ;;
+esac
+done
+shift $(($OPTIND-1))
+
+echo "You are about to setup remotes for the repos:"
+echo "cor, exp, out, smv, fds and radcal at "
+echo "git@github.com:firemodels"
+echo "Press any key to continue or <CTRL> c to abort."
+echo "You should only run this command if you have cloned these" 
+echo "repos from repos that have been forked from firemodels"
+read val
+
+CURDIR=`pwd`
+cd $CURDIR/cor
 git remote add firemodels git@github.com:firemodels/cor.git
 git remote update
 
-cd ../exp
+cd $CURDIR/exp
 git remote add firemodels git@github.com:firemodels/exp.git
 git remote update
 
-cd ../out
+cd $CURDIR/out
 git remote add firemodels git@github.com:firemodels/out.git
 git remote update
 
-cd ../smv
+cd $CURDIR/smv
 git remote add firemodels git@github.com:firemodels/smv.git
 git remote update
 
-cd ../fds
+cd $CURDIR/fds
 git remote add firemodels git@github.com:firemodels/fds.git
 git remote update
 
-cd ../radcal
+cd $CURDIR/radcal
 git remote add firemodels git@github.com:firemodels/radcal.git
 git remote update
+
+cd $CURDIR
