fedabf2261469e24bfac2e2e72fe130bfd8c6fc3
gforney <gforney@gmail.com>
2016-09-07 12:48:51 -0400

    scripts: update master instead development when updating repos

--- Utilities/Scripts/UPDATE_thishost.sh
+++ Utilities/Scripts/UPDATE_thishost.sh
@@ -6,8 +6,8 @@ host=$2
 echo Updating the GIT repository $directory on $host to the latest revision
 echo
 cd ~/$directory
-git checkout development
+git checkout master
 git remote update
-git merge origin/development
-git merge firemodels/development 
+git merge origin/master
+git merge firemodels/master 
 git describe --dirty 
