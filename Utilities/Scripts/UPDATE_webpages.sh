b449a3a017b4c5a393d523c6e7b4408c7bf67f59
gforney <gforney@gmail.com>
2016-09-21 09:54:46 -0400

    bundle scripts: updates to repo update scripts

--- Utilities/Scripts/UPDATE_webpages.sh
+++ Utilities/Scripts/UPDATE_webpages.sh
@@ -6,6 +6,8 @@ host=$2
 echo Updating the GIT repository $directory on $host to the latest revision
 echo
 cd ~/$directory
+
+git checkout nist-pages
 git remote update
 git merge origin/nist-pages
 git describe --dirty 
