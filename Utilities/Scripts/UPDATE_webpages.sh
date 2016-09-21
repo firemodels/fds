f2819345d81a18d7a73acf9d982eeb2f26725c4b
gforney <gforney@gmail.com>
2016-09-21 09:38:52 -0400

    bundle scripts: add script to update web page repos

---
+++ Utilities/Scripts/UPDATE_webpages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+directory=$1
+host=$2
+
+echo Updating the GIT repository $directory on $host to the latest revision
+echo
+cd ~/$directory
+git remote update
+git merge origin/nist-pages
+git describe --dirty 
