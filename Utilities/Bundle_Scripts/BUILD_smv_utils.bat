d43e8fc49c7b240fbbd769831540c2e7d76a6eb9
gforney <gforney@gmail.com>
2016-10-09 20:12:30 -0400

    bundle scripts: rename some scripts

--- Utilities/Bundle_Scripts/BUILD_smv_utils.bat
+++ Utilities/Bundle_Scripts/BUILD_smv_utils.bat
@@ -1,8 +1,8 @@
 @echo off
 
-call BUILD_prog_all_platforms background
-call BUILD_prog_all_platforms dem2fds
-call BUILD_prog_all_platforms fds2ascii
-call BUILD_prog_all_platforms smokediff
-call BUILD_prog_all_platforms smokezip
-call BUILD_prog_all_platforms wind2fds
+call BUILD_prog background
+call BUILD_prog dem2fds
+call BUILD_prog fds2ascii
+call BUILD_prog smokediff
+call BUILD_prog smokezip
+call BUILD_prog wind2fds
