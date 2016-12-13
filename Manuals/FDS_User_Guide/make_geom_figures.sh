9c0d788ce093ea0fb823137155cc76145eef8511
gforney <gforney@gmail.com>
2016-12-12 21:18:12 -0500

    geom vv: corrections to scripts that builds geom test cases and geom_notes

--- Manuals/FDS_User_Guide/make_geom_figures.sh
+++ Manuals/FDS_User_Guide/make_geom_figures.sh
@@ -21,11 +21,11 @@ esac
 done
 shift $(($OPTIND-1))
 
-cd ../../Verification/scripts
+cd ../../../smv/Verification/scripts
 ./Run_SMV_Cases.sh -g  -w -j GM_ $QUEUE
 
 cd $CURDIR
-cd ../../Verification/scripts
+cd ../../../smv/Verification/scripts
 ./Make_SMV_Pictures.sh -g
 cd $CURDIR
 
