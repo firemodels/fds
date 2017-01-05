4000a417b151e85f1a983862d27285af20738f41
rmcdermo <randy.mcdermott@gmail.com>
2017-01-05 17:41:57 -0500

    Matlab: revert adding position scripts

--- Utilities/Matlab/scripts/dataplot_position.m
+++
@@ -1,5 +0,0 @@
-% set paper size and position
-set(gcf,'Visible',Figure_Visibility);
-set(gcf,'Units',Paper_Units);
-set(gcf,'PaperSize',[Paper_Width Paper_Height]);
-set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
--- Utilities/Matlab/scripts/scatplot_position.m
+++
@@ -1,5 +0,0 @@
-% set paper size and position
-set(gcf,'Visible',Figure_Visibility);
-set(gcf,'Units',Paper_Units);
-set(gcf,'PaperSize',[Scat_Paper_Width Scat_Paper_Height]);
-set(gcf,'Position',[0 0 Scat_Paper_Width Scat_Paper_Height]);
