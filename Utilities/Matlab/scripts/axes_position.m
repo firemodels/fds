6d3fd689f8fafe89ede8b5f5fbb8af38abcacd38
rmcdermo <randy.mcdermott@gmail.com>
2017-01-05 17:25:02 -0500

    Matlab: fix plot position for backward_facing_step.m

---
+++ Utilities/Matlab/scripts/axes_position.m
@@ -0,0 +1,3 @@
+% set axes position
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
---
+++ Utilities/Matlab/scripts/plot_position.m
@@ -0,0 +1,5 @@
+% set paper size and position
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'Units',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
