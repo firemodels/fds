1a7803156bfa3250041ecd25402c553830875942
rmcdermo <randy.mcdermott@gmail.com>
2017-01-05 17:41:57 -0500

    Matlab: revert adding position scripts

--- Utilities/Matlab/scripts/dataaxes_position.m
+++
@@ -1,3 +0,0 @@
-% set axes position
-set(gca,'Units',Plot_Units)
-set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
--- Utilities/Matlab/scripts/scataxes_position.m
+++
@@ -1,3 +0,0 @@
-% set scatplot axes position
-set(gca,'Units',Plot_Units)
-set(gca,'Position',[Scat_Plot_X Scat_Plot_Y Scat_Plot_Width Scat_Plot_Height])
