b3b2dc3401925a27c397c768e5e24cefa062ed6a
mcgratta <mcgratta@gmail.com>
2017-01-05 13:21:39 -0500

    FDS Validation: Minor changes to LNG_Dispersion cases.

--- Validation/LNG_Dispersion/Run_All.sh
+++ Validation/LNG_Dispersion/Run_All.sh
@@ -5,16 +5,16 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-$QFDS $DEBUG -p 24 $QUEUE -d $INDIR Burro3.fds
-$QFDS $DEBUG -p 24 $QUEUE -d $INDIR Burro7.fds
-$QFDS $DEBUG -p 24 $QUEUE -d $INDIR Burro8.fds
-$QFDS $DEBUG -p 24 $QUEUE -d $INDIR Burro9.fds
+$QFDS $DEBUG -p 30 $QUEUE -d $INDIR Burro3.fds
+$QFDS $DEBUG -p 30 $QUEUE -d $INDIR Burro7.fds
+$QFDS $DEBUG -p 30 $QUEUE -d $INDIR Burro8.fds
+$QFDS $DEBUG -p 30 $QUEUE -d $INDIR Burro9.fds
 $QFDS $DEBUG -p 24 $QUEUE -d $INDIR Coyote3.fds
 $QFDS $DEBUG -p 24 $QUEUE -d $INDIR Coyote5.fds
 $QFDS $DEBUG -p 24 $QUEUE -d $INDIR Coyote6.fds
-$QFDS $DEBUG -p 24 $QUEUE -d $INDIR Falcon1.fds
-$QFDS $DEBUG -p 24 $QUEUE -d $INDIR Falcon3.fds
-$QFDS $DEBUG -p 24 $QUEUE -d $INDIR Falcon4.fds
+$QFDS $DEBUG -p 12 $QUEUE -d $INDIR Falcon1.fds
+$QFDS $DEBUG -p 12 $QUEUE -d $INDIR Falcon3.fds
+$QFDS $DEBUG -p 12 $QUEUE -d $INDIR Falcon4.fds
 $QFDS $DEBUG -p 24 $QUEUE -d $INDIR MaplinSands27.fds
 $QFDS $DEBUG -p 24 $QUEUE -d $INDIR MaplinSands34.fds
 $QFDS $DEBUG -p 24 $QUEUE -d $INDIR MaplinSands35.fds
