1c4dfc6ff1418a273456c29adaced9f298feea51
rmcdermo <randy.mcdermott@gmail.com>
2017-01-10 11:08:34 -0500

    Matlab: add umd_line_burner_2 to FDS_validation_script

--- Utilities/Matlab/scripts/umd_line_burner_2.m
+++ Utilities/Matlab/scripts/umd_line_burner_2.m
@@ -14,17 +14,25 @@ outdir = '../../../out/UMD_Line_Burner/FDS_Output_Files/';
 pltdir = '../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/UMD_Line_Burner/';
 
 
-EXP = importdata([expdir,'XO2_vs_ETA_METHANE.csv'],',',1);
-%HRR = importdata([outdir,'methane_dx_XO2_ramp_dx_p625cm_hrr.csv'],',',2);
+EXP = importdata([expdir,'CH4_A_Data.csv'],',',1);
+HRR = importdata([outdir,'methane_XO2_ramp_1fast_dx_p625cm_hrr.csv'],',',2);
+DEV = importdata([outdir,'methane_XO2_ramp_1fast_dx_p625cm_devc.csv'],',',2);
 
 XO2 = EXP.data(:,find(strcmp(EXP.colheaders,'XO2')));
 eta = EXP.data(:,find(strcmp(EXP.colheaders,'eta')));
+
+XO2_FDS = DEV.data(:,find(strcmp(DEV.colheaders,'"XO2"')));
+eta_FDS = HRR.data(:,find(strcmp(HRR.colheaders,'HRR')));
+MLR_FDS = HRR.data(:,find(strcmp(HRR.colheaders,'MLR_FUEL')));
+HOC = 50010.3475; % kJ/kg-CH4 from .out file
+eta_FDS = eta_FDS./(MLR_FDS*HOC);
 
 figure
 set(gca,'Units',Plot_Units)
 set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
 
-H(1)=plot(XO2,eta,'k.','MarkerSize',10);
+H(1)=plot(XO2,eta,'b.','MarkerSize',10); hold on
+H(2)=plot(XO2_FDS,eta_FDS,'k-');
 axis([0.09  0.21 0 1.1 ])
 xlabel('{\itX}_{O2}','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
 ylabel('\eta','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
@@ -32,7 +40,7 @@ ylabel('\eta','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Label_Font_Size)
 
-lh=legend(H,'Exp','Location','NorthWest');
+lh=legend(H,'Exp','FDS','Location','NorthWest');
 set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
 
 set(gcf,'Visible',Figure_Visibility);
