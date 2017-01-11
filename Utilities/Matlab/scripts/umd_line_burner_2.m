bac1df828d63995324a315ccdbde92e563eb9b0e
rmcdermo <randy.mcdermott@gmail.com>
2017-01-11 15:30:02 -0500

    Matlab: add high res case to umd_line_burner_2.m

--- Utilities/Matlab/scripts/umd_line_burner_2.m
+++ Utilities/Matlab/scripts/umd_line_burner_2.m
@@ -17,15 +17,14 @@ pltdir = '../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/UMD_Line_Burner/';
 
 % experimental results
 EXP = importdata([expdir,'CH4_A_Data.csv'],',',1);
-
-% dx=0.625 cm results
-HRR = importdata([outdir,'methane_XO2_ramp_dx_p625cm_hrr.csv'],',',2);
-DEV = importdata([outdir,'methane_XO2_ramp_dx_p625cm_devc.csv'],',',2);
-
 XO2   = EXP.data(:,find(strcmp(EXP.colheaders,'XO2')));
 S_XO2 = EXP.data(:,find(strcmp(EXP.colheaders,'S_XO2'))); % uncertainty
 eta   = EXP.data(:,find(strcmp(EXP.colheaders,'eta')));
 S_eta = EXP.data(:,find(strcmp(EXP.colheaders,'S_eta')));
+
+% dx=0.625 cm results
+HRR = importdata([outdir,'methane_XO2_ramp_dx_p625cm_hrr.csv'],',',2);
+DEV = importdata([outdir,'methane_XO2_ramp_dx_p625cm_devc.csv'],',',2);
 
 XO2_FDS = DEV.data(:,find(strcmp(DEV.colheaders,'"XO2"')));
 eta_FDS = HRR.data(:,find(strcmp(HRR.colheaders,'HRR')));
@@ -41,6 +40,15 @@ eta_FDS_2 = HRR2.data(:,find(strcmp(HRR2.colheaders,'HRR')));
 MLR_FDS_2 = HRR2.data(:,find(strcmp(HRR2.colheaders,'MLR_FUEL')));
 eta_FDS_2 = eta_FDS_2./(MLR_FDS_2*CH4_HOC);
 
+% dx=0.3125 cm results
+HRR3 = importdata([outdir,'methane_XO2_ramp_dx_p3125cm_hrr.csv'],',',2);
+DEV3 = importdata([outdir,'methane_XO2_ramp_dx_p3125cm_devc.csv'],',',2);
+
+XO2_FDS_3 = DEV3.data(:,find(strcmp(DEV3.colheaders,'"XO2"')));
+eta_FDS_3 = HRR3.data(:,find(strcmp(HRR3.colheaders,'HRR')));
+MLR_FDS_3 = HRR3.data(:,find(strcmp(HRR3.colheaders,'MLR_FUEL')));
+eta_FDS_3 = eta_FDS_3./(MLR_FDS_3*CH4_HOC);
+
 figure
 set(gca,'Units',Plot_Units)
 set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
@@ -51,8 +59,9 @@ set(H(1),'Color',steel_blue)
 subr= 1:10:length(XO2);
 h=errorbar(XO2(subr),eta(subr),-S_eta(subr),S_eta(subr),-S_XO2(subr),S_XO2(subr),'.','MarkerSize',10); hold on
 set(h,'Color',steel_blue)
-H(2)=plot(XO2_FDS_2,eta_FDS_2,'k--');
-H(3)=plot(XO2_FDS,eta_FDS,'k-');
+H(2)=plot(XO2_FDS_2,eta_FDS_2,'k-.');
+H(3)=plot(XO2_FDS,eta_FDS,'k--');
+H(4)=plot(XO2_FDS_3,eta_FDS_3,'k-');
 axis([0.09 0.21 0 1.1 ])
 xlabel('{\itX}_{O2}','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
 ylabel('\eta','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
@@ -60,7 +69,7 @@ ylabel('\eta','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Label_Font_Size)
 
-lh=legend(H,'Exp','FDS {\it\deltax}=1.25 cm','FDS {\it\deltax}=0.625 cm','Location','SouthEast');
+lh=legend(H,'Exp','FDS {\it\deltax}=12.5 mm','FDS {\it\deltax}=6.25 mm','FDS {\it\deltax}=3.125 mm','Location','SouthEast');
 set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
 
 set(gcf,'Visible',Figure_Visibility);
