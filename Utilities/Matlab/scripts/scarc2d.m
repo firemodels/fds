1c74bfe1d70f7d77b5c0307aa3c67f80f8a4e219
rmcdermo <randy.mcdermott@gmail.com>
2017-01-17 05:43:03 -0500

    Matlab: cleanup all svn references in scripts

--- Utilities/Matlab/scripts/scarc2d.m
+++ Utilities/Matlab/scripts/scarc2d.m
@@ -69,17 +69,8 @@ set(legend_handle,'Position',[6.3 1.75 3.7 1.5])
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Title_Font_Size)
 
-SVN_Filename = [data_dir,'scarc2d_cg_8mesh_git.txt'];
-addverstr(gca,SVN_Filename,'linear')
-% if exist(SVN_Filename,'file')
-%     SVN = importdata(SVN_Filename);
-%     x_lim = get(gca,'XLim');
-%     y_lim = get(gca,'YLim');
-%     X_SVN_Position = x_lim(1)+SVN_Scale_X*(x_lim(2)-x_lim(1));
-%     Y_SVN_Position = y_lim(1)+SVN_Scale_Y*(y_lim(2)-y_lim(1));
-%     text(X_SVN_Position,Y_SVN_Position,['SVN ',num2str(SVN)], ...
-%         'FontSize',10,'FontName',Font_Name,'Interpreter',Font_Interpreter)
-% end
+Git_Filename = [data_dir,'scarc2d_cg_8mesh_git.txt'];
+addverstr(gca,Git_Filename,'linear')
 
 % Create the PDF files
 
