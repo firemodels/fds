b78f6fab25e5f2aace52b0bc9091d3c247f0b2dc
rmcdermo <randy.mcdermott@gmail.com>
2017-01-17 14:18:40 -0500

    Matlab: fix visibility pulsating

--- Utilities/Matlab/scripts/pulsating.m
+++ Utilities/Matlab/scripts/pulsating.m
@@ -149,6 +149,7 @@ Git_Filename = [data_dir,'pulsating_FL2_16_git.txt'];
 addverstr(gca,Git_Filename,'linear')
 
 % print to pdf
+set(gcf,'Visible',Figure_Visibility);
 set(gcf,'Units',Paper_Units);
 set(gcf,'PaperSize',[Paper_Width Paper_Height]);
 set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
@@ -187,6 +188,7 @@ Git_Filename = [data_dir,'pulsating_FL2_16_git.txt'];
 addverstr(gca,Git_Filename,'loglog')
 
 % print to pdf
+set(gcf,'Visible',Figure_Visibility);
 set(gcf,'Units',Paper_Units);
 set(gcf,'PaperSize',[Paper_Width Paper_Height]);
 set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
