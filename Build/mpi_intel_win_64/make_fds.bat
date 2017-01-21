61e3e3d6042a7fa603ba6e0d4034a90af8276806
gforney <gforney@gmail.com>
2017-01-21 17:57:38 -0500

    FDS build: add a script to compute an md5 hash and do this when building mpi windows fds

--- Build/mpi_intel_win_64/make_fds.bat
+++ Build/mpi_intel_win_64/make_fds.bat
@@ -1,5 +1,6 @@
 @echo off
 set arg1=%1
+set md5hash=..\..\Utilities\Scripts\md5hash
 
 :: setup compiler environment
 if x%arg1% == xbot goto skip1
@@ -9,6 +10,7 @@ call ..\..\Utilities\Scripts\setup_intel_compilers.bat
 Title Building FDS (mpi) for 64 bit Windows
 
 make SHELL="%ComSpec%" VPATH="../../Source" -f ..\makefile mpi_intel_win_64
+%md5hash% fds_mpi_win_64.exe
 if x%arg1% == xbot goto skip2
 pause
 :skip2
