51d91eb4ce757b8f5c41803e1876f677e15b55c9
gforney <gforney@gmail.com>
2017-01-21 18:34:45 -0500

    FDS build: compute md5 hash when build osx mpi fds

--- Build/mpi_intel_osx_64/make_fds.sh
+++ Build/mpi_intel_osx_64/make_fds.sh
@@ -7,3 +7,4 @@ source ../Scripts/set_mpidist.sh eth $MPIDIST_ETH
 
 echo Building $target
 make -j4 VPATH="../../Source" -f ../makefile $target
+../../Utilities/Scripts/md5hash.sh fds_mpi_intel_osx_64
--- Utilities/Scripts/md5hash.sh
+++ Utilities/Scripts/md5hash.sh
@@ -2,10 +2,14 @@
 filein=$1
 fileout=$filein.md5
 
-notfound=`md5sum -help 2>&1 | tail -1 | grep "not found" | wc -l`
+MD5=md5sum
+if [ "`uname`" == "Darwin" ] ; then
+  MD5=md5
+fi
+notfound=`$MD5 -help 2>&1 | tail -1 | grep "not found" | wc -l`
 if [ "$notfound" == "1" ]; then
   exit
 fi
 if [ -e $filein ]; then
-  md5sum $filein > $fileout
+  $MD5 $filein > $fileout
 fi
