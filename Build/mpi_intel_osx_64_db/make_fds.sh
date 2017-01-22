06c46e9ac73c178b73dd8c297d916e4dc169106d
gforney <gforney@gmail.com>
2017-01-22 08:58:04 -0500

    FDS build: remove md5 hash computation from fds build scripts (do it when we make a bundle)

--- Build/mpi_intel_osx_64_db/make_fds.sh
+++ Build/mpi_intel_osx_64_db/make_fds.sh
@@ -8,4 +8,3 @@ source ../Scripts/set_mpidist.sh eth $MPIDIST_ETH
 
 echo Building $target
 make -j4 VPATH="../../Source" -f ../makefile $target
-../../Utilities/Scripts/md5hash.sh fds_mpi_intel_osx_64_db
