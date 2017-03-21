3c7813723bd2924fdda8f92e5380c714036c7b1c
rmcdermo <randy.mcdermott@gmail.com>
2017-03-21 14:20:40 -0400

    Matlab: add rad heat flux to umd_line_burner_2.m

--- Utilities/Matlab/scripts/umd_line_burner_2.m
+++ Utilities/Matlab/scripts/umd_line_burner_2.m
@@ -18,39 +18,11 @@ pltdir = '../../Manuals/FDS_Validation_Guide/SCRIPT_FIGURES/UMD_Line_Burner/';
 % experimental results
 EXP = importdata([expdir,'CH4_A_Data.csv'],',',1);
 XO2   = EXP.data(:,find(strcmp(EXP.colheaders,'XO2')));
+q_R   = EXP.data(:,find(strcmp(EXP.colheaders,'q_R')));
 S_XO2 = EXP.data(:,find(strcmp(EXP.colheaders,'S_XO2'))); % uncertainty
 eta   = EXP.data(:,find(strcmp(EXP.colheaders,'eta')));
 S_eta = EXP.data(:,find(strcmp(EXP.colheaders,'S_eta')));
 Chi_R = EXP.data(:,find(strcmp(EXP.colheaders,'Chi_R')));
-
-% compute CHI_R ramp
-figure
-set(gca,'Units',Plot_Units)
-set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
-
-X_O2_max = 2.077972E-01;
-X_O2_min = 0.1;
-ramp_time = @(x) (x-X_O2_max)*60./(X_O2_min-X_O2_max); % this comes from linear XO2 ramp (see input file)
-H(1)=plot(ramp_time(XO2),Chi_R); hold on
-pw_ramp_time = [0,37,45,60];
-pw_ramp_chir = [.245,.125,.03,0];
-H(2)=plot(pw_ramp_time,pw_ramp_chir);
-axis([0 60 0 0.30 ])
-xlabel('Time (s)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
-ylabel('\chi_r','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
-text(5,0.27,'Radiative Fraction Ramp','FontName',Font_Name,'FontSize',Title_Font_Size)
-set(gca,'FontName',Font_Name)
-set(gca,'FontSize',Label_Font_Size)
-lh=legend(H,'Exp','Linear Ramp','Location','NorthEast');
-set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
-git_file=[outdir,'methane_XO2_ramp_dx_p625cm_git.txt'];
-addverstr(gca,git_file,'linear');
-
-set(gcf,'Visible',Figure_Visibility);
-set(gcf,'Units',Paper_Units);
-set(gcf,'PaperSize',[Paper_Width Paper_Height]);
-set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
-print(gcf,'-dpdf',[pltdir,'methane_Chi_r_ramp_check']);
 
 % dx=0.625 cm results
 HRR = importdata([outdir,'methane_XO2_ramp_dx_p625cm_hrr.csv'],',',2);
@@ -58,6 +30,7 @@ DEV = importdata([outdir,'methane_XO2_ramp_dx_p625cm_devc.csv'],',',2);
 
 Time_FDS = DEV.data(:,find(strcmp(DEV.colheaders,'Time')));
 XO2_FDS = DEV.data(:,find(strcmp(DEV.colheaders,'"XO2"')));
+q_R_FDS = 0.5*( DEV.data(:,find(strcmp(DEV.colheaders,'"qrad1"'))) + DEV.data(:,find(strcmp(DEV.colheaders,'"qrad2"'))) );
 eta_FDS = HRR.data(:,find(strcmp(HRR.colheaders,'HRR')));
 MLR_FDS = HRR.data(:,find(strcmp(HRR.colheaders,'MLR_FUEL')));
 eta_FDS = eta_FDS./(MLR_FDS*CH4_HOC);
@@ -68,6 +41,8 @@ DEV2 = importdata([outdir,'methane_XO2_ramp_dx_1p25cm_devc.csv'],',',2);
 
 Time_FDS_2 = DEV2.data(:,find(strcmp(DEV2.colheaders,'Time')));
 XO2_FDS_2 = DEV2.data(:,find(strcmp(DEV2.colheaders,'"XO2"')));
+CHI_R_FDS_2 = DEV2.data(:,find(strcmp(DEV2.colheaders,'"CHI_R"')));
+q_R_FDS_2 = 0.5*( DEV2.data(:,find(strcmp(DEV2.colheaders,'"qrad1"'))) + DEV2.data(:,find(strcmp(DEV2.colheaders,'"qrad2"'))) );
 eta_FDS_2 = HRR2.data(:,find(strcmp(HRR2.colheaders,'HRR')));
 MLR_FDS_2 = HRR2.data(:,find(strcmp(HRR2.colheaders,'MLR_FUEL')));
 eta_FDS_2 = eta_FDS_2./(MLR_FDS_2*CH4_HOC);
@@ -77,9 +52,41 @@ HRR3 = importdata([outdir,'methane_XO2_ramp_dx_p3125cm_hrr.csv'],',',2);
 DEV3 = importdata([outdir,'methane_XO2_ramp_dx_p3125cm_devc.csv'],',',2);
 
 XO2_FDS_3 = DEV3.data(:,find(strcmp(DEV3.colheaders,'"XO2"')));
+q_R_FDS_3 = 0.5*( DEV3.data(:,find(strcmp(DEV3.colheaders,'"qrad1"'))) + DEV3.data(:,find(strcmp(DEV3.colheaders,'"qrad2"'))) );
 eta_FDS_3 = HRR3.data(:,find(strcmp(HRR3.colheaders,'HRR')));
 MLR_FDS_3 = HRR3.data(:,find(strcmp(HRR3.colheaders,'MLR_FUEL')));
 eta_FDS_3 = eta_FDS_3./(MLR_FDS_3*CH4_HOC);
+
+% compute CHI_R ramp
+figure
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+
+X_O2_max = 2.077972E-01;
+X_O2_min = 0.1;
+ramp_time = @(x) (x-X_O2_max)*60./(X_O2_min-X_O2_max); % this comes from linear XO2 ramp (see input file)
+H(1)=plot(XO2,Chi_R); hold on
+pw_ramp_time = [0,37,45,60];
+pw_ramp_XO2  = [0.21,0.142,0.123,0];
+pw_ramp_chir = [.245,.125,0,0];
+H(2)=plot(pw_ramp_XO2,pw_ramp_chir);
+H(3)=plot(XO2_FDS_2,CHI_R_FDS_2,'ksq');
+axis([0.12 0.22 0 0.30 ])
+xlabel('X_{O2}','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+ylabel('\chi_R','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+text(5,0.27,'Radiative Fraction Ramp','FontName',Font_Name,'FontSize',Title_Font_Size)
+set(gca,'FontName',Font_Name)
+set(gca,'FontSize',Label_Font_Size)
+lh=legend(H,'Exp','Linear Ramp','FDS dx=1.25cm','Location','NorthWest');
+set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
+git_file=[outdir,'methane_XO2_ramp_dx_1p25cm_git.txt'];
+addverstr(gca,git_file,'linear');
+
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'Units',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf',[pltdir,'methane_Chi_r_ramp_check']);
 
 figure
 clear H
@@ -143,9 +150,31 @@ set(gcf,'PaperSize',[Paper_Width Paper_Height]);
 set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
 print(gcf,'-dpdf',[pltdir,'methane_N2_ramp_check']);
 
+% plot heat flux
+figure
+clear H
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+H(1)=plot(XO2,q_R,'o'); hold on
+H(2)=plot(XO2_FDS_2,q_R_FDS_2,'-');
+H(3)=plot(XO2_FDS  ,q_R_FDS  ,'-');
+H(4)=plot(XO2_FDS_3,q_R_FDS_3,'-');
+axis([0.12 0.21 0 1.4])
+xlabel('{\itX}_{O2}','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+ylabel('{\itq"}_R (kW/m^2)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+set(gca,'FontName',Font_Name)
+set(gca,'FontSize',Label_Font_Size)
+lh=legend(H,'Exp','FDS dx=1.25cm','FDS dx=.625cm','FDS dx=.3125cm','Location','NorthWest');
+set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
 
+git_file=[outdir,'methane_XO2_ramp_dx_1p25cm_git.txt'];
+addverstr(gca,git_file,'linear');
 
-
+set(gcf,'Visible',Figure_Visibility);
+set(gcf,'Units',Paper_Units);
+set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+print(gcf,'-dpdf',[pltdir,'methane_rad_heat_flux']);
 
 
 
