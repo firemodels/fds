78524df3c31b0edaaf7212f99c8fc06e814a6d43
gforney <gforney@gmail.com>
2017-04-08 22:54:44 -0400

    geom_test build: corrections

--- Utilities/geom_test/intel_win_64/make_geomtest.bat
+++ Utilities/geom_test/intel_win_64/make_geomtest.bat
@@ -3,8 +3,8 @@
 :: setup compiler environment
 call ..\..\..\Utilities\Scripts\setup_intel_compilers.bat
 
-Title Building FDS for 64 bit Windows
+Title Building geom_test for 64 bit Windows
 
-make -j 4 SHELL="%ComSpec%" VPATH="../../../Source;../Source" -f ..\makefile intel_win_64
+make SHELL="%ComSpec%" VPATH="../../../Source:../Source" -f ..\makefile intel_win_64
 pause
 
