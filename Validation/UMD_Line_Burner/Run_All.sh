c9edcdeb1e9e15f88b33dcc03af3167c65f71333
rmcdermo <randy.mcdermott@gmail.com>
2017-05-18 10:36:45 -0400

    FDS Validation: add RTE_SOURCE_CORRECTION=F to UMD_Line_Burner

--- Validation/UMD_Line_Burner/Run_All.sh
+++ Validation/UMD_Line_Burner/Run_All.sh
@@ -11,21 +11,20 @@ $QFDS $DEBUG -p 16 $QUEUE -d $INDIR methane_dx_p625cm.fds
 # $QFDS $DEBUG -p 128 -n 8 $QUEUE -d $INDIR methane_dx_p3125cm.fds
 
 # extinction cases
-# $QFDS $DEBUG -p 38 -n 6 $QUEUE -d $INDIR methane_XO2_ramp_dx_1p25cm.fds
-# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_XO2_ramp_dx_p625cm.fds
-# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_XO2_ramp_dx_p3125cm.fds
 
-# $QFDS $DEBUG -p 38 -n 6 $QUEUE -d $INDIR propane_XO2_ramp_dx_1p25cm.fds
-# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR propane_XO2_ramp_dx_p625cm.fds
-# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR propane_XO2_ramp_dx_p3125cm.fds
-
-# 2 step extinction cases with CO prediction
+$QFDS $DEBUG -p 38 -n 6 $QUEUE -d $INDIR methane_XO2_ramp_dx_1p25cm.fds
+$QFDS $DEBUG -p 38 -n 6 $QUEUE -d $INDIR propane_XO2_ramp_dx_1p25cm.fds
 # $QFDS $DEBUG -p 38 -n 6 $QUEUE -d $INDIR methane_2step_XO2_ramp_dx_1p25cm.fds
-# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_2step_XO2_ramp_dx_p625cm.fds
-# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_2step_XO2_ramp_dx_p3125cm.fds
-
 # $QFDS $DEBUG -p 38 -n 6 $QUEUE -d $INDIR propane_2step_XO2_ramp_dx_1p25cm.fds
+
+$QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_XO2_ramp_dx_p625cm.fds
+$QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR propane_XO2_ramp_dx_p625cm.fds
+# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_2step_XO2_ramp_dx_p625cm.fds
 # $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR propane_2step_XO2_ramp_dx_p625cm.fds
+
+# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_XO2_ramp_dx_p3125cm.fds
+# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR propane_XO2_ramp_dx_p3125cm.fds
+# $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR methane_2step_XO2_ramp_dx_p3125cm.fds
 # $QFDS $DEBUG -p 184 -n 6 $QUEUE -d $INDIR propane_2step_XO2_ramp_dx_p3125cm.fds
 
 echo FDS cases submitted
