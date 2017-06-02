7ade06486c3dfb7e970c2b5d65ab8eddbf62dee6
rmcdermo <randy.mcdermott@gmail.com>
2017-06-02 16:12:18 -0400

    FDS Validation: extend domain for new McCaffrey input files

--- Validation/McCaffrey_Plume/Run_All.sh
+++ Validation/McCaffrey_Plume/Run_All.sh
@@ -5,11 +5,11 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_14_kW.fds 
-$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_22_kW.fds 
-$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_33_kW.fds 
-$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_45_kW.fds 
-$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_57_kW.fds 
+$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_14_kW.fds
+$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_22_kW.fds
+$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_33_kW.fds
+$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_45_kW.fds
+$QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_57_kW.fds
 
 $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_14_kW_coarse.fds
 $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_22_kW_coarse.fds
@@ -23,22 +23,22 @@ $QFDS $DEBUG -p 4 $QUEUE -d $INDIR McCaffrey_33_kW_fine.fds
 $QFDS $DEBUG -p 4 $QUEUE -d $INDIR McCaffrey_45_kW_fine.fds
 $QFDS $DEBUG -p 4 $QUEUE -d $INDIR McCaffrey_57_kW_fine.fds
 
-# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_14_kW_11.fds 
-# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_22_kW_11.fds 
-# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_33_kW_11.fds 
-# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_45_kW_11.fds 
+# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_14_kW_11.fds
+# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_22_kW_11.fds
+# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_33_kW_11.fds
+# $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_45_kW_11.fds
 # $QFDS $DEBUG $QUEUE -d $INDIR McCaffrey_57_kW_11.fds
 
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_14_kW_21.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_22_kW_21.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_33_kW_21.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_45_kW_21.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_57_kW_21.fds
+# $QFDS $DEBUG -p 36 -n 12 -q aspen -d $INDIR McCaffrey_14_kW_21.fds
+# $QFDS $DEBUG -p 36 -n 12 -q aspen -d $INDIR McCaffrey_22_kW_21.fds
+# $QFDS $DEBUG -p 36 -n 12 -q aspen -d $INDIR McCaffrey_33_kW_21.fds
+# $QFDS $DEBUG -p 36 -n 12 -q aspen -d $INDIR McCaffrey_45_kW_21.fds
+# $QFDS $DEBUG -p 36 -n 12 -q aspen -d $INDIR McCaffrey_57_kW_21.fds
 
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_14_kW_45.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_22_kW_45.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_33_kW_45.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_45_kW_45.fds
-# $QFDS $DEBUG -p 27 $QUEUE -d $INDIR McCaffrey_57_kW_45.fds
+# $QFDS $DEBUG -p 175 -n 12 -q aspen -d $INDIR McCaffrey_14_kW_45.fds
+# $QFDS $DEBUG -p 175 -n 12 -q aspen -d $INDIR McCaffrey_22_kW_45.fds
+# $QFDS $DEBUG -p 175 -n 12 -q aspen -d $INDIR McCaffrey_33_kW_45.fds
+# $QFDS $DEBUG -p 175 -n 12 -q aspen -d $INDIR McCaffrey_45_kW_45.fds
+# $QFDS $DEBUG -p 175 -n 12 -q aspen -d $INDIR McCaffrey_57_kW_45.fds
 
 echo FDS cases submitted
