69b2f89e5e0ce28e36425680ee78d3b1e2a15f11
rmcdermo <randy.mcdermott@gmail.com>
2017-06-23 13:35:47 -0400

    Matlab: fix dvcread.m for Matlab 2017

--- Utilities/Matlab/scripts/dvcread.m
+++ Utilities/Matlab/scripts/dvcread.m
@@ -2,10 +2,12 @@
 % 6-02-2009
 % dvcread.m
 %
-% function [H X] = dvcread(filename,header_row)
+% function [H X] = dvcread(filename,header_row,data_row)
 %
 % header_row = row where header names are stored; names converted to H
 % vector.
+%
+% data_row = row where numeric data starts
 %
 % X = matrix of numeric data; columns correspond to entries in H.
 %
@@ -14,23 +16,25 @@
 % lines.  In principle, xlsread should handle this but we have experienced
 % problems with this function.  Actually, the file given by 'filename' does
 % not have to be a devc file.  But we take advantage of the fact that after
-% the header lines have been read, the remaining data is numeric and 
+% the header lines have been read, the remaining data is numeric and
 % comma-delimited, so that we may use csvread at that point.  We also
 % assume that the header names are contained in the last successfully read
 % text line, since in devc files the units are typically given by the first
 % line of text.
 
-function [H X] = dvcread(filename,header_row)
+function [H X] = dvcread(filename,header_row,data_row)
 
 fid = fopen(filename,'r+');
 
 ierror = 0;
-irow = -1;
+irow = 0;
 textline = [];
 while ierror==0
     irow = irow + 1;
-    if irow==header_row; headers = textline; end
     textline = fgetl(fid);
+    if irow==header_row
+        headers = textline;
+    end
     [X ierror]=str2num(textline);
 end
 fclose(fid);
@@ -41,7 +45,8 @@ if irow>0
     H = strtrim(C{:}');
 else
     H = [];
-end  
+end
 
 % read numeric data
-X = csvread(filename,irow,0);
+NUM = importdata(filename,',',max(1,data_row-1));
+X = NUM.data;
