b1c1a9d167bfc55a21fe34652cd22a890520f1ce
Tom Milac <tmilac@users.noreply.github.com>
2017-07-27 13:29:24 -0700

    Elaborate Matlab code to analyze radiation_gas_panel Verification case

--- Utilities/Matlab/scripts/radiation_gas_panel.m
+++ Utilities/Matlab/scripts/radiation_gas_panel.m
@@ -12,24 +12,48 @@ data_dir  = '../../Verification/Radiation/';
 plot_dir = '../../Manuals/FDS_Verification_Guide/SCRIPT_FIGURES/';
 
 % Read the output data
-if ~exist([data_dir,'radiation_gas_panel_devc.csv']) 
-    display(['Error File radiation_gas_panel_devc.csv does not exist.  Skipping case.']);
+if(~exist([data_dir,'radiation_gas_panel_10cm_devc.csv'])        || ...
+   ~exist([data_dir,'radiation_gas_panel_15cm_devc.csv'])        || ...
+   ~exist([data_dir,'radiation_gas_panel_25cm_devc.csv'])        || ...
+   ~exist([data_dir,'radiation_gas_panel_38cm_devc.csv'])        || ...
+   ~exist([data_dir,'radiation_gas_panel_61cm_devc.csv'])        || ...
+   ~exist([data_dir,'radiation_gas_panel_76cm_devc.csv'])        || ...
+   ~exist([data_dir,'radiation_gas_panel_10cm_offset_devc.csv']) || ...
+   ~exist([data_dir,'radiation_gas_panel_15cm_offset_devc.csv']) || ...
+   ~exist([data_dir,'radiation_gas_panel_25cm_offset_devc.csv']) || ...
+   ~exist([data_dir,'radiation_gas_panel_38cm_offset_devc.csv']) || ...
+   ~exist([data_dir,'radiation_gas_panel_61cm_offset_devc.csv']) || ...
+   ~exist([data_dir,'radiation_gas_panel_76cm_offset_devc.csv'])) 
+    display(['Error One or more of file radiation_gas_panel_<?>_devc.csv does not exist.  Skipping case.']);
     return;
 end
 
-M = csvread([data_dir,'radiation_gas_panel_devc.csv'],2);
+M10 = csvread([data_dir,'radiation_gas_panel_10cm_devc.csv'],2);
+M15 = csvread([data_dir,'radiation_gas_panel_15cm_devc.csv'],2);
+M25 = csvread([data_dir,'radiation_gas_panel_25cm_devc.csv'],2);
+M38 = csvread([data_dir,'radiation_gas_panel_38cm_devc.csv'],2);
+M61 = csvread([data_dir,'radiation_gas_panel_61cm_devc.csv'],2);
+M76 = csvread([data_dir,'radiation_gas_panel_76cm_devc.csv'],2);
+
+M10Offset = csvread([data_dir,'radiation_gas_panel_10cm_offset_devc.csv'],2);
+M15Offset = csvread([data_dir,'radiation_gas_panel_15cm_offset_devc.csv'],2);
+M25Offset = csvread([data_dir,'radiation_gas_panel_25cm_offset_devc.csv'],2);
+M38Offset = csvread([data_dir,'radiation_gas_panel_38cm_offset_devc.csv'],2);
+M61Offset = csvread([data_dir,'radiation_gas_panel_61cm_offset_devc.csv'],2);
+M76Offset = csvread([data_dir,'radiation_gas_panel_76cm_offset_devc.csv'],2);
 
 % Collect heat fluxes computed by FDS.
-xFDS = [.10,  .15, .25, .38, .61, .76];
-yFDS = [4,5,6];
+xFDS       = [.10,  .15, .25, .38, .61, .76];
+yFDS       = [M10(end,end),       M15(end,end),       M25(end,end),       M38(end,end),       M61(end,end),       M76(end,end)];
+yOffsetFDS = [M10Offset(end,end), M15Offset(end,end), M25Offset(end,end), M38Offset(end,end), M61Offset(end,end), M76Offset(end,end)];
 
-% Heat fluxes computed using Boltzmann's law and configuration
-% factors.
-xCF = [.10,  .15,    .25,  .38,  .46,  .76];
-yCF = [71.7, 564.4,  30.8, 16.3, 11.8, 4.70];
+% Heat fluxes computed using Boltzmann's law and configuration factors.
+xCF       = [.10,  .15,    .25,  .38,  .46,  .76];
+yCF       = [71.7, 564.4,  30.8, 16.3, 11.8, 4.70];
+yCFOffset = [71.7, 564.4,  30.8, 16.3, 11.8, 4.70];
 
 % Heat fluxes reported by Simms.
-xSimms = [.10,  .15,  .25,  .38,  .61, ..76];
+xSimms = [.10,  .15,  .25,  .38,  .61,  .76];
 ySimms = [69.4, 53.3, 32.2, 17.0, 7.45, 4.92];
 
 % Plot heat flux versus distance.
@@ -39,7 +63,7 @@ set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
 
 plot(xFDS,  yFDS,  '-o', 'MarkerSize', 15, 'MarkerFaceColor', 'blue' ); hold on
 plot(xCF,   yCF,   '-s', 'MarkerSize', 15, 'MarkerFaceColor', 'green'); hold on
-plot(xSimms,ySimms,'-d', 'MarkerSize', 15, 'MarkerFaceColor', 'red' .);
+plot(xSimms,ySimms,'-d', 'MarkerSize', 15, 'MarkerFaceColor', 'red'  );
 
 xlabel('Distance from center of gas panel (m)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter,'FontName',Font_Name)
 ylabel('Heat flux (kW/m^2)','FontSize',Title_Font_Size,'Interpreter',Font_Interpreter,'FontName',Font_Name)
