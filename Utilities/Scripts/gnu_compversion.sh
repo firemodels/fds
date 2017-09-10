d854998958791cda322012327dbfb874ae90ae9d
Forney <gforney@gmail.com>
2017-09-09 20:30:22 -0400

    build  scripts: corrections to compiler version scripts

--- Utilities/Scripts/gnu_compversion.sh
+++ Utilities/Scripts/gnu_compversion.sh
@@ -11,21 +11,22 @@ is_file_installed()
   $program -help > prog_version 2>&1
   notfound=`cat prog_version | head -1 | grep "not found" | wc -l`
   rm prog_version
-  if [ "$notfound" == "1" ] ; then
+  if [ $notfound -eq 1 ] ; then
     echo 0
     exit
+  else
+    echo 1
+    exit
   fi
-  echo 1
-  exit
 }
 
 gfortran_installed=`is_file_installed gfortran`
-if [ "$gfortran_installed" == "0" ]; then
+if [ $gfortran_installed -eq 0 ]; then
   echo unknown
   exit
 fi
 
-gfortran --version |head -1> gfortran_version 2>&1 
+gfortran --version | head -1> gfortran_version 2>&1 
 GFORTRANVERSION=`cat gfortran_version | awk '{print $4}' `
 rm gfortran_version
 echo "\"Gnu gfortran $GFORTRANVERSION\""
--- Utilities/Scripts/intel_compversion.sh
+++ Utilities/Scripts/intel_compversion.sh
@@ -11,7 +11,7 @@ is_file_installed()
   $program -help > prog_version 2>&1
   notfound=`cat prog_version | head -1 | grep "not found" | wc -l`
   rm prog_version
-  if [ "$notfound" == "1" ] ; then
+  if [ $notfound -eq 1 ] ; then
     echo 0
     exit
   fi
@@ -20,12 +20,12 @@ is_file_installed()
 }
 
 ifort_installed=`is_file_installed ifort`
-if [ "$ifort_installed" == "0" ]; then
-  echo Unknown
+if [ $ifort_installed -eq 0 ]; then
+  echo unknown
   exit
 fi
 
 ifort -v > ifort_version 2>&1 
 IFORTVERSION=`cat ifort_version | awk '{print $3}' `
 rm ifort_version
-echo "\"Intel C/C++ $IFORTVERSION\""
+echo "\"Intel ifort $IFORTVERSION\""
