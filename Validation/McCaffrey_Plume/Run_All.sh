e24c7c11c1a85730ea6ad6f1f4af896fe05ff5ad
rmcdermo <randy.mcdermott@gmail.com>
2017-09-11 15:58:26 -0400

    FDS Validation: update McCaffrey input files

--- Validation/McCaffrey_Plume/Run_All.sh
+++ Validation/McCaffrey_Plume/Run_All.sh
@@ -17,10 +17,10 @@ $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_33_kW_21.fds
 $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_45_kW_21.fds
 $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_57_kW_21.fds
 
-# $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_14_kW_45.fds
-# $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_22_kW_45.fds
-# $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_33_kW_45.fds
-# $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_45_kW_45.fds
-# $QFDS $DEBUG -p 125 -n 12 $QUEUE -d $INDIR McCaffrey_57_kW_45.fds
+$QFDS $DEBUG -p 158 -n 12 $QUEUE -d $INDIR McCaffrey_14_kW_45.fds
+$QFDS $DEBUG -p 158 -n 12 $QUEUE -d $INDIR McCaffrey_22_kW_45.fds
+$QFDS $DEBUG -p 158 -n 12 $QUEUE -d $INDIR McCaffrey_33_kW_45.fds
+$QFDS $DEBUG -p 158 -n 12 $QUEUE -d $INDIR McCaffrey_45_kW_45.fds
+$QFDS $DEBUG -p 158 -n 12 $QUEUE -d $INDIR McCaffrey_57_kW_45.fds
 
 echo FDS cases submitted
