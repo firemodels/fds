9b642310b6dbe90b25cb4ddfa02b4b59292f48ae
gforney <gforney@gmail.com>
2017-09-18 18:03:36 -0400

    FDS build: remove error checking and environment setup

--- Build/mpi_intel_linux_64ib_dv/make_fds.sh
+++ Build/mpi_intel_linux_64ib_dv/make_fds.sh
@@ -3,15 +3,5 @@ arg=$1
 dir=`pwd`
 target=${dir##*/}
 
-if [ "$arg" != "-f" ]; then
-  source ../Scripts/set_mpidist.sh ib
-  if [ $mpi_error -eq 1 ]; then
-    exit
-  fi
-  if [ $setup_fortran -eq 1 ]; then
-    source $IFORT_COMPILER/bin/compilervars.sh intel64
-  fi
-fi
-
 echo Building $target with $MPIDIST
 make -j4 MPIFORT="$MPIFORT" VPATH="../../Source" -f ../makefile $target
