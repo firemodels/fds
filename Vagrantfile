7fd82ebd887d281ab9076427fee26503cac1cab9
sascha gottfried <sascha.gottfried@gmail.com>
2017-12-07 13:26:27 +0100

    add vagrant provisioning script, fix MarkDown in docs

--- Vagrantfile
+++ Vagrantfile
@@ -63,7 +63,7 @@ Vagrant.configure("2") do |config|
 
   config.vm.provider "virtualbox" do |v|
   		v.memory = 4096
-  		v.cpus = 2
+  		v.cpus = 4
   end
 
 
---
+++ provision.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -qq
+sudo apt-get install build-essential -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+sudo apt install gfortran-7 -y
+wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz
+gunzip -c openmpi-3.0.0.tar.gz | tar xf -
+cd openmpi-3.0.0/
+./configure FC=gfortran-7 CC=gcc-7 --prefix=/shared/openmpi_64 --enable-mpirun-prefix-by-default --enable-mpi-fortran --enable-static --disable-shared
+make -j 4
+export LD_LIBRARY_PATH=/shared/openmpi_64/lib:$LD_LIBRARY_PATH
+cd Build/mpi_gnu_linux_64
+./make_fds.sh
