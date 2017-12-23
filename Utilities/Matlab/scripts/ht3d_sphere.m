8e0fc20dc14d214e044db3bdbeceac43dc4fdfcf
rmcdermo <randy.mcdermott@gmail.com>
2017-12-23 05:43:49 -0500

    FDS Verification: temporarily remove parallel HT3D cases

--- Utilities/Matlab/scripts/ht3d_sphere.m
+++ Utilities/Matlab/scripts/ht3d_sphere.m
@@ -83,7 +83,7 @@ end
 
 ddir = '../../Verification/Heat_Transfer/';
 fnt = {'ht3d_sphere_75'};
-fileName = {'ht3d_sphere_25','ht3d_sphere_51','ht3d_sphere_75'};
+fileName = {'ht3d_sphere_25','ht3d_sphere_51'}; %,'ht3d_sphere_75'};
 nc_array = {25,51,75};
 dx_array = {0.25/25,0.25/51,0.25/75};
 
