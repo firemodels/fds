40a47a20f869d50c037301129a8ada9a4705cd37
rmcdermo <randy.mcdermott@gmail.com>
2018-01-05 16:27:23 -0500

    FDS Source: add SHAPE='CONE' to OBST

--- Source/geom.f90
+++ Source/geom.f90
@@ -416,7 +416,7 @@ PUBLIC :: ADD_INPLACE_NNZ_H_BYMESH,ADD_INPLACE_NNZ_H_WHLDOM,&
           GETU,GET_GASCUTFACE_SCALAR_SLICE,GETGRAD,GET_BOUNDFACE_GEOM_INFO_H, &
           GET_H_CUTFACES,GET_H_MATRIX_CC,GET_CRTCFCC_INTERPOLATION_STENCILS,GET_RCFACES_H, &
           GET_EXIMFACE_SCALAR_SLICE,GET_SOLIDCUTFACE_SCALAR_SLICE,GET_SOLIDREGFACE_SCALAR_SLICE, &
-          INIT_CUTCELL_DATA,INTERSECT_CYLINDER_AABB,INTERSECT_SPHERE_AABB, &
+          INIT_CUTCELL_DATA,INTERSECT_CONE_AABB,INTERSECT_CYLINDER_AABB,INTERSECT_SPHERE_AABB, &
           LINEARFIELDS_INTERP_TEST,MASS_CONSERVE_INIT,NUMBER_UNKH_CUTCELLS,POTENTIAL_FLOW_INIT,&
           READ_GEOM,ROTATION_MATRIX, &
           SET_CCIBM_MATVEC_DATA,SET_DOMAINDIFFLX_3D,SET_DOMAINADVFLX_3D, &
@@ -34739,6 +34739,47 @@ R_OUT(3,1) = DOT_PRODUCT(A3,B1); R_OUT(3,2) = DOT_PRODUCT(A3,B2); R_OUT(3,3) = D
 ! stop
 
 END SUBROUTINE ROTATION_MATRIX
+
+! ---------------------------- INTERSECT_CONE_AABB ----------------------------------------
+
+! This routine basically follows the INTERSECT_CYLINDER_AABB algorithm, with radius = R(Z)
+
+LOGICAL FUNCTION INTERSECT_CONE_AABB(X_IN,H,RADIUS,ROTMAT,XB)
+IMPLICIT NONE
+
+REAL(EB), INTENT(IN) :: X_IN(3),H,RADIUS,ROTMAT(3,3),XB(6)
+REAL(EB) :: X(3),U(3),V(3),DUX(2),Z0,ZH,DIST_SQUARED,R_Z
+INTEGER :: II,JJ,KK
+
+INTERSECT_CONE_AABB=.FALSE.
+
+X  = MATMUL(ROTMAT,X_IN) ! transform center
+Z0 = X(3)                ! lower cap in new reference frame
+ZH = X(3) + H            ! upper cap in new reference frame
+
+! transform vertices and test against end caps, then radius
+DO KK=5,6
+   DO JJ=3,4
+      DO II=1,2
+         V = (/XB(II),XB(JJ),XB(KK)/)
+         U = MATMUL(ROTMAT,V)
+         IF (U(3)>=Z0 .AND. U(3)<=ZH) THEN
+            ! vertex is within end-cap range, now test against radius
+            ! in new frame the distance from vertex to CONE axis only requires the 1st and 2nd vector components
+            DUX = U(1:2) - X(1:2)
+            DIST_SQUARED = DOT_PRODUCT(DUX,DUX)
+            R_Z = RADIUS*(1._EB-(U(3)-Z0)/H)
+            IF (DIST_SQUARED < R_Z*R_Z+TWO_EPSILON_EB) THEN
+               INTERSECT_CONE_AABB = .TRUE.
+               RETURN
+            ENDIF
+         ENDIF
+      ENDDO
+   ENDDO
+ENDDO
+
+RETURN
+END FUNCTION INTERSECT_CONE_AABB
 
 ! ! ---------------------------- TRIANGLE_ON_CELL_SURF ----------------------------------------
 !
