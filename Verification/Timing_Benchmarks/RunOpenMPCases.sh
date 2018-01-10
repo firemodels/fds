2b4c8dcdcc36834c2ae4ccb863b99bef366b7028
gforney <gforney@gmail.com>
2018-01-10 13:39:44 -0500

    FDS vv: output host to openmp results file

--- Verification/Timing_Benchmarks/RunOpenMPCases.sh
+++ Verification/Timing_Benchmarks/RunOpenMPCases.sh
@@ -39,16 +39,21 @@ done
 
 wait_cases_end
 
-echo 64 1 thread, 64 4 threads, 128 1 thread, 128 4 threads>$OUTFILE
+echo 64 1 thread, Host, 64 4 threads, Host, 128 1 thread, Host, 128 4 threads, Host>$OUTFILE
 for i in `seq 1 $NCASES`; do
 arg=0$i
   if [ $i -gt 9 ]; then
     arg=$i
   fi
-  TIME1=`grep Time openmp_test64a$arg.out | grep Stepping | awk '{print $7}'`
-  TIME2=`grep Time openmp_test64d$arg.out | grep Stepping | awk '{print $7}'`
+  
+  HOST1=`grep Host openmp_test64a$arg.log  | awk '{print $2}'`
+  HOST2=`grep Host openmp_test64d$arg.log  | awk '{print $2}'`
+  HOST3=`grep Host openmp_test128a$arg.log | awk '{print $2}'`
+  HOST4=`grep Host openmp_test128d$arg.log | awk '{print $2}'`
+  TIME1=`grep Time openmp_test64a$arg.out  | grep Stepping | awk '{print $7}'`
+  TIME2=`grep Time openmp_test64d$arg.out  | grep Stepping | awk '{print $7}'`
   TIME3=`grep Time openmp_test128a$arg.out | grep Stepping | awk '{print $7}'`
   TIME4=`grep Time openmp_test128d$arg.out | grep Stepping | awk '{print $7}'`
-  echo "$TIME1,$TIME2,$TIME3,$TIME4">>$OUTFILE
+  echo "$TIME1,$HOST1,$TIME2,$HOST2,$TIME3,$HOST3,$TIME4,$HOST4">>$OUTFILE
 done
 echo complete
