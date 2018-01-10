a5ed689aed3953de929d8ffe6720c1ca711b0303
gforney <gforney@gmail.com>
2018-01-10 17:27:47 -0500

    FDS vv: update openmp test script

--- Verification/Timing_Benchmarks/RunOpenMPCases.sh
+++ Verification/Timing_Benchmarks/RunOpenMPCases.sh
@@ -2,7 +2,7 @@
 
 export JOBPREFIX=MP_
 OUTFILE=openmp_summary.csv
-QFDS="../../Utilities/Scripts/qfds.sh  -I  -t -q firebot " 
+QFDS="../../Utilities/Scripts/qfds.sh  -I  -t -q bench1 " 
 NCASES=5
 
 #---------------------------------------------
@@ -24,17 +24,17 @@ arg=0$i
   if [ $i -gt 9 ]; then
     arg=$i
   fi
-  ./makecase64.sh 64 openmp_test64a$arg
-  $QFDS -o 1 openmp_test64a$arg.fds
+  ./makecase64.sh 64 test64a$arg
+  $QFDS -o 1 test64a$arg.fds
 
-  ./makecase64.sh 64 openmp_test64d$arg
-  $QFDS -o 4 openmp_test64d$arg.fds
+  ./makecase64.sh 64 test64d$arg
+  $QFDS -o 4 test64d$arg.fds
 
-  ./makecase128.sh 128 openmp_test128a$arg
-  $QFDS -o 1 openmp_test128a$arg.fds
+  ./makecase128.sh 128 test128a$arg
+  $QFDS -o 1 test128a$arg.fds
 
-  ./makecase128.sh 128 openmp_test128d$arg
-  $QFDS -o 4 openmp_test128d$arg.fds
+  ./makecase128.sh 128 test128d$arg
+  $QFDS -o 4 test128d$arg.fds
 done
 
 wait_cases_end
@@ -46,14 +46,14 @@ arg=0$i
     arg=$i
   fi
   
-  HOST1=`grep Host openmp_test64a$arg.log  | awk '{print $2}'`
-  HOST2=`grep Host openmp_test64d$arg.log  | awk '{print $2}'`
-  HOST3=`grep Host openmp_test128a$arg.log | awk '{print $2}'`
-  HOST4=`grep Host openmp_test128d$arg.log | awk '{print $2}'`
-  TIME1=`grep Time openmp_test64a$arg.out  | grep Stepping | awk '{print $7}'`
-  TIME2=`grep Time openmp_test64d$arg.out  | grep Stepping | awk '{print $7}'`
-  TIME3=`grep Time openmp_test128a$arg.out | grep Stepping | awk '{print $7}'`
-  TIME4=`grep Time openmp_test128d$arg.out | grep Stepping | awk '{print $7}'`
+  HOST1=`grep Host test64a$arg.log  | awk '{print $2}'`
+  HOST2=`grep Host test64d$arg.log  | awk '{print $2}'`
+  HOST3=`grep Host test128a$arg.log | awk '{print $2}'`
+  HOST4=`grep Host test128d$arg.log | awk '{print $2}'`
+  TIME1=`grep Time test64a$arg.out  | grep Stepping | awk '{print $7}'`
+  TIME2=`grep Time test64d$arg.out  | grep Stepping | awk '{print $7}'`
+  TIME3=`grep Time test128a$arg.out | grep Stepping | awk '{print $7}'`
+  TIME4=`grep Time test128d$arg.out | grep Stepping | awk '{print $7}'`
   echo "$TIME1,$HOST1,$TIME2,$HOST2,$TIME3,$HOST3,$TIME4,$HOST4">>$OUTFILE
 done
 echo complete
