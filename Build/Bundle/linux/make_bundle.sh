a6b1159380db6aeac40aa7e3ae080afea571e342
gforney <gforney@gmail.com>
2018-01-16 15:48:42 -0500

    bundle scripts: rename windows bundle script, create scripts directory

--- Build/Bundle/linux/make_bundle.sh
+++ Build/Bundle/linux/make_bundle.sh
@@ -24,4 +24,4 @@ export INSTALLDIR=FDS/$FDSEDITION
 export MISCTO=LIB64
 export COMPTO=INTEL
 
-$fds_smvroot/fds/Build/Bundle/bundle_generic.sh
+$fds_smvroot/fds/Build/Bundle/scripts/bundle_generic.sh
