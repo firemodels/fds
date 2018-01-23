dc4da8a4278942d779ea2d766e3db8443b865480
gforney <gforney@gmail.com>
2018-01-23 14:48:57 -0500

    bundle scripts: improve bundle script names

--- Build/Bundle/linux/make_fds.sh
+++ Build/Bundle/linux/make_fds.sh
@@ -2,5 +2,8 @@
 CURDIR=`pwd`
 cd ../../../Build/impi_intel_linux_64
 git clean -dxf
+echo
+echo "********** build FDS"
+echo
 ./make_fds.sh
 cd $CURDIR
--- Build/Bundle/osx/make_fds.sh
+++ Build/Bundle/osx/make_fds.sh
@@ -2,5 +2,8 @@
 CURDIR=`pwd`
 cd ../../../Build/mpi_intel_osx_64
 git clean -dxf
+echo
+echo "********** build FDS"
+echo
 ./make_fds.sh
 cd $CURDIR
