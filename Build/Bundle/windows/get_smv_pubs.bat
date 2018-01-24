da42b0f9e91ec0ce92390bdcaa886aa9e8879ca8
gforney <gforney@gmail.com>
2018-01-24 16:55:38 -0500

    bundle scripts: add scripts for getting fds and smokeview bot generated guides on windows

---
+++ Build/Bundle/windows/get_smv_pubs.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:: setup environment variables (defining where repository resides etc) 
+
+set envfile="%userprofile%"\fds_smv_env.bat
+IF EXIST %envfile% GOTO endif_envexist
+echo ***Fatal error.  The environment setup file %envfile% does not exist. 
+echo Create a file named %envfile% and use smv/scripts/fds_smv_env_template.bat
+echo as an example.
+echo.
+echo Aborting now...
+
+pause>NUL
+goto:eof
+
+:endif_envexist
+
+call %envfile%
+
+%svn_drive%
+
+set pubdir=%smokebotrepo%/smv/Manuals
+echo pscp %linux_hostname%:%pubdir%/SMV_Technical_Reference_Guide/SMV_Technical_Reference_Guide.pdf %userprofile%\FDS_Guides
+
+pause
+pscp %linux_hostname%:%pubdir%/SMV_Technical_Reference_Guide/SMV_Technical_Reference_Guide.pdf %userprofile%\FDS_Guides
+pscp %linux_hostname%:%pubdir%/SMV_User_Guide/SMV_User_Guide.pdf                               %userprofile%\FDS_Guides
+pscp %linux_hostname%:%pubdir%/SMV_Verification_Guide/SMV_Verification_Guide.pdf               %userprofile%\FDS_Guides
