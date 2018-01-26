1b5cd60a949f55d8e24be85a3978e132bbf1916d
Forney <gforney@gmail.com>
2018-01-26 14:40:03 -0500

    bundle scripts: minor edits

--- Build/Bundle/osx/get_fds_pubs.sh
+++ Build/Bundle/osx/get_fds_pubs.sh
@@ -11,8 +11,8 @@ if [ ! -e ~/FDS_Guides ]; then
 fi
 
 pubdir=$firebothome/.firebot/pubs
-scp $linux_hostname\:$pubdir/FDS_Config_Management_Plan.pdf    ~/FDS_Guides/.
-scp $linux_hostname\:$pubdir/FDS_Technical_Reference_Guide.pdf ~/FDS_Guides/.
-scp $linux_hostname\:$pubdir/FDS_User_Guide.pdf                ~/FDS_Guides/.
-scp $linux_hostname\:$pubdir/FDS_Validation_Guide.pdf          ~/FDS_Guides/.
-scp $linux_hostname\:$pubdir/FDS_Verification_Guide.pdf        ~/FDS_Guides/.
+scp -q $linux_hostname\:$pubdir/FDS_Config_Management_Plan.pdf    ~/FDS_Guides/.
+scp -q $linux_hostname\:$pubdir/FDS_Technical_Reference_Guide.pdf ~/FDS_Guides/.
+scp -q $linux_hostname\:$pubdir/FDS_User_Guide.pdf                ~/FDS_Guides/.
+scp -q $linux_hostname\:$pubdir/FDS_Validation_Guide.pdf          ~/FDS_Guides/.
+scp -q $linux_hostname\:$pubdir/FDS_Verification_Guide.pdf        ~/FDS_Guides/.
