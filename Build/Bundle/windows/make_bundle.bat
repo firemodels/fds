4ab64608d7db4a230ca8a10f521e0ce11a2f5e71
gforney <gforney@gmail.com>
2018-02-01 11:12:22 -0500

    bundle scripts: update bundle scripts to use new pub location

--- Build/Bundle/windows/make_bundle.bat
+++ Build/Bundle/windows/make_bundle.bat
@@ -11,7 +11,7 @@ set fdsdir=%svn_root%\fds\Build\intel_win_64
 set fdsmpidir=%svn_root%\fds\Build\impi_intel_win_64
 set basename=FDS_%fds_version%-SMV_%smv_version%_win64
 
-set in_pdf=%userprofile%\FDS_Guides
+set in_pdf=%userprofile%\.bundle\pubs
 set in_intel_dll=%userprofile%\fire-notes\INSTALL\LIBS\WINDOWS\INTEL17
 set in_shortcut=%userprofile%\fire-notes\INSTALL\repoexes
 set in_for_bundle=%svn_root%\smv\for_bundle
