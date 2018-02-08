4136e4e5b12957ca4b04931580312fae93db7315
gforney <gforney@gmail.com>
2018-02-08 09:30:57 -0500

    bundle scripts: add --long when using git describe --dirty if absent

--- Utilities/Scripts/UPDATE_latest_fds_onhost.sh
+++ Utilities/Scripts/UPDATE_latest_fds_onhost.sh
@@ -4,4 +4,4 @@ directory=$1
 host=$2
 
 ssh -q $host \( cd \~/$directory \; git checkout master \; git remote update \; git merge origin/master \; git merge firemodels/master  \)
-ssh -q $host \( cd \~/$directory \; git describe --dirty  \)
+ssh -q $host \( cd \~/$directory \; git describe --dirty --long \)
