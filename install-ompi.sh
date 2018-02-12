49f9cfd4450e0773cace956c0bdcb46ce2145cf6
Sascha Gottfried <sascha.gottfried@gmail.com>
2018-02-12 17:25:21 +0100

    refine build scripts to address Vagrant and Travis CI environments

---
+++ install-ompi.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz -q -O /tmp/openmpi-3.0.0.tar.gz
+cd /tmp
+gunzip -c /tmp/openmpi-3.0.0.tar.gz -q| tar xf -
+cd /tmp/openmpi-3.0.0/
+./configure FC=gfortran-7 CC=gcc-7 --quiet && make -j 4 --quiet && sudo make install --quiet
--- provision.sh
+++ provision.sh
@@ -3,14 +3,13 @@ sudo apt-get update -qq
 sudo apt-get install build-essential -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
-sudo apt install gfortran-7 -y
-wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz -q
-gunzip -c openmpi-3.0.0.tar.gz -q| tar xf -
-cd openmpi-3.0.0/
-./configure FC=gfortran-7 CC=gcc-7 --prefix=/vagrant/openmpi --enable-static --disable-shared --quiet
-make all -j 4 --quiet
-make install --quiet
-export PATH=/vagrant/openmpi/bin:$PATH
-export LD_LIBRARY_PATH=/vagrant/openmpi/lib:$LD_LIBRARY_PATH
-cd /vagrant/Build/mpi_gnu_linux_64
-./make_fds.sh
+sudo apt-get install gfortran-7 -y
+wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz -q -O /tmp/openmpi-3.0.0.tar.gz
+cd /tmp
+gunzip -c /tmp/openmpi-3.0.0.tar.gz -q| tar xf -
+cd /tmp/openmpi-3.0.0/
+./configure FC=gfortran-7 CC=gcc-7 --quiet && make -j 4 --quiet && sudo make install --quiet
+#ldconfig -n /vagrant/openmpi/lib
+#export PATH=/vagrant/openmpi/bin:$PATH
+#cd /vagrant/Build/mpi_gnu_linux_64
+#./make_fds.sh
