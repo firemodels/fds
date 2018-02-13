725437445cf02065df198fd0455c55f64578dace
Sascha Gottfried <sascha.gottfried@gmail.com>
2018-02-13 21:10:34 +0100

    build OpenMPI as static libraries, add git package, remove build-essentials

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 sudo apt-get update -qq
-sudo apt-get install build-essential -y
+sudo apt-get install git -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install gfortran-7 -y
@@ -8,8 +8,8 @@ wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz 
 cd /tmp
 gunzip -c /tmp/openmpi-3.0.0.tar.gz -q| tar xf -
 cd /tmp/openmpi-3.0.0/
-./configure FC=gfortran-7 CC=gcc-7 --quiet && make -j 4 --quiet && sudo make install --quiet
-#ldconfig -n /vagrant/openmpi/lib
-#export PATH=/vagrant/openmpi/bin:$PATH
-#cd /vagrant/Build/mpi_gnu_linux_64
-#./make_fds.sh
+./configure FC=gfortran-7 CC=gcc-7 --prefix=/opt/openmpi-3.0.0 --enable-static --disable-shared --quiet && make -j 4 --quiet && sudo make install --quiet
+export PATH=/opt/openmpi-3.0.0/bin:$PATH
+export LD_LIBRARY_PATH=/opt/openmpi-3.0.0/lib:$LD_LIBRARY_PATH
+cd /vagrant/Build/mpi_gnu_linux_64
+./make_fds.sh
