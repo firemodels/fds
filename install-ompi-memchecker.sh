91c42d32dc0fd69bea0d81d9079840d065b47ff4
Sascha Gottfried <sascha.gottfried@gmail.com>
2018-02-14 16:02:36 +0100

    refactoring build scripts

--- install-ompi-memchecker.sh
+++ install-ompi-memchecker.sh
@@ -4,4 +4,4 @@ wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz 
 cd /tmp
 gunzip -c /tmp/openmpi-3.0.0.tar.gz -q| tar xf -
 cd /tmp/openmpi-3.0.0/
-./configure FC=gfortran-7 CC=gcc-7 --prefix=/opt/openmpi-3.0.0-memchecker --enable-debug --enable-memchecker --with-valgrind=/usr/bin --enable-static --disable-shared --quiet && make -j 4 --quiet && sudo make install --quiet
+./configure FC=gfortran-7 CC=gcc-7 --prefix=/opt/openmpi-3.0.0-memchecker --enable-mpirun-prefix-by-default --enable-debug --enable-memchecker --with-valgrind=/usr/bin --enable-static --disable-shared --quiet && make -j 4 --quiet && sudo make install --quiet
--- install-ompi.sh
+++ install-ompi.sh
@@ -3,4 +3,4 @@ wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz 
 cd /tmp
 gunzip -c /tmp/openmpi-3.0.0.tar.gz -q| tar xf -
 cd /tmp/openmpi-3.0.0/
-./configure FC=gfortran-7 CC=gcc-7 --quiet && make -j 4 --quiet && sudo make install --quiet
+./configure FC=gfortran-7 CC=gcc-7 --enable-static --disable-shared --quiet && make -j 4 --quiet && sudo make install --quiet
