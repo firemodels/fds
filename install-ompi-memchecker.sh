bffc6549f7d043cb9f240c312573e1c052ed94e3
Sascha Gottfried <sascha.gottfried@gmail.com>
2018-02-14 14:19:14 +0100

    add script to provision Open MPI with support for memchecker/valgrind

---
+++ install-ompi-memchecker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+sudo apt-get install valgrind -y
+wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.gz -q -O /tmp/openmpi-3.0.0.tar.gz
+cd /tmp
+gunzip -c /tmp/openmpi-3.0.0.tar.gz -q| tar xf -
+cd /tmp/openmpi-3.0.0/
+./configure FC=gfortran-7 CC=gcc-7 --prefix=/opt/openmpi-3.0.0-memchecker --enable-debug --enable-memchecker --with-valgrind=/usr/bin --enable-static --disable-shared --quiet && make -j 4 --quiet && sudo make install --quiet
