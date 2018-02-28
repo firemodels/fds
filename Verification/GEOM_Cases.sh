fd26c468c04b5588885ebd608d4c5a6e22bc2367
gforney <gforney@gmail.com>
2018-02-27 20:27:52 -0500

    FDS vv: convert vv scripts to run geom cases and generate pictures in fds repo rather than smv repo

---
+++ Verification/GEOM_Cases.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+$QFDS -d Complex_Geometry geom_simple.fds
+$QFDS -d Complex_Geometry geom_azim.fds
+$QFDS -d Complex_Geometry geom_elev.fds
+$QFDS -d Complex_Geometry geom_scale.fds
+$QFDS -d Complex_Geometry geom_obst.fds
+$QFDS -d Complex_Geometry geom_sphere1a.fds
+$QFDS -d Complex_Geometry geom_sphere1c.fds
+$QFDS -d Complex_Geometry geom_sphere1e.fds
+$QFDS -d Complex_Geometry geom_sphere2.fds
+$QFDS -d Complex_Geometry geom_sphere3b.fds
+$QFDS -d Complex_Geometry geom_sphere3d.fds
+$QFDS -d Complex_Geometry geom_sphere3f.fds
+$QFDS -d Complex_Geometry geom_sphere1b.fds
+$QFDS -d Complex_Geometry geom_sphere1d.fds
+$QFDS -d Complex_Geometry geom_sphere1f.fds
+$QFDS -d Complex_Geometry geom_sphere3a.fds
+$QFDS -d Complex_Geometry geom_sphere3c.fds
+$QFDS -d Complex_Geometry geom_sphere3e.fds
+$QFDS -d Complex_Geometry geom_terrain.fds
+$QFDS -d Complex_Geometry geom_texture.fds
+$QFDS -d Complex_Geometry geom_texture2.fds
+$QFDS -d Complex_Geometry geom_texture3a.fds
+$QFDS -d Complex_Geometry geom_texture3b.fds
+$QFDS -d Complex_Geometry geom_texture4a.fds
+$QFDS -d Complex_Geometry geom_texture4b.fds
+$QFDS -d Complex_Geometry geom_arch.fds
+$QFDS -d Complex_Geometry sphere_helium_1mesh.fds
+$QFDS -p 3 -d Complex_Geometry sphere_helium_3meshes.fds
+$QFDS -p 3 -d Complex_Geometry sphere_helium_conserve_3meshes.fds
+$QFDS -d Complex_Geometry saad_CC_explicit_512_cfl_p25.fds
+$QFDS -d Complex_Geometry saad_CC_explicit_512_cfl_p125.fds
+$QFDS -d Complex_Geometry saad_CC_explicit_512_cfl_p0625.fds
+$QFDS -d Complex_Geometry saad_CC_implicit_512_cfl_p25.fds
+$QFDS -d Complex_Geometry saad_CC_implicit_512_cfl_p125.fds
+$QFDS -d Complex_Geometry saad_CC_implicit_512_cfl_p0625.fds
