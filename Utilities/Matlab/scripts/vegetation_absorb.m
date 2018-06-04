496b462be3718d2e4de665118fa04be59cd337f5
mcgratta <mcgratta@gmail.com>
2018-06-04 17:36:24 -0400

    FDS Source: Add SHAPE_FACTOR and verification case

--- Utilities/Matlab/scripts/vegetation_absorb.m
+++ Utilities/Matlab/scripts/vegetation_absorb.m
@@ -11,7 +11,7 @@ outdir = '../../Verification/WUI/';
 M = importdata([outdir,'vegetation_absorb_devc.csv'],',',2);
 
 fid = fopen([outdir,'vegetation_absorb_FDS.csv'],'wt','n');
-fprintf(fid,'%s\n','mpuv,HF');
+fprintf(fid,'%s\n','mpuv,rad');
 mpuv = [0.0 0.1 0.2 0.4 0.8 1.6];
 for i=1:6
    fprintf(fid,'%5.1f,%5.2f\n',mpuv(i),M.data(end,i+1));
