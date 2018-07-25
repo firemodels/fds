firemodels/fds

commit 431a93be1b9ad0bfe5bbe692b43a2530699f5400

FDS Source: fix AREA_ADJUST for MULT OBST SHAPE

Source/wall.f90
@@ -2088,8 +2088,8 @@ METHOD_OF_MASS_TRANSFER: SELECT CASE(SPECIES_BC_INDEX)
                IF (CORRECTOR) TSI = T      - ONE_D%T_IGN
             ENDIF
             ONE_D%MASSFLUX(N) = EVALUATE_RAMP(TSI,SF%TAU(N),SF%RAMP_INDEX(N))*SF%MASS_FLUX(N)
-            ONE_D%MASSFLUX_SPEC(N) = ONE_D%MASSFLUX(N)
             ONE_D%MASSFLUX(N) = SF%ADJUST_BURN_RATE(N)*ONE_D%MASSFLUX(N)*ONE_D%AREA_ADJUST
+            ONE_D%MASSFLUX_SPEC(N) = ONE_D%MASSFLUX(N)
          ENDIF
          MFT = MFT + ONE_D%MASSFLUX(N)
       ENDDO SUM_MASSFLUX_LOOP

Source/cons.f90
@@ -44,6 +44,7 @@ INTEGER, PARAMETER :: SPHERE_DRAG=1,CYLINDER_DRAG=2,USER_DRAG=3,SCREEN_DRAG=4,PO
 INTEGER, PARAMETER :: OLD=1,NEW=2,GUESS=3,PREVIOUS=4                                        ! Network solver indices
 INTEGER, PARAMETER :: NODE1=1,NODE2=2,LINEAR_INTERPOLATION=-1                               ! Duct mass transport interpolation
 INTEGER, PARAMETER :: HVAC_BOUNDARY=42
+INTEGER, PARAMETER :: OBST_SPHERE_TYPE=1,OBST_CYLINDER_TYPE=2,OBST_CONE_TYPE=3,OBST_BOX_TYPE=4
 INTEGER :: N_SIMPLE_CHEMISTRY_REACTIONS=1
 
 ! Species components of the lumped species
@@ -93,8 +94,7 @@ LOGICAL :: RADIATION=.TRUE.,RADIATION_COMPLETED=.TRUE.,EXCHANGE_RADIATION=.FALSE
            EXTERNAL_BOUNDARY_CORRECTION=.FALSE.,DUCT_HT=.FALSE.,DUCT_HT_INSERTED,PROCESS_ALL_MESHES=.FALSE.,&
            STORE_Q_DOT_PPP_S=.FALSE.,CORRECT_SUBGRID_TEMPERATURE=.FALSE.,STORE_OLD_VELOCITY=.FALSE.,&
            USE_ATMOSPHERIC_INTERPOLATION=.FALSE.,TWO_REAC_EXTINCT_TEST=.FALSE.,&
-           POSITIVE_ERROR_TEST=.FALSE.  ! If T, replace 'ERROR' with 'SUCCESS' label in certain error messages
-                                        ! Used to allow verification cases that check for error conditions in Firebot
+           POSITIVE_ERROR_TEST=.FALSE.,OBST_SHAPE_AREA_ADJUST=.FALSE.
 
 INTEGER :: BNDF_TIME_INTEGRALS=0
 
@@ -102,9 +102,9 @@ INTEGER, ALLOCATABLE, DIMENSION(:) :: CHANGE_TIME_STEP_INDEX
 
 ! Miscellaneous character strings
 
-CHARACTER(256) :: TITLE,RENDER_FILE,UVW_FILE='null'
-CHARACTER(40) :: CHID,RESTART_CHID
-CHARACTER(256) :: TERRAIN_IMAGE
+CHARACTER(255) :: TITLE,RENDER_FILE,UVW_FILE='null'
+CHARACTER(255) :: CHID,RESTART_CHID
+CHARACTER(255) :: TERRAIN_IMAGE
 
 ! Dates, version numbers, revision numbers
 

Source/init.f90
@@ -30,7 +30,7 @@ USE GEOMETRY_FUNCTIONS, ONLY: ASSIGN_PRESSURE_ZONE,ASSIGN_HT3D_WALL_INDICES
 USE RADCONS, ONLY: UIIDIM
 USE CONTROL_VARIABLES
 USE MATH_FUNCTIONS, ONLY:EVALUATE_RAMP
-INTEGER :: N,NN,I,J,K,IW,IC,SURF_INDEX,IOR,IERR,IIG,JJG,KKG,N_OVERLAP,IZERO
+INTEGER :: N,NN,I,J,K,IW,IC,SURF_INDEX,IOR,IERR,IIG,JJG,KKG,N_OVERLAP,IZERO,II,JJ,KK,I_OBST
 REAL(EB), INTENT(IN) :: DT
 INTEGER, INTENT(IN) :: NM
 REAL(EB) :: MU_N,ZZ_GET(1:N_TRACKED_SPECIES),CS,DELTA,DUMMY,INTEGRAL,TEMP
@@ -40,11 +40,13 @@ REAL(EB),POINTER :: XS,XF,YS,YF,ZS,ZF
 TYPE (INITIALIZATION_TYPE), POINTER :: IN=>NULL()
 TYPE (P_ZONE_TYPE), POINTER :: PZ=>NULL()
 TYPE (VENTS_TYPE), POINTER :: VT=>NULL()
-TYPE (OBSTRUCTION_TYPE), POINTER :: OB
+TYPE (OBSTRUCTION_TYPE), POINTER :: OB=>NULL()
 TYPE (WALL_TYPE), POINTER :: WC=>NULL()
 TYPE (SURFACE_TYPE), POINTER :: SF=>NULL()
-TYPE (MESH_TYPE), POINTER :: M
-TYPE (RAMPS_TYPE), POINTER :: RP
+TYPE (MESH_TYPE), POINTER :: M=>NULL()
+TYPE (RAMPS_TYPE), POINTER :: RP=>NULL()
+TYPE (MULTIPLIER_TYPE), POINTER :: MR=>NULL()
+
 
 IERR = 0
 M => MESHES(NM)
@@ -906,6 +908,84 @@ OBST_LOOP_2: DO N=1,M%N_OBST
    ENDDO
 
 ENDDO OBST_LOOP_2
+
+! For MULT OBST SHAPE, reset SF%AREA_ADJUST
+
+OBST_SHAPE_IF: IF (OBST_SHAPE_AREA_ADJUST) THEN
+
+   ! First, sum the face areas of the OBSTs with a given SURF
+   SHAPE_LOOP_1: DO IW=M%N_EXTERNAL_WALL_CELLS+1,M%N_EXTERNAL_WALL_CELLS+M%N_INTERNAL_WALL_CELLS
+      WC=>M%WALL(IW)
+      IF (WC%BOUNDARY_TYPE==NULL_BOUNDARY) CYCLE SHAPE_LOOP_1
+      II=WC%ONE_D%II
+      JJ=WC%ONE_D%JJ
+      KK=WC%ONE_D%KK
+      IOR=WC%ONE_D%IOR
+      IC=M%CELL_INDEX(II,JJ,KK)
+      I_OBST=M%OBST_INDEX_C(IC); IF (I_OBST==0)       CYCLE SHAPE_LOOP_1
+      OB=>M%OBSTRUCTION(I_OBST); IF (OB%MULT_INDEX<0) CYCLE SHAPE_LOOP_1
+      MR=>MULTIPLIER(OB%MULT_INDEX)
+      SHAPE_SELECT_1: SELECT CASE(OB%SHAPE_TYPE)
+         CASE(OBST_SPHERE_TYPE)
+            MR%FDS_AREA(1) = MR%FDS_AREA(1) + WC%ONE_D%AREA
+         CASE(OBST_CYLINDER_TYPE)
+            ! OB%SHAPE_AREA follows the same pattern as SURF_IDS: top, sides, bottom
+            SELECT CASE(IOR)
+               CASE(3);         MR%FDS_AREA(1) = MR%FDS_AREA(1) + WC%ONE_D%AREA ! top
+               CASE(-1,1,-2,2); MR%FDS_AREA(2) = MR%FDS_AREA(2) + WC%ONE_D%AREA ! side
+               CASE(-3);        MR%FDS_AREA(3) = MR%FDS_AREA(3) + WC%ONE_D%AREA ! bottom
+            END SELECT
+         CASE(OBST_CONE_TYPE)
+            SELECT CASE(IOR)
+               CASE(-1,1,-2,2,3); MR%FDS_AREA(1) = MR%FDS_AREA(1) + WC%ONE_D%AREA
+               CASE(-3);          MR%FDS_AREA(2) = MR%FDS_AREA(2) + WC%ONE_D%AREA
+            END SELECT
+         CASE(OBST_BOX_TYPE)
+            SELECT CASE(ABS(IOR))
+               CASE(1); MR%FDS_AREA(1) = MR%FDS_AREA(1) + WC%ONE_D%AREA
+               CASE(2); MR%FDS_AREA(2) = MR%FDS_AREA(2) + WC%ONE_D%AREA
+               CASE(3); MR%FDS_AREA(3) = MR%FDS_AREA(3) + WC%ONE_D%AREA
+            END SELECT
+      END SELECT SHAPE_SELECT_1
+   ENDDO SHAPE_LOOP_1
+
+   ! Next, AREA_ADJUST the WALL_CELL
+   SHAPE_LOOP_2: DO IW=M%N_EXTERNAL_WALL_CELLS+1,M%N_EXTERNAL_WALL_CELLS+M%N_INTERNAL_WALL_CELLS
+      WC=>M%WALL(IW)
+      IF (WC%BOUNDARY_TYPE==NULL_BOUNDARY) CYCLE SHAPE_LOOP_2
+      II=WC%ONE_D%II
+      JJ=WC%ONE_D%JJ
+      KK=WC%ONE_D%KK
+      IOR=WC%ONE_D%IOR
+      IC=M%CELL_INDEX(II,JJ,KK)
+      I_OBST=M%OBST_INDEX_C(IC); IF (I_OBST==0)       CYCLE SHAPE_LOOP_2
+      OB=>M%OBSTRUCTION(I_OBST); IF (OB%MULT_INDEX<0) CYCLE SHAPE_LOOP_2
+      MR=>MULTIPLIER(OB%MULT_INDEX)
+      IF (OB%SHAPE_TYPE>0) WC%ONE_D%AREA_ADJUST=1._EB
+      SHAPE_SELECT_2: SELECT CASE(OB%SHAPE_TYPE)
+         CASE(OBST_SPHERE_TYPE)
+            WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(1)/MR%FDS_AREA(1)
+         CASE(OBST_CYLINDER_TYPE)
+            SELECT CASE(IOR)
+               CASE(3);         WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(1)/MR%FDS_AREA(1) ! top
+               CASE(-1,1,-2,2); WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(2)/MR%FDS_AREA(2) ! side
+               CASE(-3);        WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(3)/MR%FDS_AREA(3) ! bottom
+            END SELECT
+         CASE(OBST_CONE_TYPE)
+            SELECT CASE(IOR)
+               CASE(-1,1,-2,2,3); WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(1)/MR%FDS_AREA(1)
+               CASE(-3);          WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(2)/MR%FDS_AREA(2)
+            END SELECT
+         CASE(OBST_BOX_TYPE)
+            SELECT CASE(ABS(IOR))
+               CASE(1); WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(1)/MR%FDS_AREA(1)
+               CASE(2); WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(2)/MR%FDS_AREA(2)
+               CASE(3); WC%ONE_D%AREA_ADJUST = OB%SHAPE_AREA(3)/MR%FDS_AREA(3)
+            END SELECT
+      END SELECT SHAPE_SELECT_2
+   ENDDO SHAPE_LOOP_2
+
+ENDIF OBST_SHAPE_IF
 
 ! Solid 3D heat and mass transfer
 

Source/read.f90
@@ -8322,13 +8322,12 @@ TYPE(OBSTRUCTION_TYPE), POINTER :: OB2=>NULL(),OBT=>NULL()
 TYPE(MULTIPLIER_TYPE), POINTER :: MR=>NULL()
 TYPE(OBSTRUCTION_TYPE), DIMENSION(:), ALLOCATABLE, TARGET :: TEMP_OBSTRUCTION
 INTEGER :: NM,NOM,N_OBST_O,IC,N,NN,NNN,NNNN,NR,N_NEW_OBST,RGB(3),N_OBST_DIM,II,JJ,KK,EVAC_N,MULT_INDEX,SHAPE_TYPE,PYRO3D_IOR
-INTEGER, PARAMETER :: SPHERE_TYPE=1,CYLINDER_TYPE=2,CONE_TYPE=3,BOX_TYPE=4
 CHARACTER(LABEL_LENGTH) :: ID,DEVC_ID,PROP_ID,SHAPE,SURF_ID,SURF_IDS(3),SURF_ID6(6),CTRL_ID,MULT_ID,MATL_ID
 CHARACTER(60) :: MESH_ID
 CHARACTER(25) :: COLOR
 LOGICAL :: EVACUATION_OBST,OVERLAY,PROCESS_OBSTS,IS_INTERSECT,PYRO3D_MASS_TRANSPORT
 REAL(EB) :: TRANSPARENCY,XB1,XB2,XB3,XB4,XB5,XB6,BULK_DENSITY,VOL_ADJUSTED,VOL_SPECIFIED,UNDIVIDED_INPUT_AREA(3),&
-            INTERNAL_HEAT_SOURCE,HEIGHT,RADIUS,XYZ(3),ORIENTATION(3),AABB(6),ROTMAT(3,3),THETA,LENGTH,WIDTH
+            INTERNAL_HEAT_SOURCE,HEIGHT,RADIUS,XYZ(3),ORIENTATION(3),AABB(6),ROTMAT(3,3),THETA,LENGTH,WIDTH,SHAPE_AREA(3)
 LOGICAL :: EMBEDDED,THICKEN,THICKEN_LOC,PERMIT_HOLE,ALLOW_VENT,EVACUATION,REMOVABLE,BNDF_FACE(-3:3),BNDF_OBST,OUTLINE,NOTERRAIN,&
            HT3D,WARN_HT3D,PYRO3D_RESIDUE
 NAMELIST /OBST/ ALLOW_VENT,BNDF_FACE,BNDF_OBST,BULK_DENSITY,&
@@ -8456,7 +8455,6 @@ MESH_LOOP: DO NM=1,NMESHES
       REMOVABLE   = .TRUE.
       XB          = -9.E30_EB
       SHAPE       = 'null'
-      SHAPE_TYPE  = -1
       XYZ         = 0._EB
       RADIUS      = -1._EB
       LENGTH      = -1._EB
@@ -8469,6 +8467,8 @@ MESH_LOOP: DO NM=1,NMESHES
       IF (.NOT.EVACUATION_ONLY(NM)) EVACUATION = .FALSE.
       IF (     EVACUATION_ONLY(NM)) EVACUATION = .TRUE.
       IF (     EVACUATION_ONLY(NM)) REMOVABLE  = .FALSE.
+      SHAPE_TYPE  = -1
+      SHAPE_AREA  = 0._EB
 
       ! Read the OBST line
 
@@ -8497,28 +8497,45 @@ MESH_LOOP: DO NM=1,NMESHES
       IF (TRIM(SHAPE)/='null') THEN
          SELECT CASE(TRIM(SHAPE))
             CASE('SPHERE')
-               SHAPE_TYPE = SPHERE_TYPE
+               SHAPE_TYPE = OBST_SPHERE_TYPE
+               SHAPE_AREA(1) = 4._EB*PI*RADIUS**2
             CASE('CYLINDER')
-               SHAPE_TYPE = CYLINDER_TYPE
+               SHAPE_TYPE = OBST_CYLINDER_TYPE
+               SHAPE_AREA(1) = PI*RADIUS**2
+               SHAPE_AREA(2) = 2._EB*PI*RADIUS*HEIGHT
+               SHAPE_AREA(3) = PI*RADIUS**2
                CALL ROTATION_MATRIX(ROTMAT,ORIENTATION,THETA)
             CASE('CONE')
-               SHAPE_TYPE = CONE_TYPE
+               SHAPE_TYPE = OBST_CONE_TYPE
+               SHAPE_AREA(1) = PI*RADIUS*( RADIUS + SQRT(HEIGHT**2 + RADIUS**2) ) - PI*RADIUS**2
+               SHAPE_AREA(2) = PI*RADIUS**2
                CALL ROTATION_MATRIX(ROTMAT,ORIENTATION,THETA)
             CASE('BOX')
-               SHAPE_TYPE = BOX_TYPE
+               SHAPE_TYPE = OBST_BOX_TYPE
+               SHAPE_AREA(1) = LENGTH*WIDTH
+               SHAPE_AREA(2) = WIDTH*HEIGHT
+               SHAPE_AREA(3) = HEIGHT*LENGTH
                CALL ROTATION_MATRIX(ROTMAT,ORIENTATION,THETA)
          END SELECT
-         IF ((SHAPE_TYPE==SPHERE_TYPE .OR. SHAPE_TYPE==CYLINDER_TYPE .OR. SHAPE_TYPE==CONE_TYPE) .AND. RADIUS<0._EB) THEN
+         IF ((SHAPE_TYPE==OBST_SPHERE_TYPE .OR. SHAPE_TYPE==OBST_CYLINDER_TYPE .OR. SHAPE_TYPE==OBST_CONE_TYPE) &
+            .AND. RADIUS<0._EB) THEN
             WRITE(MESSAGE,'(A,I0,A)')  'ERROR: OBST ',NN,' SHAPE requires RADIUS'
             CALL SHUTDOWN(MESSAGE) ; RETURN
          ENDIF
-         IF ((SHAPE_TYPE==CYLINDER_TYPE .OR. SHAPE_TYPE==CONE_TYPE) .AND. HEIGHT<0._EB) THEN
+         IF ((SHAPE_TYPE==OBST_CYLINDER_TYPE .OR. SHAPE_TYPE==OBST_CONE_TYPE) .AND. HEIGHT<0._EB) THEN
             WRITE(MESSAGE,'(A,I0,A)')  'ERROR: OBST ',NN,' SHAPE requires HEIGHT'
             CALL SHUTDOWN(MESSAGE) ; RETURN
          ENDIF
-         IF (SHAPE_TYPE==BOX_TYPE .AND. (LENGTH<0._EB .OR. WIDTH<0._EB .OR. HEIGHT<0._EB)) THEN
+         IF (SHAPE_TYPE==OBST_BOX_TYPE .AND. (LENGTH<0._EB .OR. WIDTH<0._EB .OR. HEIGHT<0._EB)) THEN
             WRITE(MESSAGE,'(A,I0,A)')  'ERROR: OBST ',NN,' BOX SHAPE requires LENGTH, WIDTH, HEIGHT'
             CALL SHUTDOWN(MESSAGE) ; RETURN
+         ENDIF
+         IF (ORIENTATION(1)==0._EB .AND. &
+             ORIENTATION(2)==0._EB .AND. &
+             ORIENTATION(3)==1._EB) THEN
+            OBST_SHAPE_AREA_ADJUST = .TRUE.
+         ELSE
+            WRITE (LU_ERR,'(A)') 'WARNING: AREA_ADJUST not applied to reoriented SHAPE. Check MASS FLUX if applicable.'
          ENDIF
       ENDIF
 
@@ -8526,7 +8543,10 @@ MESH_LOOP: DO NM=1,NMESHES
 
       MR => MULTIPLIER(0)
       DO NNN=1,N_MULT
-         IF (MULT_ID==MULTIPLIER(NNN)%ID) MR => MULTIPLIER(NNN)
+         IF (MULT_ID==MULTIPLIER(NNN)%ID) THEN
+            MULT_INDEX=NNN
+            MR => MULTIPLIER(NNN)
+         ENDIF
       ENDDO
 
       K_MULT_LOOP: DO KK=MR%K_LOWER,MR%K_UPPER
@@ -8717,23 +8737,20 @@ MESH_LOOP: DO NM=1,NMESHES
                   !AABB = (/XB1,XB2,XB3,XB4,XB5,XB6/)
                   AABB = (/X(OB%I1),X(OB%I2),Y(OB%J1),Y(OB%J2),Z(OB%K1),Z(OB%K2)/) ! possibly THICKENed OBST
                   SELECT CASE (SHAPE_TYPE)
-                     CASE (SPHERE_TYPE);   IS_INTERSECT = INTERSECT_SPHERE_AABB(XYZ,RADIUS,AABB)
-                     CASE (CYLINDER_TYPE); IS_INTERSECT = INTERSECT_CYLINDER_AABB(XYZ,HEIGHT,RADIUS,ROTMAT,AABB)
-                     CASE (CONE_TYPE);     IS_INTERSECT = INTERSECT_CONE_AABB(XYZ,HEIGHT,RADIUS,ROTMAT,AABB)
-                     CASE (BOX_TYPE);      IS_INTERSECT = INTERSECT_OBB_AABB(XYZ,LENGTH,WIDTH,HEIGHT,ROTMAT,AABB)
+                     CASE (OBST_SPHERE_TYPE);   IS_INTERSECT = INTERSECT_SPHERE_AABB(XYZ,RADIUS,AABB)
+                     CASE (OBST_CYLINDER_TYPE); IS_INTERSECT = INTERSECT_CYLINDER_AABB(XYZ,HEIGHT,RADIUS,ROTMAT,AABB)
+                     CASE (OBST_CONE_TYPE);     IS_INTERSECT = INTERSECT_CONE_AABB(XYZ,HEIGHT,RADIUS,ROTMAT,AABB)
+                     CASE (OBST_BOX_TYPE);      IS_INTERSECT = INTERSECT_OBB_AABB(XYZ,LENGTH,WIDTH,HEIGHT,ROTMAT,AABB)
                   END SELECT
                   IF (.NOT.IS_INTERSECT) THEN
                      N = N-1
                      N_OBST = N_OBST-1
                      CYCLE I_MULT_LOOP
                   ENDIF
-                  ! For now, we have to adjust the specified OBST box positions so that INPUT_AREA will be reasonable for SHAPE.
-                  OB%X1=X(OB%I1)
-                  OB%X2=X(OB%I2)
-                  OB%Y1=Y(OB%J1)
-                  OB%Y2=Y(OB%J2)
-                  OB%Z1=Z(OB%K1)
-                  OB%Z2=Z(OB%K2)
+                  ! Store SHAPE_AREA for area adjustment
+                  OB%SHAPE_TYPE = SHAPE_TYPE
+                  OB%SHAPE_AREA = SHAPE_AREA
+                  OB%MULT_INDEX = MULT_INDEX
                ENDIF
 
                ! Check to see if obstruction is completely embedded in another
@@ -13663,7 +13680,7 @@ END SUBROUTINE GET_PROPERTY_INDEX
 SUBROUTINE READ_CSVF
 USE OUTPUT_DATA
 
-CHARACTER(256) :: CSVFILE='null',UVWFILE='null'
+CHARACTER(255) :: CSVFILE='null',UVWFILE='null'
 NAMELIST /CSVF/ CSVFILE,UVWFILE
 
 N_CSVF=0

Source/type.f90
@@ -283,9 +283,9 @@ TYPE OBSTRUCTION_TYPE
    REAL(EB) :: TRANSPARENCY=1._EB,VOLUME_ADJUST=1._EB,BULK_DENSITY=-1._EB,INTERNAL_HEAT_SOURCE=0._EB
    REAL(EB), DIMENSION(3) :: TEXTURE=0._EB
    REAL(EB) :: X1=0._EB,X2=1._EB,Y1=0._EB,Y2=1._EB,Z1=0._EB,Z2=1._EB,MASS=1.E6_EB
-   REAL(EB), DIMENSION(3) :: FDS_AREA=-1._EB,INPUT_AREA=-1._EB,UNDIVIDED_INPUT_AREA=-1._EB
-   INTEGER :: I1=-1,I2=-1,J1=-1,J2=-1,K1=-1,K2=-1,COLOR_INDICATOR=-1,TYPE_INDICATOR=-1,ORDINAL=0
-   INTEGER :: DEVC_INDEX=-1,CTRL_INDEX=-1,PROP_INDEX=-1,DEVC_INDEX_O=-1,CTRL_INDEX_O=-1,MATL_INDEX=-1
+   REAL(EB), DIMENSION(3) :: FDS_AREA=-1._EB,INPUT_AREA=-1._EB,UNDIVIDED_INPUT_AREA=-1._EB,SHAPE_AREA=0._EB
+   INTEGER :: I1=-1,I2=-1,J1=-1,J2=-1,K1=-1,K2=-1,COLOR_INDICATOR=-1,TYPE_INDICATOR=-1,ORDINAL=0,SHAPE_TYPE=-1
+   INTEGER :: DEVC_INDEX=-1,CTRL_INDEX=-1,PROP_INDEX=-1,DEVC_INDEX_O=-1,CTRL_INDEX_O=-1,MATL_INDEX=-1,MULT_INDEX=-1
    LOGICAL :: HIDDEN=.FALSE.,PERMIT_HOLE=.TRUE.,ALLOW_VENT=.TRUE.,CONSUMABLE=.FALSE.,REMOVABLE=.FALSE., &
               HOLE_FILLER=.FALSE.,NOTERRAIN=.FALSE.,OVERLAY=.TRUE.
 
@@ -561,7 +561,7 @@ TYPE IBM_RCELL_TYPE
 END TYPE IBM_RCELL_TYPE
 
 TYPE CSVF_TYPE
-    CHARACTER(256) :: CSVFILE,UVWFILE
+    CHARACTER(255) :: CSVFILE,UVWFILE
 END TYPE CSVF_TYPE
 
 TYPE(CSVF_TYPE), ALLOCATABLE, DIMENSION(:) :: CSVFINFO
@@ -737,7 +737,7 @@ END TYPE P_ZONE_TYPE
 TYPE (P_ZONE_TYPE), DIMENSION(:), ALLOCATABLE, TARGET :: P_ZONE
 
 TYPE MULTIPLIER_TYPE
-   REAL(EB) :: DXB(6),DX0,DY0,DZ0
+   REAL(EB) :: DXB(6),DX0,DY0,DZ0,FDS_AREA(3)=0._EB
    INTEGER  :: I_LOWER,I_UPPER,J_LOWER,J_UPPER,K_LOWER,K_UPPER,N_LOWER,N_UPPER,N_COPIES
    LOGICAL, ALLOCATABLE, DIMENSION(:,:,:) :: SKIP
    CHARACTER(LABEL_LENGTH) :: ID

commit 96148a5191b18a9ab3d4c404f5f316e6099a0acc

FDS Source: fix error in fuel mass flux introduced by commit 431a93b

Source/wall.f90
@@ -2087,9 +2087,8 @@ METHOD_OF_MASS_TRANSFER: SELECT CASE(SPECIES_BC_INDEX)
                IF (PREDICTOR) TSI = T + DT - ONE_D%T_IGN
                IF (CORRECTOR) TSI = T      - ONE_D%T_IGN
             ENDIF
-            ONE_D%MASSFLUX(N) = EVALUATE_RAMP(TSI,SF%TAU(N),SF%RAMP_INDEX(N))*SF%MASS_FLUX(N)
-            ONE_D%MASSFLUX(N) = SF%ADJUST_BURN_RATE(N)*ONE_D%MASSFLUX(N)*ONE_D%AREA_ADJUST
-            ONE_D%MASSFLUX_SPEC(N) = ONE_D%MASSFLUX(N)
+            ONE_D%MASSFLUX_SPEC(N) = EVALUATE_RAMP(TSI,SF%TAU(N),SF%RAMP_INDEX(N))*SF%MASS_FLUX(N)*ONE_D%AREA_ADJUST
+            ONE_D%MASSFLUX(N)      = SF%ADJUST_BURN_RATE(N)*ONE_D%MASSFLUX_SPEC(N)
          ENDIF
          MFT = MFT + ONE_D%MASSFLUX(N)
       ENDDO SUM_MASSFLUX_LOOP