194d98a00aec40c85c74854c049e4b4f9a518b86
mcgratta <mcgratta@gmail.com>
2019-02-20 10:21:15 -0500

    FDS Source: Issue #7181. Isolate INIT-DEVC logic

--- Source/read.f90
+++ Source/read.f90
@@ -11887,7 +11887,11 @@ READ_DEVC_LOOP: DO NN=1,N_DEVC_READ
             XYZ(2) = MESHES(1)%YS
             XYZ(3) = MESHES(1)%ZS
             MESH_NUMBER = 1
-            MESH_DEVICE = 1
+            IF (INIT_ID/='null') THEN
+               MESH_DEVICE = 1  ! This is the case where a DEVC is assigned to particles specified on an INIT line
+            ELSE
+               IF (PROCESS(1)==MYID) MESH_DEVICE(1) = 1  ! This refers to HVAC or control logic
+            ENDIF
          ELSE
             IF (ALL(EVACUATION_ONLY)) CYCLE READ_DEVC_LOOP
             WRITE(MESSAGE,'(A,A,A)') 'WARNING: DEVC ',TRIM(ID),' is not within any mesh.'
