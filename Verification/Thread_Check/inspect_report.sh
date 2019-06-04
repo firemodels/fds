4cfe8ed287a3076a1cb44ba34a49ae4598169893
Devon Suarez <dsuarez1423@gmail.com>
2019-06-04 11:02:01 -0400

    Allow different folder names for inspect_report.sh

--- Verification/Thread_Check/inspect_report.sh
+++ Verification/Thread_Check/inspect_report.sh
@@ -10,7 +10,7 @@ REPORT_TYPE=problems
 showinput=
 
 function usage {
-echo "inspect_report.sh -h -R report-type -v output command]"
+echo "inspect_report.sh -h -R report-type -n directory-set -v output command]"
 echo "Report results from thread checker"
 echo ""
 echo "Options"
@@ -18,10 +18,12 @@ echo "-h - display this message"
 echo "-R report-type - type of report: problems or observations"
 echo "   [default: $REPORT_TYPE]"
 echo "-v - list command used to report thread checking results"
+echo "-n directory-set - prefix of directories with Inspector results"
+echo "   [default: $RESULT_DIR]" 
 exit
 }
 
-while getopts 'd:hr:R:v' OPTION
+while getopts 'd:hr:R:n:v' OPTION
 do
 case $OPTION in
   h)
@@ -32,6 +34,9 @@ case $OPTION in
    ;;
   v)
    showinput=1
+   ;;
+  n)
+   RESULT_DIR="$OPTARG"
    ;;
 esac
 done
@@ -49,7 +54,7 @@ if [ "$showinput" == "1" ] ; then
   exit
 fi
 
-find . -name "$RESULT_DIR*"|while read fname;
+find . -maxdepth 1 -name "$RESULT_DIR*"|while read fname;
 
 do
 echo $fname
