c9572c6a17c6f13f153900505fd81b7cd8e9d2c9
gforney <gforney@gmail.com>
2019-06-27 22:10:48 -0400

    FDS vv: add missing parameter to qfds.sh line in thread checking case lsit

--- Verification/FDS_thread_check.sh
+++ Verification/FDS_thread_check.sh
@@ -3,4 +3,4 @@
 # cases in this script are run with the openmp thread checker version of fds
 # these case are runot run in debug mode (as are other VV cases)
 
-$QFDS -T -d Thread_Check thread_check.fds
+$QFDS -T inspect -d Thread_Check thread_check.fds
