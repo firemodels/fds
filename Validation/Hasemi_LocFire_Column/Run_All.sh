6a684b8e5b4aa582e4e253e92408279acfbe4fcc
cnz2nist <sfeigineer@gmail.com>
2019-06-28 15:51:05 -0400

    Add Hasemi Localized fire data on steel column
    Hasemi et al. conducted a number of localzied fire tests on steel column.  A case was considered here. HT3D predicted steel temperatures were compared with the measured value. In the FDS model, TIME_SHRINK_FACTOR=10 was used. FDS model need to check. Discussion needed.

---
+++ Validation/Hasemi_LocFire_Column/Run_All.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script runs a set of Validation Cases on a Linux machine with a batch queuing system.
+# See the file Validation/Common_Run_All.sh for more information.
+export SVNROOT=`pwd`/../..
+source $SVNROOT/Validation/Common_Run_All.sh
+
+$QFDS $DEBUG $QUEUE -p 72 -n 18 -d $INDIR LocFire_Column_Test_1.fds
+
+echo FDS cases submitted
