14e1bce276d0f0166356a75bdb622f8af2fd3e4b
Devon Suarez <dsuarez1423@gmail.com>
2019-07-30 14:22:02 -0400

    Minor script update and basic README addition

--- Verification/Thread_Check/inspection.sh
+++ Verification/Thread_Check/inspection.sh
@@ -6,7 +6,7 @@ cd $CURDIR
 RESULT_DIR=$CURDIR/inspect_results
 PROCESSES=1
 QFDS="$GITROOT/fds/Utilities/Scripts/qfds.sh"
-QUEUE=
+QUEUE=batch
 
 
 function usage {
@@ -16,6 +16,7 @@ function usage {
   echo " -v   - list command that will be used to thread check"
   echo " -d   - select directory to store Inspector results"
   echo " -p   - select number of MPI processes"
+  echo " -q   - select batch queue"
   echo "input_file - input file"
   echo ""
   exit
