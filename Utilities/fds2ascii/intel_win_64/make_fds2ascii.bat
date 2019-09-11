b329a961ad71c08fe5425a873a83c6f7410be906
gforney <gforney@gmail.com>
2019-09-11 08:47:44 -0400

    fds2ascii build: correct path to compiler setup in build script

--- Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
+++ Utilities/fds2ascii/intel_win_64/make_fds2ascii.bat
@@ -1,7 +1,7 @@
 @echo off
 set from=%1
 :: setup compiler environment
-call ..\..\Scripts\setup_intel_compilers.bat
+call ..\..\..\Build\Scripts\setup_intel_compilers.bat
 
 Title Building fds2ascii for 64 bit Windows
 
