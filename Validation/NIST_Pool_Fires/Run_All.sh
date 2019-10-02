59d9d868dd410c28b52bb08bbb322ce9b4f6f757
mcgratta <mcgratta@gmail.com>
2019-10-02 17:00:34 -0400

    FDS Validation: Add 1 m methanol input files

--- Validation/NIST_Pool_Fires/Run_All.sh
+++ Validation/NIST_Pool_Fires/Run_All.sh
@@ -18,5 +18,8 @@ $QFDS $DEBUG -p 32 -n 8 $QUEUE -d $INDIR NIST_Methanol_Prescribed_0p5cm.fds
 $QFDS $DEBUG -p 96 -n 8 $QUEUE -d $INDIR NIST_Methanol_1m_pan_1cm_grid.fds
 $QFDS $DEBUG -p 12 -n 6 $QUEUE -d $INDIR NIST_Methanol_1m_pan_2cm_grid.fds
 $QFDS $DEBUG -p  2 -n 2 $QUEUE -d $INDIR NIST_Methanol_1m_pan_4cm_grid.fds
+$QFDS $DEBUG -p 96 -n 4 $QUEUE -d $INDIR NIST_Methanol_1m_pan_1cm_grid_predicted.fds
+$QFDS $DEBUG -p 12 -n 4 $QUEUE -d $INDIR NIST_Methanol_1m_pan_2cm_grid_predicted.fds
+$QFDS $DEBUG -p  2 -n 2 $QUEUE -d $INDIR NIST_Methanol_1m_pan_4cm_grid_predicted.fds
 
 echo FDS cases submitted
