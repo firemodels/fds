18b45cd7c2162d10aa60bb09560cc0d89c2dc29f
rmcdermo <randy.mcdermott@gmail.com>
2019-11-08 15:55:27 -0500

    FDS Validation: minor mod

--- Validation/UMD_Burning_Rate_Emulator/Process_Output.sh
+++ Validation/UMD_Burning_Rate_Emulator/Process_Output.sh
@@ -2,10 +2,11 @@
 PDIR=../../../out
 CUR=`pwd`
 DIR=`basename $CUR`
- 
+
 WDIR=$PDIR/$DIR
 DDIR=Current_Results
 cp $DDIR/*hrr.csv $WDIR
+cp $DDIR/*line.csv $WDIR
 cp $DDIR/*devc.csv $WDIR
 cp $DDIR/*git.txt $WDIR
 
