5276a20cfcf872700635e265c7a4f91f8a3a4ca7
gforney <gforney@gmail.com>
2020-01-27 09:00:40 -0500

    scripts: add ssh option to copy figures script to copy figures using scp (avoids nfs cross mounting problem)

--- Manuals/Copy_Firebot_Figures.sh
+++ Manuals/Copy_Firebot_Figures.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-export FIREBOTROOT=/home2/smokevis2/firebot/FireModels_clone/fds/ # blaze firebot
-#FIREBOTROOT=/home4/firebot/FireModels_clone/fds/ # burn firebot
+USE_SSH=1
+
+#uncomment following line to use regular copies (assumes that firebot directories are cross mounted on this computer)
+#USE_SSH=
+
+if [ "$USE_SSH" == "" ]; then
+  CP=cp
+  export FIREBOTROOT=/home2/smokevis2/firebot/FireModels_clone/fds/ # blaze firebot
+  #FIREBOTROOT=/home4/firebot/FireModels_clone/fds/                 # burn firebot
+fi
+
+if [ "$USE_SSH" == "1" ]; then
+  CP="scp -q"
+  export FIREBOTROOT=blaze.el.nist.gov:/home2/smokevis2/firebot/FireModels_clone/fds/ # blaze firebot
+  #FIREBOTROOT=burn.el.nist.gov:/home4/firebot/FireModels_clone/fds/                  # burn firebot
+fi
+
 export FIREBOTMANS=$FIREBOTROOT/Manuals/
 export FIREBOTVER=$FIREBOTROOT/Verification/
 export FIREBOTVAL=$FIREBOTROOT/Validation/
@@ -12,18 +27,18 @@ export FBVAL=$FIREBOTMANS/FDS_Validation_Guide/
 export BASEDIR=`pwd`
 
 # Copy Tech Guide Figures
-cp $FBTG/SCRIPT_FIGURES/* $BASEDIR/FDS_Technical_Reference_Guide/SCRIPT_FIGURES/
+$CP $FBTG/SCRIPT_FIGURES/* $BASEDIR/FDS_Technical_Reference_Guide/SCRIPT_FIGURES/
 echo Tech Guide Figures Copied
 
 # Copy User's Guide Figures
-cp $FBUG/SCRIPT_FIGURES/* $BASEDIR/FDS_User_Guide/SCRIPT_FIGURES/
+$CP $FBUG/SCRIPT_FIGURES/* $BASEDIR/FDS_User_Guide/SCRIPT_FIGURES/
 echo Users Guide Figures Copied
 
 # Copy Verification Guide Figures
-cp $FBVG/SCRIPT_FIGURES/*.pdf $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/.
-cp $FBVG/SCRIPT_FIGURES/*.png $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/.
-cp $FBVG/SCRIPT_FIGURES/*.tex $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/.
-cp $FBVG/SCRIPT_FIGURES/Scatterplots/*.tex $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/Scatterplots/.
+$CP $FBVG/SCRIPT_FIGURES/*.pdf $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/.
+$CP $FBVG/SCRIPT_FIGURES/*.png $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/.
+$CP $FBVG/SCRIPT_FIGURES/*.tex $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/.
+$CP $FBVG/SCRIPT_FIGURES/Scatterplots/*.tex $BASEDIR/FDS_Verification_Guide/SCRIPT_FIGURES/Scatterplots/.
 echo Verification Figures Copied
 
 # Copy Validation Guide Figures
