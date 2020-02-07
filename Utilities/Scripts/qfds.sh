837ce92f83663090f8c87b67f4b98a169ee07623
= <=>
2020-02-07 13:38:37 -0500

    qfds.sh: Add ppn option for SLURM

--- Utilities/Scripts/qfds.sh
+++ Utilities/Scripts/qfds.sh
@@ -764,6 +764,7 @@ if [ "$queue" != "none" ]; then
 #SBATCH -n $n_mpi_processes
 ####SBATCH --nodes=$nodes
 #SBATCH --cpus-per-task=$n_openmp_threads
+#SBATCH --ntasks-per-node=$ppn
 $SLURM_MEM
 EOF
     if [ "$walltimestring_slurm" != "" ]; then
