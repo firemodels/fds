3ddc659fa027650632c22630b300fb231e255a9d
mcgratta <mcgratta@gmail.com>
2020-03-13 10:23:03 -0400

    FDS Source: Issue #8121. Fix bugs in droplet mass and movement on GEOMs

--- Verification/FDS_Cases.sh
+++ Verification/FDS_Cases.sh
@@ -577,6 +577,7 @@ $QFDS -d Sprinklers_and_Sprays droplet_distributions.fds
 $QFDS -d Sprinklers_and_Sprays droplet_distributions_2.fds
 $QFDS -d Sprinklers_and_Sprays flow_rate.fds
 $QFDS -d Sprinklers_and_Sprays flow_rate_2.fds
+$QFDS -p 2 -d Sprinklers_and_Sprays geom_sprk_mass.fds
 $QFDS -d Sprinklers_and_Sprays particle_colors.fds
 $QFDS -d Sprinklers_and_Sprays particle_drag_U10_N16.fds
 $QFDS -d Sprinklers_and_Sprays particle_drag_U50_N16.fds
