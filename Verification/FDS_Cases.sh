7f6b05c4b12fbb1e3cccd0b6e7ee94206b3e8511
Jason Floyd <drjfloyd@gmail.com>
2020-04-04 07:43:52 -0400

    FDS Verification: Add box_burnaway8.fds to FDS_Cases.sh

--- Verification/FDS_Cases.sh
+++ Verification/FDS_Cases.sh
@@ -180,6 +180,7 @@ $QFDS -d Fires box_burn_away4.fds
 $QFDS -d Fires box_burn_away5.fds
 $QFDS -d Fires box_burn_away6.fds
 $QFDS -d Fires box_burn_away7.fds
+$QFDS -d Fires box_burn_away8.fds
 $QFDS -d Fires box_burn_away_2D.fds
 $QFDS -d Fires box_burn_away_2D_residue.fds
 $QFDS -d Fires box_burn_away1_pyro3d_vs_pyro1d.fds
