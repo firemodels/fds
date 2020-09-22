e3291218d01ef3f4dc5b4becec61292499448236
gforney <gforney@gmail.com>
2020-09-22 16:24:31 -0400

    FDS vv: remove geom_arch case from a couple of scripts

--- Verification/FDS_Cases_Subset.sh
+++ Verification/FDS_Cases_Subset.sh
@@ -4,7 +4,6 @@ $QFDS -d Complex_Geometry geom_obst.fds
 $QFDS -d Complex_Geometry geom_intersection.fds
 $QFDS -d Complex_Geometry geom_bad_inverted_normals.fds
 $QFDS -d Complex_Geometry geom_simple.fds
-$QFDS -d Complex_Geometry geom_arch.fds
 $QFDS -d Complex_Geometry sphere_helium_1mesh.fds
 $QFDS -d Complex_Geometry sphere_radiate.fds
 $QFDS -d Complex_Geometry geom_self_intersection.fds
