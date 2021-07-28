700568a162ba606b399d5f5050860428f36e7617
mcgratta <mcgratta@gmail.com>
2021-07-28 14:01:07 -0400

    FDS Validation: Add Shell_LNG_Fireballs

---
+++ Validation/Shell_LNG_Fireballs/Run_All.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script runs a set of Validation Cases on a Linux machine with a batch queuing system.
+# See the file Validation/Common_Run_All.sh for more information.
+export SVNROOT=`pwd`/../..
+source $SVNROOT/Validation/Common_Run_All.sh
+
+$QFDS $DEBUG $QUEUE -p 200 -d $INDIR Exp_2.fds
+$QFDS $DEBUG $QUEUE -p 200 -d $INDIR Exp_3.fds
+$QFDS $DEBUG $QUEUE -p 200 -d $INDIR Exp_4.fds
+
+echo FDS cases submitted
