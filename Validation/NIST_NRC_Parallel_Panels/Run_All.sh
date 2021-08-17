48133a1537085197a47fc7f7c399d8d3775477bc
mcgratta <mcgratta@gmail.com>
2021-08-17 18:05:39 -0400

    FDS Validation: Remove -n option from all run scripts

--- Validation/NIST_NRC_Parallel_Panels/Run_All.sh
+++ Validation/NIST_NRC_Parallel_Panels/Run_All.sh
@@ -5,8 +5,8 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-$QFDS $DEBUG -p 96 -n 8 $QUEUE -d $INDIR PBT_60_kW.fds
-$QFDS $DEBUG -p 96 -n 8 $QUEUE -d $INDIR PMMA_60_kW.fds
-$QFDS $DEBUG -p 96 -n 8 $QUEUE -d $INDIR PVC_60_kW.fds
+$QFDS $DEBUG -p 96 $QUEUE -d $INDIR PBT_60_kW.fds
+$QFDS $DEBUG -p 96 $QUEUE -d $INDIR PMMA_60_kW.fds
+$QFDS $DEBUG -p 96 $QUEUE -d $INDIR PVC_60_kW.fds
 
 echo FDS cases submitted
