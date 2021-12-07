af4ca216dd5fdfeea252e0e5a1591641b7716562
mcgratta <mcgratta@gmail.com>
2021-12-07 12:28:43 -0500

    FDS Source: Introduce boundary patch Poisson solver

--- Source/cons.f90
+++ Source/cons.f90
@@ -471,6 +471,7 @@ LOGICAL :: ITERATE_PRESSURE=.FALSE.                              !< Flag indicat
 LOGICAL :: ITERATE_BAROCLINIC_TERM                               !< Flag indicating if baroclinic term is iterated
 LOGICAL :: SUSPEND_PRESSURE_ITERATIONS=.TRUE.                    !< Flag for stopping pressure iterations
 LOGICAL :: INSEPARABLE_POISSON=.FALSE.                           !< Flag for solving the inseparable Poisson equation
+LOGICAL :: POISSON_ITERATION_ACCELERATOR=.FALSE.                 !< Flag for solving a Poisson equation at mesh interfaces
 REAL(EB) :: VELOCITY_TOLERANCE=0._EB                             !< Error tolerance for normal velocity at solids or boundaries
 REAL(EB) :: PRESSURE_TOLERANCE=0._EB                             !< Error tolerance for iteration of baroclinic pressure term
 REAL(EB) :: ITERATION_SUSPEND_FACTOR=0.95_EB                     !< If new velocity error is not this value of old, stop iteration
