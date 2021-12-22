8684c6eeccc24aa0fb5d2c2dcc475c6277c02d14
gforney <gforney@gmail.com>
2021-12-22 17:37:33 -0500

    FDS build: add option to build with ifx or ifort oneapi compilers

--- Build/impi_intel_linux_64_db/make_fds.sh
+++ Build/impi_intel_linux_64_db/make_fds.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+ARG=$1
+
+source ../Scripts/set_intel_compiler.sh $ARG
 
 dir=`pwd`
 target=${dir##*/}
 
-echo Building $target with Intel MPI
+echo Building $target with Intel MPI and $INTEL_IFORT
 make VPATH="../../Source" -f ../makefile $target
