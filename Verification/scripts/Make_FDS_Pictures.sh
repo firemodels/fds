00c4a3b0ea56027cbaa9db1dc6ffae3b61da6a70
mcgratta <mcgratta@gmail.com>
2022-02-03 16:52:57 -0500

    FDS Verification: Add accuracy metrics for beam_detector case

--- Verification/scripts/Make_FDS_Pictures.sh
+++ Verification/scripts/Make_FDS_Pictures.sh
@@ -95,12 +95,5 @@ if [ "$START_X" == "yes" ]; then
   source $SVNROOT/fds/Utilities/Scripts/stopXserver.sh 2>/dev/null
 fi
 
-# test smoke opacity levels in beam_detector case
-
-#cd $SVNROOT/fds/Verification/Detectors
-#../scripts/compare_csv.sh beam_detector_ss_check.csv beam_detector_ss.csv 2 0
-#../scripts/compare_csv.sh beam_detector_ss_check.csv beam_detector_ss.csv 3 0
-#../scripts/compare_csv.sh beam_detector_ss_check.csv beam_detector_ss.csv 4 0
-
 cd $CURDIR
 echo $progname complete
