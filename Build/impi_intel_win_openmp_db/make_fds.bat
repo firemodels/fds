5e2befd6dcbe8a51216dfb4e35eab8f4928a08b6
gforney <gforney@gmail.com>
2022-04-28 14:07:50 -0400

    fds build: correct windows makefile target names in windows build batch files

--- Build/impi_intel_win_openmp_db/make_fds.bat
+++ Build/impi_intel_win_openmp_db/make_fds.bat
@@ -6,9 +6,9 @@ if x%arg1% == xbot goto endif1
 call ..\Scripts\setup_intel_compilers.bat
 :endif1
 
-Title Building FDS (Intel MPI/%INTEL_IFORT%) for 64 bit Windows
+Title Building FDS (Intel MPI/%INTEL_IFORT% OpenMP) for 64 bit Windows
 
-make SHELL="%ComSpec%" VPATH="../../Source" -f ..\makefile impi_intel_win_64
+make SHELL="%ComSpec%" VPATH="../../Source" -f ..\makefile impi_intel_win_openmp_db
 if x%arg1% == xbot goto endif2
 pause
 :endif2
