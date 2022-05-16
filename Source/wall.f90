87d8f24e3cbc9b1cf4ba8c5c0a64830be9d72505
Jason Floyd <drjfloyd@gmail.com>
2022-05-16 07:29:37 -0400

    FDS Source: For 1DHT change stored value of HTCF to last value computed.

--- Source/wall.f90
+++ Source/wall.f90
@@ -3787,11 +3787,11 @@ DTMP = TMP_G-0.5_EB*(ONE_D%TMP_F_OLD+ONE_D%TMP_F)
 
 ONE_D%TMP_F_OLD = ONE_D%TMP_F  ! Save this value for use at the next time step
 
-IF (ABS(DTMP)>TWO_EPSILON_EB .AND. ABS(ONE_D%Q_CON_F)>TWO_EPSILON_EB) THEN
-   ONE_D%HEAT_TRANS_COEF = ONE_D%Q_CON_F/DTMP
-ELSE
+!IF (ABS(DTMP)>TWO_EPSILON_EB .AND. ABS(ONE_D%Q_CON_F)>TWO_EPSILON_EB) THEN
+!   ONE_D%HEAT_TRANS_COEF = ONE_D%Q_CON_F/DTMP
+!ELSE
    ONE_D%HEAT_TRANS_COEF = HTCF
-ENDIF
+!ENDIF
 
 ! If any gas massflux is non-zero or the surface temperature exceeds the ignition temperature, set the ignition time
 
