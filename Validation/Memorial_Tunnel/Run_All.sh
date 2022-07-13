d98d0c2c7bc9e03e30fb9da32ed78a549bd068dc
= <=>
2022-07-13 15:44:29 -0400

    FDS Validation: Edits to Memorial Tunnel inputs

--- Validation/Memorial_Tunnel/Run_All.sh
+++ Validation/Memorial_Tunnel/Run_All.sh
@@ -5,9 +5,23 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_501.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_502.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_605.fds
 $QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_606A.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_607.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_608.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_610.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_611.fds
 $QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_612B.fds
 $QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_615B.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_617A.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_618A.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_621A.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_622B.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_623B.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_624B.fds
+$QFDS -p 84 $DEBUG $QUEUE -d $INDIR Test_625B.fds
 $QFDS -p 84 $DEBUG $QUEUE -d $INDIR Cold_Flow_Series_1.fds
 
 echo FDS cases submitted
