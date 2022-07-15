1885296b6e94fa1f0a3fa5aa60f47a60d3c09eda
nec3 <noellecrump2023@gmail.com>
2022-07-15 13:08:08 -0400

    FDS Validation: USFS Deep Fuel Beds, fixed os mv (again)

--- Validation/USFS_Deep_Fuel_Beds/FDS_Input_Files/Build_Input_Files/build_input_files.py
+++ Validation/USFS_Deep_Fuel_Beds/FDS_Input_Files/Build_Input_Files/build_input_files.py
@@ -104,7 +104,7 @@ dfout.to_csv('paramfile.csv', index=False)
 os.system('python ../../../../Utilities/Input_File_Tools/swaps.py')
 
 # Move inpupt files up one level to FDS_Input_Files
-for f in dfout['theobald_Template.fds']:
+for f in dfout['Template.fds']:
     if os.path.isfile('../' + f):
         os.remove('../' + f)
     os.rename(f, '../' + f)
