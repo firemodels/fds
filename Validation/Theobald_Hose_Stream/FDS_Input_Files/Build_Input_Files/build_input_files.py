951a93a53ecbfc70a39e03b73b59b4866c25d6b8
rmcdermo <randy.mcdermott@gmail.com>
2022-08-05 13:13:42 -0400

    FDS Validation: updates to Theobald input files

--- Validation/Theobald_Hose_Stream/FDS_Input_Files/Build_Input_Files/build_input_files.py
+++ Validation/Theobald_Hose_Stream/FDS_Input_Files/Build_Input_Files/build_input_files.py
@@ -5,9 +5,20 @@
 import math
 import pandas as pd
 import os
+import numpy as np
 
 # read input data to dataframe
 df = pd.read_csv('../../../../../exp/Theobald_Hose_Stream/theobald_effect_1981_fds.csv', header=0)
+sf10 = pd.read_csv('../../../../../exp/Theobald_Hose_Stream/Nozzle_10_shape_factor.csv', header=0)
+sf = pd.read_csv('../../../../../exp/Theobald_Hose_Stream/Table1_relative_shape_factors.csv', header=0)
+
+def shape_factor_10(V):
+    Shape_Factor_10 = np.interp(V,sf10['V (m/s)'].values,sf10['S_f_10'].values)
+    return Shape_Factor_10
+
+def rel_shape_factor(V,Nozzle_Name):
+    Rel_Shape_Factor = np.interp(V,sf['V (m/s)'].values,sf[Nozzle_Name].values)
+    return Rel_Shape_Factor
 
 # initalize matrix to write final dataframe
 FINAL = []
@@ -41,14 +52,24 @@ for irow in df.index:
     orient_z = round(math.tan(math.radians(noz_angle)),2)           # z of orientation vector (1,0,z)
     efx_velo = 3.71*math.sqrt(noz_pres*14.504)       # efflux velocity in m/s, (bar->psi before calc)
     flowrate = round((efx_velo*math.pi*(noz_dmtr/2.0)**2)*60000.0,2) # flow rate in L/min (from m3/s)
-    We = 13864.0*noz_dmtr*efx_velo**2              # Weber Number (@ 20C, diameter(m), velocity(m/s))
-    Re = 996605.43*noz_dmtr*efx_velo            # Reynolds Number (@ 20C, diameter(m), velocity(m/s))
-    breaklength50 = 1.7*noz_dmtr*(We**0.5)*(Re*1e-4)**(-0.625) # length @ stream 50% discontinuous(m)
+    rho = 999.                                     # density of water in kg/m3
+    sigma = 72.e-3                                 # surface tension of water in N/m
+    We = rho*noz_dmtr*efx_velo**2/sigma            # Weber Number (@ 20C, diameter(m), velocity(m/s))
+    mu = 1.0016e-3                                 # dynamic viscosity Pa*s
+    Re = rho*noz_dmtr*efx_velo/mu                  # Reynolds Number (@ 20C, diameter(m), velocity(m/s))
 
-    fdsbreaklength = breaklength50*1.0        # FDS PRIMARY_BREAKUP_LENGTH (m)
+    S_f_10 = shape_factor_10(efx_velo)
+    Rel_S_f = rel_shape_factor(efx_velo,noz_name)
+
+    breaklength50 = Rel_S_f*S_f_10*noz_dmtr*(We**0.5)*(Re*1e-4)**0.625 # length @ stream 50% discontinuous(m), Theobald Eq. (2)
+
+    fdsbreaklength = breaklength50*2.0             # FDS PRIMARY_BREAKUP_LENGTH (m)
 
     paramline = [filename] + [CHID] + [TITLE] + [round(efx_velo,3)] + [flowrate] + [orient_z]
     paramline = paramline  + [max_range] + [round((max_range+0.1),1)] + [round(fdsbreaklength,2)]
+
+    # set droplet diameter to 1/10th of nozzle diameter (allow secondary breakup if necessary)
+    paramline = paramline  + [noz_dmtr*1.e6/10.]
 
 # add paramline to FINAL matrix for each irow
     FINAL = FINAL + [paramline]
