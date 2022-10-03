3c33ecdbee29876aa5305a38ef3f0a4c264c00fa
= <=>
2022-10-03 14:05:38 -0400

    FDS Verification: Add HT3D cases

--- Verification/FDS_Cases.sh
+++ Verification/FDS_Cases.sh
@@ -273,6 +273,8 @@ $QFDS -d Heat_Transfer insulated_steel_column.fds
 $QFDS -d Heat_Transfer insulated_steel_pipe.fds
 $QFDS -d Heat_Transfer insulated_steel_plate.fds
 $QFDS -d Heat_Transfer ht3d_energy_conservation.fds
+$QFDS -d Heat_Transfer ht3d_energy_conservation_2.fds
+$QFDS -d Heat_Transfer ht3d_energy_conservation_3.fds
 $QFDS -d Heat_Transfer ht3d_ibeam.fds
 $QFDS -d Heat_Transfer ht3d_network.fds
 $QFDS -d Heat_Transfer ht3d_slab.fds
