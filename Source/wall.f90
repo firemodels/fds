54054706c370e078680de274b8a680de38892f80
Jonathan Hodges <johodges@vt.edu>
2022-10-21 13:30:12 -0400

    Replace Q_CONF_F with Q_CON_F

--- Source/wall.f90
+++ Source/wall.f90
@@ -2401,7 +2401,7 @@ METHOD_OF_MASS_TRANSFER: SELECT CASE(SPECIES_BC_INDEX)
       ! Calculate smoothed incident heat flux if cone scaling is applied
       IF (SF%CONE_HEAT_FLUX > 0._EB) THEN
          TSI = MIN(T-T_BEGIN+DT, SF%CONE_FLUX_SMOOTHING_WINDOW)
-         ONE_D%Q_IN_SMOOTH = (ONE_D%Q_IN_SMOOTH *(TSI-DT) + DT*(ONE_D%Q_CONF_F+ONE_D%Q_RAD_IN))/TSI
+         ONE_D%Q_IN_SMOOTH = (ONE_D%Q_IN_SMOOTH *(TSI-DT) + DT*(ONE_D%Q_CON_F+ONE_D%Q_RAD_IN))/TSI
       ENDIF
 
       ! If the current time is before the "activation" time, T_IGN, apply simple BCs and get out
