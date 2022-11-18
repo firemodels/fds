179bec0e39487b75bcf6b3dd74e67c070ce8b82f
Julio Cesar Silva <arquitetojulio@gmail.com>
2022-11-18 19:46:57 -0300

    fds2ftmi: adapting file naming and folder structure

--- Utilities/Structural_Interaction/fds2ftmi/verification/simple_panel_verification.sh
+++ Utilities/Structural_Interaction/fds2ftmi/verification/simple_panel_verification.sh
@@ -1,8 +1,8 @@
-../../../../Build/impi_intel_linux_64/fds_impi_intel_linux_64 simple_panel.fds 
+../../../../Build/impi_intel_linux/fds_impi_intel_linux simple_panel.fds 
 cd ..
-cd intel_linux_64
+cd intel_linux
 echo Y | make_fds2ftmi.bat
 cd ..
 cd verification
-../intel_linux_64/fds2ftmi_linux_64 simple_panel 0.05 2 0 598 2 0 0 1 simple_panel_verification 
+../intel_linux/fds2ftmi_linux simple_panel 0.05 2 0 598 2 0 0 1 simple_panel_verification 
 python plots_verification.py
