2a283c5baca207f1962021099fc14d55876f234b
Julio Cesar Silva <arquitetojulio@gmail.com>
2023-03-09 19:22:13 -0300

    fds2ftmi: updating linux scripts

--- Utilities/Structural_Interaction/fds2ftmi/examples/simple_panel_hot/simple_panel_hot.sh
+++ Utilities/Structural_Interaction/fds2ftmi/examples/simple_panel_hot/simple_panel_hot.sh
@@ -1,6 +1,6 @@
-../../../../../Build/impi_intel_linux_64/fds_impi_intel_linux_64 simple_panel_hot.fds 
+../../../../../Build/impi_intel_linux/fds_impi_intel_linux simple_panel_hot.fds 
 %ANSYS% -b nolist -j simple_panel_hot_ansys -i read_geometry_hot.ans
-../../intel_linux_64/fds2ftmi_linux_64 simple_panel_hot 0.05 2 0 600 2 0 0 1 simple_panel_hot_to_ansys 
+../../intel_linux/fds2ftmi_linux simple_panel_hot 0.05 2 0 600 2 0 0 1 simple_panel_hot_to_ansys 
 %ANSYS% -b nolist -j simple_panel_hot_ansys -i run_simple_panel_hot.ans 
 %ANSYS% -b nolist -j simple_panel_hot_ansys -i plot_hot.ans 
 export DISPLAY=localhost:0.0 #
