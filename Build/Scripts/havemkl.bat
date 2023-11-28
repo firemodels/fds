1dcef2e4ed5a8980ebd8df068f7f09979d02f237
Glenn Forney <gforney@gmail.com>
2023-11-28 15:39:36 -0500

    FDS build: fix to havemkl.bat script defining mkl library location (since location changed with Intel oneAPI 2024)

--- Build/Scripts/havemkl.bat
+++ Build/Scripts/havemkl.bat
@@ -1,10 +1,13 @@
 @echo off
 if exist "%MKLROOT%\lib\intel64\mkl_blacs_intelmpi_lp64.lib" (
+set "MKLLIBDIR=%MKLROOT%\lib\intel64"
 echo 1
 exit /b
 )
 if exist "%MKLROOT%\lib\mkl_blacs_intelmpi_lp64.lib" (
+set "MKLLIBDIR=%MKLROOT%\lib"
 echo 1
 exit /b
 )
-echo 1
+echo 0
+exit /b
