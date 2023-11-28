18e00bbbf5ca490ec06334bc79a13a13c4dc8271
ericvmueller <ericvmueller@gmail.com>
2023-11-28 16:01:34 -0500

    FDS Source: update particle drag for cut cells to use CUTFACE_VELOCITIES; use inverse-distance weighting for particle-mesh interpolation

--- Source/func.f90
+++ Source/func.f90
@@ -3416,66 +3416,6 @@ COUNTS(IND)=COUNTS(IND)+WEIGHT
 END SUBROUTINE UPDATE_HISTOGRAM
 
 
-!> \brief Linearly interpolate the a mesh quantity onto a point
-!> \param X The interpolated value of the 3D array
-!> \param A The 3D array of values
-!> \param I The lower x index of the array
-!> \param J The lower y index of the array
-!> \param K The lower z index of the array
-!> \param P Fraction of the distance from the lower to upper x coordinate
-!> \param R Fraction of the distance from the lower to upper y coordinate
-!> \param S Fraction of the distance from the lower to upper z coordinate
-
-SUBROUTINE MESH_TO_PARTICLE(X,A,I,J,K,P,R,S)
-
-REAL(EB), INTENT(IN), DIMENSION(0:,0:,0:) :: A
-INTEGER, INTENT(IN) :: I,J,K
-REAL(EB), INTENT(IN) :: P,R,S
-REAL(EB), INTENT(OUT) :: X
-REAL(EB) :: PP,RR,SS
-
-PP = 1._EB-P
-RR = 1._EB-R
-SS = 1._EB-S
-X  = ((PP*A(I,J,K)  +P*A(I+1,J,K)  )*RR+(PP*A(I,J+1,K)  +P*A(I+1,J+1,K)  )*R)*SS + &
-     ((PP*A(I,J,K+1)+P*A(I+1,J,K+1))*RR+(PP*A(I,J+1,K+1)+P*A(I+1,J+1,K+1))*R)*S
-
-END SUBROUTINE MESH_TO_PARTICLE
-
-
-!> \brief Linearly interpolate the value at a point onto the mesh
-!> \param X The interpolated value of the 3D array
-!> \param A The 3D array of values
-!> \param I The lower x index of the array
-!> \param J The lower y index of the array
-!> \param K The lower z index of the array
-!> \param P Fraction of the distance from the lower to upper x coordinate
-!> \param R Fraction of the distance from the lower to upper y coordinate
-!> \param S Fraction of the distance from the lower to upper z coordinate
-
-SUBROUTINE PARTICLE_TO_MESH(X,A,I,J,K,P,R,S)
-
-REAL(EB), INTENT(INOUT), DIMENSION(0:,0:,0:) :: A
-INTEGER, INTENT(IN) :: I,J,K
-REAL(EB), INTENT(IN) :: P,R,S
-REAL(EB), INTENT(IN) :: X
-REAL(EB) :: PP,RR,SS
-
-PP = 1._EB-P
-RR = 1._EB-R
-SS = 1._EB-S
-A(I  ,J  ,K  ) = A(I  ,J  ,K  ) - X*PP*RR*SS
-A(I+1,J  ,K  ) = A(I+1,J  ,K  ) - X*P *RR*SS
-A(I  ,J+1,K  ) = A(I  ,J+1,K  ) - X*PP*R *SS
-A(I+1,J+1,K  ) = A(I+1,J+1,K  ) - X*P *R *SS
-A(I  ,J  ,K+1) = A(I  ,J  ,K+1) - X*PP*RR*S
-A(I+1,J  ,K+1) = A(I+1,J  ,K+1) - X*P *RR*S
-A(I  ,J+1,K+1) = A(I  ,J+1,K+1) - X*PP*R *S
-A(I+1,J+1,K+1) = A(I+1,J+1,K+1) - X*P *R *S
-
-END SUBROUTINE PARTICLE_TO_MESH
-
-
 !> \brief Calculate the value of polynomial function.
 !> \param N Number of coefficients in the polynomial
 !> \param TEMP The independent variable
