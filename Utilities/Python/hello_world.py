2fc333fb2f43cfeb4504f3149cd384bd61217057
rmcdermo <randy.mcdermott@gmail.com>
2024-03-28 13:38:57 -0400

    Python: add test script for firebot

---
+++ Utilities/Python/hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#McDermott
+#2024-03-28
+
+import struct
+import math
+import numpy
+import matplotlib
+import pandas
+import scipy
+import seaborn
+import pygments
+
+print('Hello World!')
