d646002052341fc9797307bc29b6f076ef358230
Randy McDermott <randy.mcdermott@gmail.com>
2024-04-02 15:57:03 -0400

    Python: begin construction of FDS_verification_script.py

---
+++ Utilities/Python/FDS_verification_script.py
@@ -0,0 +1,19 @@
+# McDermott
+# 2 April 2024
+# FDS_verification_script.py
+
+import subprocess
+
+# Scripts to run prior to dataplot
+
+print("ignition_delay...");   subprocess.run(["python3","./scripts/ignition_delay.py"])
+
+# Dataplot and scatplot options
+
+# Statistics output options
+
+# Run dataplot and scatplot scripts
+
+# Special cases
+
+print("verification scripts completed successfully!")
