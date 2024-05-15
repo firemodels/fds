fd9db1b542d1746ee79cb5386fc6978c9cae26f3
mcgratta <mcgratta@gmail.com>
2024-05-15 17:21:18 -0400

    FDS Source: Issue #12924. Remove HRRPUL and RHRRPUL

--- Utilities/Matlab/scripts/flame_height.m
+++ Utilities/Matlab/scripts/flame_height.m
@@ -2,36 +2,29 @@
 % 2-4-11
 % flame_height.m
 %
-% replaces old flame_height.f90
-%
 % integrates HRRPUL(z) from *_line.csv file to determine L_f/D (normalized flame height)
 
-%close all
-%clear all
-
-% confirm heat release rate
-check_hrr
+check_hrr  % confirm heat release rate
 
 outdir = '../../../out/Heskestad_Flame_Height/';
 expdir = '../../../exp/Heskestad_Flame_Height/';
 
-% list of line files
-filename = {'Qs=p1_RI=05_line.csv',    'Qs=p1_RI=10_line.csv',    'Qs=p1_RI=20_line.csv';   ...
-            'Qs=p2_RI=05_line.csv',    'Qs=p2_RI=10_line.csv',    'Qs=p2_RI=20_line.csv';   ...
-            'Qs=p5_RI=05_line.csv',    'Qs=p5_RI=10_line.csv',    'Qs=p5_RI=20_line.csv';   ...
-            'Qs=1_RI=05_line.csv',     'Qs=1_RI=10_line.csv',     'Qs=1_RI=20_line.csv';    ...
-            'Qs=2_RI=05_line.csv',     'Qs=2_RI=10_line.csv',     'Qs=2_RI=20_line.csv';    ...
-            'Qs=5_RI=05_line.csv',     'Qs=5_RI=10_line.csv',     'Qs=5_RI=20_line.csv';    ...
-            'Qs=10_RI=05_line.csv',    'Qs=10_RI=10_line.csv',    'Qs=10_RI=20_line.csv';   ...
-            'Qs=20_RI=05_line.csv',    'Qs=20_RI=10_line.csv',    'Qs=20_RI=20_line.csv';   ...
-            'Qs=50_RI=05_line.csv',    'Qs=50_RI=10_line.csv',    'Qs=50_RI=20_line.csv';   ...
-            'Qs=100_RI=05_line.csv',   'Qs=100_RI=10_line.csv',   'Qs=100_RI=20_line.csv';  ...
-            'Qs=200_RI=05_line.csv',   'Qs=200_RI=10_line.csv',   'Qs=200_RI=20_line.csv';  ...
-            'Qs=500_RI=05_line.csv',   'Qs=500_RI=10_line.csv',   'Qs=500_RI=20_line.csv';  ...
-            'Qs=1000_RI=05_line.csv',  'Qs=1000_RI=10_line.csv',  'Qs=1000_RI=20_line.csv'; ...
-            'Qs=2000_RI=05_line.csv',  'Qs=2000_RI=10_line.csv',  'Qs=2000_RI=20_line.csv'; ...
-            'Qs=5000_RI=05_line.csv',  'Qs=5000_RI=10_line.csv',  'Qs=5000_RI=20_line.csv'; ...
-            'Qs=10000_RI=05_line.csv', 'Qs=10000_RI=10_line.csv', 'Qs=10000_RI=20_line.csv'};
+filename = {'Qs=p1_RI=05_devc.csv',    'Qs=p1_RI=10_devc.csv',    'Qs=p1_RI=20_devc.csv';   ...
+            'Qs=p2_RI=05_devc.csv',    'Qs=p2_RI=10_devc.csv',    'Qs=p2_RI=20_devc.csv';   ...
+            'Qs=p5_RI=05_devc.csv',    'Qs=p5_RI=10_devc.csv',    'Qs=p5_RI=20_devc.csv';   ...
+            'Qs=1_RI=05_devc.csv',     'Qs=1_RI=10_devc.csv',     'Qs=1_RI=20_devc.csv';    ...
+            'Qs=2_RI=05_devc.csv',     'Qs=2_RI=10_devc.csv',     'Qs=2_RI=20_devc.csv';    ...
+            'Qs=5_RI=05_devc.csv',     'Qs=5_RI=10_devc.csv',     'Qs=5_RI=20_devc.csv';    ...
+            'Qs=10_RI=05_devc.csv',    'Qs=10_RI=10_devc.csv',    'Qs=10_RI=20_devc.csv';   ...
+            'Qs=20_RI=05_devc.csv',    'Qs=20_RI=10_devc.csv',    'Qs=20_RI=20_devc.csv';   ...
+            'Qs=50_RI=05_devc.csv',    'Qs=50_RI=10_devc.csv',    'Qs=50_RI=20_devc.csv';   ...
+            'Qs=100_RI=05_devc.csv',   'Qs=100_RI=10_devc.csv',   'Qs=100_RI=20_devc.csv';  ...
+            'Qs=200_RI=05_devc.csv',   'Qs=200_RI=10_devc.csv',   'Qs=200_RI=20_devc.csv';  ...
+            'Qs=500_RI=05_devc.csv',   'Qs=500_RI=10_devc.csv',   'Qs=500_RI=20_devc.csv';  ...
+            'Qs=1000_RI=05_devc.csv',  'Qs=1000_RI=10_devc.csv',  'Qs=1000_RI=20_devc.csv'; ...
+            'Qs=2000_RI=05_devc.csv',  'Qs=2000_RI=10_devc.csv',  'Qs=2000_RI=20_devc.csv'; ...
+            'Qs=5000_RI=05_devc.csv',  'Qs=5000_RI=10_devc.csv',  'Qs=5000_RI=20_devc.csv'; ...
+            'Qs=10000_RI=05_devc.csv', 'Qs=10000_RI=10_devc.csv', 'Qs=10000_RI=20_devc.csv'};
 
 rho_inf = 1.2;
 cp = 1;
@@ -44,35 +37,18 @@ Qdot=[151 303 756 1513 3025 7564 15127 30255 75636 151273 302545 756363 1512725 
 
 for i=1:16 % hrr loop
     for j=1:3 % resolution loop
-        
         M = csvread([outdir,filename{i,j}],2,0);
-        z = M(:,1); dz = z(2)-z(1);
-        hrrpul = M(:,2);
-        Qdot_line = sum(hrrpul)*dz;
+        L(j) = M(end,2);
         Qstar = Qdot(i)/(rho_inf*cp*T_inf*sqrt(g)*D^(5/2));
         LfD = 3.7*Qstar^(2/5)-1.02; % Heskestad correlation Lf/D
-        
-        % determine flame height
-        for n=1:length(z)
-            hrr(n) = sum(hrrpul(1:n))*dz; % cummulative heat release
-        end
-        k = find(hrr>f*Qdot_line,1);
-        if (k>1) 
-            L(j) = z(k-1)+dz*(f*Qdot(i)-hrr(k-1))/(hrr(k)-hrr(k-1));
-        elseif (k==1)
-            L(j) = dz*f*Qdot(i)/hrr(k);
-        else
-            L(j) = 0.;
-        end
-        
-    end % resolution loop
-    
+    end
     W(i,1:4) = [Qstar L(1)/D L(2)/D L(3)/D];
     H(i,1:2) = [Qstar LfD];
-    
-end % hrr loop
+end
 
 fclose('all');
+
+% Write a file with FDS-predicted flame heights
 
 header1 = {'Q*','L/D (RI=5)','L/D (RI=10)','L/D (RI=20)'};
 filename1 = [outdir,'FDS_Flame_Height.csv'];
@@ -82,6 +58,8 @@ for i=1:16
     fprintf(fid,'%f, %f, %f, %f\n',W(i,:));
 end
 fclose(fid);
+
+% Write a file with Heskestad correlation
   
 header2 = {'Q*','L/D'};
 filename2 = [expdir,'Heskestad_Correlation.csv'];
@@ -140,23 +118,4 @@ for j=1:3 % resolution loop
     fclose(fid);
     
 end % resolution loop
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
