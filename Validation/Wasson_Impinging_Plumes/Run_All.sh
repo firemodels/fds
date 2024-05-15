ba2093035f6e8d126170ab784240e420af1da95b
rmcdermo <randy.mcdermott@gmail.com>
2024-05-15 16:34:25 -0400

    FDS Validation: remove comments

--- Validation/Wasson_Impinging_Plumes/Run_All.sh
+++ Validation/Wasson_Impinging_Plumes/Run_All.sh
@@ -5,8 +5,8 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-#$QFDS $DEBUG -p 4  $QUEUE -d $INDIR wasson_50kW_1m_50mm.fds
-#$QFDS $DEBUG -p 4  $QUEUE -d $INDIR wasson_50kW_1m_25mm.fds
+$QFDS $DEBUG -p 4  $QUEUE -d $INDIR wasson_50kW_1m_50mm.fds
+$QFDS $DEBUG -p 4  $QUEUE -d $INDIR wasson_50kW_1m_25mm.fds
 $QFDS $DEBUG -p 32 $QUEUE -d $INDIR wasson_50kW_1m_12mm.fds
 
 echo FDS cases submitted
