80232c4e8b888111666c66b2d3544b4b0532cc93
Randy McDermott <randy.mcdermott@gmail.com>
2024-05-31 11:20:10 -0400

    FDS Validation: update Run_All.sh

--- Validation/OMP_Scaling_Tests/Run_All.sh
+++ Validation/OMP_Scaling_Tests/Run_All.sh
@@ -5,19 +5,19 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-$QFDS $DEBUG $QUEUE -t -o 1 -A -d $INDIR openmp_test64a.fds
-$QFDS $DEBUG $QUEUE -t -o 2 -A -d $INDIR openmp_test64b.fds
-$QFDS $DEBUG $QUEUE -t -o 3 -A -d $INDIR openmp_test64c.fds
-$QFDS $DEBUG $QUEUE -t -o 4 -A -d $INDIR openmp_test64d.fds
-$QFDS $DEBUG $QUEUE -t -o 5 -A -d $INDIR openmp_test64e.fds
-$QFDS $DEBUG $QUEUE -t -o 6 -A -d $INDIR openmp_test64f.fds
-$QFDS $DEBUG $QUEUE -t -o 7 -A -d $INDIR openmp_test64g.fds
-$QFDS $DEBUG $QUEUE -t -o 8 -A -d $INDIR openmp_test64h.fds
-$QFDS $DEBUG $QUEUE -t -o 1 -A -d $INDIR openmp_test128a.fds
-$QFDS $DEBUG $QUEUE -t -o 2 -A -d $INDIR openmp_test128b.fds
-$QFDS $DEBUG $QUEUE -t -o 3 -A -d $INDIR openmp_test128c.fds
-$QFDS $DEBUG $QUEUE -t -o 4 -A -d $INDIR openmp_test128d.fds
-$QFDS $DEBUG $QUEUE -t -o 5 -A -d $INDIR openmp_test128e.fds
-$QFDS $DEBUG $QUEUE -t -o 6 -A -d $INDIR openmp_test128f.fds
-$QFDS $DEBUG $QUEUE -t -o 7 -A -d $INDIR openmp_test128g.fds
-$QFDS $DEBUG $QUEUE -t -o 8 -A -d $INDIR openmp_test128h.fds
+$QFDS $DEBUG $QUEUE -t -o 1 -d $INDIR openmp_test64a.fds
+$QFDS $DEBUG $QUEUE -t -o 2 -d $INDIR openmp_test64b.fds
+$QFDS $DEBUG $QUEUE -t -o 3 -d $INDIR openmp_test64c.fds
+$QFDS $DEBUG $QUEUE -t -o 4 -d $INDIR openmp_test64d.fds
+$QFDS $DEBUG $QUEUE -t -o 5 -d $INDIR openmp_test64e.fds
+$QFDS $DEBUG $QUEUE -t -o 6 -d $INDIR openmp_test64f.fds
+$QFDS $DEBUG $QUEUE -t -o 7 -d $INDIR openmp_test64g.fds
+$QFDS $DEBUG $QUEUE -t -o 8 -d $INDIR openmp_test64h.fds
+$QFDS $DEBUG $QUEUE -t -o 1 -d $INDIR openmp_test128a.fds
+$QFDS $DEBUG $QUEUE -t -o 2 -d $INDIR openmp_test128b.fds
+$QFDS $DEBUG $QUEUE -t -o 3 -d $INDIR openmp_test128c.fds
+$QFDS $DEBUG $QUEUE -t -o 4 -d $INDIR openmp_test128d.fds
+$QFDS $DEBUG $QUEUE -t -o 5 -d $INDIR openmp_test128e.fds
+$QFDS $DEBUG $QUEUE -t -o 6 -d $INDIR openmp_test128f.fds
+$QFDS $DEBUG $QUEUE -t -o 7 -d $INDIR openmp_test128g.fds
+$QFDS $DEBUG $QUEUE -t -o 8 -d $INDIR openmp_test128h.fds
