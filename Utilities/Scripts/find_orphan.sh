01a82f4c01982ac4503cd0da0a9994c235433eb4
gforney <gforney@gmailcom>
2024-07-05 18:16:26 -0400

    scripts: also search for orphans in .bat files

--- Utilities/Scripts/find_orphan.sh
+++ Utilities/Scripts/find_orphan.sh
@@ -14,15 +14,18 @@ echo "*** looking for $file in $BOTREPO"
 echo "************************************************"
 cd $BOTREPO
 grep -r $file --include=*sh
+grep -r $file --include=*bat
 
 cd $FDSREPO
 echo "************************************************"
 echo "*** looking for $file in $FDSREPO"
 echo "************************************************"
 grep -r $file --include=*sh
+grep -r $file --include=*bat
 
 cd $SMVREPO
 echo "************************************************"
 echo "*** looking for $file in $SMVREPO"
 echo "************************************************"
 grep -r $file --include=*sh
+grep -r $file --include=*bat
