eae47c84b66b575897d880da8a5d75a8d9dd9cb8
gforney <gforney@gmail.com>
2024-08-12 10:14:43 -0400

    scripts: minor updates to sundials build script

--- Utilities/Scripts/build_sundials.bat
+++ Utilities/Scripts/build_sundials.bat
@@ -1,17 +1,16 @@
 @echo off
-set abort=0
-set have_setx=1
-
 call :getopts %*
 if %stopscript% == 1 exit /b
 
+set have_setx=1
 call :have_program setx       || set have_setx=0
 
+set abort=0
 call :is_file_installed cmake || set abort=1
 call :is_file_installed gcc   || set abort=1
 if %abort% == 1 exit /b
 
-:: don't checkout for 32 bit gcc for now
+:: don't check for 32 bit gcc for now
 :: gcc -v 1> find.out 2>&1
 :: type find.out | find /i /c "Target: mingw32" > find_count.out
 :: set /p have_gcc32=<find_count.out
@@ -19,7 +18,7 @@ if %abort% == 1 exit /b
 :: if %have_gcc32% == 0 echo ***error: Installed version of MinGW is not 32 bit
 :: if %have_gcc32% == 0 exit /b
 
-set INSTALLDIR=C:\sundials-6.7.0
+set INSTALLDIR=C:\sundials_test
 set SUNDIALSVERSION=v6.7.0
 
 set CURDIR=%CD%
