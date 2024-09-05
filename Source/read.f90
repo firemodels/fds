62a2892d59b7b948c1f5e5f93c0f9bda70694749
mcgratta <mcgratta@gmail.com>
2024-09-05 16:08:29 -0400

    FDS Source: Issue #13391. Make MB and DB consistent

--- Source/read.f90
+++ Source/read.f90
@@ -11859,7 +11859,8 @@ MESH_LOOP_1: DO NM=1,NMESHES
 
                IF (SURF_ID=='OPEN')                            VT%TYPE_INDICATOR =  2
                IF (SURF_ID=='MIRROR' .OR. SURF_ID=='PERIODIC') VT%TYPE_INDICATOR = -2
-               IF ((MB/='null' .OR.  PBX>-1.E5_EB .OR. PBY>-1.E5_EB .OR. PBZ>-1.E5_EB) .AND. SURF_ID=='OPEN') VT%TYPE_INDICATOR=-2
+               IF ((DB/='null' .OR. MB/='null' .OR.  PBX>-1.E5_EB .OR. PBY>-1.E5_EB .OR. PBZ>-1.E5_EB) &
+                    .AND. SURF_ID=='OPEN') VT%TYPE_INDICATOR=-2
                IF (SURF_ID=='PERIODIC FLOW ONLY') VT%SURF_INDEX = PERIODIC_FLOW_ONLY_SURF_INDEX
 
                VT%BOUNDARY_TYPE = SOLID_BOUNDARY
