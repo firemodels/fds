d0d14444e65cf751e4139059541aea808c27fc18
rmcdermo <randy.mcdermott@gmail.com>
2024-10-28 17:20:46 -0400

    Build: check CC compiler type in HYPRE build

--- Build/Scripts/HYPRE/confmake_impi_intel_linux.sh
+++ Build/Scripts/HYPRE/confmake_impi_intel_linux.sh
@@ -1,3 +1,13 @@
-./configure CC=mpiicc FC=mpiifort CFLAGS="-O3 -fno-unsafe-math-optimizations -fp-model=precise" FFLAGS="-O3 -fno-unsafe-math-optimizations -fp-model=precise" \
+# Check for mpiicx or mpiicc
+if command -v mpiicx &> /dev/null; then
+    CC=mpiicx
+elif command -v mpiicc &> /dev/null; then
+    CC=mpiicc
+else
+    echo "Error: Neither mpiicx nor mpiicc is available on this system."
+    exit 1
+fi
+
+./configure CC=$CC FC=mpiifort CFLAGS="-O3 -fno-unsafe-math-optimizations -fp-model=precise" FFLAGS="-O3 -fno-unsafe-math-optimizations -fp-model=precise" \
             --prefix=$FIREMODELS/libs/hypre/$HYPRE_VERSION
 make install
