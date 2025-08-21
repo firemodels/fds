5e1b05135dbd3a40b95d78fc716398187a1c8dae
rmcdermo <randy.mcdermott@gmail.com>
2025-08-21 11:10:29 -0400

    Matlab: remove MP5 scheme verification scripts

--- Utilities/Matlab/scripts/soborot_mass_transport.m
+++ Utilities/Matlab/scripts/soborot_mass_transport.m
@@ -23,10 +23,6 @@ chid = {'soborot_charm_cos_wave_16', ...
         'soborot_godunov_square_wave_16', ...
         'soborot_godunov_square_wave_32', ...
         'soborot_godunov_square_wave_64', ...
-        'soborot_mp5_cos_wave_128', ...
-        'soborot_mp5_cos_wave_16', ...
-        'soborot_mp5_cos_wave_32', ...
-        'soborot_mp5_cos_wave_64', ...
         'soborot_superbee_cos_wave_128', ...
         'soborot_superbee_cos_wave_16', ...
         'soborot_superbee_cos_wave_32', ...
@@ -423,62 +419,6 @@ set(gcf,'PaperSize',[Paper_Width Paper_Height]);
 set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
 print(gcf,'-dpdf',[plot_dir,'soborot_superbee_cos_wave']);
 
-% FLUX_LIMITER='MP5'
-
-M = importdata([data_dir,'soborot_mp5_cos_wave_16_devc.csv'],',',2);
-col_start = find(strcmp(M.colheaders,'"Y_TRACER-1"'));
-col_end   = find(strcmp(M.colheaders,'"Y_TRACER-16"'));
-Y_mp5_16 = M.data(end,col_start:col_end);
-
-M = importdata([data_dir,'soborot_mp5_cos_wave_32_devc.csv'],',',2);
-col_start = find(strcmp(M.colheaders,'"Y_TRACER-1"'));
-col_end   = find(strcmp(M.colheaders,'"Y_TRACER-32"'));
-Y_mp5_32 = M.data(end,col_start:col_end);
-
-M = importdata([data_dir,'soborot_mp5_cos_wave_64_devc.csv'],',',2);
-col_start = find(strcmp(M.colheaders,'"Y_TRACER-1"'));
-col_end   = find(strcmp(M.colheaders,'"Y_TRACER-64"'));
-Y_mp5_64 = M.data(end,col_start:col_end);
-
-M = importdata([data_dir,'soborot_mp5_cos_wave_128_devc.csv'],',',2);
-col_start = find(strcmp(M.colheaders,'"Y_TRACER-1"'));
-col_end   = find(strcmp(M.colheaders,'"Y_TRACER-128"'));
-Y_mp5_128 = M.data(end,col_start:col_end);
-
-figure
-set(gca,'Units',Plot_Units)
-set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
-Y_exact = zeros(1,length(r_exact));
-i_range = find(r_exact>0.25 & r_exact<0.75);
-Y_exact(i_range) = 0.5*(1. + cos(4.*pi*r_exact(i_range))); % see PERIODIC_TEST==13 in wall.f90
-H(1)=plot(r_exact,Y_exact,'k-'); hold on
-H(2)=plot(r_16,Y_mp5_16,'bo-');
-H(3)=plot(r_32,Y_mp5_32,'m*-');
-H(4)=plot(r_64,Y_mp5_64,'r^-');
-H(5)=plot(r_128,Y_mp5_128,'gsq-');
-
-xlabel('Radial Position (m)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
-ylabel('Scalar Mass Fraction','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
-axis([0 1 0 1.2])
-text(.05,1.1,'MP5','FontName',Font_Name,'FontSize',Label_Font_Size)
-
-set(gca,'FontName',Font_Name)
-set(gca,'FontSize',Label_Font_Size)
-
-lh=legend(H,'Exact','{\itn}=16','{\itn}=32','{\itn}=64','{\itn}=128');
-set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
-legend boxoff
-
-Git_Filename = [data_dir,'soborot_mp5_cos_wave_16_git.txt'];
-addverstr(gca,Git_Filename,'linear')
-
-set(gcf,'Visible',Figure_Visibility);
-set(gcf,'Units',Paper_Units);
-set(gcf,'PaperUnits',Paper_Units);
-set(gcf,'PaperSize',[Paper_Width Paper_Height]);
-set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
-print(gcf,'-dpdf',[plot_dir,'soborot_mp5_cos_wave']);
-
 % plot error
 
 Y_exact_16 = interp1(r_exact,Y_exact,r_16);
@@ -496,11 +436,6 @@ e_superbee_32 = norm(Y_superbee_32-Y_exact_32,1)/length(r_32);
 e_superbee_64 = norm(Y_superbee_64-Y_exact_64,1)/length(r_64);
 e_superbee_128 = norm(Y_superbee_128-Y_exact_128,1)/length(r_128);
 
-e_mp5_16 = norm(Y_mp5_16-Y_exact_16,1)/length(r_16);
-e_mp5_32 = norm(Y_mp5_32-Y_exact_32,1)/length(r_32);
-e_mp5_64 = norm(Y_mp5_64-Y_exact_64,1)/length(r_64);
-e_mp5_128 = norm(Y_mp5_128-Y_exact_128,1)/length(r_128);
-
 dx = L./[16 32 64 128];
 
 figure
@@ -510,7 +445,6 @@ H(1)=loglog(dx,dx.^1,'k-.'); hold on
 H(2)=loglog(dx,dx.^2,'k--');
 H(3)=loglog(dx,[e_charm_16 e_charm_32 e_charm_64 e_charm_128],'ko-');
 H(4)=loglog(dx,[e_superbee_16 e_superbee_32 e_superbee_64 e_superbee_128],'k*-');
-H(5)=loglog(dx,[e_mp5_16 e_mp5_32 e_mp5_64 e_mp5_128],'ksq-');
 
 % trap cosine wave error
 if e_charm_128>2.9e-04
@@ -518,9 +452,6 @@ if e_charm_128>2.9e-04
 end
 if e_superbee_128>6.4e-04
     display(['Error: soborot_charm_square_wave_128 out of tolerance'])
-end
-if e_mp5_128>7.9e-05
-    display(['Error: soborot_superbee_square_wave_128 out of tolerance'])
 end
 
 axis([min(dx) .1 min(dx.^2) max(dx)])
@@ -531,7 +462,7 @@ text(.009,.04,'Cosine Wave Error','FontName',Font_Name,'FontSize',Label_Font_Siz
 set(gca,'FontName',Font_Name)
 set(gca,'FontSize',Label_Font_Size)
 
-lh=legend(H,'{\itO}(\delta{\itx})','{\itO}(\delta{\itx}^2)','CHARM','Superbee','MP5');
+lh=legend(H,'{\itO}(\delta{\itx})','{\itO}(\delta{\itx}^2)','CHARM','Superbee');
 set(lh,'Location','Southeast')
 set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
 legend boxoff
