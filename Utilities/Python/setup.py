0b8af0de7f307c6d5f12cea81870e596f5a318fd
Randy McDermott <randy.mcdermott@gmail.com>
2025-09-28 10:06:24 -0400

    Python: change comment

--- Utilities/Python/setup.py
+++ Utilities/Python/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="fdsplotlib",
     version="0.0.0",
-    py_modules=["fdsplotlib"],  # dummy commit to debug actions
+    py_modules=["fdsplotlib"],  # package a single file
 )
 
