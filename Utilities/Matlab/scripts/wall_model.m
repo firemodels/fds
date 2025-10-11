4177229e6188e1725d07a5bd5e71fc3c5b5be5d6
mcgratta <mcgratta@gmail.com>
2025-10-11 17:45:43 -0400

    FDS Verification: Convert yplus and heated_channel

--- Utilities/Matlab/scripts/wall_model.m
+++ Utilities/Matlab/scripts/wall_model.m
@@ -5,8 +5,6 @@
 % This script generates all plots necessary for the wall model section
 % of the verification guide.
 
-yplus                       % Near-wall grid resolution
-heated_channel              % Heated Channel Flow
 blasius                     % Blasius boundary layer
 blasius_thermal             % Pohlhausen boundary layer
 ribbed_channel              % Ribbed channel flow
