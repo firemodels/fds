35b423dac8dbb34daecb9865a0a9d31ca489c2cd
mcgratta <mcgratta@gmail.com>
2026-02-04 13:44:49 -0500

    FDS Source: Align vectors at mesh boundaries in SMV

--- Source/init.f90
+++ Source/init.f90
@@ -1437,7 +1437,7 @@ EDGE_DIRECTION_2: SELECT CASE(IEC)
                ED%JJO_1 = JJO
                MM => MESHES(NOM)
                ZK  = MIN( REAL(MM%KBAR,EB)+ONE_M_EPS , MM%CELLSK(NINT((M%Z(KK)-MM%ZS)*MM%RDZINT))+1._EB )
-               KKO = MAX(1,FLOOR(ZK))
+               KKO = MIN(MM%KBAR,MAX(1,FLOOR(ZK)))
                ED%EDGE_INTERPOLATION_FACTOR(1) = ZK-KKO
                ED%KKO_1 = KKO
             CASE(3)
@@ -1446,7 +1446,7 @@ EDGE_DIRECTION_2: SELECT CASE(IEC)
                ED%IIO_2 = IIO
                MM => MESHES(NOM)
                YJ  = MIN( REAL(MM%JBAR,EB)+ONE_M_EPS , MM%CELLSJ(NINT((M%Y(JJ)-MM%YS)*MM%RDYINT))+1._EB )
-               JJO = MAX(1,FLOOR(YJ))
+               JJO = MIN(MM%JBAR,MAX(1,FLOOR(YJ)))
                ED%EDGE_INTERPOLATION_FACTOR(2) = YJ-JJO
                ED%JJO_2 = JJO
                ED%KKO_2 = KKO
@@ -1475,7 +1475,7 @@ EDGE_DIRECTION_2: SELECT CASE(IEC)
                ED%JJO_2 = JJO
                MM => MESHES(NOM)
                ZK  = MIN( REAL(MM%KBAR,EB)+ONE_M_EPS , MM%CELLSK(NINT((M%Z(KK)-MM%ZS)*MM%RDZINT))+1._EB )
-               KKO = MAX(1,FLOOR(ZK))
+               KKO = MIN(MM%KBAR,MAX(1,FLOOR(ZK)))
                ED%EDGE_INTERPOLATION_FACTOR(2) = ZK-KKO
                ED%KKO_2 = KKO
             CASE( 3)
@@ -1483,7 +1483,7 @@ EDGE_DIRECTION_2: SELECT CASE(IEC)
                IF (IOR<0) ED%NOM_1 =  NOM
                MM => MESHES(NOM)
                XI  = MIN( REAL(MM%IBAR,EB)+ONE_M_EPS , MM%CELLSI(NINT((M%X(II)-MM%XS)*MM%RDXINT))+1._EB )
-               IIO = MAX(1,FLOOR(XI))
+               IIO = MIN(MM%IBAR,MAX(1,FLOOR(XI)))
                ED%EDGE_INTERPOLATION_FACTOR(1) = XI-IIO
                ED%IIO_1 = IIO
                ED%JJO_1 = JJO
@@ -1512,7 +1512,7 @@ EDGE_DIRECTION_2: SELECT CASE(IEC)
                ED%IIO_1 = IIO
                MM => MESHES(NOM)
                YJ  = MIN( REAL(MM%JBAR,EB)+ONE_M_EPS , MM%CELLSJ(NINT((M%Y(JJ)-MM%YS)*MM%RDYINT))+1._EB )
-               JJO = MAX(1,FLOOR(YJ))
+               JJO = MIN(MM%JBAR,MAX(1,FLOOR(YJ)))
                ED%EDGE_INTERPOLATION_FACTOR(1) = YJ-JJO
                ED%JJO_1 = JJO
                ED%KKO_1 = KKO
@@ -1521,7 +1521,7 @@ EDGE_DIRECTION_2: SELECT CASE(IEC)
                IF (IOR<0) ED%NOM_2 =  NOM
                MM => MESHES(NOM)
                XI  = MIN( REAL(MM%IBAR,EB)+ONE_M_EPS , MM%CELLSI(NINT((M%X(II)-MM%XS)*MM%RDXINT))+1._EB )
-               IIO = MAX(1,FLOOR(XI))
+               IIO = MIN(MM%IBAR,MAX(1,FLOOR(XI)))
                ED%EDGE_INTERPOLATION_FACTOR(2) = XI-IIO
                ED%IIO_2 = IIO
                ED%JJO_2 = JJO
