18d86a3b5a624a0ad7e23b022ffd5968efa124f8
Jason Floyd <drjfloyd@gmail.com>
2026-08-18 11:52:52 -0400

    FDS Validation: Update NIST FSE and Juelich Setcom

--- Validation/Juelich_SETCOM/Run_All.sh
+++ Validation/Juelich_SETCOM/Run_All.sh
@@ -5,10 +5,10 @@
 export SVNROOT=`pwd`/../..
 source $SVNROOT/Validation/Common_Run_All.sh
 
-$QFDS $DEBUG -p 3 $QUEUE -d $INDIR setcom_0p8.fds
-$QFDS $DEBUG -p 3 $QUEUE -d $INDIR setcom_1p8.fds
-$QFDS $DEBUG -p 3 $QUEUE -d $INDIR setcom_3p7.fds
-$QFDS $DEBUG -p 3 $QUEUE -d $INDIR setcom_4p2.fds
-$QFDS $DEBUG -p 3 $QUEUE -d $INDIR setcom_5p2.fds
+$QFDS $DEBUG -p 9 $QUEUE -d $INDIR setcom_0p8.fds
+$QFDS $DEBUG -p 9 $QUEUE -d $INDIR setcom_1p8.fds
+$QFDS $DEBUG -p 9 $QUEUE -d $INDIR setcom_3p7.fds
+$QFDS $DEBUG -p 9 $QUEUE -d $INDIR setcom_4p2.fds
+$QFDS $DEBUG -p 9 $QUEUE -d $INDIR setcom_5p2.fds
 
 echo FDS cases submitted
